2086eee3a4a6ffecee4d7bd1e73c42eb5b7e60d9
HARSHA VARDHAN <hv1900019@gmail.com>
2024-09-05 21:29:39 +0530

    Time: 485 ms (5.05%), Space: 13 MB (72.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,13 +3,16 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int>p;
         int n=nums.size();
+        
         for(int i=0;i<n;i++){
-            for(int j=0;j<n&&i!=j;j++){
-                if(nums[i]+nums[j]==target){
+            for(int j=0;j<n;j++){
+                if(nums[i]+nums[j]==target&&i<j){
                     p.push_back(i);
                     p.push_back(j);
                 }
+                
             }
+    
         }
         return p;
     }
