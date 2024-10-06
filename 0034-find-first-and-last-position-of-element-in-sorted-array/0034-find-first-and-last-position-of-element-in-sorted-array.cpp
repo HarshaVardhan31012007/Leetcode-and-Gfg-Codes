b0380abeb2de1185eca815421d282843222a1faa
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-06 15:32:15 +0530

    Time: 0 ms (100%), Space: 17 MB (5.14%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,50 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-    vector<int>ans(2,-1);
+void binary_firstoccurence(vector<int>arr,int n,int target,int &ansIndex){
     int s=0;
-    int e=nums.size()-1;
+    int e=n-1;
     int mid=s+(e-s)/2;
     while(s<=e){
-        if(nums[mid]==target){
-            ans[0]=mid;
+        if(arr[mid]==target){
+            ansIndex=mid;
             e=mid-1;
         }
-        if(nums[mid]<target)
+        if(arr[mid]<target)
         s=mid+1;
-        if(nums[mid]>target)
+        if(arr[mid]>target)
         e=mid-1;
 
         mid=s+(e-s)/2;
 
     }
-     s=0;
-     e=nums.size()-1;
-     mid=s+(e-s)/2;
-     while(s<=e){
-        if(nums[mid]==target){
-            ans[1]=mid;
+}
+void binary_lastoccurence(vector<int>arr,int n,int target,int &ansIndex){
+    int s=0;
+    int e=n-1;
+    int mid=s+(e-s)/2;
+    while(s<=e){
+        if(arr[mid]==target){
+            ansIndex=mid;
             s=mid+1;
         }
-        if(nums[mid]<target)
+        if(arr[mid]<target)
         s=mid+1;
-        if(nums[mid]>target)
+        if(arr[mid]>target)
         e=mid-1;
 
         mid=s+(e-s)/2;
 
     }
-    return ans;
-
+}
+    vector<int> searchRange(vector<int>& arr, int target) {
+        int n=arr.size();
+        int ansIndex1=-1;
+        int ansIndex2=-1;
+        binary_firstoccurence(arr,n,target,ansIndex1);
+        binary_lastoccurence(arr,n,target,ansIndex2);
+        vector<int>temp(2);
+        temp[0]=ansIndex1;
+        temp[1]=ansIndex2;
+        return temp;
     }
 };
