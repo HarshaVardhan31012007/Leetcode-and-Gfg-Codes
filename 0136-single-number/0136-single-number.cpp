3356173d2f4ec07d33927b6ea7e656c6c416fa07
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-09 17:35:28 +0530

    Update 0136-single-number.cpp

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -9,14 +9,14 @@ public:
             int num=nums[i];
             freqmap[num]=freqmap[num]+1;
         }
-        
+        int ans;
         for(int i=0;i<nums.size();i++)
         {   int num=nums[i];
-            if(freqmap[num]==1)
-            return num;
+            if(freqmap[num]==1){
+                ans=num;
         }
 
-        return -1;
+        return ans;
 
     }
 
