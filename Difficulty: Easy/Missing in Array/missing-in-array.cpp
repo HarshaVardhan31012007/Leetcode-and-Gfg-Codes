396f8f150629b74c59bce2c760a8b2c035751b91
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-10 12:22:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.cpp
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -11,24 +11,13 @@ class Solution {
 
     int missingNumber(int n, vector<int>& arr) {
            sort(arr.begin(),arr.end());
-           int s=0;
-           int e=n-2;
-           int ansIndex=-1;
-           while(s<=e){
-           int mid=s+(e-s)/2;
-           int diff=arr[mid]-mid;
-           if(diff==1){
-               s=mid+1;
-               if(s>n-2)
-               ansIndex=n;
+           for(int i=0;i<n-1;i++){
+               if((i+1)==arr[i])
+               continue;
+               else 
+               return i+1;
            }
-           else if(diff>1){
-               ansIndex=mid+1;
-               e=mid-1;
-           }
-           mid=s+(e-s)/2;
-           }
-           return ansIndex;
+           return n;
     }
 };
 
