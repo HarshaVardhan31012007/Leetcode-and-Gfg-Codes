2e88a4624b1d659469bf5db380eadc23790c736a
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-10 22:01:33 +0530

    Time: 112 ms (5.08%), Space: 71.5 MB (5.25%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,16 +5,16 @@ public:
     }
     int maxSubArray(vector<int>& A, int L, int R){
         if(L > R) return INT_MIN;
-        int mid = (L + R) / 2, leftSum = 0, rightSum = 0;
-        // leftSum = max subarray sum in [L, mid-1] and starting from mid-1
-        for(int i = mid-1, curSum = 0; i >= L; i--)
-            curSum += A[i],
-            leftSum=max(leftSum, curSum);
-        // rightSum = max subarray sum in [mid+1, R] and starting from mid+1
-        for(int i = mid+1, curSum = 0; i <= R; i++)
-            curSum += A[i],
-            rightSum = max(rightSum, curSum);        
-		// return max of 3 cases 
+        int mid = (L + R) / 2, leftSum = 0, rightSum = 0;int sum1=0;
+        for(int i=mid-1;i>=L;i--){
+            sum1 += A[i];
+            leftSum=max(leftSum, sum1);
+            }
+            int sum2=0;
+        for(int i = mid+1; i <= R; i++){
+            sum2 += A[i];
+            rightSum = max(rightSum, sum2); 
+            }
         return max({maxSubArray(A, L, mid-1), maxSubArray(A, mid+1, R), leftSum + A[mid] + rightSum} );
     }	
 };
