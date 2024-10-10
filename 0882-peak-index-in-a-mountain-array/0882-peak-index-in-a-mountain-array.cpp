1d71e37084a7218751d908392497172357bbfe9f
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-10 15:33:04 +0530

    Update 0882-peak-index-in-a-mountain-array.cpp

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -4,8 +4,8 @@ public:
         int s=0;
         int n=arr.size();
         int e=n-1;
-        while(s<=e){
         int mid=s+(e-s)/2;
+        while(s<=e){
         if(mid!=0&&mid!=n-1&&arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1])
         return mid;
         if(mid!=0&&mid!=n-1&&arr[mid]>arr[mid-1]&&arr[mid]<arr[mid+1])
