449a90c90d5e297943e96787f40e780dc9815835
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-12 12:15:01 +0530

    Time: 0 ms (100%), Space: 8.3 MB (70.93%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans;
+           int s=0;
+           int e=x;
+           long long  mid=s+(e-s)/2;//lonhg lonhg int
+            while(s<=e){
+               //long long int product=mid*mid;
+                if(mid*mid==x)
+                return mid;
+
+                if(mid*mid<x){
+                ans=abs(mid);
+                s=mid+1;
+                }
+                
+                else
+                e=mid-1;
+
+                mid=s+(e-s)/2;
+            }
+        
+        return ans;
+    }
+};
