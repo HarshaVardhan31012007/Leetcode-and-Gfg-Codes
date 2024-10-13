a87fbd38fca4fd833c07f92600c34d399ee3b4e9
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-13 11:15:35 +0530

    Time: 0 ms (100%), Space: 10.3 MB (77.46%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,32 +1,6 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
-        int i=0;
-        int zc=0,oc=0,tc=0;
-        while(i<n){
-          if(nums[i]==0)
-          zc++;
-          else if(nums[i]==1)
-          oc++;
-          else if(nums[i]==2)
-          tc++;
-          i++;
-        }
-        i=0;
-        while(zc--){
-            nums[i]=0;
-            i++;
-        }
-       while(oc--){
-        nums[i]=1;
-        i++;
-       }
-       while(tc--){
-        nums[i]=2;
-        i++;
-       }
-    
-        
+        sort(nums.begin(),nums.end());
     }
 };
