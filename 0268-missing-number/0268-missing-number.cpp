d2644affa72fbeef578ad5f653a783b422cca1dc
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-22 07:24:25 +0530

    Update 0268-missing-number.cpp

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,12 +2,31 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(i==nums[i])
-            continue;
-            else
-            return i;
-        }
-        return nums.size();
+       // for(int i=0;i<nums.size();i++){
+       //     if(i==nums[i])
+       //     continue;
+        //    else
+       //     return i;
+       // }
+int n=nums.size();
+int ansIndex=n;
+int s=0;
+int e=n-1;
+while(s<=e){
+   int mid=s+(e-s)/2;
+   int diff=arr[mid]-mid;
+   if(diff==1){
+   ans=mid;
+   e=mid-1;
+}
+if(diff==0)
+s=mid+1;
+
+}
+  
+return ans;
+
+
+        //return nums.size();
     }
 };
