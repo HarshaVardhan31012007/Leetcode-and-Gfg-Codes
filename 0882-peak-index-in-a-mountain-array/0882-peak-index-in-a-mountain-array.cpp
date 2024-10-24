ea3ccaf9d6c6ec5bde60bcbd8cf4f6b720315ab5
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-24 14:54:53 +0530

    Time: 0 ms (100%), Space: 62.2 MB (95.56%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -9,11 +9,10 @@ public:
             int mid=s+(e-s)/2;
         if(arr[mid]<arr[mid+1])
         s=mid+1;
-        else if(arr[mid]<arr[mid-1])
-            e=mid-1;
         else{
           ans=mid;
-          break;}
+          e=mid-1;
+          }
         }
     return ans;
     }
