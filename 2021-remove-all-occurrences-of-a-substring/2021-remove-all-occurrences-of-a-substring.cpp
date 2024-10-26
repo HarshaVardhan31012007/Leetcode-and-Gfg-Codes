b3bee8332f33a1c244780774a8a47773c20511f6
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-26 14:25:03 +0530

    Time: 0 ms (100%), Space: 8.2 MB (23.52%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {//while(s.length()!=0&&s.find(part)<s.length())
-        // while(s.find(part)!=string::npos){
-        //     s.erase(s.find(part),part.length());
-        // }
-        // return s;
-        string x = s;
-	int n = s.size(), m = part.size(), i, j;
-	for (i = 0, j = 0; i < n; i++) {
-		x[j++] = s[i];
-		if (j >= m && x.substr(j - m, m) == part)
-			j -= m;
-	}
-	return x.substr(0, j);
+        while(s.find(part)<s.length()&&s.length()!=0){
+            s.erase(s.find(part),part.length());
+        }
+        return s;
+    //     string x = s;
+	// int n = s.size(), m = part.size(), i, j;
+	// for (i = 0, j = 0; i < n; i++) {
+	// 	x[j++] = s[i];
+	// 	if (j >= m && x.substr(j - m, m) == part)
+	// 		j -= m;
+	// }
+	// return x.substr(0, j);
     }
 };
