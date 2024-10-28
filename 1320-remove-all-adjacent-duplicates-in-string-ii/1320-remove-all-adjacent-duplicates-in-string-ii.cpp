7397803ba1c1d052f0929b1e6ce1b3294efda9e3
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-28 14:37:58 +0530

    Time: 3 ms (89.21%), Space: 12.3 MB (79.46%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool lastsame(string &ans, char&newch,int k){
-        int it=ans.size()-1;
-        for(int i=0;i<k-1;i++){
-          if(ans[it]!=newch)
-          return false;
-          it--;
-        }
-        return true;
-    }
+    // bool lastsame(string &ans, char&newch,int k){
+    //     int it=ans.size()-1;
+    //     for(int i=0;i<k-1;i++){
+    //       if(ans[it]!=newch)
+    //       return false;
+    //       it--;
+    //     }
+      //  return true;
+    //}
     string removeDuplicates(string s, int k) {
         
         //time limited exceeded
@@ -37,21 +37,37 @@ public:
        
 
        //m1
-       string ans="";
-       int n=s.size();
-       for(int i=0;i<n;i++){
-        char newch=s[i];
-        if(ans.size()<k-1)
-        ans.push_back(newch);
-        else{
-            if(lastsame(ans,newch,k)){
-                for(int i=0;i<k-1;i++)
-                ans.pop_back();
-            }
-            else
-            ans.push_back(newch);
-        }
-       }
-        return ans;
+    //    string ans="";
+    //    int n=s.size();
+    //    for(int i=0;i<n;i++){
+    //     char newch=s[i];
+    //     if(ans.size()<k-1)
+    //     ans.push_back(newch);
+    //     else{
+    //         if(lastsame(ans,newch,k)){
+    //             for(int i=0;i<k-1;i++)
+    //             ans.pop_back();
+    //         }
+    //         else
+    //         ans.push_back(newch);
+    //     }
+    //    }
+    //     return ans;
+    //m2
+    int i=0,j=0;
+    int n=s.size();
+    vector<int>count(n);
+    while(j<s.size()){
+        s[i]=s[j];
+        count[i]=1;
+        if(i>0&&s[i]==s[i-1])
+        count[i]+=count[i-1];
+        if(count[i]==k)
+        i=i-k;
+        i++;
+        j++;
+
+    }
+    return s.substr(0,i);
     }
 };
