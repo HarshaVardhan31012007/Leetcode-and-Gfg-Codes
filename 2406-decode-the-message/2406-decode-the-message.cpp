26e70a2a501975e71ad50a84378c9e2ca8ce3ee1
HARSHA VARDHAN <hv1900019@gmail.com>
2024-10-31 16:43:41 +0530

    Time: 3 ms (25.54%), Space: 10 MB (10.79%) - LeetHub

--- 2406-decode-the-message/2406-decode-the-message.cpp
+++ 2406-decode-the-message/2406-decode-the-message.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-    string decodeMessage(string key, string message) {
-        unordered_map<char,char>mpp;
+    void createmapping(unordered_map<char,char>&mpp,string &key){
         char start='a';
         int i=0;
-        char d=' ';
-        mpp[d]=d;
         while(start<='z'){//enough constraint 3 hence you no need of i<key.length()
             char c=key[i];
             if(mpp.find(c)!=mpp.end())
@@ -16,6 +13,8 @@ public:
                 i++;
             }
         }
+    }
+    string usemapping(string &message,unordered_map<char,char>mpp){
         string s="";
         for(int i=0;i<message.length();i++){
             //  int mc=message[i];
@@ -25,5 +24,39 @@ public:
              s.push_back(mppc);
         }
         return s;
+    }
+    string decodeMessage(string key, string message) {
+        // unordered_map<char,char>mpp;
+        // char start='a';
+        // int i=0;
+        // char d=' ';
+        // mpp[d]=d;
+        // while(start<='z'){//enough constraint 3 hence you no need of i<key.length()
+        //     char c=key[i];
+        //     if(mpp.find(c)!=mpp.end())
+        //     i++;
+        //     else{
+        //         mpp[c]=start;
+        //         start++;
+        //         i++;
+        //     }
+        // }
+        // string s="";
+        // for(int i=0;i<message.length();i++){
+        //     //  int mc=message[i];
+        //     //  int mppc=mpp[mc];
+        //      char mc=message[i];
+        //      char mppc=mpp[mc];
+        //      s.push_back(mppc);
+        // }
+        // return s;
+        unordered_map<char,char>mpp;
+        char d=' ';
+        mpp[d]=d;
+        createmapping(mpp,key);
+
+        string ans=usemapping(message,mpp);
+
+        return ans;
     }
 };
