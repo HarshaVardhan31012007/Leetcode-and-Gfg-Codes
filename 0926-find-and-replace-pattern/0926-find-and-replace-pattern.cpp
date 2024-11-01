4b3a7da0d240507d53c32df91ae7667ce0ca72e0
HARSHA VARDHAN <hv1900019@gmail.com>
2024-11-01 16:37:19 +0530

    Time: 3 ms (43.84%), Space: 11.4 MB (50.52%) - LeetHub

---
+++ 0926-find-and-replace-pattern/0926-find-and-replace-pattern.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void normalise(string &s){
+       unordered_map<char,char>mpp;
+       char start='a';
+       for(int i=0;i<s.size();i++){
+          char c=s[i];
+          if(mpp.find(c)==mpp.end()){
+            mpp[c]=start;
+            start++;
+          }
+       }
+
+       for(int i=0;i<s.size();i++){
+        char mappedcharacter=mpp[s[i]];
+        s[i]=mappedcharacter;
+
+       }
+    }
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        normalise(pattern);
+        vector<string>ans;
+        for(int i=0;i<words.size();i++){
+           string c=words[i];
+           string ccopy=c;
+           normalise(ccopy);
+           if(ccopy==pattern)
+           ans.push_back(c);
+        }
+        return ans;
+    }
+};
