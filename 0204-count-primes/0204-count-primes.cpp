60eb8841127f105e1a9e25e613147d1a2e3bfc25
HARSHA VARDHAN <hv1900019@gmail.com>
2024-11-01 18:38:59 +0530

    Time: 503 ms (38.8%), Space: 11.8 MB (39.78%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // bool isprime(int n){
+    //     if(n<=1)
+    //     return false;
+    //     for(int i=2;i<n;i++){//i<sqrt(n)
+    //         if(n%i==0)
+    //         return false;
+    //     }
+    //     return true;
+   // }
+    int countPrimes(int n) {
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     if(isprime(i))
+        //     count++;
+        // }
+        // return count;
+        if(n==0) return 0;
+        vector<bool>temp(n,true);
+        temp[0]=temp[1]=false;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(temp[i]){
+            ans++;
+            
+            int j=2*i;
+
+            while(j<n){
+                temp[j]=false;
+                j=j+i;
+            }
+
+            }
+
+        }
+        return ans;
+    }
+};
