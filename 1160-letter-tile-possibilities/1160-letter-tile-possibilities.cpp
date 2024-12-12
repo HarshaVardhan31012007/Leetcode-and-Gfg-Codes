122ce31ac6a05318150abd078bd3f0093b7fae34
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-12 10:11:52 +0530

    Time: 3 ms (78.87%), Space: 8.9 MB (59.85%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
-    int count=0;
-    void solve(string tiles,int i){
+    // int count=0;
+    // void solve(string tiles,int i){
+    // count++;
+    // if(i>=tiles.length())
+    // return;
+    // for(int j=i;j<tiles.length();j++){
+    //     if(i==j||tiles[i]!=tiles[j]){
+    //         swap(tiles[i],tiles[j]);
+    //         solve(tiles,i+1);
+    //     }
+    // }
+    // }
+    // int numTilePossibilities(string tiles) {
+    //     int i=0;
+    //     sort(tiles.begin(),tiles.end());
+    //     solve(tiles,i);
+    //     return count-1;//subtract empty set
+    // }
+
+    //method 2
+    void solve(string tiles,int i,int &count){
     count++;
     if(i>=tiles.length())
     return;
     for(int j=i;j<tiles.length();j++){
         if(i==j||tiles[i]!=tiles[j]){
             swap(tiles[i],tiles[j]);
-            solve(tiles,i+1);
+            solve(tiles,i+1,count);
         }
     }
     }
     int numTilePossibilities(string tiles) {
         int i=0;
+        int count=0;
         sort(tiles.begin(),tiles.end());
-        solve(tiles,i);
+        solve(tiles,i,count);
         return count-1;//subtract empty set
     }
 };
