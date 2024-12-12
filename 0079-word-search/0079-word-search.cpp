f0b0cd3804bfd964405e69e40087a6d5f7952c1d
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-12 22:04:57 +0530

    Time: 758 ms (42.66%), Space: 11.4 MB (53.42%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool solve(vector<vector<char>>& board, string word,int row,int col,int k){
+        //base case
+        if(k==word.size()){
+            return true;
+        }
+        if(row<0||col<0||row>=board.size()||col>=board[0].size()||board[row][col]!=word[k])
+        return false;//board[row][col]!='%' no need 
+       //up to this reached means word[i] match hua
+       //visited
+       char temp=board[row][col];
+       board[row][col]='%';
+       //recursive call
+       int dx[]={-1,1,0,0};
+       int dy[]={0,0,-1,1};
+       bool found=0;
+       for(int i=0;i<4;i++){
+        found=found||solve(board,word,row+dx[i],col+dy[i],k+1);
+       }
+       //unvisit
+       board[row][col]=temp;
+
+     return found;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int m=board.size();
+        int n=board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(solve(board,word,i,j,0))
+                return true;
+            }
+        }
+        return false;
+    }
+};
