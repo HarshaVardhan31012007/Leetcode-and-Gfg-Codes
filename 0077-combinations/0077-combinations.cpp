f5c59b847fc23e69c2cb4f2c78c1b5c0e3e0663f
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-12 19:23:43 +0530

    Time: 37 ms (86.29%), Space: 62.4 MB (63.5%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     void solve( vector<vector<int>>&ans,vector<int>&output,int i,int n,int k){
-         if(output.size()==k){
+        //  if(output.size()==k){
+        //     ans.push_back(output);
+        //     return;
+        //  }
+        if(k==0){
             ans.push_back(output);
             return;
-         }
+        }
          for(int j=i;j<=n;j++){
          output.push_back(j);
-         solve(ans,output,j+1,n,k);
+         //solve(ans,output,j+1,n,k);
+         solve(ans,output,j+1,n,k-1);
          output.pop_back();
          }
 
