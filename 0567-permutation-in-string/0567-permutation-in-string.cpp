d8b83c9772bc84c8788f8134063c18a29f0efb4a
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-12 12:46:44 +0530

    Time: 1610 ms (5.01%), Space: 422.3 MB (5.03%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 bool isSame(unordered_map<char,int>mpp,unordered_map<char,int>wfreq,string s1){
     for(int i=0;i<s1.length();i++){
-        if(mpp[s1[i]-'a']!=wfreq[s1[i]-'a'])
+        if(mpp[s1[i]]!=wfreq[s1[i]])//we can do with array of 26 charcters 
         return false;
     }
     return true;
@@ -11,13 +11,13 @@ bool isSame(unordered_map<char,int>mpp,unordered_map<char,int>wfreq,string s1){
         unordered_map<char,int>mpp;
         unordered_map<char,int>wfreq;
         for(int i=0;i<s1.length();i++){
-            mpp[s1[i]-'a']++;
+            mpp[s1[i]]++;
         }
         int wlen=s1.length();
         for(int i=0;i<s2.length();i++){
             int widx=0;int idx=i;
             while(widx<wlen&&idx<s2.length()){
-                wfreq[s2[idx]-'a']++;
+                wfreq[s2[idx]]++;
                 widx++;
                 idx++;
             }
