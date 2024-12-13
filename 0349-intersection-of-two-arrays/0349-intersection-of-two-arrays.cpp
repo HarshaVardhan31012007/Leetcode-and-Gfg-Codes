758cd0d9e2557c8821a20ee3b0bcee759d3804c7
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-13 20:34:32 +0530

    Time: 0 ms (100%), Space: 13.8 MB (48.83%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
+    // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    //     vector<int>ans;
+    //     for(int i=0;i<nums1.size();i++){
+    //         for(int j=0;j<nums2.size();j++){
+    //             if(nums1[i]==nums2[j]){
+    //                 ans.push_back(nums1[i]);
+    //             }
+    //         }
+    //     }
+    //    sort(ans.begin(),ans.end());
+    //     ans.erase(unique(ans.begin(),ans.end()),ans.end());
+
+    //     return ans;
+    // }
+
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int>ans;
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    ans.push_back(nums1[i]);
-                }
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int m=nums1.size();
+        int n=nums2.size();
+        int i=0;
+        int j=0;
+        while(i<m&&j<n){
+            if(nums1[i]==nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
             }
+            else if(nums1[i]<nums2[j])
+            i++;
+            else if(nums1[i]>nums2[j])
+            j++;
         }
-       sort(ans.begin(),ans.end());
         ans.erase(unique(ans.begin(),ans.end()),ans.end());
-
         return ans;
     }
 };
