ea60565ab09be7575bc23ac443ab0b0fa7a682ca
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-13 18:57:23 +0530

    Time: 11 ms (5.1%), Space: 13.9 MB (39.01%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -9,7 +9,7 @@ public:
                 }
             }
         }
-        sort(ans.begin(),ans.end());
+       sort(ans.begin(),ans.end());
         ans.erase(unique(ans.begin(),ans.end()),ans.end());
 
         return ans;
