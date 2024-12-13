b6a0223563810e9da3fcf8d8dc05ade15166628d
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-13 21:43:31 +0530

    Time: 0 ms (100%), Space: 14.2 MB (29.17%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -15,26 +15,43 @@ public:
     //     return ans;
     // }
 
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+
+    // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    //     vector<int>ans;
+    //     sort(nums1.begin(),nums1.end());
+    //     sort(nums2.begin(),nums2.end());
+    //     int m=nums1.size();
+    //     int n=nums2.size();
+    //     int i=0;
+    //     int j=0;
+    //     while(i<m&&j<n){
+    //         if(nums1[i]==nums2[j]){
+    //             ans.push_back(nums1[i]);
+    //             i++;
+    //             j++;
+    //         }
+    //         else if(nums1[i]<nums2[j])
+    //         i++;
+    //         else if(nums1[i]>nums2[j])
+    //         j++;
+    //     }
+    //     ans.erase(unique(ans.begin(),ans.end()),ans.end());
+    //     return ans;
+    // }
+        vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int>ans;
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        int m=nums1.size();
-        int n=nums2.size();
-        int i=0;
-        int j=0;
-        while(i<m&&j<n){
-            if(nums1[i]==nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i]<nums2[j])
-            i++;
-            else if(nums1[i]>nums2[j])
-            j++;
+        unordered_map<int,int>mpp;
+        for(int i=0;i<nums1.size();i++){
+            mpp[nums1[i]]++;
         }
-        ans.erase(unique(ans.begin(),ans.end()),ans.end());
+        for(int j=0;j<nums2.size();j++){
+        auto it=mpp.find(nums2[j]);
+        if(it!=mpp.end()&&it->second>0){
+            ans.push_back(nums2[j]);
+            mpp[nums2[j]]=0;
+        }
+        }
         return ans;
-    }
+        }
 };
