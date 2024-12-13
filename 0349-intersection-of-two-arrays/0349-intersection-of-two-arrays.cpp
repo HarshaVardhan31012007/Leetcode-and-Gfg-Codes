2485eef4c756101ac272a5eb038cc94989c361f8
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-13 22:19:22 +0530

    Time: 4 ms (22.49%), Space: 13.9 MB (39.01%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-    //     vector<int>ans;
-    //     for(int i=0;i<nums1.size();i++){
-    //         for(int j=0;j<nums2.size();j++){
-    //            if(nums[i]<nums[j]) break;
-    //             if(nums1[i]==nums2[j]){
-    //                 ans.push_back(nums1[i]);
-    //             }
-    //         }
-    //     }
-      // sort(ans.begin(),ans.end());
-       // ans.erase(unique(ans.begin(),ans.end()),ans.end());
-
-    //     return ans;
-    // }
-
-
-
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int>ans;
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        int m=nums1.size();
-        int n=nums2.size();
-        int i=0;
-        int j=0;
+         sort(nums1.begin(),nums1.end());
+         sort(nums2.begin(),nums2.end());
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+               if(nums1[i]<nums2[j]) break;
+                if(nums1[i]==nums2[j]){
+                    ans.push_back(nums1[i]);
+                }
+            }
+        }
+      sort(ans.begin(),ans.end());
+       ans.erase(unique(ans.begin(),ans.end()),ans.end());
+
+        return ans;
+    }
+
+
+
+    // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    //     vector<int>ans;
+    //     sort(nums1.begin(),nums1.end());
+    //     sort(nums2.begin(),nums2.end());
+    //     int m=nums1.size();
+    //     int n=nums2.size();
+    //     int i=0;
+    //     int j=0;
 
         //1
         // int las=-1;
@@ -47,30 +49,30 @@ public:
         //         j++;
         //     }
 
-        
-            while(i<m&&j<n){
+        //3 without using any function for avoiding duplicates
+          //  while(i<m&&j<n){
 
             // if((i>0)&&(nums1[i]==nums1[i-1])){//first only should be condition i>)
             //     i++;
             //     continue;
             // }
-            if((i+1<m)&&(nums1[i]==nums1[i+1])){
-                i++;
-                continue;
-            }
-            if(nums1[i]==nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i]<nums2[j])
-            i++;
-            else if(nums1[i]>nums2[j])
-            j++;
-        }
+        //     if((i+1<m)&&(nums1[i]==nums1[i+1])){
+        //         i++;
+        //         continue;
+        //     }
+        //     if(nums1[i]==nums2[j]){
+        //         ans.push_back(nums1[i]);
+        //         i++;
+        //         j++;
+        //     }
+        //     else if(nums1[i]<nums2[j])
+        //     i++;
+        //     else if(nums1[i]>nums2[j])
+        //     j++;
+        // }
     //for 2   // ans.erase(unique(ans.begin(),ans.end()),ans.end());//instead of this at starting point use set datastructure and convert into vector
-        return ans;
-    }
+    //     return ans;
+    // }
 
 
         // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
