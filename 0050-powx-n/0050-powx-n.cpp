261447e31d15706712ae0e86be9c396f00d06fec
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-14 20:20:49 +0530

    Time: 1900 ms (5.27%), Space: 8.7 MB (13.79%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     double myPow(double x, int n) {
         double product=1;
+        if(n==0) return 1.0;
+        if(x==0) return 0.0;
+        if(x==1.0) return 1.0;
+        if(x==-1.0&&n%2==0) return 1.0;
+        if(x==-1.0&&n%2!=0) return -1.0;
          if(n==INT_MIN){
-          if(x==1||x==-1)
-          return 1;
+          if(x==1.0||x==-1.0)
+          return 1.0;
           else
-          return 0;
+          return 0.0;
          }
-         if(n==INT_MAX&&(x==1||x==-1)){
+         if(n==INT_MAX&&(x==1.0||x==-1.0)){
             return x;
          }
         int N=abs(n);
-        if(x==1)
-        return 1;
+        if(x==1.0)
+        return 1.0;
         for(int i=0;i<N;i++){
         product=product*x;
         }
