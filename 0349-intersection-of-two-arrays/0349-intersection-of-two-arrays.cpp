d9f30a0657dcc098c8cef33c5120c63a8b48f82a
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-14 09:03:45 +0530

    Time: 1 ms (51.73%), Space: 13.7 MB (55.14%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -24,26 +24,27 @@ public:
 
 
 
-    // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-    //     vector<int>ans;
-    //     sort(nums1.begin(),nums1.end());
-    //     sort(nums2.begin(),nums2.end());
-    //     int m=nums1.size();
-    //     int n=nums2.size();
-    //     int i=0;
-    //     int j=0;
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int m=nums1.size();
+        int n=nums2.size();
+        int i=0;
+        int j=0;
 
         //1
         // int las=-1;
-        // while(i<m&&j<n){
-        //     if(nums1[i]==nums2[j]){
-        //         if(las!=nums1[i]){ or we can use ans.size>0&&ans.back()!=nums[i]
-        //         ans.push_back(nums1[i]);
-        //         las=nums1[i];
-        //         }
-        //         i++;
-        //         j++;
-        //     }
+        while(i<m&&j<n){
+            if(nums1[i]==nums2[j]){
+               // if(las!=nums1[i]){ 
+                if(ans.size()==0||ans.back()!=nums1[i]){
+                ans.push_back(nums1[i]);
+               // las=nums1[i];
+                }
+                i++;
+                j++;
+            }
 
         //2
         // while(i<m&&j<n){
@@ -69,30 +70,30 @@ public:
         //         i++;
         //         j++;
         //     }
-        //     else if(nums1[i]<nums2[j])
-        //     i++;
-        //     else if(nums1[i]>nums2[j])
-        //     j++;
-        // }
+            else if(nums1[i]<nums2[j])
+            i++;
+            else if(nums1[i]>nums2[j])
+            j++;
+        }
     //for 2   // ans.erase(unique(ans.begin(),ans.end()),ans.end());//instead of this at starting point use set datastructure and convert into vector
-    //     return ans;
-    // }
-
-
-        vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
-        unordered_map<int,int>mpp;
-        for(int i=0;i<nums1.size();i++){
-            mpp[nums1[i]]++;
-        }
-        for(int j=0;j<nums2.size();j++){
-        auto it=mpp.find(nums2[j]);
-        if(it!=mpp.end()&&it->second>0){
-            ans.push_back(nums2[j]);
-           // mpp[nums2[j]]=0;
-           mpp.erase(nums2[j]);
-        }
-        }
         return ans;
-        }
+    }
+
+
+        // vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        // vector<int>ans;
+        // unordered_map<int,int>mpp;
+        // for(int i=0;i<nums1.size();i++){
+        //     mpp[nums1[i]]++;
+        // }
+        // for(int j=0;j<nums2.size();j++){
+        // auto it=mpp.find(nums2[j]);
+        // if(it!=mpp.end()&&it->second>0){
+        //     ans.push_back(nums2[j]);
+        //    // mpp[nums2[j]]=0;
+        //    mpp.erase(nums2[j]);
+        // }
+        // }
+        // return ans;
+        // }
 };
