efab36affed785fb61cb0fbd21ea988f704ccf6a
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-14 12:27:14 +0530

    Time: 1 ms (45.46%), Space: 28.2 MB (12.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -32,7 +32,7 @@ public:
 
         int n=nums.size()/2;
         sort(nums.begin(),nums.end());;
-        int count=1;int ans=nums[0];
+        int count=1;int ans=-1;
         for(int i=0;i<nums.size();i++){
             if(i+1<nums.size()&&nums[i]==nums[i+1]){
                 count++;
