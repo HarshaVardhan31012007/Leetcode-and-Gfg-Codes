49f645ef67bb5878f751cf8a1451050a92c72e9e
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-14 21:56:47 +0530

    Time: 0 ms (100%), Space: 97.5 MB (10.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -26,14 +26,16 @@ public:
        
         //method 2
         int n=prices.size();
-        int maxi=0;
+        int maxi=INT_MIN;
         int bestbuy=prices[0];
         for(int i=0;i<n;i++){
-            if(prices[i]>bestbuy){
+            if(prices[i]>=bestbuy){
                 maxi=max(maxi,prices[i]-bestbuy);
             }
             bestbuy=min(bestbuy,prices[i]);
         }
         return maxi;
+
+        //method 3
     }
 };
