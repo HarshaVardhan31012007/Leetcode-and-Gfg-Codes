46e0865ec64abff426f26b47207490ac544bd91f
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-15 11:30:32 +0530

    Time: 4 ms (64.49%), Space: 16.1 MB (5.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -17,39 +17,55 @@ public:
         // return p;
 
         //method 2// if true or false varinat then this is better otherwise space is more
+        // vector<int>ans;
+        // multimap<int,int>mm;
+        // for(int i=0;i<nums.size();i++){
+        //     mm.insert({nums[i],i});
+        // }
+        // sort(nums.begin(),nums.end());
+        // int i=0;
+        // int j=nums.size()-1;
+        // while(i<j){
+        //     int sum=nums[i]+nums[j];
+        //     if(sum==target){
+        //          auto it = mm.begin();
+        //          int count=0;
+        //  while (it != mm.end()) {
+        //    int key = it->first;
+        //    if(it->first==nums[i]){
+        //     ans.push_back(it->second);
+        //     count++;
+        //    }
+        //     else if(it->first==nums[j]){
+        //     ans.push_back(it->second);
+        //     count++;
+        //    }
+        //    if(count==2)
+        //    break;
+        //    it++;
+        //   }
+        //        return ans;
+        //     }
+        //     else if(sum<target)
+        //     i++;
+        //     else if(sum>target)
+        //     j--;
+        // }
+        // return ans;
+
+        //method 3
         vector<int>ans;
-        multimap<int,int>mm;
-        for(int i=0;i<nums.size();i++){
-            mm.insert({nums[i],i});
-        }
-        sort(nums.begin(),nums.end());
-        int i=0;
-        int j=nums.size()-1;
-        while(i<j){
-            int sum=nums[i]+nums[j];
-            if(sum==target){
-                 auto it = mm.begin();
-                 int count=0;
-         while (it != mm.end()) {
-           int key = it->first;
-           if(it->first==nums[i]){
-            ans.push_back(it->second);
-            count++;
-           }
-            else if(it->first==nums[j]){
-            ans.push_back(it->second);
-            count++;
-           }
-           if(count==2)
-           break;
-           it++;
-          }
-               return ans;
+        int n=nums.size();
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            int num=nums[i];
+            int second=target-num;
+            if(mpp.find(second)!=mpp.end()){
+                ans.push_back(i);
+                ans.push_back(mpp[second]);
             }
-            else if(sum<target)
-            i++;
-            else if(sum>target)
-            j--;
+            mpp[num]=i;
+        
         }
         return ans;
     }
