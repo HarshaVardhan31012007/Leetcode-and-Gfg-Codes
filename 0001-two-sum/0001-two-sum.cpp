458f0f0a3a8ef124b4a78de58a9e8d682341dde1
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-15 11:17:25 +0530

    Time: 11 ms (47.96%), Space: 16.4 MB (5.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,57 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>p;
-        int n=nums.size();
+        // vector<int>p;
+        // int n=nums.size();
         
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    p.push_back(i);
-                    p.push_back(j);
-                }
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             p.push_back(i);
+        //             p.push_back(j);
+        //         }
                 
-            }
+        //     }
     
+        // }
+        // return p;
+
+        //method 2// if true or false varinat then this is better otherwise space is more
+        vector<int>ans;
+        multimap<int,int>mm;
+        for(int i=0;i<nums.size();i++){
+            mm.insert({nums[i],i});
         }
-        return p;
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=nums.size()-1;
+        while(i<j){
+            int sum=nums[i]+nums[j];
+            if(sum==target){
+                 auto it = mm.begin();
+                 int count=0;
+         while (it != mm.end()) {
+           int key = it->first;
+           if(it->first==nums[i]){
+            ans.push_back(it->second);
+            count++;
+           }
+            else if(it->first==nums[j]){
+            ans.push_back(it->second);
+            count++;
+           }
+           if(count==2)
+           break;
+           it++;
+          }
+               return ans;
+            }
+            else if(sum<target)
+            i++;
+            else if(sum>target)
+            j--;
+        }
+        return ans;
     }
 
 };
