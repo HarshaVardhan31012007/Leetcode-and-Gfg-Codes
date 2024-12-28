99a13bf83b6ca598d146c70a8b55f7af7f62ee3a
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-28 23:00:05 +0530

    Time: 0 ms (100%), Space: 13.6 MB (5.43%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     // ListNode* solve(ListNode* &prev,ListNode* &curr){
     //     if(curr==NULL){
+    //     // one way if use void
+    //     //   head=prev;
+    //     //    second way if use ListNode*
     //         return prev;
     //     }
     //     ListNode* forward=curr->next;
@@ -9,21 +12,31 @@ public:
     //     prev=curr;
     //     curr=forward;
     //     return solve(prev,curr);
+    // //or return solve(curr,forward);
     // }
     ListNode* reverseList(ListNode* head) {
         // ListNode* prev=NULL;
         // ListNode* curr=head;
         // ListNode* reverseLinkedlist=solve(prev,curr);
         // return reverseLinkedlist;
-
-        ListNode *prev=NULL;
-        ListNode *curr=head;
-        while(curr!=NULL){
-            ListNode* forward=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=forward;
+        // ListNode *prev=NULL;
+        // ListNode *curr=head;
+        // while(curr!=NULL){
+        //     ListNode* forward=curr->next;
+        //     curr->next=prev;
+        //     prev=curr;
+        //     curr=forward;
+        // }
+        // return prev;
+        //method 3
+        if(head==NULL||head->next==NULL){
+            return head;
         }
-        return prev;
+        ListNode *chotahead=reverseList(head->next);
+
+        head->next->next=head;
+        head->next=NULL;
+
+        return chotahead;
     }
 };
