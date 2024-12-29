bd073505c5c688d217229405ec153cf0592808ca
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-29 12:45:04 +0530

    Time: 3 ms (98.23%), Space: 11.3 MB (36.83%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,16 +8,41 @@
  */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
-         unordered_map<ListNode*,bool>mpp;
-         ListNode *temp=head;
-        while(temp!=NULL){
-            if(mpp[temp]==true){
-                return temp;
+ListNode *hasCycle(ListNode *head){
+     ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast!=NULL){
+           fast=fast->next;
+           if(fast!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+            if(fast==slow){
+                return slow;
             }
-            mpp[temp]=true;
-            temp=temp->next;
+           }
         }
-        return nullptr; 
+        return nullptr;
+}
+    ListNode *detectCycle(ListNode *head) {
+        //  unordered_map<ListNode*,bool>mpp;
+        //  ListNode *temp=head;
+        // while(temp!=NULL){
+        //     if(mpp[temp]==true){
+        //         return temp;
+        //     }
+        //     mpp[temp]=true;
+        //     temp=temp->next;
+        // }
+        // return nullptr; 
+
+        ListNode *fast=hasCycle(head);
+        if(!fast)
+        return NULL;
+        ListNode *slow=head;
+        while(slow!=fast){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        return slow;
     }
 };
