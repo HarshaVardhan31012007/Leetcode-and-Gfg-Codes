8229bbb5ee846ea873f1b4756efea0bff1505a26
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-29 11:37:00 +0530

    Time: 15 ms (7.76%), Space: 77.1 MB (69.64%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,18 +10,31 @@
  */
 class Solution {
 public:
-    void insertatTail(ListNode *&ansHead,ListNode *&ansTail,int digit){
-        if(ansHead==NULL&&ansHead==NULL){
-            ListNode *newNode=new ListNode(digit);
-            ansHead=newNode;
-            ansTail=newNode;
-        }
-        else{
-            ListNode *newNode=new ListNode(digit);
-            ansTail->next=newNode;
-            ansTail=newNode;
-        }
-    }
+     ListNode *recursive(ListNode* l1,ListNode *l2,int carry){
+        if(!l1&&!l2&&!carry)
+        return 0;//return NULL
+
+            int a=l1?l1->val:0;
+            int b=l2?l2->val:0;
+            int sum=a+b+carry;
+            int digit=sum%10;
+            carry=sum/10;
+            ListNode *ans=new ListNode(digit);
+            ans->next=recursive(l1?l1->next:l1,l2?l2->next:l2,carry);
+            return ans;
+     }
+    // void insertatTail(ListNode *&ansHead,ListNode *&ansTail,int digit){
+    //     if(ansHead==NULL&&ansHead==NULL){
+    //         ListNode *newNode=new ListNode(digit);
+    //         ansHead=newNode;
+    //         ansTail=newNode;
+    //     }
+    //     else{
+    //         ListNode *newNode=new ListNode(digit);
+    //         ansTail->next=newNode;
+    //         ansTail=newNode;
+    //     }
+    // }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         //method 1//if you sum in given nodes only no extra space 
         // int carry=0;
@@ -68,36 +81,37 @@ public:
         // return (ans==0)?l1:l2;
        
        //with using extraspace
-       int carry=0;
-        ListNode *ansHead=NULL;
-        ListNode *ansTail=NULL;
-        while(l1!=NULL&&l2!=NULL){
-            int sum=l1->val+l2->val+carry;
-            int digit=sum%10;
-            carry=sum/10;
-            insertatTail(ansHead,ansTail,digit);
-            l1=l1->next;
-            l2=l2->next;
-        }
-         while(l2!=NULL){
-            int sum=l2->val+carry;
-            int digit=sum%10;
-            carry=sum/10;
-            insertatTail(ansHead,ansTail,digit);
-            l2=l2->next;
-        }
-        while(l1!=NULL){
-            int sum=l1->val+carry;
-            int digit=sum%10;
-            carry=sum/10;
-            insertatTail(ansHead,ansTail,digit);
-            l1=l1->next;
-        }
-        if(carry){
-            insertatTail(ansHead,ansTail,carry);
-        }
-        return ansHead;
-       //method 2
+    //    int carry=0;
+    //     ListNode *ansHead=NULL;
+    //     ListNode *ansTail=NULL;
+    //     while(l1!=NULL&&l2!=NULL){
+    //         int sum=l1->val+l2->val+carry;
+    //         int digit=sum%10;
+    //         carry=sum/10;
+    //         insertatTail(ansHead,ansTail,digit);
+    //         l1=l1->next;
+    //         l2=l2->next;
+    //     }
+    //      while(l2!=NULL){
+    //         int sum=l2->val+carry;
+    //         int digit=sum%10;
+    //         carry=sum/10;
+    //         insertatTail(ansHead,ansTail,digit);
+    //         l2=l2->next;
+    //     }
+    //     while(l1!=NULL){
+    //         int sum=l1->val+carry;
+    //         int digit=sum%10;
+    //         carry=sum/10;
+    //         insertatTail(ansHead,ansTail,digit);
+    //         l1=l1->next;
+    //     }
+    //     if(carry){
+    //         insertatTail(ansHead,ansTail,carry);
+    //     }
+    //     return ansHead;
+
+       //method 2//simplifying code with extraspace//iterative
         // int carry=0;
         // ListNode *ans=new ListNode(-1);
         // ListNode *it=ans;
@@ -117,5 +131,8 @@ public:
         // ListNode *finalAns=ans->next;
         // delete ans;
         // return finalAns;
+
+        //simple code//recursive
+        return recursive(l1,l2,0);
     }
 };
