ff7de4aba42caf73a4d139a577f1194d7d51ebb3
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-29 10:58:42 +0530

    Time: 1 ms (45.34%), Space: 77.2 MB (54.28%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,83 +11,69 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        //method 1
+        // int carry=0;
+        // ListNode *temp1=l1;
+        // ListNode *temp2=l2;
+        // while(temp1!=NULL&&temp2!=NULL){
+        //     int sum=temp1->val+temp2->val+carry;
+        //     temp1->val=sum%10;
+        //     temp2->val=sum%10;
+        //     carry=sum/10;
+        //     if(temp1->next==NULL&&temp2->next==NULL&&carry!=0){
+        //     ListNode *newNode=new ListNode(carry);
+        //     temp1->next=newNode;
+        //     temp1=newNode;
+        //    }
+        //    temp1=temp1->next;
+        //    temp2=temp2->next;
+        // }
+        // int ans=0;
+        //  while(temp1==NULL&&temp2!=NULL){
+        //     int sum=temp2->val+carry;
+        //     temp2->val=sum%10;
+        //     carry=sum/10;
+        //     if(temp2->next==NULL&&carry!=0){
+        //     ListNode *newNode=new ListNode(carry);
+        //     temp2->next=newNode;
+        //     temp2=newNode;
+        //    }
+        //    temp2=temp2->next;
+        //    ans=1;
+        // }
+        // while(temp1!=NULL&&temp2==NULL){
+        //     int sum=temp1->val+carry;
+        //     temp1->val=sum%10;
+        //     carry=sum/10;
+        //      if(temp1->next==NULL&&carry!=0){
+        //     ListNode *newNode=new ListNode(carry);
+        //     temp1->next=newNode;
+        //     temp1=newNode;
+        //    }
+        //    temp1=temp1->next;
+        //    ans=0;
+        // }
+        // return (ans==0)?l1:l2;
+       
+
+       //method 2
         int carry=0;
+        ListNode *ans=new ListNode(-1);
+        ListNode *it=ans;
         ListNode *temp1=l1;
         ListNode *temp2=l2;
-        while(temp1!=NULL&&temp2!=NULL){
-            int sum=temp1->val+temp2->val+carry;
-            temp1->val=sum%10;
-            temp2->val=sum%10;
+        while(temp1||temp2||carry){
+            int a=temp1?temp1->val:0;
+            int b=temp2?temp2->val:0;
+            int sum=a+b+carry;
+            int digit=sum%10;
             carry=sum/10;
-            if(temp1->next==NULL&&temp2->next==NULL&&carry!=0){
-            ListNode *newNode=new ListNode(carry);
-            temp1->next=newNode;
-            temp1=newNode;
-           }
-           temp1=temp1->next;
-           temp2=temp2->next;
+            it->next=new ListNode(digit);
+            it=it->next;
+           temp1=temp1?temp1->next:nullptr;
+           temp2=temp2?temp2->next:nullptr;
         }
-        int ans=0;
-         while(temp1==NULL&&temp2!=NULL){
-            int sum=temp2->val+carry;
-            temp2->val=sum%10;
-            carry=sum/10;
-            if(temp2->next==NULL&&carry!=0){
-            ListNode *newNode=new ListNode(carry);
-            temp2->next=newNode;
-            temp2=newNode;
-           }
-           temp2=temp2->next;
-           ans=1;
-        }
-        while(temp1!=NULL&&temp2==NULL){
-            int sum=temp1->val+carry;
-            temp1->val=sum%10;
-            carry=sum/10;
-             if(temp1->next==NULL&&carry!=0){
-            ListNode *newNode=new ListNode(carry);
-            temp1->next=newNode;
-            temp1=newNode;
-           }
-           temp1=temp1->next;
-           ans=0;
-        }
-        return (ans==0)?l1:l2;
-        // bool ans=0;
-        // if(l2==NULL&&l1!=NULL){
-        //  while(l1!=NULL){
-        //     int sum=l1->val+carry;
-        //     l1->val=sum%10;
-        //     carry=sum/10;
-        //     l1=l1->next;
-        // }
-        // ans=1;
-        // }
-        // else if(l1==NULL&&l2!=NULL){
-        //    while(l2!=NULL){
-        //     int sum=l2->val+carry;
-        //     l2->val=sum%10;
-        //     carry=sum/10;
-        //     l2=l2->next;
-        // }
-        // ans=0;
-        // }
-        // if(carry&&ans==1){
-        //  ListNode *newNode=new ListNode(carry);
-        //  newNode->next=l1;
-        // }
-        //  if(carry&&ans==0){
-        //  LnewNode=new ListNode(carry);
-        //  newNode->next=l2;
-        // }
-        // if(ans==0){
-        //     ListNode *prev=NULL;
-        //     reverselist(prev,l2);
-        // }
-        // else{
-        //     ListNode *prev=NULL;
-        //     reverselist(prev,l1);
-        // }
-        // return newNode;
+        ListNode *finalAns=ans->next;
+        return finalAns;
     }
 };
