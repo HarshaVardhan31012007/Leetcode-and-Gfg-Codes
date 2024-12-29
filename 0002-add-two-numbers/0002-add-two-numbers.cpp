9588d7a149e5e7b355019fdbfd916b3290e08d52
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-29 11:14:33 +0530

    Time: 0 ms (100%), Space: 77.2 MB (54.28%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,8 +10,20 @@
  */
 class Solution {
 public:
+    void insertatTail(ListNode *&ansHead,ListNode *&ansTail,int digit){
+        if(ansHead==NULL&&ansHead==NULL){
+            ListNode *newNode=new ListNode(digit);
+            ansHead=newNode;
+            ansTail=newNode;
+        }
+        else{
+            ListNode *newNode=new ListNode(digit);
+            ansTail->next=newNode;
+            ansTail=newNode;
+        }
+    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        //method 1
+        //method 1//if you sum in given nodes only no extra space 
         // int carry=0;
         // ListNode *temp1=l1;
         // ListNode *temp2=l2;
@@ -29,7 +41,7 @@ public:
         //    temp2=temp2->next;
         // }
         // int ans=0;
-        //  while(temp1==NULL&&temp2!=NULL){
+        //  while(temp2!=NULL){
         //     int sum=temp2->val+carry;
         //     temp2->val=sum%10;
         //     carry=sum/10;
@@ -41,7 +53,7 @@ public:
         //    temp2=temp2->next;
         //    ans=1;
         // }
-        // while(temp1!=NULL&&temp2==NULL){
+        // while(temp1!=NULL){
         //     int sum=temp1->val+carry;
         //     temp1->val=sum%10;
         //     carry=sum/10;
@@ -55,26 +67,55 @@ public:
         // }
         // return (ans==0)?l1:l2;
        
-
-       //method 2
-        int carry=0;
-        ListNode *ans=new ListNode(-1);
-        ListNode *it=ans;
-        ListNode *temp1=l1;
-        ListNode *temp2=l2;
-        while(temp1||temp2||carry){
-            int a=temp1?temp1->val:0;
-            int b=temp2?temp2->val:0;
-            int sum=a+b+carry;
+       //with using extraspace
+       int carry=0;
+        ListNode *ansHead=NULL;
+        ListNode *ansTail=NULL;
+        while(l1!=NULL&&l2!=NULL){
+            int sum=l1->val+l2->val+carry;
             int digit=sum%10;
             carry=sum/10;
-            it->next=new ListNode(digit);
-            it=it->next;
-           temp1=temp1?temp1->next:nullptr;
-           temp2=temp2?temp2->next:nullptr;
+            insertatTail(ansHead,ansTail,digit);
+            l1=l1->next;
+            l2=l2->next;
         }
-        ListNode *finalAns=ans->next;
-        delete ans;
-        return finalAns;
+         while(l2!=NULL){
+            int sum=l2->val+carry;
+            int digit=sum%10;
+            carry=sum/10;
+            insertatTail(ansHead,ansTail,digit);
+            l2=l2->next;
+        }
+        while(l1!=NULL){
+            int sum=l1->val+carry;
+            int digit=sum%10;
+            carry=sum/10;
+            insertatTail(ansHead,ansTail,digit);
+            l1=l1->next;
+        }
+        if(carry){
+            insertatTail(ansHead,ansTail,carry);
+        }
+        return ansHead;
+       //method 2
+        // int carry=0;
+        // ListNode *ans=new ListNode(-1);
+        // ListNode *it=ans;
+        // ListNode *temp1=l1;
+        // ListNode *temp2=l2;
+        // while(temp1||temp2||carry){
+        //     int a=temp1?temp1->val:0;
+        //     int b=temp2?temp2->val:0;
+        //     int sum=a+b+carry;
+        //     int digit=sum%10;
+        //     carry=sum/10;
+        //     it->next=new ListNode(digit);
+        //     it=it->next;
+        //    temp1=temp1?temp1->next:nullptr;
+        //    temp2=temp2?temp2->next:nullptr;
+        // }
+        // ListNode *finalAns=ans->next;
+        // delete ans;
+        // return finalAns;
     }
 };
