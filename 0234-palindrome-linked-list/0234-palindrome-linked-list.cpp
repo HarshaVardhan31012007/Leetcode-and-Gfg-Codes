e04e943026d450f58e233f1d8fb48d74c649ff99
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-29 08:36:36 +0530

    Time: 4 ms (53.87%), Space: 132.1 MB (7.9%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,53 +10,73 @@
  */
 class Solution {
 public:
-    ListNode *findmiddle(ListNode*&middlekaprev,ListNode *head){
-    ListNode *slow=head;
-    ListNode *fast=head;
-    while(fast!=NULL){
-        fast=fast->next;
-        if(fast!=NULL){
-            fast=fast->next;
-            middlekaprev=slow;
-            slow=slow->next;
+    // ListNode *findmiddle(ListNode*&middlekaprev,ListNode *head){
+    // ListNode *slow=head;
+    // ListNode *fast=head;
+    // while(fast!=NULL){
+    //     fast=fast->next;
+    //     if(fast!=NULL){
+    //         fast=fast->next;
+    //         middlekaprev=slow;
+    //         slow=slow->next;
+    //     }
+    // }
+    //    return slow;
+    // }
+    // ListNode *reverselist(ListNode *prev,ListNode *curr){
+    //    while(curr!=NULL){
+    //     ListNode* forward=curr->next;
+    //     curr->next=prev;
+    //     prev=curr;
+    //     curr=forward;
+    //    }
+    //    return prev;
+    // }
+
+    bool checkpalindrome(vector<int>&ans){
+        int i=0;
+        int j=ans.size()-1;
+        while(i<j){
+            if(ans[i]!=ans[j])
+            return false;
+            i++;
+            j--;
         }
-    }
-       return slow;
-    }
-    ListNode *reverselist(ListNode *prev,ListNode *curr){
-       while(curr!=NULL){
-        ListNode* forward=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=forward;
-       }
-       return prev;
+        return true;
     }
     bool isPalindrome(ListNode* head) {
-        //empty list
-        if(head==NULL)
-        return true;
-        //single node
-        if(head->next==NULL)
-        return true;
-        //find middle and prev
-        ListNode *middlekaprev=NULL;
-        ListNode *middle=findmiddle(middlekaprev,head);
-        //break;
-        middlekaprev->next=NULL;
-        ListNode* prev=NULL;
-        ListNode *curr=middle;
-        ListNode* temphead2=reverselist(prev,curr);
-        ListNode*temphead1=head;
-        while(temphead1!=NULL){
-            if(temphead1->val!=temphead2->val){
-                return false;
-            }
-            else{
-                temphead1=temphead1->next;
-                temphead2=temphead2->next;
-            }
+        vector<int>ans;
+        ListNode *temp=head;
+        while(temp!=NULL){
+            ans.push_back(temp->val);
+            temp=temp->next;
         }
-        return true;
+        return checkpalindrome(ans);
+        //method 2
+        // //empty list
+        // if(head==NULL)
+        // return true;
+        // //single node
+        // if(head->next==NULL)
+        // return true;
+        // //find middle and prev
+        // ListNode *middlekaprev=NULL;
+        // ListNode *middle=findmiddle(middlekaprev,head);
+        // //break;
+        // middlekaprev->next=NULL;
+        // ListNode* prev=NULL;
+        // ListNode *curr=middle;
+        // ListNode* temphead2=reverselist(prev,curr);
+        // ListNode*temphead1=head;
+        // while(temphead1!=NULL){
+        //     if(temphead1->val!=temphead2->val){
+        //         return false;
+        //     }
+        //     else{
+        //         temphead1=temphead1->next;
+        //         temphead2=temphead2->next;
+        //     }
+        // }
+        // return true;
     }
 };
