920fca67c060fab024baaf1c03fdbe7b3599b134
HARSHA VARDHAN <hv1900019@gmail.com>
2024-12-31 06:42:29 +0530

    Time: 3 ms (91.66%), Space: 15.4 MB (9.21%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,33 +16,48 @@ public:
 
 class Solution {
 public:
+    Node *solve(Node* head,unordered_map<Node*,Node*>&mpp){
+        if(head==NULL)
+        return NULL;
+        Node*newHead=new Node(head->val);
+        mpp[head]=newHead;
+        newHead->next=solve(head->next,mpp);
+        if(head->random){
+            newHead->random=mpp[head->random];
+        }//else case we no need to go map me by default zeros hota hai
+        return newHead;
+    }
     Node* copyRandomList(Node* head) {
         //method 1
-        Node *temp=head;
-        Node *ansHead=NULL;
-        Node *ansTail=NULL;
+        // Node *temp=head;
+        // Node *ansHead=NULL;
+        // Node *ansTail=NULL;
+        // unordered_map<Node*,Node*>mpp;
+        // while(temp!=NULL){
+        //    Node *newNode=new Node(temp->val);
+        //    if(!ansTail&&!ansHead){
+        //     ansHead=newNode;
+        //     ansTail=newNode;
+        //    } 
+        //    else{
+        //     ansTail->next=newNode;
+        //     ansTail=newNode;
+        //    }
+        //    mpp[temp]=newNode;
+        //    temp=temp->next;
+        // }
+        // temp=head;
+        // while(temp!=NULL){
+        //     if(temp->random)
+        //     mpp[temp]->random=mpp[temp->random];
+        //     else
+        //     mpp[temp]->random=nullptr;
+        //     temp=temp->next;
+        // }
+        // return ansHead;
+        
+        //method 2
         unordered_map<Node*,Node*>mpp;
-        while(temp!=NULL){
-           Node *newNode=new Node(temp->val);
-           if(!ansTail&&!ansHead){
-            ansHead=newNode;
-            ansTail=newNode;
-           } 
-           else{
-            ansTail->next=newNode;
-            ansTail=newNode;
-           }
-           mpp[temp]=newNode;
-           temp=temp->next;
-        }
-        temp=head;
-        while(temp!=NULL){
-            if(temp->random)
-            mpp[temp]->random=mpp[temp->random];
-            else
-            mpp[temp]->random=nullptr;
-            temp=temp->next;
-        }
-        return ansHead;
+        return solve(head,mpp);
     }
 };
