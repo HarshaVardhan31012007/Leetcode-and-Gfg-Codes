f718b45d6dc9e6a355d5f903982828ec0e462564
HARSHA VARDHAN <hv1900019@gmail.com>
2025-01-04 15:47:24 +0530

    Time: 10 ms (35.8%), Space: 16.1 MB (9.58%) - LeetHub

---
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -0,0 +1,94 @@
+class Solution {
+public:
+    auto findprevsmaller(vector<int>&arr){
+        vector<int>prev;
+        stack<int>st;
+        st.push(-1);
+        for(int i=0;i<arr.size();i++){
+            int element=arr[i];
+            while(st.top()!=-1&&arr[st.top()]>element){//equality hona if next no equality
+               st.pop();
+            }
+            prev.push_back(st.top());
+            st.push(i);
+        }
+        return prev;
+    }
+    auto findnextsmaller(vector<int>&arr){
+        //vector<int>next(arr.size(),-1);
+        vector<int>next;
+        stack<int>st;
+        st.push(-1);
+        for(int i=arr.size()-1;i>=0;i--){
+            int element=arr[i];
+            while(st.top()!=-1&&arr[st.top()]>=element){//no equality if prev has eqaulity
+               st.pop();
+            }
+            next.push_back(st.top());
+            st.push(i);
+        }
+        return next;
+    }
+    long long int sumSubarrayMins(vector<int>& arr) {
+        auto prev=findprevsmaller(arr);
+        auto next=findnextsmaller(arr);
+        //another way
+        reverse(next.begin(),next.end());
+        long long sum=0;
+        for(int i=0;i<arr.size();i++){
+            next[i]=next[i]==-1?arr.size():next[i];
+            long int plen=i-prev[i];
+            long int nlen=next[i]-i;
+           sum=sum+(plen*nlen)*arr[i];
+        }
+        return sum;
+    }
+     auto findprevsmaller1(vector<int>&arr){
+        vector<int>prev;
+        stack<int>st;
+        st.push(-1);
+        for(int i=0;i<arr.size();i++){
+            int element=arr[i];
+            while(st.top()!=-1&&arr[st.top()]<element){//equality hona if next no equality
+               st.pop();
+            }
+            prev.push_back(st.top());
+            st.push(i);
+        }
+        return prev;
+    }
+    auto findnextsmaller1(vector<int>&arr){
+        //vector<int>next(arr.size(),-1);
+        vector<int>next;
+        stack<int>st;
+        st.push(-1);
+        for(int i=arr.size()-1;i>=0;i--){
+            int element=arr[i];
+            while(st.top()!=-1&&arr[st.top()]<=element){//no equality if prev has eqaulity
+               st.pop();
+            }
+            next.push_back(st.top());
+            st.push(i);
+        }
+        return next;
+    }
+    long long int sumSubarrayMaxs(vector<int>& arr) {
+        auto prev=findprevsmaller1(arr);
+        auto next=findnextsmaller1(arr);
+        //another way
+        reverse(next.begin(),next.end());
+        long long sum=0;
+        for(int i=0;i<arr.size();i++){
+            next[i]=next[i]==-1?arr.size():next[i];
+            long long int plen=i-prev[i];
+            long long int nlen=next[i]-i;
+           sum=sum+(plen*nlen)*arr[i];
+        }
+        return sum;
+    }
+    long long subArrayRanges(vector<int>& nums) {
+        long long int sum1=sumSubarrayMins(nums);
+        long long int sum2=sumSubarrayMaxs(nums);
+        return sum2-sum1;
+    }
+};
