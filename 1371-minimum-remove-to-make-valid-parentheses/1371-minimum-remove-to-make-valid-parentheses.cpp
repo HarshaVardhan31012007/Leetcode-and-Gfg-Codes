51f7935ec2a958fbe4612d0d2b4ab68036987ede
HARSHA VARDHAN <hv1900019@gmail.com>
2025-01-05 23:43:07 +0530

    Time: 2 ms (92.93%), Space: 13.5 MB (59.09%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,26 +1,64 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-    stack<int>st;
-     for(int i=0;i<s.length();i++){
+    // stack<int>st;
+    //  for(int i=0;i<s.length();i++){
      
-        if(s[i]=='('){
-            st.push(i);
-        }
-        else if(s[i]==')'){
-            if(!st.empty()){
-                st.pop();
-            }
-            else{
-                s[i]='#';
-            }
-        }
+    //     if(s[i]=='('){
+    //         st.push(i);
+    //     }
+    //     else if(s[i]==')'){
+    //         if(!st.empty()){
+    //             st.pop();
+    //         }
+    //         else{
+    //             s[i]='#';
+    //         }
+    //     }
+    // }
+    // while(!st.empty()){
+    //     s[st.top()]='#';
+    //     st.pop();
+    // }
+    //    string ans="";
+    //    for(int i=0;i<s.length();i++){
+    //     if(s[i]!='#'){
+    //         ans.push_back(s[i]);
+    //     }
+    //    }
+    //    return ans;
+
+    int n=s.size();
+    int count=0;
+    for(int i=0;i<n;i++){
+       if(s[i]=='('){
+        count++;
+       }
+       else if(s[i]==')'){
+          if(count==0){
+            s[i]='#';
+          }
+          else{
+            count--;
+          }
+       }
     }
-    while(!st.empty()){
-        s[st.top()]='#';
-        st.pop();
+     count=0;
+    for(int i=n-1;i>=0;i--){
+       if(s[i]==')'){
+        count++;
+       }
+       else if(s[i]=='('){
+          if(count==0){
+            s[i]='#';
+          }
+          else{
+            count--;
+          }
+       }
     }
-       string ans="";
+
+        string ans="";
        for(int i=0;i<s.length();i++){
         if(s[i]!='#'){
             ans.push_back(s[i]);
