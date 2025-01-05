64ca9a3c5e3851b93a7a0e60ec45dbbe6d581e59
HARSHA VARDHAN <hv1900019@gmail.com>
2025-01-05 17:17:29 +0530

    Time: 3 ms (49.44%), Space: 10.6 MB (62.51%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int>st;
+        int result=0;
+        int num=0;
+        int sign=1;
+        for(int i=0;i<s.length();i++){
+            if(s[i]>='0'&&s[i]<='9'){
+               num=num*10+(s[i]-'0');
+            }
+            else if(s[i]=='+'){
+                result=result+num*sign;
+                sign=1;
+                num=0;
+            }
+            else if(s[i]=='-'){
+                result=result+num*sign;
+                sign=-1;
+                num=0;
+            }
+            else if(s[i]=='('){
+                st.push(result);
+                st.push(sign);
+                result=0;
+                sign=1;
+                num=0;
+            }
+            else if(s[i]==')'){
+                result+=sign*num;
+                num=0;
+                result*=st.top();
+                st.pop();
+                result+=st.top();
+                st.pop();
+            }
+        }
+        result+=sign*num;
+
+        return result;
+    }
+};
