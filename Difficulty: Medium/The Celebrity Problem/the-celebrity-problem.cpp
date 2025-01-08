26a4c30ca11f343a95a3aada362b467c86c6cd18
HARSHA VARDHAN <hv1900019@gmail.com>
2025-01-08 08:37:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -43,11 +43,11 @@ class Solution {
     
     int celebrity(vector<vector<int> >& mat) {
         //method 2
-       stack<int>s;
-       int n=mat.size();
-       for(int i=0;i<mat.size();i++){
-           s.push(i);
-       }
+    //   stack<int>s;
+    //   int n=mat.size();
+    //   for(int i=0;i<mat.size();i++){
+    //       s.push(i);
+    //   }
        
     //   while(s.size()!=-1){
     //       int a=s.top();
@@ -65,7 +65,7 @@ class Solution {
         //  a might be celebriy not exact
         //       s.push(a);
         //   }
-        //it also works but above is more correct
+        //it also works Prefer this only
         //  else{
         //       s.push(a);
         //   }
@@ -84,31 +84,26 @@ class Solution {
         //   }
         //   return mightbecelebrity;
         
-        
-        
-        while(!s.empty()){
-            int a=s.top();
-            s.pop();
-            if(!s.empty()){
-            int b=s.top();
-            s.pop();
-            if(mat[a][b]==1&&mat[b][a]==0){
-                s.push(b);
+        //method 3
+        //two pointer// similar to that if elseif condition
+        int n=mat.size();
+        int top=0;
+        int bottom=n-1;
+        while(top<bottom){
+            if(mat[top][bottom]){
+                top++;
             }
-            if(mat[b][a]==1&&mat[a][b]==0){
-                s.push(a);
+            else if(mat[bottom][top]){
+                bottom--;
             }
-            if(s.empty()){
-                return -1;
-            }
-            }
-            else if(s.empty()){
-                s.push(a);
-                break;
+            else{
+                top++;
+                bottom--;
             }
         }
-          int mightbecelebrity=s.top();
-        s.pop();
+        if(top>bottom)
+        return -1;
+          int mightbecelebrity=top;//or bottom
           int i=mightbecelebrity;
           for(int j=0;j<mat[i].size();j++){
           if(mat[i][j]==1)
@@ -120,6 +115,45 @@ class Solution {
           return -1;
           }
           return mightbecelebrity;
+          
+          
+          
+          
+        //don't follow this it is correct but confusing
+        // while(!s.empty()){
+        //     int a=s.top();
+        //     s.pop();
+        //     if(!s.empty()){
+        //     int b=s.top();
+        //     s.pop();
+        //     if(mat[a][b]==1&&mat[b][a]==0){
+        //         s.push(b);
+        //     }
+        //     if(mat[b][a]==1&&mat[a][b]==0){
+        //         s.push(a);
+        //     }
+        //     if(s.empty()){
+        //         return -1;
+        //     }
+        //     }
+        //     else if(s.empty()){
+        //         s.push(a);
+        //         break;
+        //     }
+        // }
+        //   int mightbecelebrity=s.top();
+        // s.pop();
+        //   int i=mightbecelebrity;
+        //   for(int j=0;j<mat[i].size();j++){
+        //   if(mat[i][j]==1)
+        //   return -1;
+        //   }
+        //   int j=mightbecelebrity;
+        //   for(int i=0;i<mat.size();i++){
+        //   if(i!=j&&mat[i][j]==0)
+        //   return -1;
+        //   }
+        //   return mightbecelebrity;
         
     }
 };
