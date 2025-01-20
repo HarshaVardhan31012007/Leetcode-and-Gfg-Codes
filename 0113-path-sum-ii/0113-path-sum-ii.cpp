f2ae294add502c4cbf14ca80771f966a1d956a03
HARSHA VARDHAN <hv1900019@gmail.com>
2025-01-20 21:35:03 +0530

    Time: 0 ms (100%), Space: 21 MB (50.43%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -11,7 +11,23 @@
  */
 class Solution {
 public:
-    void solve(TreeNode *root,int target,int sum,vector<int>path,vector<vector<int>>&ans){
+    // void solve(TreeNode *root,int target,int sum,vector<int>path,vector<vector<int>>&ans){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     sum=sum+root->val;
+    //     path.push_back(root->val);
+    //     if(root->left==NULL&&root->right==NULL){
+    //         if(sum==target){
+    //             ans.push_back(path);
+    //         }
+    //         return;
+    //     }
+    //     solve(root->left,target,sum,path,ans);
+    //     solve(root->right,target,sum,path,ans);
+    // }
+
+     void solve(TreeNode *root,int target,int sum,vector<int>&path,vector<vector<int>>&ans){
         if(root==NULL){
             return;
         }
@@ -21,10 +37,12 @@ public:
             if(sum==target){
                 ans.push_back(path);
             }
+            path.pop_back();
             return;
         }
         solve(root->left,target,sum,path,ans);
         solve(root->right,target,sum,path,ans);
+        path.pop_back();
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>>ans;
