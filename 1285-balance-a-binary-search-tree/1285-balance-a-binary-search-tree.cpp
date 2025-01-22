d64e34ffae98b539305bc0067a221919578194df
HARSHA VARDHAN <hv1900019@gmail.com>
2025-01-22 22:28:12 +0530

    Time: 9 ms (78.34%), Space: 65.8 MB (49.94%) - LeetHub

--- 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
@@ -30,7 +30,8 @@ public:
     TreeNode *bulidTree(vector<int>&in,int i,int j){
          if(i>j)
          return NULL;
-        int mid=ceil((i+j)/2);
+        //int mid=ceil((i+j)/2);
+        int mid=(i+j+1)/2;
         TreeNode *root=new TreeNode(in[mid]);
         root->left=bulidTree(in,i,mid-1);
         root->right=bulidTree(in,mid+1,j);
