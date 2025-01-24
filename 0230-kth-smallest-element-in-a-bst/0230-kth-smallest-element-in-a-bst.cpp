28a5c204c84efad9c2cecfc3b09b7ec10578f4bb
HARSHA VARDHAN <hv1900019@gmail.com>
2025-01-24 21:05:15 +0530

    Time: 0 ms (100%), Space: 24.3 MB (66.45%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,17 +11,34 @@
  */
 class Solution {
 public:
-    void buildinorder(TreeNode *root,vector<int>&inorder){
+    // void buildinorder(TreeNode *root,vector<int>&inorder){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     buildinorder(root->left,inorder);
+    //     inorder.push_back(root->val);
+    //     buildinorder(root->right,inorder);
+    // }
+
+     void traverse(TreeNode *root,int &count,int k,int &ans){
         if(root==NULL){
             return;
         }
-        buildinorder(root->left,inorder);
-        inorder.push_back(root->val);
-        buildinorder(root->right,inorder);
+        traverse(root->left,count,k,ans);
+        count++;
+        if(count==k){
+            ans=root->val;
+            return;
+        }
+        traverse(root->right,count,k,ans);
     }
     int kthSmallest(TreeNode* root, int k) {
-        vector<int>inorder;
-        buildinorder(root,inorder);
-        return inorder[k-1];
+        // vector<int>inorder;
+        // buildinorder(root,inorder);
+        // return inorder[k-1];
+        int count=0;
+        int ans=-1;
+        traverse(root,count,k,ans);
+        return ans;
     }
 };
