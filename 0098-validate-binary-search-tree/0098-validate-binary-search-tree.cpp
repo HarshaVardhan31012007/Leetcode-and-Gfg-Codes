39d6624e8282cecff25346d930f71186898c19b8
HARSHA VARDHAN <hv1900019@gmail.com>
2025-01-24 22:41:47 +0530

    Time: 0 ms (100%), Space: 22.2 MB (9.08%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,29 +11,51 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode *root,long mini,long maxi){
-        // if(root==NULL){
-        //     return true;
-        // }
-        // bool currans=root->val>mini&&root->val<maxi;
-        // if(currans==false){
-        //     return false;
-        // }
-        // bool leftans=solve(root->left,mini,root->val);
-        // bool rightans=solve(root->right,root->val,maxi);
-        // //no need to check currans 
-        // //agar yahi tak reach then curr ans is true
-        // return leftans&&rightans;
-
-
+    void buildinorder(TreeNode *root,vector<int>&inorder){
         if(root==NULL){
-            return true;
+            return;
         }
-        return root->val>mini&&root->val<maxi&&solve(root->left,mini,root->val)&&solve(root->right,root->val,maxi);
+        buildinorder(root->left,inorder);
+        inorder.push_back(root->val);
+        buildinorder(root->right,inorder);
     }
+
+    // bool solve(TreeNode *root,long mini,long maxi){
+    //     // if(root==NULL){
+    //     //     return true;
+    //     // }
+    //     // bool currans=root->val>mini&&root->val<maxi;
+    //     // if(currans==false){
+    //     //     return false;
+    //     // }
+    //     // bool leftans=solve(root->left,mini,root->val);
+    //     // bool rightans=solve(root->right,root->val,maxi);
+    //     // //no need to check currans 
+    //     // //agar yahi tak reach then curr ans is true
+    //     // return leftans&&rightans;
+
+
+    //     if(root==NULL){
+    //         return true;
+    //     }
+    //     return root->val>mini&&root->val<maxi&&solve(root->left,mini,root->val)&&solve(root->right,root->val,maxi);
+    // }
     bool isValidBST(TreeNode* root) {
-        long mini=LONG_MIN;
-        long maxi=LONG_MAX;
-        return solve(root,mini,maxi);
+        // long mini=LONG_MIN;
+        // long maxi=LONG_MAX;
+        // return solve(root,mini,maxi);
+
+
+         vector<int>inorder;
+        buildinorder(root,inorder);
+        int prev=inorder[0];
+        for(int i=1;i<inorder.size();i++){
+            int curr=inorder[i];
+            if(prev>=curr){
+                return false;
+            }
+            prev=curr;
+        } 
+        return true;
     }
 };
