d5133f85afcab7b41049f9f7a54ad7e37cd6cb00
HARSHA VARDHAN <hv1900019@gmail.com>
2025-01-25 18:18:29 +0530

    Time: 0 ms (100%), Space: 34.9 MB (81.74%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
@@ -11,39 +11,52 @@
  */
 class Solution {
 public:
-    void buildtree(TreeNode *root,vector<int>&inorder,int &i){
+    // void buildtree(TreeNode *root,vector<int>&inorder,int &i){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     buildtree(root->left,inorder,i);
+    //     root->val=inorder[i++];
+    //     buildtree(root->right,inorder,i);
+    // }
+    // void buildinorder(TreeNode *root,vector<int>&inorder){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     buildinorder(root->left,inorder);
+    //     inorder.push_back(root->val);
+    //     buildinorder(root->right,inorder);
+    // }
+
+    void solve(TreeNode *root,int &sum){
         if(root==NULL){
             return;
         }
-        buildtree(root->left,inorder,i);
-        root->val=inorder[i++];
-        buildtree(root->right,inorder,i);
-    }
-    void buildinorder(TreeNode *root,vector<int>&inorder){
-        if(root==NULL){
-            return;
-        }
-        buildinorder(root->left,inorder);
-        inorder.push_back(root->val);
-        buildinorder(root->right,inorder);
+        solve(root->right,sum);
+        sum=sum+root->val;
+        root->val=sum;
+        solve(root->left,sum);
     }
     TreeNode* convertBST(TreeNode* root) {
-        vector<int>inorder;
-        buildinorder(root,inorder);
-        //int sum=0;
-        for(int i=inorder.size()-1;i>=0;i--){
-            // sum=sum+inorder[i];
-            // inorder[i]=sum;
-            int curr=inorder[i];
-            int next=0;
-            if(i+1<inorder.size()){
-                next=inorder[i+1];
-            }
-            int sum=curr+next;
-            inorder[i]=sum;
-        } 
-        int i=0;
-        buildtree(root,inorder,i);
+        // vector<int>inorder;
+        // buildinorder(root,inorder);
+        // //int sum=0;
+        // for(int i=inorder.size()-1;i>=0;i--){
+        //     // sum=sum+inorder[i];
+        //     // inorder[i]=sum;
+        //     int curr=inorder[i];
+        //     int next=0;
+        //     if(i+1<inorder.size()){
+        //         next=inorder[i+1];
+        //     }
+        //     int sum=curr+next;
+        //     inorder[i]=sum;
+        // } 
+        // int i=0;
+        // buildtree(root,inorder,i);
+        // return root;
+        int sum=0;
+        solve(root,sum);
         return root;
     }
 };
