d3782978774d9b93224f3fe28838b27ac9f094f5
HARSHA VARDHAN <hv1900019@gmail.com>
2025-01-31 15:06:36 +0530

    Time: 0 ms (100%), Space: 23.5 MB (92.94%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -22,14 +22,31 @@ public:
 
 
 //method 2
-int D=0;
-int height(TreeNode *root){
-    if(!root)return 0;
-    int lh=height(root->left);
-    int rh=height(root->right);
-    D=max(D,lh+rh);
-    return max(lh,rh)+1;
-}
+// int D=0;
+// int height(TreeNode *root){
+//     if(!root)return 0;
+//     int lh=height(root->left);
+//     int rh=height(root->right);
+//     D=max(D,lh+rh);
+//     return max(lh,rh)+1;
+// }
+    
+     //method 3
+     pair<int,int>solve(TreeNode *root){
+        if(root==NULL){
+            pair<int,int>p=make_pair(0,0);
+            return p;
+        }
+        pair<int,int>left=solve(root->left);
+        pair<int,int>right=solve(root->right);
+        int op1=left.first;
+        int op2=right.first;
+        int op3=left.second+right.second;
+        pair<int,int>ans;
+        ans.first=max(op1,max(op2,op3));
+        ans.second=max(left.second,right.second)+1;
+        return ans;
+    }
     int diameterOfBinaryTree(TreeNode* root) {
         // if(root==NULL){
         //     return 0;
@@ -42,7 +59,11 @@ int height(TreeNode *root){
 
 
         //method 2
-        height(root);
-        return D;
+        //height(root);
+        //return D;
+
+        //method 3
+        pair<int,int>p=solve(root);
+        return p.first;
     }
 };
