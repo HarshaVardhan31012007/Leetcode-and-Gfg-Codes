78fed2b5d3cecfc86e8bd06e4c2e7a7928f4710c
HARSHA VARDHAN <hv1900019@gmail.com>
2025-02-01 10:52:55 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.46%) - LeetHub

--- 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.cpp
+++ 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.cpp
@@ -11,21 +11,33 @@
  */
 class Solution {
 public:
-    bool issumtree=true;
-    int sum(TreeNode *root){
-        if(!root) return 0;
-        if(root->left==NULL&&root->right==NULL){
-            return root->val;
-        }
-        int l=sum(root->left);
-        int r=sum(root->right);
-        if(issumtree&&(l+r)!=root->val){
-            issumtree=false;
-        }
-        return l+r+root->val;
+    // bool issumtree=true;
+    // int sum(TreeNode *root){
+    //     if(!root) return 0;
+    //     if(root->left==NULL&&root->right==NULL){
+    //         return root->val;
+    //     }
+    //     int l=sum(root->left);
+    //     int r=sum(root->right);
+    //     if(issumtree&&(l+r)!=root->val){
+    //         issumtree=false;
+    //     }
+    //     return l+r+root->val;
+    // }
+    // bool checkTree(TreeNode* root) {
+    //     sum(root);
+    //     return issumtree;
+    // }
+    
+    pair<bool,int>solve(TreeNode *root){
+        if(!root) return {true,0};
+        if(!root->left&&!root->right) return {true,root->val};
+        pair<bool,int>l=solve(root->left);
+        pair<bool,int>r=solve(root->right);
+        return {l.first&&r.first&&l.second+r.second==root->val,l.second=r.second+root->val};
     }
-    bool checkTree(TreeNode* root) {
-        sum(root);
-        return issumtree;
+    bool checkTree(TreeNode *root){
+        pair<bool,int>ans=solve(root);
+        return ans.first;
     }
 };
