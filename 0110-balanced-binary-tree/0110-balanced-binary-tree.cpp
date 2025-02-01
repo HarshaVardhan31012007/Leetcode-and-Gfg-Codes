4deb9cadadd2d44461c71ef36f83aa1aab2dc1b8
HARSHA VARDHAN <hv1900019@gmail.com>
2025-02-01 10:24:04 +0530

    Time: 0 ms (100%), Space: 22.8 MB (97.72%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,18 +11,35 @@
  */
 class Solution {
 public:
-bool isbalanced=true;
-int height(TreeNode *root){
-    if(!root)return 0;
-    int lh=height(root->left);
-    int rh=height(root->right);
-    if(isbalanced&&abs(lh-rh)>1){
-        isbalanced=false;
-    }
-    return max(lh,rh)+1;
-}
-    bool isBalanced(TreeNode* root) {
-        height(root);
-        return isbalanced;
-    }
+// bool isbalanced=true;
+// int height(TreeNode *root){
+//     if(!root)return 0;
+//     int lh=height(root->left);
+//     int rh=height(root->right);
+//     if(isbalanced&&abs(lh-rh)>1){
+//         isbalanced=false;
+//     }
+//     return max(lh,rh)+1;
+// }
+//     bool isBalanced(TreeNode* root) {
+//         height(root);
+//         return isbalanced;
+//     }
+     
+      pair<bool,int>solve(TreeNode *root){
+         if(root==NULL){
+           pair<bool,int>p=make_pair(true,0);
+           return p;
+        }
+        pair<bool,int>left=solve(root->left);
+        pair<bool,int>right=solve(root->right);
+        pair<bool,int>ans;
+        ans.first=left.first&&right.first&&abs(left.second-right.second)<=1;
+        ans.second=max(left.second,right.second)+1;
+        return ans;
+      }
+      bool isBalanced(TreeNode *root){
+        pair<bool,int>ans=solve(root);
+        return ans.first;
+      }
 };
