aceea78335f650b97fe0ac81faa41efec75e3494
HARSHA VARDHAN <hv1900019@gmail.com>
2025-02-01 16:43:12 +0530

    Time: 5 ms (12.1%), Space: 16.7 MB (12.16%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,61 +11,95 @@
  */
 class Solution {
 public:
-    struct s{
-        vector<int>data;
-        int x1;
-        int y;
-    };
-    int find(vector<s>&v,int i,int j){
-        for(int k=0;k<v.size();k++){
-            if(v[k].x1==i&&v[k].y==j){
-                return k;
-            }
-        }
-        return -1;
-    }
-    void solve(TreeNode *root,vector<s>&v,int l,int x,int &minx,int &maxx,int &lmax){
+    //method 1
+    // struct s{
+    //     vector<int>data;
+    //     int x1;
+    //     int y;
+    // };
+    // int find(vector<s>&v,int i,int j){
+    //     for(int k=0;k<v.size();k++){
+    //         if(v[k].x1==i&&v[k].y==j){
+    //             return k;
+    //         }
+    //     }
+    //     return -1;
+    // }
+    // void solve(TreeNode *root,vector<s>&v,int l,int x,int &minx,int &maxx,int &lmax){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     lmax=max(l,lmax);
+    //     minx=min(minx,x);
+    //     maxx=max(maxx,x);
+    //     int index=find(v,x,l);
+    //     if(index==-1){
+    //     vector<int>p;
+    //     p.push_back(root->val);
+    //     v.push_back({p,x,l});
+    //     }
+    //     else{
+    //        v[index].data.push_back(root->val);
+    //        sort(v[index].data.begin(),v[index].data.end());
+    //     }
+    //     solve(root->left,v,l+1,x-1,minx,maxx,lmax);
+    //     solve(root->right,v,l+1,x+1,minx,maxx,lmax);
+    // }
+    // vector<vector<int>> verticalTraversal(TreeNode* root) {
+    //     vector<s>v;
+    //     vector<vector<int>>ans;
+    //     int l=0;
+    //     int x=0;
+    //     int minx=INT_MAX;
+    //     int maxx=INT_MIN;
+    //     int lmax=INT_MIN;
+    //     solve(root,v,l,x,minx,maxx,lmax);
+    //     for(int i=minx;i<=maxx;i++){
+    //         vector<int>temp;
+    //         for(int j=0;j<=lmax;j++){
+    //             int index=find(v,i,j);
+    //             if(index!=-1){
+    //             for(int k=0;k<v[index].data.size();k++){
+    //             temp.push_back(v[index].data[k]);
+    //             }
+    //             }
+    //         }
+    //         if(!temp.empty())
+    //         ans.push_back(temp);
+    //     }
+    //     return ans;
+    // }
+
+    //method 2
+    void solve(TreeNode *root,map<int,map<int,vector<int>>>&mpp,int r,int c){
         if(root==NULL){
             return;
         }
-        lmax=max(l,lmax);
-        minx=min(minx,x);
-        maxx=max(maxx,x);
-        int index=find(v,x,l);
-        if(index==-1){
-        vector<int>p;
-        p.push_back(root->val);
-        v.push_back({p,x,l});
+        mpp[c][r].push_back(root->val);
+        if(mpp[c][r].size()>1){
+            sort(mpp[c][r].begin(),mpp[c][r].end());
         }
-        else{
-           v[index].data.push_back(root->val);
-           sort(v[index].data.begin(),v[index].data.end());
-        }
-        solve(root->left,v,l+1,x-1,minx,maxx,lmax);
-        solve(root->right,v,l+1,x+1,minx,maxx,lmax);
+        solve(root->left,mpp,r+1,c-1);
+        solve(root->right,mpp,r+1,c+1);
     }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<s>v;
-        vector<vector<int>>ans;
-        int l=0;
-        int x=0;
-        int minx=INT_MAX;
-        int maxx=INT_MIN;
-        int lmax=INT_MIN;
-        solve(root,v,l,x,minx,maxx,lmax);
-        for(int i=minx;i<=maxx;i++){
-            vector<int>temp;
-            for(int j=0;j<=lmax;j++){
-                int index=find(v,i,j);
-                if(index!=-1){
-                for(int k=0;k<v[index].data.size();k++){
-                temp.push_back(v[index].data[k]);
-                }
-                }
-            }
-            if(!temp.empty())
-            ans.push_back(temp);
-        }
+      vector<vector<int>>ans;
+      map<int,map<int,vector<int>>>mpp;
+      if(root==NULL){
         return ans;
+      }
+      int r=0;
+      int c=0;
+      solve(root,mpp,r,c);
+      for(auto i:mpp){
+        vector<int>temp;
+        for(auto j:i.second){
+            for(auto k:j.second){
+                temp.push_back(k);
+            }
+        }
+        ans.push_back(temp);
+      }
+      return ans;
     }
 };
