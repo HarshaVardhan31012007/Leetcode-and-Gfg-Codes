cead081961e473c5a501e211af00abc14f89e070
HARSHA VARDHAN <hv1900019@gmail.com>
2025-02-02 13:45:11 +0530

    Time: 34 ms (12.96%), Space: 75.9 MB (8.87%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -11,24 +11,49 @@
  */
 class Solution {
 public:
-    int ans=0;
-    void check(TreeNode *root,int &ans,long long int k){
+    //method 1
+    // int ans=0;
+    // void check(TreeNode *root,int &ans,long long int k){
+    //    if(root==NULL){
+    //     return;
+    //    }
+    //    if(k==root->val){
+    //     ans=ans+1;
+    //    }
+    //    k=k-root->val;
+    //    check(root->left,ans,k);
+    //    check(root->right,ans,k);
+    // }
+    // int pathSum(TreeNode* root, long long int targetSum) {
+    //     if(root){
+    //     check(root,ans,targetSum);
+    //     pathSum(root->left,targetSum);
+    //     pathSum(root->right,targetSum);
+    //     }
+    //     return ans;
+    // }
+
+    //method 2
+    void check(TreeNode *root,long long int k,int &ans,vector<int>v){
        if(root==NULL){
         return;
        }
-       if(k==root->val){
-        ans=ans+1;
+       v.push_back(root->val);
+       check(root->left,k,ans,v);
+       check(root->right,k,ans,v);
+       int size=v.size();
+       long long int sum=0;
+       for(int i=size-1;i>=0;i--){
+        sum=sum+v[i];
+          if(sum==k){
+            ans++;
+          }
        }
-       k=k-root->val;
-       check(root->left,ans,k);
-       check(root->right,ans,k);
     }
     int pathSum(TreeNode* root, long long int targetSum) {
-        if(root){
-        check(root,ans,targetSum);
-        pathSum(root->left,targetSum);
-        pathSum(root->right,targetSum);
-        }
+         int ans=0;
+         vector<int>v;
+         check(root,targetSum,ans,v);
         return ans;
     }
 };
