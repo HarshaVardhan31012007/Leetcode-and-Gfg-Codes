f6a7f6f5ff6fbb69c9a4a575f4164aa949c9df5d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-02-16 17:14:32 +0530

    Time: 0 ms (100%), Space: 13.9 MB (30.72%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.cpp
@@ -11,18 +11,38 @@
  */
 class Solution {
 public:
-    int count(TreeNode *root){
-        if(root==NULL) return 0;
-        return 1+count(root->left)+count(root->right);
-    }
-    bool isCBT(TreeNode *root,int index,int total){
-        if(root==NULL)return true;
-        if(index>total) return false;
-        return isCBT(root->left,2*index,total)&&isCBT(root->right,2*index+1,total);
-    }
+    // int count(TreeNode *root){
+    //     if(root==NULL) return 0;
+    //     return 1+count(root->left)+count(root->right);
+    // }
+    // bool isCBT(TreeNode *root,int index,int total){
+    //     if(root==NULL)return true;
+    //     if(index>total) return false;
+    //     return isCBT(root->left,2*index,total)&&isCBT(root->right,2*index+1,total);
+    // }
     bool isCompleteTree(TreeNode* root) {
-        int total=count(root);
-        int index=1;
-        return isCBT(root,index,total);
+        // int total=count(root);
+        // int index=1;
+        // return isCBT(root,index,total);
+
+        queue<TreeNode*>q;
+        q.push(root);
+        bool nullFound=0;
+        while(!q.empty()){
+            TreeNode *front=q.front();
+            q.pop();
+            if(front==NULL){
+                nullFound=1;
+            }
+            else{
+                if(nullFound==true)
+                return false;
+                else{
+                    q.push(front->left);
+                    q.push(front->right);
+                }
+            }
+        }
+        return true;
     }
 };
