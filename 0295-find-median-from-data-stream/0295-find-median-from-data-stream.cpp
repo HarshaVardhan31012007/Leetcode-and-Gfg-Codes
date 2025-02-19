310c77bedeca10c6a6fda32afc2612ba1b9d791d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-02-19 08:32:26 +0530

    Time: 67 ms (40.35%), Space: 124.9 MB (95.73%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,58 +1,83 @@
 class MedianFinder {
 public:
+    // priority_queue<int>maxh;
+    // priority_queue<int,vector<int>,greater<int>>minh;
+    // MedianFinder() {
+        
+    // }
+    
+    // void addNum(int num) {
+    //     if(minh.size()==maxh.size()){
+    //         if(num>findMedian()){
+    //           minh.push(num);
+    //         }
+    //         else{
+    //             maxh.push(num);
+    //         }
+    //     }
+    //     else if(minh.size()+1==maxh.size()){
+    //         if(num<findMedian()){
+    //             int x=maxh.top();
+    //             maxh.pop();
+    //             minh.push(x);
+    //             maxh.push(num);
+    //         }
+    //         else{
+    //             minh.push(num);
+    //         }
+    //     }
+    //     else if(minh.size()==maxh.size()+1){
+    //         if(num>findMedian()){
+    //             int x=minh.top();
+    //             minh.pop();
+    //             maxh.push(x);
+    //             minh.push(num);
+    //         }
+    //         else{
+    //             maxh.push(num);
+    //         }
+    //     }
+    // }
+    
+    // double findMedian() {
+    //     if(minh.size()==0&&maxh.size()==0)
+    //     return 0;
+    //     else if(minh.size()==maxh.size()){
+    //         return (minh.top()+maxh.top())/2.0;
+    //     }
+    //     else if(minh.size()+1==maxh.size()){
+    //         return maxh.top();
+    //     }
+    //     else if(minh.size()==maxh.size()+1){
+    //         return minh.top();
+    //     }
+    //     else
+    //     return -1;
+    // }
+
+
     priority_queue<int>maxh;
     priority_queue<int,vector<int>,greater<int>>minh;
-    MedianFinder() {
-        
-    }
+    MedianFinder() {}
     
     void addNum(int num) {
-        if(minh.size()==maxh.size()){
-            if(num>findMedian()){
-              minh.push(num);
-            }
-            else{
-                maxh.push(num);
-            }
+       if(maxh.size()==0||num>maxh.top()) 
+       minh.push(num);
+       else
+        maxh.push(num);
+        if(maxh.size()>minh.size()+1){
+            minh.push(maxh.top());
+            maxh.pop();
         }
-        else if(minh.size()+1==maxh.size()){
-            if(num<findMedian()){
-                int x=maxh.top();
-                maxh.pop();
-                minh.push(x);
-                maxh.push(num);
-            }
-            else{
-                minh.push(num);
-            }
-        }
-        else if(minh.size()==maxh.size()+1){
-            if(num>findMedian()){
-                int x=minh.top();
-                minh.pop();
-                maxh.push(x);
-                minh.push(num);
-            }
-            else{
-                maxh.push(num);
-            }
+        else if(maxh.size()<minh.size()){
+           maxh.push(minh.top());
+           minh.pop();
         }
     }
     
     double findMedian() {
-        if(minh.size()==0&&maxh.size()==0)
-        return 0;
-        else if(minh.size()==maxh.size()){
-            return (minh.top()+maxh.top())/2.0;
-        }
-        else if(minh.size()+1==maxh.size()){
-            return maxh.top();
-        }
-        else if(minh.size()==maxh.size()+1){
-            return minh.top();
-        }
-        else
-        return -1;
+        if(maxh.size()==minh.size()) return (maxh.top()+minh.top())/2.0;
+        return maxh.top();
     }
 };
 
