6a8f149c379c4b18efd70ed0dca1f41beb2214f4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-02-22 23:49:37 +0530

    Time: 75 ms (16.66%), Space: 153 MB (23.34%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -11,15 +11,16 @@ public:
     // };
 
 
-     class compare{
-       public:
-       bool operator()(pair<int,int> a,pair<int,int> b){
-          return a.first<b.first;
-       }
-    };
+    //  class compare{
+    //    public:
+    //    bool operator()(pair<int,int> a,pair<int,int> b){
+    //       return a.first<b.first;
+    //    }
+    // };
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int>ans;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,compare>pq;
+        //priority_queue<pair<int,int>,vector<pair<int,int>>,compare>pq;
+        priority_queue<pair<int,int>>pq;
         for(int i=0;i<k;i++){
             pq.push({nums[i],i});
         }
