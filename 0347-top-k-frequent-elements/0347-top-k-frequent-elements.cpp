86217ac36890973acf7352d617effde5aefb7cec
HARSHA VARDHAN <hv1900019@gmail.com>
2025-02-24 08:51:53 +0530

    Time: 0 ms (100%), Space: 17.8 MB (91.61%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // priority_queue<int,vector<int>,greater<int>>pq;
+        
+        // vector<int>ans;
+        // unordered_map<int,int>mpp;
         // for(int i=0;i<nums.size();i++){
-        //     pq.push(nums[i]);
+        //   mpp[nums[i]]++;
         // }
-        // int freq=0;int prev=-1;
+        // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        // for(auto it=mpp.begin();it!=mpp.end();it++){
+        //      if(pq.size()<k){
+        //         pq.push({it->second,it->first});
+        //      }
+        //      else{
+        //         if(it->second>pq.top().first){
+        //             pq.pop();
+        //             pq.push({it->second,it->first});
+        //         }
+        //      }
+        // }
         // while(!pq.empty()){
-        //    if(freq==0||pq.top()==prev){
-        //    freq++;
-        //    }
-        //    else{
-        //    if(k--){
-        //     ans.push_back(prev);
-        //    }
-        //    freq=1;
-        //    }
-        //    prev=pq.top();
-        //    pq.pop();
+        //     ans.push_back(pq.top().second);
+        //     pq.pop();
         // }
         // return ans;
-        
+
+
         vector<int>ans;
         unordered_map<int,int>mpp;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++)
           mpp[nums[i]]++;
-        }
+
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
         for(auto it=mpp.begin();it!=mpp.end();it++){
-             if(pq.size()<k){
-                pq.push({it->second,it->first});
-             }
-             else{
-                if(it->second>pq.top().first){
-                    pq.pop();
-                    pq.push({it->second,it->first});
-                }
+            pq.push({it->second,it->first});
+             if(pq.size()>k){
+                pq.pop();
              }
         }
         while(!pq.empty()){
