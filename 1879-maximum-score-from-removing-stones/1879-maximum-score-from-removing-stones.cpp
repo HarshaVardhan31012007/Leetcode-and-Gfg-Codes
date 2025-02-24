d2a9ff3856e16917d2048c2096d65ca53a724bf0
HARSHA VARDHAN <hv1900019@gmail.com>
2025-02-24 23:34:55 +0530

    Time: 54 ms (59.06%), Space: 7.9 MB (53.41%) - LeetHub

---
+++ 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        priority_queue<int>pq;
+        pq.push(a);pq.push(b);pq.push(c);
+        int ans=0;
+        while(true){
+            int top1=pq.top();pq.pop();
+            int top2=pq.top();pq.pop();
+            if(top2==0) break;
+            top1--;top2--;
+            ans++;
+            pq.push(top1);pq.push(top2);
+        }
+        return ans;
+    }
+};
