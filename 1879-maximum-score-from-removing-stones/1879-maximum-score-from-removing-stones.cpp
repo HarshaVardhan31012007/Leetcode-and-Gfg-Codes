633bb7d196e2cc5a5209c1e6af2b14649c31057f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-02-24 23:38:02 +0530

    Time: 55 ms (56.53%), Space: 8 MB (53.41%) - LeetHub

--- 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.cpp
+++ 1879-maximum-score-from-removing-stones/1879-maximum-score-from-removing-stones.cpp
@@ -4,13 +4,20 @@ public:
         priority_queue<int>pq;
         pq.push(a);pq.push(b);pq.push(c);
         int ans=0;
-        while(true){
+        // while(true){
+        //     int top1=pq.top();pq.pop();
+        //     int top2=pq.top();pq.pop();
+        //     if(top2==0) break;
+        //     top1--;top2--;
+        //     ans++;
+        //     pq.push(top1);pq.push(top2);
+        // }
+        while(pq.size()>1){
             int top1=pq.top();pq.pop();
             int top2=pq.top();pq.pop();
-            if(top2==0) break;
-            top1--;top2--;
-            ans++;
-            pq.push(top1);pq.push(top2);
+            top1--;top2--;ans++;
+            if(top1>0)pq.push(top1);
+            if(top2>0)pq.push(top2);
         }
         return ans;
     }
