abde959823b07966f87592c02758c57e35ff2d6d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-02-28 21:55:00 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -8,14 +8,28 @@ public:
     // }
 
     bool isAnagram(string s, string t) {
-        int freq[26]={0};
+        //for lowecase only
+        // int freq[26]={0};
+        // for(int i=0;i<s.size();i++){
+        //     freq[s[i]-'a']++;
+        // }
+        // for(int i=0;i<t.size();i++){
+        //     freq[t[i]-'a']--;
+        // }
+        // for(int i=0;i<26;i++){
+        //     if(freq[i]!=0) return false;
+        // }
+        // return true;
+
+        //for characters
+        int freq[256]={0};// as ascii is from 0 to 255
         for(int i=0;i<s.size();i++){
-            freq[s[i]-'a']++;
+            freq[s[i]]++;
         }
         for(int i=0;i<t.size();i++){
-            freq[t[i]-'a']--;
+            freq[t[i]]--;
         }
-        for(int i=0;i<26;i++){
+        for(int i=0;i<256;i++){
             if(freq[i]!=0) return false;
         }
         return true;
