9788d3bca9885515abae2bbdeed38465b4335393
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-01 12:14:43 +0530

    Time: 43 ms (17.28%), Space: 47.4 MB (5.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    // vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+    //     vector<vector<string>>v;
+    //     unordered_map<string,vector<string>>mpp;
+
+    //     for(int i=0;i<strs.size();i++){
+    //         string curr=strs[i];
+    //         sort(curr.begin(),curr.end());
+    //         mpp[curr].push_back(strs[i]);
+    //     }
+
+    //     // for(auto each:mpp){
+    //     //     v.push_back(each.second);
+    //     // }
+    //     for(auto it=mpp.begin();it!=mpp.end();it++){
+    //         v.push_back(it->second);
+    //     }
+    //     return v;
+    // }
+   
+     std::array<int,256>hash(string k){
+        std::array<int,256>hash={0};
+        for(int i=0;i<k.size();i++){
+            hash[k[i]]++;
+        }
+        return hash;
+     }
+     vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
         vector<vector<string>>v;
-        unordered_map<string,vector<string>>mpp;
+        map<std::array<int,256>,vector<string>>mpp;
 
         for(int i=0;i<strs.size();i++){
-            string curr=strs[i];
-            sort(curr.begin(),curr.end());
-            mpp[curr].push_back(strs[i]);
+            mpp[hash(strs[i])].push_back(strs[i]);
         }
 
-        // for(auto each:mpp){
-        //     v.push_back(each.second);
-        // }
-        for(auto it=mpp.begin();it!=mpp.end();it++){
-            v.push_back(it->second);
+        for(auto each:mpp){
+            v.push_back(each.second);
         }
         return v;
     }
