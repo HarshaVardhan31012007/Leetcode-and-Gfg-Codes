25e05d2c40e701d18942e7804e8fc309e0656284
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-02 11:40:18 +0530

    Time: 0 ms (100%), Space: 11.9 MB (70.05%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string output="";int mini=INT_MAX;
-        for(int i=0;i<strs.size();i++){
-            int p=strs[i].size();
-            mini=min(mini,p);
-        }
-        if(mini==INT_MAX) mini=0;
-        for(int i=0;i<mini;i++){
+        // string output="";int mini=INT_MAX;
+        // for(int i=0;i<strs.size();i++){
+        //     int p=strs[i].size();
+        //     mini=min(mini,p);
+        // }
+        // if(mini==INT_MAX) mini=0;
+        // for(int i=0;i<mini;i++){
+        //     bool issame=true;
+        //     char ch=strs[0][i];
+        //     for(int j=1;j<strs.size();j++){
+        //         if(strs[j][i]!=ch){
+        //         issame=false;
+        //         break;
+        //         }
+        //     }
+        //     if(issame)
+        //     output+=ch;
+        //     else
+        //     break;
+        // }
+        // return output;
+
+
+        string output="";
+        for(int i=0;i<strs[0].size();i++){
             bool issame=true;
             char ch=strs[0][i];
             for(int j=1;j<strs.size();j++){
