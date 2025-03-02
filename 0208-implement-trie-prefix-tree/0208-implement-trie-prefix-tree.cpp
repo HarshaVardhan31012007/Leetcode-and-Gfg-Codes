b07809f54cc0f0d943b31822d357e1e096be9a0a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-02 11:06:57 +0530

    Time: 111 ms (5.01%), Space: 141.9 MB (5.92%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,75 @@
+class TrieNode{
+        public:
+        char data;
+        unordered_map<char,TrieNode*>children;
+        bool isterminal;
+        TrieNode(char val){
+            this->data=val;
+            isterminal=false;
+        }
+    };
+    void insertHelp(TrieNode *root,string k){
+        if(k.length()==0){
+            root->isterminal=true;
+            return;
+        }
+        char ch=k[0];
+        TrieNode *child;
+        if(root->children.count(ch)==0){
+            child=new TrieNode(ch);
+            root->children[ch]=child;
+        }
+        else{
+            child=root->children[ch];
+        }
+        insertHelp(child,k.substr(1));
+    }
+    bool searchWord(TrieNode *root,string k){
+        if(k.length()==0){
+            return root->isterminal==true;
+        }
+        char ch=k[0];
+        if(root->children.count(ch)==0){
+            return false;
+        }
+        return searchWord(root->children[ch],k.substr(1));
+    }
+    bool searchPrefix(TrieNode *root,string k){
+        if(k.length()==0){
+            return true;
+        }
+        char ch=k[0];
+        if(root->children.count(ch)==0){
+            return false;
+        }
+        return searchPrefix(root->children[ch],k.substr(1));
+    }
+
+
+class Trie {
+public:
+    TrieNode *root;
+    Trie() {
+       root=new TrieNode(-1); 
+    }
+    
+    void insert(string word) {
+        insertHelp(root,word);
+    }
+    
+    bool search(string word) {
+        return searchWord(root,word);
+    }
+    
+    bool startsWith(string prefix) {
+        return searchPrefix(root,prefix);
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
