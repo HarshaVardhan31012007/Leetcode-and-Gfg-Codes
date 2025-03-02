bf2ffd49bb945cf1b335d36ced03ac68c1623ced
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-02 12:03:25 +0530

    Time: 0 ms (100%), Space: 11.8 MB (70.05%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string output="";int mini=INT_MAX;
-        for(int i=0;i<strs.size();i++){
-            int p=strs[i].size();
-            mini=min(mini,p);
-        }
-        for(int i=0;i<mini;i++){
-            bool issame=true;
-            char ch=strs[0][i];
-            for(int j=1;j<strs.size();j++){
-                if(strs[j][i]!=ch){
-                issame=false;
-                break;
-                }
-            }
-            if(issame)
-            output+=ch;
-            else
-            break;
-        }
-        return output;
-
-        //not correct out of bound access karta hai
-        // string output="";
-        // for(int i=0;i<strs[0].size();i++){
+        // string output="";int mini=INT_MAX;
+        // for(int i=0;i<strs.size();i++){
+        //     int p=strs[i].size();
+        //     mini=min(mini,p);
+        // }
+        // for(int i=0;i<mini;i++){
         //     bool issame=true;
         //     char ch=strs[0][i];
         //     for(int j=1;j<strs.size();j++){
@@ -39,6 +21,29 @@ public:
         //     break;
         // }
         // return output;
+
+    
+        string output="";
+        for(int i=0; ;i++){
+            bool issame=true;
+            if(i>=strs[0].size()) break;
+            char ch=strs[0][i];
+            for(int j=1;j<strs.size();j++){
+                if(i>=strs[j].size()){
+                    issame=false;
+                    break;
+                }
+                if(strs[j][i]!=ch){
+                issame=false;
+                break;
+                }
+            }
+            if(issame)
+            output+=ch;
+            else
+            break;
+        }
+        return output;
 
 
 
