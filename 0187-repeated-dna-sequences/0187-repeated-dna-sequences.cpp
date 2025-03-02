0430d2bb0e5c0f501635d712e40510960bc2e0d1
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-02 22:27:30 +0530

    Time: 43 ms (43.45%), Space: 26 MB (56.14%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    // vector<string> findRepeatedDnaSequences(string s) {
-    //     vector<string>ans;
-    //     unordered_map<string,int>visited;
-    //     for(int i=0;i<10;i++){
-    //         for(int j=0;j<s.length()-9;j++){
-    //            string temp=s.substr(j,10);
-    //            visited[temp]++;
-    //            if(visited[temp]>1){
-    //              ans.push_back(temp);
-    //              visited[temp]=INT_MIN;
-    //            }
-    //         }
-    //     }
-    //     return ans;
-    // }
+    vector<string> findRepeatedDnaSequences(string s) {
+        vector<string>ans;
+        unordered_map<string,int>visited;
+        for(int i=0;i<10;i++){
+            for(int j=i;j<s.length();j=j+10){
+               string temp=s.substr(j,10);
+               visited[temp]++;
+               if(visited[temp]>1){
+                 ans.push_back(temp);
+                 visited[temp]=INT_MIN;
+               }
+            }
+        }
+        return ans;
+    }
 
-    // vector<string> findRepeatedDnaSequences(string s) {
-    //     vector<string>ans;
-    //     unordered_map<string,int>visited;
-    //         for(int j=0;j<s.length();j++){
-    //            string temp=s.substr(j,10);
-    //            visited[temp]++;
-    //            if(visited[temp]>1){
-    //              ans.push_back(temp);
-    //              visited[temp]=INT_MIN;
-    //            }
-    //         }
-    //     return ans;
-    // }
 
     // vector<string> findRepeatedDnaSequences(string s) {
     //     vector<string>ans;
@@ -48,17 +35,17 @@ public:
     //     return ans;
     // }
 
-    vector<string> findRepeatedDnaSequences(string s) {
-        vector<string>ans;
-        unordered_map<string,int>visited;
-            if(s.length()<10) return ans;
-            for(int j=0;j<s.length()-9;j++){
-               string temp=s.substr(j,10);
-               visited[temp]++;
-            }
-            for(auto &[substr,freq]:visited){
-                if(freq>1)ans.push_back(substr);
-            }
-        return ans;
-    }
+    // vector<string> findRepeatedDnaSequences(string s) {
+    //     vector<string>ans;
+    //     unordered_map<string,int>visited;
+    //         if(s.length()<10) return ans;
+    //         for(int j=0;j<s.length()-9;j++){
+    //            string temp=s.substr(j,10);
+    //            visited[temp]++;
+    //         }
+    //         for(auto &[substr,freq]:visited){
+    //             if(freq>1)ans.push_back(substr);
+    //         }
+    //     return ans;
+    // }
 };
