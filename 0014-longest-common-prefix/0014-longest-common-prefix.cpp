e96aa5e0416f6441be69e53eaff9ebda872a2a87
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-02 11:52:38 +0530

    Time: 0 ms (100%), Space: 11.8 MB (70.05%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -23,21 +23,47 @@ public:
         // }
         // return output;
 
+        //not correct out of bound access karta hai
+        // string output="";
+        // for(int i=0;i<strs[0].size();i++){
+        //     bool issame=true;
+        //     char ch=strs[0][i];
+        //     for(int j=1;j<strs.size();j++){
+        //         if(strs[j][i]!=ch){
+        //         issame=false;
+        //         break;
+        //         }
+        //     }
+        //     if(issame)
+        //     output+=ch;
+        //     else
+        //     break;
+        // }
+        // return output;
+
+
 
         string output="";
-        for(int i=0;i<strs[0].size();i++){
-            bool issame=true;
-            char ch=strs[0][i];
-            for(int j=1;j<strs.size();j++){
-                if(strs[j][i]!=ch){
-                issame=false;
+        int i=0;
+        while(true){
+            char ch=0;
+            for(int j=0;j<strs.size();j++){
+                if(i>=strs[j].size()){
+                    ch=0;
+                    break;
+                }
+                if(ch==0){
+                    ch=strs[j][i];
+                }
+                else if(strs[j][i]!=ch){
+                ch=0;
                 break;
                 }
             }
-            if(issame)
-            output+=ch;
-            else
+            if(ch==0)
             break;
+            output+=ch;
+            i++;
         }
         return output;
     }
