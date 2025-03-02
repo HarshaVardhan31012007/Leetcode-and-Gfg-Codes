f9b1f79f640cedb5f2775baa5b299301ef23ccc1
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-02 11:55:25 +0530

    Time: 3 ms (14.91%), Space: 11.9 MB (53.27%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        // string output="";int mini=INT_MAX;
-        // for(int i=0;i<strs.size();i++){
-        //     int p=strs[i].size();
-        //     mini=min(mini,p);
-        // }
-        // if(mini==INT_MAX) mini=0;
-        // for(int i=0;i<mini;i++){
-        //     bool issame=true;
-        //     char ch=strs[0][i];
-        //     for(int j=1;j<strs.size();j++){
-        //         if(strs[j][i]!=ch){
-        //         issame=false;
-        //         break;
-        //         }
-        //     }
-        //     if(issame)
-        //     output+=ch;
-        //     else
-        //     break;
-        // }
-        // return output;
+        string output="";int mini=INT_MAX;
+        for(int i=0;i<strs.size();i++){
+            int p=strs[i].size();
+            mini=min(mini,p);
+        }
+        for(int i=0;i<mini;i++){
+            bool issame=true;
+            char ch=strs[0][i];
+            for(int j=1;j<strs.size();j++){
+                if(strs[j][i]!=ch){
+                issame=false;
+                break;
+                }
+            }
+            if(issame)
+            output+=ch;
+            else
+            break;
+        }
+        return output;
 
         //not correct out of bound access karta hai
         // string output="";
@@ -43,28 +42,28 @@ public:
 
 
 
-        string output="";
-        int i=0;
-        while(true){
-            char ch=0;
-            for(int j=0;j<strs.size();j++){
-                if(i>=strs[j].size()){
-                    ch=0;
-                    break;
-                }
-                if(ch==0){
-                    ch=strs[j][i];
-                }
-                else if(strs[j][i]!=ch){
-                ch=0;
-                break;
-                }
-            }
-            if(ch==0)
-            break;
-            output+=ch;
-            i++;
-        }
-        return output;
+        // string output="";
+        // int i=0;
+        // while(true){
+        //     char ch=0;
+        //     for(int j=0;j<strs.size();j++){
+        //         if(i>=strs[j].size()){
+        //             ch=0;
+        //             break;
+        //         }
+        //         if(ch==0){
+        //             ch=strs[j][i];
+        //         }
+        //         else if(strs[j][i]!=ch){
+        //         ch=0;
+        //         break;
+        //         }
+        //     }
+        //     if(ch==0)
+        //     break;
+        //     output+=ch;
+        //     i++;
+        // }
+        // return output;
     }
 };
