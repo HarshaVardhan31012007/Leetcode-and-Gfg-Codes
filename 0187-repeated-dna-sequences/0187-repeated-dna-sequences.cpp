7a51ba748943f16f89c4e55ea213512e8c668596
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-02 22:08:38 +0530

    Time: 39 ms (68%), Space: 26.4 MB (7%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
+    // vector<string> findRepeatedDnaSequences(string s) {
+    //     vector<string>ans;
+    //     unordered_map<string,int>visited;
+    //     for(int i=0;i<10;i++){
+    //         for(int j=0;j<s.length()-9;j++){
+    //            string temp=s.substr(j,10);
+    //            visited[temp]++;
+    //            if(visited[temp]>1){
+    //              ans.push_back(temp);
+    //              visited[temp]=INT_MIN;
+    //            }
+    //         }
+    //     }
+    //     return ans;
+    // }
+
     vector<string> findRepeatedDnaSequences(string s) {
         vector<string>ans;
         unordered_map<string,int>visited;
-        //for(int i=0;i<10;i++){
             for(int j=0;j<s.length();j++){
                string temp=s.substr(j,10);
                visited[temp]++;
@@ -12,7 +27,6 @@ public:
                  visited[temp]=INT_MIN;
                }
             }
-        //}
         return ans;
     }
 };
