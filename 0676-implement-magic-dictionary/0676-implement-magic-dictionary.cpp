8b6b58e0f0d796b4be6af021a25359400f6ae0ac
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-03 11:44:11 +0530

    Time: 340 ms (19.72%), Space: 193.8 MB (17.73%) - LeetHub

---
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.cpp
@@ -0,0 +1,60 @@
+class TrieNode{
+        public:
+        char data;
+        unordered_map<char,TrieNode*>children;
+        bool isterminal;
+        TrieNode(char val){
+            this->data=val;
+            isterminal=false;
+        }
+    };
+
+    void insertHelp(TrieNode *root,string k,int i=0){
+        if(i>=k.length()){
+            root->isterminal=true;
+            return;
+        }
+        if(root->children.count(k[i])==0)
+        root->children[k[i]]=new TrieNode(k[i]);
+        insertHelp(root->children[k[i]],k,i+1);
+    }
+
+    bool searchW(TrieNode *root,string k,int i=0,int change=0){
+          if(i>=k.length()){
+            return root->isterminal&&change==1;
+          }
+          if(change>1) return false;
+          bool matched=false;
+          for(auto &[val,node]:root->children){
+          if(val!=k[i])
+          matched=matched||searchW(node,k,i+1,change+1);
+          else
+          matched=matched||searchW(node,k,i+1,change);
+          if(matched==true)
+          return true;
+          }
+          return matched;
+    }
+class MagicDictionary {
+public:
+    TrieNode *root;
+    MagicDictionary() {
+        root=new TrieNode('-');
+    }
+    
+    void buildDict(vector<string> dictionary) {
+        for(auto each:dictionary)
+        insertHelp(root,each);
+    }
+    
+    bool search(string searchWord) {
+        return searchW(root,searchWord);
+    }
+};
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary* obj = new MagicDictionary();
+ * obj->buildDict(dictionary);
+ * bool param_2 = obj->search(searchWord);
+ */
