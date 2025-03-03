9a09ce86531ea6a248dc3efb029e0befe7a3f92a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-03 10:14:01 +0530

    Time: 1358 ms (5.02%), Space: 688.7 MB (5.41%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -8,68 +8,74 @@ class TrieNode{
             isterminal=false;
         }
     };
+    // void insertHelp(TrieNode *root,string k,int i=0){
+    //     if(i>=k.length()){
+    //         root->isterminal=true;
+    //         return;
+    //     }
+    //     char ch=k[i];
+    //     TrieNode *child;
+    //     if(root->children.count(ch)==0){
+    //         child=new TrieNode(ch);
+    //         root->children[ch]=child;
+    //     }
+    //     else{
+    //         child=root->children[ch];
+    //     }
+    //     insertHelp(child,k,i+1);
+    // }
+
+    // bool searchW(TrieNode *root,string k,int i=0){
+    //     if(i>=k.length()){
+    //         return root->isterminal==true;
+    //     }
+    //     char ch=k[i];
+    //     if(root->children.count(ch)==0&&ch!='.'){
+    //         return false;
+    //     }
+    //     TrieNode *child;
+    //     if(root->children.count(ch)==1){
+    //             child=root->children[ch];
+    //     }
+    //     else{
+    //             bool fans=0;
+    //             for(auto each:root->children){
+    //             child=each.second;
+    //             bool ans=searchW(child,k,i+1);
+    //             if(ans==1){
+    //             fans=1;
+    //              break;
+    //             }
+    //             }
+    //          return fans;
+    //     }
+    //     return searchW(child,k,i+1);
+    // }
+
     void insertHelp(TrieNode *root,string k,int i=0){
         if(i>=k.length()){
             root->isterminal=true;
             return;
         }
-        char ch=k[i];
-        TrieNode *child;
-        if(root->children.count(ch)==0){
-            child=new TrieNode(ch);
-            root->children[ch]=child;
-        }
-        else{
-            child=root->children[ch];
-        }
-        insertHelp(child,k,i+1);
+        if(root->children.count(k[i])==0)
+        root->children[k[i]]=new TrieNode(k[i]);
+        insertHelp(root->children[k[i]],k,i+1);
     }
-    // bool searchWord(TrieNode *root,string k){
-    //     if(k.length()==0){
-    //         return root->isterminal==true;
-    //     }
-    //     char ch=k[0];
-    //     if(root->children.count(ch)==0){
-    //         return false;
-    //     }
-    //     return searchWord(root->children[ch],k.substr(1));
-    // }
-    // bool searchPrefix(TrieNode *root,string k){
-    //     if(k.length()==0){
-    //         return true;
-    //     }
-    //     char ch=k[0];
-    //     if(root->children.count(ch)==0){
-    //         return false;
-    //     }
-    //     return searchPrefix(root->children[ch],k.substr(1));
-    // }
 
     bool searchW(TrieNode *root,string k,int i=0){
         if(i>=k.length()){
-            return root->isterminal==true;
+            return root->isterminal;
         }
-        char ch=k[i];
-        if(root->children.count(ch)==0&&ch!='.'){
-            return false;
-        }
-        TrieNode *child;
-        if(root->children.count(ch)==1){
-                child=root->children[ch];
+        bool matched=false;
+        if(k[i]=='.'){
+            for(auto &[ch,child]:root->children)
+            matched=matched||searchW(child,k,i+1);
         }
         else{
-                bool fans=0;
-                for(auto each:root->children){
-                child=each.second;
-                bool ans=searchW(child,k,i+1);
-                if(ans==1){
-                fans=1;
-                 break;
-                }
-                }
-             return fans;
+            if(root->children.count(k[i])==1)
+            return searchW(root->children[k[i]],k,i+1);
         }
-        return searchW(child,k,i+1);
+        return matched;
     }
 class WordDictionary {
 public:
