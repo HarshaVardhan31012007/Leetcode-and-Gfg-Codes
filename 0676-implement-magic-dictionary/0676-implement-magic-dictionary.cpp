42f7cdae96c58dbd2ccc4203b3429ca06d042623
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-03 12:01:02 +0530

    Time: 314 ms (20.52%), Space: 171.5 MB (18.33%) - LeetHub

--- 0676-implement-magic-dictionary/0676-implement-magic-dictionary.cpp
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.cpp
@@ -23,15 +23,15 @@ class TrieNode{
           if(i>=k.length()){
             return root->isterminal&&change==1;
           }
-          if(change>1) return false;
+          //if(change>1) return false;
           bool mismatched=false;
           for(auto &[val,node]:root->children){
-          if(val!=k[i])
+          if(val!=k[i]&&change==0)
           mismatched=mismatched||searchW(node,k,i+1,change+1);
-          else
+          else if(val==k[i])
           mismatched=mismatched||searchW(node,k,i+1,change);
-          if(mismatched==true)
-          return true;
+        //   if(mismatched==true)
+        //   return true;
           }
           return mismatched;
     }
