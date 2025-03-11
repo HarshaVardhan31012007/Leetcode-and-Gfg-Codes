5916f2d8559bd69056601edabf4b3cfb4ee04642
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-11 09:35:38 +0530

    Time: 1083 ms (11.87%), Space: 554.8 MB (26.15%) - LeetHub

--- 0746-prefix-and-suffix-search/0746-prefix-and-suffix-search.cpp
+++ 0746-prefix-and-suffix-search/0746-prefix-and-suffix-search.cpp
@@ -1,108 +1,103 @@
-// class TrieNode{
-//         public:
-//         char data;
-//         unordered_map<char,TrieNode*>children;
-//         bool isterminal;
-//         int index;
-//         TrieNode(char val){
-//             this->index=-1;
-//             this->data=val;
-//             isterminal=false;
-//         }
-//     };
-//     void insertHelp(TrieNode *root,string k,int j,int i=0){
-//         if(i>=k.length()){
-//             root->isterminal=true;
-//             root->index=j;
-//             return;
-//         }
-//         if(root->children.count(k[i])==0)
-//         root->children[k[i]]=new TrieNode(k[i]);
-//         insertHelp(root->children[k[i]],k,j,i+1);
-//     }
-//     bool issubstr(string p,string q){
-//         if(p.size()>=q.size()&&p.substr(p.size()-q.size(),q.size())==q)
-//         return 1;
-//         return 0;
-//     }
-//     void func(TrieNode *root,string q,int j,int &ans){
-//         if(j>=q.size()&&root->isterminal){
-//             ans=max(ans,root->index);
-//             if(q=="dj")
-//             cout<<ans<<endl;
-//             return;
-//         }
-//         if(j>=q.size()||(j>=1&&root->children.count(q[j])==0)) return;
-//         for(auto &[val,child]:root->children){
-//            if(val==q[j]){
-//            func(child,q,j+1,ans);
-//            func(child,q,0,ans);
-//            }
-//            else{
-//            func(child,q,j,ans);
-//            } 
-//         }
-//     }
-//     void search(TrieNode *root,string p,string q,int i,int &ans){
-//         if(i>=p.size()){
-//             if(issubstr(p,q)&&root->isterminal){
-//                 ans=max(ans,root->index);
-//                 func(root,q,0,ans);
+//method 1 using maps
+// class WordFilter {
+// public:
+// unordered_map<string,int>mpp;
+//     WordFilter(vector<string>& words) {
+//         for(int i=0;i<words.size();i++){
+//             string word=words[i];
+//             for(int j=1;j<=word.size();j++){
+//                 string pre=word.substr(0,j);
+//                 for(int k=0;k<word.size();k++){
+//                     string suf=word.substr(k);
+//                     mpp[pre+'{'+suf]=i;
+//                 }
 //             }
-//             else{
-//                 int j=0;
-//                 func(root,q,0,ans);
-//                 int k=0;
-//                 int i=p.size()-1;
-//                    for(int j=q.size()-1;j>=0;j--){
-//                     if(i>=0){
-//                     if(p[i]==q[j]){
-//                        if(k==0)
-//                        k=j+1;
-//                        i--;
-//                     }
-//                     else{
-//                         if(k!=0){
-//                         k=0;
-//                         if(p[p.size()-1]==q[j]){
-//                            if(k==0)
-//                            k=j+1;
-//                            i=p.size()-2;
-//                         }
-//                         }
-//                     }
-//                     }
+//         }
+//     }
+    
+//     int f(string pref, string suff) {
+//        if(mpp.find(pref+'{'+suff)!=mpp.end())
+//        return mpp[pref+'{'+suff];
+//        return -1;
+//     }
+// };
 
-//                    }
-//                    cout<<k<<" "<<ans<<endl;
-//                  if(k!=0)
-//                 func(root,q,k,ans);
-//             }
-//             return;
-//         }
-//         if(root->children.find(p[i])!=root->children.end())
-//         search(root->children[p[i]],p,q,i+1,ans);
-//     }
+
+//method 2 using trieNode
+class TrieNode{
+    public:
+    char data;
+    unordered_map<char,TrieNode*>children;
+    bool isterminal;
+    int index;
+    TrieNode(char val){
+        this->data=val;
+        this->isterminal=false;
+        this->index=-1;
+    }
+};
+void insert(TrieNode *root,string k,int i,int index){
+  if(i>=k.length()){
+    root->isterminal=true;
+    return;
+  }
+  if(root->children.find(k[i])==root->children.end()){
+    root->children[k[i]]=new TrieNode(k[i]);
+  }
+  root->children[k[i]]->index=index;
+  insert(root->children[k[i]],k,i+1,index);
+}
+int search(TrieNode *root,string k,int i){
+    if(i>=k.length()){
+       return root->index;
+    }
+    if(root->children.find(k[i])!=root->children.end())
+    return search(root->children[k[i]],k,i+1);
+    return -1;
+}
 class WordFilter {
 public:
-unordered_map<string,int>mpp;
+    TrieNode *root;
     WordFilter(vector<string>& words) {
+        root=new TrieNode('-');
+        //MLE
+        // for(int i=0;i<words.size();i++){
+        //     string word=words[i];
+        //     for(int j=1;j<=word.size();j++){
+        //         string pre=word.substr(0,j);
+        //         for(int k=0;k<word.size();k++){
+        //             string suf=word.substr(k);
+        //             insert(root,pre+'{'+suf,0,i);
+        //         }
+        //     }
+        // }
+
+
         for(int i=0;i<words.size();i++){
             string word=words[i];
-            for(int j=1;j<=word.size();j++){
-                string pre=word.substr(0,j);
-                for(int k=0;k<word.size();k++){
-                    string suf=word.substr(k);
-                    mpp[pre+'{'+suf]=i;
+            string suf="";
+                for(int k=word.size()-1;k>=0;k--){
+                    suf=word[k]+suf;
+                    insert(root,suf+'{'+word,0,i);
                 }
-            }
         }
     }
     
     int f(string pref, string suff) {
-       if(mpp.find(pref+'{'+suff)!=mpp.end())
-       return mpp[pref+'{'+suff];
-       return -1;
+       // return search(root,suff+'{'+pref,0);
+
+       string s=suff+"{"+pref;
+        int ans=-1;
+        TrieNode*curr=root;
+        for(char &ch:s){
+            if(curr->children.count(ch)==0){
+                return -1;
+            }else{
+                ans=curr->children[ch]->index;
+                curr=curr->children[ch];
+            }
+        }
+        return ans;
     }
 };
 
