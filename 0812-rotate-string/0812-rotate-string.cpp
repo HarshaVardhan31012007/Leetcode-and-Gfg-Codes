1cb587c60ef71e72c31f2ef12765f0cfca1fb0b9
HARSHA VARDHAN <hv1900019@gmail.com>
2025-03-15 10:42:37 +0530

    Time: 18 ms (1.27%), Space: 8 MB (83.71%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        string temp=s;
-        for(int i=0;i<s.length();i++){
-            char t=s[i];
-            temp.erase(temp.begin());
-            temp=temp+t;
-            if(temp==goal)
-            return true;
-        }
+        // string temp=s;
+        // for(int i=0;i<s.length();i++){
+        //     char t=s[i];
+        //     temp.erase(temp.begin());
+        //     temp=temp+t;
+        //     if(temp==goal)
+        //     return true;
+        // }
+        // return false;
+        if(s.length()!=goal.length())
         return false;
+
+        string temp=s+s;
+        int i=0;int j=0;
+        while(i<temp.length()){
+            cout<<temp[i]<<" "<<goal[j]<<endl;
+            if(temp[i]==goal[j]){
+                j++;
+            }
+            else{
+                i=i-j;
+                j=0;
+            }
+            if(j>=goal.length()) break;
+            i++;
+        }
+        return j==goal.length();
     }
 };
