f968f25531681d21681448395d8d692384493a09
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-18 19:00:27 +0530

    Time: 27 ms (6.03%), Space: 18 MB (99.29%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,77 @@
 class Solution {
 public:
-    // bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-    // }
+   //Valid topological sort means no cycle hence we should check cycle present or not
 
-   bool checkCycle(vector<vector<int>>& prerequisites,unordered_map<int,bool>&visited,unordered_map<int,bool>&path,int s){
-    visited[s]=1;path[s]=1;
-    for(auto &each:prerequisites){
-        int k;
-        if(each[1]==s) k=each[0];
-        else continue;
-        if(!visited[k]){
-            bool ans=checkCycle(prerequisites,visited,path,k);
-            if(ans==true)
-            return true;
+   //By BFS
+
+    void topoSort(vector<vector<int>>& prerequisites,vector<int>&ans,int n){
+        vector<int>indegree(n);
+        queue<int>q;
+        for(auto &each:prerequisites){
+            indegree[each[0]]++; 
         }
-        else if(visited[k]==true&&path[k]==true){
-            return true;
-        }
-    }
-    path[s]=false;
-    return false;
-   }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,bool>visited;
-        unordered_map<int,bool>path;
-        int n=numCourses;
-        bool ans=0;
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                ans=checkCycle(prerequisites,visited,path,i);
-                if(ans==true){
-                    return false;
-                }
+            if(indegree[i]==0)
+            q.push(i);
+        }
+        while(!q.empty()){
+            auto front=q.front();
+            ans.push_back(front);
+            q.pop();
+            for(auto &each:prerequisites){
+                int k;
+                if(each[1]==front) k=each[0];
+                else continue;
+                indegree[k]--;
+                if(indegree[k]==0)
+                q.push(k);
             }
         }
-        return true;
     }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>ans;
+        int n=numCourses;
+        topoSort(prerequisites,ans,n);
+        if(ans.size()==n)
+        return true;
+        return false;
+    }
+
+
+//by DFS
+
+
+//    bool checkCycle(vector<vector<int>>& prerequisites,unordered_map<int,bool>&visited,unordered_map<int,bool>&path,int s){
+//     visited[s]=1;path[s]=1;
+//     for(auto &each:prerequisites){
+//         int k;
+//         if(each[1]==s) k=each[0];
+//         else continue;
+//         if(!visited[k]){
+//             bool ans=checkCycle(prerequisites,visited,path,k);
+//             if(ans==true)
+//             return true;
+//         }
+//         else if(visited[k]==true&&path[k]==true){
+//             return true;
+//         }
+//     }
+//     path[s]=false;
+//     return false;
+//    }
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         unordered_map<int,bool>visited;
+//         unordered_map<int,bool>path;
+//         int n=numCourses;
+//         bool ans=0;
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 ans=checkCycle(prerequisites,visited,path,i);
+//                 if(ans==true){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
 };
