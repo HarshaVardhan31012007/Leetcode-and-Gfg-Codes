e7f4a6c0a64571bc08915589a4c633af905c9495
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-22 12:18:33 +0530

    Time: 2134 ms (5.02%), Space: 39.2 MB (14.46%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    void bfs(unordered_map<string,list<string>>&adjList,unordered_map<string,bool>&visited,unordered_map<string,string>&parent,string &s){
+        queue<string>q;
+        q.push(s);
+        visited[s]=1;
+        parent[s]="\0";
+        while(!q.empty()){
+            auto front=q.front();
+            q.pop();
+            for(auto &nbr:adjList[front]){
+                if(!visited[nbr]){
+                    visited[nbr]=1;
+                    parent[nbr]=front;
+                    q.push(nbr);
+                }
+            }
+        }
+    }
+    bool check(string &a,string &b){
+        int count=0;
+        for(int i=0;i<a.size();i++){
+            if(a[i]!=b[i]){
+                count++;
+            }
+        }
+        return count==1;
+    }
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string,list<string>>adjList;
+        bool flag=0;
+        for(int i=0;i<wordList.size();i++){
+            if(wordList[i]==endWord)
+            flag=1;
+        }
+        if(!flag) return 0;
+        wordList.push_back(beginWord);
+        for(int i=0;i<wordList.size();i++){
+            string word=wordList[i];
+            for(int j=0;j<wordList.size();j++){
+                string word1=wordList[j];
+                if(word!=word1){
+                    if(check(word,word1)){
+                        adjList[word].push_back(word1);
+                        adjList[word1].push_back(word);
+                    }
+                }
+            }
+        }
+       unordered_map<string,bool>visited;
+       unordered_map<string,string>parent;
+       bfs(adjList,visited,parent,beginWord);
+       string node=endWord;
+       int count=0;
+       while(node!="\0"){
+        if(node==beginWord||parent[node]!="\0")
+        count++;
+        node=parent[node];
+       }
+       return count;
+    }
+};
