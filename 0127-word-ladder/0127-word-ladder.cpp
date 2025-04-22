8c55f9b7a54fc7c784e5079d889549717d6937bd
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-22 13:16:01 +0530

    Time: 87 ms (50.18%), Space: 23.3 MB (26.68%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,65 +1,101 @@
 class Solution {
 public:
-    void bfs(unordered_map<string,list<string>>&adjList,unordered_map<string,bool>&visited,unordered_map<string,string>&parent,string &s){
-        queue<string>q;
-        q.push(s);
-        visited[s]=1;
-        parent[s]="\0";
-        while(!q.empty()){
+    // void bfs(unordered_map<string,list<string>>&adjList,unordered_map<string,bool>&visited,unordered_map<string,string>&parent,string &s){
+    //     queue<string>q;
+    //     q.push(s);
+    //     visited[s]=1;
+    //     parent[s]="\0";
+    //     while(!q.empty()){
+    //         auto front=q.front();
+    //         q.pop();
+    //         for(auto &nbr:adjList[front]){
+    //             if(!visited[nbr]){
+    //                 visited[nbr]=1;
+    //                 parent[nbr]=front;
+    //                 q.push(nbr);
+    //             }
+    //         }
+    //     }
+    // }
+    // bool check(string &a,string &b){
+    //     int count=0;
+    //     for(int i=0;i<a.size();i++){
+    //         if(a[i]!=b[i]){
+    //             count++;
+    //         }
+    //     }
+    //     return count==1;
+    // }
+    // int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    //     unordered_map<string,list<string>>adjList;
+    //     bool flag=0;bool flag1=0;
+    //     for(int i=0;i<wordList.size();i++){
+    //         if(wordList[i]==endWord)
+    //         flag=1;
+    //         if(wordList[i]==beginWord)
+    //         flag1=1;
+    //     }
+    //     if(!flag) return 0;
+    //     if(!flag1)
+    //     wordList.push_back(beginWord);
+    //     for(int i=0;i<wordList.size();i++){
+    //         string word=wordList[i];
+    //         for(int j=0;j<wordList.size();j++){
+    //             string word1=wordList[j];
+    //             if(word!=word1){
+    //                 if(check(word,word1)){
+    //                     adjList[word].push_back(word1);
+    //                     adjList[word1].push_back(word);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //    unordered_map<string,bool>visited;
+    //    unordered_map<string,string>parent;
+    //    bfs(adjList,visited,parent,beginWord);
+    //    string node=endWord;
+    //    int count=0;
+    //    while(node!="\0"){
+    //     if(node==beginWord||parent[node]!="\0")
+    //     count++;
+    //     node=parent[node];
+    //    }
+    //    return count;
+    // }
+
+
+
+
+
+    //method 2 bset solution
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+       queue<pair<string,int>>q;
+       unordered_map<string,bool>visited;
+       unordered_set<string>st(wordList.begin(),wordList.end());
+       q.push({beginWord,0});
+       visited[beginWord]=1;
+       while(!q.empty()){
             auto front=q.front();
             q.pop();
-            for(auto &nbr:adjList[front]){
-                if(!visited[nbr]){
-                    visited[nbr]=1;
-                    parent[nbr]=front;
-                    q.push(nbr);
+            if(front.first==endWord){
+                return front.second+1;
+            }
+            else{
+                string word=front.first;
+                for(int i=0;i<word.length();i++){
+                   char original=word[i];
+                   for(char ch='a';ch<='z';ch++){
+                        word[i]=ch;
+                        if(st.find(word)!=st.end()&&!visited[word]){
+                            visited[word]=1;
+                            q.push({word,front.second+1});
+                        }
+                   }
+                   word[i]=original;       
                 }
             }
-        }
-    }
-    bool check(string &a,string &b){
-        int count=0;
-        for(int i=0;i<a.size();i++){
-            if(a[i]!=b[i]){
-                count++;
-            }
-        }
-        return count==1;
-    }
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string,list<string>>adjList;
-        bool flag=0;bool flag1=0;
-        for(int i=0;i<wordList.size();i++){
-            if(wordList[i]==endWord)
-            flag=1;
-            if(wordList[i]==beginWord)
-            flag1=1;
-        }
-        if(!flag) return 0;
-        if(!flag1)
-        wordList.push_back(beginWord);
-        for(int i=0;i<wordList.size();i++){
-            string word=wordList[i];
-            for(int j=0;j<wordList.size();j++){
-                string word1=wordList[j];
-                if(word!=word1){
-                    if(check(word,word1)){
-                        adjList[word].push_back(word1);
-                        adjList[word1].push_back(word);
-                    }
-                }
-            }
-        }
-       unordered_map<string,bool>visited;
-       unordered_map<string,string>parent;
-       bfs(adjList,visited,parent,beginWord);
-       string node=endWord;
-       int count=0;
-       while(node!="\0"){
-        if(node==beginWord||parent[node]!="\0")
-        count++;
-        node=parent[node];
        }
-       return count;
+       return 0;
     }
 };
