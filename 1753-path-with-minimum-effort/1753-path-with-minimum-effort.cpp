65682561c2f945140a3cbc106563fe69386e74d0
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-22 18:35:58 +0530

    Time: 137 ms (22.95%), Space: 52.7 MB (13.52%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,46 +1,6 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        // int m=heights.size();
-        // int n=heights[0].size();
-        // set<pair<int,pair<int,int>>>st;
-        // map<pair<int,int>,pair<int,int>>parent;
-        // vector<vector<int>>effort(m,vector<int>(n,INT_MAX));
-        // st.insert({0,{0,0}});
-        // parent[{0,0}]={-1,-1};
-        // effort[0][0]=0;
-        // while(!st.empty()){
-        //     auto front=*st.begin();
-        //     st.erase(st.begin());
-        //     vector<pair<int,int>>dir={{0,-1},{0,1},{1,0},{-1,0}};
-        //     for(auto &d:dir){
-        //         int oldX=front.second.first;
-        //         int oldY=front.second.second;
-        //         int newX=front.second.first+d.first;
-        //         int newY=front.second.second+d.second;
-        //         if(newX>=0 && newX<m && newY>=0 && newY<n && parent[{oldX,oldY}]!= make_pair(newX,newY) && (abs(heights[newX][newY]-heights[oldX][oldY])<effort[newX][newY])){
-        //             if(st.find({effort[newX][newY],{newX,newY}})!=st.end()){
-        //                 st.erase({effort[newX][newY],{newX,newY}});
-        //             }
-        //             effort[newX][newY]=abs(heights[newX][newY]-heights[oldX][oldY]);
-        //             parent[{newX,newY}]={oldX,oldY};
-        //             st.insert({effort[newX][newY],{newX,newY}});
-        //         }
-        //     }
-        // }
-        //     pair<int,int>node={m-1,n-1};
-        //     int maxi=INT_MIN;
-        //     while(node!=make_pair(-1,-1)){
-        //       maxi=max(maxi,effort[node.first][node.second]);
-        //       node=parent[node];
-        //     }
-        //     return maxi;
-
-
-
-
-
-
 
         int m=heights.size();
         int n=heights[0].size();
@@ -69,6 +29,6 @@ public:
                 }
             }
         }
-          return effort[m-1][n-1];
+        return effort[m-1][n-1];
     }
 };
