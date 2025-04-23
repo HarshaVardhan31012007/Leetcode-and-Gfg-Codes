860c7be1fdc1da49f7e9a3b5661fc1bf82604680
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-23 10:50:56 +0530

    Time: 3 ms (42.59%), Space: 19.5 MB (64.25%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -3,7 +3,7 @@ public:
     void dfs(vector<vector<int>>& isConnected,unordered_map<int,bool>&visited,int n,int s){
         visited[s]=1;
         for(int i=0;i<n;i++){
-            if(isConnected[s][i]==1&&!visited[i]){
+            if(isConnected[s][i]&&!visited[i]){
                 dfs(isConnected,visited,n,i);
             }
         }
