a0d0d82c98f4963bbb7692df4b3eb4965bbaee35
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-23 17:50:08 +0530

    Time: 0 ms (100%), Space: 18.3 MB (45.92%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -5,21 +5,40 @@ public:
        for(auto &flight:flights){
         adjList[flight[0]].push_back({flight[1],flight[2]});
        }
-       priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-       vector<int>visited(n,INT_MAX);
-       visited[src]=0;
-       pq.push({0,src,0});
-       while(!pq.empty()){
-        auto front=pq.top();
-        pq.pop();
-        if(front[1]==dst) return front[0];
-        if(front[2]>k) continue;
-        if(visited[front[1]]<front[2]) continue;
-        visited[front[1]]=min(visited[front[1]],front[2]);
+    //    priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+    //    vector<int>visited(n,INT_MAX);
+    //    visited[src]=0;
+    //    pq.push({0,src,0});
+    //    while(!pq.empty()){
+    //     auto front=pq.top();
+    //     pq.pop();
+    //     if(front[1]==dst) return front[0];
+    //     if(front[2]>k) continue;
+    //     if(visited[front[1]]<front[2]) continue;
+    //     visited[front[1]]=min(visited[front[1]],front[2]);
+    //     for(auto &nbr:adjList[front[1]]){
+    //         pq.push({front[0]+nbr.second,nbr.first,front[2]+1});
+    //     }
+    //    }
+    //    return -1;
+
+
+
+    vector<int>distance(n,1e9);
+    distance[src]=0;
+    queue<vector<int>>q;
+    q.push({0,src,0});
+    while(!q.empty()){
+        auto front=q.front();
+        q.pop();
+        if(front[0]>k) continue;
         for(auto &nbr:adjList[front[1]]){
-            pq.push({front[0]+nbr.second,nbr.first,front[2]+1});
+            if(front[2]+nbr.second<distance[nbr.first]&&front[0]<=k){
+                    distance[nbr.first]=front[2]+nbr.second;
+                    q.push({front[0]+1,nbr.first,distance[nbr.first]});
+            }
         }
-       }
-       return -1;
+    }
+       return (distance[dst]==1e9)?-1:distance[dst];
     }
 };
