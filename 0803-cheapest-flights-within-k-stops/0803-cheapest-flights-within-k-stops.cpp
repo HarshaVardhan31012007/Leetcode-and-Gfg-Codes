9c39b091c4c1bc92d589fc0cb67d190d05127685
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-23 17:33:30 +0530

    Time: 8 ms (21.37%), Space: 19.9 MB (11.69%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {    
-        unordered_map<int,list<pair<int,int>>>adjList;
-        for(int i=0;i<flights.size();i++){
-            adjList[flights[i][0]].push_back({flights[i][1],flights[i][2]});
+       unordered_map<int,list<pair<int,int>>>adjList;
+       for(auto &flight:flights){
+        adjList[flight[0]].push_back({flight[1],flight[2]});
+       }
+       priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+       vector<int>visited(n,INT_MAX);
+       visited[src]=0;
+       pq.push({0,src,0});
+       while(!pq.empty()){
+        auto front=pq.top();
+        pq.pop();
+        if(front[1]==dst) return front[0];
+        if(front[2]>k) continue;
+        if(visited[front[1]]<front[2]) continue;
+        visited[front[1]]=min(visited[front[1]],front[2]);
+        for(auto &nbr:adjList[front[1]]){
+            pq.push({front[0]+nbr.second,nbr.first,front[2]+1});
         }
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
-        pq.push({0,src,0});
-        vector<int>visited(n,INT_MAX);
-        visited[src]=0;
-        while(!pq.empty()){
-            auto curr=pq.top();
-            pq.pop();
-            if(curr[1]==dst) return curr[0];
-            if(curr[2]>k) continue;
-            if(visited[curr[1]]<curr[2]) continue;
-            visited[curr[1]]=min(visited[curr[1]],curr[2]);
-            for(auto &nbr:adjList[curr[1]]){
-                pq.push({curr[0]+nbr.second,nbr.first,curr[2]+1});
-            }
-        }
-        return -1;
+       }
+       return -1;
     }
 };
