5867e23528b5bea06a2b97bc4bf08e50c940c800
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-25 11:29:17 +0530

    Time: 485 ms (10.26%), Space: 225.9 MB (8.16%) - LeetHub

--- 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.cpp
@@ -1,44 +1,79 @@
 class Solution {
 public:
-int find(int k,vector<int>&path){
-    for(int i=0;i<path.size();i++){
-        if(path[i]==k)
-        return i;
-    }
-    return -1;
-}
-    void dfs(unordered_map<int,list<int>>&adjList,unordered_map<int,bool>&visited,unordered_map<int,bool>&pvisited,vector<int>&path,int &maxi,int s,int n){
+
+
+// int find(int k,vector<int>&path){
+//     for(int i=0;i<path.size();i++){
+//         if(path[i]==k)
+//         return i;
+//     }
+//     return -1;
+// }
+//     void dfs(unordered_map<int,list<int>>&adjList,unordered_map<int,bool>&visited,unordered_map<int,bool>&pvisited,vector<int>&path,int &maxi,int s,int n){
+//           visited[s]=1;
+//           pvisited[s]=1;
+//           path.push_back(s);
+//           for(auto &nbr:adjList[s]){
+//              if(!visited[nbr]){
+//                 dfs(adjList,visited,pvisited,path,maxi,nbr,n);
+//              }
+//              else if(visited[nbr]&&pvisited[nbr]){
+//                 int b=path.size()-find(nbr,path);
+//                 maxi=max(maxi,b);
+//              }
+//           }
+//           path.pop_back();
+//           pvisited[s]=0;
+//     }
+//     int longestCycle(vector<int>& edges) {
+//         int n=edges.size();
+//         unordered_map<int,list<int>>adjList;
+//         for(int i=0;i<edges.size();i++){
+//             adjList[i].push_back(edges[i]);
+//         }
+//         vector<int>path;
+//         unordered_map<int,bool>visited;
+//         unordered_map<int,bool>pvisited;
+//         int maxi=INT_MIN;
+//         for(int i=0;i<n;i++){
+//         if(!visited[i]){
+//         dfs(adjList,visited,pvisited,path,maxi,i,n);
+//         }
+//         }
+//         return (maxi==INT_MIN)?-1:maxi;
+//     }
+
+
+
+    void dfs(vector<int>&edges,unordered_map<int,bool>&visited,unordered_map<int,bool>&pvisited,vector<int>&dist,int &maxi,int s,int n,int length){
           visited[s]=1;
           pvisited[s]=1;
-          path.push_back(s);
-          for(auto &nbr:adjList[s]){
+          dist[s]=length;
+          int nbr=edges[s];
+          if(nbr!=-1){
              if(!visited[nbr]){
-                dfs(adjList,visited,pvisited,path,maxi,nbr,n);
+                dfs(edges,visited,pvisited,dist,maxi,nbr,n,length+1);
              }
              else if(visited[nbr]&&pvisited[nbr]){
-                int b=path.size()-find(nbr,path);
+                int b=dist[s]-dist[nbr]+1;
                 maxi=max(maxi,b);
              }
           }
-          path.pop_back();
           pvisited[s]=0;
     }
     int longestCycle(vector<int>& edges) {
         int n=edges.size();
-        unordered_map<int,list<int>>adjList;
-        for(int i=0;i<edges.size();i++){
-            adjList[i].push_back(edges[i]);
-        }
-        vector<int>path;
+        vector<int>dist(n,0);
+        int length=0;
         unordered_map<int,bool>visited;
         unordered_map<int,bool>pvisited;
-        int maxi=INT_MIN;
+        int maxi=-1;
         for(int i=0;i<n;i++){
         if(!visited[i]){
-        dfs(adjList,visited,pvisited,path,maxi,i,n);
-        pvisited.clear();
+        dfs(edges,visited,pvisited,dist,maxi,i,n,length);
         }
         }
-        return (maxi==INT_MIN)?-1:maxi;
+        return maxi;
     }
+
 };
