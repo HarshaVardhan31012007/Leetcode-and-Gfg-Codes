fa5eb54669b2f6dd611bd828fb8a9d1ae1f4bb86
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-27 17:53:40 +0530

    Time: 64 ms (8.64%), Space: 28.3 MB (5.11%) - LeetHub

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void dijkstrasalgo(unordered_map<int,list<pair<int,int>>>&adjList,int d,int s,int n,int &count){
+        set<pair<int,int>>st;
+        st.insert({0,s});
+        vector<int>distance(n,INT_MAX);
+        distance[s]=0;
+        while(!st.empty()){
+            auto front=*st.begin();
+            st.erase(st.begin());
+            if(front.second!=s&&front.first<=d){
+                count++;
+            }
+            for(auto &nbr:adjList[front.second]){
+                 if(distance[front.second]+nbr.second<distance[nbr.first]){
+                    if(st.find({distance[nbr.first],nbr.first})!=st.end()){
+                        st.erase({distance[nbr.first],nbr.first});
+                    }
+                    distance[nbr.first]=distance[front.second]+nbr.second;
+                    st.insert({distance[nbr.first],nbr.first});
+                 }
+            }
+        }
+    }
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        unordered_map<int,list<pair<int,int>>>adjList;
+        for(auto &edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            int w=edge[2];
+            adjList[u].push_back({v,w});
+            adjList[v].push_back({u,w});
+        }
+        int ans=INT_MAX;int city=-1;
+        for(int i=0;i<n;i++){
+           int count=0;
+           dijkstrasalgo(adjList,distanceThreshold,i,n,count);
+           ans=min(ans,count);
+           if(ans==count)
+           city=i;
+           cout<<count<<endl;
+        }
+        return city;
+    }
+};
