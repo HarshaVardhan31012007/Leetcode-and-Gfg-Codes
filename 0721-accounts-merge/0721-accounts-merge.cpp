8c09d7bcbb9ff7a19ff648e6d46e5124f7272e27
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-27 19:18:15 +0530

    Time: 46 ms (45.63%), Space: 46.7 MB (31.57%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int findParent(int u,vector<int>&parent){
+       if(u==parent[u]) return u;
+       return parent[u]=findParent(parent[u],parent);
+    }
+    void unionSet(int u,int v,vector<int>&parent,vector<int>&rank){
+        u=findParent(u,parent);
+        v=findParent(v,parent);
+        if(rank[u]<rank[v]){
+            parent[u]=v;
+            rank[v]++;
+        }
+        else{
+            parent[v]=u;
+            rank[u]++;
+        }
+    }
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        int n=accounts.size();
+        vector<int>parent(n);
+        vector<int>rank(n,0);
+        for(int i=0;i<n;i++)
+        parent[i]=i;
+        unordered_map<string,int>mpp;
+        for(int i=0;i<accounts.size();i++){
+            for(int j=1;j<accounts[i].size();j++){
+                string mail=accounts[i][j];
+                auto it=mpp.find(mail);
+                if(it==mpp.end()) mpp[mail]=i;
+                else unionSet(i,it->second,parent,rank);
+            }
+        }
+            unordered_map<int,set<string>>preAns;
+            for(auto it:mpp){
+                int accountNo=it.second;
+                string mail=it.first;
+                accountNo=findParent(accountNo,parent);
+                preAns[accountNo].insert(mail);
+            }
+            vector<vector<string>>ans;
+            for(auto it:preAns){
+               vector<string>v;
+               v.push_back(accounts[it.first][0]);
+               for(auto each:it.second) v.push_back(each);
+               ans.push_back(v);
+            }
+             return ans;
+    }
+};
