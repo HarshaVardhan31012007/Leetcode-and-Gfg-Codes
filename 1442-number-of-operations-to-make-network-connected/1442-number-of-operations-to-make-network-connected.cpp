7325195fea65477f31c0330ee81b29d31143282c
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-27 16:01:12 +0530

    Time: 0 ms (100%), Space: 41.9 MB (96.94%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,28 +1,74 @@
 class Solution {
 public:
-    void dfs(int s,int n,unordered_map<int,bool>&visited,unordered_map<int,list<int>>&adjList){
-        visited[s]=1;
-        for(auto &nbr:adjList[s]){
-            if(!visited[nbr])
-            dfs(nbr,n,visited,adjList);
+    // void dfs(int s,int n,unordered_map<int,bool>&visited,unordered_map<int,list<int>>&adjList){
+    //     visited[s]=1;
+    //     for(auto &nbr:adjList[s]){
+    //         if(!visited[nbr])
+    //         dfs(nbr,n,visited,adjList);
+    //     }
+    // }
+    //int makeConnected(int n, vector<vector<int>>& connections) {
+        // if(connections.size()<n-1)
+        // return -1;
+        // unordered_map<int,list<int>>adjList;
+        // for(auto &edge:connections){
+        //     adjList[edge[0]].push_back(edge[1]);
+        //     adjList[edge[1]].push_back(edge[0]);
+        // }
+        // unordered_map<int,bool>visited;
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     if(!visited[i]){
+        //         dfs(i,n,visited,adjList);
+        //         count++;
+        //     }
+        // }
+        // return count-1;
+    //}
+
+
+
+
+
+    //method 2
+    int findParent(int u,vector<int>&parent){
+       if(u==parent[u]) return u;
+       return parent[u]=findParent(parent[u],parent);
+    }
+    void unionSet(int u,int v,vector<int>&parent,vector<int>&rank){
+        u=findParent(u,parent);
+        v=findParent(v,parent);
+        if(rank[u]<rank[v]){
+            parent[u]=v;
+            rank[v]++;
+        }
+        else{
+            parent[v]=u;
+            rank[u]++;
         }
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size()<n-1)
-        return -1;
-        unordered_map<int,list<int>>adjList;
+        vector<int>parent(n);
+        vector<int>rank(n,0);
+        for(int i=0;i<n;i++)
+        parent[i]=i;
+        int extraedges=0;
         for(auto &edge:connections){
-            adjList[edge[0]].push_back(edge[1]);
-            adjList[edge[1]].push_back(edge[0]);
-        }
-        unordered_map<int,bool>visited;
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i,n,visited,adjList);
-                count++;
+            int u=edge[0];
+            int v=edge[1];
+            if(findParent(u,parent)!=findParent(v,parent)){
+                 unionSet(u,v,parent,rank);
+            }
+            else{
+                extraedges++;
             }
         }
-        return count-1;
+        int count=0;
+        for(int i=0;i<n;i++){
+          if(parent[i]==i)
+          count++;
+        }
+        int ans=count-1;
+        return (extraedges>=ans)?ans:-1;
     }
 };
