86c7360b9c6d99b0600bafff728127dac3265ad2
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-29 13:20:52 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.52%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
-    int solveusingMem(int n,vector<int>&dp){
-        if(n==1||n==0)
-        return n;
-        if(dp[n]!=-1)
-        return dp[n];
-        return dp[n]=solveusingMem(n-1,dp)+solveusingMem(n-2,dp);
-    }
-    int fib(int n) {
+    // int solveusingMem(int n,vector<int>&dp){
+    //     if(n==1||n==0)
+    //     return n;
+    //     if(dp[n]!=-1)
+    //     return dp[n];
+    //     return dp[n]=solveusingMem(n-1,dp)+solveusingMem(n-2,dp);
+    // }
+    // int fib(int n) {
+    //     vector<int>dp(n+1,-1);
+    //     return solveusingMem(n,dp);
+    // }
+
+    int solveusingTab(int n){
         vector<int>dp(n+1,-1);
-        return solveusingMem(n,dp);
+        dp[0]=0;
+        if(n==0) return 0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    int fib(int n){
+        return solveusingTab(n);
     }
 };
