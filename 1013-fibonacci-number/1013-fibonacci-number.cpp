2d901c6541556877d1fbba374494b7472978ab09
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-29 13:25:28 +0530

    Time: 0 ms (100%), Space: 8.4 MB (11.52%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -14,8 +14,8 @@ public:
 
     int solveusingTab(int n){
         vector<int>dp(n+1,-1);
+         if(n==0||n==1) return n;
         dp[0]=0;
-        if(n==0) return 0;
         dp[1]=1;
         for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
