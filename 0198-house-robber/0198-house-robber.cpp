c24c270188c78f8c9a587b73c609baaffca4a818
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-29 16:09:28 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -50,14 +50,32 @@ public:
     // }
 
 
+    // int solve(vector<int>&nums,int n){
+    //     vector<int>dp(n+2,-1);
+    //     dp[n]=0;
+    //     dp[n+1]=0;
+    //     for(int i=n-1;i>=0;i--){
+    //         dp[i]=max(nums[i]+dp[i+2],dp[i+1]);
+    //     }
+    //     return dp[0];
+    // }
+    // int rob(vector<int>& nums) {
+    //     if(nums.size()==1) return nums[0];
+    //     int n=nums.size();
+    //     return solve(nums,n);
+    // }
+
+
     int solve(vector<int>&nums,int n){
-        vector<int>dp(n+2,-1);
-        dp[n]=0;
-        dp[n+1]=0;
+        int prev1=0;
+        int prev2=0;
+        int curr;
         for(int i=n-1;i>=0;i--){
-            dp[i]=max(nums[i]+dp[i+2],dp[i+1]);
+           curr=max(nums[i]+prev2,prev1);
+           prev2=prev1;
+           prev1=curr;
         }
-        return dp[0];
+        return curr;
     }
     int rob(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
