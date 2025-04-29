17a248c1033f46a4d0a25f3a358871d49533596d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-29 16:01:02 +0530

    Time: 0 ms (100%), Space: 10.6 MB (45.1%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,21 +16,52 @@ public:
     //     return maxi;
     // }
 
+    
+    // int solve(vector<int>&nums,int i,int ans){
+    //     if(i>=nums.size()) {
+    //         return ans;
+    //     }
+    //     int l=solve(nums,i+2,ans+nums[i]);
+    //     int r=solve(nums,i+1,ans);
+    //     return max(l,r);
+    // }
+    // int rob(vector<int>& nums) {
+    //     if(nums.size()==1) return nums[0];
+    //     int ans=0;
+    //     return solve(nums,0,ans);
+    // }
 
-    int solve(vector<int>&nums,int i,vector<int>&dp){
-        if(i>=nums.size()) {
-            return 0;
+     
+    // int solve(vector<int>&nums,int i,vector<int>&dp){
+    //     if(i>=nums.size()) {
+    //         return 0;
+    //     }
+    //     if(dp[i]!=-1)
+    //     return dp[i];
+    //     int l=nums[i]+solve(nums,i+2,dp);
+    //     int r=solve(nums,i+1,dp);
+    //     return dp[i]=max(l,r);
+    // }
+    // int rob(vector<int>& nums) {
+    //     if(nums.size()==1) return nums[0];
+    //     int n=nums.size();
+    //     vector<int>dp(n,-1);
+    //     return solve(nums,0,dp);
+    // }
+
+
+    int solve(vector<int>&nums,int n){
+        vector<int>dp(n+2,-1);
+        dp[n]=0;
+        dp[n+1]=0;
+        for(int i=n-1;i>=0;i--){
+            dp[i]=max(nums[i]+dp[i+2],dp[i+1]);
         }
-        if(dp[i]!=-1)
-        return dp[i];
-        int l=nums[i]+solve(nums,i+2,dp);
-        int r=solve(nums,i+1,dp);
-        return dp[i]=max(l,r);
+        return dp[0];
     }
     int rob(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return solve(nums,0,dp);
+        return solve(nums,n);
     }
 };
