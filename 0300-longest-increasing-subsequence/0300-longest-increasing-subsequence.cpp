b9712d08c5877ac117b6d91298dcafc2ef936d98
HARSHA VARDHAN <hv1900019@gmail.com>
2025-04-30 12:41:40 +0530

    Time: 562 ms (13.84%), Space: 296.6 MB (11.23%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    // void findSubsequences(vector<int>& nums,vector<int>&res,int i,int &maxi){
+    //     if(i>=nums.size()){
+    //     int n=res.size();
+    //     maxi=max(maxi,n);
+    //     return;
+    //     }
+    //     if(res.empty()||res.back()<nums[i]){
+    //     res.push_back(nums[i]);
+    //     findSubsequences(nums,res,i+1,maxi);
+    //     res.pop_back();
+    //     }
+    //     findSubsequences(nums,res,i+1,maxi);
+    // }
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int i=0;
+    //     vector<int>res;
+    //     int maxi=INT_MIN;
+    //     findSubsequences(nums,res,i,maxi);
+    //     return maxi;
+    // }
+
+
+    // int findSubsequences(vector<int>& nums,int i,int prev){
+    //     if(i>=nums.size()){
+    //     return 0;
+    //     }
+    //     int l=0;
+    //     if(prev<nums[i]){
+    //     l=1+findSubsequences(nums,i+1,nums[i]);
+    //     }
+    //     int r=findSubsequences(nums,i+1,prev);
+    //     return max(l,r);
+    // }
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int i=0;
+    //     int n=nums.size();
+    //     return findSubsequences(nums,i,INT_MIN);
+    // }
+
+
+    int findSubsequences(vector<int>& nums,int i,int prev,vector<vector<int>>&dp){
+        if(i>=nums.size()){
+        return 0;
+        }
+        if(dp[prev+1][i]!=-1)
+        return dp[prev+1][i];
+        int l=0;
+        if(prev==-1||nums[prev]<nums[i]){
+        l=1+findSubsequences(nums,i+1,i,dp);
+        }
+        int r=findSubsequences(nums,i+1,prev,dp);
+        return dp[prev+1][i]=max(l,r);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int i=0;
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return findSubsequences(nums,i,-1,dp);
+    }
+};
