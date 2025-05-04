7186cd05cd68c9f32a453c7c9fab990828dfa850
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-04 18:35:09 +0530

    Time: 26 ms (40.67%), Space: 13.9 MB (46.89%) - LeetHub

--- 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
@@ -1,14 +1,47 @@
 class Solution {
 public:
-    int solve(map<pair<int,int>,int>&maxi,int s,int e,vector<vector<int>>&dp){
-        if(s>=e) return 0;
-        if(dp[s][e]!=-1)
-        return dp[s][e];
-        int ans=INT_MAX;
+    // int solve(map<pair<int,int>,int>&maxi,int s,int e,vector<vector<int>>&dp){
+    //     if(s>=e) return 0;
+    //     if(dp[s][e]!=-1)
+    //     return dp[s][e];
+    //     int ans=INT_MAX;
+    //     for(int i=s;i<e;i++){
+    //        ans=min(ans,maxi[{s,i}]*maxi[{i+1,e}]+solve(maxi,s,i,dp)+solve(maxi,i+1,e,dp));
+    //     }
+    //     return dp[s][e]=ans;
+    // }
+    //  int mctFromLeafValues(vector<int>& arr) {
+    //     int n=arr.size();
+    //     map<pair<int,int>,int> maxi;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i;j<n;j++){
+    //             if(i==j)
+    //             maxi[{i,j}]=arr[j];
+    //             else{
+    //                 maxi[{i,j}]=max(maxi[{i,j-1}],arr[j]);
+    //             }
+    //         }
+    //     }
+    //     vector<vector<int>>dp(n,vector<int>(n,-1));
+    //     return solve(maxi,0,n-1,dp);
+    // }
+
+    int solve(map<pair<int,int>,int>&maxi,int n){
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int s=n-1;s>=0;s--){
+            for(int e=0;e<=n-1;e++){
+                if(s>=e)
+                continue;
+                else{
+         int ans=INT_MAX;
         for(int i=s;i<e;i++){
-           ans=min(ans,maxi[{s,i}]*maxi[{i+1,e}]+solve(maxi,s,i,dp)+solve(maxi,i+1,e,dp));
+           ans=min(ans,maxi[{s,i}]*maxi[{i+1,e}]+dp[s][i]+dp[i+1][e]);
         }
-        return dp[s][e]=ans;
+        dp[s][e]=ans;
+                }
+            }
+        }
+        return dp[0][n-1];
     }
     int mctFromLeafValues(vector<int>& arr) {
         int n=arr.size();
@@ -22,7 +55,6 @@ public:
                 }
             }
         }
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return solve(maxi,0,n-1,dp);
+        return solve(maxi,n);
     }
 };
