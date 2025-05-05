a90a5f8244f4fbfc0b84c3a4ec9c17249e358a58
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-05 17:57:05 +0530

    Time: 32 ms (55.48%), Space: 31.6 MB (21.72%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -28,26 +28,61 @@ public:
     //     return solve(p,s,0,0,dp);
     // }
 
-    bool solve(string &p,string &q,int i,int j,vector<vector<int>>&dp,vector<int>&star){
-        if(i>=p.length()&&j>=q.length()) return 1;
-        if(i>=p.length()) return 0;
-        if(j>=q.length()){
-           int len=p.length()-i;
-           return len==star[i];
-        }
-        if(dp[i][j]!=-1)
-        return dp[i][j];
+    // bool solve(string &p,string &q,int i,int j,vector<vector<int>>&dp,vector<int>&star){
+    //     if(i>=p.length()&&j>=q.length()) return 1;
+    //     if(i>=p.length()) return 0;
+    //     if(j>=q.length()){
+    //        int len=p.length()-i;
+    //        return len==star[i];
+    //     }
+    //     if(dp[i][j]!=-1)
+    //     return dp[i][j];
+    //     int ans=0;
+    //     if(p[i]=='?'||p[i]==q[j])
+    //     ans=ans||solve(p,q,i+1,j+1,dp,star);
+    //     else if(p[i]=='*')
+    //     ans=ans||solve(p,q,i,j+1,dp,star)||solve(p,q,i+1,j,dp,star);
+    //     return dp[i][j]=ans;
+    // }
+    //  bool isMatch(string s, string p) {
+    //     int m=p.size();
+    //     int n=s.size();
+    //     vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+    //     vector<int>star(m+1,0);
+    //     star[m]=0;
+    //     for(int i=m-1;i>=0;i--){
+    //         if(p[i]=='*')
+    //         star[i]=1+star[i+1];
+    //         else
+    //         star[i]=star[i+1];
+    //     }
+    //     return solve(p,s,0,0,dp,star);
+    // }
+
+
+    bool solve(string &p,string &q,int m,int n,vector<int>&star){
+      vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+      dp[m][n]=1;
+      for(int i=0;i<=m;i++){
+        int len=p.length()-i;
+        if(len==star[i])
+        dp[i][n]=1;
+      }
+      for(int i=m-1;i>=0;i--){
+        for(int j=n-1;j>=0;j--){
         int ans=0;
         if(p[i]=='?'||p[i]==q[j])
-        ans=ans||solve(p,q,i+1,j+1,dp,star);
+        ans=ans||dp[i+1][j+1];
         else if(p[i]=='*')
-        ans=ans||solve(p,q,i,j+1,dp,star)||solve(p,q,i+1,j,dp,star);
-        return dp[i][j]=ans;
+        ans=ans||dp[i][j+1]||dp[i+1][j];
+        dp[i][j]=ans;
+        }
+      }
+      return dp[0][0];
     }
-     bool isMatch(string s, string p) {
+    bool isMatch(string s, string p) {
         int m=p.size();
         int n=s.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
         vector<int>star(m+1,0);
         star[m]=0;
         for(int i=m-1;i>=0;i--){
@@ -56,20 +91,6 @@ public:
             else
             star[i]=star[i+1];
         }
-        return solve(p,s,0,0,dp,star);
+        return solve(p,s,m,n,star);
     }
-
-
-    // bool solve(string &p,string &q,int i,int j){
-    //   vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-    //   dp[m][n]=1;
-    //   for(int i=0;i<=m;j++){
-        
-    //   }
-    // }
-    // bool isMatch(string s, string p) {
-    //     int m=p.size();
-    //     int n=s.size();
-    //     return solve(p,s,m,n);
-    // }
 };
