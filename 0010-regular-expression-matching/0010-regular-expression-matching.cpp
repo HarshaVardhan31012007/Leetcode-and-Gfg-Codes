54393fb8ca01919f4a7dfd58caf2f7d0c0184b4b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-06 08:47:01 +0530

    Time: 0 ms (100%), Space: 9.6 MB (20.45%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
-    bool solve(string &s,string &p,int i,int j){
+    // bool solve(string &s,string &p,int i,int j){
+    //   if(i==s.length()&&j==p.length()) return 1;
+    //   if(j==p.length()) return 0;
+    //   bool curr=i<s.length()&&(p[j]=='.'||s[i]==p[j]);
+    //   if(j+1<p.length()&&p[j+1]=='*'){
+    //     int emptycase=solve(s,p,i,j+2);
+    //     int precase=curr&&solve(s,p,i+1,j);
+    //     return emptycase||precase;
+    //   }
+    //   else if(curr)
+    //   return solve(s,p,i+1,j+1);
+    //   else
+    //   return false;
+    // }
+    // bool isMatch(string s, string p) {
+    //     return solve(s,p,0,0);
+    // }
+
+
+     bool solve(string &s,string &p,int i,int j,vector<vector<int>>&dp){
       if(i==s.length()&&j==p.length()) return 1;
       if(j==p.length()) return 0;
+      if(dp[i][j]!=-1)
+      return dp[i][j];
       bool curr=i<s.length()&&(p[j]=='.'||s[i]==p[j]);
       if(j+1<p.length()&&p[j+1]=='*'){
-        int emptycase=solve(s,p,i,j+2);
-        int precase=curr&&solve(s,p,i+1,j);
-        return emptycase||precase;
+        int emptycase=solve(s,p,i,j+2,dp);
+        int precase=curr&&solve(s,p,i+1,j,dp);
+        return dp[i][j]=emptycase||precase;
       }
       else if(curr)
-      return solve(s,p,i+1,j+1);
+      return dp[i][j]=solve(s,p,i+1,j+1,dp);
       else
-      return false;
+      return dp[i][j]=false;
     }
     bool isMatch(string s, string p) {
-        return solve(s,p,0,0);
+        int m=s.size();
+        int n=p.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return solve(s,p,0,0,dp);
     }
 };
