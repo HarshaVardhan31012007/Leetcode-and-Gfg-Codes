4d995d319351bd50d1138bc6c36769a8ad6f49b8
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-08 13:41:13 +0530

    Time: 71 ms (41.81%), Space: 13.3 MB (42.61%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&num,vector<int>&dp){
-        if(dp[n]!=-1)
-        return dp[n];
-        int ans=INT_MAX;
-        for(auto &each:num){
-            if(each<=n)
-            ans=min(ans,1+solve(n-each,num,dp));
+    // int solve(int n,vector<int>&num,vector<int>&dp){
+    //     if(dp[n]!=-1)
+    //     return dp[n];
+    //     int ans=INT_MAX;
+    //     for(auto &each:num){
+    //         if(each<=n)
+    //         ans=min(ans,1+solve(n-each,num,dp));
+    //     }
+    //     return dp[n]=(ans==INT_MAX)?0:ans;
+    // }
+    // int numSquares(int n) {
+    //     double a=sqrt(n);
+    //     if(a==(int)a) return 1;
+    //     vector<int>num;
+    //     for(int i=1;i<n;i++){
+    //         if(sqrt(i)==(int)sqrt(i))
+    //         num.push_back(i);
+    //     }
+    //     vector<int>dp(n+1,-1);
+    //    return solve(n,num,dp);
+    // }
+
+
+     int solve(int n,vector<int>&num){
+        vector<int>dp(n+1,0);
+        for(int i=1;i<=n;i++){
+            int ans=INT_MAX;
+            for(auto &each:num){
+            if(each<=i)
+            ans=min(ans,1+dp[i-each]);
         }
-        return dp[n]=(ans==INT_MAX)?0:ans;
+        dp[i]=(ans==INT_MAX)?0:ans;
+        }
+        return dp[n];
     }
     int numSquares(int n) {
         double a=sqrt(n);
@@ -18,7 +43,6 @@ public:
             if(sqrt(i)==(int)sqrt(i))
             num.push_back(i);
         }
-        vector<int>dp(n+1,-1);
-       return solve(n,num,dp);
+       return solve(n,num);
     }
 };
