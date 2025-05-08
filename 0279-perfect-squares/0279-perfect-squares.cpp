cc695c0371c600102b98ec6da4e6358d47dc5383
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-08 14:49:17 +0530

    Time: 65 ms (45.28%), Space: 13.4 MB (42.61%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,44 +1,19 @@
 class Solution {
 public:
-     int solve(int n,vector<int>&num,vector<int>&dp){
-        if(n==0)
-        return 0;
-        if(dp[n]!=-1)
-        return dp[n];
-        int ans=INT_MAX;
-        for(auto &each:num){
-            if(each<=n){
-            int t=solve(n-each,num,dp);
-            if(t!=INT_MAX)
-            ans=min(ans,1+t);
-            }
-        }
-        return dp[n]=ans;
-    }
-    int numSquares(int n) {
-        double a=sqrt(n);
-        if(a==(int)a) return 1;
-        vector<int>num;
-        for(int i=1;i<n;i++){
-            if(sqrt(i)==(int)sqrt(i))
-            num.push_back(i);
-        }
-        vector<int>dp(n+1,-1);
-       return solve(n,num,dp);
-    }
-
-
-    //  int solve(int n,vector<int>&num){
-    //     vector<int>dp(n+1,0);
-    //     for(int i=1;i<=n;i++){
-    //         int ans=INT_MAX;
-    //         for(auto &each:num){
-    //         if(each<=i)
-    //         ans=min(ans,1+dp[i-each]);
-    //     }
-    //     dp[i]=(ans==INT_MAX)?0:ans;
-    //     }
+    //  int solve(int n,vector<int>&num,vector<int>&dp){
+    //     if(n==0)
+    //     return 0;
+    //     if(dp[n]!=-1)
     //     return dp[n];
+    //     int ans=INT_MAX;
+    //     for(auto &each:num){
+    //         if(each<=n){
+    //         int t=solve(n-each,num,dp);
+    //         if(t!=INT_MAX)
+    //         ans=min(ans,1+t);
+    //         }
+    //     }
+    //     return dp[n]=ans;
     // }
     // int numSquares(int n) {
     //     double a=sqrt(n);
@@ -48,6 +23,34 @@ public:
     //         if(sqrt(i)==(int)sqrt(i))
     //         num.push_back(i);
     //     }
-    //    return solve(n,num);
+    //     vector<int>dp(n+1,-1);
+    //    return solve(n,num,dp);
     // }
+
+
+     int solve(int n,vector<int>&num){
+        vector<int>dp(n+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            int ans=INT_MAX;
+            for(auto &each:num){
+            if(each<=i){
+            if(dp[i-each]!=INT_MAX)
+            ans=min(ans,1+dp[i-each]);
+            }
+        }
+        dp[i]=ans;
+        }
+        return dp[n];
+    }
+    int numSquares(int n) {
+        double a=sqrt(n);
+        if(a==(int)a) return 1;
+        vector<int>num;
+        for(int i=1;i<n;i++){
+            if(sqrt(i)==(int)sqrt(i))
+            num.push_back(i);
+        }
+       return solve(n,num);
+    }
 };
