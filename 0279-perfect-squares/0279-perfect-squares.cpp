321bdeed2198e5534fd8a53820601588e598a5e7
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-08 15:40:31 +0530

    Time: 79 ms (34.26%), Space: 13.9 MB (33.63%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -56,7 +56,7 @@ public:
 
 
     int solve(int n,vector<int>&dp){
-        if(n==0) return 1;
+        if(n==0) return 0;
         if(dp[n]!=-1)
         return dp[n];
         int end=sqrt(n);
@@ -70,6 +70,6 @@ public:
     }
     int numSquares(int n){
         vector<int>dp(n+1,-1);
-        return solve(n,dp)-1;
+        return solve(n,dp);
     }
 };
