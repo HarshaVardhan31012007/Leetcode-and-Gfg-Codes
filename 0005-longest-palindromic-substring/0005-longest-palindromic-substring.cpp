beb83305e2807e73c6e2938b34e846bb006392b5
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-09 22:10:36 +0530

    Time: 407 ms (15.1%), Space: 292.7 MB (9.16%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,44 +1,50 @@
 class Solution {
 public:
-    bool checkPalindrome(string &k,int i,int j){
-        while(i<j){
-            if(k[i]!=k[j]){
-                return false;
-            }
-            i++;j--;
-        }
-        return true;
-    }
-    string longestPalindrome(string s) {
-        string ans="";
-        for(int i=0;i<s.length();i++){
-            for(int j=i;j<s.length();j++){
-                 if(j-i+1>ans.size()&&checkPalindrome(s,i,j)){
-                    ans=s.substr(i,j-i+1);
-                 }
-            }
-        }
-        return ans;
-    }
-
-
-    // string solve(string &s,int i,int j){
-    //   if(i>j) return "";
-    //   if(i==j){
-    //     string k="";
-    //     k+=s[i];
-    //     return k;
-    //   } 
-    //   if(s[i]==s[j])
-    //   return s[i]+solve(s,i+1,j-1)+s[j];
-    //   else{
-    //     string a=solve(s,i+1,j);string b=solve(s,i,j-1);
-    //     if(a.length()<b.length()) return b;
-    //     else return a;
-    //   }
+    // bool checkPalindrome(string &k,int i,int j){
+    //     while(i<j){
+    //         if(k[i]!=k[j]){
+    //             return false;
+    //         }
+    //         i++;j--;
+    //     }
+    //     return true;
     // }
     // string longestPalindrome(string s) {
-    //    int n=s.length();
-    //    return solve(s,0,n-1);
+    //     string ans="";
+    //     for(int i=0;i<s.length();i++){
+    //         for(int j=i;j<s.length();j++){
+    //              if(j-i+1>ans.size()&&checkPalindrome(s,i,j)){
+    //                 ans=s.substr(i,j-i+1);
+    //              }
+    //         }
+    //     }
+    //     return ans;
     // }
+
+    int maxlen=1;int start=0;
+    bool solve(string &s,int i,int j,vector<vector<int>>&dp){
+        if(i>=j) return true;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        bool flag=0;
+        if(s[i]==s[j])
+        flag=solve(s,i+1,j-1,dp);
+        if(flag){
+            if(maxlen<j-i+1){
+                start=i;
+                maxlen=j-i+1;
+            }
+        }
+        return dp[i][j]=flag;
+    }
+    string longestPalindrome(string s) {
+        int n=s.length();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1)); 
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                solve(s,i,j,dp);
+            }
+        }
+        return s.substr(start,maxlen);
+    }
 };
