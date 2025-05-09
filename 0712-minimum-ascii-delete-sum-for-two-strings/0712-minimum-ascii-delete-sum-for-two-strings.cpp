24828aa2e22607b0d70660ecfa48ce1d0efbc642
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-09 15:18:31 +0530

    Time: 13 ms (78.11%), Space: 18.4 MB (26.01%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,13 +1,39 @@
 class Solution {
 public:
-    int solve(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
-      if(i>=s1.size()||j>=s2.size()) return 0;
-      if(dp[i][j]!=-1)
-      return dp[i][j];
-      if(s1[i]==s2[j])
-      return dp[i][j]=s1[i]+solve(s1,s2,i+1,j+1,dp);
-      else
-      return dp[i][j]=max(solve(s1,s2,i+1,j,dp),solve(s1,s2,i,j+1,dp));
+    // int solve(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
+    //   if(i>=s1.size()||j>=s2.size()) return 0;
+    //   if(dp[i][j]!=-1)
+    //   return dp[i][j];
+    //   if(s1[i]==s2[j])
+    //   return dp[i][j]=s1[i]+solve(s1,s2,i+1,j+1,dp);
+    //   else
+    //   return dp[i][j]=max(solve(s1,s2,i+1,j,dp),solve(s1,s2,i,j+1,dp));
+    // }
+    // int minimumDeleteSum(string s1, string s2) {
+    //     int sum=0;
+    //     for(auto &i:s1)
+    //     sum=sum+i;
+    //     for(auto &j:s2)
+    //     sum=sum+j;
+    //     int m=s1.size();int n=s2.size();
+    //     vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+    //     return sum-2*solve(s1,s2,0,0,dp);
+    // }
+
+
+
+    int solve(string &s1,string &s2){
+        int m=s1.size();int n=s2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(s1[i]==s2[j])
+                dp[i][j]=s1[i]+dp[i+1][j+1];
+                else
+                dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
     }
     int minimumDeleteSum(string s1, string s2) {
         int sum=0;
@@ -15,8 +41,6 @@ public:
         sum=sum+i;
         for(auto &j:s2)
         sum=sum+j;
-        int m=s1.size();int n=s2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return sum-2*solve(s1,s2,0,0,dp);
+        return sum-2*solve(s1,s2);
     }
 };
