d4652ff76473da7d26a0f4a19ded7418965607b8
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-09 15:56:36 +0530

    Time: 15 ms (72.92%), Space: 18.3 MB (51.44%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -61,25 +61,50 @@ public:
     // }
 
 
-    int solve(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
-        if(i==s1.size()||j==s2.size()){
-            int cost=0;
-            for(int x=i;x<s1.size();x++) cost+=s1[x];
-            for(int x=j;x<s2.size();x++) cost+=s2[x];
-            return cost; 
-        }
-         if(dp[i][j]!=-1)
-        return dp[i][j];
+    // int solve(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
+    //     if(i==s1.size()||j==s2.size()){
+    //         int cost=0;
+    //         for(int x=i;x<s1.size();x++) cost+=s1[x];
+    //         for(int x=j;x<s2.size();x++) cost+=s2[x];
+    //         return cost; 
+    //     }
+    //      if(dp[i][j]!=-1)
+    //     return dp[i][j];
+    //     if(s1[i]==s2[j])
+    //     return dp[i][j]=solve(s1,s2,i+1,j+1,dp);
+    //     else
+    //     return dp[i][j]=min(s1[i]+solve(s1,s2,i+1,j,dp),s2[j]+solve(s1,s2,i,j+1,dp));
+    // }
+    // int minimumDeleteSum(string s1, string s2) {
+    //    int m=s1.size();
+    //    int n=s2.size();
+    //    vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+    //    return solve(s1,s2,0,0,dp);
+    // }
+
+
+    int solve(string &s1,string &s2){
+     int m=s1.size();
+       int n=s2.size();
+       vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+       for(int i=m-1;i>=0;i--){
+        dp[i][n]=s1[i]+dp[i+1][n];
+       }
+       for(int j=n-1;j>=0;j--){
+        dp[m][j]=s2[j]+dp[m][j+1];
+       }
+       for(int i=m-1;i>=0;i--){
+        for(int j=n-1;j>=0;j--){
         if(s1[i]==s2[j])
-        return dp[i][j]=solve(s1,s2,i+1,j+1,dp);
+        dp[i][j]=dp[i+1][j+1];
         else
-        return dp[i][j]=min(s1[i]+solve(s1,s2,i+1,j,dp),s2[j]+solve(s1,s2,i,j+1,dp));
+        dp[i][j]=min(s1[i]+dp[i+1][j],s2[j]+dp[i][j+1]);
+        }
+       }
+       return dp[0][0];
     }
     int minimumDeleteSum(string s1, string s2) {
-       int m=s1.size();
-       int n=s2.size();
-       vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-       return solve(s1,s2,0,0,dp);
+       return solve(s1,s2);
     }
 
 };
