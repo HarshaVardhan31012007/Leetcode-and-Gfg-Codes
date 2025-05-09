b58601b3770134db44df002edd5df0768b51e2b4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-09 15:20:26 +0530

    Time: 3 ms (99.84%), Space: 9.5 MB (95.31%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -22,18 +22,34 @@ public:
 
 
 
+    // int solve(string &s1,string &s2){
+    //     int m=s1.size();int n=s2.size();
+    //     vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+    //     for(int i=m-1;i>=0;i--){
+    //         for(int j=n-1;j>=0;j--){
+    //             if(s1[i]==s2[j])
+    //             dp[i][j]=s1[i]+dp[i+1][j+1];
+    //             else
+    //             dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+
     int solve(string &s1,string &s2){
         int m=s1.size();int n=s2.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        vector<int>curr(n+1,0);
+        vector<int>next(n+1,0);
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 if(s1[i]==s2[j])
-                dp[i][j]=s1[i]+dp[i+1][j+1];
+                curr[j]=s1[i]+next[j+1];
                 else
-                dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                curr[j]=max(next[j],curr[j+1]);
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
     int minimumDeleteSum(string s1, string s2) {
         int sum=0;
