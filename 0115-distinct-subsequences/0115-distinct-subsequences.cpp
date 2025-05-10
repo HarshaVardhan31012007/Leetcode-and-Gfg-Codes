1877c2ee3abc6d57a7787b59d62330be912a6fe2
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-10 13:59:39 +0530

    Time: 27 ms (72.51%), Space: 26.9 MB (64.38%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -56,20 +56,41 @@ public:
     // }
 
 
+    //  int solve(string &s,string &t){
+    //    int m=s.size();int n=t.size();
+    //     vector<int>curr(n+1,0);
+    //     curr[n]=1;
+    //     for(int i=m-1;i>=0;i--){
+    //         for(int j=0;j<=n-1;j++){
+    //                 long long ans=0;
+    //                 if(s[i]==t[j])
+    //                 ans+=curr[j+1];
+    //                 ans+=curr[j];
+    //                 curr[j]=ans;
+    //         }
+    //     }
+    //     return curr[0];
+    // }
+    // int numDistinct(string s, string t) {
+    //     return solve(s,t);
+    // }
+
+
      int solve(string &s,string &t){
        int m=s.size();int n=t.size();
-        vector<int>curr(n+1,0);
-        curr[n]=1;
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<=m;i++)
+        dp[i][n]=1;
         for(int i=m-1;i>=0;i--){
-            for(int j=0;j<=n-1;j++){
-                    long long ans=0;
+            for(int j=n-1;j>=0;j--){
+                    long long int l=0;
                     if(s[i]==t[j])
-                    ans+=curr[j+1];
-                    ans+=curr[j];
-                    curr[j]=ans;
+                    l=dp[i+1][j+1];
+                    long long int r=dp[i+1][j];
+                     dp[i][j]=l+r;
             }
         }
-        return curr[0];
+        return dp[0][0];
     }
     int numDistinct(string s, string t) {
         return solve(s,t);
