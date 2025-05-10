5e133dec31ce49dd6bcfd330fce59b691146d620
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-10 23:11:35 +0530

    Time: 2305 ms (5.04%), Space: 8 MB (64.76%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
+    int solve(int s,int e){
+        if(s>=e) return 1;
+        int count=0;
+        for(int i=s;i<=e;i++){
+          int l=solve(s,i-1);
+          int r=solve(i+1,e);
+          count+=l*r;
+        }
+        return count;
+    }
     int numTrees(int n) {
+        //method 1
         // if(n==0||n==1)
         // return  1;
         // int ans=0;
@@ -9,14 +20,19 @@ public:
         // }
         // return ans;
 
+        //Method 2 //dp
 
-        vector<int>result(n+1,0);
-        result[0]=result[1]=1;
-        for(int i=2;i<=n;i++){
-            for(int j=0;j<i;j++){
-                result[i]+=result[j]*result[i-j-1];
-            }
-        }
-        return result[n];
+        // vector<int>result(n+1,0);
+        // result[0]=result[1]=1;
+        // for(int i=2;i<=n;i++){
+        //     for(int j=0;j<i;j++){
+        //         result[i]+=result[j]*result[i-j-1];
+        //     }
+        // }
+        // return result[n];
+
+
+        //method 3
+        return solve(1,n);
     }
 };
