cbca623840897b75653fba8541e9bcab7a8c6dcf
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-10 13:53:46 +0530

    Time: 27 ms (72.51%), Space: 27 MB (64.38%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
-    int solve(string &s,string &t,int i,int j,vector<vector<int>>&dp){
-       if(j>=t.size())
-         return 1;
-       if(i>=s.size()) 
-       return 0;
-       if(dp[i][j]!=-1)
-       return dp[i][j];
-       int l=0;
-       if(s[i]==t[j])
-       l=solve(s,t,i+1,j+1,dp);
-       int r=solve(s,t,i+1,j,dp);
-       return dp[i][j]=l+r;
+    // int solve(string &s,string &t,int i,int j,vector<vector<int>>&dp){
+    //    if(j>=t.size())
+    //      return 1;
+    //    if(i>=s.size()) 
+    //    return 0;
+    //    if(dp[i][j]!=-1)
+    //    return dp[i][j];
+    //    int l=0;
+    //    if(s[i]==t[j])
+    //    l=solve(s,t,i+1,j+1,dp);
+    //    int r=solve(s,t,i+1,j,dp);
+    //    return dp[i][j]=l+r;
+    // }
+    // int numDistinct(string s, string t) {
+    //     int m=s.size();int n=t.size();
+    //     vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+    //     return solve(s,t,0,0,dp);
+    // }
+
+
+    long long int solve(string &s,string &t){
+       int m=s.size();int n=t.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=0;i<=m;i++)
+        dp[i][n]=1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                    long long ans=0;
+                    if(s[i]==t[j])
+                    ans+=dp[i+1][j+1];
+                    ans+=dp[i+1][j];
+                     dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
     }
-    int numDistinct(string s, string t) {
-        int m=s.size();int n=t.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solve(s,t,0,0,dp);
+    long long int numDistinct(string s, string t) {
+        return solve(s,t);
     }
 };
