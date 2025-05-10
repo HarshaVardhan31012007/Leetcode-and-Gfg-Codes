96cc7512d8a789bfa8b075797c34981cc2a55321
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-10 16:56:18 +0530

    Time: 0 ms (100%), Space: 10.2 MB (72.22%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,12 +1,35 @@
 class Solution {
 public:
-   bool check(string &temp,vector<string>& wordDict){
-        for(auto &i:wordDict){
-           if(i==temp)
-           return true;
-        }
-        return false;
-    }
+//    bool check(string &temp,vector<string>& wordDict){
+//         for(auto &i:wordDict){
+//            if(i==temp)
+//            return true;
+//         }
+//         return false;
+//     }
+//     void solve(int i,string &s,vector<string>& wordDict,vector<string>&ans,string t){
+//         if(i==s.size()) {
+//             t.pop_back();
+//             ans.push_back(t);
+//         }
+//         string temp="";
+//         for(int j=i;j<s.length();j++){
+//             temp=temp+s[j];
+//             if(check(temp,wordDict)){
+//              solve(j+1,s,wordDict,ans,t+temp+" ");
+//             }
+//         }
+//     }
+//     vector<string> wordBreak(string s, vector<string>& wordDict) {
+//         vector<string>ans;
+//         string t="";
+//         solve(0,s,wordDict,ans,t);
+//         return ans;
+//     }
+
+
+
+    unordered_map<string,bool>mpp;
     void solve(int i,string &s,vector<string>& wordDict,vector<string>&ans,string t){
         if(i==s.size()) {
             t.pop_back();
@@ -15,7 +38,7 @@ public:
         string temp="";
         for(int j=i;j<s.length();j++){
             temp=temp+s[j];
-            if(check(temp,wordDict)){
+            if(mpp[temp]==1){
              solve(j+1,s,wordDict,ans,t+temp+" ");
             }
         }
@@ -23,6 +46,8 @@ public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         vector<string>ans;
         string t="";
+        for(auto &i:wordDict)
+        mpp[i]=1;
         solve(0,s,wordDict,ans,t);
         return ans;
     }
