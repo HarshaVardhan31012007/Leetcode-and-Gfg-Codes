2fe961a22da15c347e0ffff22510a279394fee74
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-10 22:51:56 +0530

    Time: 0 ms (100%), Space: 8.1 MB (20.34%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int numTrees(int n) {
-        if(n==0||n==1)
-        return  1;
-        int ans=0;
-        for(int i=0;i<n;i++){
-             ans+=numTrees(i)*numTrees(n-i-1);
+        // if(n==0||n==1)
+        // return  1;
+        // int ans=0;
+        // for(int i=0;i<n;i++){
+        //      ans+=numTrees(i)*numTrees(n-i-1);
+        // }
+        // return ans;
+
+
+        vector<int>result(n+1,0);
+        result[0]=result[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<i;j++){
+                result[i]+=result[j]*result[i-j-1];
+            }
         }
-        return ans;
+        return result[n];
     }
 };
