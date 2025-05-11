3e7c1584109b583e334d24d569ff55a249621829
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-11 12:31:38 +0530

    Time: 0 ms (100%), Space: 10.4 MB (72.54%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
+    // bool ans=0;
+    // void solve(vector<int>&piles,int i,int j,int flag,int ans1,int ans2){
+    //     if(i>j){
+    //        if(!ans)
+    //        ans=ans1>ans2;
+    //        return;
+    //     }
+    //     if(flag){
+    //     solve(piles,i+1,j,!flag,ans1+piles[i],ans2);
+    //     solve(piles,i,j-1,!flag,ans1+piles[j],ans2);
+    //     }
+    //     else{
+    //     solve(piles,i+1,j,!flag,ans1,ans2+piles[i]);
+    //     solve(piles,i,j-1,!flag,ans1,ans2+piles[j]);
+    //     }
+    // }
+    // bool stoneGame(vector<int>& piles) {
+    //     int n=piles.size();
+    //     solve(piles,0,n-1,1,0,0);
+    //     return ans;
+    // }
+
+    bool stoneGame(vector<int>&piles){
         return 1;
     }
 };
