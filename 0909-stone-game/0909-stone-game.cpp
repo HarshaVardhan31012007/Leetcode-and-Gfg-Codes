1629504c2465e06b73298f45046a78114eff1e43
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-11 13:38:22 +0530

    Time: 12 ms (32%), Space: 20 MB (12.31%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -2,63 +2,43 @@ class Solution {
 public:
     //for even and odd no of piles
 
-    int solve(vector<int>&piles,int i,int j,vector<vector<int>>&dp){
-       if(i>j) return 0;
-       if(dp[i][j]!=-1) return dp[i][j];
-       if((j-i+1)%2==0){
-        return dp[i][j]=max(piles[i]+solve(piles,i+1,j,dp),piles[j]+solve(piles,i,j-1,dp));
-       }
-       else{
-        return dp[i][j]=min(-piles[i]+solve(piles,i+1,j,dp),-piles[j]+solve(piles,i,j-1,dp));
-       }
-    }
-    bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(piles,0,n-1,dp)>0;
-    }
-
-
-    //    int flag=1;
-    //   int solve(vector<int>&piles,int i,int j,vector<vector<int>>&dp){
+    // int solve(vector<int>&piles,int i,int j,vector<vector<int>>&dp){
     //    if(i>j) return 0;
     //    if(dp[i][j]!=-1) return dp[i][j];
-    //    if(flag){
-    //    int l=piles[i]+piles(piles,i+1,j);
-    //    int r=piles[j]+solve(piles,i,j-1);
-    //    return dp[i][j]=max(l,r);
+    //    if((j-i+1)%2==0){
+    //     return dp[i][j]=max(piles[i]+solve(piles,i+1,j,dp),piles[j]+solve(piles,i,j-1,dp));
     //    }
     //    else{
-    //      int l=piles[i]+piles(piles,i+1,j);
-    //    int r=piles[j]+solve(piles,i,j-1);
-    //    return dp[i][j]=max(l,r);
+    //     return dp[i][j]=min(-piles[i]+solve(piles,i+1,j,dp),-piles[j]+solve(piles,i,j-1,dp));
     //    }
     // }
     // bool stoneGame(vector<int>& piles) {
     //     int n=piles.size();
-    //     int sum=accumulate(piles.begin(),piles.end(),0);
     //     vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-    //     int ans=solve(piles,0,n-1,dp);
-    //     return ans>sum-ans;
-    //}
+    //     return solve(piles,0,n-1,dp)>0;
+    // }
+
+
     
-    //  int solve(vector<int>&piles){
-    //     int n=piles.size();
-    //     int sum=accumulate(piles.begin(),piles.end(),0);
-    //     vector<vector<int>>dp(n+2,vector<int>(n+2,0));
-    //     for(int i=n-1;i>=0;i--){
-    //         for(int j=0;j<=n-1;j++){
-    //         if(i>j) continue;
-    //         int l=piles[i]+max(dp[i+2][j],dp[i+1][j-1]);
-    //         int r=piles[j]+max(dp[i+1][j-1],dp[i][j-2]);
-    //         dp[i][j]=max(l,r);
-    //         }
-    //     }
-    //     return sum>sum-dp[0][n-1];
-    // }
-    // bool stoneGame(vector<int>& piles) {
-    //     return solve(piles);
-    // }
+     int solve(vector<int>&piles){
+       int n=piles.size();
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=n-1;j++){
+                if(i>j) continue;
+                    if((j-i+1)%2==0){
+        dp[i+1][j+1]=max(piles[i]+dp[i+2][j+1],piles[j]+dp[i+1][j]);
+       }
+       else{
+        dp[i+1][j+1]=min(-piles[i]+dp[i+2][j+1],-piles[j]+dp[i+1][j]);
+            }
+        }
+        }
+        return dp[1][n];
+    }
+    bool stoneGame(vector<int>& piles) {
+        return solve(piles)>0;
+    }
 
     //for even piles only
 
