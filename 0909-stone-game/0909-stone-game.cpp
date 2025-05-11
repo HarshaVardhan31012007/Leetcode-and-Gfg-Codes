3d78861b5609540b9503efd5feef1b6c077c36e7
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-11 12:54:24 +0530

    Time: 13 ms (30.69%), Space: 19.6 MB (25.11%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    // bool ans=0;
-    // void solve(vector<int>&piles,int i,int j,int flag,int ans1,int ans2){
-    //     if(i>j){
-    //        if(!ans)
-    //        ans=ans1>ans2;
-    //        return;
-    //     }
-    //     if(flag){
-    //     solve(piles,i+1,j,!flag,ans1+piles[i],ans2);
-    //     solve(piles,i,j-1,!flag,ans1+piles[j],ans2);
-    //     }
-    //     else{
-    //     solve(piles,i+1,j,!flag,ans1,ans2+piles[i]);
-    //     solve(piles,i,j-1,!flag,ans1,ans2+piles[j]);
-    //     }
-    // }
-    // bool stoneGame(vector<int>& piles) {
-    //     int n=piles.size();
-    //     solve(piles,0,n-1,1,0,0);
-    //     return ans;
-    // }
-
-    bool stoneGame(vector<int>&piles){
-        return 1;
+    int solve(vector<int>&piles,int i,int j,vector<vector<int>>&dp){
+       if(i>j) return 0;
+       if(dp[i][j]!=-1) return dp[i][j];
+       int l=piles[i]+max(solve(piles,i+2,j,dp),solve(piles,i+1,j-1,dp));
+       int r=piles[j]+max(solve(piles,i+1,j-1,dp),solve(piles,i,j-2,dp));
+       return dp[i][j]=max(l,r);
     }
+    bool stoneGame(vector<int>& piles) {
+        int n=piles.size();
+        int sum=accumulate(piles.begin(),piles.end(),0);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        int ans=solve(piles,0,n-1,dp);
+        return ans>sum-ans;
+    }
+
+    // bool stoneGame(vector<int>&piles){
+    //     return 1;
+    // }
 };
