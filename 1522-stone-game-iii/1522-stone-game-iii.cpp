6dc663584e9889a6236c9a0d71318ce99ea49c8b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-12 17:05:02 +0530

    Time: 272 ms (16.47%), Space: 219.1 MB (17.19%) - LeetHub

--- 1522-stone-game-iii/1522-stone-game-iii.cpp
+++ 1522-stone-game-iii/1522-stone-game-iii.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
-    int solve(vector<int>& arr,int n,int i,int flag,vector<vector<int>>&dp){
-        if(i==arr.size()) return 0;
-        if(dp[i][flag]!=-1) return dp[i][flag];
+    // int solve(vector<int>& arr,int n,int i,int flag,vector<vector<int>>&dp){
+    //     if(i==arr.size()) return 0;
+    //     if(dp[i][flag]!=-1) return dp[i][flag];
+    //     int total=0;
+    //     int ans=flag==1?INT_MIN:INT_MAX;
+    //     for(int j=0;j<3;j++){
+    //      if(i+j>=arr.size()) break;
+    //      total+=arr[i+j];
+    //      if(flag)
+    //      ans=max(ans,total+solve(arr,n,i+j+1,!flag,dp));
+    //      else
+    //      ans=min(ans,-total+solve(arr,n,i+j+1,!flag,dp));
+    //     }
+    //     return dp[i][flag]=ans;
+    // }
+    // string stoneGameIII(vector<int>& stoneValue) {
+    //    int n=stoneValue.size();
+    //    vector<vector<int>>dp(n+1,vector<int>(2,-1));
+    //    int ans=solve(stoneValue,n,0,1,dp);
+    //    if(ans>0)
+    //    return "Alice";
+    //    else if(ans<0)
+    //    return "Bob";
+    //    else
+    //    return "Tie";
+    // }
+
+
+    int solve(vector<int>& arr){
+       int n=arr.size();
+       vector<vector<int>>dp(n+1,vector<int>(2,0));
+       for(int i=n-1;i>=0;i--){
+        for(int flag=0;flag<=1;flag++){
         int total=0;
         int ans=flag==1?INT_MIN:INT_MAX;
         for(int j=0;j<3;j++){
          if(i+j>=arr.size()) break;
          total+=arr[i+j];
          if(flag)
-         ans=max(ans,total+solve(arr,n,i+j+1,!flag,dp));
+         ans=max(ans,total+dp[i+j+1][!flag]);
          else
-         ans=min(ans,-total+solve(arr,n,i+j+1,!flag,dp));
+         ans=min(ans,-total+dp[i+j+1][!flag]);
         }
-        return dp[i][flag]=ans;
+        dp[i][flag]=ans;
+        }
+       }
+       return dp[0][1];
     }
     string stoneGameIII(vector<int>& stoneValue) {
-       int n=stoneValue.size();
-       vector<vector<int>>dp(n+1,vector<int>(2,-1));
-       int ans=solve(stoneValue,n,0,1,dp);
+       int ans=solve(stoneValue);
        if(ans>0)
        return "Alice";
        else if(ans<0)
