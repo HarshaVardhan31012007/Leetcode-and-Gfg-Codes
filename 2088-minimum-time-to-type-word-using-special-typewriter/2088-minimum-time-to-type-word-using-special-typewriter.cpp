ed760846b34744cae0c4df941005efe7c111c132
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-15 21:21:41 +0530

    Time: 0 ms (100%), Space: 8.4 MB (33.59%) - LeetHub

---
+++ 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minTimeToType(string word) {
+        char ch='a';
+        int count=0;
+        for(int i=0;i<word.length();i++){
+             if(word[i]==ch)
+             count++;
+             else{
+                int clock=1;
+                int mini=min((word[i]-ch+26)%26,(ch-word[i]+26)%26);
+                count+=mini+1;
+                ch=word[i];
+             }
+        }
+        return count;
+    }
+};
