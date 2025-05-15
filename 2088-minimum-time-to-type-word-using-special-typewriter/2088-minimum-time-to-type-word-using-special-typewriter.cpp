8e15b1fdcdcfc8a1a086740ec183706b6d804ecf
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-15 21:38:30 +0530

    Time: 0 ms (100%), Space: 8.4 MB (33.59%) - LeetHub

--- 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.cpp
+++ 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
+    // int minTimeToType(string word) {
+    //     char ch='a';
+    //     int count=0;
+    //     for(int i=0;i<word.length();i++){
+    //          if(word[i]==ch)
+    //          count++;
+    //          else{
+    //             int mini=min((word[i]-ch+26)%26,(ch-word[i]+26)%26);
+    //             count+=mini+1;
+    //             ch=word[i];
+    //          }
+    //     }
+    //     return count;
+    // }
+
+
     int minTimeToType(string word) {
-        char ch='a';
-        int count=0;
-        for(int i=0;i<word.length();i++){
-             if(word[i]==ch)
-             count++;
-             else{
-                int mini=min((word[i]-ch+26)%26,(ch-word[i]+26)%26);
-                count+=mini+1;
-                ch=word[i];
-             }
+        int n=word.length();
+        int count=n;
+        int a=abs(word[0]-'a');
+        count+=min(a,26-a);
+        for(int i=1;i<n;i++){
+                a=abs(word[i]-word[i-1]);
+                count+=min(a,26-a);
         }
         return count;
     }
