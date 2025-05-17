92e7b4b579d7ba537de50fba4c05d76f30b0b186
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-17 21:15:40 +0530

    Time: 35 ms (49.79%), Space: 31.4 MB (59.42%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    // int solve(string &s,int i,int j,vector<vector<int>>&dp){
-    //     if(i>j) return 0;
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     if(s[i]==s[j])
-    //     return dp[i][j]=solve(s,i+1,j-1,dp);
-    //     else
-    //     return dp[i][j]=1+min(solve(s,i+1,j,dp),solve(s,i,j-1,dp));
-    // }
-    // int minInsertions(string s) {
-    //     vector<vector<int>>dp(s.length()+1,vector<int>(s.length()+1,-1));
-    //     return  solve(s,0,s.length()-1,dp);
-    // }
+    int solve(string &s,int i,int j,vector<vector<int>>&dp){
+        if(i>=j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==s[j])
+        return dp[i][j]=solve(s,i+1,j-1,dp);
+        else
+        return dp[i][j]=1+min(solve(s,i+1,j,dp),solve(s,i,j-1,dp));
+    }
+    int minInsertions(string s) {
+        vector<vector<int>>dp(s.length()+1,vector<int>(s.length()+1,-1));
+        return  solve(s,0,s.length()-1,dp);
+    }
 
 
     // int solve(string &s){
@@ -33,27 +33,27 @@ public:
 
 
 
-     int solve(string &s){
-        string p=s;int n=s.size();
-        reverse(p.begin(),p.end());
-       vector<int>curr(n+1,0);
-       vector<int>next(n+1,0);
-        for(int i=n-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-               if(s[i]==p[j])
-               curr[j]=1+next[j+1];
-               else
-               curr[j]=max(next[j],curr[j+1]);
-            }
-            next=curr;
-        }
-        return curr[0];
-       }
-      int longestPalindromeSubseq(string s) {
-        return solve(s);
-    }
-    int minInsertions(string s) {
-        return  s.length()-longestPalindromeSubseq(s);
-    }
+    //  int solve(string &s){
+    //     string p=s;int n=s.size();
+    //     reverse(p.begin(),p.end());
+    //    vector<int>curr(n+1,0);
+    //    vector<int>next(n+1,0);
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=n-1;j>=0;j--){
+    //            if(s[i]==p[j])
+    //            curr[j]=1+next[j+1];
+    //            else
+    //            curr[j]=max(next[j],curr[j+1]);
+    //         }
+    //         next=curr;
+    //     }
+    //     return curr[0];
+    //    }
+    //   int longestPalindromeSubseq(string s) {
+    //     return solve(s);
+    // }
+    // int minInsertions(string s) {
+    //     return  s.length()-longestPalindromeSubseq(s);
+    // }
 
 };
