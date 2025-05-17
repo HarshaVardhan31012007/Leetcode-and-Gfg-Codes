f11cc21d23be126f6c49dec291e850083f09f564
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-17 21:05:09 +0530

    Time: 19 ms (89.25%), Space: 9.7 MB (94.53%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -14,20 +14,46 @@ public:
     // }
 
 
-    int solve(string &s){
-        int n=s.length();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    // int solve(string &s){
+    //     int n=s.length();
+    //     vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=i+1;j<=n-1;j++){
+    //                 if(s[i]==s[j])
+    //                dp[i][j]=dp[i+1][j-1];
+    //                else
+    //                dp[i][j]=1+min(dp[i+1][j],dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[0][n-1];
+    // }
+    // int minInsertions(string s) {
+    //     return  solve(s);
+    // }
+
+
+
+     int solve(string &s){
+        string p=s;int n=s.size();
+        reverse(p.begin(),p.end());
+       vector<int>curr(n+1,0);
+       vector<int>next(n+1,0);
         for(int i=n-1;i>=0;i--){
-            for(int j=i+1;j<=n-1;j++){
-                    if(s[i]==s[j])
-                   dp[i][j]=dp[i+1][j-1];
-                   else
-                   dp[i][j]=1+min(dp[i+1][j],dp[i][j-1]);
+            for(int j=n-1;j>=0;j--){
+               if(s[i]==p[j])
+               curr[j]=1+next[j+1];
+               else
+               curr[j]=max(next[j],curr[j+1]);
             }
+            next=curr;
         }
-        return dp[0][n-1];
+        return curr[0];
+       }
+      int longestPalindromeSubseq(string s) {
+        return solve(s);
     }
     int minInsertions(string s) {
-        return  solve(s);
+        return  s.length()-longestPalindromeSubseq(s);
     }
+
 };
