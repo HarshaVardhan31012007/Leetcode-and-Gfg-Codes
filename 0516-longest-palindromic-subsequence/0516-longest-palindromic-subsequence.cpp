73bdc6e4499c26c11d44fabe426a50cf2bb7aa71
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-17 21:02:48 +0530

    Time: 46 ms (91.58%), Space: 9.5 MB (91.27%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -94,6 +94,27 @@ public:
     // }
 
 
+    //  int solve(string &s){
+    //     string p=s;int n=s.size();
+    //     reverse(p.begin(),p.end());
+    //    vector<int>curr(n+1,0);
+    //    vector<int>next(n+1,0);
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=n-1;j>=0;j--){
+    //            if(s[i]==p[j])
+    //            curr[j]=1+next[j+1];
+    //            else
+    //            curr[j]=max(next[j],curr[j+1]);
+    //         }
+    //         next=curr;
+    //     }
+    //     return curr[0];
+    //    }
+    //   int longestPalindromeSubseq(string s) {
+    //     return solve(s);
+    // }
+
+
      int solve(string &s){
         string p=s;int n=s.size();
         reverse(p.begin(),p.end());
