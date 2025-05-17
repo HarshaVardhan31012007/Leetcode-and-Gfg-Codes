2d60762a5144ce2b02d785805cd13f6558266715
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-17 17:04:14 +0530

    Time: 0 ms (100%), Space: 10 MB (30.28%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -40,25 +40,43 @@ public:
     // }
 
 
-     bool solve(string &s1,string &s2,string &s3){
-         int m=s1.size();int n=s2.size();int p=s3.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        if(m+n!=p) return false;
-        dp[m][n]=1;
-        for(int i=m;i>=0;i--){
-            for(int j=n;j>=0;j--){
-                    if(i==m&&j==n) continue;
-                    bool ans=0;
-                    if(j<s2.size()&&s2[j]==s3[i+j])
-                    ans=ans||dp[i][j+1];
-                    if(i<s1.size()&&s1[i]==s3[i+j])
-                    ans=ans||dp[i+1][j];
-                    dp[i][j]=ans;
-            }
-        }
-        return dp[0][0];
+
+
+     bool solve(string &s1,string &s2,string &s3,int i,int j,vector<vector<int>>&dp){
+       if(i==s1.size()&&j==s2.size()) return 1;
+       if(dp[i][j]!=-1) return dp[i][j];
+       bool ans=0;
+       if(j<s2.size()&&s2[j]==s3[i+j])
+       ans=ans||solve(s1,s2,s3,i,j+1,dp);
+       if(i<s1.size()&&s1[i]==s3[i+j])
+       ans=ans||solve(s1,s2,s3,i+1,j,dp);
+       return dp[i][j]=ans;
     }
     bool isInterleave(string s1, string s2, string s3) {
-        return solve(s1,s2,s3);
+        int m=s1.size();int n=s2.size();int p=s3.size();
+        if(m+n!=p) return false;
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return solve(s1,s2,s3,0,0,dp);
     }
+    //  bool solve(string &s1,string &s2,string &s3){
+    //      int m=s1.size();int n=s2.size();int p=s3.size();
+    //     vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+    //     if(m+n!=p) return false;
+    //     dp[m][n]=1;
+    //     for(int i=m;i>=0;i--){
+    //         for(int j=n;j>=0;j--){
+    //                 if(i==m&&j==n) continue;
+    //                 bool ans=0;
+    //                 if(j<s2.size()&&s2[j]==s3[i+j])
+    //                 ans=ans||dp[i][j+1];
+    //                 if(i<s1.size()&&s1[i]==s3[i+j])
+    //                 ans=ans||dp[i+1][j];
+    //                 dp[i][j]=ans;
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+    // bool isInterleave(string s1, string s2, string s3) {
+    //     return solve(s1,s2,s3);
+    // }
 };
