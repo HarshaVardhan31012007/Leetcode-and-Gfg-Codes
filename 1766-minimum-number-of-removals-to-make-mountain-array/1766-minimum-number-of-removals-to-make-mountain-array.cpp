e4d8e1545ae1442fe17330a39476cb92184d1e77
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-17 22:36:46 +0530

    Time: 490 ms (5.23%), Space: 189.7 MB (5.06%) - LeetHub

---
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>&nums,int s,int e){
+        vector<int>ans;
+        if(s<=e)
+        ans.push_back(nums[s]);
+        for(int i=s+1;i<=e;i++){
+            if(ans.back()<nums[i])
+            ans.push_back(nums[i]);
+            else{
+                auto it=lower_bound(ans.begin(),ans.end(),nums[i]);
+                int idx=it-ans.begin();
+                ans[idx]=nums[i];
+            }
+        }
+        return ans.size();
+    }
+
+    int lengthOfLIS1(vector<int>&nums,int s,int e){
+        vector<int>ans;
+        if(s<=e)
+        ans.push_back(nums[e]);
+        for(int i=e-1;i>=s;i--){
+            if(ans.back()<nums[i])
+            ans.push_back(nums[i]);
+            else{
+                auto it=lower_bound(ans.begin(),ans.end(),nums[i]);
+                int idx=it-ans.begin();
+                 ans[idx]=nums[i];
+            }
+        }
+        return ans.size();
+    }
+    int solve(vector<int>&nums){
+         int ans=INT_MAX;
+         for(int i=1;i<nums.size()-1;i++){
+            int a=lengthOfLIS(nums,0,i);int b=lengthOfLIS1(nums,i,nums.size()-1);
+            int c=nums.size()+1-a-b;
+            if(a>1&&b>1) 
+            ans=min(ans,c);
+         }
+         return ans;
+    }
+    int minimumMountainRemovals(vector<int>& nums) {
+        return solve(nums);
+    }
+};
