877c8b973218815493462f19ac1bc1738603a7ba
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-19 16:04:24 +0530

    Time: 273 ms (32.23%), Space: 62.3 MB (9.11%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int target,int sum,int i,int t,vector<vector<int>>&dp){
+    // int solve(vector<int>&nums,int target,int sum,int i,int t,vector<vector<int>>&dp){
+    //     if(i==nums.size()){
+    //         if(sum==target)
+    //         return 1;
+    //         return 0;
+    //     }
+    //     if(dp[i][sum+t]!=-1) return dp[i][sum+t];
+    //     int a=solve(nums,target,sum-nums[i],i+1,t,dp);
+    //     int b=solve(nums,target,sum+nums[i],i+1,t,dp);
+    //     return dp[i][sum+t]=a+b;
+    // }
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //     int t=accumulate(nums.begin(),nums.end(),0);
+    //     vector<vector<int>>dp(nums.size()+1,vector<int>(2*t+1,-1));
+    //     return solve(nums,target,0,0,t,dp);
+    // }
+
+     int solve(vector<int>&nums,int target,int i,map<pair<int,int>,int>&dp){
         if(i==nums.size()){
-            if(sum==target)
+            if(target==0)
             return 1;
             return 0;
         }
-        if(dp[i][sum+t]!=-1) return dp[i][sum+t];
-        int a=solve(nums,target,sum-nums[i],i+1,t,dp);
-        int b=solve(nums,target,sum+nums[i],i+1,t,dp);
-        return dp[i][sum+t]=a+b;
+        if(dp.find({i,target})!=dp.end()) return dp[{i,target}];
+        int a=solve(nums,target-nums[i],i+1,dp);
+        int b=solve(nums,target+nums[i],i+1,dp);
+        return dp[{i,target}]=a+b;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int t=accumulate(nums.begin(),nums.end(),0);
-        vector<vector<int>>dp(nums.size()+1,vector<int>(2*t+1,-1));
-        return solve(nums,target,0,0,t,dp);
+        map<pair<int,int>,int>dp;
+        return solve(nums,target,0,dp);
     }
 };
