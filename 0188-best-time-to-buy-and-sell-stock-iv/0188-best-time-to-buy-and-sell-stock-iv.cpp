8c063e3ee7216f4cbbef8461060dd7f59b520367
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-19 13:59:24 +0530

    Time: 6 ms (63.77%), Space: 17.9 MB (50.33%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,25 +1,43 @@
 class Solution {
 public:
-      int solve(vector<int>&prices,int k){
-          vector<vector<int>>curr(2,vector<int>(k+1,0));
-          vector<vector<int>>next(2,vector<int>(k+1,0));
-        for(int i=prices.size()-1;i>=0;i--){
-            for(int flag=0;flag<=1;flag++){
-                for(int limit=1;limit<=k;limit++){
-                    int profit=0;
-                    if(flag)
-                    profit=max(profit,-prices[i]+next[0][limit]);
-                    else
-                    profit=max(profit,prices[i]+next[1][limit-1]);
-                    profit=max(profit,next[flag][limit]);
-                    curr[flag][limit]=profit;
-                }
-            }
-            next=curr;
-        }
-        return curr[1][k];
-     }
+    //   int solve(vector<int>&prices,int k){
+    //       vector<vector<int>>curr(2,vector<int>(k+1,0));
+    //       vector<vector<int>>next(2,vector<int>(k+1,0));
+    //     for(int i=prices.size()-1;i>=0;i--){
+    //         for(int flag=0;flag<=1;flag++){
+    //             for(int limit=1;limit<=k;limit++){
+    //                 int profit=0;
+    //                 if(flag)
+    //                 profit=max(profit,-prices[i]+next[0][limit]);
+    //                 else
+    //                 profit=max(profit,prices[i]+next[1][limit-1]);
+    //                 profit=max(profit,next[flag][limit]);
+    //                 curr[flag][limit]=profit;
+    //             }
+    //         }
+    //         next=curr;
+    //     }
+    //     return curr[1][k];
+    //  }
+    // int maxProfit(int k, vector<int>& prices) {
+    //      return solve(prices,k);
+    // }
+
+
+
+    int solve(vector<int>&prices,int op,int i,int k,vector<vector<int>>&dp){
+        if(i==prices.size()||op==2*k) return 0;
+        if(dp[i][op]!=-1) return dp[i][op];
+        int profit=0;
+        if(op%2==0)
+        profit=max(profit,-prices[i]+solve(prices,op+1,i+1,k,dp));
+        else
+        profit=max(profit,prices[i]+solve(prices,op+1,i+1,k,dp));
+        profit=max(profit,solve(prices,op,i+1,k,dp));
+        return dp[i][op]=profit;
+    }
     int maxProfit(int k, vector<int>& prices) {
-         return solve(prices,k);
+         vector<vector<int>>dp(prices.size()+1,vector<int>(2*k+1,-1));
+         return solve(prices,0,0,k,dp);
     }
 };
