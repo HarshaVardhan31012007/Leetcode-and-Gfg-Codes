41ac3d5c6a45fcbdc7a4fd33fccc9033f727cbd3
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-19 11:27:00 +0530

    Time: 64 ms (59.33%), Space: 79.5 MB (74.27%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -17,22 +17,46 @@ public:
     //  }
 
    
-       int solve(vector<int>&prices){
-          vector<vector<vector<int>>>dp(prices.size()+1,vector<vector<int>>(2,vector<int>(3,0)));
+    //    int solve(vector<int>&prices){
+    //       vector<vector<vector<int>>>dp(prices.size()+1,vector<vector<int>>(2,vector<int>(3,0)));
+    //     for(int i=prices.size()-1;i>=0;i--){
+    //         for(int flag=0;flag<=1;flag++){
+    //             for(int limit=1;limit<=2;limit++){
+    //     int profit=0;
+    //     if(flag)
+    //      profit=max(profit,-prices[i]+dp[i+1][0][limit]);
+    //      else
+    //     profit=max(profit,prices[i]+dp[i+1][1][limit-1]);
+    //     profit=max(profit,dp[i+1][flag][limit]);
+    //     dp[i][flag][limit]=profit;
+    //             }
+    //         }
+    //     }
+    //     return dp[0][1][2];
+    //  }
+    //  int maxProfit(vector<int>& prices) {
+    //     return solve(prices);
+    //  }
+
+
+     int solve(vector<int>&prices){
+          vector<vector<int>>curr(2,vector<int>(3,0));
+          vector<vector<int>>next(2,vector<int>(3,0));
         for(int i=prices.size()-1;i>=0;i--){
             for(int flag=0;flag<=1;flag++){
                 for(int limit=1;limit<=2;limit++){
         int profit=0;
         if(flag)
-         profit=max(profit,-prices[i]+dp[i+1][0][limit]);
+         profit=max(profit,-prices[i]+next[0][limit]);
          else
-        profit=max(profit,prices[i]+dp[i+1][1][limit-1]);
-        profit=max(profit,dp[i+1][flag][limit]);
-        dp[i][flag][limit]=profit;
+        profit=max(profit,prices[i]+next[1][limit-1]);
+        profit=max(profit,next[flag][limit]);
+        curr[flag][limit]=profit;
                 }
             }
+            next=curr;
         }
-        return dp[0][1][2];
+        return curr[1][2];
      }
      int maxProfit(vector<int>& prices) {
         return solve(prices);
