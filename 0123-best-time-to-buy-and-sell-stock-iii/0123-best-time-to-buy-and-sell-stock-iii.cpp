9f9fb65c7ad07da2e7441eab2825147fb75b8a37
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-19 11:16:05 +0530

    Time: 437 ms (28.42%), Space: 224 MB (21.09%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-       int solve(vector<int>&prices,int i,int flag,vector<vector<vector<int>>>&dp,int count){
-        if(i==prices.size()||count>=2) return 0;
-        if(dp[i][flag][count]!=-1) return dp[i][flag][count];
+       int solve(vector<int>&prices,int i,int flag,vector<vector<vector<int>>>&dp,int limit){
+        if(i==prices.size()||limit==0) return 0;
+        if(dp[i][flag][limit]!=-1) return dp[i][flag][limit];
         int profit=0;
         if(flag)
-         profit=max(profit,-prices[i]+solve(prices,i+1,0,dp,count));
+         profit=max(profit,-prices[i]+solve(prices,i+1,0,dp,limit));
          else
-        profit=max(profit,prices[i]+solve(prices,i+1,1,dp,count+1));
-        profit=max(profit,solve(prices,i+1,flag,dp,count));
-        return dp[i][flag][count]=profit;
+        profit=max(profit,prices[i]+solve(prices,i+1,1,dp,limit-1));
+        profit=max(profit,solve(prices,i+1,flag,dp,limit));
+        return dp[i][flag][limit]=profit;
      }
      int maxProfit(vector<int>& prices) {
         vector<vector<vector<int>>>dp(prices.size()+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        return solve(prices,0,1,dp,0);
+        return solve(prices,0,1,dp,2);
      }
+
+
+   
 };
