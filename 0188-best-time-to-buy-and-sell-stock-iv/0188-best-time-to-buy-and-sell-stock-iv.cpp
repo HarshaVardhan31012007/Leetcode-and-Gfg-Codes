c1eb7a684addc5ddd053f3a270b5bf362fd24b16
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-19 14:06:52 +0530

    Time: 7 ms (61.47%), Space: 17.6 MB (59.61%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -25,19 +25,39 @@ public:
 
 
 
-    int solve(vector<int>&prices,int op,int i,int k,vector<vector<int>>&dp){
-        if(i==prices.size()||op==2*k) return 0;
-        if(dp[i][op]!=-1) return dp[i][op];
+    // int solve(vector<int>&prices,int op,int i,int k,vector<vector<int>>&dp){
+    //     if(i==prices.size()||op==2*k) return 0;
+    //     if(dp[i][op]!=-1) return dp[i][op];
+    //     int profit=0;
+    //     if(op%2==0)
+    //     profit=max(profit,-prices[i]+solve(prices,op+1,i+1,k,dp));
+    //     else
+    //     profit=max(profit,prices[i]+solve(prices,op+1,i+1,k,dp));
+    //     profit=max(profit,solve(prices,op,i+1,k,dp));
+    //     return dp[i][op]=profit;
+    // }
+    // int maxProfit(int k, vector<int>& prices) {
+    //      vector<vector<int>>dp(prices.size()+1,vector<int>(2*k+1,-1));
+    //      return solve(prices,0,0,k,dp);
+    // }
+
+
+     int solve(vector<int>&prices,int k,vector<vector<int>>&dp){
+        for(int i=prices.size()-1;i>=0;i--){
+            for(int op=2*k-1;op>=0;op--){
         int profit=0;
         if(op%2==0)
-        profit=max(profit,-prices[i]+solve(prices,op+1,i+1,k,dp));
+        profit=max(profit,-prices[i]+dp[i+1][op+1]);
         else
-        profit=max(profit,prices[i]+solve(prices,op+1,i+1,k,dp));
-        profit=max(profit,solve(prices,op,i+1,k,dp));
-        return dp[i][op]=profit;
+        profit=max(profit,prices[i]+dp[i+1][op+1]);
+        profit=max(profit,dp[i+1][op]);
+        dp[i][op]=profit;
+        }
+        }
+        return dp[0][0];
     }
     int maxProfit(int k, vector<int>& prices) {
-         vector<vector<int>>dp(prices.size()+1,vector<int>(2*k+1,-1));
-         return solve(prices,0,0,k,dp);
+         vector<vector<int>>dp(prices.size()+1,vector<int>(2*k+1,0));
+         return solve(prices,k,dp);
     }
 };
