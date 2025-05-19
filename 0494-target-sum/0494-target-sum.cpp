f646eb6c37a6d3063eae8c2b1ed4facb78b3fdd9
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-19 16:31:42 +0530

    Time: 23 ms (49.39%), Space: 26.3 MB (26.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -16,21 +16,62 @@ public:
     //     vector<vector<int>>dp(nums.size()+1,vector<int>(2*t+1,-1));
     //     return solve(nums,target,0,0,t,dp);
     // }
-
+    
      int solve(vector<int>&nums,int target){
-       map<pair<int,int>,int>dp;
-       int t=accumulate(nums.begin(),nums.end(),0);
-       dp[{nums.size(),0}]=1;
-       for(int i=nums.size()-1;i>=0;i--){
+        int t=accumulate(nums.begin(),nums.end(),0);
+        vector<vector<int>>dp(nums.size()+1,vector<int>(2*t+1,0));
         for(int sum=-t;sum<=t;sum++){
-            int a=dp[{i+1,sum-nums[i]}];
-            int b=dp[{i+1,sum+nums[i]}];
-            dp[{i,sum}]=a+b;
+            if(sum==target)
+            dp[nums.size()][sum+t]=1;
         }
-       }
-       return dp[{0,target}];
+        for(int i=nums.size()-1;i>=0;i--){
+            for(int sum=-t;sum<=t;sum++){
+        int a=0;
+        if(sum-nums[i]>=-t)
+        a=dp[i+1][sum-nums[i]+t];
+        int b=0;
+        if(sum+nums[i]<=t)
+        b=dp[i+1][sum+nums[i]+t];
+        dp[i][sum+t]=a+b;
+            }
+        }
+        return dp[0][0+t];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         return solve(nums,target);
     }
+
+    //  int solve(vector<int>&nums,int target,int i,map<pair<int,int>,int>&dp){
+    //     if(i==nums.size()){
+    //         if(target==0)
+    //         return 1;
+    //         return 0;
+    //     }
+    //     if(dp.find({i,target})!=dp.end()) return dp[{i,target}];
+    //     int a=solve(nums,target-nums[i],i+1,dp);
+    //     int b=solve(nums,target+nums[i],i+1,dp);
+    //     return dp[{i,target}]=a+b;
+    // }
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //     map<pair<int,int>,int>dp;
+    //     return solve(nums,target,0,dp);
+    //}
+
+
+    //  int solve(vector<int>&nums,int target){
+    //    map<pair<int,int>,int>dp;
+    //    int t=accumulate(nums.begin(),nums.end(),0);
+    //    dp[{nums.size(),0}]=1;
+    //    for(int i=nums.size()-1;i>=0;i--){
+    //     for(int sum=-t;sum<=t;sum++){
+    //         int a=dp[{i+1,sum-nums[i]}];
+    //         int b=dp[{i+1,sum+nums[i]}];
+    //         dp[{i,sum}]=a+b;
+    //     }
+    //    }
+    //    return dp[{0,target}];
+    // }
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //     return solve(nums,target);
+    // }
 };
