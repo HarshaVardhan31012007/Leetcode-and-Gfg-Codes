5f8e2d7f20f3990d68d2c42d7f5b0c06056454e3
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-19 09:40:58 +0530

    Time: 4 ms (26.58%), Space: 19.7 MB (26.17%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -17,20 +17,40 @@ public:
     // }
 
 
-     int solve(vector<int>&prices,int i,int flag,vector<vector<int>>&dp){
-        if(i==prices.size()) return 0;
-        if(dp[i][flag]!=-1) return dp[i][flag];
-        int profit=0;
-        if(flag)
-         profit=max(profit,-prices[i]+solve(prices,i+1,0,dp));
-         else
-        profit=max(profit,prices[i]+solve(prices,i+1,1,dp));
-        profit=max(profit,solve(prices,i+1,flag,dp));
-        return dp[i][flag]=profit;
+    //  int solve(vector<int>&prices,int i,int flag,vector<vector<int>>&dp){
+    //     if(i==prices.size()) return 0;
+    //     if(dp[i][flag]!=-1) return dp[i][flag];
+    //     int profit=0;
+    //     if(flag)
+    //      profit=max(profit,-prices[i]+solve(prices,i+1,0,dp));
+    //      else
+    //     profit=max(profit,prices[i]+solve(prices,i+1,1,dp));
+    //     profit=max(profit,solve(prices,i+1,flag,dp));
+    //     return dp[i][flag]=profit;
+    //  }
+    //  int maxProfit(vector<int>& prices) {
+    //     vector<vector<int>>dp(prices.size()+1,vector<int>(2,-1));
+    //     return solve(prices,0,1,dp);
+    //  }
+
+
+     int solve(vector<int>&prices){
+         vector<vector<int>>dp(prices.size()+1,vector<int>(2,0));
+         for(int i=prices.size()-1;i>=0;i--){
+            for(int flag=0;flag<=1;flag++){
+                int profit=0;
+                if(flag)
+                profit=max(profit,-prices[i]+dp[i+1][0]);
+                else
+                profit=max(profit,prices[i]+dp[i+1][1]);
+                profit=max(profit,dp[i+1][flag]);
+                dp[i][flag]=profit;
+            }
+         }
+         return dp[0][1];
      }
      int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp(prices.size()+1,vector<int>(2,-1));
-        return solve(prices,0,1,dp);
+        return solve(prices);
      }
 
 };
