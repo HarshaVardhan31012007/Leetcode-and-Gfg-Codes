d4225c184b76455e91f885b258407b22b15ba1a4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-20 13:49:52 +0530

    Time: 0 ms (100%), Space: 9.9 MB (54.97%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int s,int e,vector<vector<int>>&dp){
+    // int solve(vector<int>&nums,int s,int e,vector<vector<int>>&dp){
+    //   if(s==e) return nums[s];
+    //   if(dp[s][e]!=-1) return dp[s][e];
+    //   return dp[s][e]=max(nums[s]-solve(nums,s+1,e,dp),nums[e]-solve(nums,s,e-1,dp));
+    // }
+    // bool predictTheWinner(vector<int>& nums) {
+    //     int n=nums.size();
+    //     //if(n%2==0) return true;//optimization
+    //     vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+    //     int ans=solve(nums,0,n-1,dp);
+    //     return ans>=0;
+    // }
+
+
+
+     int solve(vector<int>&nums,int s,int e,vector<vector<int>>&dp){
       if(s==e) return nums[s];
       if(dp[s][e]!=-1) return dp[s][e];
       return dp[s][e]=max(nums[s]-solve(nums,s+1,e,dp),nums[e]-solve(nums,s,e-1,dp));
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        //if(n%2==0) return true;//optimization
+        if(n%2==0) return true;//optimization
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         int ans=solve(nums,0,n-1,dp);
         return ans>=0;
