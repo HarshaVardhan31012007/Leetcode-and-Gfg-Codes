8e2f22360129b19b31cc8eae58e3caba6582c992
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-20 17:32:38 +0530

    Time: 7 ms (61.5%), Space: 13.2 MB (31.96%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,16 +1,42 @@
 class Solution {
 public:
-    int solve(string &word1,string &word2,int i,int j,vector<vector<int>>&dp){
-        if(j==word2.size()) return word1.size()-i;
-        if(i==word1.size()) return word2.size()-j;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(word1[i]==word2[j])
-        return solve(word1,word2,i+1,j+1,dp);
-        else
-        return dp[i][j]=1+min(solve(word1,word2,i+1,j+1,dp),min(solve(word1,word2,i,j+1,dp),solve(word1,word2,i+1,j,dp)));
-    }
-    int minDistance(string word1, string word2) {
-    vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,-1));
-       return solve(word1,word2,0,0,dp); 
+    // int solve(string &word1,string &word2,int i,int j,vector<vector<int>>&dp){
+    //     if(j==word2.size()) return word1.size()-i;
+    //     if(i==word1.size()) return word2.size()-j;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(word1[i]==word2[j])
+    //     return solve(word1,word2,i+1,j+1,dp);
+    //     else
+    //     return 1+min(solve(word1,word2,i+1,j+1,dp),min(solve(word1,word2,i,j+1,dp),solve(word1,word2,i+1,j,dp)));
+    // }
+    // int minDistance(string word1, string word2) {
+    // vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,-1));
+    //    return solve(word1,word2,0,0,dp); 
+    // }
+
+     int solve(string &word1,string &word2){
+        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,-1));
+        for(int i=0;i<word1.size()+1;i++){
+            dp[i][word2.size()]=word1.size()-i;
+        }
+        for(int j=0;j<word2.size()+1;j++){
+            dp[word1.size()][j]=word2.size()-j;
+        }
+        for(int i=word1.size()-1;i>=0;i--){
+           for(int j=word2.size()-1;j>=0;j--){
+             if(word1[i]==word2[j])
+             dp[i][j]=dp[i+1][j+1];
+             else{
+                int op1=dp[i][j+1];
+                int op2=dp[i+1][j];
+                int op3=dp[i+1][j+1];
+                dp[i][j]=1+min({op1,op2,op3});
+             }
+           }
+        }
+        return dp[0][0];
+     }
+     int minDistance(string word1, string word2) {
+       return solve(word1,word2); 
     }
 };
