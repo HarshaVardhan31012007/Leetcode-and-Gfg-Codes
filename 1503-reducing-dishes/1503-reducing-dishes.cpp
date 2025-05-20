37f7d13f53d832492d734f6ef6cbc7645eb94328
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-20 13:13:29 +0530

    Time: 14 ms (41.15%), Space: 25 MB (28.32%) - LeetHub

--- 1503-reducing-dishes/1503-reducing-dishes.cpp
+++ 1503-reducing-dishes/1503-reducing-dishes.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    int solve(vector<int>&satisfaction,int i,int time,vector<vector<int>>&dp){
-        if(i==satisfaction.size()) return 0;
-        if(dp[i][time]!=-1) return dp[i][time];
-         int l=satisfaction[i]*time+solve(satisfaction,i+1,time+1,dp);
-         int r=0;
-        if(satisfaction[i]<0)//optimization
-        r=solve(satisfaction,i+1,time,dp);
-        return dp[i][time]=max(l,r);
+    // int solve(vector<int>&satisfaction,int i,int time,vector<vector<int>>&dp){
+    //     if(i==satisfaction.size()) return 0;
+    //     if(dp[i][time]!=-1) return dp[i][time];
+    //      int l=satisfaction[i]*time+solve(satisfaction,i+1,time+1,dp);
+    //      int r=0;
+    //     if(satisfaction[i]<0)//optimization
+    //     r=solve(satisfaction,i+1,time,dp);
+    //     return dp[i][time]=max(l,r);
+    // }
+    // int maxSatisfaction(vector<int>& satisfaction) {
+    //     sort(satisfaction.begin(),satisfaction.end());
+    //     vector<vector<int>>dp(satisfaction.size()+1,vector<int>(satisfaction.size()+1,-1));
+    //     return solve(satisfaction,0,1,dp);
+    // }
+
+
+     int solve(vector<int>&satisfaction){
+         vector<vector<int>>dp(satisfaction.size()+1,vector<int>(satisfaction.size()+2,0));
+        for(int i=satisfaction.size()-1;i>=0;i--){
+          for(int time=satisfaction.size();time>=1;time--){
+         int l=satisfaction[i]*time+dp[i+1][time+1];
+         int r=dp[i+1][time];
+        dp[i][time]=max(l,r);
+          }
+        }
+        return dp[0][1];
     }
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(),satisfaction.end());
-        vector<vector<int>>dp(satisfaction.size()+1,vector<int>(satisfaction.size()+1,-1));
-        return solve(satisfaction,0,1,dp);
+        return solve(satisfaction);
     }
 };
