f53afb2854791191d561c1b56171270a41e7f629
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-20 13:45:59 +0530

    Time: 22 ms (31.63%), Space: 9.6 MB (85.14%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int solve(vector<int>&nums,int s,int e){
-      if(s>e) return 0;
-      int ans=INT_MIN;
-      ans=max(ans,max(nums[s]-solve(nums,s+1,e),nums[e]-solve(nums,s,e-1)));
-      return ans;
+      if(s==e) return nums[s];
+      return max(nums[s]-solve(nums,s+1,e),nums[e]-solve(nums,s,e-1));
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        if(n%2==0) return true;
+        if(n%2==0) return true;//optimization
         int ans=solve(nums,0,n-1);
         return ans>=0;
     }
