ad405abbcbba1f0fcac4a862b8b6a46f93602979
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-20 12:58:43 +0530

    Time: 17 ms (34.27%), Space: 25.1 MB (16.11%) - LeetHub

---
+++ 1503-reducing-dishes/1503-reducing-dishes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int solve(vector<int>&satisfaction,int i,int time,vector<vector<int>>&dp){
+        if(i==satisfaction.size()) return 0;
+        if(dp[i][time]!=-1) return dp[i][time];
+         int l=satisfaction[i]*time+solve(satisfaction,i+1,time+1,dp);
+         int r=0;
+         if(satisfaction[i]<0)
+        r=solve(satisfaction,i+1,time,dp);
+        return dp[i][time]=max(l,r);
+    }
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.begin(),satisfaction.end());
+        vector<vector<int>>dp(satisfaction.size()+1,vector<int>(satisfaction.size()+1,-1));
+        return solve(satisfaction,0,1,dp);
+    }
+};
