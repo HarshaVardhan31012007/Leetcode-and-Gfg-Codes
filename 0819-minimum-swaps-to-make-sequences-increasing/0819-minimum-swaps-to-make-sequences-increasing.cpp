b778e0209cdb5523cb153fe512aeb5652ebd8082
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-20 12:11:49 +0530

    Time: 95 ms (42.14%), Space: 124.8 MB (46.63%) - LeetHub

--- 0819-minimum-swaps-to-make-sequences-increasing/0819-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0819-minimum-swaps-to-make-sequences-increasing/0819-minimum-swaps-to-make-sequences-increasing.cpp
@@ -35,24 +35,49 @@ public:
     // }
 
 
-    int solve(vector<int>& nums1, vector<int>& nums2,int i,int isswap,vector<vector<int>>&dp){
-       if(i==nums1.size()) return 0;
-       if(dp[i][isswap]!=-1) return dp[i][isswap];
-       int ans=INT_MAX;
-       int p1=nums1[i-1];
-       int p2=nums2[i-1];
-       if(isswap)
-       swap(p1,p2);
-       if(p1<nums2[i] && p2<nums1[i])
-       ans=min(ans,1+solve(nums1,nums2,i+1,1,dp));
-       if(p1<nums1[i] && p2<nums2[i])
-       ans=min(ans,solve(nums1,nums2,i+1,0,dp));
-       return dp[i][isswap]=ans;
+    // int solve(vector<int>& nums1, vector<int>& nums2,int i,int isswap,vector<vector<int>>&dp){
+    //    if(i==nums1.size()) return 0;
+    //    if(dp[i][isswap]!=-1) return dp[i][isswap];
+    //    int ans=INT_MAX;
+    //    int p1=nums1[i-1];
+    //    int p2=nums2[i-1];
+    //    if(isswap)
+    //    swap(p1,p2);
+    //    if(p1<nums2[i] && p2<nums1[i])
+    //    ans=min(ans,1+solve(nums1,nums2,i+1,1,dp));
+    //    if(p1<nums1[i] && p2<nums2[i])
+    //    ans=min(ans,solve(nums1,nums2,i+1,0,dp));
+    //    return dp[i][isswap]=ans;
+    // }
+    // int minSwap(vector<int>& nums1, vector<int>& nums2) {
+    //     nums1.insert(nums1.begin(),-1);
+    //     nums2.insert(nums2.begin(),-1);
+    //     vector<vector<int>>dp(nums1.size()+1,vector<int>(2,-1));
+    //     return solve(nums1,nums2,1,0,dp);
+    // }
+
+
+     int solve(vector<int>& nums1, vector<int>& nums2){
+       vector<vector<int>>dp(nums1.size()+1,vector<int>(2,0));
+       for(int i=nums1.size()-1;i>=1;i--){
+        for(int isswap=1;isswap>=0;isswap--){
+                int ans=INT_MAX;
+                int p1=nums1[i-1];
+                int p2=nums2[i-1];
+                if(isswap)
+                swap(p1,p2);
+                if(p1<nums2[i] && p2<nums1[i])
+                ans=min(ans,1+dp[i+1][1]);
+                if(p1<nums1[i] && p2<nums2[i])
+                ans=min(ans,dp[i+1][0]);
+                dp[i][isswap]=ans;
+        }
+       }
+       return dp[1][0];
     }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         nums1.insert(nums1.begin(),-1);
         nums2.insert(nums2.begin(),-1);
-        vector<vector<int>>dp(nums1.size()+1,vector<int>(2,-1));
-        return solve(nums1,nums2,1,0,dp);
+        return solve(nums1,nums2);
     }
 };
