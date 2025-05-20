6c2cc621279393f9242b4060b9aecad486574b38
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-20 12:06:31 +0530

    Time: 91 ms (46.63%), Space: 137.3 MB (29.84%) - LeetHub

--- 0819-minimum-swaps-to-make-sequences-increasing/0819-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0819-minimum-swaps-to-make-sequences-increasing/0819-minimum-swaps-to-make-sequences-increasing.cpp
@@ -19,18 +19,40 @@ public:
 
 
 
-    int solve(vector<int>& nums1, vector<int>& nums2,int p1,int p2,int i,int isswap,vector<vector<int>>&dp){
+    // int solve(vector<int>& nums1, vector<int>& nums2,int p1,int p2,int i,int isswap,vector<vector<int>>&dp){
+    //    if(i==nums1.size()) return 0;
+    //    if(dp[i][isswap]!=-1) return dp[i][isswap];
+    //    int ans=INT_MAX;
+    //    if(p1<nums2[i] && p2<nums1[i])
+    //    ans=min(ans,1+solve(nums1,nums2,nums2[i],nums1[i],i+1,1,dp));
+    //    if(p1<nums1[i] && p2<nums2[i])
+    //    ans=min(ans,solve(nums1,nums2,nums1[i],nums2[i],i+1,0,dp));
+    //    return dp[i][isswap]=ans;
+    // }
+    // int minSwap(vector<int>& nums1, vector<int>& nums2) {
+    //     vector<vector<int>>dp(nums1.size()+1,vector<int>(2,-1));
+    //     return solve(nums1,nums2,-1,-1,0,0,dp);
+    // }
+
+
+    int solve(vector<int>& nums1, vector<int>& nums2,int i,int isswap,vector<vector<int>>&dp){
        if(i==nums1.size()) return 0;
        if(dp[i][isswap]!=-1) return dp[i][isswap];
        int ans=INT_MAX;
+       int p1=nums1[i-1];
+       int p2=nums2[i-1];
+       if(isswap)
+       swap(p1,p2);
        if(p1<nums2[i] && p2<nums1[i])
-       ans=min(ans,1+solve(nums1,nums2,nums2[i],nums1[i],i+1,1,dp));
+       ans=min(ans,1+solve(nums1,nums2,i+1,1,dp));
        if(p1<nums1[i] && p2<nums2[i])
-       ans=min(ans,solve(nums1,nums2,nums1[i],nums2[i],i+1,0,dp));
+       ans=min(ans,solve(nums1,nums2,i+1,0,dp));
        return dp[i][isswap]=ans;
     }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        nums1.insert(nums1.begin(),-1);
+        nums2.insert(nums2.begin(),-1);
         vector<vector<int>>dp(nums1.size()+1,vector<int>(2,-1));
-        return solve(nums1,nums2,-1,-1,0,0,dp);
+        return solve(nums1,nums2,1,0,dp);
     }
 };
