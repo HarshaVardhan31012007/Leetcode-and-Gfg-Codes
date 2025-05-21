f43d21a6561ac4db35f33544b99d72f79dec44d5
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-21 11:47:17 +0530

    Time: 10 ms (63.61%), Space: 18.4 MB (51.07%) - LeetHub

---
+++ 1489-pizza-with-3n-slices/1489-pizza-with-3n-slices.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int solve(vector<int>&slices,int i,int n,int endIndex,vector<vector<int>>&dp){
+        if(n==0||i>endIndex) return 0; 
+        if(dp[i][n]!=-1) return dp[i][n];
+        int l=slices[i]+solve(slices,i+2,n-1,endIndex,dp);
+        int r=solve(slices,i+1,n,endIndex,dp);
+        return dp[i][n]=max(l,r);
+    }
+    int maxSizeSlices(vector<int>& slices) {
+        vector<vector<int>>dp(slices.size()+1,vector<int>(slices.size()/3+1,-1));
+        vector<vector<int>>dp1(slices.size()+1,vector<int>(slices.size()/3+1,-1));
+        return max(solve(slices,0,slices.size()/3,slices.size()-2,dp),solve(slices,1,slices.size()/3,slices.size()-1,dp1));
+    }
+};
