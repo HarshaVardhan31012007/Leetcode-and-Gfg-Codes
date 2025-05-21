16c805e32333f9819a77f33dd3769cb4f44ffa94
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-21 16:17:44 +0530

    Time: 0 ms (100%), Space: 27.5 MB (97.36%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -23,23 +23,49 @@ public:
     // }
 
 
-    void solve(vector<vector<char>>& matrix,int row,int col,vector<vector<int>>&dp,int &maxi){
+    // void solve(vector<vector<char>>& matrix,int row,int col,vector<vector<int>>&dp,int &maxi){
+    //    for(int i=row-1;i>=0;i--){
+    //     for(int j=col-1;j>=0;j--){
+    //    int right=dp[i][j+1];
+    //    int diagonal=dp[i+1][j+1];
+    //    int down=dp[i+1][j];
+    //    if(matrix[i][j]=='1'){
+    //        dp[i][j]=1+min(right,min(diagonal,down));
+    //        maxi=max(maxi,dp[i][j]);
+    //    }
+    //     }
+    //    }
+    // }
+    // int maximalSquare(vector<vector<char>>& matrix) {
+    //     vector<vector<int>>dp(matrix.size()+1,vector<int>(matrix[0].size()+1,0));
+    //     int maxi=0;
+    //     solve(matrix,matrix.size(),matrix[0].size(),dp,maxi);
+    //     return maxi*maxi;
+    // }
+
+
+
+     void solve(vector<vector<char>>& matrix,vector<int>&curr,vector<int>&next,int row,int col,int &maxi){
        for(int i=row-1;i>=0;i--){
         for(int j=col-1;j>=0;j--){
-       int right=dp[i][j+1];
-       int diagonal=dp[i+1][j+1];
-       int down=dp[i+1][j];
+       int right=curr[j+1];
+       int diagonal=next[j+1];
+       int down=next[j];
        if(matrix[i][j]=='1'){
-           dp[i][j]=1+min(right,min(diagonal,down));
-           maxi=max(maxi,dp[i][j]);
+           curr[j]=1+min(right,min(diagonal,down));
+           maxi=max(maxi,curr[j]);
        }
+       else
+       curr[j]=0;
         }
+        next=curr;
        }
     }
     int maximalSquare(vector<vector<char>>& matrix) {
-        vector<vector<int>>dp(matrix.size()+1,vector<int>(matrix[0].size()+1,0));
+        vector<int>curr(matrix[0].size()+1,0);
+        vector<int>next(matrix[0].size()+1,0);
         int maxi=0;
-        solve(matrix,matrix.size(),matrix[0].size(),dp,maxi);
+        solve(matrix,curr,next,matrix.size(),matrix[0].size(),maxi);
         return maxi*maxi;
     }
 };
