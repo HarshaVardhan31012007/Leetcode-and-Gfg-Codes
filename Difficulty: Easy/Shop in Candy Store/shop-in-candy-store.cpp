753f83cbc76404d126a4c6c2153a6b4b5a339256
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-22 21:42:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Shop in Candy Store/shop-in-candy-store.cpp
+++ Difficulty: Easy/Shop in Candy Store/shop-in-candy-store.cpp
@@ -1,29 +1,52 @@
 class Solution {
   public:
     vector<int> candyStore(int candies[], int N, int p) {
-        vector<int>arr;
-        for(int i=0;i<N;i++)
-        arr.push_back(candies[i]);
-        sort(arr.begin(),arr.end());
-        int ans1=0;
+        // vector<int>arr;
+        // for(int i=0;i<N;i++)
+        // arr.push_back(candies[i]);
+        // sort(arr.begin(),arr.end());
+        // int ans1=0;
+        // int K=p;
+        // for(int i=0;i<arr.size();i++){
+        //     ans1+=arr[i];
+        //     while(K--&&arr.size()>i+1)
+        //     arr.pop_back();
+        //     K=p;
+        // }
+        // vector<int>brr;
+        // for(int i=0;i<N;i++)
+        // brr.push_back(candies[i]);
+        // sort(brr.rbegin(),brr.rend());
+        // int ans2=0;
+        // K=p;
+        // for(int i=0;i<brr.size();i++){
+        //     ans2+=brr[i];
+        //     while(K--&&brr.size()>i+1)
+        //     brr.pop_back();
+        //     K=p;
+        // }
+        // return {ans1,ans2};
+        
+        
+        
+        //Two pointer approach
         int K=p;
-        for(int i=0;i<arr.size();i++){
-            ans1+=arr[i];
-            while(K--&&arr.size()>i+1)
-            arr.pop_back();
-            K=p;
+        sort(candies,candies+N);
+        int buy=0;
+        int free=N-1;
+        int ans1=0;
+        while(buy<=free){
+            ans1+=candies[buy];
+            buy++;
+            free=free-K;
         }
-        vector<int>brr;
-        for(int i=0;i<N;i++)
-        brr.push_back(candies[i]);
-        sort(brr.rbegin(),brr.rend());
+        buy=N-1;
+        free=0;
         int ans2=0;
-        K=p;
-        for(int i=0;i<brr.size();i++){
-            ans2+=brr[i];
-            while(K--&&brr.size()>i+1)
-            brr.pop_back();
-            K=p;
+        while(free<=buy){
+            ans2+=candies[buy];
+            buy--;
+            free=free+K;
         }
         return {ans1,ans2};
     }
