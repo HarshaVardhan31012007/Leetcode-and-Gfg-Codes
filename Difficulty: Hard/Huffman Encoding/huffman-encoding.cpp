4b0e8c792993fa53e6597d657b9288139f106ea2
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-22 13:20:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Huffman Encoding/huffman-encoding.cpp
@@ -0,0 +1,54 @@
+class Solution {
+  public:
+  class node{
+     public:
+     pair<int,char>p;
+     node *left;
+     node *right;
+     node(int a,char b){
+         this->p={a,b};
+         this->left=NULL;
+         this->right=NULL;
+     }
+  };
+  class comp{
+      public:
+      bool operator()(node *&a,node *&b){
+          return a->p.first>b->p.first;
+      }
+  };
+    void solve(node *root,vector<string>&ans,string temp){
+        if(!root->left&&!root->right){
+            ans.push_back(temp);
+            return;
+        }
+        solve(root->left,ans,temp+'0');
+        solve(root->right,ans,temp+'1');
+    }
+    vector<string> huffmanCodes(string S, vector<int> f, int N) {
+        priority_queue<node*,vector<node*>,comp>pq;
+        for(int i=0;i<f.size();i++){
+            pq.push(new node(f[i],S[i]));
+        }
+        node *root=NULL;
+        while(!pq.empty()){
+            node *a=pq.top();
+            pq.pop();
+            node *b=NULL;
+            if(!pq.empty()){
+            b=pq.top();
+            pq.pop();
+            }
+            if(b!=NULL){
+            root=new node(a->p.first+b->p.first,'\0');
+            root->left=a;
+            root->right=b;
+            pq.push(root);
+            }
+        }
+        vector<string>ans;
+        string temp="";
+        solve(root,ans,temp);
+        return ans;
+    }
+};
