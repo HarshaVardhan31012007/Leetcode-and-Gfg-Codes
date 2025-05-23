9551271f65db4820c688ed741f7a2094c31693b4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-23 12:26:50 +0530

    Time: 4 ms (44.21%), Space: 12.2 MB (58.62%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string ans="";
-        string temp="";
-        for(int i=0;i<s.length();i++){
-            if(s[i]==' '){
-                reverse(temp.begin(),temp.end());
-                ans=ans+temp+s[i];
-                temp="";
-            }
-            else{
-                temp.push_back(s[i]);
-            }
+        // string ans="";
+        // string temp="";
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i]==' '){
+        //         reverse(temp.begin(),temp.end());
+        //         ans=ans+temp+s[i];
+        //         temp="";
+        //     }
+        //     else{
+        //         temp.push_back(s[i]);
+        //     }
+        // }
+        // reverse(temp.begin(),temp.end());
+        // ans=ans+temp;
+        // return ans;
+
+        int i=0;
+        int j;
+        for(j=0;j<s.length();j++){
+              if(s[j]==' '){
+              reverse(s.begin()+i,s.begin()+j);
+              i=j+1;
+              }
         }
-        reverse(temp.begin(),temp.end());
-        ans=ans+temp;
-        return ans;
+        reverse(s.begin()+i,s.begin()+j);
+        return s;
     }
 };
