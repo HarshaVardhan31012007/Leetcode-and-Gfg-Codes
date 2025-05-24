2a7ce02b4b598a3544e264294db1a36ecdd42014
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-24 11:49:20 +0530

    Time: 19 ms (78.22%), Space: 139.2 MB (38.34%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -10,12 +10,20 @@ public:
       }  
        ans.push_back(nums[dq.front()]);
      for(int i=k;i<nums.size();i++){
-        while(!dq.empty()&&nums[dq.back()]<nums[i])
-        dq.pop_back();
-        dq.push_back(i);
+        //addition and removal
+        // while(!dq.empty()&&nums[dq.back()]<nums[i])
+        // dq.pop_back();
+        // dq.push_back(i);
+        // while(!dq.empty()&&i-dq.front()>=k){
+        //     dq.pop_front();
+        // }
+        // removal and then addition
         while(!dq.empty()&&i-dq.front()>=k){
             dq.pop_front();
         }
+        while(!dq.empty()&&nums[dq.back()]<nums[i])
+        dq.pop_back();
+        dq.push_back(i);
         ans.push_back(nums[dq.front()]);
      }
      return ans;
