dda63ec13745de9829da2765ca89d9be76a6fe42
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-24 21:36:14 +0530

    Time: 13 ms (12.83%), Space: 20.7 MB (27.84%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-
-        unordered_map<int,int>freqmap;
-
-        for(int i=0;i<nums.size();i++)
-        {
-            int num=nums[i];
-            freqmap[num]=freqmap[num]+1;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            ans=ans^nums[i];
         }
-        int ans;
-        for(int i=0;i<nums.size();i++)
-        {   int num=nums[i];
-            if(freqmap[num]==1){
-                ans=num;
-        }
-
         return ans;
-
     }
-
-}; 
+};
