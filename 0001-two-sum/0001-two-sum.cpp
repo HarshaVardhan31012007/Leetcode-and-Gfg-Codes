50325639bd56c592c788de07ae7b34437ee13838
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-24 13:20:32 +0530

    Time: 2 ms (77.13%), Space: 14.7 MB (57.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int,int>>v;
+        // vector<pair<int,int>>v;
+        // for(int i=0;i<nums.size();i++){
+        //     v.push_back({nums[i],i});
+        // }
+        // sort(v.begin(),v.end());
+        // int s=0;
+        // int e=nums.size()-1;
+        // while(s<e){
+        //     int sum=v[s].first+v[e].first;
+        //     if(sum==target){
+        //         return {v[s].second,v[e].second};
+        //     }
+        //     else if(sum>target)
+        //     e--;
+        //     else if(sum<target)
+        //     s++;
+        // }
+        // return {-1,-1};
+
+
+        unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++){
-            v.push_back({nums[i],i});
-        }
-        sort(v.begin(),v.end());
-        int s=0;
-        int e=nums.size()-1;
-        while(s<e){
-            int sum=v[s].first+v[e].first;
-            if(sum==target){
-                return {v[s].second,v[e].second};
+            int t=target-nums[i];
+            if(mpp.find(t)!=mpp.end()){
+                return {mpp[t],i};
             }
-            else if(sum>target)
-            e--;
-            else if(sum<target)
-            s++;
+            mpp[nums[i]]=i;
         }
         return {-1,-1};
     }
