ab22810856255c17121c79c32fbb475eb0f897cb
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-24 11:48:21 +0530

    Time: 36 ms (30.5%), Space: 139 MB (82.92%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-    // class compare{
-    //    public:
-    //    bool operator()(pair<int,int> a,pair<int,int> b){
-    //       if(a.first!=b.first)
-    //       return a.first<b.first;
-    //       else
-    //       return a.second<b.second;
-    //    }
-    // };
-
-
-    //  class compare{
-    //    public:
-    //    bool operator()(pair<int,int> a,pair<int,int> b){
-    //       return a.first<b.first;
-    //    }
-    // };
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int>ans;
-        //priority_queue<pair<int,int>,vector<pair<int,int>>,compare>pq;
-        priority_queue<pair<int,int>>pq;
-        for(int i=0;i<k;i++){
-            pq.push({nums[i],i});
+      deque<int>dq;
+      vector<int>ans;
+      for(int i=0;i<k;i++){
+         while(!dq.empty()&&nums[dq.back()]<nums[i])
+         dq.pop_back();
+         dq.push_back(i);
+      }  
+       ans.push_back(nums[dq.front()]);
+     for(int i=k;i<nums.size();i++){
+        while(!dq.empty()&&nums[dq.back()]<nums[i])
+        dq.pop_back();
+        dq.push_back(i);
+        while(!dq.empty()&&i-dq.front()>=k){
+            dq.pop_front();
         }
-        ans.push_back(pq.top().first);
-        for(int i=k;i<nums.size();i++){
-            // pq.push({nums[i],i});
-            // whilei-pq.top().second>=k)
-            // pq.pop();
-            while(!pq.empty()&&i-pq.top().second>=k)
-            pq.pop();
-             pq.push({nums[i],i});
-            ans.push_back(pq.top().first);
-        }
-        return ans;
+        ans.push_back(nums[dq.front()]);
+     }
+     return ans;
     }
 };
