8e33b1531c7e945db74e14569bc323ac5bb27ec9
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-24 18:59:45 +0530

    Time: 5 ms (71.7%), Space: 11.3 MB (83.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+    // if(s.length()<t.length()) return "";
+    //    int start=0;
+    //    int end=0;
+    //    int len=INT_MAX;
+    //    int ansIdx=0;int count=0;
+    //    unordered_map<char,int>freq1;
+    //    unordered_map<char,int>freq2;
+    //    for(auto &each:t)
+    //    freq2[each]++;
+    //    while(end<s.length()){
+    //     freq1[s[end]]++;
+    //     if(freq1[s[end]]<=freq2[s[end]])
+    //     count++;
+    //     while(count==t.length()){       
+    //                     if(end-start+1<len){
+    //             len=end-start+1;
+    //             ansIdx=start;
+    //         }
+    //         if(freq1[s[start]]<=freq2[s[start]]) count--;
+    //          freq1[s[start]]--;
+    //          start++;
+    //     }
+    //     end++;
+    //    }
+    //    return len==INT_MAX?"":s.substr(ansIdx,len);
+
+
     if(s.length()<t.length()) return "";
        int start=0;
        int end=0;
        int len=INT_MAX;
-       int ansIdx=0;int count=0;
-       unordered_map<char,int>freq1;
-       unordered_map<char,int>freq2;
+       int ansIdx=0;int count=t.length();
+       unordered_map<char,int>freq;
        for(auto &each:t)
-       freq2[each]++;
+       freq[each]++;
        while(end<s.length()){
-        freq1[s[end]]++;
-        if(freq1[s[end]]<=freq2[s[end]])
-        count++;
-        while(count==t.length()){       
-                        if(end-start+1<len){
-                len=end-start+1;
-                ansIdx=start;
+        if(freq[s[end]]>0) count--;
+        freq[s[end]]--;
+        while(count==0){       
+            if(end-start+1<len){
+            len=end-start+1;
+            ansIdx=start;
             }
-            if(freq1[s[start]]<=freq2[s[start]]) count--;
-             freq1[s[start]]--;
-             start++;
+            freq[s[start]]++;
+            if(freq[s[start]]>0) count++;
+            start++;
         }
         end++;
        }
