223c2fa70ce605e87fbc84bd5a58a331873861c0
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-26 16:50:37 +0530

    Time: 23 ms (7.69%), Space: 22.9 MB (73.89%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    bool findtotalhours(vector<int>&piles,int h,int k){
-       int n=piles.size();
-       long sum=0;
-       for(int i=0;i<n;i++){
-          sum=sum+ceil(piles[i]/(double)k);
+    bool possible(int k,vector<int>&piles,int h){
+       for(int i=0;i<piles.size();i++){
+          h=h-ceil(piles[i]/(double)k);
        }
-       return sum<=h;
+       return h>=0;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-      int s=1;
-      int e=*max_element(piles.begin(),piles.end());
-      int ans=0;
-      while(s<=e){
-        int mid=s+(e-s)/2;
-        int k=mid;
-        if(findtotalhours(piles,h,k)){
-             ans=mid;
-             e=mid-1;
+        int s=1;
+        int e=*max_element(piles.begin(),piles.end());
+         if(h==piles.size()) return e;
+        int ans=-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(possible(mid,piles,h)){
+               ans=mid;
+               e=mid-1;
+            }
+            else
+            s=mid+1;
         }
-        else
-        s=mid+1;
-      }
-      return ans;
+        return ans;
     }
 };
