27bfd742cb3da71a1dc6cc7ce65675894dc0159a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-27 10:56:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,83 +1,32 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    bool possible(vector<int>&arr,int n,int k,int sol){//sol means solution
-        int pageSum=0;
-        int c=1;
-        for(int i=0;i<n;i++){
-            if(arr[i]>sol)
-            return false;
-            if(pageSum+arr[i]>sol){
-                c++;
-                if(c>k)
-                return false;
-                pageSum=arr[i];
-            }
-            else{
-                pageSum+=arr[i];
-            }
+    bool possible(vector<int>&arr,int mid,int k,int total){
+        int c=1;int sum=0;
+        for(int i=0;i<arr.size();i++){
+        if(arr[i]>mid) return false;
+           sum=sum+arr[i];
+           if(sum>mid){
+               sum=arr[i];
+               c++;
+           }
         }
-        return true;
+        return c<=k;
     }
     int findPages(vector<int> &arr, int k) {
-        int n=arr.size();
-        if(n<k) return -1;
-        int s=0;
-        int e=accumulate(arr.begin(),arr.end(),0);
+        if(k>arr.size()) return -1;
+        int total=accumulate(arr.begin(),arr.end(),0);
+        int s=*min_element(arr.begin(),arr.end());
+        int e=total;
         int ans=-1;
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(possible(arr,n,k,mid)){
+            if(possible(arr,mid,k,total)){
                 ans=mid;
                 e=mid-1;
             }
-            else{
-                s=mid+1;
-            }
+            else
+            s=mid+1;
         }
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int d;
-        vector<int> arr, brr, crr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        ss.clear();
-        ss.str(input);
-        while (ss >> number) {
-            crr.push_back(number);
-        }
-        d = crr[0];
-        int n = arr.size();
-        Solution ob;
-        int ans = ob.findPages(arr, d);
-        cout << ans << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
