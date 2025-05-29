905993e85e81521ccd65f06f2115f920691fa82e
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-29 10:53:11 +0530

    Time: 0 ms (100%), Space: 36.5 MB (50.01%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    // int bs(vector<int>&arr,int s,int e,int t){
-    //     int ans=-1;
-    //     while(s<=e){
-    //         int mid=s+(e-s)/2;
-    //         if(arr[mid]<t)
-    //         s=mid+1;
-    //         if(arr[mid]>=t){
-    //             ans=mid;
-    //             e=mid-1;
-    //         }
-    //     }
-    //     return ans;
-    // }
+    int bs(vector<int>&arr,int s,int e,int t){
+        int ans=e+1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]<t)
+            s=mid+1;
+            if(arr[mid]>=t){
+                ans=mid;
+                e=mid-1;
+            }
+        }
+        return ans;
+    }
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         // priority_queue<pair<int,int>>pq;
         // for(int i=0;i<arr.size();i++){
@@ -30,34 +30,39 @@ public:
 
         
 
-        int s=0;
-        int e=arr.size()-1;
-        vector<int>ans;
-        while(e-s>=k){
-            if(x-arr[s]>arr[e]-x)
-            s++;
-            else
-            e--;
-        }
-        for(int i=s;i<=e;i++){
-           ans.push_back(arr[i]);
-        }
-        return ans;
-
-
-
-
-        // int idx=bs(arr,0,arr.size()-1,x);
-        // int i=idx-1;int j=idx;
+        // int s=0;
+        // int e=arr.size()-1;
         // vector<int>ans;
-        // while(j-i-1<k){
-        //     if(i<0||abs(arr[i]-x)>abs(arr[j]-x)){
-        //         ans.push_back(arr[j++]);
-        //     }
-        //     else if(j>=arr.size()||abs(arr[i]-x)<=abs(arr[j]-x)){
-        //         ans.insert(ans.begin(),arr[i--]);
-        //     }
+        // while(e-s>=k){
+        //     if(x-arr[s]>arr[e]-x)
+        //     s++;
+        //     else
+        //     e--;
+        // }
+        // for(int i=s;i<=e;i++){
+        //    ans.push_back(arr[i]);
         // }
         // return ans;
+
+
+
+
+        int idx=bs(arr,0,arr.size()-1,x);
+        int l=idx-1;int h=idx;
+        vector<int>ans;
+        while(h-l-1<k){
+           if(l<0)
+           h++;
+           else if(h==arr.size())
+           l--;
+           else if(arr[h]-x < x-arr[l])
+           h++;
+           else
+           l--;
+        }
+        for(int i=l+1;i<=h-1;i++){
+            ans.push_back(arr[i]);
+        }
+        return ans;
     }
 };
