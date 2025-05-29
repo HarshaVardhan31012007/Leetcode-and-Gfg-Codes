5ba811a49f9e9ae8d7320c0d49b15c4900144f07
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-29 14:24:37 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.53%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.cpp
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -12,14 +12,23 @@ public:
             //  j--;
             //  if(a==1&&b==1)
             //  swap(s[i++],s[j--]);
+           
 
+            bool a=s[i]>=97&&s[i]<=122||s[i]>=65&&s[i]<=90;
+             bool b=s[j]>=97&&s[j]<=122||s[j]>=65&&s[j]<=90;
+             if(a==0)
+             i++;
+             if(b==0)
+             j--;
+             if(a==1&&b==1)
+             swap(s[i++],s[j--]);
 
-            if(isalpha(s[i])&&isalpha(s[j]))
-            swap(s[i++],s[j--]);
-            else if(!isalpha(s[i]))
-            i++;
-            else
-            j--;
+            // if(isalpha(s[i])&&isalpha(s[j]))
+            // swap(s[i++],s[j--]);
+            // else if(!isalpha(s[i]))
+            // i++;
+            // else
+            // j--;
         }
         return s;
     }
