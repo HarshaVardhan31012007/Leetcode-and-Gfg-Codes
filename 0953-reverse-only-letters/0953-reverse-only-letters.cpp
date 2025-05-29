89ab95e42dede30ca329f5f17310981c997aba2a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-29 14:19:36 +0530

    Time: 0 ms (100%), Space: 7.9 MB (82.42%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.cpp
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -4,14 +4,22 @@ public:
         int i=0;
         int j=s.length()-1;
         while(i<j){
-             bool a=s[i]>='a'&&s[i]<='z'||s[i]>='A'&&s[i]<='Z';
-             bool b=s[j]>='a'&&s[j]<='z'||s[j]>='A'&&s[j]<='Z';
-             if(a==0)
-             i++;
-             if(b==0)
-             j--;
-             if(a==1&&b==1)
-             swap(s[i++],s[j--]);
+            //  bool a=s[i]>='a'&&s[i]<='z'||s[i]>='A'&&s[i]<='Z';
+            //  bool b=s[j]>='a'&&s[j]<='z'||s[j]>='A'&&s[j]<='Z';
+            //  if(a==0)
+            //  i++;
+            //  if(b==0)
+            //  j--;
+            //  if(a==1&&b==1)
+            //  swap(s[i++],s[j--]);
+
+
+            if(isalpha(s[i])&&isalpha(s[j]))
+            swap(s[i++],s[j--]);
+            else if(!isalpha(s[i]))
+            i++;
+            else
+            j--;
         }
         return s;
     }
