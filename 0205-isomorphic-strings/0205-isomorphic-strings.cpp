ae69fc68753152bfca06ccde6ebc405ddae33128
HARSHA VARDHAN <hv1900019@gmail.com>
2025-05-31 13:58:03 +0530

    Time: 0 ms (100%), Space: 9.3 MB (56.72%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int arr[256]={0};int brr[256]={0};
-        for(int i=0;i<s.length();i++){
-            if(arr[s[i]]==0&&brr[t[i]]==0){
-                     arr[s[i]]=t[i];
-                     brr[t[i]]=s[i];
-            }
-            else if(t[i]!=arr[s[i]])
+       int arr[256]={0};
+       int brr[256]={0};
+       for(int i=0;i<s.length();i++){
+         if(!arr[s[i]]&&!brr[t[i]]){
+            arr[s[i]]=t[i];
+            brr[t[i]]=s[i];
+         }
+         else{
+            if(arr[s[i]]!=t[i])
             return false;
-        }
-        return true;
+         }
+       }
+       return true;
     }
 };
