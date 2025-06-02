21b909057bc2aea3f452e4765fc6e5e9ab7ba6de
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-02 08:26:22 +0530

    Time: 0 ms (100%), Space: 12.5 MB (99.35%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,55 +1,88 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-       int i=0;char ch=0;int count=0;//here ch means prev
-       for(int j=0;j<chars.size();j++){
-         if(ch==0)
-         ch=chars[j];
-         if(ch==chars[j])
-         count++;
-         else{
-            chars[i++]=ch;
-            if(count>1){
-            //   string temp="";
-            //   while(count>0){
-            //     char p=count%10+'0';
-            //     temp=p+temp;
-            //     count/=10;
-            //   }
-            //   for(int j=0;j<temp.size();j++)
-            //   chars[i++]=temp[j];
+    //    int i=0;char ch=0;int count=0;//here ch means prev
+    //    for(int j=0;j<chars.size();j++){
+    //      if(ch==0)
+    //      ch=chars[j];
+    //      if(ch==chars[j])
+    //      count++;
+    //      else{
+    //         chars[i++]=ch;
+    //         if(count>1){
+    //         //   string temp="";
+    //         //   while(count>0){
+    //         //     char p=count%10+'0';
+    //         //     temp=p+temp;
+    //         //     count/=10;
+    //         //   }
+    //         //   for(int j=0;j<temp.size();j++)
+    //         //   chars[i++]=temp[j];
 
-            // string k=to_string(count);
-            // for(auto &each:k)
-            // chars[i++]=each;
-            // }
-            // ch=chars[j];
-            // count=1;
+    //         // string k=to_string(count);
+    //         // for(auto &each:k)
+    //         // chars[i++]=each;
+    //         // }
+    //         // ch=chars[j];
+    //         // count=1;
               
-              int start=i;
-              while(count>0){
-                chars[i++]=count%10+'0';
+    //           int start=i;
+    //           while(count>0){
+    //             chars[i++]=count%10+'0';
+    //             count/=10;
+    //           }
+    //           reverse(chars.begin()+start,chars.begin()+i);
+    //      }
+    //       ch=chars[j];
+    //       count=1;
+    //      }
+    //    }
+    //    if(ch!=0){
+    //    chars[i++]=ch;
+    //    if(count>1){
+    //     string temp="";
+    //     while(count>0){
+    //     char p=count%10+'0';
+    //     temp=p+temp;
+    //     count/=10;
+    //     }
+    //      for(int j=0;j<temp.size();j++)
+    //     chars[i++]=temp[j];   
+    //    }                    
+    //    }
+    //    return i;
+    // }
+
+
+
+     int count=1;int idx=0;int prev=chars[0];
+     for(int i=1;i<chars.size();i++){
+        if(prev==chars[i])
+        count++;
+        else{
+            chars[idx++]=prev;
+            if(count>1){
+            int start=idx;
+            while(count>0){
+                chars[idx++]=count%10+'0';
                 count/=10;
-              }
-              reverse(chars.begin()+start,chars.begin()+i);
-         }
-          ch=chars[j];
-          count=1;
-         }
-       }
-       if(ch!=0){
-       chars[i++]=ch;
-       if(count>1){
-        string temp="";
-        while(count>0){
-        char p=count%10+'0';
-        temp=p+temp;
-        count/=10;
+            }
+            reverse(chars.begin()+start,chars.begin()+idx);
+            }
+            prev=chars[i];
+            count=1;
         }
-         for(int j=0;j<temp.size();j++)
-        chars[i++]=temp[j];   
-       }                    
-       }
-       return i;
+     }
+     chars[idx++]=prev;
+     if(count>1){
+       int start=idx;
+        while(count>0){
+            chars[idx++]=count%10+'0';
+            count/=10;
+            }
+        reverse(chars.begin()+start,chars.begin()+idx);
+     }
+
+       return idx;
     }
 };
