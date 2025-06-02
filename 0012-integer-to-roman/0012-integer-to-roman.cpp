1c39dcf299d21aee067930cb1ccdbae37e44b4ed
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-02 17:52:36 +0530

    Time: 0 ms (100%), Space: 9.3 MB (67.56%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -4,11 +4,21 @@ public:
         int  values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
         string strs[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         string ans="";
-        for(int i=0;i<13;i++){
-            while(num>=values[i]){
-                num-=values[i];
+        // for(int i=0;i<13;i++){
+        //     while(num>=values[i]){
+        //         num-=values[i];
+        //         ans+=strs[i];
+        //     }
+        //     if(num==0) break;
+        // }
+        int i=0;
+        while(num>0){
+            if(num>=values[i]){
                 ans+=strs[i];
+                num-=values[i];
             }
+            else
+            i++;
         }
         return ans;
     }
