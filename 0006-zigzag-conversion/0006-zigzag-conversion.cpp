09e896ed139ae949cbc6070378bed3460f0b070f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-03 14:17:28 +0530

    Time: 2 ms (80.77%), Space: 13.9 MB (66.34%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,7 +1,35 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-     if(numRows==1) return s;
+    //  if(numRows==1) return s;// no need for this approach as it starting from row=0 every time
+    //   int i=0;
+    //   int direction=1;
+    //   int row=0;
+    //   vector<string>zigzag(numRows);
+    //    while(true){
+    //     if(direction){
+    //         while(i<s.length()&&row<numRows){
+    //             zigzag[row++].push_back(s[i++]);
+    //         }
+    //         row=numRows-2;
+    //     }
+    //     else{
+    //         while(i<s.length()&&row>=1){
+    //             zigzag[row--].push_back(s[i++]);
+    //         }
+    //         row=0;
+    //     }
+    //     if(i>=s.length()) break;
+    //     direction=!direction;
+    //    }
+    //    string ans="";
+    //    for(int i=0;i<zigzag.size();i++){
+    //     ans+=zigzag[i];
+    //    }
+    //    return ans;
+
+
+     if(numRows==1) return s;// need for this approach as it starting from row=1 every time expect for first
       int i=0;
       int direction=1;
       int row=0;
@@ -14,10 +42,10 @@ public:
             row=numRows-2;
         }
         else{
-            while(i<s.length()&&row>=1){
+            while(i<s.length()&&row>=0){
                 zigzag[row--].push_back(s[i++]);
             }
-            row=0;
+            row=1;
         }
         if(i>=s.length()) break;
         direction=!direction;
