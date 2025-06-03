1683fef652723540ff9bc26b7d141804d87aa506
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-03 17:39:06 +0530

    Time: 3 ms (77.16%), Space: 16.9 MB (57.4%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,48 +1,66 @@
 class Solution {
 public:
-    static bool comp(string &a,string &b){
-       string p=a+b;
-       string q=b+a;
-    //    int i=0;
-    //    while(i<p.length()){
-    //     if(p[i]==q[i]){
-    //         i++;
+    // static bool comp(string &a,string &b){
+    //    string p=a+b;
+    //    string q=b+a;
+    // //    int i=0;
+    // //    while(i<p.length()){
+    // //     if(p[i]==q[i]){
+    // //         i++;
+    // //     }
+    // //     else
+    // //     return p[i]<q[i];
+    // //    }
+    // //    return p[0]<q[0];
+    //     return p<q;
+    // }
+    // string largestNumber(vector<int>& nums) {
+    //     vector<string>arr;
+    //     for(int i=0;i<nums.size();i++){
+    //         // string k="";
+    //         // int n=nums[i];
+    //         // while(n>0){
+    //         //    char ch=n%10+'0';
+    //         //    k=ch+k;
+    //         // }
+
+    //         // string k=to_string(nums[i]);
+    //         // arr.push_back(k);
+
+    //         arr.push_back(to_string(nums[i]));
     //     }
-    //     else
-    //     return p[i]<q[i];
-    //    }
-    //    return p[0]<q[0];
-        return p<q;
-    }
-    string largestNumber(vector<int>& nums) {
+    //     sort(arr.begin(),arr.end(),comp);
+    //     if(arr[arr.size()-1]=="0") return "0";
+    //     string ans="";
+    //     for(int i=arr.size()-1;i>=0;i--){
+    //         ans+=arr[i];
+    //     }
+
+
+    //     // int i=0;
+    //     // while(i<ans.length()&&ans[i]=='0')
+    //     // i++;
+    //     // if(i==ans.length()) return "0";
+    //     // return ans.substr(i);
+
+    //    return ans;
+    // }
+
+      static bool comp(string &a,string &b){
+        string p=a+b;
+        string q=b+a;
+        return p>q;
+      }
+      string largestNumber(vector<int>& nums) {
         vector<string>arr;
         for(int i=0;i<nums.size();i++){
-            // string k="";
-            // int n=nums[i];
-            // while(n>0){
-            //    char ch=n%10+'0';
-            //    k=ch+k;
-            // }
-
-            // string k=to_string(nums[i]);
-            // arr.push_back(k);
-
             arr.push_back(to_string(nums[i]));
         }
         sort(arr.begin(),arr.end(),comp);
-        if(arr[arr.size()-1]=="0") return "0";
+        if(arr[0]=="0") return "0";
         string ans="";
-        for(int i=arr.size()-1;i>=0;i--){
-            ans+=arr[i];
-        }
-
-
-        // int i=0;
-        // while(i<ans.length()&&ans[i]=='0')
-        // i++;
-        // if(i==ans.length()) return "0";
-        // return ans.substr(i);
-
-       return ans;
-    }
+        for(int i=0;i<arr.size();i++)
+        ans+=arr[i];
+        return ans;
+      }
 };
