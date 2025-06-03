6058e3cd0ba93387508d7cfe70528554dc442775
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-03 17:31:09 +0530

    Time: 7 ms (26.73%), Space: 17 MB (41.22%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -3,15 +3,16 @@ public:
     static bool comp(string &a,string &b){
        string p=a+b;
        string q=b+a;
-       int i=0;
-       while(i<p.length()){
-        if(p[i]==q[i]){
-            i++;
-        }
-        else
-        return p[i]<q[i];
-       }
-       return p[0]<q[0];
+    //    int i=0;
+    //    while(i<p.length()){
+    //     if(p[i]==q[i]){
+    //         i++;
+    //     }
+    //     else
+    //     return p[i]<q[i];
+    //    }
+    //    return p[0]<q[0];
+        return p<q;
     }
     string largestNumber(vector<int>& nums) {
         vector<string>arr;
@@ -22,18 +23,26 @@ public:
             //    char ch=n%10+'0';
             //    k=ch+k;
             // }
-            string k=to_string(nums[i]);
-            arr.push_back(k);
+
+            // string k=to_string(nums[i]);
+            // arr.push_back(k);
+
+            arr.push_back(to_string(nums[i]));
         }
         sort(arr.begin(),arr.end(),comp);
+        if(arr[arr.size()-1]=="0") return "0";
         string ans="";
         for(int i=arr.size()-1;i>=0;i--){
             ans+=arr[i];
         }
-        int i=0;
-        while(i<ans.length()&&ans[i]=='0')
-        i++;
-        if(i==ans.length()) return "0";
-        return ans.substr(i);
+
+
+        // int i=0;
+        // while(i<ans.length()&&ans[i]=='0')
+        // i++;
+        // if(i==ans.length()) return "0";
+        // return ans.substr(i);
+
+       return ans;
     }
 };
