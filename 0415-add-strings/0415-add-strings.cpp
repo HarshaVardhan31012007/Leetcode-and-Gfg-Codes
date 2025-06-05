3459760d55d1fcaf8a754d9ce51a56a28e45f55f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-05 18:37:26 +0530

    Time: 0 ms (100%), Space: 8.9 MB (99.62%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
+        // string ans="";
+        // int i=num1.length()-1;int j=num2.length()-1;
+        // int carry=0;
+        // while(i>=0&&j>=0){
+        //     int sum=(num1[i--]-'0')+(num2[j--]-'0')+carry;
+        //     ans+=(sum%10)+'0';
+        //     carry=sum/10;
+        // }
+        // while(i>=0){
+        //     int sum=(num1[i--]-'0')+carry;
+        //     ans+=(sum%10)+'0';
+        //     carry=sum/10;
+        // }
+        // while(j>=0){
+        //     int sum=carry+(num2[j--]-'0');
+        //     ans+=(sum%10)+'0';
+        //     carry=sum/10;
+        // }
+        // if(carry)
+        // ans+=carry+'0';
+        // reverse(ans.begin(),ans.end());
+        // return ans;
+
+
         string ans="";
         int i=num1.length()-1;int j=num2.length()-1;
         int carry=0;
-        while(i>=0&&j>=0){
-            int sum=(num1[i--]-'0')+(num2[j--]-'0')+carry;
-            ans+=(sum%10)+'0';
+        while(i>=0||j>=0||carry){
+            int a=i>=0?num1[i--]-'0':0;
+            int b=j>=0?num2[j--]-'0':0;
+            int sum=a+b+carry;
+            ans+=sum%10+'0';
             carry=sum/10;
         }
-        while(i>=0){
-            int sum=(num1[i--]-'0')+carry;
-            ans+=(sum%10)+'0';
-            carry=sum/10;
-        }
-        while(j>=0){
-            int sum=carry+(num2[j--]-'0');
-            ans+=(sum%10)+'0';
-            carry=sum/10;
-        }
-        if(carry)
-        ans+=carry+'0';
         reverse(ans.begin(),ans.end());
         return ans;
     }
