4f7eabd1b19ef5103cf31bc8c04413f20d69ea92
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-05 22:47:18 +0530

    Time: 4 ms (16.68%), Space: 12.8 MB (5.61%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {//while(s.length()!=0&&s.find(part)<s.length())
-        // while(s.find(part)!=string::npos){
-        //     s.erase(s.find(part),part.length());
-        // }
-        // return s;
-        // while(s.find(part)!=-1){
-        //     s.erase(s.find(part),part.length());
-        // }
-        // return s;
-        // int findkaIndex=s.find(part);
-        // while(findkaIndex!=-1){
-        //     s.erase(s.find(part),part.length());
-        //     findkaIndex=s.find(part);
-        // }
-        // return s;
-    //     string x = s;
-	// int n = s.size(), m = part.size(), i, j;
-	// for (i = 0, j = 0; i < n; i++) {
-	// 	x[j++] = s[i];
-	// 	if (j >= m && x.substr(j - m, m) == part)
-	// 		j -= m;
-	// }
-	// return x.substr(0, j);
-int findkaIndex=s.find(part);
-        while(1){
-            if(findkaIndex==string::npos)
-            return s;
-            else
-            s.erase(s.find(part),part.length());
-            findkaIndex=s.find(part);
+    void solve(string &s,string &part){
+          
+        auto idx=s.find(part);
+        if(idx!=string::npos){
+            string left=s.substr(0,idx);
+            string right=s.substr(idx+part.size(),s.size());
+            s=left+right;
+            solve(s,part);
         }
-        return s;
+        else
+        return;
+    }
+    string removeOccurrences(string s, string part) {
 
 
+    //  int findkaIndex=s.find(part);
+    //     while(1){
+    //         if(findkaIndex==string::npos)
+    //         return s;
+    //         else
+    //         s.erase(findkaIndex,part.length());
+    //         findkaIndex=s.find(part);
+    //     }
+    //     return s;
 
+     
+     solve(s,part);
+      return s;
     }
 };
