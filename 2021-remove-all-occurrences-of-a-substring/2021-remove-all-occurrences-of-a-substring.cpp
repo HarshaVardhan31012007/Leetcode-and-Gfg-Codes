7d9d200acce1b7cb2e437cae4f4952966166cf99
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-05 22:48:44 +0530

    Time: 2 ms (41.85%), Space: 9.4 MB (70.75%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    void solve(string &s,string &part){
+    // void solve(string &s,string &part){
           
-        auto idx=s.find(part);
-        if(idx!=string::npos){
-            string left=s.substr(0,idx);
-            string right=s.substr(idx+part.size(),s.size());
-            s=left+right;
-            solve(s,part);
-        }
-        else
-        return;
-    }
+    //     auto idx=s.find(part);
+    //     if(idx!=string::npos){
+    //         string left=s.substr(0,idx);
+    //         string right=s.substr(idx+part.size(),s.size());
+    //         s=left+right;
+    //         solve(s,part);
+    //     }
+    //     else
+    //     return;
+    // }
     string removeOccurrences(string s, string part) {
 
 
-    //  int findkaIndex=s.find(part);
-    //     while(1){
-    //         if(findkaIndex==string::npos)
-    //         return s;
-    //         else
-    //         s.erase(findkaIndex,part.length());
-    //         findkaIndex=s.find(part);
-    //     }
-    //     return s;
+     int findkaIndex=s.find(part);
+        while(1){
+            if(findkaIndex==string::npos)
+            return s;
+            else
+            s.erase(findkaIndex,part.length());
+            findkaIndex=s.find(part);
+        }
+        return s;
 
      
-     solve(s,part);
-      return s;
+    //  solve(s,part);
+    //   return s;
     }
 };
