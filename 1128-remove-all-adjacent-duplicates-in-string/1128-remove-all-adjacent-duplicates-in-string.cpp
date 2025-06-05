2bebe659f6072cf72fb55e75c08e1332080c94b6
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-05 16:54:31 +0530

    Time: 0 ms (100%), Space: 13.3 MB (90.3%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        string ans="";
-        for(int i=0;i<s.length();i++){
-            if(ans.empty()||ans.back()!=s[i])
-            ans.push_back(s[i]);
-            else
-            ans.pop_back();
+        // string ans="";
+        // for(int i=0;i<s.length();i++){
+        //     if(ans.empty()||ans.back()!=s[i])
+        //     ans.push_back(s[i]);
+        //     else
+        //     ans.pop_back();
+        // }
+        // return ans;
+
+        
+
+        int i=0;int n=s.length();
+        for(int j=0;j<n;j++,i++){
+            s[i]=s[j];
+            if(i>0&&s[i]==s[i-1])
+            i=i-2;
         }
-        return ans;
+        return s.substr(0,i);
     }
 };
