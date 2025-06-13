795b9ca57f24b205e1cbfc104ed075b5eb032b6f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-13 12:17:21 +0530

    Time: 6 ms (6.02%), Space: 8.7 MB (21.43%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -12,19 +12,32 @@ public:
 
 
         int ans=0;
-        bool flag=0;
-        if(x<0){
-        flag=1;
-        if(x==INT_MIN)
-        return 0;
-        x=-x;
-        }
         while(x){
-           if(ans>INT_MAX/10)
+            int digit=x%10;
+           if(ans>INT_MAX/10||ans==INT_MAX/10&&digit>7)
            return 0;
-           ans=ans*10+x%10;
+           if(ans<INT_MIN/10||ans==INT_MIN/10&&digit<-8)
+           return 0;
+           ans=ans*10+digit;
            x=x/10;
         }
-        return flag==1?-ans:ans;
+        return ans;
+
+
+        // int ans=0;
+        // bool flag=0;
+        // if(x<0){
+        // flag=1;
+        // if(x==INT_MIN)
+        // return 0;
+        // x=-x;
+        // }
+        // while(x){
+        //    if(ans>INT_MAX/10)
+        //    return 0;
+        //    ans=ans*10+x%10;
+        //    x=x/10;
+        // }
+        // return flag==1?-ans:ans;
     }
 };
