d36ec31e5e32ecf4deccc0d2184435e110e2dd24
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-13 13:01:45 +0530

    Time: 2 ms (33.75%), Space: 10.7 MB (57.57%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -18,9 +18,9 @@ public:
        dp[0]=0;
        for(int i=1;i<n+1;i++){
         if(i&1)
-        dp[i]=1+dp[i/2];
+        dp[i]=1+dp[i>>1];
         else
-        dp[i]=dp[i/2];
+        dp[i]=dp[i>>1];
        }
        return dp;
     }
