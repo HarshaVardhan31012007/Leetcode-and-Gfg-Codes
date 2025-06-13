c8dbf8c2423da986031cf0216cfb8a4a013ce3e5
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-13 12:33:20 +0530

    Time: 0 ms (100%), Space: 8.2 MB (47.36%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -8,11 +8,19 @@ public:
     //    } 
     //    return count;
 
-     int count=0;
-       while(n){
-        if((n&1)==1) count++;
-        n=n>>1;
-       } 
-       return count;
+    //  int count=0;
+    //    while(n){
+    //     if((n&1)==1) count++;
+    //     n=n>>1;
+    //    } 
+    //    return count;
+
+
+    int count=0;
+    while(n){
+      count++;
+      n=n&n-1;
+    }
+    return count;
     }
 };
