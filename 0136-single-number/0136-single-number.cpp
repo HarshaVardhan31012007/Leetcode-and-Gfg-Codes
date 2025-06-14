8d5cc78308b83420e19191ee739dbebb981af58c
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-14 16:47:29 +0530

    Time: 15 ms (11.13%), Space: 24 MB (9.83%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            ans=ans^nums[i];
+        // int ans=0;
+        // for(int i=0;i<nums.size();i++){
+        //     ans=ans^nums[i];
+        // }
+        // return ans;
+
+        unordered_map<int,int>mpp;
+        for(auto &num:nums)
+        mpp[num]++;
+        for(auto &each:mpp){
+            if(each.second&1)
+            return each.first;
         }
-        return ans;
+        return -1;
     }
 };
