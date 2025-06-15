f0ef0691cba6c90011eee436f44917b48e4ef9e9
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-15 22:00:27 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.29%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,17 +4,31 @@ public:
        // sort(nums.begin(),nums.end());
 
 
-       int zc=0;int oc=0;int tc=0;
-       for(auto &num:nums){
-        if(num==0)
-        zc++;
-        else if(num==1)
-        oc++;
-        else
-        tc++;
-       }
-       fill(nums.begin(),nums.begin()+zc,0);
-       fill(nums.begin()+zc,nums.begin()+zc+oc,1);
-       fill(nums.begin()+zc+oc,nums.end(),2);
+    //    int zc=0;int oc=0;int tc=0;
+    //    for(auto &num:nums){
+    //     if(num==0)
+    //     zc++;
+    //     else if(num==1)
+    //     oc++;
+    //     else
+    //     tc++;
+    //    }
+    //    fill(nums.begin(),nums.begin()+zc,0);
+    //    fill(nums.begin()+zc,nums.begin()+zc+oc,1);
+    //    fill(nums.begin()+zc+oc,nums.end(),2);
+
+      int l=0;
+      int m=0;
+      int h=nums.size()-1;
+      while(m<=h){
+        if(nums[m]==0){
+            swap(nums[m],nums[l]);
+            l++;m++;
+        }
+        else if(nums[m]==1)
+        m++;
+        else 
+        swap(nums[m],nums[h--]);
+      }
     }
 };
