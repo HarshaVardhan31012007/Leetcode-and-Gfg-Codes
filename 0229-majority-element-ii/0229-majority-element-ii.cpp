6a4640c382f7faa07e5a92f1d80439f704645a20
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-16 22:55:06 +0530

    Time: 0 ms (100%), Space: 23.7 MB (46.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -18,15 +18,48 @@ public:
     //   return ans;
 
 
-    unordered_map<int,int>mpp;
-    vector<int>ans;
+    // unordered_map<int,int>mpp;
+    // vector<int>ans;
+    // for(int i=0;i<nums.size();i++){
+    //   mpp[nums[i]]++;
+    // }
+    //  for(auto &mp:mpp){
+    //   if(mp.second>nums.size()/3)
+    //   ans.push_back(mp.first);
+    //  }
+    //   return ans;
+    // }
+
+    int ans1=-1;int freq1=0;
+    int ans2=-2;int freq2=0;
     for(int i=0;i<nums.size();i++){
-      mpp[nums[i]]++;
+        if(freq1==0&&nums[i]!=ans2){
+            ans1=nums[i];
+            freq1=1;
+        }
+        else if(freq2==0&&nums[i]!=ans1){
+            ans2=nums[i];
+            freq2=1;
+        }
+        else if(ans1==nums[i])
+        freq1++;
+        else if(ans2==nums[i])
+        freq2++;
+        else{
+            freq1--;
+            freq2--;
+        }
     }
-     for(auto &mp:mpp){
-      if(mp.second>nums.size()/3)
-      ans.push_back(mp.first);
-     }
-      return ans;
+
+    vector<int>ans;
+    int c1=0;int c2=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]==ans1) c1++;
+        if(nums[i]==ans2) c2++;
+    }
+    if(c1>nums.size()/3) ans.push_back(ans1);
+    if(c2>nums.size()/3) ans.push_back(ans2);
+    sort(ans.begin(),ans.end());
+    return ans;
     }
 };
