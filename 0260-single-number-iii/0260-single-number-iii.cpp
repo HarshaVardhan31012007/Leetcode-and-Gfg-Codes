bf96bd2414fce6ce3ff82ca68c58dbf8cb2bf9cc
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-16 19:24:41 +0530

    Time: 6 ms (10.01%), Space: 14.8 MB (11.97%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            if((i==0||nums[i]!=nums[i-1])&&(i==nums.size()-1||nums[i]!=nums[i+1]))
-            ans.push_back(nums[i]);
-        }
-        return ans;
+        // sort(nums.begin(),nums.end());
+        // vector<int>ans;
+        // for(int i=0;i<nums.size();i++){
+        //     if((i==0||nums[i]!=nums[i-1])&&(i==nums.size()-1||nums[i]!=nums[i+1]))
+        //     ans.push_back(nums[i]);
+        // }
+        // return ans;
+
+      unordered_map<int,int>mpp;
+      for(int i=0;i<nums.size();i++){
+        mpp[nums[i]]++;
+      }
+      int ans1=-1;int ans2=-1;
+      for(int i=0;i<nums.size();i++){
+          if(mpp[nums[i]]==1&&(ans1!=nums[i]&&ans2!=nums[i])){
+            if(ans1==-1)
+            ans1=nums[i];
+            else
+            ans2=nums[i];
+          }
+      }
+          return {ans1,ans2};
     }
 };
