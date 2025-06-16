9a2293a0be889d7b9f8b055932df2f3c2d797f88
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-16 19:51:35 +0530

    Time: 0 ms (100%), Space: 64.6 MB (89.88%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
@@ -16,7 +16,7 @@ public:
         // return {ans,maxi};
 
 
-          int ans=0;int maxi=INT_MIN;
+        int ans=0;int maxi=INT_MIN;
         for(int i=0;i<mat.size();i++){
            int count=accumulate(mat[i].begin(),mat[i].end(),0);
            if(count>maxi){
