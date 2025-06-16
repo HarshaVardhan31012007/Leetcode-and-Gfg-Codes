83839596881d508e858b411bbf2086f50afb9364
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-16 21:17:27 +0530

    Time: 0 ms (100%), Space: 28 MB (84.49%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -48,9 +48,11 @@ public:
         int freq=0;
         int ans=0;
         for(int i=0;i<nums.size();i++){
-            if(freq==0)
+            if(freq==0){
             ans=nums[i];
-            if(ans==nums[i])
+            freq=1;
+            }
+            else if(ans==nums[i])
             freq++;
             else{
                 freq--;
