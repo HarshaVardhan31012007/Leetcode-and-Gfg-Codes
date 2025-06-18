8cb8d8c745a3fcd7628f2bc7651a25d143d49f26
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-18 18:22:49 +0530

    Time: 7 ms (35.54%), Space: 22.1 MB (81.17%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -24,17 +24,42 @@ public:
     // }
 
 
-    bool solve(string &s,int i,int j,int count){
-        if(count>1) return false;
-        if(i>=j) return true;
-        if(s[i]==s[j])
-        return solve(s,i+1,j-1,count);
-        else
-        return solve(s,i,j-1,count+1)||solve(s,i+1,j,count+1);
+    // bool solve(string &s,int i,int j,int count){
+    //     if(count>1) return false;
+    //     if(i>=j) return true;
+    //     if(s[i]==s[j])
+    //     return solve(s,i+1,j-1,count);
+    //     else
+    //     return solve(s,i,j-1,count+1)||solve(s,i+1,j,count+1);
+    // }
+    // bool validPalindrome(string s) {
+    //   int i=0;
+    //   int j=s.length()-1;
+    //   return solve(s,i,j,0);
+    // }
+
+   
+    bool checkp(string &temp,int i,int j){
+        while(i<j){
+            if(temp[i]!=temp[j])
+            return false;
+            i++;
+            j--;
+        }
+        return true;
     }
     bool validPalindrome(string s) {
       int i=0;
       int j=s.length()-1;
-      return solve(s,i,j,0);
+       while(i<j){
+        if(s[i]==s[j]){
+            i++;
+            j--;
+        }
+        else{
+            return checkp(s,i+1,j)||checkp(s,i,j-1);
+        }
+       }
+       return true;
     }
 };
