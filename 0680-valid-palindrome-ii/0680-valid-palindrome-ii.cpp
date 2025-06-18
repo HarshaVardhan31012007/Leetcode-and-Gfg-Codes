de03a4a7fd47230aefdcc4f91ff40c3b3ca883a4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-18 18:17:03 +0530

    Time: 3 ms (72.19%), Space: 22.2 MB (64.24%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
-    bool checkp(string &ans,int s,int e){
-        while(s<=e){
+    // bool checkpalindrome(string &temp,int i,int j){
+    //     int i=0;int j=temp.size()-1;
+    //     while(i<j){
+    //         if(temp[i]!=temp[j])
+    //         return false;
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
+    // bool validPalindrome(string s) {
+    //     if(checkpalindrome(s)) return true;
+    //     for(int i=0;i<s.size();i++){
+    //         string temp=s;
+    //         for(int i=0;i<s.size();i++){
+    //             temp.erase(i,1);
+    //             if(checkpalindrome(temp)) return true;
+    //             temp=s;
+    //         }
+    //     }
+    //     return false;
+    // }
 
-            if(ans[s]!=ans[e]){
-            return false;
-            }
-            else{
-                s++;
-                e--;
-            }
 
-        }
-
-        return true;
-
+    bool solve(string &s,int i,int j,int count){
+        if(count>1) return false;
+        if(i>=j) return true;
+        if(s[i]==s[j])
+        return solve(s,i+1,j-1,count);
+        else
+        return solve(s,i,j-1,count+1)||solve(s,i+1,j,count+1);
     }
     bool validPalindrome(string s) {
-        int i=0,j=s.size()-1;
-        while(i<=j){
-            if(s[i]==s[j]){
-                i++;
-                j--;
-            }
-            else{
-                bool a=checkp(s,i+1,j);
-                bool b=checkp(s,i,j-1);
-                cout<<a<<b<<endl;
-                bool c=a||b;
-                return c;
-            }
-        }
-        return true;
+      int i=0;
+      int j=s.length()-1;
+      return solve(s,i,j,0);
     }
 };
