54b839f51cf3c25f1943abd3e8e38076b65b0658
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-19 16:04:20 +0530

    Time: 0 ms (100%), Space: 9.8 MB (50.23%) - LeetHub

--- 2406-decode-the-message/2406-decode-the-message.cpp
+++ 2406-decode-the-message/2406-decode-the-message.cpp
@@ -1,62 +1,19 @@
 class Solution {
 public:
-    void createmapping(unordered_map<char,char>&mpp,string &key){
-        char start='a';
-        int i=0;
-        while(start<='z'){//enough constraint 3 hence you no need of i<key.length()
-            char c=key[i];
-            if(mpp.find(c)!=mpp.end())
-            i++;
-            else{
-                mpp[c]=start;
-                start++;
-                i++;
+    string decodeMessage(string key, string message) {
+        unordered_map<char,char>mpp;
+        int j=0;
+        for(int i=0;i<key.length();i++){
+            if(key[i]==' ') continue;
+            if(mpp.find(key[i])==mpp.end()){
+                mpp[key[i]]=j+'a';
+                j++;
             }
         }
-    }
-    string usemapping(string &message,unordered_map<char,char>mpp){
-        string s="";
         for(int i=0;i<message.length();i++){
-            //  int mc=message[i];
-            //  int mppc=mpp[mc];
-             char mc=message[i];
-             char mppc=mpp[mc];
-             s.push_back(mppc);
+           if(message[i]==' ') continue;
+           message[i]=mpp[message[i]];
         }
-        return s;
-    }
-    string decodeMessage(string key, string message) {
-        // unordered_map<char,char>mpp;
-        // char start='a';
-        // int i=0;
-        // char d=' ';
-        // mpp[d]=d;
-        // while(start<='z'){//enough constraint 3 hence you no need of i<key.length()
-        //     char c=key[i];
-        //     if(mpp.find(c)!=mpp.end())
-        //     i++;
-        //     else{
-        //         mpp[c]=start;
-        //         start++;
-        //         i++;
-        //     }
-        // }
-        // string s="";
-        // for(int i=0;i<message.length();i++){
-        //     //  int mc=message[i];
-        //     //  int mppc=mpp[mc];
-        //      char mc=message[i];
-        //      char mppc=mpp[mc];
-        //      s.push_back(mppc);
-        // }
-        // return s;
-        unordered_map<char,char>mpp;
-        char d=' ';
-        mpp[d]=d;
-        createmapping(mpp,key);
-
-        string ans=usemapping(message,mpp);
-
-        return ans;
+        return message;
     }
 };
