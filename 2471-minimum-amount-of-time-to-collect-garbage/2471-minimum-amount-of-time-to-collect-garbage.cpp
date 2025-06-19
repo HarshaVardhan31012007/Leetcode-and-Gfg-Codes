179924c642bcd36a7f6e9394191ac1bebc08b8e3
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-19 16:38:50 +0530

    Time: 63 ms (5.03%), Space: 107.2 MB (59.68%) - LeetHub

--- 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,49 +1,43 @@
 class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
-        int pickP=0;
-        int travelP=0;
-        int lastHouseP=0;
-
-        int pickM=0;
-        int travelM=0;
-        int lastHouseM=0;
-
-        int pickG=0;
-        int travelG=0;
-        int lastHouseG=0;
-
+        int countP=0;int countG=0;int countM=0;
         for(int i=0;i<garbage.size();i++){
-            string curr_character=garbage[i];
-            for(int j=0;j<curr_character.length();j++){
-                char garbagetype=curr_character[j];
-                if(garbagetype=='P'){
-                    pickP++;
-                    lastHouseP=i;
-                }
-                if(garbagetype=='M'){
-                    pickM++;
-                    lastHouseM=i;
-                }
-                if(garbagetype=='G'){
-                    pickG++;
-                    lastHouseG=i;
-                }
+            for(auto &each:garbage[i]){
+                if(each=='P') countP++;
+                else if(each=='G') countG++;
+                else countM++;
+             }
         }
+        int ans=0;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<garbage.size();j++){
+                for(auto &each:garbage[j]){
+                     if(each=='P'){
+                       if(i==0){
+                       countP--;
+                       ans++;
+                       }
+                     }
+                     else if(each=='G'){
+                       if(i==1){
+                       countG--;
+                       ans++;
+                     }
+                     }
+                     else{
+                        if(i==2){
+                        countM--;
+                        ans++;
+                        }
+                     }
+                }
+                if(i==0&&countP==0) break;
+                if(i==1&&countG==0) break;
+                if(i==2&&countM==0) break;
+                 ans+=travel[j];
+            }
         }
-        for(int i=0;i<lastHouseP;i++)
-        travelP=travelP+travel[i];
-
-        for(int i=0;i<lastHouseM;i++)
-        travelM=travelM+travel[i];
-
-        for(int i=0;i<lastHouseG;i++)
-        travelG=travelG+travel[i];
-        
-        int totalpicktime=pickP+pickM+pickG;
-        int totaltraveltime=travelP+travelM+travelG;
-        int totaltime=totalpicktime+totaltraveltime;
-
-        return totaltime;
+        return ans;
     }
 };
