917500ad38a43a3ceda4d6fb5a801d270d0ac20d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-19 16:07:04 +0530

    Time: 2 ms (30.36%), Space: 9.5 MB (81.4%) - LeetHub

--- 2406-decode-the-message/2406-decode-the-message.cpp
+++ 2406-decode-the-message/2406-decode-the-message.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     string decodeMessage(string key, string message) {
         unordered_map<char,char>mpp;
-        int j=0;
+        //int j=0;
+        char start='a';
         for(int i=0;i<key.length();i++){
             if(key[i]==' ') continue;
             if(mpp.find(key[i])==mpp.end()){
-                mpp[key[i]]=j+'a';
-                j++;
+                // mpp[key[i]]=j+'a';
+                // j++;
+                mpp[key[i]]=start++;
             }
         }
         for(int i=0;i<message.length();i++){
