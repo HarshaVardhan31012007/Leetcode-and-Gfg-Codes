9550ce5dd883b040de7af04591b2dae38b2e3700
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-19 17:21:44 +0530

    Time: 3 ms (98.04%), Space: 107.2 MB (59.68%) - LeetHub

--- 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
@@ -41,25 +41,80 @@ public:
         // return ans;
 
 
+        // int ans=0;
+        // int lastP=0;
+        // int lastG=0;
+        // int lastM=0;
+        // for(int i=0;i<garbage.size();i++){
+        //     for(auto &each:garbage[i]){
+        //         if(each=='P') lastP=i;
+        //         else if(each=='G') lastG=i;
+        //         else lastM=i;
+        //         ans++;
+        //     }
+        // }
+        // // vector<int>prefix(travel.size()+1,0);
+        // // for(int i=1;i<travel.size()+1;i++){
+        // //     prefix[i]=travel[i-1]+prefix[i-1];
+        // // }
+        // // ans+=prefix[lastP]+prefix[lastG]+prefix[lastM];
+
+
+        // for(int i=0;i<lastP;i++)
+        // ans+=travel[i];
+        // for(int i=0;i<lastM;i++)
+        // ans+=travel[i];
+        // for(int i=0;i<lastG;i++)
+        // ans+=travel[i];
+        // return ans;
+
+
+
+        // int ans=0;
+        // int lastP=0;
+        // int lastG=0;
+        // int lastM=0;
+        // for(int i=0;i<garbage.size();i++){
+        //     for(auto &each:garbage[i]){
+        //         if(each=='P') lastP=i;
+        //         else if(each=='G') lastG=i;
+        //         else lastM=i;
+        //         ans++;
+        //     }
+        // }
+        // // vector<int>prefix(travel.size()+1,0);
+        // // for(int i=1;i<travel.size()+1;i++){
+        // //     prefix[i]=travel[i-1]+prefix[i-1];
+        // // }
+        // // ans+=prefix[lastP]+prefix[lastG]+prefix[lastM];
+
+
+        // for(int i=0;i<lastP;i++)
+        // ans+=travel[i];
+        // for(int i=0;i<lastM;i++)
+        // ans+=travel[i];
+        // for(int i=0;i<lastG;i++)
+        // ans+=travel[i];
+        // return ans;
+
+
         int ans=0;
         int lastP=0;
         int lastG=0;
         int lastM=0;
-        for(int i=0;i<garbage.size();i++){
-            for(auto &each:garbage[i]){
-                if(each=='P') lastP=i;
-                else if(each=='G') lastG=i;
-                else lastM=i;
-                ans++;
+        for(int i=garbage.size()-1;i>=0;i--){
+            if(lastP==0||lastG==0||lastM==0){
+                for(auto &each:garbage[i]){
+                    if(each=='P'&&lastP==0)
+                    lastP=i;
+                    if(each=='M'&&lastM==0)
+                    lastM=i;
+                    if(each=='G'&&lastG==0)
+                    lastG=i;
+                }
             }
+            ans+=garbage[i].size();
         }
-        // vector<int>prefix(travel.size()+1,0);
-        // for(int i=1;i<travel.size()+1;i++){
-        //     prefix[i]=travel[i-1]+prefix[i-1];
-        // }
-        // ans+=prefix[lastP]+prefix[lastG]+prefix[lastM];
-
-
         for(int i=0;i<lastP;i++)
         ans+=travel[i];
         for(int i=0;i<lastM;i++)
