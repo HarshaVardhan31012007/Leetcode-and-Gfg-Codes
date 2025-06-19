1dc281ac8bf0301eed7b91d6c4b39e583451ea85
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-19 21:46:09 +0530

    Time: 2 ms (20.1%), Space: 8.3 MB (51.84%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.cpp
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -1,12 +1,30 @@
-string str;
+// string str;
+// class Solution {
+// public:
+//     static bool comp(char &first,char &second){
+//       return str.find(first)<str.find(second);
+//     }
+//     string customSortString(string order, string s) {
+//         str=order;
+//         sort(s.begin(),s.end(),comp);
+//         return s;
+//     }
+// };
+
+
 class Solution {
 public:
-    static bool comp(char &first,char &second){
-      return str.find(first)<str.find(second);
+    string str;
+
+    bool comp(char first, char second) {
+        return str.find(first) < str.find(second);
     }
+
     string customSortString(string order, string s) {
-        str=order;
-        sort(s.begin(),s.end(),comp);
+        str = order;
+        sort(s.begin(), s.end(), [this](char a, char b) {
+            return comp(a, b);
+        });
         return s;
     }
 };
