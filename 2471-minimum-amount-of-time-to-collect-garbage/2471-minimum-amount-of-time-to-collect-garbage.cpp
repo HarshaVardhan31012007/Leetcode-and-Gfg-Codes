377e6d8a9335419c9b4142b669cdf8f6e73982c5
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-19 17:10:59 +0530

    Time: 16 ms (71.81%), Space: 107.2 MB (30.27%) - LeetHub

--- 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
@@ -53,11 +53,19 @@ public:
                 ans++;
             }
         }
-        vector<int>prefix(travel.size()+1,0);
-        for(int i=1;i<travel.size()+1;i++){
-            prefix[i]=travel[i-1]+prefix[i-1];
-        }
-        ans+=prefix[lastP]+prefix[lastG]+prefix[lastM];
+        // vector<int>prefix(travel.size()+1,0);
+        // for(int i=1;i<travel.size()+1;i++){
+        //     prefix[i]=travel[i-1]+prefix[i-1];
+        // }
+        // ans+=prefix[lastP]+prefix[lastG]+prefix[lastM];
+
+
+        for(int i=0;i<lastP;i++)
+        ans+=travel[i];
+        for(int i=0;i<lastM;i++)
+        ans+=travel[i];
+        for(int i=0;i<lastG;i++)
+        ans+=travel[i];
         return ans;
     }
 };
