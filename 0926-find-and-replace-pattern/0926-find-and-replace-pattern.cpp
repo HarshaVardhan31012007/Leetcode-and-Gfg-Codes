6ec1b4843e2943b9154b12b09dbbcbed48794bd5
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-20 21:17:31 +0530

    Time: 0 ms (100%), Space: 10.8 MB (99.27%) - LeetHub

--- 0926-find-and-replace-pattern/0926-find-and-replace-pattern.cpp
+++ 0926-find-and-replace-pattern/0926-find-and-replace-pattern.cpp
@@ -1,57 +1,56 @@
 class Solution {
 public:
-        //vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-        // vector<string>ans;
-        // for(int i=0;i<words.size();i++){
-        //     string s=words[i];
-        //     string t=pattern;
-        //     int arr[256]={0};
-        //     int brr[256]={0};
-        //     bool flag=1;
-        //     for(int i=0;i<s.length();i++){
-        //     if(!arr[s[i]]&&!brr[t[i]]){
-        //         arr[s[i]]=t[i];
-        //         brr[t[i]]=s[i];
-        //     }
-        //     else{
-        //         if(arr[s[i]]!=t[i])
-        //         flag=0;
-        //     }
-        // }
-        // if(flag)
-        // ans.push_back(s);
-        // }
-        // return ans;
-         //}
-
-
-        void normalise(string &s){
-       unordered_map<char,char>mpp;
-       char start='a';
-       for(int i=0;i<s.length();i++){//
-          char c=s[i];
-          if(mpp.find(c)==mpp.end()){
-            mpp[c]=start;
-            start++;
-          }
-       }
-
-       for(int i=0;i<s.length();i++){
-        char mappedcharacter=mpp[s[i]];
-        s[i]=mappedcharacter;
-       }
-    }
-    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-        normalise(pattern);
+        vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         vector<string>ans;
         for(int i=0;i<words.size();i++){
-           string c=words[i];
-           string ccopy=c;
-           normalise(ccopy);
-        //    if(ccopy==pattern)
-        if(ccopy.compare(pattern)==0)
-           ans.push_back(c);
+            string s=words[i];
+            string t=pattern;
+            char arr[26]={0};
+            char brr[26]={0};
+            bool flag=1;
+            for(int i=0;i<s.length();i++){
+            if(!arr[s[i]-'a']&&!brr[t[i]-'a']){
+                arr[s[i]-'a']=t[i];
+                brr[t[i]-'a']=s[i];
+            }
+            else{
+                if(arr[s[i]-'a']!=t[i])
+                flag=0;
+            }
+        }
+        if(flag)
+        ans.push_back(s);
         }
         return ans;
-    }
+         }
+
+
+    //     void normalise(string &s){
+    //    unordered_map<char,char>mpp;
+    //    char start='a';
+    //    for(int i=0;i<s.length();i++){//
+    //       char c=s[i];
+    //       if(mpp.find(c)==mpp.end()){
+    //         mpp[c]=start;
+    //         start++;
+    //       }
+    //    }
+
+    //    for(int i=0;i<s.length();i++){
+    //     char mappedcharacter=mpp[s[i]];
+    //     s[i]=mappedcharacter;
+    //    }
+    // }
+    // vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+    //     normalise(pattern);
+    //     vector<string>ans;
+    //     for(int i=0;i<words.size();i++){
+    //        string ccopy=words[i];
+    //        normalise(ccopy);
+    //     //    if(ccopy==pattern)
+    //     if(ccopy.compare(pattern)==0)
+    //        ans.push_back(c);
+    //     }
+    //     return ans;
+    // }
 };
