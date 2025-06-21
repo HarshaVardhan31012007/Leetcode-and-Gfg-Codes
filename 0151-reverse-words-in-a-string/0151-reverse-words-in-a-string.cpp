4c9e6bea8fd6ee489896c8f58e2257f621a3383b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-21 10:08:14 +0530

    Time: 3 ms (42.96%), Space: 10 MB (89.36%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,53 +1,19 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.length();
-        // vector<string>v;
-        // int j=0;
-        // int k=0;
-        // for(int i=0;i<n;i++){
-          
-        //   if(s[i]!=' '){
-        //     j++;
-        //   }
-
-        //   if(s[i]==' '){
-        //     if(j!=0){
-        //     v.push_back(s.substr(k,j));
-        //     }
-        //     j=0;
-        //     k=i+1;
-        //   }
-        // }
-        // if(j!=0)
-        // v.push_back(s.substr(k,j));
-        // string p=v[v.size()-1];
-        // for(int i=v.size()-2;i>=0;i--)
-        // p+=" "+v[i];
-
-        // return p;
-
-        //method 2
         reverse(s.begin(),s.end());
-        int start=0,end=0;
-       // int i=0;
-       // while(i<n)
-          for(int i=0;i<n;i++){
-            //skip spaces
-            while(i<n&&s[i]==' ')
-            i++;
-            //out of bound check
-            if(i==n)
-            break;
-            //copy char to correct positions
-            while(i<n&&s[i]!=' ')
-            s[end++]=s[i++];
-            //reverse each word in one one iteration
-            reverse(s.begin()+start,s.begin()+end);
-            s[end++]=' ';
-            start=end;
+        int i=0;int j=0;
+        for(int k=0;k<s.length();k++){
+            while(k<s.length()&&s[k]==' ') k++;
+            if(k==s.length()) break;
+            while(k<s.length()&&s[k]!=' '){
+                s[j++]=s[k++];
+            }
+            reverse(s.begin()+i,s.begin()+j);
+            s[j++]=' ';
+            i=j;
         }
-        s.resize(end-1);
+        s.resize(j-1);
         return s;
     }
 };
