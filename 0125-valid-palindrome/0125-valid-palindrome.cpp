6f94dcf53e7a79065bd242319a1c48c183d7105e
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-24 11:43:43 +0530

    Time: 2 ms (38.77%), Space: 9.9 MB (58%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,13 +1,35 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int i=0;
+        // int i=0;
+        // for(int j=0;j<s.size();j++){
+        //     bool flag=isalpha(s[j])||isdigit(s[j]);
+        //     if(flag){
+        //         s[i]=s[j];
+        //         // if(s[i]>='A'&&s[i]<='Z')
+        //         // s[i]=s[i]-'A'+'a';
+        //         if(isupper(s[i]))
+        //         s[i]=tolower(s[i]);
+        //         i++;
+        //     }
+        // }
+        // int j=i-1;
+        // i=0;
+        // while(i<j){
+        //     if(s[i]!=s[j])
+        //     return false;
+        //     i++;
+        //     j--;
+        // }
+        // return true;
+
+
+
+           int i=0;
         for(int j=0;j<s.size();j++){
             bool flag=isalpha(s[j])||isdigit(s[j]);
             if(flag){
-                s[i]=s[j];
-                // if(s[i]>='A'&&s[i]<='Z')
-                // s[i]=s[i]-'A'+'a';
+                swap(s[i],s[j]);
                 if(isupper(s[i]))
                 s[i]=tolower(s[i]);
                 i++;
