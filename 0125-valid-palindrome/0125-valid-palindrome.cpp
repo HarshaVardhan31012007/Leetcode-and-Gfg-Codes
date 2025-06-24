9e8f4f473c95dff6423c09f5d6d1b11996dbe432
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-24 16:57:47 +0530

    Time: 3 ms (31.53%), Space: 9.9 MB (58%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -25,23 +25,41 @@ public:
 
 
 
-           int i=0;
-        for(int j=0;j<s.size();j++){
-            bool flag=isalpha(s[j])||isdigit(s[j]);
-            if(flag){
-                swap(s[i],s[j]);
-                if(isupper(s[i]))
-                s[i]=tolower(s[i]);
-                i++;
-            }
-        }
-        int j=i-1;
-        i=0;
+        //    int i=0;
+        // for(int j=0;j<s.size();j++){
+        //     bool flag=isalpha(s[j])||isdigit(s[j]);
+        //     if(flag){
+        //         swap(s[i],s[j]);
+        //         if(isupper(s[i]))
+        //         s[i]=tolower(s[i]);
+        //         i++;
+        //     }
+        // }
+        // int j=i-1;
+        // i=0;
+        // while(i<j){
+        //     if(s[i]!=s[j])
+        //     return false;
+        //     i++;
+        //     j--;
+        // }
+        // return true;
+
+        int i=0;int j=s.length()-1;
         while(i<j){
-            if(s[i]!=s[j])
+           if(!isalpha(s[i])&&!isdigit(s[i])){
+             i++;
+             continue;
+           }
+           else if(!isalpha(s[j])&&!isdigit(s[j])){
+             j--;
+             continue;
+           }
+           else{
+            if(tolower(s[i])!=tolower(s[j]))
             return false;
-            i++;
-            j--;
+            i++;j--;
+           }
         }
         return true;
     }
