86fa66a2b0328388345ce7a175877611780c8495
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-25 12:53:18 +0530

    Time: 32 ms (18.96%), Space: 17.4 MB (84.83%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& arr, int target) {
-        int n=arr.size();
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<n;i++){
-            if(i>0&&arr[i]==arr[i-1]) continue;
-           int j=i+1;
-           while(j<n){
-           int p=j+1;
-           int q=n-1;
-           while(p<q){
-           
-            long long int sum=(long long int)arr[i]+(long long int)arr[j]+(long long int)arr[p]+(long long int)arr[q];
-           //not correct//  int sum=arr[i]+arr[j]+arr[p]+arr[q];
-            if(sum<target)
-            p++;
-            else if(sum>target)
-            q--;
-            else{
-                vector<int>temp={arr[i],arr[j],arr[p],arr[q]};
-                ans.push_back(temp);
+        for(int i=0;i<nums.size();i++){
+            if(i>0&&nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<nums.size();j++){
+                if(j>i+1&&nums[j]==nums[j-1]) continue;
+                int p=j+1;int q=nums.size()-1;
+                while(p<q){
+                long long sum=(long long)nums[i]+(long long)nums[j]+(long long)nums[p]+(long long)nums[q];
+                if(sum==target)
+                ans.push_back({nums[i],nums[j],nums[p++],nums[q--]});
+                else if(sum<target)
                 p++;
-                q--; 
-                
-             while(p<q&&arr[p]==arr[p-1]) p++;
-               }
-               
-             }
-               j++;
-        while(j<n&&arr[j]==arr[j-1]) j++;
-        }
+                else
+                q--;
+                while(q+1<nums.size()&&q>=0&&nums[q]==nums[q+1]) q--;
+                }
+            }
         }
         return ans;
     }
