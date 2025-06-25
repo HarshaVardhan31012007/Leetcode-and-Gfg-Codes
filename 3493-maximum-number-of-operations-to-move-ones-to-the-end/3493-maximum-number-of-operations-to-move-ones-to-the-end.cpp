144fa5f554638f92190d27b63b9dd505e3168d70
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-25 11:46:24 +0530

    Time: 8 ms (36.04%), Space: 15.8 MB (85.28%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -4,7 +4,7 @@ public:
        int ans=0;int temp=0;
        for(int i=0;i<s.length()-1;i++){
         if(s[i]=='1') temp++;
-        if(s[i]=='1'&&s[i+1]=='0') 
+        if(s[i]=='1'&&s[i+1]=='0')
         ans+=temp;
        }
        return ans;
