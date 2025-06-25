9e9f1607e64a1ef641f7e1b9a6b403bac707b10f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-25 13:19:55 +0530

    Time: 22 ms (62.63%), Space: 17.4 MB (58.79%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -5,22 +5,27 @@ public:
         vector<vector<int>>ans;
         for(int i=0;i<nums.size();i++){
             if(i>0&&nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<nums.size();j++){
-                if(j>i+1&&nums[j]==nums[j-1]) continue;
+            for(int j=i+1;j<nums.size();){
+               // if(j>i+1&&nums[j]==nums[j-1]) continue;//1
                 int p=j+1;int q=nums.size()-1;
                 while(p<q){
                 long long sum=(long long)nums[i]+(long long)nums[j]+(long long)nums[p]+(long long)nums[q];
                 if(sum==target){
                 ans.push_back({nums[i],nums[j],nums[p++],nums[q--]});
+                //optimization//need//
                 while(p<q&&nums[q]==nums[q+1]) q--;
                 }
                 else if(sum<target)
                 p++;
                 else
                 q--;
-                while(p<q&&q+1<nums.size()&&nums[q]==nums[q+1]) q--;
-                while(p<q&&p>j+1&&nums[p]==nums[p-1]) p++;
+                //optimization//no need//
+                // while(p<q&&q+1<nums.size()&&nums[q]==nums[q+1]) q--;
+                // while(p<q&&p>j+1&&nums[p]==nums[p-1]) p++;
                 }
+                 //instead of 1 this also correct
+                j++;
+                while(j<nums.size()&&nums[j]==nums[j-1]) j++;
             }
         }
         return ans;
