df83bcf5c7a813cb0029516b375d779e9994b5c8
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-25 09:31:29 +0530

    Time: 16 ms (34.15%), Space: 31.5 MB (51.92%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -21,26 +21,45 @@ public:
 
 
 
+    //    vector<int>ans(2);
+    //    int n=grid.size();
+    //    for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //        int row=(abs(grid[i][j])-1)/n;
+    //        int col=(abs(grid[i][j])-1)%n;
+    //        if(grid[row][col]<0)
+    //        ans[0]=n*row+col+1;
+    //        else
+    //        grid[row][col]*=-1;
+    //     }
+    //    }
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         if(grid[i][j]>0){
+    //         ans[1]=n*i+j+1;
+    //         break;
+    //         }
+    //     }
+    // }
+    //     return ans;
+
+
+
        vector<int>ans(2);
+       unordered_map<int,int>mpp;
        int n=grid.size();
+       int sum=((n*n)*((n*n)+1))/2;
+       int asum=0;
        for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-           int row=(abs(grid[i][j])-1)/n;
-           int col=(abs(grid[i][j])-1)%n;
-           if(grid[row][col]<0)
-           ans[0]=n*row+col+1;
-           else
-           grid[row][col]*=-1;
+          asum+=grid[i][j];
+          if(mpp.find(grid[i][j])!=mpp.end()){
+            ans[0]=grid[i][j];
+          }
+          mpp[grid[i][j]]=1;
         }
        }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(grid[i][j]>0){
-            ans[1]=n*i+j+1;
-            break;
-            }
-        }
-    }
-        return ans;
+       ans[1]=sum-asum+ans[0];
+       return ans;
     }
 };
