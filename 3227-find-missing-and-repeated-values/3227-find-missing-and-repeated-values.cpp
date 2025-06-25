eb6a048daa9453084ef428ff4716994c73049821
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-25 09:55:32 +0530

    Time: 0 ms (100%), Space: 25.7 MB (85.31%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -63,23 +63,64 @@ public:
     //    return ans;
 
 
-       vector<int>ans(2);
-       long long int n=grid.size();
-       int sum=((n*n)*((n*n)+1))/2;
-       long long int prod=((n*n)*((n*n)+1)*(2*(n*n)+1))/6;
-       int asum=0;
-       long long int aprod=0;
-       for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-          asum+=grid[i][j];
-          aprod+=grid[i][j]*grid[i][j];
+    //    vector<int>ans(2);
+    //    long long int n=grid.size();
+    //    int sum=((n*n)*((n*n)+1))/2;
+    //    long long int prod=((n*n)*((n*n)+1)*(2*(n*n)+1))/6;
+    //    int asum=0;
+    //    long long int aprod=0;
+    //    for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //       asum+=grid[i][j];
+    //       aprod+=grid[i][j]*grid[i][j];
+    //     }
+    //    }
+    //    int a=sum-asum;
+    //    int b=prod-aprod;
+    //    b=b/a;
+    //    ans[0]=(b-a)/2;
+    //    ans[1]=(a+b)/2;
+    //    return ans;
+
+
+        int xr=0;
+        int n=grid.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                xr=xr^grid[i][j];
+                xr=xr^(n*i+j+1);
+            }
         }
-       }
-       int a=sum-asum;
-       int b=prod-aprod;
-       b=b/a;
-       ans[0]=(b-a)/2;
-       ans[1]=(a+b)/2;
-       return ans;
+        int bitNo=0;
+        while(1){
+            if((xr & (1<<bitNo))){
+                break;
+            }
+            bitNo++;
+        }
+        int one=0;int zero=0;
+         for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] & (1<<bitNo))
+                one=one^grid[i][j];
+                else
+                zero=zero^grid[i][j];
+            }
+        }
+        for(int i=0;i<n*n;i++){
+            if(i+1 & (1<<bitNo))
+            one=one^(i+1);
+            else
+            zero=zero^(i+1);
+        }
+        int count=0;
+         for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+               if(grid[i][j]==one)
+               count++;
+            }
+        }
+        if(count==2) return {one,zero};
+        return {zero,one};
     }
 };
