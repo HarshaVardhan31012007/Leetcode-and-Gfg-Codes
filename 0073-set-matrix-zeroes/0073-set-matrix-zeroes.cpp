6065c3e8388386e3831b4761d08618ada4e13047
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-27 12:31:14 +0530

    Time: 0 ms (100%), Space: 18.6 MB (78%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,21 +2,40 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();int n=matrix[0].size();
-        queue<pair<int,int>>q;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                 q.push({i,j});
-                }
+        // queue<pair<int,int>>q;
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j]==0){
+        //          q.push({i,j});
+        //         }
+        //     }
+        // }
+        // while(!q.empty()){
+        //     auto p=q.front();
+        //     q.pop();
+        //     fill(matrix[p.first].begin(),matrix[p.first].end(),0);
+        //     for(int i=0;i<m;i++){
+        //         matrix[i][p.second]=0;
+        //     }
+        // }
+
+       int col0=1;
+       for(int i=0;i<m;i++){
+        if(matrix[i][0]==0) col0=0;
+        for(int j=1;j<n;j++){
+            if(matrix[i][j]==0){
+                matrix[i][0]=0;
+                matrix[0][j]=0;
             }
         }
-        while(!q.empty()){
-            auto p=q.front();
-            q.pop();
-            fill(matrix[p.first].begin(),matrix[p.first].end(),0);
-            for(int i=0;i<m;i++){
-                matrix[i][p.second]=0;
-            }
+       }
+       for(int i=m-1;i>=0;i--){
+        for(int j=1;j<n;j++){
+           if(matrix[i][0]==0||matrix[0][j]==0)
+           matrix[i][j]=0;
         }
+        if(col0==0) matrix[i][0]=0;
+       }
+
     }
 };
