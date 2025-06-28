711ba5a5be2441838fac147814df4e2b22617dbd
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-28 22:21:35 +0530

    Time: 183 ms (57.54%), Space: 12.7 MB (45.16%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -5,8 +5,10 @@ public:
         vector<bool>arr(n,1);
         arr[0]=arr[1]=0;
         for(int i=2;i<sqrt(n);i++){
+            if(arr[i]){
             for(int j=i*i;j<n;j=j+i){
                 arr[j]=0;
+            }
             }
         }
         return accumulate(arr.begin(),arr.end(),0);
