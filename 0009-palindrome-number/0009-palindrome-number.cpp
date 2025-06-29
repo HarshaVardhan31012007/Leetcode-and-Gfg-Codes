e0c3c4e081d96f1232742110c0bd323658d276ea
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-29 10:29:29 +0530

    Time: 0 ms (100%), Space: 8.7 MB (42.37%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -5,7 +5,7 @@ public:
         while(x>0){
             int digit=x%10;
             if(ans>INT_MAX/10)
-            return -1;
+            return -1;//return 0
             ans=ans*10+digit;
             x=x/10;
         }
