c90f4de7e6bf65f078aa33b5d9fe4b51dfca4a47
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-29 10:32:01 +0530

    Time: 3 ms (46.75%), Space: 10.9 MB (9.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    int reverseI(int x){
-        int ans=0;
-        while(x>0){
-            int digit=x%10;
-            if(ans>INT_MAX/10)
-            return -1;//return 0
-            ans=ans*10+digit;
-            x=x/10;
-        }
-        return ans;
-    }
+    // int reverseI(int x){
+    //     int ans=0;
+    //     while(x>0){
+    //         int digit=x%10;
+    //         if(ans>INT_MAX/10)
+    //         return -1;//return 0
+    //         ans=ans*10+digit;
+    //         x=x/10;
+    //     }
+    //     return ans;
+    // }
     bool isPalindrome(int x) {
         //m1 integer to string and two pointers
+        string s=to_string(x);
+        int n=s.length();
+        for(int i=0;i<n/2;i++){
+            if(s[i]!=s[n-i-1])
+            return false;
+        }
+        return true;
         //m2
-        if(x<0) return false;
-        int reversex=reverseI(x);
-        return x==reversex;
+        // if(x<0) return false;
+        // int reversex=reverseI(x);
+        // return x==reversex;
     }
 };
