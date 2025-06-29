05173f161824b7ce60aa7e6625a75a3d9f12bf6c
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-29 12:34:38 +0530

    Time: 0 ms (100%), Space: 8 MB (10.35%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+      if(n<=0) return false;
+    //   vector<int>ans(n+1,1);
+    //   ans[0]=ans[1]=0;
+    //   for(int i=2;i<=sqrt(n);i++){
+    //     if(ans[i]){
+    //         for(int j=i*i;j<=n;j+=i){
+    //             ans[j]=false;
+    //         }
+    //     }
+    //   }
+    //   for(int i=7;i<n+1;i++){
+    //     if(ans[i]&&n%i==0)
+    //     return false;
+    //   }
+    //   return true;
+
+    int x=n;
+    while(x%2==0)
+    x=x/2;
+    while(x%3==0)
+    x=x/3;
+    while(x%5==0)
+    x=x/5;
+    return x==1;
+    }
+};
