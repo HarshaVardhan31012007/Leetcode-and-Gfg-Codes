c2c6348f85493b8c8e8553e8d14d96095afbd66f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-06-29 17:10:03 +0530

    Time: 7 ms (48.65%), Space: 11.4 MB (52.01%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -25,16 +25,28 @@ public:
     
     int nthUglyNumber(int n) {
       //elements may repeat //use set 2*3 3*2 ssame
-      set<long long int>st;
-      st.insert(1);
-      n--;
-      while(n--){
-        long long front=*st.begin();
-        st.erase(st.begin());
-        st.insert(front*2);
-        st.insert(front*3);
-        st.insert(front*5);
-      }
-      return *st.begin();
+    //   set<long long int>st;
+    //   st.insert(1);
+    //   n--;
+    //   while(n--){
+    //     long long front=*st.begin();
+    //     st.erase(st.begin());
+    //     st.insert(front*2);
+    //     st.insert(front*3);
+    //     st.insert(front*5);
+    //   }
+    //   return *st.begin();
+
+
+    vector<int>dp(n,0);
+    dp[0]=1;
+    int x=0;int y=0;int z=0;
+    for(int i=1;i<n;i++){
+       dp[i]=min({2*dp[x],3*dp[y],5*dp[z]});
+       if(dp[i]==2*dp[x]) x++;
+       if(dp[i]==3*dp[y]) y++;
+       if(dp[i]==5*dp[z]) z++;
+    }
+       return dp[n-1];
     }
 };
