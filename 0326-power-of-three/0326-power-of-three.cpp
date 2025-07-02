daabd5cb211348ca304e144cc5fd10b6e2722675
HARSHA VARDHAN <hv1900019@gmail.com>
2025-07-02 23:06:50 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.59%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<1) return false;
-        while(n%3==0)
-        n=n/3;
-        return n==1;
+        // if(n<1) return false;
+        // while(n%3==0)
+        // n=n/3;
+        // return n==1;
          
         // if(n<=0) return false;
-        // int a=pow(3,19);
+        // int a=pow(3,19);// by binary search nikal sakhte hai max poer of 3 less than or equal to INT_MAX
         // return a % n ==0; 
+
+        if(n==1) return true;
+        if(n==0||n%3!=0) return false;
+        return isPowerOfThree(n/3);
     }
 };
