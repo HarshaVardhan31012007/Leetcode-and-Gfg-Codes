51aeca326fe7426483e3ac8172178d126f3e0871
HARSHA VARDHAN <hv1900019@gmail.com>
2025-07-03 20:54:29 +0530

    Time: 19 ms (61.91%), Space: 36.1 MB (99.65%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -1,7 +1,48 @@
 class Solution {
 public:
+
+   //recursion not correct
+
+    //int findLHS(vector<int>& nums) {
+    //    sort(nums.begin(),nums.end());
+    //    int i=0;int ans=0;
+    //    for(int j=0;j<nums.size();j++){
+    //     while(nums[j]-nums[i]>1)
+    //     i++;
+    //     if(nums[j]-nums[i]==1)
+    //     ans=max(ans,j-i+1);
+    //    }
+    //    return ans;
+    //    }
+
+    // int solve(vector<int>&nums,int i,int maxi,int mini){
+    //     if(i>=nums.size()) return 0; 
+    //     int l=0;
+    //     int tmax=max(maxi,nums[i]);
+    //     int tmin=min(nums[i],mini);
+    //     if(abs(tmax-tmin)<=1)
+    //     l=1+solve(nums,i+1,tmax,tmin);
+    //     int r=solve(nums,i+1,maxi,mini);
+    //     return max(l,r);
+    // }
+    // int findLHS(vector<int>& nums) {
+    //   int ans=solve(nums,0,INT_MIN,INT_MAX);
+    //   sort(nums.begin(),nums.end());
+    //   int mini=INT_MAX;int maxi=INT_MIN;
+    //   for(int i=0;i<nums.size()-1;i++){
+    //     if(nums[i+1]-nums[i]<mini)
+    //     mini=nums[i+1]-nums[i];
+    //     if(nums[i+1]-nums[i]>maxi)
+    //     maxi=nums[i+1]-nums[i];
+    //   }
+    //   if(mini>1) return 0;
+    //   if(maxi<1) return 0;
+    //   return ans;
+    // }
+
+    
     int findLHS(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
+     sort(nums.begin(),nums.end());
        int i=0;int ans=0;
        for(int j=0;j<nums.size();j++){
         while(nums[j]-nums[i]>1)
@@ -10,5 +51,5 @@ public:
         ans=max(ans,j-i+1);
        }
        return ans;
-    }
+     }
 };
