d6fcd23731266fd6992fc3fcca44a7b60ee24d57
HARSHA VARDHAN <hv1900019@gmail.com>
2025-07-03 20:46:01 +0530

    Time: 4 ms (36.05%), Space: 9.1 MB (75.11%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
+    string s="a";
     char kthCharacter(int k) {
-        string s="a";
-        while(s.length()<k){
-          int n=s.length();
+        // string s="a";
+        // while(s.length()<k){
+        //   int n=s.length();
+        // for(int i=0;i<n;i++){
+        //     int b=(s[i]-'a'+1)%26;
+        //     s+=b+'a';
+        // }
+        // }
+        // return s[k-1];
+
+
+        if(s.length()>=k)
+        return s[k-1];
+        int n=s.length();
         for(int i=0;i<n;i++){
             int b=(s[i]-'a'+1)%26;
             s+=b+'a';
         }
-        }
-        return s[k-1];
+        return kthCharacter(k);
     }
 };
