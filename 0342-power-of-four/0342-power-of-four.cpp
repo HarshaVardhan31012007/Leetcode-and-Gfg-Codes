86d51136228614c573aee6e67404b019c8d20247
HARSHA VARDHAN <hv1900019@gmail.com>
2025-07-03 21:18:57 +0530

    Time: 0 ms (100%), Space: 7.9 MB (52.12%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        return (n>0)&&((n&(n-1))==0)&&(n%3==1);
+        // return (n>0)&&((n&(n-1))==0)&&(n%3==1);
+        
+        if(n<=0) return false;
+        if(n==1) return true;
+        double ans=log10(n)/log10(4);
+        return (int)ans==ans;
     }
 };
