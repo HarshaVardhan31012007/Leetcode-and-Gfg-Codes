6e54805e3f48a6ff0d403db37311c4eec50aeb3c
HARSHA VARDHAN <hv1900019@gmail.com>
2025-07-04 19:01:07 +0530

    Time: 30 ms (62.07%), Space: 18.9 MB (61.6%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -17,44 +17,43 @@ public:
     // }
 
 
-    // int solve(vector<int>&coins,int amount,vector<int>&dp){
-    //     if(amount<0) return INT_MAX;
-    //     if(amount==0){
-    //         return 0;
-    //     }
-    //     if(dp[amount]!=-1)
-    //     return dp[amount];
-    //     int mini=INT_MAX;
-    //     for(auto &each:coins){
-    //           if(each<=amount){
-    //         int curr=solve(coins,amount-each,dp);
-    //         if(curr!=INT_MAX)
-    //         mini=min(mini,1+curr);
-    //          }
-    //     }
-    //     return dp[amount]=mini;
-    // }
-    // int coinChange(vector<int>& coins, int amount) {
-    //      vector<int>dp(amount+1,-1);
-    //     int mini=solve(coins,amount,dp);
-    //     return (mini==INT_MAX)?-1:mini;
-    // }
-
-
-
-    int solve(vector<int>&coins,int amount){
-        vector<int>dp(amount+1,INT_MAX);
-        dp[0]=0;
-        for(int a=1;a<amount+1;a++){
-            for(auto &each:coins){
-                if(each<=a&&dp[a-each]!=INT_MAX)
-                dp[a]=min(dp[a],1+dp[a-each]);
-            }
+    int solve(vector<int>&coins,int amount,vector<int>&dp){
+        if(amount==0){
+            return 0;
         }
+        if(dp[amount]!=-1)
         return dp[amount];
+        int mini=INT_MAX;
+        for(auto &each:coins){
+              if(each<=amount){
+            int curr=solve(coins,amount-each,dp);
+            if(curr!=INT_MAX)
+            mini=min(mini,1+curr);
+             }
+        }
+        return dp[amount]=mini;
     }
     int coinChange(vector<int>& coins, int amount) {
-        int mini=solve(coins,amount);
+         vector<int>dp(amount+1,-1);
+        int mini=solve(coins,amount,dp);
         return (mini==INT_MAX)?-1:mini;
     }
+
+
+
+    // int solve(vector<int>&coins,int amount){
+    //     vector<int>dp(amount+1,INT_MAX);
+    //     dp[0]=0;
+    //     for(int a=1;a<amount+1;a++){
+    //         for(auto &each:coins){
+    //             if(each<=a&&dp[a-each]!=INT_MAX)
+    //             dp[a]=min(dp[a],1+dp[a-each]);
+    //         }
+    //     }
+    //     return dp[amount];
+    // }
+    // int coinChange(vector<int>& coins, int amount) {
+    //     int mini=solve(coins,amount);
+    //     return (mini==INT_MAX)?-1:mini;
+    // }
 };
