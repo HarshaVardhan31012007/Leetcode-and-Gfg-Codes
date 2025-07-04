efa9e79fd0247e26f942e184d98c6381974df622
HARSHA VARDHAN <hv1900019@gmail.com>
2025-07-04 10:21:20 +0530

    Time: 0 ms (100%), Space: 28.6 MB (14.35%) - LeetHub

---
+++ 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    //Brute
+
+    // void func(string &s){
+    //   int n=s.length();
+    //   for(int i=0;i<n;i++){
+    //     if(s[i]=='z')
+    //     s+='a';
+    //     else
+    //     s+=s[i]+1;
+    //   }  
+    // }
+    // char kthCharacter(long long k, vector<int>& operations) {
+    //     string s="a";
+    //     for(auto &operation:operations){
+    //         if(s.length()>=k) break;
+    //         if(operation==0)
+    //         s=s+s;
+    //         else
+    //         func(s);
+    //     }
+    //     return s[k-1];
+    // }
+
+
+    char kthCharacter(long long k, vector<int>& operations) {
+       int count=0;
+       while(k>1){
+        long long int val=ceil(log2(k));
+        if(operations[val-1]==1) count++;
+        k=k-(long long)pow(2,val-1);
+       }
+       return 'a'+(count%26);
+    }
+};
