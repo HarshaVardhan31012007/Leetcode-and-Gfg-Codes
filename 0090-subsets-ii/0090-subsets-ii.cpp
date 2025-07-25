e550e1a0464511b2b67d373e2ad266d38b1b09eb
HARSHA VARDHAN <hv1900019@gmail.com>
2025-07-25 23:07:59 +0530

    Time: 2 ms (43.98%), Space: 10.3 MB (85.13%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&temp,int i,int f){     if(i==nums.size()){
+    // void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&temp,int i,int f){     if(i==nums.size()){
+    //         ans.push_back(temp);
+    //         return;
+    //     }
+    //     if(f==-1||(!(nums[i]==nums[i-1]&&f==0))){
+    //     temp.push_back(nums[i]);
+    //     solve(nums,ans,temp,i+1,1);
+    //     temp.pop_back();
+    //     }
+
+    //     solve(nums,ans,temp,i+1,0);
+    // }
+    // vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    //     sort(nums.begin(),nums.end());
+    //     vector<vector<int>>ans;
+    //     vector<int>temp;
+    //     solve(nums,ans,temp,0,-1);
+    //     return ans;
+    // }
+
+
+    void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&temp,int i){     if(i==nums.size()){
             ans.push_back(temp);
             return;
         }
-        if(f==-1||(!(nums[i]==nums[i-1]&&f==0))){
         temp.push_back(nums[i]);
-        solve(nums,ans,temp,i+1,1);
+        solve(nums,ans,temp,i+1);
         temp.pop_back();
-        }
-        solve(nums,ans,temp,i+1,0);
+        int idx=i+1;
+        while(idx<nums.size()&&nums[idx]==nums[idx-1]) 
+        idx=idx+1;
+        solve(nums,ans,temp,idx);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         vector<int>temp;
-        solve(nums,ans,temp,0,-1);
+        solve(nums,ans,temp,0);
         return ans;
     }
 };
