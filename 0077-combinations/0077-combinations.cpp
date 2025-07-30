4076068e9114bc3caaa8ff68f5f8bc5299ffb9ea
HARSHA VARDHAN <hv1900019@gmail.com>
2025-07-30 08:32:14 +0530

    Time: 35 ms (87.89%), Space: 62.4 MB (79.78%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -5,16 +5,16 @@ public:
             ans.push_back(temp);
             return;
         }
-        for(int j=i+1;j<=n;j++){
+        for(int j=i;j<=n;j++){
             temp.push_back(j);
-            solve(n,k-1,j,ans,temp);
+            solve(n,k-1,j+1,ans,temp);
             temp.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>>ans;
         vector<int>temp;
-        solve(n,k,0,ans,temp);
+        solve(n,k,1,ans,temp);
         return ans;
     }
 };
