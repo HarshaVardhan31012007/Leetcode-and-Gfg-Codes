a9628a1e84c589278a58ebf9659b9436a2834ffd
HARSHA VARDHAN <hv1900019@gmail.com>
2025-07-30 22:03:53 +0530

    Time: 289 ms (57.8%), Space: 10.7 MB (60.99%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>>& board, string word,int row,int col,int k){
-        //base case
-        if(k==word.size()){
-            return true;
+    bool isSafe(vector<vector<char>>&board,string &word,vector<vector<bool>>&visited,int i,int row,int col){
+        if(row<0||col<0||row>=board.size()||col>=board[0].size())
+        return false;
+        if(visited[row][col]==true)
+        return false;
+        if(word[i]!=board[row][col])
+        return false;
+        return true;
+    }
+    bool solve(vector<vector<char>>&board,string &word,vector<vector<bool>>&visited,int i,int row,int col){
+        if(i==word.size()) return true;
+        int dx[]={0,0,-1,1};
+        int dy[]={-1,1,0,0};
+        for(int k=0;k<4;k++){
+            int newRow=row+dx[k];
+            int newCol=col+dy[k];
+            if(isSafe(board,word,visited,i,newRow,newCol)){
+                 visited[newRow][newCol]=1;
+                 bool ans=solve(board,word,visited,i+1,newRow,newCol);
+                 if(ans) return true;
+                 visited[newRow][newCol]=0;
+            }
         }
-        if(row<0||col<0||row>=board.size()||col>=board[0].size()||board[row][col]!=word[k])
-        return false;//board[row][col]!='%' no need 
-       //up to this reached means word[i] match hua
-       //visited
-       char temp=board[row][col];
-       board[row][col]='%';
-       //recursive call
-       int dx[]={-1,1,0,0};
-       int dy[]={0,0,-1,1};
-    //    bool found=0;
-    //    for(int i=0;i<4;i++){
-    //     found=found||solve(board,word,row+dx[i],col+dy[i],k+1);
-    //    }
-    bool found=solve(board,word,row+1,col,k+1)||solve(board,word,row,col+1,k+1)||solve(board,word,row-1,col,k+1)||solve(board,word,row,col-1,k+1);
-
-       //unvisit
-       board[row][col]=temp;
-
-     return found;
+        return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int m=board.size();
-        int n=board[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(solve(board,word,i,j,0))
-                return true;
+        vector<vector<bool>>visited(board.size(),vector<bool>(board[0].size(),0));    
+         for(int row=0;row<board.size();row++){
+            for(int col=0;col<board[0].size();col++){
+                   if(board[row][col]==word[0]){
+                      visited[row][col]=1;
+                      bool ans=solve(board,word,visited,1,row,col);
+                      if(ans) return true;
+                      visited[row][col]=0;
+                   }
             }
         }
         return false;
