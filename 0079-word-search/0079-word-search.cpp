ea24eb6de74830d821619ba5585313fba1f03c9f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-07-30 22:14:22 +0530

    Time: 223 ms (73.73%), Space: 10.3 MB (94.66%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,39 +1,91 @@
+// class Solution {
+// public:
+//     bool isSafe(vector<vector<char>>&board,string &word,vector<vector<bool>>&visited,int i,int row,int col){
+//         if(row<0||col<0||row>=board.size()||col>=board[0].size())
+//         return false;
+//         if(visited[row][col]==true)
+//         return false;
+//         if(word[i]!=board[row][col])
+//         return false;
+//         return true;
+//     }
+//     bool solve(vector<vector<char>>&board,string &word,vector<vector<bool>>&visited,int i,int row,int col){
+//         if(i==word.size()) return true;
+//         int dx[]={0,0,-1,1};
+//         int dy[]={-1,1,0,0};
+//         for(int k=0;k<4;k++){
+//             int newRow=row+dx[k];
+//             int newCol=col+dy[k];
+//             if(isSafe(board,word,visited,i,newRow,newCol)){
+//                  visited[newRow][newCol]=1;
+//                  bool ans=solve(board,word,visited,i+1,newRow,newCol);
+//                  if(ans) return true;
+//                  visited[newRow][newCol]=0;
+//             }
+//         }
+//         return false;
+//     }
+//     bool exist(vector<vector<char>>& board, string word) {
+//         vector<vector<bool>>visited(board.size(),vector<bool>(board[0].size(),0));    
+//          for(int row=0;row<board.size();row++){
+//             for(int col=0;col<board[0].size();col++){
+//                    if(board[row][col]==word[0]){
+//                       visited[row][col]=1;
+//                       bool ans=solve(board,word,visited,1,row,col);
+//                       if(ans) return true;
+//                       visited[row][col]=0;
+//                    }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
+
+
+
+//No need of visited 2d array 
+//In the same 2d array do replace with one value and store actual value in temp variable
+// so that when youa re backtracking you can repalce it
+
 class Solution {
 public:
-    bool isSafe(vector<vector<char>>&board,string &word,vector<vector<bool>>&visited,int i,int row,int col){
+    bool isSafe(vector<vector<char>>&board,string &word,int i,int row,int col){
         if(row<0||col<0||row>=board.size()||col>=board[0].size())
         return false;
-        if(visited[row][col]==true)
+        if(board[row][col]=='%')
         return false;
         if(word[i]!=board[row][col])
         return false;
         return true;
     }
-    bool solve(vector<vector<char>>&board,string &word,vector<vector<bool>>&visited,int i,int row,int col){
+    bool solve(vector<vector<char>>&board,string &word,int i,int row,int col){
         if(i==word.size()) return true;
         int dx[]={0,0,-1,1};
         int dy[]={-1,1,0,0};
         for(int k=0;k<4;k++){
             int newRow=row+dx[k];
             int newCol=col+dy[k];
-            if(isSafe(board,word,visited,i,newRow,newCol)){
-                 visited[newRow][newCol]=1;
-                 bool ans=solve(board,word,visited,i+1,newRow,newCol);
+            if(isSafe(board,word,i,newRow,newCol)){
+                 char temp=board[row][col];
+                 board[row][col]='%';
+                 bool ans=solve(board,word,i+1,newRow,newCol);
                  if(ans) return true;
-                 visited[newRow][newCol]=0;
+                 board[row][col]=temp;
             }
         }
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<bool>>visited(board.size(),vector<bool>(board[0].size(),0));    
          for(int row=0;row<board.size();row++){
             for(int col=0;col<board[0].size();col++){
                    if(board[row][col]==word[0]){
-                      visited[row][col]=1;
-                      bool ans=solve(board,word,visited,1,row,col);
+                      char temp=board[row][col];
+                      board[row][col]='%';
+                      bool ans=solve(board,word,1,row,col);
                       if(ans) return true;
-                      visited[row][col]=0;
+                      board[row][col]=temp;
                    }
             }
         }
