5ed49297638d9c2b99ff68a33c5e88663b5e38f8
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-03 07:08:29 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.07%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,int i,vector<vector<int>>&ans){
-        if(i==nums.size()){
-            ans.push_back(nums);
-            return;
-        }
-        for(int j=i;j<nums.size();j++){
-            swap(nums[i],nums[j]);
-            solve(nums,i+1,ans);
-            swap(nums[i],nums[j]);
-        }
-    }
+    // void solve(vector<int>&nums,int i,vector<vector<int>>&ans){
+    //     if(i==nums.size()){
+    //         ans.push_back(nums);
+    //         return;
+    //     }
+    //     for(int j=i;j<nums.size();j++){
+    //         swap(nums[i],nums[j]);
+    //         solve(nums,i+1,ans);
+    //         swap(nums[i],nums[j]);
+    //     }
+    //}
     vector<vector<int>> permute(vector<int>& nums) {
+        // vector<vector<int>>ans;
+        // solve(nums,0,ans);
+        // return ans;
+
         vector<vector<int>>ans;
-        solve(nums,0,ans);
+        sort(nums.begin(),nums.end());
+        ans.push_back(nums);
+        while(next_permutation(nums.begin(),nums.end())){
+           ans.push_back(nums);
+        }
         return ans;
     }
 };
