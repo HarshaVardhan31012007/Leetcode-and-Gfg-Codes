e16bf7f12df8e60f232c33734657c3ed021f229d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-05 16:31:38 +0530

    Time: 8 ms (29.37%), Space: 15.6 MB (37.96%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,50 @@
+// class Solution {
+// public:
+//     void solve(vector<int>& candidates, int target,set<vector<int>>&ans,vector<int>&temp){
+//         if(target==0){
+//             // you shouldnot sort temp array as it is passed buy reference 
+//             //whatever element included may change duting backtracking // dont sort
+//             vector<int>a(temp.begin(),temp.end());
+//             sort(a.begin(),a.end());
+//             ans.insert(a);
+//             return;
+//         }
+//         for(auto &candidate:candidates){
+//             if(candidate<=target){
+//             temp.push_back(candidate);
+//             solve(candidates,target-candidate,ans,temp);
+//             temp.pop_back();
+//             }
+//         }
+//     }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         set<vector<int>>ans;
+//         vector<int>temp;
+//         solve(candidates,target,ans,temp);
+//         return vector<vector<int>>(ans.begin(),ans.end());
+//     }
+// };
+
+
+
 class Solution {
 public:
-    void solve(vector<int>& candidates, int target,set<vector<int>>&ans,vector<int>&temp){
+    void solve(vector<int>& candidates, int target,set<vector<int>>&ans,vector<int>&temp,int i){
         if(target==0){
-            // you shouldnot sort temp array as it is passed buy reference 
-            //whatever element included may change duting backtracking // dont sort
-            vector<int>a(temp.begin(),temp.end());
-            sort(a.begin(),a.end());
-            ans.insert(a);
-            return;
-        }
-        for(auto &candidate:candidates){
-            if(candidate<=target){
-            temp.push_back(candidate);
-            solve(candidates,target-candidate,ans,temp);
-            temp.pop_back();
-            }
-        }
+              ans.insert(temp);
+              return;
+        } 
+        if(i==candidates.size()||target<0) return;
+        temp.push_back(candidates[i]);
+        solve(candidates,target-candidates[i],ans,temp,i+1);
+        solve(candidates,target-candidates[i],ans,temp,i);
+        temp.pop_back();
+        solve(candidates,target,ans,temp,i+1);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         set<vector<int>>ans;
         vector<int>temp;
-        solve(candidates,target,ans,temp);
+        solve(candidates,target,ans,temp,0);
         return vector<vector<int>>(ans.begin(),ans.end());
     }
 };
