331418f6e2c702a5dbaedc7dfc30db37b4341aa9
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-08 00:43:55 +0530

    Time: 30 ms (90.83%), Space: 53.3 MB (98.93%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    bool isPalindrome(string str){
-        string k=str;
-        reverse(str.begin(),str.end());
-        return k==str;
+    bool isSafe(string &str,int s,int e){
+        if(s>=e) return true;
+        if(str[s]!=str[e]) return false;
+        return isSafe(str,s+1,e-1);
     }
-    void solve(string s,vector<vector<string>>&ans,vector<string>&partitions){
-      if(s.length()==0){
-        ans.push_back(partitions);
-        return;
-      }
-      for(int i=0;i<s.length();i++){
-        string part=s.substr(0,i+1);
-        if(isPalindrome(part)){
-            partitions.push_back(part);
-            solve(s.substr(i+1),ans,partitions);
-            partitions.pop_back();
+    void solve(string &str,int s,int e,vector<vector<string>>&ans,vector<string>&temp){
+        if(s>e){
+            ans.push_back(temp);
+            return;
         }
-      }
+        for(int i=s;i<=e;i++){
+            // if(isSafe(str,s,i)&&isSafe(str,i+1,e)){
+            //      temp.push_back(str.substr(s,i+1));
+            //      temp.push_back(str.substr(i+1,e+1));
+            //      ans.push_back(temp);
+
+            // }
+            if(isSafe(str,s,i)){
+                temp.push_back(str.substr(s,i-s+1));
+                solve(str,i+1,e,ans,temp);
+                temp.pop_back();
+            }
+        }
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>>ans;
-        vector<string>partitions;
-        solve(s,ans,partitions);
+        vector<string>temp;
+        solve(s,0,s.length()-1,ans,temp);
         return ans;
     }
 };
