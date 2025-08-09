77f41ce65e127de34cca187aad5a869b9eaa4d33
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-09 21:50:51 +0530

    Time: 0 ms (100%), Space: 10.4 MB (86.94%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -11,17 +11,38 @@ public:
     //         swap(nums[i],nums[j]);
     //     }
     //}
-    vector<vector<int>> permute(vector<int>& nums) {
-        // vector<vector<int>>ans;
-        // solve(nums,0,ans);
-        // return ans;
+    // vector<vector<int>> permute(vector<int>& nums) {
+    //     // vector<vector<int>>ans;
+    //     // solve(nums,0,ans);
+    //     // return ans;
 
-        vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-        ans.push_back(nums);
-        while(next_permutation(nums.begin(),nums.end())){
-           ans.push_back(nums);
+    //     vector<vector<int>>ans;
+    //     sort(nums.begin(),nums.end());
+    //     ans.push_back(nums);
+    //     while(next_permutation(nums.begin(),nums.end())){
+    //        ans.push_back(nums);
+    //     }
+    //     return ans;
+    // }
+
+
+     void solve(vector<int>&nums,vector<vector<int>>&ans,vector<int>&temp,int i){
+        if(i==temp.size()){
+            ans.push_back(temp);
+            return;
         }
+        for(int j=0;j<temp.size();j++){
+             if(temp[j]==INT_MAX){
+                temp[j]=nums[i];
+                solve(nums,ans,temp,i+1);
+                temp[j]=INT_MAX;
+             }
+        }
+     }
+     vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>temp(nums.size(),INT_MAX);
+        solve(nums,ans,temp,0);
         return ans;
     }
 };
