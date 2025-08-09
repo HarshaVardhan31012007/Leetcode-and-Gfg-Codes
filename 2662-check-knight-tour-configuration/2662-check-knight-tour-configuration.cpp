e69fc8535f3107ce84c10f318c658e9fcd2b8fdd
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-09 11:25:27 +0530

    Time: 0 ms (100%), Space: 31.6 MB (88.23%) - LeetHub

--- 2662-check-knight-tour-configuration/2662-check-knight-tour-configuration.cpp
+++ 2662-check-knight-tour-configuration/2662-check-knight-tour-configuration.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool solve(vector<vector<int>>& grid,int row,int col,int count){
-        if(count==(grid.size()*grid.size())) return true;
+     //   if(count==(grid.size()*grid.size())) return true;
         if(row<0||col<0||row>=grid.size()||col>=grid[0].size()||grid[row][col]==-1||grid[row][col]!=count) return false;
+        if(count==(grid.size()*grid.size()-1)) return true;
         int temp=grid[row][col];
         grid[row][col]=-1;
         bool ans=solve(grid,row+1,col+2,count+1)||solve(grid,row-1,col+2,count+1)||solve(grid,row+2,col+1,count+1)||solve(grid,row-2,col+1,count+1)||solve(grid,row+2,col-1,count+1)||solve(grid,row-2,col-1,count+1)||solve(grid,row+1,col-2,count+1)||solve(grid,row-1,col-2,count+1);
