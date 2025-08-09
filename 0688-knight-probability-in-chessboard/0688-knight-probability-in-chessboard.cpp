18a3b973e79fbd73adc88ad304ba3845d8b02ba6
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-09 12:55:56 +0530

    Time: 3 ms (95.35%), Space: 10.8 MB (75.81%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    unordered_map<int,unordered_map<int,double>>mpp;
-    double solve(int n,int k,int row,int col){
+    // unordered_map<int,unordered_map<int,double>>mpp;
+    // double solve(int n,int k,int row,int col){
+    //     if(row<0||col<0||row>=n||col>=n) return 0;
+    //     if(k==0) {
+    //         return 1;
+    //     }
+    //     if(mpp[k][n*row+col]) return mpp[k][n*row+col];
+    //     //if(mpp[k].count(n*row+col)) return mpp[k][n*row+col];// this is recommended as sometimes value is actually zero  ex:- in 3*3 marrix horse at centre
+    //     return mpp[k][n*row+col]=(solve(n,k-1,row+1,col-2)+solve(n,k-1,row-1,col-2)+solve(n,k-1,row-2,col-1)+solve(n,k-1,row+2,col-1)+solve(n,k-1,row-2,col+1)+solve(n,k-1,row+2,col+1)+solve(n,k-1,row+1,col+2)+solve(n,k-1,row-1,col+2))/8.0;
+    // }
+    // double knightProbability(int n, int k, int row, int col) {
+    //     double ans=solve(n,k,row,col);
+    //     return ans;
+    // }
+
+
+
+    double solve(int n,int k,int row,int col,vector<vector<double>>&dp){
         if(row<0||col<0||row>=n||col>=n) return 0;
         if(k==0) {
             return 1;
         }
-        if(mpp[k][n*row+col]) return mpp[k][n*row+col];
-        //if(mpp[k].count(n*row+col)) return mpp[k][n*row+col];
-        return mpp[k][n*row+col]=(solve(n,k-1,row+1,col-2)+solve(n,k-1,row-1,col-2)+solve(n,k-1,row-2,col-1)+solve(n,k-1,row+2,col-1)+solve(n,k-1,row-2,col+1)+solve(n,k-1,row+2,col+1)+solve(n,k-1,row+1,col+2)+solve(n,k-1,row-1,col+2))/8.0;
+        if(dp[k][n*row+col]!=-1) return dp[k][n*row+col];
+        return dp[k][n*row+col]=(solve(n,k-1,row+1,col-2,dp)+solve(n,k-1,row-1,col-2,dp)+solve(n,k-1,row-2,col-1,dp)+solve(n,k-1,row+2,col-1,dp)+solve(n,k-1,row-2,col+1,dp)+solve(n,k-1,row+2,col+1,dp)+solve(n,k-1,row+1,col+2,dp)+solve(n,k-1,row-1,col+2,dp))/8.0;
     }
     double knightProbability(int n, int k, int row, int col) {
-        double ans=solve(n,k,row,col);
+        vector<vector<double>>dp(k+1,vector<double>(n*n,-1));
+        double ans=solve(n,k,row,col,dp);
         return ans;
     }
 };
