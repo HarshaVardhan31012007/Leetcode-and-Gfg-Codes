87ed77e0d97dba4e0749d3ef0aede01730818a14
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-09 19:12:19 +0530

    Time: 23 ms (81.97%), Space: 7.9 MB (81.97%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int solve(vector<int>&nums,int n,int i){
         if(i==n) {
-            for(int i=0;i<nums.size();i++)
-            cout<<nums[i];
-            cout<<endl;
             return 1;
         }
         int ans=0;
