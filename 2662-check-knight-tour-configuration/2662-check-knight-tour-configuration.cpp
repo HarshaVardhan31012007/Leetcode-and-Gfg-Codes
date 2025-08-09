95bd4c1a604060813ec10b94cf42da5b31e5da12
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-09 11:26:46 +0530

    Time: 0 ms (100%), Space: 31.7 MB (88.23%) - LeetHub

--- 2662-check-knight-tour-configuration/2662-check-knight-tour-configuration.cpp
+++ 2662-check-knight-tour-configuration/2662-check-knight-tour-configuration.cpp
@@ -4,10 +4,11 @@ public:
      //   if(count==(grid.size()*grid.size())) return true;
         if(row<0||col<0||row>=grid.size()||col>=grid[0].size()||grid[row][col]==-1||grid[row][col]!=count) return false;
         if(count==(grid.size()*grid.size()-1)) return true;
-        int temp=grid[row][col];
-        grid[row][col]=-1;
+        //visited no need ad grid[row][col]!=count take care of it
+        // int temp=grid[row][col];
+        // grid[row][col]=-1;
         bool ans=solve(grid,row+1,col+2,count+1)||solve(grid,row-1,col+2,count+1)||solve(grid,row+2,col+1,count+1)||solve(grid,row-2,col+1,count+1)||solve(grid,row+2,col-1,count+1)||solve(grid,row-2,col-1,count+1)||solve(grid,row+1,col-2,count+1)||solve(grid,row-1,col-2,count+1);
-        grid[row][col]=temp;
+        //grid[row][col]=temp;
         return ans;
     }
     bool checkValidGrid(vector<vector<int>>& grid) {
