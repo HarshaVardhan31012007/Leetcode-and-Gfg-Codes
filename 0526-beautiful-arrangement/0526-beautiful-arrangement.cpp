c2636ff1e83c048295080cbe38ed6743af75209c
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-09 21:52:31 +0530

    Time: 54 ms (22.13%), Space: 8.1 MB (29.27%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -23,25 +23,24 @@
 //     }
 // };
 
-
 class Solution {
 public:
-    int solve(vector<int>&nums,int n,int k){
-        if(k==n+1) {
+    int solve(vector<int>& nums, int n, int k) {
+        if (k == n + 1) {
             return 1;
         }
-        int ans=0;
-        for(int j=1;j<=n;j++){
-                if(nums[j]==0&&(j%k==0||k%j==0)){
-                nums[j]=k;
-                ans+=solve(nums,n,k+1);
-                nums[j]=0;
-                }
+        int ans = 0;
+        for (int j = 1; j <= n; j++) {
+            if (nums[j] == 0 && (j % k == 0 || k % j == 0)) {
+                nums[j] = k;
+                ans += solve(nums, n, k + 1);
+                nums[j] = 0;
+            }
         }
         return ans;
     }
     int countArrangement(int n) {
-       vector<int>nums(n+1);
-       return solve(nums,n,1);
+        vector<int> nums(n + 1);
+        return solve(nums, n, 1);
     }
 };
