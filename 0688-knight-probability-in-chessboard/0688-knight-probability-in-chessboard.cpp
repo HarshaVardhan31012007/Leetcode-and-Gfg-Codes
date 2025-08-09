1ae6792415a979532eec19bd0e679c26cb9f6af4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-09 12:46:35 +0530

    Time: 49 ms (20.8%), Space: 26.8 MB (11.23%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -7,6 +7,7 @@ public:
             return 1;
         }
         if(mpp[k][n*row+col]) return mpp[k][n*row+col];
+        //if(mpp[k].count(n*row+col)) return mpp[k][n*row+col];
         return mpp[k][n*row+col]=(solve(n,k-1,row+1,col-2)+solve(n,k-1,row-1,col-2)+solve(n,k-1,row-2,col-1)+solve(n,k-1,row+2,col-1)+solve(n,k-1,row-2,col+1)+solve(n,k-1,row+2,col+1)+solve(n,k-1,row+1,col+2)+solve(n,k-1,row-1,col+2))/8.0;
     }
     double knightProbability(int n, int k, int row, int col) {
