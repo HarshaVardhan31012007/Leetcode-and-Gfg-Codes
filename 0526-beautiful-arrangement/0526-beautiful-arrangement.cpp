f0bcfba8bce56d0a914ba89e090baaea995c0fed
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-09 21:37:58 +0530

    Time: 51 ms (28.37%), Space: 8 MB (37.96%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,24 +1,47 @@
+// class Solution {
+// public:
+//     int solve(vector<int>&nums,int n,int i){
+//         if(i==n) {
+//             return 1;
+//         }
+//         int ans=0;
+//         for(int j=i;j<n;j++){
+//             if(nums[j]%(i+1)==0||(i+1)%nums[j]==0){
+//                 swap(nums[i],nums[j]);
+//                 ans+=solve(nums,n,i+1);
+//                 swap(nums[i],nums[j]);
+//             }
+//         }
+//         return ans;
+//     }
+//     int countArrangement(int n) {
+//        vector<int>nums(n);
+//        for(int i=0;i<n;i++){
+//         nums[i]=i+1;
+//        }
+//        return solve(nums,n,0);
+//     }
+// };
+
+
 class Solution {
 public:
-    int solve(vector<int>&nums,int n,int i){
-        if(i==n) {
+    int solve(vector<int>&nums,int n,int k){
+        if(k==n+1) {
             return 1;
         }
         int ans=0;
-        for(int j=i;j<n;j++){
-            if(nums[j]%(i+1)==0||(i+1)%nums[j]==0){
-                swap(nums[i],nums[j]);
-                ans+=solve(nums,n,i+1);
-                swap(nums[i],nums[j]);
-            }
+        for(int j=1;j<=n;j++){
+                if(nums[j]==0&&(j%k==0||k%j==0)){
+                nums[j]=k;
+                ans+=solve(nums,n,k+1);
+                nums[j]=0;
+                }
         }
         return ans;
     }
     int countArrangement(int n) {
-       vector<int>nums(n);
-       for(int i=0;i<n;i++){
-        nums[i]=i+1;
-       }
-       return solve(nums,n,0);
+       vector<int>nums(n+1);
+       return solve(nums,n,1);
     }
 };
