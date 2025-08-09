0e15583f38ebe57bd41d9589e5b2ea1e31c0b0d8
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-09 12:44:38 +0530

    Time: 49 ms (20.8%), Space: 26.9 MB (11.23%) - LeetHub

---
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    unordered_map<int,unordered_map<int,double>>mpp;
+    double solve(int n,int k,int row,int col){
+        if(row<0||col<0||row>=n||col>=n) return 0;
+        if(k==0) {
+            return 1;
+        }
+        if(mpp[k].count(n*row+col)) return mpp[k][n*row+col];
+        return mpp[k][n*row+col]=(solve(n,k-1,row+1,col-2)+solve(n,k-1,row-1,col-2)+solve(n,k-1,row-2,col-1)+solve(n,k-1,row+2,col-1)+solve(n,k-1,row-2,col+1)+solve(n,k-1,row+2,col+1)+solve(n,k-1,row+1,col+2)+solve(n,k-1,row-1,col+2))/8.0;
+    }
+    double knightProbability(int n, int k, int row, int col) {
+        double ans=solve(n,k,row,col);
+        return ans;
+    }
+};
