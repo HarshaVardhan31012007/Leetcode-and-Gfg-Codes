44aa262adf418d2de3612037405483f00c5a9f71
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-12 21:22:45 +0530

    Time: 127 ms (90.46%), Space: 81.1 MB (39.3%) - LeetHub

---
+++ 1758-distribute-repeating-integers/1758-distribute-repeating-integers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool solve(vector<int>&nums,vector<int>&quantity,int i){
+    if(i==quantity.size()) return true;
+        for(auto &num:nums){
+            if(num>=quantity[i]){
+                num-=quantity[i];
+                if(solve(nums,quantity,i+1))
+                return true;
+                else
+                num+=quantity[i];
+            }
+        }
+        return false;
+    }
+    bool canDistribute(vector<int>& nums, vector<int>& quantity) {
+        sort(quantity.rbegin(),quantity.rend());
+        unordered_map<int,int>mpp;
+        for(auto &num:nums){
+            mpp[num]++;
+        }
+        vector<int>freq;
+        for(auto &mp:mpp){
+            freq.push_back(mp.second);
+        }
+        return solve(freq,quantity,0);
+    }
+};
