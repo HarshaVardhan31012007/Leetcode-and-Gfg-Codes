11e3df84f883dd8af6fad9c48381733b7f14c348
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-14 17:32:48 +0530

    Time: 3 ms (30.63%), Space: 95 MB (79.23%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,27 +1,76 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, vector<int>& nums2,vector<int>&temp){
+    // void merge(vector<int>& nums1, vector<int>& nums2,vector<int>&temp){
+    //     int i=0;int j=0;int k=0;
+    //     while(i<nums1.size()&&j<nums2.size()){
+    //         if(nums1[i]<nums2[j])
+    //         temp[k++]=nums1[i++];
+    //         else
+    //         temp[k++]=nums2[j++];
+    //     }
+    //     while(i<nums1.size()){
+    //          temp[k++]=nums1[i++];
+    //     }
+    //     while(j<nums2.size()){
+    //          temp[k++]=nums2[j++];
+    //     }
+    // }
+    // double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    //     vector<int>temp(nums1.size()+nums2.size(),0);
+    //     merge(nums1,nums2,temp);
+    //     int n=temp.size();
+    //     if(n&1)
+    //     return (double)temp[n/2];
+    //     else
+    //     return (temp[n/2-1]+temp[n/2])/2.0;
+    // }
+
+
+
+    void merge(vector<int>& nums1, vector<int>& nums2,int &a,int &b){
         int i=0;int j=0;int k=0;
+        int idx=-1;int idx1=-1;
+        int n=nums1.size()+nums2.size();
+        if(n&1)
+        idx=n/2;
+        else{
+        idx=n/2-1;
+        idx1=n/2;
+        }
         while(i<nums1.size()&&j<nums2.size()){
-            if(nums1[i]<nums2[j])
-            temp[k++]=nums1[i++];
-            else
-            temp[k++]=nums2[j++];
+            if(nums1[i]<nums2[j]){
+                if(k==idx) a=nums1[i];
+                else if(k==idx1) b=nums1[i];
+                k++;
+                i++;
+            }
+            else{
+            if(k==idx) a=nums2[j];
+            else if(k==idx1) b=nums2[j];
+            k++;
+            j++;
+            }
         }
         while(i<nums1.size()){
-             temp[k++]=nums1[i++];
+            if(k==idx) a=nums1[i];
+            else if(k==idx1) b=nums1[i];
+            k++;
+            i++;
         }
         while(j<nums2.size()){
-             temp[k++]=nums2[j++];
+            if(k==idx) a=nums2[j];
+            else if(k==idx1) b=nums2[j];
+            j++;
+            k++;
         }
     }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>temp(nums1.size()+nums2.size(),0);
-        merge(nums1,nums2,temp);
-        int n=temp.size();
+        int a=-1;int b=-1;
+        merge(nums1,nums2,a,b);
+        int n=nums1.size()+nums2.size();
         if(n&1)
-        return (double)temp[n/2];
+        return (double)(a);
         else
-        return (temp[n/2-1]+temp[n/2])/2.0;
+        return (a+b)/2.0;
     }
 };
