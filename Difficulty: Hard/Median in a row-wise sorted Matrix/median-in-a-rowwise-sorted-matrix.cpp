c8d3c3d8faabe93c32aa4df4d7fffa6469d412a7
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-16 11:45:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    int find(vector<vector<int>> &mat,int mid){
+        int count=0;
+        for(int i=0;i<mat.size();i++){
+            count+=upper_bound(mat[i].begin(),mat[i].end(),mid)-mat[i].begin();
+        }
+        return count;
+    }
+    int median(vector<vector<int>> &mat) {
+       int n=mat.size();int m=mat[0].size();
+       int l=mat[0][0];int h=mat[0][m-1];
+       for(int i=1;i<n;i++){
+           l=min(l,mat[i][0]);
+           h=max(h,mat[i][m-1]);
+       }
+       int ans=-1;
+       int left=(n*m)/2;
+       while(l<=h){
+           int mid=(l+h)/2;
+           if(find(mat,mid)>left){
+               ans=mid;
+               h=mid-1;
+           }
+           else{
+               l=mid+1;
+           }
+       }
+       return ans;
+    }
+};
