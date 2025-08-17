d862985a50b4dfa8c57104b07458d71b0ad0192a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-17 19:05:27 +0530

    Time: 0 ms (100%), Space: 10 MB (25.58%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -10,37 +10,25 @@
  */
 class Solution {
 public:
-    int getLength(ListNode *head){
-        ListNode* temp=head;
-        int len=0;
-        while(temp!=NULL){
-         len++;
-         temp=temp->next;
-        }
-        return len;
-    }
     ListNode* middleNode(ListNode* head) {
-        //method 1
-        int len=getLength(head);
-         int jumps=len/2;
-         ListNode *temp=head;
-         while(jumps--){
-            temp=temp->next;
-         }
-         return temp;
-        // ListNode *slow=head;
-        // ListNode *fast=head;
-        // while(fast!=NULL){
-        //     fast=fast->next;
-        //     // if(fast==NULL)
-        //     // break;
-        //     // fast=fast->next;
-        //     // slow=slow->next;
-        //     if(fast!=NULL){
-        //     fast=fast->next;
-        //     slow=slow->next;
-        //     }
-        // }
-        // return slow;
+        //   ListNode* slow=head;
+        //   ListNode* fast=head;
+        //   while(fast!=NULL){
+        //      fast=fast->next;
+        //       if(fast!=NULL){
+        //       fast=fast->next;
+        //       slow=slow->next;
+        //       }
+        //   }
+        //   return slow;
+
+
+         ListNode* slow=head;
+          ListNode* fast=head;
+          while(fast&&fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
+          }
+          return slow;
     }
 };
