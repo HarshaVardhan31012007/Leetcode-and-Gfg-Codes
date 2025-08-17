9b9b65d5c3c3e7d180e2ff2a64b22121adb72882
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-17 18:33:19 +0530

    Time: 0 ms (100%), Space: 13.5 MB (40.35%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,19 +10,32 @@
  */
 class Solution {
 public:
-    void solve(ListNode *&prev,ListNode *&curr){
+    // void solve(ListNode *&prev,ListNode *&curr){
+    //     if(curr==NULL){
+    //         return;
+    //     }
+    //     ListNode* forward=curr->next;
+    //     curr->next=prev;
+    //     prev=curr;
+    //     curr=forward;
+    //     solve(prev,curr);
+    // }
+    // ListNode* reverseList(ListNode* head) {
+    //     ListNode *prev=NULL;ListNode *curr=head;
+    //     solve(prev,curr);
+    //     return prev;
+    // }
+
+    ListNode* solve(ListNode *&prev,ListNode *&curr){
         if(curr==NULL){
-            return;
+            return prev;
         }
         ListNode* forward=curr->next;
         curr->next=prev;
-        prev=curr;
-        curr=forward;
-        solve(prev,curr);
+        return solve(curr,forward);
     }
     ListNode* reverseList(ListNode* head) {
         ListNode *prev=NULL;ListNode *curr=head;
-        solve(prev,curr);
-        return prev;
+        return solve(prev,curr);
     }
 };
