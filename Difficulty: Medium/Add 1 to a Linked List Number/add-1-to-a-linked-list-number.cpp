f2c6ac0d6bc611eaa24286d996d999bd46e9b2cb
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-20 21:20:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
@@ -1,31 +1,3 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node* next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-void printList(Node* node) {
-    while (node != NULL) {
-        cout << node->data % 10;
-        node = node->next;
-    }
-    cout << "\n";
-}
-
-
-// } Driver Code Ends
-// User function template for C++
-
 /*
 
 struct Node
@@ -40,99 +12,37 @@ struct Node
 };
 
 */
-void solve(Node *head,int &carry){
-    if(!head)
-    return;
-    solve(head->next,carry);
-    int sum=head->data+carry;
-    head->data=sum%10;
-    carry=sum/10;
-}
+
 class Solution {
   public:
-    // Node* reverselist(Node *&head){
-    //     Node *prev=NULL;
-    //     Node *curr=head;
-    //     while(curr!=NULL){
-    //         Node *forward=curr->next;
-    //         curr->next=prev;
-    //         prev=curr;
-    //         curr=forward;
-    //     }
-    //     return prev;
-    // }
+    Node* reverseLL(Node* head){
+        Node* prev=NULL;
+        Node* temp=head;
+        while(temp!=NULL){
+            Node* forward=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=forward;
+        }
+        return prev;
+    }
     Node* addOne(Node* head) {
-        // //reverse
-        // head=reverselist(head);
-        // //add 1
-        // int carry=1;
-        // Node *temp=head;
-        // while(temp!=NULL){
-        //     int sum=carry+temp->data;
-        //     temp->data=sum%10;
-        //     carry=sum/10;
-        //     if(carry==0)
-        //     break;
-        //     //special case for last node
-        //     if(temp->next==NULL&&carry!=0){
-        //         Node *newNode=new Node(carry);
-        //         temp->next=newNode;
-        //         temp=newNode;
-        //     }
-        //     temp=temp->next;
-        // }
-        // //reverse
-        // head=reverselist(head);
-        
-        // return head;
-        
-        //method 2
-        int carry=1;
-        solve(head,carry);
-        if(carry){
-            Node *newNode=new Node(carry);
-            newNode->next=head;
-            head=newNode;
-        }
-        return head;
+       head=reverseLL(head);
+       Node *curr=head;
+       int carry=1;
+       while(curr!=NULL){
+           int sum=curr->data+carry;
+           curr->data=sum%10;
+           carry=sum/10;
+           if(!curr->next&&carry){
+             curr->next=new Node(carry);
+             carry=0;
+           }
+           if(carry==0)
+           break;
+           curr=curr->next;
+       }
+       head=reverseLL(head);
+       return head;
     }
-};
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        if (arr.empty()) {
-            cout << -1 << endl;
-            continue;
-        }
-
-        int data = arr[0];
-        struct Node* head = new Node(data);
-        struct Node* tail = head;
-        for (int i = 1; i < arr.size(); ++i) {
-            data = arr[i];
-            tail->next = new Node(data);
-            tail = tail->next;
-        }
-        Solution ob;
-        head = ob.addOne(head);
-        printList(head);
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
+};                                                        
