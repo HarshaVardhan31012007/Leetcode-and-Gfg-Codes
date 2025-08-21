38a604147c52a93133e6be673b4c67301f85eca2
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-21 21:12:46 +0530

    Time: 15 ms (42.72%), Space: 65.1 MB (69.91%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,78 +1,16 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-    //   for(int i=0;i<nums.size();i++){
-    //     for(int j=i+1;j<nums.size();j++){
-    //         if(nums[i]==nums[j])
-    //         return nums[i];
-    //     }
-    //   }
-    //   return -1;
-
-    // unordered_map<int,int>mpp;
-    // for(auto &num:nums){
-    //     mpp[num]++;
-    //     if(mpp[num]>1)
-    //     return num;
-    // }
-    // return -1;
-
-    // sort(nums.begin(),nums.end());
-    // for(int i=0;i<nums.size();i++){
-    //     if(i>0&&nums[i]==nums[i-1])
-    //     return nums[i];
-    // }
-    // return -1;
-
-
-    // for(int i=0;i<nums.size();i++){
-    //     int idx=abs(nums[i]);
-    //     if(nums[idx-1]>0)
-    //     nums[idx-1]*=-1;
-    //     else
-    //     return idx;
-    // }
-    // return -1;
-
-    // int i=0;
-    // while(i<nums.size()){
-    //     int idx=nums[i];
-    //     if(nums[idx-1]!=nums[i])
-    //     swap(nums[idx-1],nums[i]);
-    //     else
-    //     i++;
-    // }
-    //return nums.back();
-
-
-
-    //  int i=0;
-    // while(i<nums.size()){
-    //     int idx=nums[i];
-    //     if(nums[idx]!=nums[i])
-    //     swap(nums[idx],nums[i]);
-    //     else
-    //     i++;
-    // }
-    // return nums[0];
-
-
-     //positioning method
-    //  while(arr[arr[0]]!=arr[0]){
-    //     swap(arr[arr[0]],arr[0]);
-    //  }
-    //  return arr[0];
-
-    int slow=nums[0];int fast=nums[0];
-    do{
-        slow=nums[slow];
-        fast=nums[nums[fast]];
-    } while(slow!=fast);
-    slow=nums[0];
-    while(slow!=fast){
-        slow=nums[slow];
-        fast=nums[fast];
-    }
-    return slow;
+        int slow=nums[0];int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        fast=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
     }
 };
