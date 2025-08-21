ba262078027fe823e9c04cbdc4a68b52699148d4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-08-21 20:25:48 +0530

    Time: 4 ms (21.4%), Space: 77.1 MB (44.15%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,22 +10,35 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans=new ListNode(-1);
-        ListNode* it=ans;
-        int carry=0;
-        while(l1||l2||carry){
+    // ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    //     ListNode* ans=new ListNode(-1);
+    //     ListNode* it=ans;
+    //     int carry=0;
+    //     while(l1||l2||carry){
+    //         int a=l1!=NULL?l1->val:0;
+    //         int b=l2!=NULL?l2->val:0;
+    //         int sum=a+b+carry;
+    //         it->next=new ListNode(sum%10);
+    //         it=it->next;
+    //          carry=sum/10;
+    //         l1=l1!=NULL?l1->next:l1;
+    //         l2=l2!=NULL?l2->next:l2;
+    //     }
+    //     ListNode* fans=ans->next;
+    //     delete ans;
+    //     return fans;
+    // }
+
+     ListNode* solve(ListNode* l1,ListNode* l2,int carry){
+        if(!l1&&!l2&&!carry) return NULL;
             int a=l1!=NULL?l1->val:0;
             int b=l2!=NULL?l2->val:0;
             int sum=a+b+carry;
-            it->next=new ListNode(sum%10);
-            it=it->next;
-             carry=sum/10;
-            l1=l1!=NULL?l1->next:l1;
-            l2=l2!=NULL?l2->next:l2;
-        }
-        ListNode* fans=ans->next;
-        delete ans;
-        return fans;
+            ListNode* ans=new ListNode(sum%10);
+            ans->next=solve(l1!=NULL?l1->next:l1,l2!=NULL?l2->next:l2,sum/10);
+            return ans;
+     }
+     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+       return solve(l1,l2,0);
     }
 };
