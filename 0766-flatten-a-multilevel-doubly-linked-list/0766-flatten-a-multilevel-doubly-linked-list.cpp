cdb63ebec745d6c86a766c955e8228ba976641f8
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-05 10:00:53 +0530

    Time: 3 ms (66.27%), Space: 11.9 MB (8.7%) - LeetHub

--- 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.cpp
@@ -11,22 +11,55 @@ public:
 
 class Solution {
 public:
-    Node* flatten(Node* head) {
-        if(head==NULL) return NULL;
-        if(head->child){
-          Node* temp=head->next;
-          head->child->prev=head;
-          head->next=flatten(head->child);
-          head->child=NULL;
-          Node* p=head;
-          while(p->next!=NULL)
-          p=p->next;
-          p->next=temp;
-          if(temp)
-          temp->prev=p;
+    // Node* flatten(Node* head) {
+    //     if(head==NULL) return NULL;
+    //     if(head->child){
+    //       Node* temp=head->next;
+    //       head->child->prev=head;
+    //       head->next=flatten(head->child);
+    //       head->child=NULL;
+    //       Node* p=head;
+    //       while(p->next!=NULL)
+    //       p=p->next;
+    //       p->next=temp;
+    //       if(temp)
+    //       temp->prev=p;
+    //     }
+    //     else
+    //     head->next=flatten(head->next);
+    //     return head;
+    // }
+
+
+
+    Node* solve(Node* head){
+        Node* it=head;
+        Node* tail=NULL;
+        while(it){
+            if(it->child){
+                Node* childTail=solve(it->child);
+                cout<<it->val<<endl;
+                cout<<childTail->val<<endl;
+                childTail->next=it->next;
+                if(it->next)
+                it->next->prev=childTail;
+                it->next=it->child;
+                it->child->prev=it;
+                it->child=NULL;
+                tail=childTail;
+                it=childTail->next;
+            }
+            else{
+            tail=it;
+            it=it->next;
+            }
         }
-        else
-        head->next=flatten(head->next);
-        return head;
+        if(tail)
+        cout<<tail->val<<endl;
+        return tail;
+    }
+    Node* flatten(Node* head) {
+      solve(head);
+      return head;
     }
 };
