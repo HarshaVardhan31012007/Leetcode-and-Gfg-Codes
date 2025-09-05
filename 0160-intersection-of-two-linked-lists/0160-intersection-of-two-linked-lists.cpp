586823b6d259b86dc03757de57132e229e207330
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-05 13:05:36 +0530

    Time: 37 ms (60.49%), Space: 18.4 MB (82.04%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -10,14 +10,36 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         // this is wrong because when it is becoming null at in that iteration you should move to head
+        // ListNode* a=headA;
+        // ListNode* b=headB;
+        // while(a!=b){
+        //     if(!a) a=headB;
+        //     if(!b) b=headA;
+        //     a=a->next;
+        //     b=b->next;
+        // }
+        // return a;
+
+        
+        // ListNode* a=headA;
+        // ListNode* b=headB;
+        // while(a!=b){
+        //     if(!a) a=headB;
+        //     if(!b) b=headA;
+        //     if(a==b) return a;// here iteration completely perfectly that means if null in previous iteration it correct now
+        //     a=a->next;
+        //     b=b->next;
+        // }
+        // return a;
+
         ListNode* a=headA;
         ListNode* b=headB;
         while(a!=b){
-            if(!a) a=headB;
-            if(!b) b=headA;
-            if(a==b) return a;
             a=a->next;
             b=b->next;
+            if(a==NULL&&b==NULL) return NULL; 
+            if(!a) a=headB;
+            if(!b) b=headA;
         }
         return a;
 
