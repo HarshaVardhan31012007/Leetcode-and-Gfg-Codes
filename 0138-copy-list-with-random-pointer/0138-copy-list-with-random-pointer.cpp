08e6a43c812669c6b318a3884b026bd0044b30df
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-06 11:27:50 +0530

    Time: 3 ms (91.63%), Space: 15.2 MB (45.66%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,79 +16,28 @@ public:
 
 class Solution {
 public:
-    // Node *solve(Node* head,unordered_map<Node*,Node*>&mpp){
-    //     if(head==NULL)
-    //     return NULL;
-    //     Node*newHead=new Node(head->val);
-    //     mpp[head]=newHead;
-    //     newHead->next=solve(head->next,mpp);
-    //     if(head->random){
-    //         newHead->random=mpp[head->random];
-    //     }//else case we no need to go map me by default zeros hota hai
-    //     return newHead;
-    // }
     Node* copyRandomList(Node* head) {
-        //method 1
-        // Node *temp=head;
-        // Node *ansHead=NULL;
-        // Node *ansTail=NULL;
-        // unordered_map<Node*,Node*>mpp;
-        // while(temp!=NULL){
-        //    Node *newNode=new Node(temp->val);
-        //    if(!ansTail&&!ansHead){
-        //     ansHead=newNode;
-        //     ansTail=newNode;
-        //    } 
-        //    else{
-        //     ansTail->next=newNode;
-        //     ansTail=newNode;
-        //    }
-        //    mpp[temp]=newNode;
-        //    temp=temp->next;
-        // }
-        // temp=head;
-        // while(temp!=NULL){
-        //     if(temp->random)
-        //     mpp[temp]->random=mpp[temp->random];
-        //     else
-        //     mpp[temp]->random=nullptr;
-        //     temp=temp->next;
-        // }
-        // return ansHead;
-        
-        //method 2//recursive
-        // unordered_map<Node*,Node*>mpp;
-        // return solve(head,mpp);
-
-        //method 3
-        if(head==NULL)
-        return NULL;
-        //clone A->A'
-        Node *temp=head;
-        while(temp!=NULL){
-            Node *cloneNode=new Node(temp->val);
-            cloneNode->next=temp->next;
-            temp->next=cloneNode;
-            temp=temp->next->next;
-        }
-        //asign random links of A' with help of A
-        temp=head;
-        while(temp!=NULL){
-            Node *clonedNode=temp->next;
-            clonedNode->random=temp->random?temp->random->next:nullptr;
-            temp=temp->next->next;
-        }
-        //detacH A from A'
-        temp=head;
-        Node *clonedHead=head->next;
-        while(temp!=NULL){
-            Node *clonedNode=temp->next;
-            temp->next=temp->next->next;
-            if(clonedNode->next){
-                clonedNode->next=clonedNode->next->next;
-            }
-            temp=temp->next;
-        }
-        return clonedHead;
+       if(!head) return head;
+       unordered_map<Node*,Node*>mpp;
+       mpp[nullptr]=nullptr;
+       Node* newHead=new Node(head->val);
+       Node* ans=newHead;
+       mpp[head]=ans;
+       Node* temp=head;
+       temp=temp->next;
+       while(temp!=NULL){
+         ans->next=new Node(temp->val);
+         ans=ans->next;
+         mpp[temp]=ans;
+         temp=temp->next;
+       }
+       ans->next=NULL;
+       temp=head;
+       while(temp!=NULL){
+        Node* a=temp->random;
+        mpp[temp]->random=mpp[a];
+        temp=temp->next;
+       }
+       return newHead;
     }
 };
