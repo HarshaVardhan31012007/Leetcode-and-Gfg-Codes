a882258db7ea5a78c8dfbefc6b4ca46642fefe6b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-06 13:16:28 +0530

    Time: 0 ms (100%), Space: 16.3 MB (93.67%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -47,38 +47,69 @@ public:
 
 
 
-     ListNode* reverseLL(ListNode* prev,ListNode* curr,int &len){
-         while(curr!=NULL){
+    //  ListNode* reverseLL(ListNode* prev,ListNode* curr,int &len){
+    //      while(curr!=NULL){
+    //         len++;
+    //         ListNode* forward=curr->next;
+    //         curr->next=prev;
+    //         prev=curr;
+    //         curr=forward;
+    //     }
+    //     return prev;
+    // }
+    // ListNode* rotateRight(ListNode* head, int k) {
+    //     if(!head) return head;
+    //     ListNode* prev=NULL;
+    //     ListNode* curr=head;
+    //     int len=0;
+    //     curr=reverseLL(prev,curr,len);
+    //     prev=NULL;
+    //     k=k%len;
+    //     head=curr;
+    //     for(int i=0;i<k;i++){
+    //         ListNode* forward=curr->next;
+    //         curr->next=prev;
+    //         prev=curr;
+    //         curr=forward;
+    //     }
+    //     ListNode* a=NULL;
+    //     ListNode* b=curr;
+    //     len=0;
+    //     if(k!=0)
+    //     head->next=reverseLL(a,b,len);
+    //     else
+    //     return reverseLL(a,b,len);
+    //     return prev;
+    // }
+
+
+    int getLength(ListNode* head){
+        int len=0;
+        while(head!=NULL){
             len++;
-            ListNode* forward=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=forward;
+            head=head->next;
         }
-        return prev;
+        return len;
     }
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head) return head;
-        ListNode* prev=NULL;
-        ListNode* curr=head;
-        int len=0;
-        curr=reverseLL(prev,curr,len);
-        prev=NULL;
+        if(!head) return head;// len=0 then k%len is not valid //here rotate same we get null
+        int len=getLength(head);
         k=k%len;
-        head=curr;
-        for(int i=0;i<k;i++){
-            ListNode* forward=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=forward;
+        if(k==0)
+        return head;
+        int moves=len-k-1;
+        ListNode* newHead=NULL;
+        ListNode* it=head;
+        for(int i=0;i<moves;i++){
+           it=it->next;
         }
-        ListNode* a=NULL;
-        ListNode* b=curr;
-        len=0;
-        if(k!=0)
-        head->next=reverseLL(a,b,len);
-        else
-        return reverseLL(a,b,len);
-        return prev;
+        newHead=it->next;
+        it->next=NULL;
+        it=newHead;
+        while(it->next){
+            it=it->next;
+        }
+        it->next=head;
+        return newHead;
     }
 };
