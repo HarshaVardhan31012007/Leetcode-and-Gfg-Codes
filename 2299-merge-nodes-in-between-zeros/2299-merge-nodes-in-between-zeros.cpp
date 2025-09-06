e68da33d899dc58793e0d543b354db053955ce4d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-06 19:37:00 +0530

    Time: 67 ms (12.91%), Space: 273.9 MB (59.86%) - LeetHub

--- 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/2299-merge-nodes-in-between-zeros.cpp
@@ -35,29 +35,56 @@ public:
     // }
 
 
+    // ListNode* mergeNodes(ListNode* head) {
+    //     ListNode* ans=head;
+    //     ListNode* prev=head;
+    //     ListNode* temp=head->next;
+    //     int sum=0;
+    //     while(temp!=NULL){
+    //         if(temp->val==0){
+    //             temp->val=sum;
+    //             prev=prev->next;
+    //             sum=0;
+    //         }
+    //         else{
+    //         sum+=temp->val;
+    //         prev->next=temp->next;
+    //         temp->next=NULL;
+    //         delete temp;
+    //         }
+    //         temp=prev->next;
+    //     }
+    //     ListNode* finalans=ans->next;
+    //     //this is correct but online judge of leetcode after submitting it may use it
+    //     // ans->next=NULL;
+    //     // delete ans;
+    //     return finalans;
+    // }
+
     ListNode* mergeNodes(ListNode* head) {
-        ListNode* ans=head;
-        ListNode* prev=head;
-        ListNode* temp=head->next;
-        int sum=0;
-        while(temp!=NULL){
-            if(temp->val==0){
-                temp->val=sum;
-                prev=prev->next;
-                sum=0;
-            }
-            else{
-            sum+=temp->val;
-            prev->next=temp->next;
-            temp->next=NULL;
-            delete temp;
-            }
-            temp=prev->next;
-        }
-        ListNode* finalans=ans->next;
-        //this is correct but online judge of leetcode after submitting it may use it
-        // ans->next=NULL;
-        // delete ans;
-        return finalans;
+       ListNode* prev=NULL;
+       ListNode* slow=head;
+       ListNode* fast=head->next;
+       int sum=0;
+       while(fast!=NULL){
+           if(fast->val!=0){
+            sum=sum+fast->val;
+           }
+           else{
+            slow->val=sum;
+            prev=slow;
+            slow=slow->next;
+            sum=0;
+           }
+           fast=fast->next;
+       }
+       prev->next=NULL;
+       while(slow){
+        ListNode* temp=slow->next;
+        slow->next=NULL;
+        delete slow;
+        slow=temp;
+       }
+       return head;
     }
 };
