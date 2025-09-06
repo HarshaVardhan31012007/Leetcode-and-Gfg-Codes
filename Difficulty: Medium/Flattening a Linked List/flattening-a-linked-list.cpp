20d30d2307ee7cad6fcac489aba5b71c7269493d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-06 10:06:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -15,36 +15,55 @@ public:
 
 class Solution {
   public:
-    Node *flatten(Node *root) {
-        if(!root) return root;
-        flatten(root->next);
-        if(root->bottom&&root->next){
-                Node* it1=root->next;
-                Node* it2=root->bottom;
-                Node* it=root;
-                while(it1&&it2){
-                    if(it1->data<=it2->data){
-                        it->bottom=it1;
-                        it=it->bottom;
-                        it1=it1->bottom;
-                    } else{
-                        it->bottom=it2;
-                        it=it->bottom;
-                        it2=it2->bottom;
-                    }
-                }
-                if(it1){
-                    it->bottom=it1;
-                }
-                if(it2){
-                    it->bottom=it2;
-                }
+    // Node *flatten(Node *root) {
+    //     if(!root) return root;
+    //     flatten(root->next);
+    //     if(root->bottom&&root->next){
+    //             Node* it1=root->next;
+    //             Node* it2=root->bottom;
+    //             Node* it=root;
+    //             while(it1&&it2){
+    //                 if(it1->data<=it2->data){
+    //                     it->bottom=it1;
+    //                     it=it->bottom;
+    //                     it1=it1->bottom;
+    //                 } else{
+    //                     it->bottom=it2;
+    //                     it=it->bottom;
+    //                     it2=it2->bottom;
+    //                 }
+    //             }
+    //             if(it1){
+    //                 it->bottom=it1;
+    //             }
+    //             if(it2){
+    //                 it->bottom=it2;
+    //             }
+    //     }
+    //     else if(root->next){
+    //         root->bottom=root->next;
+    //         root->next=NULL;
+    //     }
+    //     //cout<<root->data<<endl;
+    //     return root;
+    // }
+    Node* merge(Node* a,Node* b){
+        if(!a) return b;
+        if(!b) return a;
+        Node* ans=0;
+        if(a->data<=b->data){
+            ans=a;
+            ans->bottom=merge(a->bottom,b);//a->bottom=merge(a->bottom,b);
         }
-        else if(root->next){
-            root->bottom=root->next;
-            root->next=NULL;
+        else{
+            ans=b;
+            ans->bottom=merge(a,b->bottom);
         }
-        //cout<<root->data<<endl;
+        return ans;
+    }
+    Node* flatten(Node* root){
+        if(!root) return 0;
+        root=merge(root,flatten(root->next));
         return root;
     }
 };
