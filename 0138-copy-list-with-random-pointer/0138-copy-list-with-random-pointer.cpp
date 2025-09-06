17e57b60a02acf7deb687a5d26253a7bdeaa7080
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-06 11:47:31 +0530

    Time: 2 ms (92.47%), Space: 15 MB (68.83%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,28 +16,58 @@ public:
 
 class Solution {
 public:
-    Node* copyRandomList(Node* head) {
+    // Node* copyRandomList(Node* head) {
+    //    if(!head) return head;
+    //    unordered_map<Node*,Node*>mpp;
+    //    mpp[nullptr]=nullptr;
+    //    Node* newHead=new Node(head->val);
+    //    Node* ans=newHead;
+    //    mpp[head]=ans;
+    //    Node* temp=head;
+    //    temp=temp->next;
+    //    while(temp!=NULL){
+    //      ans->next=new Node(temp->val);
+    //      ans=ans->next;
+    //      mpp[temp]=ans;
+    //      temp=temp->next;
+    //    }
+    //    ans->next=NULL;
+    //    temp=head;
+    //    while(temp!=NULL){
+    //     Node* a=temp->random;
+    //     mpp[temp]->random=mpp[a];
+    //     temp=temp->next;
+    //    }
+    //    return newHead;
+    // }
+
+
+     Node* copyRandomList(Node* head) {
        if(!head) return head;
-       unordered_map<Node*,Node*>mpp;
-       mpp[nullptr]=nullptr;
-       Node* newHead=new Node(head->val);
-       Node* ans=newHead;
-       mpp[head]=ans;
        Node* temp=head;
-       temp=temp->next;
        while(temp!=NULL){
-         ans->next=new Node(temp->val);
-         ans=ans->next;
-         mpp[temp]=ans;
-         temp=temp->next;
+        Node* newNode=new Node(temp->val);
+        newNode->next=temp->next;
+        temp->next=newNode;
+        temp=temp->next->next;
        }
-       ans->next=NULL;
        temp=head;
        while(temp!=NULL){
-        Node* a=temp->random;
-        mpp[temp]->random=mpp[a];
-        temp=temp->next;
+        if(temp->random){
+           temp->next->random=temp->random->next;
+        }
+        temp=temp->next->next;
        }
+       Node* newHead=head->next;
+       Node* it=newHead;
+       temp=head;
+       while(it->next!=NULL){
+        temp->next=it->next;
+        it->next=it->next->next;
+        temp=temp->next;
+        it=it->next;
+       }
+       temp->next=NULL;
        return newHead;
     }
 };
