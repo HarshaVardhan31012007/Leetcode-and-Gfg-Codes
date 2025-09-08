6753a19cc72e7e1be25736473633a7fd09f790c2
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-08 21:09:48 +0530

    Time: 5 ms (22.16%), Space: 186.7 MB (13.18%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.cpp
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.cpp
@@ -33,16 +33,45 @@ public:
         return 1+getLength(head->next);
     }
     ListNode* swapNodes(ListNode* head, int k) {
-        int len=getLength(head);
+        //here kth node from end can be first and kth node from start can be at end when k=n //here for each node we are going from head,but it is no need
+
+        //we can make sure k=n and k=1 same right so swap value if k=1 // this optimization in below
+
+        // int len=getLength(head);
+        // if(len==0||((len&1)&&k==(len+1)/2)) return head;
+        // //when node is null ans is null//constraint lo null is not possible
+        // //when kth nodes from end and start are same,no swapping,head is ans
+        // ListNode* a=NULL;
+        // ListNode* temp=head;
+        // for(int i=0;i<k-1;i++){
+        //    temp=temp->next;
+        // }
+        // a=temp;
+        // temp=head;
+        // for(int i=0;i<len-k;i++)
+        // temp=temp->next;
+        // //here temp is at kth node from end
+        // swap(temp->val,a->val);
+        // return head;
+
+
+ //here kth node from end can be first and kth node from start can be at end when k=n 
+ //here for each node we are going from head,but it is no need
+ //we can make sure k=n and k=1 same right so swap value if k=1 // this optimization in below
+         int len=getLength(head);
         if(len==0||((len&1)&&k==(len+1)/2)) return head;
+        //when node is null ans is null//constraint lo null is not possible
+        //when kth nodes from end and start are same,no swapping,head is ans
+        int len1=len-k+1;
+        if(len-k+1<k)
+        swap(k,len1);
         ListNode* a=NULL;
         ListNode* temp=head;
         for(int i=0;i<k-1;i++){
            temp=temp->next;
         }
         a=temp;
-        temp=head;
-        for(int i=0;i<len-k;i++)
+        for(int i=k-1;i<len-k;i++)
         temp=temp->next;
         //here temp is at kth node from end
         swap(temp->val,a->val);
