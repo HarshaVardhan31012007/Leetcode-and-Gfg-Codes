8437f277ac548a21a51256eb4bdc99b666051393
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-09 22:45:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Expression contains redundant bracket or not/expression-contains-redundant-bracket-or-not.cpp
+++ Difficulty: Medium/Expression contains redundant bracket or not/expression-contains-redundant-bracket-or-not.cpp
@@ -1,83 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int checkRedundancy(string s){
-        // stack<char>st;
-        // for(int i=0;i<s.length();i++){
-        //     char ch=s[i];
-         
-        //     if(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='('){
-        //         st.push(ch);
-        //     }
-        //     if(ch==')'&&!st.empty()){
-        //         if(st.top()=='('){//(ab) in this case this code fails but this is not a valid testcases
-        //           st.pop();
-        //           return 1;
-        //         }
-        //         else{//it only comes when it is not redundant like when blw brackets operators are there
-        //             while(st.top()!='('){
-        //                 st.pop();
-        //             }
-        //             st.pop();
-        //         }
-        //     }
-            
-        // }
-        
-        
-        
-        //if you want to consider that above (ab) case also
-        //follow this
+    bool checkRedundancy(string &s) {
         stack<char>st;
         for(int i=0;i<s.length();i++){
-            char ch=s[i];
-         
-            if(ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='('){
-                st.push(ch);
+            if(s[i]=='('){
+                st.push(s[i]);
             }
-            else{
-                if(ch==')'&&!st.empty()){
-                    bool isReduntant=true;
-                    while(st.top()!='('){
-                        if(st.top()=='+'||st.top()=='-'||st.top()=='*'||st.top()=='/'){
-                            isReduntant=false;
-                        }
-                        st.pop();
-                    }
-                    if(isReduntant==true)
-                    return true;
+            else if(s[i]==')'){
+                if(!st.empty()&&st.top()=='(')
+                return true;
+                while(st.top()!='('){
                     st.pop();
-                 }
                 }
+                st.pop();
+            }
+            else if(s[i]=='+'||s[i]=='-'||s[i]=='*'||s[i]=='/'){
+                st.push(s[i]);
+            }
         }
         return false;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    while (t--) {
-
-        string s;
-        cin >> s;
-
-        Solution obj;
-        int res = obj.checkRedundancy(s);
-
-        cout << res << endl;
-    
-cout << "~" << "\n";
-}
-}
-
-// } Driver Code Ends
