a34d5b711fb6db005546812674311c4cc8d0ac91
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-09 22:09:03 +0530

    Time: 0 ms (100%), Space: 8.8 MB (41.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,20 +3,16 @@ public:
     bool isValid(string s) {
         stack<char>st;
         for(int i=0;i<s.length();i++){
-            char ch=s[i];
-            if(ch=='{'||ch=='('||ch=='['){
-                st.push(ch);
-            }
-            else if(ch=='}'&&!st.empty()&&st.top()=='{'){
-                st.pop();
-            }
-            else if(ch==')'&&!st.empty()&&st.top()=='('){
-                st.pop();
-            }
-            else if(ch==']'&&!st.empty()&&st.top()=='['){
-                st.pop();
-            }
+            if(s[i]=='{'||s[i]=='('||s[i]=='[')
+            st.push(s[i]);
             else{
+                if(s[i]=='}'&&!st.empty()&&st.top()=='{')
+                st.pop();
+                else if(s[i]==']'&&!st.empty()&&st.top()=='[')
+                st.pop();
+                else if(s[i]==')'&&!st.empty()&&st.top()=='(')
+                st.pop();
+                else
                 return false;
             }
         }
