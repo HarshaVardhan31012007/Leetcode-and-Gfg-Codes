8b7d794e005d54f02564848c46ba1ca7b49c280b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-09 08:40:51 +0530

    Time: 0 ms (100%), Space: 11.3 MB (20.36%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -10,12 +10,39 @@
  */
 class Solution {
 public:
-    ListNode* swapPairs(ListNode* head) {
+    // ListNode* swapPairs(ListNode* head) {
+    //     if(!head||!head->next) return head;
+    //     ListNode* temp=head->next->next;
+    //     head->next->next=head;
+    //     ListNode* newHead=head->next;
+    //     head->next=swapPairs(temp);
+    //     return newHead;
+    // }
+
+
+
+     ListNode* swapPairs(ListNode* head) {
         if(!head||!head->next) return head;
-        ListNode* temp=head->next->next;
-        head->next->next=head;
-        ListNode* newHead=head->next;
-        head->next=swapPairs(temp);
-        return newHead;
+        ListNode* first=head;
+        ListNode* sec=head->next;
+        ListNode* prev=NULL;
+        while(first&&sec){
+             ListNode* third=sec->next;
+             sec->next=first;
+             first->next=third;
+             if(prev){
+                prev->next=sec;
+             }else{
+                head=sec;
+             }
+             prev=first;
+             first=third;
+             if(third){
+                sec=third->next;
+             }
+             else
+             sec=NULL;
+        }
+        return head;
     }
 };
