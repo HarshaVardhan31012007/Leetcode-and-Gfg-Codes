3c24bdd9348a30c199e6f657449a3cacbbcbe8bf
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-10 22:21:53 +0530

    Time: 62 ms (5.48%), Space: 95.8 MB (5.49%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
-    void findnextsmaller(vector<int>& heights, vector<int>&next){
-        stack<int>st;
-        st.push(heights.size());
-        int n=heights.size();
-        for(int i=n-1;i>=0;i--){
-          int element=heights[i];
-          while(st.top()!=heights.size()&&element<=heights[st.top()]){
-            st.pop();
-          }
-          next.push_back(st.top());
-          st.push(i);
+    void nextSmaller(vector<int>& heights,vector<int>&nexts){
+        stack<int>s;
+        s.push(-1);
+        for(int i=heights.size()-1;i>=0;i--){
+            while(s.top()!=-1&&heights[s.top()]>=heights[i]){
+                s.pop();
+            }
+            if(s.top()!=-1)
+            nexts.push_back(s.top());
+            else
+            nexts.push_back(heights.size());
+            s.push(i);
         }
     }
-     void findprevsmaller(vector<int>& heights, vector<int>&prev){
-        stack<int>st;
-        st.push(-1);
-        int n=heights.size();
-        for(int i=0;i<n;i++){
-          int element=heights[i];
-          while(st.top()!=-1&&element<=heights[st.top()]){
-            st.pop();
-          }
-          prev.push_back(st.top());
-          st.push(i);
+    void prevSmaller(vector<int>& heights,vector<int>&prevs){
+        stack<int>s;
+        s.push(-1);
+        for(int i=0;i<heights.size();i++){
+            while(s.top()!=-1&&heights[s.top()]>=heights[i]){
+                s.pop();
+            }
+            prevs.push_back(s.top());
+            s.push(i);
         }
     }
     int largestRectangleArea(vector<int>& heights) {
-        vector<int>prev;
-        vector<int>next;
-        findnextsmaller(heights,next);
-        reverse(next.begin(),next.end());
-        findprevsmaller(heights,prev);
-        int maxarea=INT_MIN;
+        vector<int>nexts;
+        vector<int>prevs;
+        nextSmaller(heights,nexts);
+        prevSmaller(heights,prevs);
+        reverse(nexts.begin(),nexts.end());
+        int maxi=INT_MIN;
         for(int i=0;i<heights.size();i++){
-            int width=next[i]-prev[i]-1;
-            int height=heights[i];
-            maxarea=max(maxarea,width*height);
+            int w=nexts[i]-prevs[i]-1;
+            int h=heights[i];
+            maxi=max(maxi,w*h);
         }
-        return maxarea;
+        return maxi;
     }
 };
