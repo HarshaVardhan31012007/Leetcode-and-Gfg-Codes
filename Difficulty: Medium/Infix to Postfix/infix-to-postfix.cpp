0e431626d1a583c22f6f35227cabd765fd5062c7
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-14 10:20:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
+++ Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
@@ -1,6 +1,5 @@
 class Solution {
   public:
-    // Function to convert an infix expression to a postfix expression.
     int priority(char ch){
         if(ch=='^'){
             return 3;
@@ -32,7 +31,11 @@ class Solution {
                 st.pop();
             }
             else{
-                while(!st.empty()&&priority(ch)<=priority(st.top())){
+                 while(ch=='^'&&!st.empty()&&priority(ch)<priority(st.top())){
+                    ans=ans+st.top();
+                    st.pop();
+                }
+                while(ch!='^'&&!st.empty()&&priority(ch)<=priority(st.top())){
                     ans=ans+st.top();
                     st.pop();
                 }
@@ -45,60 +48,4 @@ class Solution {
         }
         return ans;
     }
-    
-    
-    //infix to prefix
-    // void reverse(string &s){
-    //     int i=0;
-    //     int j=s.length()-1;
-    //     while(i<=j){
-    //         int temp=s[i];
-    //         s[i]=s[j];
-    //         s[j]=temp;
-    //         i++;
-    //         j--;
-    //     }
-    // }
-    // string infixToPrefix(string& s) {
-    //     stack<char>st; 
-    //     reverse(s);
-    //     string ans="";
-    //     for(int i=0;i<s.length();i++){
-    //         char ch=s[i];
-    //         if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z'||ch>='0'&&ch<='9'){
-    //             ans=ans+ch;
-    //         }
-    //         else if(ch=='(')
-    //         st.push(ch);
-    //         else if(ch==')'){
-    //             while(st.top()!='('){
-    //               ans=ans+st.top();
-    //               st.pop();
-    //             }
-    //             st.pop();
-    //         }
-    //         else{
-    //             if(ch=='^'){
-    //             while(!st.empty()&&priority(ch)<priority(st.top())){
-    //                 ans=ans+st.top();
-    //                 st.pop();
-    //             }
-    //             }
-    //             else{
-    //                 while(!st.empty()&&priority(ch)<=priority(st.top())){
-    //                 ans=ans+st.top();
-    //                 st.pop();
-    //             }
-    //             }
-    //             st.push(ch);
-    //         }
-    //     }
-    //     while(!st.empty()){
-    //         ans=ans+st.top();
-    //         st.pop();
-    //     }
-    //     reverse(ans);
-    //     return ans;
-    //}
-    
 };
