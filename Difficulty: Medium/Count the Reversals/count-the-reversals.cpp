97042b218b0cbaa971741d922b32ce0034d2d367
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-15 20:19:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the Reversals/count-the-reversals.cpp
+++ Difficulty: Medium/Count the Reversals/count-the-reversals.cpp
@@ -1,51 +1,26 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-int countRev (string s);
-int main()
-{
-    int t; cin >> t;
-    while (t--)
-    {
-        string s; cin >> s;
-        cout << countRev (s) << '\n';
-    
-cout << "~" << "\n";
-}
-}
-
-// Contributed By: Pranay Bansal
-// } Driver Code Ends
-
-
-int countRev (string s)
-{   if(s.size() & 1)
-    return -1;
-    stack<int>st;
-    for(int i=0;i<s.length();i++){
-        if(s[i]=='{'){
-            st.push(s[i]);
-        }
-        else{
-            if(!st.empty()&&s[i]=='}'&&st.top()=='{'){// no need of s[i]==')'
-                st.pop();
+class Solution {
+  public:
+    int countMinReversals(string s) {
+        stack<char>st;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(st.empty()){
+                if(s[i]=='}'){
+                    s[i]='{';
+                    count++;
+                }
+                st.push(s[i]);
             }
             else{
+                if(s[i]=='}'){
+                   st.pop();
+                }
+                else
                 st.push(s[i]);
             }
         }
+        if(st.size()&1)
+        return -1;
+        return count+st.size()/2;
     }
-        int ans=0;
-        while(!st.empty()){
-            char a=st.top();
-            st.pop();
-            char b=st.top();
-            st.pop();
-            if(a==b)
-            ans=ans+1;
-            else
-            ans=ans+2;
-        }
-        return ans;
-    }
+};
