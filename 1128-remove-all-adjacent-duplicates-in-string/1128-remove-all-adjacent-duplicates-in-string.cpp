9191ca1bcdfc90004454c19651f83a664f2f5f4d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-15 20:43:36 +0530

    Time: 752 ms (9.36%), Space: 783.9 MB (5.85%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        // string ans="";
-        // for(int i=0;i<s.length();i++){
-        //     if(ans.empty()||ans.back()!=s[i])
-        //     ans.push_back(s[i]);
-        //     else
-        //     ans.pop_back();
-        // }
-        // return ans;
-
-
-        int i=0;int n=s.length();
-        for(int j=0;j<n;j++,i++){
-            s[i]=s[j];
-            if(i>0&&s[i]==s[i-1])
-            i=i-2;
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            bool flag=1;
+            while(!st.empty()&&st.top()==s[i]){
+                flag=0;
+                st.pop();
+            }
+            if(flag)
+            st.push(s[i]);
         }
-        return s.substr(0,i);
+        string ans="";
+        while(!st.empty()){
+            ans=st.top()+ans;
+            st.pop();
+        }
+        return ans;
     }
 };
