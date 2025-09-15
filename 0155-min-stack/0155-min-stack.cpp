441aee3db45d36d72369599ae74f0030922a3fb1
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-15 21:23:41 +0530

    Time: 3 ms (61.63%), Space: 23.3 MB (77.17%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,27 +1,59 @@
 class MinStack {
 public:
-    vector<pair<int,int>>v;
+    // vector<pair<int,int>>v;
+    // MinStack() {
+        
+    // }
+    
+    // void push(int val) {
+    //     if(v.empty())
+    //     v.push_back(make_pair(val,val));
+    //     else
+    //     v.push_back(make_pair(val,min(val,v.back().second)));
+    // }
+    
+    // void pop() {
+    //     v.pop_back();
+    // }
+    
+    // int top() {
+    //     return v.back().first;
+    // }
+    
+    // int getMin() {
+    //     return v.back().second;
+    // }
+
+
+
+
+    stack<int>s;
+    stack<int>s1;
     MinStack() {
         
     }
     
     void push(int val) {
-        if(v.empty())
-        v.push_back(make_pair(val,val));
-        else
-        v.push_back(make_pair(val,min(val,v.back().second)));
+        if(s.empty()){
+        s.push(val);
+        s1.push(val);
+        }
+        else{
+            s.push(val);
+            s1.push(min(s1.top(),val));
+        }
     }
     
     void pop() {
-        v.pop_back();
+        s.pop();
+        s1.pop();
     }
     
     int top() {
-        return v.back().first;
+        return s.top();
     }
-    
     int getMin() {
-        return v.back().second;
+         return s1.top();
     }
 };
 
