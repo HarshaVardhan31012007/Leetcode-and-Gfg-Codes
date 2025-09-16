2f629da1da3cef7dc4fb7babddb67476a3bdc13b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-16 22:29:24 +0530

    Time: 99 ms (5.03%), Space: 45 MB (5.08%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,69 +1,31 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-    // stack<int>st;
-    //  for(int i=0;i<s.length();i++){
-     
-    //     if(s[i]=='('){
-    //         st.push(i);
-    //     }
-    //     else if(s[i]==')'){
-    //         if(!st.empty()){
-    //             st.pop();
-    //         }
-    //         else{
-    //             s[i]='#';
-    //         }
-    //     }
-    // }
-    // while(!st.empty()){
-    //     s[st.top()]='#';
-    //     st.pop();
-    // }
-    //    string ans="";
-    //    for(int i=0;i<s.length();i++){
-    //     if(s[i]!='#'){
-    //         ans.push_back(s[i]);
-    //     }
-    //    }
-    //    return ans;
-
-    int n=s.size();
-    int count=0;
-    for(int i=0;i<n;i++){
-       if(s[i]=='('){
-        count++;
-       }
-       else if(s[i]==')'){
-          if(count==0){
-            s[i]='#';
-          }
-          else{
-            count--;
-          }
-       }
-    }
-     count=0;
-    for(int i=n-1;i>=0;i--){
-       if(s[i]==')'){
-        count++;
-       }
-       else if(s[i]=='('){
-          if(count==0){
-            s[i]='#';
-          }
-          else{
-            count--;
-          }
-       }
-    }
-
-        string ans="";
-       for(int i=0;i<s.length();i++){
-        if(s[i]!='#'){
-            ans.push_back(s[i]);
+        stack<int>st;
+        for(int i=0;i<s.length();i++){
+            if(isalpha(s[i])) continue;
+            if(s[i]=='(')
+            st.push(i);
+            else{
+                if(!st.empty()&&s[st.top()]=='('){
+                    st.pop();
+                }
+                else
+                st.push(i);
+            }
         }
-       }
-       return ans;
+        unordered_map<int,bool>mpp;
+        while(!st.empty()){
+            int idx=st.top();
+            st.pop();
+            mpp[idx]=1;
+        }
+        string temp="";
+        for(int i=0;i<s.length();i++){
+            if(!mpp[i]){
+                temp+=s[i];
+            }
+        }
+        return temp;
     }
 };
