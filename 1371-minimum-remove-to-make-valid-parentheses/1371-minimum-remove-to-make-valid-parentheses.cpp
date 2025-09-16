937bde1eed5bc3cd85a772b078a9ee59cc535200
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-16 22:39:09 +0530

    Time: 8 ms (61.58%), Space: 14 MB (69.92%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
-    string minRemoveToMakeValid(string s) {
+    // string minRemoveToMakeValid(string s) {
+    //     stack<int>st;
+    //     for(int i=0;i<s.length();i++){
+    //         if(isalpha(s[i])) continue;
+    //         if(s[i]=='(')
+    //         st.push(i);
+    //         else{
+    //             if(!st.empty()&&s[st.top()]=='('){
+    //                 st.pop();
+    //             }
+    //             else
+    //             st.push(i);
+    //         }
+    //     }
+    //     unordered_map<int,bool>mpp;
+    //     while(!st.empty()){
+    //         int idx=st.top();
+    //         st.pop();
+    //         mpp[idx]=1;
+    //     }
+    //     string temp="";
+    //     for(int i=0;i<s.length();i++){
+    //         if(!mpp[i]){
+    //             temp+=s[i];
+    //         }
+    //     }
+    //     return temp;
+    // }
+
+
+
+     string minRemoveToMakeValid(string s) {
         stack<int>st;
         for(int i=0;i<s.length();i++){
             if(isalpha(s[i])) continue;
@@ -11,18 +42,17 @@ public:
                     st.pop();
                 }
                 else
-                st.push(i);
+                s[i]='#';
             }
         }
-        unordered_map<int,bool>mpp;
         while(!st.empty()){
             int idx=st.top();
             st.pop();
-            mpp[idx]=1;
+            s[idx]='#';
         }
         string temp="";
         for(int i=0;i<s.length();i++){
-            if(!mpp[i]){
+            if(s[i]!='#'){
                 temp+=s[i];
             }
         }
