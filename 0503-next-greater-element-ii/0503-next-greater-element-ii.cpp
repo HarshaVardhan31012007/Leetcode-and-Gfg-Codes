811ac633e818130918934aa2186facda7e2ef1df
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-16 20:32:26 +0530

    Time: 3 ms (75.49%), Space: 31 MB (5.01%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
-     void nextGreater(vector<int>& nums2,vector<int>&nextg){
+    //  void nextGreater(vector<int>& nums,vector<int>&nextg){
+    //      stack<int>s;
+    //      s.push(INT_MAX);
+    //      for(int i=nums.size()-1;i>=0;i--){
+    //         while(s.top()<=nums[i]){
+    //             s.pop();
+    //         }
+    //         if(s.top()!=INT_MAX)
+    //         nextg.push_back(s.top());
+    //         else
+    //         nextg.push_back(-1);
+    //         s.push(nums[i]);
+    //      }
+    // }
+    // vector<int> nextGreaterElements(vector<int>& nums) {
+    //     int n=nums.size();
+    //     for(int i=0;i<n;i++)
+    //     nums.push_back(nums[i]);
+    //     vector<int>nextg; 
+    //     nextGreater(nums,nextg);
+    //     return vector(nextg.rbegin(),nextg.rbegin()+n);
+    // }
+
+
+
+     void nextGreater(vector<int>& nums,vector<int>&nextg,int &n){
          stack<int>s;
          s.push(INT_MAX);
-         for(int i=nums2.size()-1;i>=0;i--){
-            while(s.top()<=nums2[i]){
+         for(int i=2*n-1;i>=0;i--){
+            int val=nums[i%n];
+            while(s.top()<=val){
                 s.pop();
             }
             if(s.top()!=INT_MAX)
             nextg.push_back(s.top());
             else
             nextg.push_back(-1);
-            s.push(nums2[i]);
+            s.push(val);
          }
     }
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        nums.push_back(nums[i]);
         vector<int>nextg; 
-        nextGreater(nums,nextg);
+        int n=nums.size();
+        nextGreater(nums,nextg,n);
         return vector(nextg.rbegin(),nextg.rbegin()+n);
     }
 };
