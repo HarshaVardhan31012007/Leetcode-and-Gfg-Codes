1825cecc3d80dc568e7e21a7858aa44bc20d1821
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-16 16:25:59 +0530

    Time: 3 ms (42.07%), Space: 12.4 MB (80.98%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    // void nextGreater(vector<int>& nums2,vector<int>&nextg){
+    //      stack<int>s;
+    //      s.push(-1);
+    //      for(int i=nums2.size()-1;i>=0;i--){
+    //         while(!s.empty()&&s.top()!=-1&&s.top()<nums2[i]){
+    //             s.pop();
+    //         }
+    //         nextg.push_back(s.top());
+    //         s.push(nums2[i]);
+    //      }
+    // }
+    // int find(vector<int>&nums2,int k){
+    //     for(int i=0;i<nums2.size();i++){
+    //         if(nums2[i]==k)
+    //         return i;
+    //     }
+    //     return -1;
+    // }
+    // vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    //     vector<int>nextg;
+    //     nextGreater(nums2,nextg);
+    //     reverse(nextg.begin(),nextg.end());
+    //     vector<int>ans(nums1.size());
+    //     for(int i=0;i<nums1.size();i++){
+    //         int idx=find(nums2,nums1[i]);
+    //         ans[i]=nextg[idx];
+    //     }
+    //     return ans;
+    // }
+
+
+
     void nextGreater(vector<int>& nums2,vector<int>&nextg){
          stack<int>s;
          s.push(-1);
@@ -21,11 +53,10 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int>nextg;
         nextGreater(nums2,nextg);
-        reverse(nextg.begin(),nextg.end());
         vector<int>ans(nums1.size());
         for(int i=0;i<nums1.size();i++){
             int idx=find(nums2,nums1[i]);
-            ans[i]=nextg[idx];
+            ans[i]=nextg[nextg.size()-1-idx];
         }
         return ans;
     }
