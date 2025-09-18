08a2c14c767249c868a7a6d47248f0bbf494eca5
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-18 20:02:00 +0530

    Time: 18 ms (35.75%), Space: 36.8 MB (27.73%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
+    // int minSwaps(string s) {
+    //     stack<char>st;
+    //     for(int i=0;i<s.length();i++){
+    //         if(s[i]=='['){
+    //             st.push(s[i]);
+    //         }
+    //         else{
+    //             if(!st.empty()&&st.top()=='[')
+    //             st.pop();
+    //             else
+    //             st.push(s[i]);
+    //         }
+    //     }
+    //     // if(st.size()==0)
+    //     // return 0;
+    //     // if(st.size()==2)
+    //     // return 1;
+    //     return ceil(st.size()/4.0);
+    // }
+
+
+
     int minSwaps(string s) {
         stack<char>st;
+        int count=0;
         for(int i=0;i<s.length();i++){
             if(s[i]=='['){
                 st.push(s[i]);
             }
             else{
-                if(!st.empty()&&st.top()=='[')
+                if(!st.empty())
                 st.pop();
                 else
-                st.push(s[i]);
+                count++;
             }
         }
         // if(st.size()==0)
         // return 0;
         // if(st.size()==2)
         // return 1;
-        return ceil(st.size()/4.0);
+        return ceil((count+st.size())/4.0);
     }
 };
