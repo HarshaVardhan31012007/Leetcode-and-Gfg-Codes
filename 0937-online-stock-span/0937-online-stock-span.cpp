105829e384361ca7e5cfbd51749bc613c8394688
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-18 13:50:13 +0530

    Time: 18 ms (95.03%), Space: 92 MB (89.29%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,38 +1,19 @@
 class StockSpanner {
 public:
-    // vector<int>ans;
-    // StockSpanner() {
-        
-    // }
-    
-    // int next(int price) {
-    //     ans.push_back(price);
-    //     int count=1;
-    //     int t=ans[ans.size()-1];
-    //     for(int i=ans.size()-2;i>=0;i--){
-    //         if(ans[i]<=t)
-    //         count++;
-    //         else
-    //         break;
-    //     }
-    //     return count;
-    // }
-
-
-
+    int idx;
     stack<pair<int,int>>st;
     StockSpanner() {
-        
+        idx=-1;
     }
     
     int next(int price) {
-       int span=1;
-       while(!st.empty()&&st.top().first<=price){
-        span+=st.top().second;
-        st.pop();
-       }
-       st.push({price,span});
-       return span;
+        while(!st.empty()&&st.top().second<=price){
+            st.pop();
+        }
+        int curr=st.empty()?-1:st.top().first;
+        idx++;
+        st.push({idx,price});
+        return idx-curr;
     }
 };
 
