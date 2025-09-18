82e182908fef07fc36906100a5c20e31094004fd
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-18 19:44:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the Reversals/count-the-reversals.cpp
+++ Difficulty: Medium/Count the Reversals/count-the-reversals.cpp
@@ -27,30 +27,56 @@ class Solution {
     
     
     
-    int countMinReversals(string s) {
+    // int countMinReversals(string s) {
+    //     if(s.size()&1) return -1;
+    //     stack<char>st;
+    //     int count=0;
+    //     for(int i=0;i<s.length();i++){
+    //       if(s[i]=='{')
+    //       st.push(s[i]);
+    //       else{
+    //           if(!st.empty()&&st.top()=='{')
+    //           st.pop();
+    //           else
+    //           st.push('}');
+    //       }
+    //     }
+    //     while(st.size()>1){
+    //         int b=st.top();st.pop();
+    //         int a=st.top();st.pop();
+    //         if(a==b){
+    //             count+=1;
+    //         }
+    //         else{
+    //             count+=2;
+    //         }
+    //     }
+    //     return count;
+    // }
+    
+    
+    
+     int countMinReversals(string s) {
         if(s.size()&1) return -1;
         stack<char>st;
         int count=0;
+        int count1=0;
         for(int i=0;i<s.length();i++){
-           if(s[i]=='{')
-           st.push(s[i]);
-           else{
-               if(!st.empty()&&st.top()=='{')
-               st.pop();
-               else
-               st.push('}');
-           }
+          if(s[i]=='{'){
+          st.push(s[i]);
+          count++;
+          }
+          else{
+              if(!st.empty()&&st.top()=='{'){
+              st.pop();
+              count--;
+              }
+              else{
+              st.push('}');
+              count1++;
+              }
+          }
         }
-        while(st.size()>1){
-            int b=st.top();st.pop();
-            int a=st.top();st.pop();
-            if(a==b){
-                count+=1;
-            }
-            else{
-                count+=2;
-            }
-        }
-        return count;
+        return ceil(count/2.0)+ceil(count1/2.0);
     }
 };
