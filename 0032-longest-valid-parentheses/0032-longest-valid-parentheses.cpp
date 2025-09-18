24d6f1b8cda1db401aa03cc5e76a236a98598321
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-18 20:29:13 +0530

    Time: 2 ms (40.66%), Space: 11.6 MB (31.84%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
+        // stack<int>st;
+        // st.push(-1);
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i]=='(')
+        //     st.push(i);
+        //     else{
+        //         if(!st.empty()&&st.top()!=-1&&s[st.top()]=='('){
+        //            st.pop();
+        //         }
+        //         else{
+        //             st.push(i);
+        //         }
+        //     }
+        // }
+        // st.push(s.length());
+        // int maxi=INT_MIN;
+        // while(st.size()>1){
+        //     int b=st.top();
+        //     st.pop();
+        //     int a=st.top();
+        //     maxi=max(maxi,b-a-1);
+        // }
+        // return maxi;
+
+
+
         stack<int>st;
         st.push(-1);
+        int maxi=INT_MIN;
         for(int i=0;i<s.length();i++){
             if(s[i]=='(')
             st.push(i);
             else{
-                if(!st.empty()&&st.top()!=-1&&s[st.top()]=='('){
-                   st.pop();
-                }
-                else{
-                    st.push(i);
-                }
+              st.pop();
+              if(st.empty()){
+                 st.push(i);
+              }
+              else{
+                 int curr=i-st.top();
+                 maxi=max(maxi,curr);
+              }
             }
         }
-        st.push(s.length());
-        int maxi=INT_MIN;
-        while(st.size()>1){
-            int b=st.top();
-            st.pop();
-            int a=st.top();
-            maxi=max(maxi,b-a-1);
-        }
-        return maxi;
+        return maxi==INT_MIN?0:maxi;
     }
 };
