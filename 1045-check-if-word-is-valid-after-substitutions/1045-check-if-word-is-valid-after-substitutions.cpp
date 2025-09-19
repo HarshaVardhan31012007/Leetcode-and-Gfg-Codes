f8d4411ca956419d33e7cceaaca1feaf40c34c55
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-19 14:17:42 +0530

    Time: 514 ms (11.65%), Space: 683.1 MB (6.67%) - LeetHub

--- 1045-check-if-word-is-valid-after-substitutions/1045-check-if-word-is-valid-after-substitutions.cpp
+++ 1045-check-if-word-is-valid-after-substitutions/1045-check-if-word-is-valid-after-substitutions.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isValid(string s) {
-        // stack<string>st;
+
+         // stack<string>st;
         // for(int i=0;i<s.length();i++){
         //      if(s[i]=='a') st.push(string(1,'a'));
         //      else if(s[i]=='b'){
@@ -26,25 +27,42 @@ public:
 
 
 
-         stack<string>st;
-        for(int i=0;i<s.length();i++){
-             if(s[i]=='a') st.push(string(1,'a'));
-             else if(s[i]=='b'){
-                if(!st.empty()&&st.top()=="a"){
-                    st.pop();
-                    st.push("ab");
-                }
-                else
-                return false;
-             }
-             else{
-             if(!st.empty()&&s[i]=='c'&&st.top()=="ab"){
-                 st.pop();
-             }
-             else
-             return false;
-             }
+        //  stack<string>st;
+        // for(int i=0;i<s.length();i++){
+        //      if(s[i]=='a') st.push(string(1,'a'));
+        //      else if(s[i]=='b'){
+        //         if(!st.empty()&&st.top()=="a"){
+        //             st.pop();
+        //             st.push("ab");
+        //         }
+        //         else
+        //         return false;
+        //      }
+        //      else{
+        //      if(!st.empty()&&s[i]=='c'&&st.top()=="ab"){
+        //          st.pop();
+        //      }
+        //      else
+        //      return false;
+        //      }
+        // }
+        // return st.empty();
+
+
+
+        
+       // if(s.size()==0) return true;
+        if(s=="") return true;
+       int idx=s.find("abc");
+        if(idx!=string::npos){
+            string l=s.substr(0,idx);
+            string r=s.substr(idx+3);
+            //don't do s=l+r and pass s
+            //because l+r creates one copy
+            //and s pass by value and again one copy
+            return isValid(l+r);
+            //only one copy
         }
-        return st.empty();
+        return false;
     }
 };
