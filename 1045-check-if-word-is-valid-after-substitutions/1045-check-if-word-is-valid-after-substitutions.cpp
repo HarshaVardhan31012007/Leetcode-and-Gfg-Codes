788b5b9687fb6f27403533c49f9aa35e0c5f1e15
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-19 10:52:52 +0530

    Time: 14 ms (23.82%), Space: 20.2 MB (13.26%) - LeetHub

---
+++ 1045-check-if-word-is-valid-after-substitutions/1045-check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<string>st;
+        for(int i=0;i<s.length();i++){
+             if(s[i]=='a') st.push(string(1,'a'));
+             else if(s[i]=='b'){
+                if(!st.empty()&&st.top()=="a"){
+                    st.pop();
+                    st.push("ab");
+                }
+                else{
+                    st.push(string(1,'b'));
+                }
+             }
+             else{
+             bool flag=0;
+             if(!st.empty()&&s[i]=='c'&&st.top()=="ab"){
+                 flag=1;
+                 st.pop();
+             }
+             if(!flag) st.push(string(1,s[i]));
+             }
+        }
+        return st.empty();
+    }
+};
