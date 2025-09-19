b7bc582f9708c10c77592d5a1014b11f67f28104
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-19 10:30:22 +0530

    Time: 9 ms (45.38%), Space: 21.1 MB (20.53%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,68 +1,51 @@
 class Solution {
-    void findnextsmaller(vector<int>& heights, vector<int>&next){
-        stack<int>st;
-        st.push(heights.size());
-        int n=heights.size();
-        for(int i=n-1;i>=0;i--){
-          int element=heights[i];
-          while(st.top()!=heights.size()&&element<=heights[st.top()]){
-            st.pop();
-          }
-          next.push_back(st.top());
-          st.push(i);
-        }
-    }
-     void findprevsmaller(vector<int>& heights, vector<int>&prev){
-        stack<int>st;
-        st.push(-1);
-        int n=heights.size();
-        for(int i=0;i<n;i++){
-          int element=heights[i];
-          while(st.top()!=-1&&element<=heights[st.top()]){
-            st.pop();
-          }
-          prev.push_back(st.top());
-          st.push(i);
-        }
-    }
-    int largestRectangleArea(vector<int>& heights) {
-        vector<int>prev;
-        vector<int>next;
-        findnextsmaller(heights,next);
-        reverse(next.begin(),next.end());
-        findprevsmaller(heights,prev);
-        int maxarea=INT_MIN;
-        for(int i=0;i<heights.size();i++){
-            int width=next[i]-prev[i]-1;
-            int height=heights[i];
-            maxarea=max(maxarea,width*height);
-        }
-        return maxarea;
-    }
 public:
+    void nextSmaller(vector<int>&temp,vector<int>&nexts){
+         stack<int>s;
+        for(int i=temp.size()-1;i>=0;i--){
+              while(!s.empty()&&temp[s.top()]>=temp[i]){
+                s.pop();
+              }
+              if(s.empty()) nexts[i]=temp.size();
+              else nexts[i]=s.top();
+              s.push(i);
+        }
+    }
+     void prevSmaller(vector<int>&temp,vector<int>&prevs){
+        stack<int>s;
+        for(int i=0;i<temp.size();i++){
+              while(!s.empty()&&temp[s.top()]>=temp[i]){
+                s.pop();
+              }
+              if(s.empty()) prevs[i]=-1;
+              else prevs[i]=s.top();
+              s.push(i);
+        }
+    }
+    int find(vector<int>&temp){
+        vector<int>nexts(temp.size());
+        nextSmaller(temp,nexts);
+        vector<int>prevs(temp.size());
+        prevSmaller(temp,prevs);
+        int maxi=INT_MIN;
+        for(int i=0;i<temp.size();i++){
+            int w=nexts[i]-prevs[i]-1;
+            int h=temp[i];
+            maxi=max(maxi,w*h);
+        }
+        return maxi;
+    }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        vector<vector<int>>v;
-        int m=matrix.size();
-        int n=matrix[0].size();
-        for(int i=0;i<m;i++){
-            vector<int>t;
-            for(int j=0;j<n;j++){
-                t.push_back(matrix[i][j]-'0');
+        vector<int>temp(matrix[0].size(),0);
+        int ans=INT_MIN;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                temp[j]=(matrix[i][j]=='0'?0:(1+temp[j]));
             }
-            v.push_back(t);
+            int curr=find(temp);
+            cout<<curr<<endl;
+            ans=max(ans,curr);
         }
-        int area=largestRectangleArea(v[0]);
-        for(int i=1;i<m;i++){
-            for(int j=0;j<n;j++){
-               if(v[i][j]){
-                v[i][j]+=v[i-1][j];
-               }
-               else{
-                v[i][j]=0;
-               }
-            }
-            area=max(area,largestRectangleArea(v[i]));
-        }
-        return area;
+        return ans;
     }
 };
