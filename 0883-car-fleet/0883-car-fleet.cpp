c0b4348194d4c257f09f0d2d24c074989d0c0587
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-20 16:34:55 +0530

    Time: 51 ms (50.3%), Space: 114.2 MB (11.3%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.cpp
+++ 0883-car-fleet/0883-car-fleet.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        //  vector<pair<int,int>>v;
+        //  for(int i=0;i<position.size();i++){
+        //        v.push_back({position[i],speed[i]});
+        //  }
+        //  sort(v.begin(),v.end());
+        //  stack<double>st;
+        //  for(int i=0;i<v.size();i++){
+        //     double currTime=(target-v[i].first)/((double)v[i].second);
+        //     while(!st.empty()&&st.top()<=currTime){
+        //         st.pop();
+        //     }
+        //     st.push(currTime);
+        //  }
+        //  return st.size();
+
+
          vector<pair<int,int>>v;
          for(int i=0;i<position.size();i++){
                v.push_back({position[i],speed[i]});
@@ -9,11 +25,21 @@ public:
          stack<double>st;
          for(int i=0;i<v.size();i++){
             double currTime=(target-v[i].first)/((double)v[i].second);
-            while(!st.empty()&&st.top()<=currTime){
-                st.pop();
-            }
             st.push(currTime);
          }
-         return st.size();
+         int count=0;
+         while(st.size()>1){
+            double b=st.top();
+            st.pop();
+            double a=st.top();
+            st.pop();
+            if(a<=b){
+                st.push(b);
+            }else{
+               count++;
+               st.push(a);
+            }
+         }
+         return count+1;
     }
 };
