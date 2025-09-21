e8ca5ca4c024e8bf0e9556f5eccc617a776f6277
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-21 12:05:24 +0530

    Time: 3 ms (29.82%), Space: 21.5 MB (89.43%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,9 +1,40 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
+        //optimal
+        // vector<int>v;
+        // for(int i=0;i<asteroids.size();i++){
+        //     bool destroy=false;
+        //     while(!v.empty()&&asteroids[i]<0&&v.back()>0){
+        //         if(abs(asteroids[i])<v.back()){
+        //         destroy=true;
+        //         break;
+        //         }
+        //         else if(abs(asteroids[i])==v.back()){
+        //             destroy=true;
+        //             v.pop_back();
+        //             break;
+        //         }
+        //         else{
+        //             v.pop_back();
+        //         }
+        //     }
+        //     if(!destroy)
+        //     v.push_back(asteroids[i]);
+        // }
+        // return v;
+
+
+        
         vector<int>v;
         for(int i=0;i<asteroids.size();i++){
             bool destroy=false;
+            if(asteroids[i]>0)
+            v.push_back(asteroids[i]);
+            else{
+            if(v.empty()||v.back()<0)
+            v.push_back(asteroids[i]);
+            else{
             while(!v.empty()&&asteroids[i]<0&&v.back()>0){
                 if(abs(asteroids[i])<v.back()){
                 destroy=true;
@@ -20,6 +51,8 @@ public:
             }
             if(!destroy)
             v.push_back(asteroids[i]);
+            }
+            }
         }
         return v;
     }
