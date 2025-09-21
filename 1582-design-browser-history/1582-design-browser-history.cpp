475095d7320f93e109d11e1859ca3c16d36cdb08
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-21 14:59:26 +0530

    Time: 31 ms (45.54%), Space: 66.3 MB (28.94%) - LeetHub

--- 1582-design-browser-history/1582-design-browser-history.cpp
+++ 1582-design-browser-history/1582-design-browser-history.cpp
@@ -40,32 +40,80 @@ public:
     // }
 
 
-    vector<string>v;
-    int i;
+    // vector<string>v;
+    // int i;
+    // BrowserHistory(string homepage) {
+    //    i=0;
+    //    v.push_back(homepage);
+    // }
+    
+    // void visit(string url) {
+    //     if(i+1<v.size())
+    //     v.erase(v.begin()+i+1,v.end());
+    //     v.push_back(url);
+    //     i++;
+    // }
+    
+    // string back(int steps) {
+    //    i=i-steps;
+    //    if(i<0)
+    //    i=0;
+    //    return v[i];
+    // }
+    
+    // string forward(int steps) {
+    //    i=i+steps;
+    //    if(i>=v.size())
+    //    i=v.size()-1;
+    //    return v[i];
+    // }
+
+
+    class Node{
+        public:
+        string data;
+        Node* next;
+        Node* prev;
+        Node(string val){
+            this->data=val;
+            this->next=NULL;
+            this->prev=NULL;
+        }
+    };
+    Node* head=NULL;
+    Node* temp=head;
     BrowserHistory(string homepage) {
-       i=0;
-       v.push_back(homepage);
+      head=new Node(homepage);
+      temp=head;
     }
     
     void visit(string url) {
-        if(i+1<v.size())
-        v.erase(v.begin()+i+1,v.end());
-        v.push_back(url);
-        i++;
+        Node* temp2=temp;
+        Node* temp1=temp->next;
+        temp->next=new Node(url);
+        temp=temp->next;
+        temp->prev=temp2;
+        while(temp1){
+            Node* t=temp1->next;
+            temp1->next=NULL;
+            temp1->prev=NULL;
+            delete temp1;
+            temp1=t;
+        }
     }
     
     string back(int steps) {
-       i=i-steps;
-       if(i<0)
-       i=0;
-       return v[i];
+        while(steps--&&temp->prev){
+            temp=temp->prev;
+        }
+        return temp->data;
     }
     
     string forward(int steps) {
-       i=i+steps;
-       if(i>=v.size())
-       i=v.size()-1;
-       return v[i];
+        while(steps--&&temp->next){
+            temp=temp->next;
+        }
+        return temp->data;
     }
 };
 
