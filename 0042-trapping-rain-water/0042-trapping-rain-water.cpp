38d58e7e24248ca8711b9fc69c66024fff87ae78
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-21 16:34:39 +0530

    Time: 6 ms (5.75%), Space: 28.6 MB (5.12%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void nextGreater(vector<int>&height,vector<int>&nextg){
+         stack<int>st;
+         for(int i=height.size()-1;i>=0;i--){
+            while(!st.empty()&&height[st.top()]<height[i]){
+                st.pop();
+            }
+            if(!st.empty())
+            nextg[i]=st.top();
+            st.push(i);
+         }
+    }
+    void prevGreater(vector<int>&height,vector<int>&prevg){
+         stack<int>st;
+         for(int i=0;i<height.size();i++){
+            while(!st.empty()&&height[st.top()]<=height[i]){
+                st.pop();
+            }
+            if(!st.empty())
+            prevg[i]=st.top();
+            st.push(i);
+         }
+    }
+    int trap(vector<int>& height) {
+
+        vector<int>prevg(height.size(),-1);
+        prevGreater(height,prevg);
+        cout<<"jkjnfi"<<endl;
+        vector<int>nextg(height.size(),height.size());
+        nextGreater(height,nextg);
+        cout<<"jkjnfi"<<endl;
+        int ans=0;
+        for(int i=0;i<height.size();i++){
+            int h1=-1;
+            if(prevg[i]!=-1&&nextg[i]!=height.size())
+            h1=min(height[prevg[i]],height[nextg[i]]);
+            int h2=height[i];
+            int h=0;
+            if(h1!=-1)
+            h=h1-h2;
+            int w=nextg[i]-prevg[i]-1;
+            ans+=w*h;
+        }
+        return ans;
+
+    }
+};
