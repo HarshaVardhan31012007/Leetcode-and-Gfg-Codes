5aae57435ce83d2acd97c3a3a6fcddda384a84f8
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-22 23:13:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Interleave the First Half of the Queue with Second Half/interleave-the-first-half-of-the-queue-with-second-half.cpp
+++ Difficulty: Medium/Interleave the First Half of the Queue with Second Half/interleave-the-first-half-of-the-queue-with-second-half.cpp
@@ -1,60 +1,17 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
-
-class Solution{
-public:
-    vector<int> rearrangeQueue(queue<int> &q){
-        vector<int>ans;
-        int half=q.size()/2;
-        queue<int>q2;
-        while(half--){
-            int a=q.front();
-            q.pop();
-            q2.push(a);
-        }
-        while(!q.empty()){
-          ans.push_back(q2.front());
-          q2.pop();
-          ans.push_back(q.front());
-          q.pop();
-        }
-        return ans;
+class Solution {
+  public:
+    void rearrangeQueue(queue<int> &q) {
+    //   int half=q.size()/2;
+    //   queue<int>q1;
+    //   for(int i=0;i<half;i++){
+    //       q1.push(q.front());
+    //       q.pop();
+    //   }
+    //   while(half--){
+    //       q.push(q1.front());
+    //       q1.pop();
+    //       q.push(q.front());
+    //       q.pop();
+    //   }
     }
 };
-
-//{ Driver Code Starts.
-
-int main(){
-    
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            int a;
-            cin>>a;
-            q.push(a);
-        }
-        Solution ob;
-        vector<int> ans=ob.rearrangeQueue(q);
-        for(int i=0;i<ans.size();i++){
-            cout<<ans[i]<<" ";
-        }
-        cout<<endl;
-    
-cout << "~" << "\n";
-}
-    
-    return 0;
-}
-// } Driver Code Ends
