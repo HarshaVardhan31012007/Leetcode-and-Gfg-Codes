6d30db1797e8d4cea25b069b71a12d0c7c76b0f3
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-23 23:38:45 +0530

    Time: 0 ms (100%), Space: 112.5 MB (16.75%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,84 +1,21 @@
 class Solution {
 public:
-    // int check(vector<int>& gas, vector<int>& cost,int k,int i,int tank,int n){
-    // //    if(tank<cost[i]){
-    // //      return -1;
-    // //    }
-    // //    if(i==k){
-    // //     return k;
-    // //    }
-    // //    return check(gas,cost,k,(i+1)%n,tank-cost[i]+gas[(i+1)%n],n);
-
-    //  while(i!=k){
-    //     if(tank<cost[i]){
-    //         return -1;
-    //     }
-    //     tank=tank-cost[i]+gas[(i+1)%n];
-    //     i=(i+1)%n;
-    //  }
-    //  return k;
-    // }
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        //method 1
-        //brute force
-        // int ans=-1;
-        // int n=gas.size();
-        // for(int i=0;i<gas.size();i++){
-        //     int tank=gas[i];
-        //      if(tank<cost[i]){
-        //        ans=-1;
-        //        continue;
-        //     }
-        //    tank=tank-cost[i]+gas[(i+1)%n];
-        //    ans=check(gas,cost,i,(i+1)%n,tank,n);
-        //    if(ans!=-1){
-        //     break;
-        //    }
-        // }
-        // return ans;
-
-        //method 2
-        // int balance=0;
-        // int deficient=0;
-        // int start=0;
-        // int n=gas.size();
-        // for(int i=0;i<n;i++){
-        //     balance=balance+gas[i]-cost[i];
-        //     if(balance<0){
-        //         deficient=deficient+balance;
-        //         start=i+1;
-        //         balance=0;
-        //     }
-        // }
-        // if(balance+deficient>=0){
-        //     return start;
-        // }
-        // return -1;
-
-
-        //method 3
         int balance=0;
         int deficient=0;
-        int n=gas.size();
-        // deque<int>q;
-        queue<int>q;
-        for(int i=0;i<n;i++){
-            balance=balance+gas[i]-cost[i];
-            // q.push_back(i);
-            q.push(i);
-            if(balance<0){
-                deficient=deficient+balance;
-                // while(!q.empty())
-                // q.pop_back();
-                while(!q.empty()){
-                    q.pop();
-                }
-                balance=0;
-            }
+        int start=0;
+        for(int i=0;i<gas.size();i++){
+             if(balance+gas[i]<cost[i]){
+                  deficient+=balance+gas[i]-cost[i];
+                  start=i+1;
+                  balance=0;
+             }
+             else{
+                balance+=gas[i]-cost[i];
+             }
         }
-        if(balance+deficient>=0){
-            return q.front();
-        }
+        if(balance+deficient>=0)
+        return start;
         return -1;
     }
 };
