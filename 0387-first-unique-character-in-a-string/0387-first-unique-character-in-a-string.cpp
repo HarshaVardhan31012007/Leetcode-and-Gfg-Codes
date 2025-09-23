3a42b7e3b33d8057a84c7493c3d3349d136696e4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-23 22:05:22 +0530

    Time: 11 ms (57.66%), Space: 20.5 MB (5.03%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
+        // int arr[26]={0};
+        // for(int i=0;i<s.length();i++){
+        //     arr[s[i]-'a']++;
+        // }
+        // for(int i=0;i<s.length();i++){
+        //     if(arr[s[i]-'a']==1)
+        //     return i;
+        // }
+        // return -1;
+
+
+        queue<int>q;
         int arr[26]={0};
         for(int i=0;i<s.length();i++){
-            arr[s[i]-'a']++;
+               char ch=s[i];
+               arr[ch-'a']++;
+               q.push(i);
+               while(!q.empty()){
+                    int front=q.front();
+                    if(arr[s[front]-'a']==1)
+                    break;
+                    else
+                    q.pop();
+               }
         }
-        for(int i=0;i<s.length();i++){
-            if(arr[s[i]-'a']==1)
-            return i;
-        }
-        return -1;
+        return q.empty()?-1:q.front();
     }
 };
