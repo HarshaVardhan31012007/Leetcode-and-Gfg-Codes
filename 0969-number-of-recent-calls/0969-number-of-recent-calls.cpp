0982b00e7938d41da197d60520a282d8360d6a36
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-27 01:15:09 +0530

    Time: 1313 ms (5%), Space: 417.6 MB (6.63%) - LeetHub

---
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.cpp
@@ -0,0 +1,29 @@
+class RecentCounter {
+public:
+    stack<int>st;
+    stack<int>st1;
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        int count=1;
+        while(!st.empty()&&st.top()>=t-3000){
+            count++;
+            st1.push(st.top());
+            st.pop();
+        }
+        while(!st1.empty()){
+            st.push(st1.top());
+            st1.pop();
+        }
+        st.push(t);
+        return count;
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
