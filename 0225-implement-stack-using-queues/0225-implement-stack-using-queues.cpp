2a7ad2d22442c71a50a3f1d3ce7f89931c1be4a5
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-26 19:20:29 +0530

    Time: 0 ms (100%), Space: 9.6 MB (18.62%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,53 +1,35 @@
 class MyStack {
 public:
-// queue<int>q;
-queue<int>q1;
-queue<int>q2;
+    queue<int>q1;
+    queue<int>q2;
     MyStack() {
         
     }
     
     void push(int x) {
-    //    q.push(x);
-    //    for(int i=0;i<q.size()-1;i++){
-    //     int front=q.front();
-    //     q.pop();
-    //     q.push(front);
-    //    } 
-
-    q1.push(x);
-    while(!q2.empty()){
-        int e=q2.front();
-        q2.pop();
-        q1.push(e);
-    }
-    while(!q1.empty()){
-         int e=q1.front();
-        q1.pop();
-        q2.push(e);
-    }
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        q1.push(x);
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
     }
     
     int pop() {
-        // int top=q.front();
-        // q.pop();
-        // return top;
-
-        int top=q2.front();
-        q2.pop();
-        return top;
+        int ans=q1.front();
+        q1.pop();
+        return ans;
     }
     
     int top() {
-    //return q.front();
-
-    return q2.front();
+        return q1.front();
     }
     
     bool empty() {
-    //    return q.empty(); 
-
-    return q2.empty();
+        return q1.empty();
     }
 };
 
