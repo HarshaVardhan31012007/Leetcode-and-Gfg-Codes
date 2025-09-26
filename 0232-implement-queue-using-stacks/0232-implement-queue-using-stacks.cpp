9095a2ee313ff53878cf1702cba46918683dd30b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-26 18:56:20 +0530

    Time: 0 ms (100%), Space: 9.8 MB (27.19%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,71 +1,35 @@
 class MyQueue {
 public:
+    stack<int>s1;
+    stack<int>s2;
     MyQueue() {
         
     }
-    stack<int>s1;
-    stack<int>s2;
+    
     void push(int x) {
-    //   while(!s1.empty()){
-    //     int e=s1.top();
-    //     s1.pop();
-    //     s2.push(e);
-    //   } 
-    //   s1.push(x);
-    //   while(!s2.empty()){
-    //     int e=s2.top();
-    //     s2.pop();
-    //     s1.push(e);
-    //   }
-
-    s1.push(x);
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        s1.push(x);
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
     
     int pop() {
-        // int e=s1.top();
-        // s1.pop();
-        // return e;
-         
-         int ans;
-        if(!s2.empty()){
-           ans=s2.top();
-           s2.pop();
-        }
-        else{
-            while(!s1.empty()){
-                int e=s1.top();
-                s1.pop();
-                s2.push(e);
-            }
-            ans=s2.top();
-            s2.pop();
-        }
-        return ans;
+         int ans=s1.top();
+         s1.pop();
+         return ans;
     }
     
     int peek() {
-        // return s1.top();
-
-          int ans;
-        if(!s2.empty()){
-           ans=s2.top();
-        }
-        else{
-            while(!s1.empty()){
-                int e=s1.top();
-                s1.pop();
-                s2.push(e);
-            }
-            ans=s2.top();
-        }
-        return ans;
-
+        return s1.top();
     }
     
     bool empty() {
-        // return s1.empty();
-
-        return s1.empty()&&s2.empty();
+        return s1.empty();
     }
 };
 
