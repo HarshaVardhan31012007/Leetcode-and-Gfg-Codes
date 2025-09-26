e5ac34aac6499a5e46401b9c38b23d1838184954
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-26 19:21:32 +0530

    Time: 0 ms (100%), Space: 9.5 MB (18.62%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,21 +1,49 @@
 class MyStack {
 public:
+    // queue<int>q1;
+    // queue<int>q2;
+    // MyStack() {
+        
+    // }
+    
+    // void push(int x) {
+    //     while(!q1.empty()){
+    //         q2.push(q1.front());
+    //         q1.pop();
+    //     }
+    //     q1.push(x);
+    //     while(!q2.empty()){
+    //         q1.push(q2.front());
+    //         q2.pop();
+    //     }
+    // }
+    
+    // int pop() {
+    //     int ans=q1.front();
+    //     q1.pop();
+    //     return ans;
+    // }
+    
+    // int top() {
+    //     return q1.front();
+    // }
+    
+    // bool empty() {
+    //     return q1.empty();
+    // }
+
+
     queue<int>q1;
-    queue<int>q2;
     MyStack() {
         
     }
     
     void push(int x) {
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        q1.push(x);
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }
+       q1.push(x);
+       for(int i=0;i<q1.size()-1;i++){
+        q1.push(q1.front());
+        q1.pop();
+       }
     }
     
     int pop() {
