ffb16ce621bed57d80aea77185f2f7bc00a7f6e3
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-26 18:01:20 +0530

    Time: 18 ms (81.79%), Space: 109 MB (82.51%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<long long>P(n,0);
-        P[0]=nums[0];
-        for(int i=1;i<n;i++){
-            P[i]=P[i-1]+nums[i];
-        }
-        int minlength=INT_MAX;
-        deque<int>dq;
-        for(int j=0;j<n;j++){
-            //sum from starting to that index
-           if(P[j]>=k){
-            minlength=min(minlength,j+1);
-           }
-           //finding minimum length in case between we get ans 
-           while(!dq.empty()&&P[j]-P[dq.front()]>=k){
-            minlength=min(minlength,j-dq.front());
-            dq.pop_front();
-           }
-           //momotomic queue
-           while(!dq.empty()&&P[j]<=P[dq.back()]){
-            dq.pop_back();
-           }
-           dq.push_back(j);
-        }
-        return minlength==INT_MAX?-1:minlength;
-    }
+          deque<int>dq;
+
+          vector<long int>prefix(nums.size());
+          prefix[0]=nums[0];
+          for(int i=1;i<nums.size();i++){
+              prefix[i]=nums[i]+prefix[i-1];
+          }
+          int minLength=INT_MAX;
+          for(int j=0;j<nums.size();j++){
+              if(prefix[j]>=k) minLength=min(minLength,j+1);
+              while(!dq.empty()&&(prefix[j]-prefix[dq.front()]>=k)){
+                minLength=min(minLength,j-dq.front());
+                dq.pop_front();
+              }
+              while(!dq.empty()&&prefix[j]<=prefix[dq.back()])
+              dq.pop_back();
+              dq.push_back(j);
+          }
+          return minLength==INT_MAX?-1:minLength;
+    } 
 };
