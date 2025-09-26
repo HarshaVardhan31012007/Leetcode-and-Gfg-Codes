e0705747e69bcf4d04549b67a4b6bc8c09a30d95
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-27 01:20:45 +0530

    Time: 10 ms (88.28%), Space: 64.1 MB (66.87%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.cpp
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.cpp
@@ -1,24 +1,37 @@
 class RecentCounter {
 public:
-    stack<int>st;
-    stack<int>st1;
+    // stack<int>st;
+    // stack<int>st1;
+    // RecentCounter() {
+        
+    // }
+    
+    // int ping(int t) {
+    //     int count=1;
+    //     while(!st.empty()&&st.top()>=t-3000){
+    //         count++;
+    //         st1.push(st.top());
+    //         st.pop();
+    //     }
+    //     while(!st1.empty()){
+    //         st.push(st1.top());
+    //         st1.pop();
+    //     }
+    //     st.push(t);
+    //     return count;
+    // }
+
+   
+    queue<int>q;
     RecentCounter() {
         
     }
     
     int ping(int t) {
-        int count=1;
-        while(!st.empty()&&st.top()>=t-3000){
-            count++;
-            st1.push(st.top());
-            st.pop();
-        }
-        while(!st1.empty()){
-            st.push(st1.top());
-            st1.pop();
-        }
-        st.push(t);
-        return count;
+       while(!q.empty()&&q.front()<t-3000)
+       q.pop();
+       q.push(t);
+       return q.size();
     }
 };
 
