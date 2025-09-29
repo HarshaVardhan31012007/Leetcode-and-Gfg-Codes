01f533f06356be64204127802e3348bc44ec86ca
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-29 11:41:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (65.34%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    class Node{
-        public:
-        int data;
-        Node* next;
-        Node(int val){
-            this->data=val;
-            this->next=NULL;
-        }
-    };
+    // class Node{
+    //     public:
+    //     int data;
+    //     Node* next;
+    //     Node(int val){
+    //         this->data=val;
+    //         this->next=NULL;
+    //     }
+    // };
+    int solve(int n,int k){
+        if(n==1) return 1;
+        return (solve(n-1,k)+k-1)%n+1;
+    }
     int findTheWinner(int n, int k) {
         // queue<int>q;
         // for(int i=1;i<=n;i++){
@@ -25,25 +29,31 @@ public:
         // return q.front();
 
 
-        Node* head=new Node(1);
-        Node* it=head;
-        for(int i=2;i<=n;i++){
-            it->next=new Node(i);
-            it=it->next;
-        }
-        it->next=head;
-        Node* prev=it;// if k is 1 then we need this
-        it=head;
-        while(it->next!=it){
-            for(int i=0;i<k-1;i++){
-                prev=it;
-                it=it->next;
-            }
-            prev->next=it->next;
-            it->next=NULL;
-            delete it;
-            it=prev->next;
-        }
-        return it->data;
+
+
+        // Node* head=new Node(1);
+        // Node* it=head;
+        // for(int i=2;i<=n;i++){
+        //     it->next=new Node(i);
+        //     it=it->next;
+        // }
+        // it->next=head;
+        // Node* prev=it;// if k is 1 then we need this
+        // it=head;
+        // while(it->next!=it){
+        //     for(int i=0;i<k-1;i++){
+        //         prev=it;
+        //         it=it->next;
+        //     }
+        //     prev->next=it->next;
+        //     it->next=NULL;
+        //     delete it;
+        //     it=prev->next;
+        // }
+        // return it->data;
+
+
+        //Recursion
+        return solve(n,k);
     }
 };
