8af349d71cab00ae7064660ec479e2b2c2f496a2
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-29 10:24:37 +0530

    Time: 5 ms (41.13%), Space: 8.7 MB (27.2%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,18 +1,49 @@
 class Solution {
 public:
+    class Node{
+        public:
+        int data;
+        Node* next;
+        Node(int val){
+            this->data=val;
+            this->next=NULL;
+        }
+    };
     int findTheWinner(int n, int k) {
-        queue<int>q;
-        for(int i=1;i<=n;i++){
-            q.push(i);
+        // queue<int>q;
+        // for(int i=1;i<=n;i++){
+        //     q.push(i);
+        // }
+        // while(q.size()>1){
+        //     int count=k-1;
+        //     while(count--){
+        //         q.push(q.front());
+        //         q.pop();
+        //     }
+        //     q.pop();
+        // }
+        // return q.front();
+
+
+        Node* head=new Node(1);
+        Node* it=head;
+        for(int i=2;i<=n;i++){
+            it->next=new Node(i);
+            it=it->next;
         }
-        while(q.size()>1){
-            int count=k-1;
-            while(count--){
-                q.push(q.front());
-                q.pop();
+        it->next=head;
+        Node* prev=it;// if k is 1 then we need this
+        it=head;
+        while(it->next!=it){
+            for(int i=0;i<k-1;i++){
+                prev=it;
+                it=it->next;
             }
-            q.pop();
+            prev->next=it->next;
+            it->next=NULL;
+            delete it;
+            it=prev->next;
         }
-        return q.front();
+        return it->data;
     }
 };
