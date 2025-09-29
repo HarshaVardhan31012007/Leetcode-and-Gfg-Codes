d31a97593c536d7411f35de4dba2791b920e6677
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-29 11:42:49 +0530

    Time: 0 ms (100%), Space: 7.9 MB (80.24%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -9,9 +9,15 @@ public:
     //         this->next=NULL;
     //     }
     // };
+
+
+    // int solve(int n,int k){
+    //     if(n==1) return 1;
+    //     return (solve(n-1,k)+k-1)%n+1;
+    // }
     int solve(int n,int k){
-        if(n==1) return 1;
-        return (solve(n-1,k)+k-1)%n+1;
+        if(n==1) return 0;
+        return (solve(n-1,k)+k)%n;
     }
     int findTheWinner(int n, int k) {
         // queue<int>q;
@@ -54,6 +60,6 @@ public:
 
 
         //Recursion
-        return solve(n,k);
+        return solve(n,k)+1;
     }
 };
