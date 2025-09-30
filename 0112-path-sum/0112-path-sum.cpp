623e4fd84c64ac1ce276522173c9703cc02f58b1
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-30 17:32:19 +0530

    Time: 0 ms (100%), Space: 21.4 MB (95.04%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -11,41 +11,14 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode* root, int target,int sum){
-         //checking if empty or not
-         if(root==NULL){
-            return false;
-        }
-        //i not empty check for pathsum if it is leaf node
-        sum=sum+root->val;
-        if(root->left==NULL&&root->right==NULL){
-            if(sum==target){
-                return true;
-            }
-            return false;
-        }
-        bool leftans=solve(root->left,target,sum);
-        bool rightans=solve(root->right,target,sum);
-         return leftans||rightans;
-    }
     bool hasPathSum(TreeNode* root, int targetSum) {
-        // //cghecking if empty or not
-        //  if(root==NULL){
-        //     return false;
-        // }
-        // //i not empty check for sum if it is leaf node
-        // if(root->left==NULL&&root->right==NULL){
-        //     if(targetSum-root->val==0){
-        //         return true;
-        //     }
-        //     return false;
-        // }
-        // bool leftans=hasPathSum(root->left,targetSum-root->val);
-        // bool rightans=hasPathSum(root->right,targetSum-root->val);
-        // return leftans||rightans;
-
-
-        int sum=0;
-        return solve(root,targetSum,sum);
+        if(!root) return false;
+        if(!root->left&&!root->right){
+            targetSum-=root->val;
+            if(targetSum==0)
+            return true;
+            return false;
+        }
+        return hasPathSum(root->left,targetSum-root->val)||hasPathSum(root->right,targetSum-root->val);
     }
 };
