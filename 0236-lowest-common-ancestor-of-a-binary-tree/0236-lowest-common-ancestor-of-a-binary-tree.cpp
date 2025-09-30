12a04f68506a60d0d4dae54ee37328f57b33d45e
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-30 17:15:06 +0530

    Time: 312 ms (5.01%), Space: 17.2 MB (97.49%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,19 +9,33 @@
  */
 class Solution {
 public:
+    // TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    //     if(root==NULL) return NULL;
+    //     if(root==p||root==q) return root;
+    //     TreeNode* l=lowestCommonAncestor(root->left,p,q);
+    //     TreeNode* r=lowestCommonAncestor(root->right,p,q);
+    //     if(l!=NULL&&r!=NULL)
+    //     return root;
+    //     else if(l!=NULL){
+    //         return l;
+    //     }
+    //     else if(r!=NULL){
+    //         return r;
+    //     }
+    //     return NULL;
+    // }
+   
+
+    bool check(TreeNode* root,TreeNode* t){
+        if(!root) return 0;
+        return root==t||check(root->left,t)||check(root->right,t);
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL) return NULL;
         if(root==p||root==q) return root;
-        TreeNode* l=lowestCommonAncestor(root->left,p,q);
-        TreeNode* r=lowestCommonAncestor(root->right,p,q);
-        if(l!=NULL&&r!=NULL)
+        bool flag=check(root->left,p);
+        bool flag1=check(root->left,q);
+        if(flag&&flag1) return lowestCommonAncestor(root->left,p,q);
+        else if(!flag&&!flag1) return lowestCommonAncestor(root->right,p,q);
         return root;
-        else if(l!=NULL){
-            return l;
-        }
-        else if(r!=NULL){
-            return r;
-        }
-        return NULL;
     }
 };
