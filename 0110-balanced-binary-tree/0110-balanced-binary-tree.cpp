674c5629308401c06b4462f6f854f60f3af45126
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-30 13:40:22 +0530

    Time: 0 ms (100%), Space: 23.1 MB (23.47%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,18 +11,30 @@
  */
 class Solution {
 public:
-    int solve(TreeNode* root,bool &flag) {
+    // int solve(TreeNode* root,bool &flag) {
+    //     if(!root) return -1;
+    //     int l=solve(root->left,flag);
+    //     int r=solve(root->right,flag);
+    //     bool flag1=abs(l-r)<=1;
+    //     if(flag&&!flag1)
+    //     flag=0;
+    //     return 1+max(l,r);
+    // }
+    // bool isBalanced(TreeNode* root) {
+    //      bool flag=1;
+    //      solve(root,flag);
+    //      return flag;
+    // }
+
+
+    int solve(TreeNode* root) {
         if(!root) return -1;
-        int l=solve(root->left,flag);
-        int r=solve(root->right,flag);
-        bool flag1=abs(l-r)<=1;
-        if(flag&&!flag1)
-        flag=0;
+        int l=solve(root->left);
+        int r=solve(root->right);
         return 1+max(l,r);
     }
     bool isBalanced(TreeNode* root) {
-         bool flag=1;
-         solve(root,flag);
-         return flag;
+        if(!root) return true;
+        return isBalanced(root->left)&&isBalanced(root->right)&&abs(solve(root->left)-solve(root->right))<=1;
     }
 };
