e3c44f87dab1bf0f9586dc12f558cb9714331425
HARSHA VARDHAN <hv1900019@gmail.com>
2025-09-30 11:22:37 +0530

    Time: 0 ms (100%), Space: 19 MB (75.64%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,31 +11,8 @@
  */
 class Solution {
 public:
-    // void solve(TreeNode *root,int &len,int step){
-    //     if(root==NULL){
-    //         len=max(len,step);
-    //         return;
-    //     }
-    //     solve(root->left,len,step+1);
-    //     solve(root->right,len,step+1);
-    // }
-
-     int solve(TreeNode *root){
-        if(root==NULL){
-            return 0;
-        }
-        int leftans=solve(root->left);
-        int rightans=solve(root->right);
-        int finalans=max(leftans,rightans)+1;
-        return finalans;
-    }
-
     int maxDepth(TreeNode* root) {
-        // int len=INT_MIN;
-        // int step=0;
-        // solve(root,len,step);
-        // return len;
-
-        return solve(root);
+        if(!root) return 0;
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
     }
 };
