3b3e0e1ee69281114b0395dd07d932e8cefd501b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-02 21:17:34 +0530

    Time: 0 ms (100%), Space: 13.4 MB (16.2%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,53 +1,32 @@
 class Solution {
 public:
-    // void solve(vector<vector<int>>&triangle,int &ans,int sum,int i,int col){
-    //     if(i==triangle.size()){
-    //        ans=min(ans,sum);
-    //        return;
-    //     }
-        
-    //     solve(triangle,ans,sum+triangle[i][col],i+1,col);
-    //     solve(triangle,ans,sum+triangle[i][col],i+1,col+1);
-    
+    // int solve(vector<vector<int>>& triangle,int i,int j,vector<vector<int>>&dp){
+    //     if(i==triangle.size()) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int l=triangle[i][j]+solve(triangle,i+1,j,dp);
+    //     int r=triangle[i][j+1]+solve(triangle,i+1,j+1,dp);
+    //     return dp[i][j]=min(l,r);
     // }
-    // int minimumTotal(vector<vector<int>>& triangle) {  
-    //     int ans=INT_MAX;
-    //     int sum=0;
-    //     int col=0;
-    //     solve(triangle,ans,sum,0,col);
-    //     return ans;
-    // }
-
-    //method2
-    // int solve(vector<vector<int>>&triangle,int row,int col,vector<vector<int>>&dp){
-    //     // if(row==triangle.size()-1){
-    //     //    return triangle[row][col];
-    //     // }
-    //     if(row==triangle.size()) return 0;
-    //     if(dp[row][col]!=-1) return dp[row][col];
-    //     int left=triangle[row][col]+solve(triangle,row+1,col,dp);
-    //     int right=triangle[row][col]+solve(triangle,row+1,col+1,dp);
-    //     return dp[row][col]=min(left,right);
-         
-    // }
-    // int minimumTotal(vector<vector<int>>& triangle) {  
+    // int minimumTotal(vector<vector<int>>& triangle) {
+    //     int i=1;
+    //     int j=0;
     //     vector<vector<int>>dp(triangle.size()+1,vector<int>(triangle.size()+1,-1));
-    //     return solve(triangle,0,0,dp);
+    //    return triangle[0][0]+solve(triangle,i,j,dp);
     // }
 
 
-    int solve(vector<vector<int>>&triangle){
-         vector<vector<int>>dp(triangle.size()+1,vector<int>(triangle.size()+1,0));
-        for(int row=triangle.size()-1;row>=0;row--){
-         for(int col=row;col>=0;col--){
-        int left=triangle[row][col]+dp[row+1][col];
-        int right=triangle[row][col]+dp[row+1][col+1];
-        dp[row][col]=min(left,right);
-         }
+     int solve(vector<vector<int>>&dp,vector<vector<int>>& triangle){
+        for(int i=triangle.size()-1;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int l=triangle[i][j]+dp[i+1][j];
+                int r=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(l,r);
+            }
         }
         return dp[0][0];
     }
-    int minimumTotal(vector<vector<int>>& triangle) {  
-        return solve(triangle);
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<vector<int>>dp(triangle.size()+1,vector<int>(triangle.size()+1,0));
+       return solve(dp,triangle);
     }
 };
