c105390ca73b816afd557f87d59bbf8a545cd70a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-03 12:34:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (12.69%) - LeetHub

--- 3336-water-bottles-ii/3336-water-bottles-ii.cpp
+++ 3336-water-bottles-ii/3336-water-bottles-ii.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     int maxBottlesDrunk(int numBottles, int numExchange) {
+        // int ans=0;
+        // int empty=0;
+        // while(numBottles!=0||empty>=numExchange){
+        //     if(empty>=numExchange){
+        //         empty-=numExchange;
+        //         numBottles+=1;
+        //         numExchange+=1;
+        //     }
+        //     else{
+        //        empty+=numBottles;
+        //        ans+=numBottles;
+        //        numBottles=0;
+        //     }
+        // }
+        // return ans;
+
+
         int ans=0;
-        int empty=0;
-        while(numBottles!=0||empty>=numExchange){
-            if(empty>=numExchange){
-                empty-=numExchange;
-                numBottles+=1;
-                numExchange+=1;
-            }
-            else{
-               empty+=numBottles;
-               ans+=numBottles;
-               numBottles=0;
-            }
+        while(numBottles>=numExchange){
+            ans+=numExchange;
+            //numBottles-=numExchange-1;
+            numBottles=numBottles-numExchange+1;
+            numExchange+=1;
         }
-        return ans;
+        return ans+numBottles;
     }
 };
