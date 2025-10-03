29ae7ea60207b284bc863272e3971dfc02a002ff
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-03 11:34:13 +0530

    Time: 0 ms (100%), Space: 8 MB (10.38%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
+        // int ans=0;
+        // int empty=0;
+        // while(numBottles!=0){
+        // ans+=numBottles;
+        // int newnumBottles=(empty+numBottles)/numExchange;
+        // empty=(empty+numBottles)%numExchange;
+        // numBottles=newnumBottles;
+        // }
+        // return ans;
+
+
         int ans=0;
-        int empty=0;
-        while(numBottles!=0){
-        ans+=numBottles;
-        int newnumBottles=(empty+numBottles)/numExchange;
-        empty=(empty+numBottles)%numExchange;
-        numBottles=newnumBottles;
+        while(numBottles>=numExchange){
+             ans+=numExchange;
+             numBottles-=numExchange;
+             numBottles+=1;
         }
-        return ans;
+        return ans+numBottles;
     }
 };
