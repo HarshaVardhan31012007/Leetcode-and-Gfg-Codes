e46455c49cb3f82e1ed8d001266691cc101f06e4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-04 16:46:29 +0530

    Time: 3 ms (54.66%), Space: 11.3 MB (41.07%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
-    // int solve(vector<int>& values,int s,int e,vector<vector<int>>&dp){
-    //     if(s+1==e) return 0;
-    //     if(dp[s][e]!=-1) return dp[s][e];
-    //     int ans=INT_MAX;
-    //     for(int j=s+1;j<e;j++){
-    //         ans=min(ans,values[s]*values[e]*values[j]+solve(values,s,j,dp)+solve(values,j,e,dp));
-    //     }
-    //     return dp[s][e]=ans;
-    // }
-    // int minScoreTriangulation(vector<int>& values) {
-    //    vector<vector<int>>dp(values.size()+1,vector<int>(values.size()+1,-1));
-    //    return solve(values,0,values.size()-1,dp);
-    // }
-
-     int solve(vector<int>& values,vector<vector<int>>&dp){
-        for(int s=values.size()-1;s>=0;s--){
-            for(int e=s+2;e<values.size();e++){
-                int ans=INT_MAX;
-                for(int j=s+1;j<e;j++){
-                    ans=min(ans,values[s]*values[e]*values[j]+dp[s][j]+dp[j][e]);
-                }
-                dp[s][e]=ans;
-            }
+    int solve(vector<int>&values,int s,int e,vector<vector<int>>&dp){
+       if(e-s+1<=2) return 0;
+       if(dp[s][e]!=-1) return dp[s][e];
+        int maxi=INT_MAX;;
+        for(int i=s+1;i<e;i++){
+             maxi=min(maxi,values[s]*values[i]*values[e]+solve(values,s,i,dp)+solve(values,i,e,dp));
         }
-        return dp[0][values.size()-1];
+        return dp[s][e]=maxi;
     }
     int minScoreTriangulation(vector<int>& values) {
-       vector<vector<int>>dp(values.size()+1,vector<int>(values.size()+1,0));
-       return solve(values,dp);
+        vector<vector<int>>dp(values.size()+1,vector<int>(values.size()+1,-1));
+        return solve(values,0,values.size()-1,dp);
     }
 };
