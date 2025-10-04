99a66c357574a6ba4a9d27b372403ae56b04f07e
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-04 22:29:29 +0530

    Time: 0 ms (100%), Space: 9.1 MB (35.95%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
+        // int i=rowIndex;
+        // vector<int>temp(1,1);
+        // long long int c=1;
+        // for(int j=1;j<=i;j++){
+        //     c=c*(i-j+1)/j;
+        //     temp.push_back(c);
+        // }
+        // return temp;
+
         int i=rowIndex;
-        vector<int>temp(1,1);
+        i++;
+        vector<int>temp;
         long long int c=1;
         for(int j=1;j<=i;j++){
-            c=c*(i-j+1)/j;
             temp.push_back(c);
+            c=c*(i-j)/j;
         }
         return temp;
     }
