936e25199297529a78e83f432cb5d328269f1f48
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-04 22:23:12 +0530

    Time: 373 ms (14.6%), Space: 382.4 MB (22.53%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
-        while(nums.size()>1){
-            for(int i=0;i<nums.size()-1;i++){
-                nums[i]=(nums[i]+nums[i+1])%10;
-            }
-            nums.pop_back();
-        }
-        return nums[0];
 
         // queue<int>q;
         // for(int i=0;i<nums.size();i++){
@@ -23,5 +16,32 @@ public:
         //     q.pop();
         // }
         // return q.front();
+          
+        vector<int>temp;
+        while(nums.size()>1){
+            for(int i=0;i<nums.size()-1;i++){
+                temp.push_back((nums[i]+nums[i+1])%10);
+            }
+            nums=move(temp);
+        }
+        return nums[0];
+        
+         // while(nums.size()>1){
+        //     for(int i=0;i<nums.size()-1;i++){
+        //         nums[i]=(nums[i]+nums[i+1])%10;
+        //     }
+        //     nums.pop_back();
+        // }
+        // return nums[0];
+
+            // int n=nums.size();
+            // int ans=nums[0];
+            // int i=n-1;
+            // long long int c=1;
+            // for(int j=1;j<=i;j++){
+            //     c=c*(i-j+1)/j;
+            //     ans+=c*nums[j];
+            // }
+            // return ans%10;
     }
 };
