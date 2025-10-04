261503ccf036e53f8d2231dc3101a47c8f31d21b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-04 09:15:01 +0530

    Time: 0 ms (100%), Space: 62.9 MB (48.17%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n=height.size();
-        //   int maxi=INT_MIN;
-        // for(int i=0;i<n;i++){
-        //    for(int j=i+1;j<n;j++){
-        //     int h=min(height[i],height[j]);
-        //     int curr=(j-i)*h;
-        //     maxi=max(maxi,curr);
-        //    }
-        // }
-        // return maxi;
-
-        //method 2
-        int i=0;
-        int j=n-1;
-        int ans=INT_MIN;
-        while(i<j){
-            int w=j-i;
-            int h=min(height[i],height[j]);
-            int v=w*h;
-            ans=max(ans,v);
-            (height[i]<height[j])?i++:j--;
+        int l=0;
+        int r=height.size()-1;
+        int maxi=INT_MIN;
+        while(l<r){
+            if(height[l]<height[r]){
+                maxi=max(maxi,height[l]*(r-l));
+                l++;
+            }
+            else{
+                maxi=max(maxi,height[r]*(r-l));
+                r--;
+            }
         }
-        return ans;
+        return maxi;
     }
 };
