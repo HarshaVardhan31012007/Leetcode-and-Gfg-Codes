ade0a69f8577bbc9b3af76fa1a3dfd4bae070d5f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-04 12:25:26 +0530

    Time: 1 ms (46.39%), Space: 10.5 MB (63%) - LeetHub

--- 0830-largest-triangle-area/0830-largest-triangle-area.cpp
+++ 0830-largest-triangle-area/0830-largest-triangle-area.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<points.size();i++){
             for(int j=i+1;j<points.size();j++){
                 for(int k=j+1;k<points.size();k++){
-                    ans=max(ans,abs(0.5*(points[i][0]*(points[j][1]-points[k][1])+points[j][0]*(points[k][1]-points[i][1])+points[k][0]*(points[i][1]-points[j][1]))));
+                    ans=max(ans,0.5*abs(points[i][0]*(points[j][1]-points[k][1])+points[j][0]*(points[k][1]-points[i][1])+points[k][0]*(points[i][1]-points[j][1])));
                 }
             }
         }
