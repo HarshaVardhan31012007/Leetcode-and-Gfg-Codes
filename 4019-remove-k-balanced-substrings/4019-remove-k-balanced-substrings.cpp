719f17e50a9c4c68d3aa02b17c08cd457013c6ba
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-05 11:46:46 +0530

    Time: 117 ms (14.29%), Space: 31.5 MB (71.43%) - LeetHub

--- 4019-remove-k-balanced-substrings/4019-remove-k-balanced-substrings.cpp
+++ 4019-remove-k-balanced-substrings/4019-remove-k-balanced-substrings.cpp
@@ -38,33 +38,52 @@ public:
         // return str;
 
 
+          string temp="";
+         for(int i=0;i<k;i++){
+             temp+='(';
+         }
+         for(int i=0;i<k;i++){
+             temp+=')';
+         }
+        //O(m*n)//s.compare(idx,len,compare_string) saves space
+        int j=0;
+        for(int i=0;i<s.length();i++){
+            s[j]=s[i];
+            while(j>=2*k-1&&!s.compare(j-2*k+1,2*k,temp)){
+               j-=2*k;
+            }
+            j++;
+        }
+        return s.substr(0,j);
 
-        stack<pair<char,int>>st;
-        string ans="";
-        for(auto &ch:s){
-              if(!st.empty()&&st.top().first==ch)
-              st.top().second++;
-              else
-              st.push({ch,1});
-              if(st.size()>=2){
-                auto top2=st.top();st.pop();
-                auto& top1=st.top();
-                if(top1.first=='('&&top2.first==')'&&top1.second>=k&&top2.second>=k){
-                    top1.second-=k;
-                    top2.second-=k;
-                    if(!top1.second)
-                    st.pop();
-                }
-                else
-                st.push(top2);
-              }
-        }
-        while(!st.empty()){
-            while(st.top().second--)
-            ans+=st.top().first;
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+
+
+        // stack<pair<char,int>>st;
+        // string ans="";
+        // for(auto &ch:s){
+        //       if(!st.empty()&&st.top().first==ch)
+        //       st.top().second++;
+        //       else
+        //       st.push({ch,1});
+        //       if(st.size()>=2){
+        //         auto top2=st.top();st.pop();
+        //         auto& top1=st.top();
+        //         if(top1.first=='('&&top2.first==')'&&top1.second>=k&&top2.second>=k){
+        //             top1.second-=k;
+        //             top2.second-=k;
+        //             if(!top1.second)
+        //             st.pop();
+        //         }
+        //         else
+        //         st.push(top2);
+        //       }
+        // }
+        // while(!st.empty()){
+        //     while(st.top().second--)
+        //     ans+=st.top().first;
+        //     st.pop();
+        // }
+        // reverse(ans.begin(),ans.end());
+        // return ans;
     }
 };
