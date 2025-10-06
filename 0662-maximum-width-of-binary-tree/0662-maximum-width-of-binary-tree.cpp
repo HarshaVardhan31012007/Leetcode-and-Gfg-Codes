00886c9c9106bb7d0f52985d2852a845ea586c1a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-07 01:08:23 +0530

    Time: 0 ms (100%), Space: 19.9 MB (11.87%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -12,21 +12,55 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
+        //MLE
+        // deque<TreeNode*>q;
+        // q.push_back(root);
+        // int width=1;
+        // while(!q.empty()){
+        //     int size=q.size();
+        //     for(int i=0;i<size;i++){
+        //         auto front=q.front();
+        //         q.pop_front();
+        //         if(front==NULL){
+        //             q.push_back(NULL);
+        //             q.push_back(NULL);
+        //         }
+        //         else{
+        //             q.push_back(front->left);
+        //             q.push_back(front->right);
+        //         }
+        //     }
+        //     while(!q.empty()&&q.back()==NULL){
+        //         q.pop_back();
+        //     }
+        //     while(!q.empty()&&q.front()==NULL){
+        //         q.pop_front();
+        //     }
+        //     if(q.front()!=NULL&&q.back()!=NULL)
+        //     width=max(width,(int)q.size());
+        // }
+        // return width;
+
+
+
         queue<pair<TreeNode*,unsigned long long>>q;
+        unsigned long long int width=1;
         q.push({root,1});
-        unsigned long long maxi=0;
         while(!q.empty()){
-             int size=q.size();
-             maxi=max(maxi,q.back().second-q.front().second+1);
-             for(int i=0;i<size;i++){
-                 auto front=q.front();
-                 q.pop();
-                 if(front.first->left)
-                 q.push({front.first->left,2*front.second});
-                 if(front.first->right)
-                 q.push({front.first->right,2*front.second+1});
-             }
+            unsigned long long int size=q.size();
+            for(unsigned long long int i=0;i<size;i++){
+            auto front=q.front().first;
+            unsigned long long idx=q.front().second;
+            q.pop();
+            if(front->left)
+            q.push({front->left,2*idx});
+            if(front->right)
+            q.push({front->right,2*idx+1});
+            }
+            if(q.size()>=2){
+                width=max(width,q.back().second-q.front().second+1);
+            }
         }
-        return maxi;
+        return width;
     }
 };
