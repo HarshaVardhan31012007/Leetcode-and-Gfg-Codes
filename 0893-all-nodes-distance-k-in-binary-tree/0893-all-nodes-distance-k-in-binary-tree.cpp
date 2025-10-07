cebd4cfd6f7d8c32c1102c1c6390f8e3f7723949
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-07 21:55:17 +0530

    Time: 4 ms (64.19%), Space: 15.3 MB (69.04%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -56,48 +56,95 @@ public:
     // }
 
 
-
-
-    void preorder(TreeNode* root,unordered_map<int,list<int>>&adjList){
-        if(!root) return;
-        preorder(root->left,adjList);
-        preorder(root->right,adjList);
+     void createPmap(TreeNode* root,map<TreeNode*,TreeNode*>&parent){
         if(root->left){
-            adjList[root->val].push_back(root->left->val);
-            adjList[root->left->val].push_back(root->val);
+            parent[root->left]=root;
+            createPmap(root->left,parent);
         }
         if(root->right){
-            adjList[root->val].push_back(root->right->val);
-            adjList[root->right->val].push_back(root->val);
+            parent[root->right]=root;
+            createPmap(root->right,parent);
         }
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-       unordered_map<int,list<int>>adjList;
-       preorder(root,adjList);
-       vector<int>ans;
-       //bfs
-       queue<int>q;
-       unordered_map<int,bool>visited;
-       q.push(target->val);
-       visited[target->val]=true;
-       while(!q.empty()&&k>0){
+        map<TreeNode*,TreeNode*>parent;
+        createPmap(root,parent);
+        queue<TreeNode*>q;
+        unordered_map<TreeNode*,bool>visited;
+        q.push(target);
+        visited[target]=1;
+        vector<int>ans;
+        while(!q.empty()&&k>0){
             int size=q.size();
             for(int i=0;i<size;i++){
-                auto front=q.front();
-                q.pop();
-                for(auto &adj:adjList[front]){
-                    if(!visited[adj]){
-                        visited[adj]=true;
-                        q.push(adj);
-                    }
-                }
+            auto front=q.front();
+            q.pop();
+            if(front->left&&!visited[front->left]){
+                visited[front->left]=true;
+                q.push(front->left);
+            }
+            if(front->right&&!visited[front->right]){
+                visited[front->right]=true;
+                q.push(front->right);
+            }
+            if(parent[front]&&!visited[parent[front]]){
+                visited[parent[front]]=true;
+                q.push(parent[front]);
+            }
             }
             k--;
-       }
-       while(!q.empty()){
-          ans.push_back(q.front());
-          q.pop();
-       }
-       return ans;
+        }
+           while(!q.empty()){
+                ans.push_back(q.front()->val);
+                q.pop();
+           }
+           return ans;
     }
+
+
+
+
+    // void preorder(TreeNode* root,unordered_map<int,list<int>>&adjList){
+    //     if(!root) return;
+    //     preorder(root->left,adjList);
+    //     preorder(root->right,adjList);
+    //     if(root->left){
+    //         adjList[root->val].push_back(root->left->val);
+    //         adjList[root->left->val].push_back(root->val);
+    //     }
+    //     if(root->right){
+    //         adjList[root->val].push_back(root->right->val);
+    //         adjList[root->right->val].push_back(root->val);
+    //     }
+    // }
+    // vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        //graph
+    //    unordered_map<int,list<int>>adjList;
+    //    preorder(root,adjList);
+    //    vector<int>ans;
+    //    //bfs
+    //    queue<int>q;
+    //    unordered_map<int,bool>visited;
+    //    q.push(target->val);
+    //    visited[target->val]=true;
+    //    while(!q.empty()&&k>0){
+    //         int size=q.size();
+    //         for(int i=0;i<size;i++){
+    //             auto front=q.front();
+    //             q.pop();
+    //             for(auto &adj:adjList[front]){
+    //                 if(!visited[adj]){
+    //                     visited[adj]=true;
+    //                     q.push(adj);
+    //                 }
+    //             }
+    //         }
+    //         k--;
+    //    }
+    //    while(!q.empty()){
+    //       ans.push_back(q.front());
+    //       q.pop();
+    //    }
+    //    return ans;
+    // }
 };
