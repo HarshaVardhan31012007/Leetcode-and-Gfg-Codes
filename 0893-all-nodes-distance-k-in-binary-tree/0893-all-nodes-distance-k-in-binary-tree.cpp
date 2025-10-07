b1db947b4c4a268eeb691d9f0d84989f9f716233
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-07 21:07:31 +0530

    Time: 6 ms (37.7%), Space: 15.2 MB (83.07%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,52 +9,49 @@
  */
 class Solution {
 public:
-    void create(TreeNode *root,unordered_map<TreeNode *,TreeNode*>&mpp){
-        if(!root) return;
+    void createPmap(TreeNode* root,map<TreeNode*,TreeNode*>&parent){
         if(root->left){
-        mpp[root->left]=root;
-        create(root->left,mpp);
+            parent[root->left]=root;
+            createPmap(root->left,parent);
         }
-        if(root->right){ 
-        mpp[root->right]=root;
-        create(root->right,mpp);
+        if(root->right){
+            parent[root->right]=root;
+            createPmap(root->right,parent);
         }
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode *,TreeNode*>mpp;
-        create(root,mpp);
-        mpp[root]=NULL;
+        map<TreeNode*,TreeNode*>parent;
+        createPmap(root,parent);
+        queue<pair<TreeNode*,int>>q;
         unordered_map<TreeNode*,bool>visited;
-        visited[target]=true;
-        queue<TreeNode*>q;
-        q.push(target);
-        int l=0;
-        while(l!=k){//!q.empty()
-            int size=q.size();
-            //if(i==k) break;
-            l++;
-            for(int i=0;i<size;i++){
-                auto front=q.front();
-                q.pop();
-                if(front->left&&visited.find(front->left)==visited.end()){
-                    q.push(front->left);
-                    visited[front->left]=true;
-                }
-                if(front->right&&visited.find(front->right)==visited.end()){
-                    q.push(front->right);
-                    visited[front->right]=true;
-                }
-                if(mpp[front]&&visited.find(mpp[front])==visited.end()){
-                    q.push(mpp[front]);
-                    visited[mpp[front]]=true;
-                }
-            }
-        }
+        q.push({target,0});
+        visited[target]=1;
         vector<int>ans;
-        while(!q.empty()){
-            ans.push_back(q.front()->val);
+        while(!q.empty()&&k>0){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+            auto front=q.front().first;
+            int dist=q.front().second;
             q.pop();
+            if(front->left&&!visited[front->left]){
+                visited[front->left]=true;
+                q.push({front->left,dist+1});
+            }
+            if(front->right&&!visited[front->right]){
+                visited[front->right]=true;
+                q.push({front->right,dist+1});
+            }
+            if(parent[front]&&!visited[parent[front]]){
+                visited[parent[front]]=true;
+                q.push({parent[front],dist+1});
+            }
+            }
+            k--;
         }
-        return ans;
+           while(!q.empty()){
+                ans.push_back(q.front().first->val);
+                q.pop();
+           }
+           return ans;
     }
 };
