3e7361c9b4fb88ba1cede81d3d445338849ca280
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-07 21:50:27 +0530

    Time: 4 ms (64.19%), Space: 17.2 MB (5.3%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,49 +9,95 @@
  */
 class Solution {
 public:
-    void createPmap(TreeNode* root,map<TreeNode*,TreeNode*>&parent){
+    // void createPmap(TreeNode* root,map<TreeNode*,TreeNode*>&parent){
+    //     if(root->left){
+    //         parent[root->left]=root;
+    //         createPmap(root->left,parent);
+    //     }
+    //     if(root->right){
+    //         parent[root->right]=root;
+    //         createPmap(root->right,parent);
+    //     }
+    // }
+    // vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+    //     map<TreeNode*,TreeNode*>parent;
+    //     createPmap(root,parent);
+    //     queue<pair<TreeNode*,int>>q;
+    //     unordered_map<TreeNode*,bool>visited;
+    //     q.push({target,0});
+    //     visited[target]=1;
+    //     vector<int>ans;
+    //     while(!q.empty()&&k>0){
+    //         int size=q.size();
+    //         for(int i=0;i<size;i++){
+    //         auto front=q.front().first;
+    //         int dist=q.front().second;
+    //         q.pop();
+    //         if(front->left&&!visited[front->left]){
+    //             visited[front->left]=true;
+    //             q.push({front->left,dist+1});
+    //         }
+    //         if(front->right&&!visited[front->right]){
+    //             visited[front->right]=true;
+    //             q.push({front->right,dist+1});
+    //         }
+    //         if(parent[front]&&!visited[parent[front]]){
+    //             visited[parent[front]]=true;
+    //             q.push({parent[front],dist+1});
+    //         }
+    //         }
+    //         k--;
+    //     }
+    //        while(!q.empty()){
+    //             ans.push_back(q.front().first->val);
+    //             q.pop();
+    //        }
+    //        return ans;
+    // }
+
+
+
+
+    void preorder(TreeNode* root,unordered_map<int,list<int>>&adjList){
+        if(!root) return;
+        preorder(root->left,adjList);
+        preorder(root->right,adjList);
         if(root->left){
-            parent[root->left]=root;
-            createPmap(root->left,parent);
+            adjList[root->val].push_back(root->left->val);
+            adjList[root->left->val].push_back(root->val);
         }
         if(root->right){
-            parent[root->right]=root;
-            createPmap(root->right,parent);
+            adjList[root->val].push_back(root->right->val);
+            adjList[root->right->val].push_back(root->val);
         }
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        map<TreeNode*,TreeNode*>parent;
-        createPmap(root,parent);
-        queue<pair<TreeNode*,int>>q;
-        unordered_map<TreeNode*,bool>visited;
-        q.push({target,0});
-        visited[target]=1;
-        vector<int>ans;
-        while(!q.empty()&&k>0){
+       unordered_map<int,list<int>>adjList;
+       preorder(root,adjList);
+       vector<int>ans;
+       //bfs
+       queue<int>q;
+       unordered_map<int,bool>visited;
+       q.push(target->val);
+       visited[target->val]=true;
+       while(!q.empty()&&k>0){
             int size=q.size();
             for(int i=0;i<size;i++){
-            auto front=q.front().first;
-            int dist=q.front().second;
-            q.pop();
-            if(front->left&&!visited[front->left]){
-                visited[front->left]=true;
-                q.push({front->left,dist+1});
-            }
-            if(front->right&&!visited[front->right]){
-                visited[front->right]=true;
-                q.push({front->right,dist+1});
-            }
-            if(parent[front]&&!visited[parent[front]]){
-                visited[parent[front]]=true;
-                q.push({parent[front],dist+1});
-            }
+                auto front=q.front();
+                q.pop();
+                for(auto &adj:adjList[front]){
+                    if(!visited[adj]){
+                        visited[adj]=true;
+                        q.push(adj);
+                    }
+                }
             }
             k--;
-        }
-           while(!q.empty()){
-                ans.push_back(q.front().first->val);
-                q.pop();
-           }
-           return ans;
+       }
+       while(!q.empty()){
+          ans.push_back(q.front());
+          q.pop();
+       }
+       return ans;
     }
 };
