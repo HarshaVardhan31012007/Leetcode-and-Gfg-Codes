1fd57bb765e3fc15230141f83206b524d3c4003f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-07 18:41:05 +0530

    Time: 131 ms (36.72%), Space: 114.8 MB (36.16%) - LeetHub

---
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rains) {
+        unordered_map<int,int>mpp;
+        set<int>zeros;
+        vector<int>ans(rains.size(),-1);
+        for(int i=0;i<rains.size();i++){
+            if(rains[i]==0)
+            zeros.insert(i);
+            else{
+                if(mpp.find(rains[i])!=mpp.end()){
+                    if(zeros.empty()){
+                        return {};
+                    }
+                    else{
+                        auto it=zeros.upper_bound(mpp[rains[i]]);
+                        if(it==zeros.end())
+       return {};
+                    ans[*it]=rains[i];
+                   zeros.erase(it);
+                        mpp[rains[i]]=i;
+                    }
+                }
+                else{
+                    mpp[rains[i]]=i;
+                }
+            }
+        }
+        for(auto &each:zeros){
+            ans[each]=2;//any number you can write
+        }
+        return ans;
+    }
+};
