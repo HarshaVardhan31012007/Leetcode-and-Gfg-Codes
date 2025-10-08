ed24dbae6e0551028b8067c55784e616c308cd26
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-08 14:02:48 +0530

    Time: 40 ms (75.85%), Space: 128.3 MB (25.75%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -39,21 +39,19 @@ public:
     // }
 
 
-
-
-     int binarysearch(vector<int>&potions,long long success,int spell){
+    int binarysearch(vector<int>&potions,long long success,int spell){
         int i=0;
-        int j=potions.size();//imp here n-1
-        while(i<j){
+        int j=potions.size()-1;
+        while(i<=j){
             int mid=i+(j-i)/2;
             long long p=spell*((long long)potions[mid]);
             if(p>=success){
-                j=mid;
+                j=mid-1;
             }
             else
             i=mid+1;
         }
-        return j;
+        return i;
      }
      vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
          sort(potions.begin(),potions.end());
@@ -64,4 +62,32 @@ public:
          }
          return ans;
     }
+
+
+
+
+
+    //  int binarysearch(vector<int>&potions,long long success,int spell){
+    //     int i=0;
+    //     int j=potions.size();//imp here n-1
+    //     while(i<j){
+    //         int mid=i+(j-i)/2;
+    //         long long p=spell*((long long)potions[mid]);
+    //         if(p>=success){
+    //             j=mid;
+    //         }
+    //         else
+    //         i=mid+1;
+    //     }
+    //     return j;
+    //  }
+    //  vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+    //      sort(potions.begin(),potions.end());
+    //      vector<int>ans;
+    //      for(int i=0;i<spells.size();i++){
+    //          int idx=binarysearch(potions,success,spells[i]);
+    //          ans.push_back(potions.size()-idx);
+    //      }
+    //      return ans;
+    // }
 };
