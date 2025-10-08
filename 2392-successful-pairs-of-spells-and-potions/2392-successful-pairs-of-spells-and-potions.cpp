6d15ee4fa43387e767dfdd5f2036989a145c8c58
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-08 12:57:56 +0530

    Time: 47 ms (39.82%), Space: 128.2 MB (42.11%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -4,7 +4,7 @@ public:
          sort(potions.begin(),potions.end());
          vector<int>ans;
          for(int i=0;i<spells.size();i++){
-              int count=potions.end()-lower_bound(potions.begin(),potions.end(),((double)success)/spells[i]);
+              int count=potions.end()-lower_bound(potions.begin(),potions.end(),((double)success)/spells[i]);// double is imp because potions 1 2 3 4 5 and spells 5 1 3 if you take 7/5=1 but here 1 should not accept so take double
               ans.push_back(count);
          }
          return ans;
