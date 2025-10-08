c669d0f7c2ff8708b40bdfd8c7a359ca34ed296a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-08 13:20:24 +0530

    Time: 47 ms (39.82%), Space: 128.4 MB (25.75%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -12,6 +12,35 @@ public:
 
 
 
+    //  int binarysearch(vector<int>&potions,long long success,int spell){
+    //     int i=0;
+    //     int j=potions.size()-1;
+    //     int idx=potions.size();
+    //     while(i<=j){
+    //         int mid=i+(j-i)/2;
+    //         long long p=spell*((long long)potions[mid]);
+    //         if(p>=success){
+    //             idx=mid;
+    //             j=mid-1;
+    //         }
+    //         else
+    //         i=mid+1;
+    //     }
+    //     return idx;
+    //  }
+    //  vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+    //      sort(potions.begin(),potions.end());
+    //      vector<int>ans;
+    //      for(int i=0;i<spells.size();i++){
+    //          int idx=binarysearch(potions,success,spells[i]);
+    //          ans.push_back(potions.size()-idx);
+    //      }
+    //      return ans;
+    // }
+
+
+
+
      int binarysearch(vector<int>&potions,long long success,int spell){
         int i=0;
         int j=potions.size()-1;
