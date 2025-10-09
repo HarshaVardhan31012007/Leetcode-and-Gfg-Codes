c989a2ecdfb79ee03bbb1bc24860532e71c65924
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-09 19:49:50 +0530

    Time: 115 ms (96.97%), Space: 45.5 MB (43.43%) - LeetHub

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.cpp
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -18,7 +18,23 @@ public:
         // }
         // return start;
 
-
+         
+        // int n=skill.size();
+        // int m=mana.size();
+        // vector<long long int>prefix(n,0);
+        // prefix[0]=skill[0];
+        // for(int i=1;i<n;i++){
+        //     prefix[i]=(long long)skill[i]+prefix[i-1];
+        // }
+        // long long int start=0;
+        // for(int j=0;j<m-1;j++){
+        //     long long int currstart=start;
+        //     for(int i=0;i<n;i++){
+        //         long long int time=currstart+(prefix[i]*(long long)mana[j]);
+        //         start=max(start,time-((i>0?prefix[i-1]:0)*(long long)mana[j+1]));
+        //     }
+        // }
+        // return (long long)start+(prefix[n-1]*(long long)mana[m-1]);
 
         int n=skill.size();
         int m=mana.size();
@@ -30,9 +46,10 @@ public:
         long long int start=0;
         for(int j=0;j<m-1;j++){
             long long int currstart=start;
-            for(int i=0;i<n;i++){
+            start=currstart+prefix[0]*mana[j];
+            for(int i=1;i<n;i++){
                 long long int time=currstart+(prefix[i]*(long long)mana[j]);
-                start=max(start,time-((i>0?prefix[i-1]:0)*(long long)mana[j+1]));
+                start=max(start,time-(prefix[i-1]*(long long)mana[j+1]));
             }
         }
         return (long long)start+(prefix[n-1]*(long long)mana[m-1]);
