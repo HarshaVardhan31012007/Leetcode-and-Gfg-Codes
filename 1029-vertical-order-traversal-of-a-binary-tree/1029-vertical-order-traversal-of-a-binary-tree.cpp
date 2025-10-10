66bb2ac8f65dba819aa9381b13b03e4c7ddc45c7
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-10 23:43:46 +0530

    Time: 3 ms (38.9%), Space: 15.8 MB (63.09%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -45,7 +45,7 @@ public:
     //     solve(root->left,v,l+1,x-1,minx,maxx,lmax);
     //     solve(root->right,v,l+1,x+1,minx,maxx,lmax);
     // }
-    // vector<vector<int>> verticalTraversal(TreeNode* root) {
+     vector<vector<int>> verticalTraversal(TreeNode* root) {
     //     vector<s>v;
     //     vector<vector<int>>ans;
     //     int l=0;
@@ -71,38 +71,38 @@ public:
     // }
 
     //method 2
-    void solve(TreeNode *root,map<int,map<int,vector<int>>>&mpp,int r,int c){
-        if(root==NULL){
-            return;
-        }
-        mpp[c][r].push_back(root->val);
-        // if(mpp[c][r].size()>1){
-        //     sort(mpp[c][r].begin(),mpp[c][r].end());
-        // }
-        solve(root->left,mpp,r+1,c-1);
-        solve(root->right,mpp,r+1,c+1);
-    }
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-      vector<vector<int>>ans;
-      map<int,map<int,vector<int>>>mpp;
-      if(root==NULL){
-        return ans;
-      }
-      int r=0;
-      int c=0;
-      solve(root,mpp,r,c);
-      for(auto i:mpp){
-        vector<int>temp;
-        for(auto j:i.second){
-        //    for(auto k:j.second){
-        //         temp.push_back(k);
-        //    }
-        sort(j.second.begin(),j.second.end());
-        temp.insert(temp.end(),j.second.begin(),j.second.end());
-        }
-        ans.push_back(temp);
-      }
-      return ans;
+    // void solve(TreeNode *root,map<int,map<int,vector<int>>>&mpp,int r,int c){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     mpp[c][r].push_back(root->val);
+    //     // if(mpp[c][r].size()>1){
+    //     //     sort(mpp[c][r].begin(),mpp[c][r].end());
+    //     // }
+    //     solve(root->left,mpp,r+1,c-1);
+    //     solve(root->right,mpp,r+1,c+1);
+    // }
+    // vector<vector<int>> verticalTraversal(TreeNode* root) {
+    //   vector<vector<int>>ans;
+    //   map<int,map<int,vector<int>>>mpp;
+    //   if(root==NULL){
+    //     return ans;
+    //   }
+    //   int r=0;
+    //   int c=0;
+    //   solve(root,mpp,r,c);
+    //   for(auto i:mpp){
+    //     vector<int>temp;
+    //     for(auto j:i.second){
+    //     //    for(auto k:j.second){
+    //     //         temp.push_back(k);
+    //     //    }
+    //     sort(j.second.begin(),j.second.end());
+    //     temp.insert(temp.end(),j.second.begin(),j.second.end());
+    //     }
+    //     ans.push_back(temp);
+    //   }
+    //   return ans;
 
 
 
@@ -139,31 +139,29 @@ public:
     //   return ans;
 
     //method 4
-    //   vector<vector<int>>ans;
-    //   map<int,map<int,multiset<int>>>mpp;
-    //   queue<pair<TreeNode*,pair<int,int>>>q;
-    //   if(root==NULL){
-    //     return ans;
-    //   }
-    //   q.push({root,make_pair(0,0)});
-    //   while(!q.empty()){
-    //       auto front=q.front();
-    //       q.pop();
-    //       mpp[front.second.second][front.second.first].insert(front.first->val);
-    //       if(front.first->left!=NULL){
-    //         q.push({front.first->left,{front.second.first+1,front.second.second-1}});
-    //       }
-    //       if(front.first->right!=NULL){
-    //         q.push({front.first->right,{front.second.first+1,front.second.second+1}});
-    //       }
-    //   }
-    //      for(auto i:mpp){
-    //     vector<int>temp;
-    //     for(auto j:i.second){
-    //     temp.insert(temp.end(),j.second.begin(),j.second.end());
-    //     }
-    //     ans.push_back(temp);
-    //   }
-    //   return ans;
+         queue<pair<TreeNode*,pair<int,int>>>q;
+         map<int,map<int,multiset<int>>>mpp;// set dont allow duplicate //here node values may be same
+         vector<vector<int>>ans;
+         if(!root) return ans;
+         q.push({root,{0,0}});
+         while(!q.empty()){
+              auto front=q.front().first;
+              int x=q.front().second.first;
+              int y=q.front().second.second;
+              q.pop();
+              mpp[x][y].insert(front->val);
+              if(front->left)
+              q.push({front->left,{x-1,y+1}});
+              if(front->right)
+              q.push({front->right,{x+1,y+1}});
+         }
+         for(auto &i:mpp){
+            vector<int>temp;
+            for(auto &j:i.second){
+                temp.insert(temp.end(),j.second.begin(),j.second.end());
+            }
+            ans.push_back(temp);
+         }
+         return ans;
     }
 };
