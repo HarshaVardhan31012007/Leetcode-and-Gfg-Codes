3cf993cc6bd1cf91d489b97adf6f33eef1c8692a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-10 15:27:02 +0530

    Time: 140 ms (59.16%), Space: 147 MB (75%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -20,19 +20,29 @@ public:
 
 
 
-    int maximumEnergy(vector<int>& energy, int k) {
+    // int maximumEnergy(vector<int>& energy, int k) {
+    //     int ans=INT_MIN;
+    //     unordered_map<int,int>mpp;
+    //     for(int i=0;i<energy.size();i++){
+    //          int idx=i%k;
+    //          mpp[idx]=max(mpp[idx]+energy[i],energy[i]);
+    //          if(i+k>=energy.size())
+    //          ans=max(ans,mpp[idx]);
+    //     }
+    //     return ans;
+    // }
+
+
+     int maximumEnergy(vector<int>& energy, int k) {
+        vector<int>dp(k,0);
         int ans=INT_MIN;
-        unordered_map<int,int>mpp;
         for(int i=0;i<energy.size();i++){
              int idx=i%k;
-             mpp[idx]=max(mpp[idx]+energy[i],energy[i]);
+             dp[idx]=max(dp[idx]+energy[i],energy[i]);
              if(i+k>=energy.size())
-             ans=max(ans,mpp[idx]);
+             ans=max(ans,dp[idx]);
         }
         return ans;
     }
-
-
-
 
 };
