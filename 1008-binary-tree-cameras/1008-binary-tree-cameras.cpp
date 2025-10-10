d79b2bbf9b5a9f12fb67d8b5e5cdc5cae967a762
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-10 08:32:12 +0530

    Time: 0 ms (100%), Space: 21.2 MB (88.63%) - LeetHub

--- 1008-binary-tree-cameras/1008-binary-tree-cameras.cpp
+++ 1008-binary-tree-cameras/1008-binary-tree-cameras.cpp
@@ -11,7 +11,11 @@
  */
 class Solution {
 public:
-    int solve(TreeNode *root,int &cameras){
+//1//covered
+//2//camera
+//0//not covered
+    
+    int solve(TreeNode* root,int &cameras){
         if(!root) return 1;
         int l=solve(root->left,cameras);
         int r=solve(root->right,cameras);
@@ -22,14 +26,14 @@ public:
         if(l==2||r==2){
             return 1;
         }
+        //both 1//similar to leaf case
         return 0;
     }
     int minCameraCover(TreeNode* root) {
-        int cameras=0;
-        int rootstate=solve(root,cameras);
-        if(rootstate==0){
-            cameras++;
-        }
-        return cameras;
+       int cameras=0;
+       int ans=solve(root,cameras);
+       if(ans==0)
+       cameras++;
+       return cameras;
     }
 };
