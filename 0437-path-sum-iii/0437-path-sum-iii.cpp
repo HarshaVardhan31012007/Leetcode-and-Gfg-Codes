354b8d0a22516ad6e7f8d0acd5325dfbcba0715c
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-11 00:47:09 +0530

    Time: 10 ms (45.33%), Space: 18.7 MB (97.79%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -11,25 +11,48 @@
  */
 class Solution {
 public:
-    int solve(TreeNode* root,int t,unordered_map<long long,int>&visited,long long int &sum){
-        if(!root) return 0;
-        sum+=root->val;
-        visited[sum]++;
-        int l=solve(root->left,t,visited,sum);
-        int r=solve(root->right,t,visited,sum);
-        int curr=0;
-        visited[sum]--;
-        if(visited[sum-t]>0){
-            curr=visited[sum-t];
-        }
-        if(sum==t)
-        curr++;
-        sum-=root->val;
-        return curr+l+r;
+    // int solve(TreeNode* root,int t,unordered_map<long long,int>&visited,long long int &sum){
+    //     if(!root) return 0;
+    //     sum+=root->val;
+    //     visited[sum]++;
+    //     int l=solve(root->left,t,visited,sum);
+    //     int r=solve(root->right,t,visited,sum);
+    //     int curr=0;
+    //     visited[sum]--;
+    //     if(visited[sum-t]>0){
+    //         curr=visited[sum-t];
+    //     }
+    //     if(sum==t)
+    //     curr++;
+    //     sum-=root->val;
+    //     return curr+l+r;
+    // }
+    // int pathSum(TreeNode* root, int targetSum) {
+    //     unordered_map<long long,int>visited;
+    //     long long int sum=0;
+    //     return solve(root,targetSum,visited,sum);
+    // } 
+
+    
+    int ans=0;
+    void check(TreeNode *root,int &ans,long long int k){
+       if(root==NULL){
+        return;
+       }
+       if(k==root->val){
+        ans=ans+1;
+       }
+       k=k-root->val;
+       check(root->left,ans,k);
+       check(root->right,ans,k);
     }
-    int pathSum(TreeNode* root, int targetSum) {
-        unordered_map<long long,int>visited;
-        long long int sum=0;
-        return solve(root,targetSum,visited,sum);
-    } 
+    int pathSum(TreeNode* root, long long int targetSum) {
+        if(root){
+        check(root,ans,targetSum);
+        pathSum(root->left,targetSum);
+        pathSum(root->right,targetSum);
+        }
+        return ans;
+    }
+
 };
