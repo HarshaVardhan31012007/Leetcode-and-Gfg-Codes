b2d3abe6e17462ae95dd11fe486fbdf2ddd80f3f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-11 16:18:55 +0530

    Time: 0 ms (100%), Space: 10.7 MB (87.22%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,29 +12,29 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        TreeNode *curr=root;
-        vector<int>ans;
-        while(curr){
-            if(curr->left==NULL){
-            ans.push_back(curr->val);
-            curr=curr->right;
-            }
-            else{
-                TreeNode *pred=curr->left;
-                while(pred->right!=curr&&pred->right){
-                    pred=pred->right;
-                }
-                if(pred->right==NULL){
-                    pred->right=curr;
-                    curr=curr->left;
-                }
-                else{
-                    pred->right=NULL;
+          TreeNode* curr=root;
+          vector<int>ans;
+          while(curr){
+                if(curr->left==NULL){// no predecessor
                     ans.push_back(curr->val);
                     curr=curr->right;
                 }
-            }
-        }
-        return ans;
+                else{//predecessor exist
+                     TreeNode* pred=curr->left;
+                     while(pred->right!=curr&&pred->right)
+                     pred=pred->right;
+                     //if link is not created
+                     if(pred->right==NULL){
+                         pred->right=curr;
+                         curr=curr->left;
+                     }
+                     else{// link already there
+                         pred->right=NULL;
+                         ans.push_back(curr->val);
+                         curr=curr->right;
+                     }
+                }
+          }
+          return ans;
     }
 };
