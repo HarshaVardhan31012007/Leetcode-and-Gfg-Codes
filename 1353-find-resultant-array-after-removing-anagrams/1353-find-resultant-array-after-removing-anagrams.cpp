0340de6c971f3c1e179b2810eb633c3838a95f25
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-13 18:08:56 +0530

    Time: 12 ms (8.81%), Space: 20.4 MB (9.39%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -27,18 +27,40 @@ public:
 
 
 
-     array<int,26>find(string &str){
-        array<int,26>ans={0};
-        for(auto &ch:str)
-        ans[ch-'a']++;
-        return ans;
-    }
+    //  array<int,26>find(string &str){
+    //     array<int,26>ans={0};
+    //     for(auto &ch:str)
+    //     ans[ch-'a']++;
+    //     return ans;
+    // }
+    // vector<string> removeAnagrams(vector<string>& words) {
+    //     vector<string>ans;
+    //     array<int,26>prev=find(words[0]);
+    //     ans.push_back(words[0]);
+    //     for(int i=1;i<words.size();i++){
+    //        array<int,26>curr=find(words[i]);
+    //        if(curr!=prev){
+    //           prev=curr;
+    //           ans.push_back(words[i]);
+    //        }
+    //     }
+    //     return ans;
+    // }
+
+
+
+
+    
     vector<string> removeAnagrams(vector<string>& words) {
         vector<string>ans;
-        array<int,26>prev=find(words[0]);
+        unordered_map<char,int>prev;
+        for(auto &ch:words[0])
+        prev[ch]++;
         ans.push_back(words[0]);
         for(int i=1;i<words.size();i++){
-           array<int,26>curr=find(words[i]);
+           unordered_map<char,int>curr;
+           for(auto &ch:words[i])
+           curr[ch]++;
            if(curr!=prev){
               prev=curr;
               ans.push_back(words[i]);
