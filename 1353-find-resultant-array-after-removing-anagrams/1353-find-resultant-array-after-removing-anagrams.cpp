ac54c103324d3bf484907dc02018099b56c06e8f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-13 18:05:02 +0530

    Time: 0 ms (100%), Space: 16 MB (75.29%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
-    array<int,26>find(string &str){
+    // array<int,26>find(string &str){
+    //     array<int,26>ans={0};
+    //     for(auto &ch:str)
+    //     ans[ch-'a']++;
+    //     return ans;
+    // }
+    // vector<string> removeAnagrams(vector<string>& words) {
+    //     vector<string>ans;
+    //     stack<pair<array<int,26>,int>>st;
+    //     for(int i=0;i<words.size();i++){
+    //        array<int,26>curr=find(words[i]);
+    //        if(!st.empty()&&st.top().first==curr)
+    //        continue;
+    //        else
+    //        st.push({curr,i});
+    //     }
+    //     while(!st.empty()){
+    //         ans.push_back(words[st.top().second]);
+    //         st.pop();
+    //     }
+    //     reverse(ans.begin(),ans.end());
+    //     return ans;
+    // }
+
+
+
+
+     array<int,26>find(string &str){
         array<int,26>ans={0};
         for(auto &ch:str)
         ans[ch-'a']++;
@@ -8,19 +35,15 @@ public:
     }
     vector<string> removeAnagrams(vector<string>& words) {
         vector<string>ans;
-        stack<pair<array<int,26>,int>>st;
-        for(int i=0;i<words.size();i++){
+        array<int,26>prev=find(words[0]);
+        ans.push_back(words[0]);
+        for(int i=1;i<words.size();i++){
            array<int,26>curr=find(words[i]);
-           if(!st.empty()&&st.top().first==curr)
-           continue;
-           else
-           st.push({curr,i});
+           if(curr!=prev){
+              prev=curr;
+              ans.push_back(words[i]);
+           }
         }
-        while(!st.empty()){
-            ans.push_back(words[st.top().second]);
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
