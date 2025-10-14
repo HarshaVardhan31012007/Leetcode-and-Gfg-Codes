00230c26ef6866361e25d224447e60fd64168793
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-14 19:17:16 +0530

    Time: 12 ms (68.58%), Space: 40.4 MB (75.22%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
@@ -2,26 +2,46 @@ class Solution {
 public:
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
         if(nums.size()<2*k) return false;
-        for(int i=0;i<=nums.size()-2*k;i++){
-            int j=0;
-            int prev=nums[i+j];
-            j++;
-            bool flag=1;
-            for(;j<k&&flag;j++){
-                 if(nums[i+j]<=prev){
-                    flag=0;
-                 }
-                 prev=nums[i+j];
+        // for(int i=0;i<=nums.size()-2*k;i++){
+        //     int j=0;
+        //     int prev=nums[i+j];
+        //     j++;
+        //     bool flag=1;
+        //     for(;j<k&&flag;j++){
+        //          if(nums[i+j]<=prev){
+        //             flag=0;
+        //          }
+        //          prev=nums[i+j];
+        //     }
+        //     prev=nums[i+j];
+        //     j++;
+        //     for(;j<2*k&&flag;j++){
+        //         if(nums[i+j]<=prev){
+        //             flag=0;
+        //         }
+        //         prev=nums[i+j];
+        //     }
+        //     if(flag)
+        //     return true;
+        // }
+        // return false;
+
+
+
+
+        int prevCount=0;
+        int currCount=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>nums[i-1]){
+                currCount++;
             }
-            prev=nums[i+j];
-            j++;
-            for(;j<2*k&&flag;j++){
-                if(nums[i+j]<=prev){
-                    flag=0;
-                }
-                prev=nums[i+j];
+            else{
+                prevCount=currCount;
+                currCount=1;
             }
-            if(flag)
+            if(currCount>=2*k)
+            return true;
+            if(min(prevCount,currCount)>=k)
             return true;
         }
         return false;
