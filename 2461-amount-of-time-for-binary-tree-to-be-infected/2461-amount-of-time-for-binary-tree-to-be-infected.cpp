4a70d8ccdc6b38436b5735623df03403842b57ce
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-14 14:39:01 +0530

    Time: 184 ms (51.16%), Space: 197.6 MB (33.61%) - LeetHub

--- 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -11,63 +11,49 @@
  */
 class Solution {
 public:
-    void solve(TreeNode *root,int k,TreeNode *&target,unordered_map<TreeNode*,TreeNode*>&mpp){
-        if(root==NULL){
-            return;
+    void createMap(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&parent,TreeNode* &s,int &target){
+        if(!root) return;
+        if(root->val==target)
+        s=root;
+        if(root->left){
+            parent[root->left]=root;
+            createMap(root->left,parent,s,target);
         }
-        if(root->val==k){
-           target=root;
+        if(root->right){
+            parent[root->right]=root;
+            createMap(root->right,parent,s,target);
         }
-        if(root->left)
-        mpp[root->left]=root;
-        if(root->right)
-        mpp[root->right]=root;
-        solve(root->left,k,target,mpp);
-        solve(root->right,k,target,mpp);
-    }
-    int solve1(TreeNode *curr,unordered_map<TreeNode*,TreeNode*>&mpp){
-        unordered_map<TreeNode*,bool>visited;
-        int T=0;
-        queue<TreeNode*>q;
-        visited[curr]=1;
-        q.push(curr);
-        while(!q.empty()){
-          int size=q.size();
-          bool a=0;
-          for(int i=0;i<size;i++){
-          TreeNode *front=q.front();
-          q.pop();
-          if(front->left&&!visited[front->left]){
-               q.push(front->left);
-               visited[front->left]=1;
-               a=1;
-          }
-          if(front->right&&!visited[front->right]){
-               q.push(front->right);
-               visited[front->right]=1;
-               a=1;
-          }
-          if(mpp[front]&&!visited[mpp[front]]){
-            visited[mpp[front]]=1;
-            q.push(mpp[front]);
-            a=1;
-          }
-          }
-           if(a){
-             T++;
-           }
-        }
-        return T;
     }
     int amountOfTime(TreeNode* root, int start) {
-        //find target
-        //mapping for parent element
-        //you can use any traversal
-        //level order or preorder or inorder or postorder
-        TreeNode *target=NULL;
-        unordered_map<TreeNode*,TreeNode*>mpp;
-        solve(root,start,target,mpp);
-        int ans=solve1(target,mpp);
-        return ans;
+        TreeNode* s=NULL;
+        unordered_map<TreeNode*,TreeNode*>parent;
+        createMap(root,parent,s,start);
+        queue<TreeNode*>q;
+        unordered_map<TreeNode*,bool>visited;
+        q.push(s);
+        visited[s]=true;
+        int time=0;
+        while(!q.empty()){
+             int size=q.size();
+             for(int i=0;i<size;i++){
+                auto front=q.front();
+                q.pop();
+                if(front->left&&!visited[front->left]){
+                    q.push(front->left);
+                    visited[front->left]=true;
+                }
+                if(front->right&&!visited[front->right]){
+                    q.push(front->right);
+                    visited[front->right]=true;
+                }
+                auto parentNode=parent[front];
+                if(parentNode&&!visited[parentNode]){
+                    q.push(parentNode);
+                    visited[parentNode]=true;
+                }
+             }
+             time++;
+        }
+        return time-1;
     }
 };
