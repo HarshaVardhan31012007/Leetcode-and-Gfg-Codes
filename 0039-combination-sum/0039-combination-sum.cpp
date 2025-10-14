05e65694c1e866399ecd7ae29fa624040e16691f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-14 10:21:48 +0530

    Time: 4 ms (41.48%), Space: 14.1 MB (53.73%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -26,6 +26,29 @@
 // };
 
 
+class Solution {
+public:
+    void solve(vector<int>& candidates, int target,vector<vector<int>>&ans,vector<int>&temp,int i){
+        if(target==0){
+            ans.push_back(temp);
+            return;
+        }
+        if(i>=candidates.size()||target<0) return;
+        for(int j=i;j<candidates.size();j++){
+            temp.push_back(candidates[j]);
+            solve(candidates,target-candidates[j],ans,temp,j);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        solve(candidates,target,ans,temp,0);
+        return ans;
+    }
+};
+
+
 
 // class Solution {
 // public:
@@ -50,23 +73,23 @@
 // };
 
 
-class Solution {
-public:
-    void solve(vector<int>& candidates, int target,vector<vector<int>>&ans,vector<int>&temp,int i){
-        if(target==0){
-              ans.push_back(temp);
-              return;
-        } 
-        if(i==candidates.size()||target<0) return;
-        temp.push_back(candidates[i]);
-        solve(candidates,target-candidates[i],ans,temp,i);
-        temp.pop_back();
-        solve(candidates,target,ans,temp,i+1);
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        vector<int>temp;
-        solve(candidates,target,ans,temp,0);
-        return ans;
-    }
-};
+// class Solution {
+// public:
+    // void solve(vector<int>& candidates, int target,vector<vector<int>>&ans,vector<int>&temp,int i){
+    //     if(target==0){
+    //           ans.push_back(temp);
+    //           return;
+    //     } 
+    //     if(i==candidates.size()||target<0) return;
+    //     temp.push_back(candidates[i]);
+    //     solve(candidates,target-candidates[i],ans,temp,i);
+    //     temp.pop_back();
+    //     solve(candidates,target,ans,temp,i+1);
+    // }
+    // vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    //     vector<vector<int>>ans;
+    //     vector<int>temp;
+    //     solve(candidates,target,ans,temp,0);
+    //     return ans;
+    // }
+//};
