d812d7bd0177b845ce518cb461a24ab7189853d1
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-14 15:48:23 +0530

    Time: 27 ms (30.96%), Space: 58.2 MB (11.06%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -11,24 +11,48 @@
  */
 class Solution {
 public:
-    string solve(TreeNode* root,unordered_map<string,TreeNode*>&subtree,vector<TreeNode*>&ans){
+    // string solve(TreeNode* root,unordered_map<string,TreeNode*>&subtree,vector<TreeNode*>&ans){
+    //     if(root==NULL) return "NULL";
+    //     string left=solve(root->left,subtree,ans);
+    //     string right=solve(root->right,subtree,ans);
+    //     string curr=to_string(root->val)+" "+left+" "+right;
+    //     if(subtree.find(curr)!=subtree.end()){
+    //         if(subtree[curr]){
+    //         ans.push_back(root);
+    //         subtree[curr]=NULL;
+    //         }
+    //     }
+    //     else
+    //     subtree[curr]=root;
+    //     return curr;
+    // }
+    // vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+    //     vector<TreeNode*>ans;
+    //     unordered_map<string,TreeNode*>subtree;
+    //     solve(root,subtree,ans);
+    //     return ans;
+    // }
+
+
+
+    string solve(TreeNode* root,unordered_map<string,int>&subtree,vector<TreeNode*>&ans){
         if(root==NULL) return "NULL";
         string left=solve(root->left,subtree,ans);
         string right=solve(root->right,subtree,ans);
         string curr=to_string(root->val)+" "+left+" "+right;
         if(subtree.find(curr)!=subtree.end()){
-            if(subtree[curr]){
+            if(subtree[curr]==1){
             ans.push_back(root);
-            subtree[curr]=NULL;
             }
+            subtree[curr]++;
         }
         else
-        subtree[curr]=root;
+        subtree[curr]=1;
         return curr;
     }
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
         vector<TreeNode*>ans;
-        unordered_map<string,TreeNode*>subtree;
+        unordered_map<string,int>subtree;
         solve(root,subtree,ans);
         return ans;
     }
