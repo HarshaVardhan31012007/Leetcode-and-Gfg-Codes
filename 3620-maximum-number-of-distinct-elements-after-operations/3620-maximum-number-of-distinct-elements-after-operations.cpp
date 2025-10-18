c91256d12e3ec0091ca89252c9ba317493844f7f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-18 22:49:25 +0530

    Time: 128 ms (38.8%), Space: 97.8 MB (46.06%) - LeetHub

---
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    // int maxDistinctElements(vector<int>& nums, int k) {
+    //     sort(nums.begin(),nums.end());
+    //     unordered_map<int,int>mpp;
+    //     for(int i=0;i<nums.size();i++){
+    //         mpp[nums[i]]++;
+    //     }
+    //     int ans=0;
+    //     for(int i=0;i<nums.size();i++){
+    //         if(i>0&&nums[i]==nums[i-1]){
+    //             for(int j=-k;j<=k;j++){
+    //                 int newn=nums[i]+j;
+    //                 if(!mpp.count(newn)){
+    //                 mpp[newn]=1;
+    //                 mpp[nums[i]]--;
+    //                 ans++;
+    //                 break;
+    //                 }
+    //             }
+    //         }
+    //         else
+    //         ans++;
+    //     }
+    //     return ans;
+    // }
+
+
+    // int solve(vector<int>&nums,int k,int i,unordered_map<int,int>&mpp){
+    //     if(i>=nums.size()) return 0;
+    //     int l=0;
+    //     if(i==0||nums[i]!=nums[i-1])
+    //     l=1+solve(nums,k,i+1,mpp);
+    //     int r=solve()
+    // }
+    int maxDistinctElements(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int ans = 0, prev = -1e9;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int l = max(nums[i] - k, prev + 1);
+            if (l <= nums[i] + k) {
+                prev = l, ans++;
+            }
+        }
+        return ans;
+    }
+};
