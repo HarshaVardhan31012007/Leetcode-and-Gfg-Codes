c64020bb712e9f3e106e950740867fa0a0ee1a6a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-19 19:58:04 +0530

    Time: 63 ms (43.97%), Space: 135.8 MB (17.26%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.cpp
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.cpp
@@ -1,34 +1,51 @@
 class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int value) {
+    //    for(int i=0;i<nums.size();i++){
+    //         if(nums[i]>=0){
+    //             nums[i]=nums[i]%value;
+    //         }
+    //         else{
+    //            nums[i]=abs(nums[i]);
+    //            nums[i]=nums[i]%value;
+    //            if(nums[i]>0)
+    //            nums[i]=-nums[i]+value;
+    //         }
+    //    }
+    //    sort(nums.begin(),nums.end());
+    //    vector<int>temp(nums.size());
+    //    for(int j=0;j<nums.size();j++){
+    //         if(j>0&&nums[j]==nums[j-1]){
+    //            temp[j]=temp[j-1]+value;
+    //         }
+    //         else
+    //         temp[j]=nums[j];
+    //    }
+    //    int ans=0;
+    //    sort(temp.begin(),temp.end());
+    //     for(int j=0;j<temp.size();j++){
+    //        if(temp[j]==ans)
+    //        ans++;
+    //        else
+    //        return ans;
+    //    }
+    //    return ans;
+
+
+
+
+       unordered_map<int,int>freq;
        for(int i=0;i<nums.size();i++){
-            if(nums[i]>=0){
-                nums[i]=nums[i]%value;
-            }
-            else{
-               nums[i]=abs(nums[i]);
-               nums[i]=nums[i]%value;
-               if(nums[i]>0)
-               nums[i]=-nums[i]+value;
-            }
+          if(nums[i]>0)
+          freq[nums[i]%value]++;
+          else
+          freq[(nums[i]%value+value)%value]++;
        }
-       sort(nums.begin(),nums.end());
-       vector<int>temp(nums.size());
-       for(int j=0;j<nums.size();j++){
-            if(j>0&&nums[j]==nums[j-1]){
-               temp[j]=temp[j-1]+value;
-            }
-            else
-            temp[j]=nums[j];
+       int MEX=0;
+       while(freq[MEX%value]>0){
+          freq[MEX%value]--;
+          MEX++;
        }
-       int ans=0;
-       sort(temp.begin(),temp.end());
-        for(int j=0;j<temp.size();j++){
-           if(temp[j]==ans)
-           ans++;
-           else
-           return ans;
-       }
-       return ans;
+       return MEX;
     }
 };
