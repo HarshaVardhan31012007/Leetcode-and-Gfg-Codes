225fe4c9c964c86f1576cffad7b4c3734ee6a00b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-20 11:10:26 +0530

    Time: 804 ms (5.02%), Space: 48 MB (29.8%) - LeetHub

--- 0753-open-the-lock/0753-open-the-lock.cpp
+++ 0753-open-the-lock/0753-open-the-lock.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
          queue<pair<string,int>>q;
-         set<string>st(deadends.begin(),deadends.end());
          unordered_map<string,bool>visited;
          q.push({"0000",0});
          visited["0000"]=1;
@@ -10,7 +9,7 @@ public:
               auto front=q.front().first;
               auto num=q.front().second;
               q.pop();
-              if(st.find(front)!=st.end())
+              if(find(deadends.begin(),deadends.end(),front)!=deadends.end())
               continue;
               if(front==target)
               return num;
@@ -32,5 +31,37 @@ public:
               }
          }
          return -1;
+
+        //  queue<pair<string,int>>q;
+        //  set<string>st(deadends.begin(),deadends.end());
+        //  unordered_map<string,bool>visited;
+        //  q.push({"0000",0});
+        //  visited["0000"]=1;
+        //  while(!q.empty()){
+        //       auto front=q.front().first;
+        //       auto num=q.front().second;
+        //       q.pop();
+        //       if(st.find(front)!=st.end())
+        //       continue;
+        //       if(front==target)
+        //       return num;
+        //       string temp=front;
+        //       for(int i=0;i<temp.length();i++){
+        //          char original=temp[i];
+        //          temp[i]=((temp[i]-'0'+1)%10)+'0';
+        //          if(!visited[temp]){
+        //             visited[temp]=1;
+        //             q.push({temp,num+1});
+        //          }
+        //          temp[i]=original;
+        //          temp[i]=((temp[i]-'0'-1+10)%10)+'0';
+        //          if(!visited[temp]){
+        //             visited[temp]=1;
+        //             q.push({temp,num+1});
+        //          }
+        //          temp[i]=original;
+        //       }
+        //  }
+        //  return -1;
     }
 };
