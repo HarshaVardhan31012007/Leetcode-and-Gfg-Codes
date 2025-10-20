f903e3321fe7c285956c8fada9653ab863c78129
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-20 09:08:20 +0530

    Time: 0 ms (100%), Space: 17.7 MB (42.92%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
@@ -3,14 +3,22 @@ public:
     int finalValueAfterOperations(vector<string>& operations) {
         int ans=0;
         for(auto &operation:operations){
-            if(operation=="X++")
-            ans++;
-            else if(operation=="++X")
-            ++ans;
-            else if(operation=="X--")
-            ans--;
-            else
-            --ans;
+            // if(operation=="X++")
+            // ans++;
+            // else if(operation=="++X")
+            // ++ans;
+            // else if(operation=="X--")
+            // ans--;
+            // else
+            // --ans;
+
+
+            if(operation[1]=='+'){
+                   ans++; 
+            }
+            else{
+                 ans--;
+            }
         }
         return ans;
     }
