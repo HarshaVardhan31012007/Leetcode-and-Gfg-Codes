3319407f7c71235de3f4cd347a6e2b0b0796fe6b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-22 13:18:59 +0530

    Time: 8 ms (53.67%), Space: 98.7 MB (41.28%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n=s.length();
+        vector<long long int>v(n,0);
+        for(auto &shift:shifts){
+            int &s=shift[0];int e=shift[1]+1;
+            if(shift[2]==1){
+                 v[s]=v[s]+1;
+                 if(e<n)
+                 v[e]=v[e]-1;
+            }
+            else{
+                 v[s]=v[s]-1;
+                 if(e<n)
+                 v[e]=v[e]+1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            v[i]+=i>0?v[i-1]:0;
+            int curr=s[i]-'a';
+            curr=(curr+v[i]%26+26)%26;
+            s[i]=curr+'a';
+        }
+        return s;
+    }
+};
