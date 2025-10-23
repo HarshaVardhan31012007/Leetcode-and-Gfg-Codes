17f1b087b51c06c18b11cf3a7e9cdd6207d1b8b1
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-23 12:43:58 +0530

    Time: 2 ms (89.06%), Space: 8.5 MB (91.08%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -1,11 +1,34 @@
 class Solution {
 public:
     bool hasSameDigits(string s) {
-        for(int i=0;i<s.length()-2;i++){
-            for(int j=0;j<s.length()-i-1;j++){
-                int a=s[j]-'a';int b=s[j+1]-'a';
-                s[j]=(a+b)%10+'a';
+        // for(int i=0;i<s.length()-2;i++){
+        //     for(int j=0;j<s.length()-i-1;j++){
+        //         int a=s[j]-'a';int b=s[j+1]-'a';
+        //         s[j]=(a+b)%10+'a';
+        //     }
+        // }
+        // return s[0]==s[1];
+
+
+
+        // long long int c=1;
+        // int i=s.length()-1;
+        // long long int a=0,b=0;
+        // for(int j=1;j<=i;j++){
+        //      a=(a%10+(s[j-1]-'a')%10*c%10)%10;
+        //      b=(b%10+(s[j]-'a')%10*c%10)%10;
+        //      c=c*(i-j)/j;
+        // }
+        // return a==b;
+
+
+
+        int n=s.length();
+        while(n>2){
+            for(int i=0;i<n-1;i++){
+                s[i]=((s[i]-'0')+(s[i+1]-'0'))%10+'0';
             }
+            n--;
         }
         return s[0]==s[1];
     }
