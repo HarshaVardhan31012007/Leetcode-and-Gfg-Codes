cb34e8d3a6d5bfe858ad39c7fd275c8eaf5fecdc
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-26 00:05:19 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.15%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int num=n/7;
+        // int num=n/7;
+        // int rem=n%7;
+        // long long sum=28;
+        // long long prev=sum;
+        // for(int i=0;i<num-1;i++){
+        //     sum+=prev+7;
+        //     prev=prev+7;
+        // }
+        // if(num==0) sum=0;
+        // int day=num+1;
+        // while(rem--){
+        //     sum+=day;
+        //     day++;
+        // }
+        // return sum;
+
         int rem=n%7;
-        long long sum=28;
-        long long prev=sum;
-        for(int i=0;i<num-1;i++){
-            sum+=prev+7;
-            prev=prev+7;
-        }
-        if(num==0) sum=0;
-        int day=num+1;
+        n=n/7;
+        long long int num=28*n+(n*(n-1)*7)/2;
+        int day=n+1;
         while(rem--){
-            sum+=day;
+            num+=day;
             day++;
         }
-        return sum;
+        return num;
     }
 };
