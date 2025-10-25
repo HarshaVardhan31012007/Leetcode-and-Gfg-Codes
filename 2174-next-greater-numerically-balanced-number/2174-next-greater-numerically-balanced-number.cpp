cf55f4aaeab9ec9d8b7948c1b179cb37e4b1152d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-26 01:17:59 +0530

    Time: 528 ms (56.61%), Space: 254.8 MB (31.37%) - LeetHub

--- 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    // bool isBalanced(int n){
+    //     vector<int>digits(10,0);
+    //     int k=n;
+    //     while(k){
+    //         digits[k%10]++;
+    //         k=k/10;
+    //     }
+    //     for(int i=0;i<10;i++){
+    //         if(digits[i]!=0&&digits[i]!=i)
+    //         return 0;
+    //     }
+    //     return 1;
+    // }
+    // int nextBeautifulNumber(int n) {
+    //     for(int i=n+1;i<=1224444;i++){
+    //          if(isBalanced(i))
+    //          return i;
+    //     }
+    //     return -1;
+    // }
+
+    
+
     bool isBalanced(int n){
         vector<int>digits(10,0);
         int k=n;
