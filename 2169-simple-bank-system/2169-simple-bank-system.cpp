245abece0111115b46b69f7a7a828a602164e38b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-26 10:42:37 +0530

    Time: 21 ms (18.76%), Space: 128.2 MB (14.24%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.cpp
+++ 2169-simple-bank-system/2169-simple-bank-system.cpp
@@ -1,5 +1,39 @@
 class Bank {
 public:
+    // vector<long long>bank;
+    // int n;
+    // Bank(vector<long long>& balance) {
+    //     n=balance.size();
+    //     for(int i=0;i<balance.size();i++){
+    //         bank.push_back(balance[i]);
+    //     }
+    // }
+    // bool isValid(int no){
+    //     return no>=1&&no<=n;
+    // }
+    // bool transfer(int account1, int account2, long long money) {
+    //     if(!isValid(account1)||!isValid(account2)) return false;
+    //     if(money>bank[account1-1]) return false;
+    //     bank[account1-1]-=money;
+    //     bank[account2-1]+=money;
+    //     return true;
+    // }
+    
+    // bool deposit(int account, long long money) {
+    //     if(!isValid(account)) return false;
+    //     bank[account-1]+=money;
+    //     return true;
+    // }
+    
+    // bool withdraw(int account, long long money) {
+    //     if(!isValid(account)) return false;
+    //     if(money>bank[account-1]) return  false;
+    //     bank[account-1]-=money;
+    //     return true;
+    // }
+
+
+
     vector<long long>bank;
     int n;
     Bank(vector<long long>& balance) {
@@ -12,11 +46,12 @@ public:
         return no>=1&&no<=n;
     }
     bool transfer(int account1, int account2, long long money) {
-        if(!isValid(account1)||!isValid(account2)) return false;
-        if(money>bank[account1-1]) return false;
-        bank[account1-1]-=money;
-        bank[account2-1]+=money;
-        return true;
+       if(!withdraw(account1,money)) return false;
+       if(!deposit(account2,money)){
+          deposit(account1,money);
+          return false;
+       }
+       return true;
     }
     
     bool deposit(int account, long long money) {
