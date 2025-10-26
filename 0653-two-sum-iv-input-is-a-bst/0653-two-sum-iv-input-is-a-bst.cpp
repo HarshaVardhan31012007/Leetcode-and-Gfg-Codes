3b236943e32c41c4e2e3e2c4c089a755587ba05a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-26 11:11:48 +0530

    Time: 3 ms (53.65%), Space: 37.2 MB (60.5%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -11,19 +11,44 @@
  */
 class Solution {
 public:
-    unordered_map<int,int>mpp;
-    bool solve(TreeNode* root,int k){
-        if(!root) return 0;
-        if(mpp.count(k-root->val))
-        return true;
-        mpp[root->val]++;
-        bool  l=solve(root->left,k);
-        if(l) return true;
-        bool r=solve(root->right,k);
-        if(r) return true;
-        return false;
+    // unordered_map<int,int>mpp;
+    // bool solve(TreeNode* root,int k){
+    //     if(!root) return 0;
+    //     if(mpp.count(k-root->val))
+    //     return true;
+    //     mpp[root->val]++;
+    //     bool  l=solve(root->left,k);
+    //     if(l) return true;
+    //     bool r=solve(root->right,k);
+    //     if(r) return true;
+    //     return false;
+    // }
+    // bool findTarget(TreeNode* root, int k) {
+    //     return solve(root,k);
+    // }
+
+
+
+   
+    void build(TreeNode* root,vector<int>&inorder){
+        if(!root) return;
+        build(root->left,inorder);
+        inorder.push_back(root->val);
+        build(root->right,inorder);
     }
     bool findTarget(TreeNode* root, int k) {
-        return solve(root,k);
+        vector<int>inorder;
+        build(root,inorder);
+        int i=0;
+        int j=inorder.size()-1;
+        while(i<j){
+            if(inorder[i]+inorder[j]==k) return true;
+            else if(inorder[i]+inorder[j]<k){
+                i++;
+            }
+            else
+            j--;
+        }
+        return false;
     }
 };
