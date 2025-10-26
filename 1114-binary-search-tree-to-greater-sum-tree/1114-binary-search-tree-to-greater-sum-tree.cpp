8cfa05c0afcef759ad25e273fb2226912ea697b7
HARSHA VARDHAN <hv1900019@gmail.com>
2025-10-26 11:38:40 +0530

    Time: 0 ms (100%), Space: 11.6 MB (6.75%) - LeetHub

--- 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.cpp
@@ -11,13 +11,37 @@
  */
 class Solution {
 public:
-    int prev=0;
+    // int prev=0;
+    // TreeNode* bstToGst(TreeNode* root) {
+    //     if(!root) return NULL;
+    //     TreeNode* r=bstToGst(root->right);
+    //     root->val+=prev;
+    //     prev=root->val;
+    //     TreeNode* l=bstToGst(root->left);
+    //     return root;
+    // }
+
+
+    void build(TreeNode* root,vector<long long int>&inorder){
+        if(!root) return;
+        build(root->left,inorder);
+        inorder.push_back(root->val);
+        build(root->right,inorder);
+    }
+    void traverse(TreeNode* root,vector<long long int>&inorder,int &j){
+        if(!root) return;
+        traverse(root->left,inorder,j);
+        root->val=inorder[j++];
+        traverse(root->right,inorder,j);
+    }
     TreeNode* bstToGst(TreeNode* root) {
-        if(!root) return NULL;
-        TreeNode* r=bstToGst(root->right);
-        root->val+=prev;
-        prev=root->val;
-        TreeNode* l=bstToGst(root->left);
+        vector<long long int>inorder;
+        build(root,inorder);
+        for(int i=(int)inorder.size()-2;i>=0;i--){
+            inorder[i]+=inorder[i+1];
+        }
+        int j=0;
+        traverse(root,inorder,j);
         return root;
     }
 };
