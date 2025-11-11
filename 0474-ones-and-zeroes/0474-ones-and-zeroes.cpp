a7059769acd5c70df97e6cf2451ce2f74c5dafeb
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-12 00:16:14 +0530

    Time: 172 ms (38.95%), Space: 106.9 MB (18.78%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,94 +1,30 @@
 class Solution {
 public:
-    // int solve(vector<pair<int,int>>&arr,int m,int n,int i,vector<vector<vector<int>>>&dp){
-    //     if(i==arr.size()) return 0;
-    //     if(dp[i][m][n]!=-1) return dp[i][m][n];
-    //     int l=0;
-    //     if(m-arr[i].first>=0&&n-arr[i].second>=0)
-    //     l=1+solve(arr,m-arr[i].first,n-arr[i].second,i+1,dp);
-    //     int r=solve(arr,m,n,i+1,dp);
-    //     return dp[i][m][n]=max(l,r);
-    // }
-    // int findMaxForm(vector<string>& strs, int m, int n) {
-    //     vector<pair<int,int>>arr;// 1st-no.of zeros and 2nd-no.of ones
-    //     for(auto &str:strs){
-    //         int one=0;
-    //         int zero=0;
-    //         for(auto &each:str){
-    //             if(each=='0')
-    //             zero++;
-    //             if(each=='1')
-    //             one++;
-    //         }
-    //         arr.push_back({zero,one});
-    //     }
-    //     vector<vector<vector<int>>>dp(strs.size()+1,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
-    //      return solve(arr,m,n,0,dp);
-    // }
-
-
-    // int solve(vector<pair<int,int>>&arr,int m,int n){
-    //      vector<vector<vector<int>>>dp(arr.size()+1,vector<vector<int>>(m+1,vector<int>(n+1,0)));
-    //      for(int i=arr.size()-1;i>=0;i--){
-    //         for(int j=0;j<=m;j++){
-    //             for(int k=0;k<=n;k++){
-    //             int l=0;
-    //             if(j-arr[i].first>=0&&k-arr[i].second>=0)
-    //             l=1+dp[i+1][j-arr[i].first][k-arr[i].second];
-    //             int r=dp[i+1][j][k];
-    //             dp[i][j][k]=max(l,r);
-    //             }
-    //         }
-    //      }
-    //      return dp[0][m][n];
-    // }
-    // int findMaxForm(vector<string>& strs, int m, int n) {
-    //     vector<pair<int,int>>arr;// 1st-no.of zeros and 2nd-no.of ones
-    //     for(auto &str:strs){
-    //         int one=0;
-    //         int zero=0;
-    //         for(auto &each:str){
-    //             if(each=='0')
-    //             zero++;
-    //             if(each=='1')
-    //             one++;
-    //         }
-    //         arr.push_back({zero,one});
-    //     }
-    //      return solve(arr,m,n);
-    // }
-
-
-     int solve(vector<pair<int,int>>&arr,int m,int n){
-         vector<vector<int>>curr(m+1,vector<int>(n+1,0));
-         vector<vector<int>>next(m+1,vector<int>(n+1,0));
-         for(int i=arr.size()-1;i>=0;i--){
-            for(int j=0;j<=m;j++){
-                for(int k=0;k<=n;k++){
-                int l=0;
-                if(j-arr[i].first>=0&&k-arr[i].second>=0)
-                l=1+next[j-arr[i].first][k-arr[i].second];
-                int r=next[j][k];
-                curr[j][k]=max(l,r);
-                }
-            }
-             next=curr;
-         }
-         return curr[m][n];
+    int calculate(string &str){
+        int count=0;
+        for(auto &ch:str){
+            if(ch=='0')
+            count++;
+        }
+        return count;
+    }
+    int solve(vector<pair<int,int>>&v,int i,int m,int n,vector<vector<vector<int>>>&dp){
+        if(i>=v.size()) return 0;
+        if(dp[i][m][n]!=-1) return dp[i][m][n];
+        int inc=0;
+        if(v[i].first<=m&&v[i].second<=n)
+        inc=1+solve(v,i+1,m-v[i].first,n-v[i].second,dp);
+        int exc=solve(v,i+1,m,n,dp);
+        return dp[i][m][n]=max(inc,exc);
     }
     int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<pair<int,int>>arr;// 1st-no.of zeros and 2nd-no.of ones
+        vector<pair<int,int>>v;
         for(auto &str:strs){
-            int one=0;
-            int zero=0;
-            for(auto &each:str){
-                if(each=='0')
-                zero++;
-                if(each=='1')
-                one++;
-            }
-            arr.push_back({zero,one});
+            int zeroc=calculate(str);
+            int onec=str.length()-zeroc;
+            v.push_back({zeroc,onec});
         }
-         return solve(arr,m,n);
+        vector<vector<vector<int>>>dp(strs.size()+1,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
+        return solve(v,0,m,n,dp);
     }
 };
