2f4662e64edd7ed7902254a7139f3e4124d137bf
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-13 00:31:49 +0530

    Time: 4 ms (4.86%), Space: 31.7 MB (34.03%) - LeetHub

---
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int fgcd(int x,int y){
+        while(x){
+            int r=y%x;
+            y=x;
+            x=r;
+        }
+        return y;
+    }
+    int minOperations(vector<int>& nums) {
+        int x=nums[0];
+        int y=nums[1];
+        int c=0;
+        int gcd=fgcd(x,y);
+        if(x==1) c++;
+        if(y==1) c++;
+        for(int i=2;i<nums.size();i++){
+            if(nums[i]==1) c++;
+            gcd=fgcd(gcd,nums[i]);
+        }
+        if(c>0) return nums.size()-c;
+        if(gcd!=1) return -1;
+        int ans=0;
+        int n=nums.size();
+        while(n>1){
+            for(int i=0;i<n-1;i++){
+                int ans=fgcd(nums[i],nums[i+1]);
+                if(ans==1){
+                     return n+((int)nums.size()-n)*2;
+                }
+                nums[i]=ans;
+            }
+            n--;
+        }
+        return -1;
+    }
+};
