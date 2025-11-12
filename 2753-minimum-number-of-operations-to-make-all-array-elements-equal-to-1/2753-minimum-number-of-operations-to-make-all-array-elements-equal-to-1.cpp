1ac8eb7106387e32f3d4ced4ef9b8aac6cd0ff6a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-13 00:45:19 +0530

    Time: 0 ms (100%), Space: 31.7 MB (34.03%) - LeetHub

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -40,20 +40,42 @@ public:
 
 
     
-    int minOperations(vector<int>& nums) {
-        int x=nums[0];
-        int y=nums[1];
+    // int minOperations(vector<int>& nums) {
+    //     int x=nums[0];
+    //     int y=nums[1];
+    //     int c=0;
+    //     int gcd=__gcd(x,y);
+    //     if(x==1) c++;
+    //     if(y==1) c++;
+    //     for(int i=2;i<nums.size();i++){
+    //         if(nums[i]==1) c++;
+    //         gcd=__gcd(gcd,nums[i]);
+    //     }
+    //     if(c>0) return nums.size()-c;
+    //     if(gcd!=1) return -1;
+    //     int n=nums.size();
+    //     while(n>1){
+    //         for(int i=0;i<n-1;i++){
+    //             int ans=__gcd(nums[i],nums[i+1]);
+    //             if(ans==1){
+    //                  return n+((int)nums.size()-n)*2;
+    //             }
+    //             nums[i]=ans;
+    //         }
+    //         n--;
+    //     }
+    //     return -1;
+    // }
+
+
+
+     int minOperations(vector<int>& nums) {
         int c=0;
-        int gcd=__gcd(x,y);
-        if(x==1) c++;
-        if(y==1) c++;
-        for(int i=2;i<nums.size();i++){
-            if(nums[i]==1) c++;
-            gcd=__gcd(gcd,nums[i]);
+        for(auto &num:nums){
+            if(num==1)
+            c++;
         }
         if(c>0) return nums.size()-c;
-        if(gcd!=1) return -1;
-        int ans=0;
         int n=nums.size();
         while(n>1){
             for(int i=0;i<n-1;i++){
