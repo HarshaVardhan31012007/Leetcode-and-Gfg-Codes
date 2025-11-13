290268e091b0ee9b225e1d1e3cff5e0b14abada4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-13 10:44:02 +0530

    Time: 10 ms (18.84%), Space: 15.8 MB (53.99%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int maxOperations(string s) {
+        // int ans=0;
+        // int i=0;
+        // int prev=0;
+        // while(i<s.length()){
+        //     int c=0;
+        //     if(s[i]=='1'){
+        //         while(i<s.length()&&s[i]=='1'){
+        //             c++;i++;
+        //         }
+        //         if(i<s.length()&&s[i]=='0'){
+        //             ans+=prev+c;
+        //             prev=prev+c;
+        //         }
+        //     }
+        //     i++;
+        // }
+        // return ans;
+
+
+
         int ans=0;
+        int c=0;
         int i=0;
-        int prev=0;
-        while(i<s.length()){
-            int c=0;
-            if(s[i]=='1'){
-                while(i<s.length()&&s[i]=='1'){
-                    c++;i++;
-                }
-                if(i<s.length()&&s[i]=='0'){
-                    ans+=prev+c;
-                    prev=prev+c;
-                }
-            }
+        while(i<(int)s.length()-1){
+            if(s[i]=='1') c++;
+            if(s[i]=='1'&&s[i+1]=='0')
+            ans+=c;
             i++;
         }
         return ans;
