5054c7ec54745c51424435b371bfeee1b5e70129
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-14 11:29:38 +0530

    Time: 19 ms (79.62%), Space: 86.4 MB (89.34%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -15,22 +15,54 @@ public:
 
 
 
+
+        // vector<vector<int>>matrix(n,vector<int>(n,0));
+        // for(auto &query:queries){
+        //     int row1=query[0];int row2=query[2];
+        //     int col1=query[1];int col2=query[3];
+        //     for(int j=col1;j<=col2;j++){
+        //         matrix[row1][j]++;
+        //     }
+        //     if(row2+1<n){
+        //         for(int j=col1;j<=col2;j++){
+        //             matrix[row2+1][j]--;
+        //         }
+        //     }
+        // }
+        // for(int j=0;j<n;j++){
+        //     for(int i=1;i<n;i++){
+        //         matrix[i][j]+=matrix[i-1][j];
+        //     }
+        // }
+        // return matrix;
+
+
+
+
         vector<vector<int>>matrix(n,vector<int>(n,0));
         for(auto &query:queries){
             int row1=query[0];int row2=query[2];
             int col1=query[1];int col2=query[3];
-            for(int j=col1;j<=col2;j++){
-                matrix[row1][j]++;
-            }
-            if(row2+1<n){
-                for(int j=col1;j<=col2;j++){
-                    matrix[row2+1][j]--;
-                }
-            }
+            matrix[row1][col1]++;
+            if(col2+1<n)
+            matrix[row1][col2+1]--;
+            if(row2+1<n)
+            matrix[row2+1][col1]--;
+            if(row2+1<n&&col2+1<n)
+            matrix[row2+1][col2+1]++;
         }
-        for(int j=0;j<n;j++){
-            for(int i=1;i<n;i++){
-                matrix[i][j]+=matrix[i-1][j];
+        for(int i=0;i<n;i++){
+             for(int j=0;j<n;j++){
+                int up=0;
+                if(i>0)
+                up=matrix[i-1][j];
+                int left=0;
+                if(j>0)
+                left=matrix[i][j-1];
+                int common=0;
+                if(i>0&&j>0)
+                common=matrix[i-1][j-1];
+                matrix[i][j]+=up+left-common;
             }
         }
         return matrix;
