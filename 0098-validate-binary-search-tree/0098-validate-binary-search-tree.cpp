f5a99c43c1912f48fd09b52d9810f697ed463a98
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-14 13:31:14 +0530

    Time: 0 ms (100%), Space: 21.9 MB (75.21%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,26 +11,46 @@
  */
 class Solution {
 public:
-    class Info{
-        public:
-        long int mini;
-        long int maxi;
-        bool isBST;
-    };
-    Info solve(TreeNode* root){
-        if(!root) return {LONG_MAX,LONG_MIN,true};
-        Info l=solve(root->left);
-        Info r=solve(root->right);
-        if(root->val>l.maxi&&root->val<r.mini&&l.isBST&&r.isBST){
-            Info curr;
-            curr.mini=min((long)root->val,l.mini);
-            curr.maxi=max((long)root->val,r.maxi);
-            curr.isBST=true;
-            return curr;
-        }
-        return {LONG_MAX,LONG_MIN,false};
-    }
-    bool isValidBST(TreeNode* root) {
-        return solve(root).isBST;
+    //checking from bottom
+
+
+    // class Info{
+    //     public:
+    //     long int mini;
+    //     long int maxi;
+    //     bool isBST;
+    // };
+    // Info solve(TreeNode* root){
+    //     if(!root) return {LONG_MAX,LONG_MIN,true};
+    //     Info l=solve(root->left);
+    //     Info r=solve(root->right);
+    //     if(root->val>l.maxi&&root->val<r.mini&&l.isBST&&r.isBST){
+    //         Info curr;
+    //         curr.mini=min((long)root->val,l.mini);
+    //         curr.maxi=max((long)root->val,r.maxi);
+    //         curr.isBST=true;
+    //         return curr;
+    //     }
+    //     return {LONG_MAX,LONG_MIN,false};
+    // }
+    // bool isValidBST(TreeNode* root) {
+    //     return solve(root).isBST;
+    // }
+
+
+
+    //checking from top
+    bool isValidBST(TreeNode* root,long int mini=LONG_MIN,long int maxi=LONG_MAX) {
+         if(!root) return true;
+         if(root->val>mini&&root->val<maxi){
+              bool l=isValidBST(root->left,mini,root->val);
+              if(!l)
+              return false;
+              bool r=isValidBST(root->right,root->val,maxi);
+              if(!r)
+              return false;
+              return true;
+         }
+         return false;
     }
 };
