a4e467db76165336f4db502f0dcd552422ce9530
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-16 20:35:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/BST with Dead End/bst-with-dead-end.cpp
+++ Difficulty: Medium/BST with Dead End/bst-with-dead-end.cpp
@@ -1,134 +1,24 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h>
-
-using namespace std;
-
-struct Node
-{
-	int data;
-	struct Node *left;
-	struct Node *right;
-	
-	Node(int x){
-	    data = x;
-	    left = NULL;
-	    right = NULL;
-	}
-};
-
-
-void insert(Node ** tree, int val)
-{
-    Node *temp = NULL;
-    if(!(*tree))
-    {
-        temp = new Node(val);
-        *tree = temp;
-        return;
-    }
-
-    if(val < (*tree)->data)
-    {
-        insert(&(*tree)->left, val);
-    }
-    else if(val > (*tree)->data)
-    {
-        insert(&(*tree)->right, val);
-    }
-
-}
-
-
-
-
-int getCountOfNode(Node *root, int l, int h)
-{
-
-    if (!root) return 0;
-
-
-    if (root->data == h && root->data == l)
-        return 1;
-
-    if (root->data <= h && root->data >= l)
-         return 1 + getCountOfNode(root->left, l, h) +
-                    getCountOfNode(root->right, l, h);
-
-    else if (root->data < l)
-         return getCountOfNode(root->right, l, h);
-
-
-    else return getCountOfNode(root->left, l, h);
-}
-
-
-// } Driver Code Ends
 /*The Node structure is
-struct Node {
-int data;
-Node * right, * left;
+class Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
 };*/
 
-/*You are required to complete below method */
-class Solution{
+class Solution {
   public:
-    bool isdead(Node *root,unordered_map<int,bool>&visited){
-        if(root==NULL){
-            return 0;
-        }
-        visited[root->data]=true;
-        if(root->left==NULL&&root->right==NULL){
-            int x=root->data+1;
-            int y=root->data-1==0?root->data:root->data-1;
-            if(visited.find(x)!=visited.end()&&visited.find(y)!=visited.end()){
-                return true;
-            }
-            return false;
-        }
-        int lans=isdead(root->left,visited);
-        int rans=isdead(root->right,visited);
-        return lans||rans;
-    }
-    bool isDeadEnd(Node *root)
-    {
-        unordered_map<int,bool>visited;
-        return isdead(root,visited);
+    bool isDeadEnd(Node *root,int mini=0,int maxi=INT_MAX) {
+       if(!root) return false;
+       if(!root->left&&!root->right&&root->data==mini+1&&root->data==maxi-1){
+           return true;
+       }
+       return isDeadEnd(root->left,mini,root->data)||isDeadEnd(root->right,root->data,maxi);
     }
 };
-
-//{ Driver Code Starts.
-// bool isDeadEnd(Node *root);
-int main()
-{
-    int T;
-    cin>>T;
-    while(T--)
-    {
-        Node *root;
-        Node *tmp;
-    //int i;
-
-        root = NULL;
-
-        int N;
-        cin>>N;
-        for(int i=0;i<N;i++)
-        {
-            int k;
-            cin>>k;
-            insert(&root, k);
-
-        }
-
-     Solution ob;
-     cout<<ob.isDeadEnd(root);
-     cout<<endl;
-    
-cout << "~" << "\n";
-}
-}
-
-
-// } Driver Code Ends
