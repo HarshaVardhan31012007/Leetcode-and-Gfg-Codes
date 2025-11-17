de39d1eb37cceb86bcbb1dc926d9a9a7a601fc4d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-17 11:16:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Replace every element with the least greater element on its right/replace-every-element-with-the-least-greater-element-on-its-right.cpp
+++ Difficulty: Medium/Replace every element with the least greater element on its right/replace-every-element-with-the-least-greater-element-on-its-right.cpp
@@ -1,69 +1,33 @@
-//{ Driver Code Starts
-/* Driver program to test above function */
-
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 //Back-end complete function Template for C++
 
-class Solution{
-    public:
-    class Node
-    {
-       public:
-       int data;
-       Node *left;
-       Node *right;
-       Node(int val):data(val),left(0),right(0){};
+class Solution {
+  public:
+    class Node{
+        public:
+        int data;
+        Node* left;
+        Node* right;
+        Node(int val):data(val),left(0),right(0){}
     };
-    Node *build(Node *root,int &succ,int val){
-        if(!root)
-        return new Node(val);
-        if(val>=root->data){//equality important if it is equal it cant be successor 
-            root->right=build(root->right,succ,val);
+    Node* build(Node* root,int val,int &succ){
+        if(!root) return new Node(val);
+        if(val>=root->data){
+            root->right=build(root->right,val,succ);
         }
         else{
             succ=root->data;
-            root->left=build(root->left,succ,val);
+            root->left=build(root->left,val,succ);
         }
         return root;
     }
     vector<int> findLeastGreater(vector<int>& arr, int n) {
-        vector<int>ans(arr.size(),-1);
-        Node *root=NULL;
-        for(int i=arr.size()-1;i>=0;i--){
+        vector<int>ans(n);
+        Node* root=NULL;
+        for(int i=n-1;i>=0;i--){
             int succ=-1;
-            root=build(root,succ,arr[i]);
+            root=build(root,arr[i],succ);
             ans[i]=succ;
         }
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n;
-	    cin >> n;
-	    vector<int>arr(n);
-	    for(int i = 0 ; i < n; i++){
-	        cin >> arr[i];
-	    }
-	    Solution ob;
-	    vector<int> ans = ob.findLeastGreater(arr, n);
-	    for(auto it : ans){
-	        cout << it << " ";
-	    }
-	    cout <<endl;
-	
-cout << "~" << "\n";
-}
-	return 0;
-}
-
-// } Driver Code Ends
