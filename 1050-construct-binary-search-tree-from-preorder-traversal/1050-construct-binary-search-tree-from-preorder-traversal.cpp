554b2b883dcf9df99dbd073896e457f228751d71
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-17 17:38:12 +0530

    Time: 0 ms (100%), Space: 16.4 MB (68.64%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,21 +11,38 @@
  */
 class Solution {
 public:
-    TreeNode* build(vector<int>&preorder,int s,int e){
-        if(s>e) return NULL;
-        TreeNode* root=new TreeNode(preorder[s]);
-        int j=s+1;
-        while(j<=e){
-            if(preorder[j]>root->val){
-                break;
-            }
-            j++;
-        }
-        root->left=build(preorder,s+1,j-1);
-        root->right=build(preorder,j,e);
-        return root;
+    // TreeNode* build(vector<int>&preorder,int s,int e){
+    //     if(s>e) return NULL;
+    //     TreeNode* root=new TreeNode(preorder[s]);
+    //     int j=s+1;
+    //     while(j<=e){
+    //         if(preorder[j]>root->val){
+    //             break;
+    //         }
+    //         j++;
+    //     }
+    //     root->left=build(preorder,s+1,j-1);
+    //     root->right=build(preorder,j,e);
+    //     return root;
+    // }
+    // TreeNode* bstFromPreorder(vector<int>& preorder) {
+    //     return build(preorder,0,preorder.size()-1);
+    // }
+
+
+
+    TreeNode* build(vector<int>&preorder,int &i,int mini,int maxi){
+         if(i>=preorder.size()) return NULL;
+         if(preorder[i]>mini&&preorder[i]<maxi){
+            TreeNode* root=new TreeNode(preorder[i++]);
+            root->left=build(preorder,i,mini,root->val);
+            root->right=build(preorder,i,root->val,maxi);
+            return root;
+         }
+         return NULL;
     }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        return build(preorder,0,preorder.size()-1);
+        int i=0;
+        return build(preorder,i,0,1001);
     }
 };
