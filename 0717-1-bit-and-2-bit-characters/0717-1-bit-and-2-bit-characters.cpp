0750040580ee6d49c65fad3494033a7f64e7f402
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-18 21:36:20 +0530

    Time: 0 ms (100%), Space: 13.2 MB (8.09%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     bool isOneBitCharacter(vector<int>& bits) {
-           int i=0;
-           while(i<(int)bits.size()-1){
-              if(bits[i]==0){
-                i++;
-              }
-              else{
-                i=i+2;
-              }
-           }
-           if(i==bits.size()) return false;
-           return true;
+        //    int i=0;
+        //    while(i<(int)bits.size()-1){
+        //       if(bits[i]==0){
+        //         i++;
+        //       }
+        //       else{
+        //         i=i+2;
+        //       }
+        //    }
+        //    if(i==bits.size()) return false;
+        //    return true;
+
+
+        if(bits.size()==1) return true;
+        if(bits[bits.size()-2]==0) return true;
+        int i=bits.size()-2;
+        while(i>=0){
+            if(bits[i]==0) break;
+            i--;
+        }
+        int len=bits.size()-i-2;
+        if(len&1) return false;
+        return true;
     }
 };
