532e073a7c68ece78f28c604a549c3028491a7f1
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-18 22:11:17 +0530

    Time: 19 ms (13.14%), Space: 18.9 MB (82.31%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -18,21 +18,39 @@ public:
 
 class Solution {
 public:
-    void solve(Node* a,Node* b,int l,int &maxl){
-        if(!a&&!b) return;
-        if(l>maxl){
-            maxl=l;
-            solve(b->left,b->right,l+1,maxl);
-        }
-        a->next=b;
-        solve(a->left,a->right,l+1,maxl);
-        solve(a->right,b->left,l+1,maxl);
-    }
+    // void solve(Node* a,Node* b,int l,int &maxl){
+    //     if(!a&&!b) return;
+    //     if(l>maxl){
+    //         maxl=l;
+    //         solve(b->left,b->right,l+1,maxl);
+    //     }
+    //     a->next=b;
+    //     solve(a->left,a->right,l+1,maxl);
+    //     solve(a->right,b->left,l+1,maxl);
+    // }
+    // Node* connect(Node* root) {
+    //     if(!root||!root->left&&!root->right) return root;
+    //     int l=1;
+    //     int maxl=0;
+    //     solve(root->left,root->right,l,maxl);
+    //     return root;
+    // }
+
+
+
     Node* connect(Node* root) {
-        if(!root||!root->left&&!root->right) return root;
-        int l=1;
-        int maxl=0;
-        solve(root->left,root->right,l,maxl);
-        return root;
+         if(!root) return root;
+         Node* level=root;
+         while(level->left){
+            Node* curr=level;
+            while(curr){
+                curr->left->next=curr->right;
+                if(curr->next)
+                curr->right->next=curr->next->left;
+                curr=curr->next;
+            }
+            level=level->left;
+         }
+         return root;
     }
 };
