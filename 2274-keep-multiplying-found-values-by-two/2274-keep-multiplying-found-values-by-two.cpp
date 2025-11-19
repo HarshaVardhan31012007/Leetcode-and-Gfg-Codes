770775622755045f8f9c79335065a91d92574d5d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-19 10:21:17 +0530

    Time: 7 ms (14.27%), Space: 16 MB (6.93%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cpp
@@ -10,15 +10,26 @@ public:
         // return original;
 
 
-        sort(nums.begin(),nums.end());
-        int s=0;
-        int e=nums.size()-1;
-        auto it=lower_bound(nums.begin()+s,nums.end(),original);
-        while(it!=nums.end()){
-            if(*it!=original) break;
-            s=(it-nums.begin())+1;
-            original=original*2;
-            it=lower_bound(nums.begin()+s,nums.end(),original);
+        // sort(nums.begin(),nums.end());
+        // int s=0;
+        // int e=nums.size()-1;
+        // auto it=lower_bound(nums.begin()+s,nums.end(),original);
+        // while(it!=nums.end()){
+        //     if(*it!=original) break;
+        //     s=(it-nums.begin())+1;
+        //     original=original*2;
+        //     it=lower_bound(nums.begin()+s,nums.end(),original);
+        // }
+        // return original;
+
+
+
+        unordered_map<int,bool>mpp;
+        for(auto &num:nums){
+            mpp[num]=1;
+        }
+        while(mpp.count(original)==1){
+            original=2*original;
         }
         return original;
     }
