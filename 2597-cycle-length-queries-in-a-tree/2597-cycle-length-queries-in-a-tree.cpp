ce3835014ec94533c91ccd98a869fadd0985df16
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-19 08:55:45 +0530

    Time: 3 ms (98.12%), Space: 70.7 MB (54.46%) - LeetHub

--- 2597-cycle-length-queries-in-a-tree/2597-cycle-length-queries-in-a-tree.cpp
+++ 2597-cycle-length-queries-in-a-tree/2597-cycle-length-queries-in-a-tree.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
-    int find(int k,unordered_map<int,bool>&mpp){
-        if(mpp[k]||k==1) return k;
-        mpp[k]=1;
-        return find(k/2,mpp);
-    }
+    // int find(int k,unordered_map<int,bool>&mpp){
+    //     if(mpp[k]||k==1) return k;
+    //     mpp[k]=1;
+    //     return find(k/2,mpp);
+    // }
+    // vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) {
+    //     vector<int>ans;
+    //     for(auto &query:queries){
+    //         int a=query[0];int b=query[1];
+    //         unordered_map<int,bool>mpp;
+    //         find(a,mpp);
+    //         int n=find(b,mpp);
+    //         int h1=0,h2=0;
+    //         while(a!=n){
+    //             h1++;
+    //             a=a/2;
+    //         }
+    //         while(b!=n){
+    //             h2++;
+    //             b=b/2;
+    //         }
+    //         ans.push_back(h1+h2+1);
+    //     }
+    //     return ans;
+    // }
+
+
+
+    
     vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) {
         vector<int>ans;
         for(auto &query:queries){
             int a=query[0];int b=query[1];
-            unordered_map<int,bool>mpp;
-            find(a,mpp);
-            int n=find(b,mpp);
-            int h1=0,h2=0;
-            while(a!=n){
-                h1++;
-                a=a/2;
+            if(a<b) swap(a,b);
+            int diff=(int)log2(a)-(int)log2(b);
+            int n=diff;
+            while(n--)
+            a=a>>1;
+            int same=0;
+            while(a!=b){
+                same++;
+                a=a>>1;
+                b=b>>1;
             }
-            while(b!=n){
-                h2++;
-                b=b/2;
-            }
-            ans.push_back(h1+h2+1);
+            ans.push_back(same+diff+same+1);
         }
         return ans;
     }
