45dbca54b05e5bf642eda07e770548787ec51c15
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-19 15:23:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Is Binary Tree Heap/is-binary-tree-heap.cpp
+++ Difficulty: Medium/Is Binary Tree Heap/is-binary-tree-heap.cpp
@@ -1,9 +1,6 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// Tree Node
-struct Node {
+/*
+class Node {
+   public:
     int data;
     Node *left;
     Node *right;
@@ -13,146 +10,26 @@ struct Node {
         left = right = NULL;
     }
 };
-
-// Function to Build Tree
-Node *buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node *> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node *currNode = queue.front();
-        queue.pop();
-
-        // Get the current Node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current Node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current Node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
-// User Function template for C++
-
-// Structure of node
-/*struct Node {
-    int data;
-    Node *left;
-    Node *right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};*/
+*/
 
 class Solution {
   public:
-    bool isCBT(Node* root) {
-        queue<Node*>q;
-        q.push(root);
-        bool nullFound=0;
-        while(!q.empty()){
-            Node *front=q.front();
-            q.pop();
-            if(front==NULL){
-                nullFound=1;
-            }
-            else{
-                if(nullFound==true)
-                return false;
-                else{
-                    q.push(front->left);
-                    q.push(front->right);
-                }
-            }
-        }
-        return true;
+    int solve(Node* root,int &maxi,bool &flag,int i){
+        if(!root) return 0;
+        if(root->left&&root->data<root->left->data&&flag)
+        flag=false;
+        if(root->right&&root->data<root->right->data&&flag)
+        flag=false;
+        maxi=max(maxi,i);
+        int l=solve(root->left,maxi,flag,2*i);
+        int r=solve(root->right,maxi,flag,2*i+1);
+        return l+r+1;
     }
-    bool property(Node *root){
-        if(!root||!root->left&&!root->right) return true;
-        bool lans=property(root->left);
-        bool rans=property(root->right);
-        bool option1=true;
-        if(root->left&&root->data<root->left->data)
-        option1=false;
-        bool option2=true;
-        if(root->right&&root->data<root->right->data)
-        option2=false;
-        bool currans=option1&&option2;
-        return currans&&lans&&rans;
-    }
-    bool isHeap(struct Node* tree) {
-        return isCBT(tree)&&property(tree);
+    bool isHeap(Node* tree) {
+        bool flag=true;
+        int maxi=INT_MIN;
+        int i=1;
+        int count=solve(tree,maxi,flag,i);
+        return flag&&(count==maxi);
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    scanf("%d ", &tc);
-    while (tc--) {
-        string treeString;
-        getline(cin, treeString);
-        Solution ob;
-        Node *root = buildTree(treeString);
-        if (ob.isHeap(root))
-            cout << "true" << endl;
-        else
-            cout << "false" << endl;
-
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-// } Driver Code Ends
