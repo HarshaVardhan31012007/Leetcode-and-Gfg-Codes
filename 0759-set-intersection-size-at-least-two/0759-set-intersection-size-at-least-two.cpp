a8be45901e85265fd73cf5165ac2e0f0893ecb5f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-20 22:41:31 +0530

    Time: 4 ms (65.43%), Space: 21.7 MB (90.12%) - LeetHub

---
+++ 0759-set-intersection-size-at-least-two/0759-set-intersection-size-at-least-two.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    typedef vector<int>vec;
+    int intersectionSizeTwo(vector<vector<int>>& intervals) {
+       auto lambda=[](vec &a,vec &b){
+          if(a[1]!=b[1])
+            return a[1]<b[1];
+            return a[0]>b[0];
+       };
+       sort(intervals.begin(),intervals.end(),lambda);
+       int n=intervals.size();
+       int result=0;
+       int first=-1;
+       int second=-1;
+       for(int i=0;i<n;i++){
+          int l=intervals[i][0];
+          int r=intervals[i][1];
+          if(l<=first) continue;
+          if(l>second){
+            result+=2;
+            first=r-1;
+            second=r;
+          }
+          else{//l<=second
+            result+=1;
+            first=second;
+            second=r;
+          }
+       }
+       return result;
+    }
+};
