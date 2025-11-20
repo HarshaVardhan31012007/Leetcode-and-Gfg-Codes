4a004e9e1f94f611e8819b0d45498dabb338a4d5
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-20 21:18:18 +0530

    Time: 52 ms (92.25%), Space: 148.7 MB (27.86%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,5 +1,46 @@
 class MedianFinder {
 public:
+    // priority_queue<int>pq;
+    // priority_queue<int,vector<int>,greater<>>pq1;
+    // MedianFinder() {
+        
+    // }
+    
+    // void addNum(int num) {
+    //     // if(pq.empty())
+    //     // pq.push(num);
+    //     // else{
+    //     //     if(num>pq.top())
+    //     //     pq1.push(num);
+    //     //     else
+    //     //     pq.push(num);
+    //     // }
+    //     if(pq.empty()||num>pq.top())
+    //     pq1.push(num);
+    //     else
+    //     pq.push(num);
+    //     if(pq.size()>pq1.size()+1){
+    //         pq1.push(pq.top());
+    //         pq.pop();
+    //     }
+    //     else if(pq1.size()>pq.size()+1){
+    //         pq.push(pq1.top());
+    //         pq1.pop();
+    //     }
+    // }
+    
+    // double findMedian() {
+    //     if(pq.size()==pq1.size())
+    //     return (pq.top()+pq1.top())/2.0;
+    //     else{
+    //         if(pq.size()>pq1.size())
+    //         return pq.top();
+    //         else
+    //         return pq1.top();
+    //     }
+    // }
+
+
     priority_queue<int>pq;
     priority_queue<int,vector<int>,greater<>>pq1;
     MedianFinder() {
@@ -7,19 +48,15 @@ public:
     }
     
     void addNum(int num) {
-        if(pq.empty())
+        if(pq.empty()||num>pq.top())
+        pq1.push(num);
+        else
         pq.push(num);
-        else{
-            if(num>pq.top())
-            pq1.push(num);
-            else
-            pq.push(num);
-        }
         if(pq.size()>pq1.size()+1){
             pq1.push(pq.top());
             pq.pop();
         }
-        else if(pq1.size()>pq.size()+1){
+        else if(pq.size()<pq1.size()){
             pq.push(pq1.top());
             pq1.pop();
         }
@@ -28,12 +65,7 @@ public:
     double findMedian() {
         if(pq.size()==pq1.size())
         return (pq.top()+pq1.top())/2.0;
-        else{
-            if(pq.size()>pq1.size())
-            return pq.top();
-            else
-            return pq1.top();
-        }
+        return pq.top();
     }
 };
 
