0b213002daa5ad4e8348d3e7c64d42d432c8a40a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-20 18:57:40 +0530

    Time: 82 ms (59.36%), Space: 36.5 MB (99.44%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,52 +1,43 @@
-
 class Solution {
 public:
-    class Info{
-       public:
-       int data;
-       int r;
-       int c;
-       Info(int val,int x,int y){
-        this->data=val;
-        this->r=x;
-        this->c=y;
-       }
-    };
-    class compare{
+     class var{
         public:
-        bool operator()(Info *a,Info *b){
-            return a->data>b->data;
+        int data;
+        int row;
+        int col;
+    };
+    class comp{
+        public:
+        bool operator()(var &a,var &b){
+           return a.data>b.data;
         }
     };
-    vector<int> smallestRange(vector<vector<int>>& nums) {
-        priority_queue<Info*,vector<Info*>,compare>pq;
-        int mini=INT_MAX;
+     vector<int> smallestRange(vector<vector<int>>& mat) {
+        priority_queue<var,vector<var>,comp>pq;
         int maxi=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            maxi=max(maxi,nums[i][0]);
-            mini=min(mini,nums[i][0]);
-            pq.push(new Info(nums[i][0],i,0));
+        int mini=INT_MAX;
+        for(int i=0;i<mat.size();i++){
+            maxi=max(maxi,mat[i][0]);
+            mini=min(mini,mat[i][0]);
+            pq.push({mat[i][0],i,0});
         }
-        int ans_start=mini;
-        int ans_end=maxi;
+        int amini=mini;
+        int amaxi=maxi;
         while(!pq.empty()){
-            Info *top=pq.top();
+            var top=pq.top();
             pq.pop();
-            mini=top->data;
-            if(maxi-mini<ans_end-ans_start){
-                ans_end=maxi;
-                ans_start=mini;
+            mini=top.data;
+            if(maxi-mini<amaxi-amini){
+                amaxi=maxi;
+                amini=mini;
             }
-            if(top->c+1<nums[top->r].size()){
-                maxi=max(maxi,nums[top->r][top->c+1]);
-                pq.push(new Info(nums[top->r][top->c+1],top->r,top->c+1));
+            if(top.col+1<mat[top.row].size()){
+                maxi=max(maxi,mat[top.row][top.col+1]);
+                pq.push({mat[top.row][top.col+1],top.row,top.col+1});
             }
             else
             break;
         }
-        vector<int>ans;
-        ans.push_back(ans_start);
-        ans.push_back(ans_end);
-        return ans;
+        return {amini,amaxi};
     }
 };
