2730b1e900e44b3dc060c19514a43e8a1080bb86
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-22 10:41:14 +0530

    Time: 61 ms (89.77%), Space: 14.3 MB (91.82%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -22,18 +22,45 @@ public:
 
 
    
+    // int countPalindromicSubsequence(string s) {
+    //     int n=s.length();
+    //     unordered_map<char,vector<int>>mpp;
+    //     for(int i=0;i<n;i++){
+    //         mpp[s[i]].push_back(i);
+    //     }
+    //     int ans=0;
+    //     for(auto it=mpp.begin();it!=mpp.end();it++){
+    //         vector<int>&vec=it->second;
+    //         if(vec.size()<2) continue;
+    //         int a=vec[0];
+    //         int b=vec[vec.size()-1];
+    //         int freq[26]={0};
+    //         for(int j=a+1;j<b;j++){
+    //             if(freq[s[j]-'a']==0){
+    //                 ans++;
+    //             }
+    //             freq[s[j]-'a']++;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
     int countPalindromicSubsequence(string s) {
         int n=s.length();
         unordered_map<char,vector<int>>mpp;
         for(int i=0;i<n;i++){
+            if(mpp.find(s[i])==mpp.end()||mpp[s[i]].size()==1)
             mpp[s[i]].push_back(i);
+            else
+            mpp[s[i]][1]=i;
         }
         int ans=0;
         for(auto it=mpp.begin();it!=mpp.end();it++){
             vector<int>&vec=it->second;
             if(vec.size()<2) continue;
             int a=vec[0];
-            int b=vec[vec.size()-1];
+            int b=vec[1];
             int freq[26]={0};
             for(int j=a+1;j<b;j++){
                 if(freq[s[j]-'a']==0){
