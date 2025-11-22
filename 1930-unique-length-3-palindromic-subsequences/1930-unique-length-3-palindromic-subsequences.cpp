73169913c2813a6952d12bc35eb1db3c409f7db9
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-22 10:36:41 +0530

    Time: 45 ms (92.27%), Space: 25.8 MB (12.5%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    // void solve(string &s,int i,string temp,int c,set<string>&st){
+    //     if(c==0){
+    //         if(temp[0]==temp[2])
+    //         st.insert(temp);
+    //         return;
+    //     }
+    //     if(i==s.length()) return;
+    //     if(c>0)
+    //     solve(s,i+1,temp+s[i],c-1,st);
+    //     solve(s,i+1,temp,c,st);
+    // }
+    // int countPalindromicSubsequence(string s) {
+    //     set<string>st;
+    //     string temp="";
+    //     solve(s,0,temp,3,st);
+    //     return st.size();
+    // }
+
+
+
+
+   
+    int countPalindromicSubsequence(string s) {
+        int n=s.length();
+        unordered_map<char,vector<int>>mpp;
+        for(int i=0;i<n;i++){
+            mpp[s[i]].push_back(i);
+        }
+        int ans=0;
+        for(auto it=mpp.begin();it!=mpp.end();it++){
+            vector<int>&vec=it->second;
+            if(vec.size()<2) continue;
+            int a=vec[0];
+            int b=vec[vec.size()-1];
+            int freq[26]={0};
+            for(int j=a+1;j<b;j++){
+                if(freq[s[j]-'a']==0){
+                    ans++;
+                }
+                freq[s[j]-'a']++;
+            }
+        }
+        return ans;
+    }
+};
