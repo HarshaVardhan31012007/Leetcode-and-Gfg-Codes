1f1acac7f4e7e12328ce21dc622a1de37192d4fc
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-22 13:12:38 +0530

    Time: 4 ms (34.23%), Space: 17.8 MB (83.11%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,48 +1,26 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        // vector<int>ans;
-        // unordered_map<int,int>mpp;
-        // for(int i=0;i<nums.size();i++){
-        //   mpp[nums[i]]++;
-        // }
-        // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        // for(auto it=mpp.begin();it!=mpp.end();it++){
-        //      if(pq.size()<k){
-        //         pq.push({it->second,it->first});
-        //      }
-        //      else{
-        //         if(it->second>pq.top().first){
-        //             pq.pop();
-        //             pq.push({it->second,it->first});
-        //         }
-        //      }
-        // }
-        // while(!pq.empty()){
-        //     ans.push_back(pq.top().second);
-        //     pq.pop();
-        // }
-        // return ans;
-
-
-        vector<int>ans;
-        unordered_map<int,int>mpp;
-        for(int i=0;i<nums.size();i++)
-          mpp[nums[i]]++;
-
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        for(auto it=mpp.begin();it!=mpp.end();it++){
-            pq.push({it->second,it->first});
-             if(pq.size()>k){
-                pq.pop();
-             }
+        unordered_map<int,int>freq;
+        for(auto &num:nums){
+            freq[num]++;
         }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        int count=0;
+        for(auto it=freq.begin();it!=freq.end();it++){
+            if(count<k){
+                pq.push({it->second,it->first});
+                count++;
+            }
+            else if(it->second>=pq.top().first){
+                pq.pop();
+                pq.push({it->second,it->first});
+            }
+        }
+        vector<int>ans;
         while(!pq.empty()){
-            // ans.push_back(pq.top().second);
-            auto [frq,num]=pq.top();
-            ans.push_back(num);
-            pq.pop();
+             ans.push_back(pq.top().second);
+             pq.pop();
         }
         return ans;
     }
