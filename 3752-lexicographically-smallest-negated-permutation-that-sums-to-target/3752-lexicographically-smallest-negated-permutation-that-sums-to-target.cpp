4b291bec6f7ee21828914b2ef618484d05fa7bbc
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-23 20:48:25 +0530

    Time: 20 ms (57.14%), Space: 85.6 MB (85.71%) - LeetHub

---
+++ 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    // bool compareb(vector<int>&ans,vector<int>&a){
+    //     for(int i=0;i<ans.size();i++){
+    //         if(ans[i]>a[i])
+    //             return true;
+    //         else if(ans[i]<a[i])
+    //             return false;
+    //     }
+    //     return false;
+    // }
+    // void solve(int n,int i,long long target,int sum,vector<int>&temp,vector<int>&ans){
+    //     if(i>n){
+    //         if(sum==target){
+    //             vector<int>a(temp);
+    //             sort(a.begin(),a.end());
+    //             if(ans.empty())
+    //                 ans=a;
+    //             else if(compareb(ans,a))
+    //                 ans=a;
+    //         }
+    //         return;
+    //     }
+    //     temp.push_back(i);
+    //     solve(n,i+1,target,sum+i,temp,ans);
+    //     temp.pop_back();
+    //     temp.push_back(-i);
+    //     solve(n,i+1,target,sum-i,temp,ans);
+    //     temp.pop_back();
+    // }
+    // vector<int> lexSmallestNegatedPerm(int n, long long target) {
+    //      vector<int>temp;
+    //      vector<int>ans;
+    //      solve(n,1,target,0,temp,ans);
+    //      return ans;
+    // }
+
+
+
+  
+    vector<int> lexSmallestNegatedPerm(int n, long long target) {
+        long long S=1LL*n*(n+1)/2;
+        if(target>S||target<-S||((S-target)&1)) return {};
+        long long diff=(S-target)/2;
+        vector<int>ans;
+        int last=INT_MAX;
+        for(int i=n;i>=1;i--){
+            if(diff>=i*1LL){
+                ans.push_back(-i);
+                diff-=i;
+                if(diff==0){
+                    last=i-1;
+                    break;
+                }
+            }
+            else{
+                last=i;
+                break;
+            }
+        }
+        if(diff!=0)
+        ans.push_back(-diff);
+        for(int i=1;i<=last;i++){
+            if(i!=diff)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
