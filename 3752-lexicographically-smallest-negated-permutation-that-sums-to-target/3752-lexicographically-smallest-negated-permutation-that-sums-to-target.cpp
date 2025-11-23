48014819ce9f68b30dba9c3fac196a8e9df7aaf0
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-23 20:53:04 +0530

    Time: 23 ms (50%), Space: 85.6 MB (85.71%) - LeetHub

--- 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.cpp
+++ 3752-lexicographically-smallest-negated-permutation-that-sums-to-target/3752-lexicographically-smallest-negated-permutation-that-sums-to-target.cpp
@@ -38,12 +38,43 @@ public:
 
 
   
+    // vector<int> lexSmallestNegatedPerm(int n, long long target) {
+    //     long long S=1LL*n*(n+1)/2;
+    //     if(target>S||target<-S||((S-target)&1)) return {};
+    //     long long diff=(S-target)/2;//diff should be long long as target 10 power 10
+    //     vector<int>ans;
+    //     int last=INT_MAX;
+    //     for(int i=n;i>=1;i--){
+    //         if(diff>=i*1LL){// i also should be long long so that comparable
+    //             ans.push_back(-i);
+    //             diff-=i;
+    //             if(diff==0){
+    //                 last=i-1;
+    //                 break;
+    //             }
+    //         }
+    //         else{
+    //             last=i;
+    //             break;
+    //         }
+    //     }
+    //     if(diff!=0)
+    //     ans.push_back(-diff);
+    //     for(int i=1;i<=last;i++){
+    //         if(i!=diff)
+    //             ans.push_back(i);
+    //     }
+    //     return ans;
+    // }
+
+
+
     vector<int> lexSmallestNegatedPerm(int n, long long target) {
         long long S=1LL*n*(n+1)/2;
         if(target>S||target<-S||((S-target)&1)) return {};
         long long diff=(S-target)/2;//diff should be long long as target 10 power 10
         vector<int>ans;
-        int last=INT_MAX;
+        int last=-1;
         for(int i=n;i>=1;i--){
             if(diff>=i*1LL){// i also should be long long so that comparable
                 ans.push_back(-i);
