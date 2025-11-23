21b41e4c96c3f223986333c697106d34ee12cec6
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-23 21:45:09 +0530

    Time: 50 ms (12.72%), Space: 51.9 MB (7.31%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,23 +1,50 @@
 class Solution {
 public:
-    bool check(vector<int>&nums,int i,int t){
-        if(i>=nums.size()){
-            if(t==0)
-            return true;
-            return false;
+    // bool check(vector<int>&nums,int i,int t){
+    //     if(i>=nums.size()){
+    //         if(t==0)
+    //         return true;
+    //         return false;
+    //     }
+    //     bool l=false;
+    //     if(t>=nums[i])
+    //     l=check(nums,i+1,t-nums[i]);
+    //     bool e=check(nums,i+1,t);
+    //     return l||e;
+    // }
+    // int maxSumDivThree(vector<int>& nums) {
+    //     int sum=accumulate(nums.begin(),nums.end(),0);
+    //     int rem=sum%3;
+    //     if(rem==0) return sum;
+    //     while(rem<=sum){
+    //         if(check(nums,0,rem))
+    //           return sum-rem;
+    //         rem+=3;
+    //     }
+    //     return 0;
+    // }
+
+
+     bool check(vector<int>&nums,vector<vector<int>>&dp,int t){
+        for(int i=nums.size()-1;i>=0;i--){
+            for(int j=0;j<=t;j++){
+                bool l=false;
+                if(j>=nums[i])
+                l=dp[i+1][j-nums[i]];
+                bool e=dp[i+1][j];
+                dp[i][j]=l||e;
+            }
         }
-        bool l=false;
-        if(t>=nums[i])
-        l=check(nums,i+1,t-nums[i]);
-        bool e=check(nums,i+1,t);
-        return l||e;
+        return dp[0][t];
     }
     int maxSumDivThree(vector<int>& nums) {
         int sum=accumulate(nums.begin(),nums.end(),0);
         int rem=sum%3;
         if(rem==0) return sum;
         while(rem<=sum){
-            if(check(nums,0,rem))
+            vector<vector<int>>dp(nums.size()+1,vector<int>(rem+1,0));
+            dp[nums.size()][0]=1;
+            if(check(nums,dp,rem))
               return sum-rem;
             rem+=3;
         }
