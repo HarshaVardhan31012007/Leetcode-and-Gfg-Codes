67dc56481f719e32ff1acd4d891c589c4ac0d5fb
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-23 21:48:49 +0530

    Time: 11 ms (65.34%), Space: 29.3 MB (77.11%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -25,26 +25,54 @@ public:
     // }
 
 
-     bool check(vector<int>&nums,vector<vector<int>>&dp,int t){
+    //  bool check(vector<int>&nums,vector<vector<int>>&dp,int t){
+    //     for(int i=nums.size()-1;i>=0;i--){
+    //         for(int j=0;j<=t;j++){
+    //             bool l=false;
+    //             if(j>=nums[i])
+    //             l=dp[i+1][j-nums[i]];
+    //             bool e=dp[i+1][j];
+    //             dp[i][j]=l||e;
+    //         }
+    //     }
+    //     return dp[0][t];
+    // }
+    // int maxSumDivThree(vector<int>& nums) {
+    //     int sum=accumulate(nums.begin(),nums.end(),0);
+    //     int rem=sum%3;
+    //     if(rem==0) return sum;
+    //     while(rem<=sum){
+    //         vector<vector<int>>dp(nums.size()+1,vector<int>(rem+1,0));
+    //         dp[nums.size()][0]=1;
+    //         if(check(nums,dp,rem))
+    //           return sum-rem;
+    //         rem+=3;
+    //     }
+    //     return 0;
+    // }
+
+
+
+    bool check(vector<int>&nums,vector<int>&curr,int &t){
         for(int i=nums.size()-1;i>=0;i--){
-            for(int j=0;j<=t;j++){
+            for(int j=t;j>=0;j--){
                 bool l=false;
                 if(j>=nums[i])
-                l=dp[i+1][j-nums[i]];
-                bool e=dp[i+1][j];
-                dp[i][j]=l||e;
+                l=curr[j-nums[i]];
+                bool e=curr[j];
+                curr[j]=l||e;
             }
         }
-        return dp[0][t];
+        return curr[t];
     }
     int maxSumDivThree(vector<int>& nums) {
         int sum=accumulate(nums.begin(),nums.end(),0);
         int rem=sum%3;
         if(rem==0) return sum;
         while(rem<=sum){
-            vector<vector<int>>dp(nums.size()+1,vector<int>(rem+1,0));
-            dp[nums.size()][0]=1;
-            if(check(nums,dp,rem))
+            vector<int>curr(rem+1,0);
+            curr[0]=1;
+            if(check(nums,curr,rem))
               return sum-rem;
             rem+=3;
         }
