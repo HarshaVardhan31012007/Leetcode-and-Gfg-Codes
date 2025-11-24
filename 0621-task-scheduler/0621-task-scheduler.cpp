d601a6e8cd72a09a77267712ec00ece51c91801e
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-24 21:49:36 +0530

    Time: 38 ms (34.05%), Space: 52.8 MB (6.36%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int>frq;
-
-        for(auto each:tasks)
-        frq[each]++;
-
-        priority_queue<pair<int,char>>pq;
-
-        for(auto e:frq)
-        pq.push({e.second,e.first});
-
-        int time=0;
+        priority_queue<pair<int,int>>pq;
+        int freq[26]={0};
+        for(auto &task:tasks){
+            freq[task-'A']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i])
+            pq.push({freq[i],i});
+        }
+        int ans=0;
         while(!pq.empty()){
             int cycle=n+1;
-            vector<pair<int,char>>temp;
+            vector<pair<int,int>>v;
             while(cycle>0&&!pq.empty()){
-                auto [freq,task]=pq.top();
-                pq.pop();
-                cycle--;
-                time++;
-                freq--;
-                temp.push_back({freq,task});
+                 auto top=pq.top();
+                 pq.pop();
+                 top.first--;
+                 cycle--;
+                 ans++;
+                 v.push_back(top);
             }
-
-            for(auto each:temp){
+            for(auto &each:v){
                 if(each.first>0)
                 pq.push(each);
             }
             if(!pq.empty())
-            time+=cycle;
+            ans+=cycle;
         }
-        return time;
+        return ans;
     }
 };
