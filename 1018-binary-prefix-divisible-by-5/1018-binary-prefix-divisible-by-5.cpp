86dd9629b8ca84283289a88c8e58dc1de5fbc7b4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-24 10:47:20 +0530

    Time: 0 ms (100%), Space: 18.4 MB (37.78%) - LeetHub

--- 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
@@ -5,7 +5,7 @@ public:
         // long long curr=0;
         // for(int i=0;i<nums.size();i++){
         //     curr=((curr<<1)+nums[i])%5;
-        //     if((curr%5)==0)
+        //     if(curr==0)
         //     ans[i]=true;
         // }
         // return ans;
