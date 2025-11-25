b25bb42ce38adfd4c40d06952d7c4061ed30021a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-25 09:56:25 +0530

    Time: 122 ms (12.84%), Space: 144.3 MB (21.88%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    //   deque<int>dq;
-    //   vector<int>ans;
-    //   for(int i=0;i<k;i++){
-    //      while(!dq.empty()&&nums[dq.back()]<nums[i])
-    //      dq.pop_back();
-    //      dq.push_back(i);
-    //   }  
-    //    ans.push_back(nums[dq.front()]);
-    //  for(int i=k;i<nums.size();i++){
-    //     //addition and removal
-    //     // while(!dq.empty()&&nums[dq.back()]<nums[i])
-    //     // dq.pop_back();
-    //     // dq.push_back(i);
-    //     // while(!dq.empty()&&i-dq.front()>=k){
-    //     //     dq.pop_front();
-    //     // }
-    //     // removal and then addition
-    //     while(!dq.empty()&&i-dq.front()>=k){
-    //         dq.pop_front();
-    //     }
-    //     while(!dq.empty()&&nums[dq.back()]<nums[i])
-    //     dq.pop_back();
-    //     dq.push_back(i);
-    //     ans.push_back(nums[dq.front()]);
-    //  }
-    //  return ans;
-
-    vector<int>ans;
-    priority_queue<pair<int,int>>pq;
-    for(int i=0;i<k;i++){
-        pq.push({nums[i],i});
-    }
-    ans.push_back(pq.top().first);
-    for(int i=k;i<nums.size();i++){
-        while(!pq.empty()&&i-pq.top().second>=k)
-        pq.pop();
-        pq.push({nums[i],i});
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        priority_queue<pair<int,int>>pq;
+        vector<int>ans;
+        for(int i=0;i<k;i++){
+            while(!pq.empty()&&pq.top().first<=arr[i]){
+                pq.pop();
+            }
+            pq.push({arr[i],i});
+        }
         ans.push_back(pq.top().first);
-    }
-      return ans;
+        for(int i=k;i<arr.size();i++){
+            while(!pq.empty()&&i-pq.top().second>=k) pq.pop();
+            while(!pq.empty()&&pq.top().first<=arr[i]){
+                pq.pop();
+            }
+            pq.push({arr[i],i});
+            ans.push_back(pq.top().first);
+        }
+        return ans;
     }
 };
