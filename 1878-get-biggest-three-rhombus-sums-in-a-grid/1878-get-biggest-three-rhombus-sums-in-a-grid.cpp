7446806deecd4886e0dc99b70278e041fed7869d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-25 11:48:05 +0530

    Time: 178 ms (39.57%), Space: 58.8 MB (51.08%) - LeetHub

--- 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.cpp
+++ 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -1,6 +1,66 @@
 class Solution {
 public:
-    class Point{
+    // class Point{
+    //     public:
+    //     int x;
+    //     int y;
+    // };
+    // int length1(vector<vector<int>>& grid,Point a,Point b){
+    //     int sum=0;
+    //     for(int i=a.x+1,j=a.y-1;i<b.x&&j>b.y;i++,j--){
+    //           sum+=grid[i][j];
+    //     }
+    //     return sum;
+    // }
+    // int length2(vector<vector<int>>& grid,Point a,Point b){
+    //     int sum=0;
+    //     for(int i=a.x+1,j=a.y+1;i<b.x&&j<b.y;i++,j++){
+    //           sum+=grid[i][j];
+    //     }
+    //     return sum;
+    // }
+    // vector<int>find(vector<vector<int>>& grid,int i,int j,int m,int n){
+    //     int maxlength=min({i+1,j+1,m-i,n-j});
+    //     vector<int>rhombus(maxlength,0);
+    //     rhombus[0]=grid[i][j];
+    //     for(int k=1;k<maxlength;k++){
+    //          Point l={i,j-k};
+    //          Point r={i,j+k};
+    //          Point u={i-k,j};
+    //          Point d={i+k,j};
+    //          rhombus[k]+=length1(grid,u,l)+length2(grid,l,d)+length1(grid,r,d)+length2(grid,u,r)+grid[i][j-k]+grid[i][j+k]+grid[i-k][j]+grid[i+k][j];
+    //     }
+    //     return rhombus;
+    // }
+    // vector<int> getBiggestThree(vector<vector<int>>& grid) {
+    //     int m=grid.size();
+    //     int n=grid[0].size();
+    //     priority_queue<int,vector<int>,greater<>>pq;
+    //     unordered_map<int,bool>mpp;
+    //     for(int i=0;i<m;i++){
+    //         for(int j=0;j<n;j++){
+    //             vector<int>v=find(grid,i,j,m,n);
+    //             for(auto& sum:v){
+    //                 if(!mpp.count(sum)){
+    //                     pq.push(sum);
+    //                     if(pq.size()>3)
+    //                     pq.pop();
+    //                     mpp[sum]=1;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     vector<int>ans;
+    //     while(!pq.empty()){
+    //         ans.push_back(pq.top());
+    //         pq.pop();
+    //     }
+    //     reverse(ans.begin(),ans.end());
+    //     return ans;
+    // }
+
+
+     class Point{
         public:
         int x;
         int y;
@@ -19,18 +79,27 @@ public:
         }
         return sum;
     }
-    vector<int>find(vector<vector<int>>& grid,int i,int j,int m,int n){
+    void find(vector<vector<int>>& grid,int i,int j,int &m,int &n,unordered_map<int,bool>&mpp,priority_queue<int,vector<int>,greater<>>&pq){
         int maxlength=min({i+1,j+1,m-i,n-j});
-        vector<int>rhombus(maxlength,0);
-        rhombus[0]=grid[i][j];
+        if(!mpp.count(grid[i][j])){
+           pq.push(grid[i][j]);
+           if(pq.size()>3)
+           pq.pop();
+           mpp[grid[i][j]]=1;
+        }
         for(int k=1;k<maxlength;k++){
              Point l={i,j-k};
              Point r={i,j+k};
              Point u={i-k,j};
              Point d={i+k,j};
-             rhombus[k]+=length1(grid,u,l)+length2(grid,l,d)+length1(grid,r,d)+length2(grid,u,r)+grid[i][j-k]+grid[i][j+k]+grid[i-k][j]+grid[i+k][j];
+             int sum=length1(grid,u,l)+length2(grid,l,d)+length1(grid,r,d)+length2(grid,u,r)+grid[i][j-k]+grid[i][j+k]+grid[i-k][j]+grid[i+k][j];
+                if(!mpp.count(sum)){
+                    pq.push(sum);
+                    if(pq.size()>3)
+                    pq.pop();
+                    mpp[sum]=1;
+                }
         }
-        return rhombus;
     }
     vector<int> getBiggestThree(vector<vector<int>>& grid) {
         int m=grid.size();
@@ -39,15 +108,7 @@ public:
         unordered_map<int,bool>mpp;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                vector<int>v=find(grid,i,j,m,n);
-                for(auto& sum:v){
-                    if(!mpp.count(sum)){
-                        pq.push(sum);
-                        if(pq.size()>3)
-                        pq.pop();
-                        mpp[sum]=1;
-                    }
-                }
+                find(grid,i,j,m,n,mpp,pq);
             }
         }
         vector<int>ans;
