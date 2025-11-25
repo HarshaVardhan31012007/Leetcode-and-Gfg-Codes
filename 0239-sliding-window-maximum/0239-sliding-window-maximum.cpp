439fcb59371d10cc30b6706ce3f5a5e45d750bb2
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-25 10:11:10 +0530

    Time: 67 ms (16.2%), Space: 153.2 MB (17.76%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -51,20 +51,35 @@ public:
 
 
 
-        priority_queue<pair<int,int>>pq;
+    //     priority_queue<pair<int,int>>pq;
+    //     vector<int>ans;
+    //     for(int i=0;i<k;i++){
+    //         while(!pq.empty()&&pq.top().first<=arr[i]){//this means whole heap is less than current //here this loop empties the heap
+    //            pq.pop();
+    //         }
+    //         pq.push({arr[i],i});
+    //     }
+    //     ans.push_back(pq.top().first);
+    //     for(int i=k;i<arr.size();i++){
+    //         while(!pq.empty()&&i-pq.top().second>=k) pq.pop();
+    //         while(!pq.empty()&&pq.top().first<=arr[i]){
+    //             pq.pop();
+    //         }
+    //         pq.push({arr[i],i});
+    //         ans.push_back(pq.top().first);
+    //     }
+    //     return ans;
+    // }
+
+
+      priority_queue<pair<int,int>>pq;
         vector<int>ans;
         for(int i=0;i<k;i++){
-            while(!pq.empty()&&pq.top().first<=arr[i]){//this means whole heap is less than current //here this loop empties the heap
-               pq.pop();
-            }
             pq.push({arr[i],i});
         }
         ans.push_back(pq.top().first);
         for(int i=k;i<arr.size();i++){
             while(!pq.empty()&&i-pq.top().second>=k) pq.pop();
-            while(!pq.empty()&&pq.top().first<=arr[i]){
-                pq.pop();
-            }
             pq.push({arr[i],i});
             ans.push_back(pq.top().first);
         }
