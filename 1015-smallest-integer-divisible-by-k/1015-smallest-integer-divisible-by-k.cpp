739d4b558f3fabda08525fbe654fb419ba15c5c7
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-25 22:18:34 +0530

    Time: 20 ms (13.58%), Space: 15.9 MB (7.85%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int smallestRepunitDivByK(int k) {
+        //  int n=0;
+        //  int ans=0;
+        //  unordered_map<int,bool>mpp;
+        //  while(1){
+        //     n=(n*10+1)%k;
+        //     if(mpp.count(n)) break;
+        //     ans++;
+        //     if(n==0) return ans;
+        //     mpp[n]=1;
+        //  }
+        //  return -1;
+
          int n=0;
          int ans=0;
-         unordered_map<int,bool>mpp;
+         unordered_set<int>st;
          while(1){
             n=(n*10+1)%k;
-            if(mpp.count(n)) break;
             ans++;
             if(n==0) return ans;
-            mpp[n]=1;
+            if(st.find(n)!=st.end()){
+                break;
+            }
+            else
+            st.insert(n);
          }
          return -1;
     }
