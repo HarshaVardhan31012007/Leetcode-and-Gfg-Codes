aee92a80163ed523ccff19fd377e4e0bfab0adb1
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-25 09:35:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Merge two binary Max heaps/merge-two-binary-max-heaps.cpp
+++ Difficulty: Easy/Merge two binary Max heaps/merge-two-binary-max-heaps.cpp
@@ -1,79 +1,26 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-// C++ program to merge two max heaps.
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-    public:
-    void heapify(vector<int>&ans,int i,int size){
+class Solution {
+  public:
+    void heapify(vector<int>&a,int n,int i){
+        int lindex=2*i+1;
+        int rindex=2*i+2;
         int k=i;
-        if(2*i+1<size&&ans[2*i+1]>ans[k])
-        k=2*i+1;
-        if(2*i+2<size&&ans[2*i+2]>ans[k])
-        k=2*i+2;
+        if(lindex<n&&a[lindex]>a[k])
+        k=lindex;
+        if(rindex<n&&a[rindex]>a[k])
+        k=rindex;
         if(i!=k){
-            swap(ans[k],ans[i]);
-            heapify(ans,k,size);
+            swap(a[i],a[k]);
+            heapify(a,n,k);
         }
     }
     vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
-        vector<int>ans(a.begin(),a.end());
-        ans.insert(ans.end(),b.begin(),b.end());
-        for(int i=ans.size()/2-1;i>=0;i--)
-        heapify(ans,i,ans.size());
-        return ans;
+        a.insert(a.end(),b.begin(),b.end());
+        n=a.size();
+        for(int i=n/2-1;i>=0;i--){
+            heapify(a,n,i);
+        }
+        return a;
     }
 };
-
-//{ Driver Code Starts.
-
-bool isMerged(vector<int> &arr1, vector<int> &arr2, vector<int> &merged){
-    if (arr1.size() + arr2.size() != merged.size()){
-        return false;
-    }
-    arr1.insert(arr1.end(),arr2.begin(),arr2.end());
-    sort(arr1.begin(),arr1.end());
-    vector<int> mergedCopy =merged;
-    sort(mergedCopy.begin(),mergedCopy.end());
-    if (arr1 != mergedCopy){
-        return false;
-    }
-    for(int i = 1; i<merged.size(); i++){
-        if(merged[i] > merged[(i-1)/2])
-            return false;
-    }
-    return true;
-}
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m, i;
-        cin >> n >> m;
-        vector<int> a(n,0),b(m,0);
-        for (i = 0; i < n; i++) {
-            cin >> a[i];
-        }
-        for (i = 0; i < m; i++) {
-            cin >> b[i];
-        }
-        vector<int> merged, copyA = a, copyB = b;
-        Solution obj;
-        merged = obj.mergeHeaps(a, b, n, m);
-        bool flag = isMerged(copyA, copyB, merged);
-        if(flag == false) cout<<0<<endl;
-        else cout<<1<<endl;
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-
-// } Driver Code Ends
