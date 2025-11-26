f5931ac394958d0a51007c740044f7cedb23b391
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-26 17:30:36 +0530

    Time: 142 ms (91.71%), Space: 70.3 MB (93.13%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -19,25 +19,54 @@ public:
     // }
 
 
-     const int mod=1e9+7;
+    // const int mod=1e9+7;
+    // int solve(vector<vector<int>>&grid,int k){
+    //     vector<vector<vector<int>>>dp(grid.size()+1,vector<vector<int>>(grid[0].size()+1,vector<int>(k+1,0)));
+    //     for(int i=grid.size()-1;i>=0;i--){
+    //         for(int j=grid[0].size()-1;j>=0;j--){
+    //             for(int r=0;r<k;r++){
+    //                 int rem=(r+grid[i][j]%k)%k;
+    //                 if(i==grid.size()-1&&j==grid[0].size()-1){
+    //                     if(rem==0) dp[i][j][r]=1;
+    //                     else dp[i][j][r]=0;
+    //                     continue;
+    //                 }
+    //                 int right=dp[i][j+1][rem];
+    //                 int down=dp[i+1][j][rem];
+    //                 dp[i][j][r]=(right+down)%mod;
+    //             }
+    //         }
+    //     }
+    //     return dp[0][0][0];
+    // }
+    // int numberOfPaths(vector<vector<int>>& grid, int k) {
+    //     return solve(grid,k);
+    // }
+
+
+
+
+    const int mod=1e9+7;
     int solve(vector<vector<int>>&grid,int k){
-        vector<vector<vector<int>>>dp(grid.size()+1,vector<vector<int>>(grid[0].size()+1,vector<int>(k+1,0)));
+        vector<vector<int>>curr(grid[0].size()+1,vector<int>(k+1,0));
+        vector<vector<int>>next(grid[0].size()+1,vector<int>(k+1,0));
         for(int i=grid.size()-1;i>=0;i--){
             for(int j=grid[0].size()-1;j>=0;j--){
                 for(int r=0;r<k;r++){
                     int rem=(r+grid[i][j]%k)%k;
                     if(i==grid.size()-1&&j==grid[0].size()-1){
-                        if(rem==0) dp[i][j][r]=1;
-                        else dp[i][j][r]=0;
+                        if(rem==0) curr[j][r]=1;
+                        else curr[j][r]=0;
                         continue;
                     }
-                    int right=dp[i][j+1][rem];
-                    int down=dp[i+1][j][rem];
-                    dp[i][j][r]=(right+down)%mod;
+                    int right=curr[j+1][rem];
+                    int down=next[j][rem];
+                    curr[j][r]=(right+down)%mod;
                 }
             }
+            next=curr;
         }
-        return dp[0][0][0];
+        return curr[0][0];
     }
     int numberOfPaths(vector<vector<int>>& grid, int k) {
         return solve(grid,k);
