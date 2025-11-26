cb165dc542657ecd99c8c8a34844fce06a809496
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-26 21:45:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th Largest Sum Contiguous Subarray/kth-largest-sum-contiguous-subarray.cpp
+++ Difficulty: Medium/K-th Largest Sum Contiguous Subarray/kth-largest-sum-contiguous-subarray.cpp
@@ -1,58 +1,18 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 
 class Solution {
   public:
     int kthLargest(vector<int> &arr, int k) {
-        priority_queue<int,vector<int>,greater<int>>pq;
+        priority_queue<int,vector<int>,greater<>>pq;
         for(int i=0;i<arr.size();i++){
             int sum=0;
             for(int j=i;j<arr.size();j++){
-                sum=sum+arr[j];
-                if(pq.size()<k){
-                   pq.push(sum); 
-                }
-                else{
-                    if(sum>=pq.top()){
-                        pq.pop();
-                        pq.push(sum);
-                    }
-                }
+                 sum+=arr[j];
+                 pq.push(sum);
+                 if(pq.size()>k)
+                 pq.pop();
             }
         }
         return pq.top();
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution obj;
-        int res = obj.kthLargest(arr, k);
-        cout << res << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
