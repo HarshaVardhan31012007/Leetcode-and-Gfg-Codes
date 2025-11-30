785066da5a8f2fa18d8a09c7beead52fede0e51c
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-30 12:26:32 +0530

    Time: 7 ms (42.73%), Space: 23.9 MB (59.7%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    static bool comp(vector<int>&a,vector<int>&b){
+        if(a[0]!=b[0])
+        return a[0]<b[0];
+        return a[1]>b[1];
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
+        sort(intervals.begin(),intervals.end(),comp);
+        ans.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][1]<=ans.back()[1]){
+                continue;
+            }
+            if(intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                ans.back()[1]=intervals[i][1];
+            }
+        }
+        return ans;
+    }
+};
