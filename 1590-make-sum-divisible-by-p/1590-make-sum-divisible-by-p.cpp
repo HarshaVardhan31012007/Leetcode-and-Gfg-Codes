ec7737681cb24998f19bd8efcaa375d49119e5b3
HARSHA VARDHAN <hv1900019@gmail.com>
2025-11-30 22:31:44 +0530

    Time: 83 ms (53.19%), Space: 91.1 MB (18.17%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-        vector<int>prefix(nums.size());
-        prefix[0]=nums[0]%p;
-        for(int i=1;i<nums.size();i++){
-             prefix[i]=(prefix[i-1]+nums[i]%p)%p;
-        }
-        if(prefix[nums.size()-1]==0) return 0;
         vector<int>suffix(nums.size());
         suffix[nums.size()-1]=nums[nums.size()-1]%p;
         for(int i=nums.size()-2;i>=0;i--){
             suffix[i]=(suffix[i+1]+nums[i]%p)%p;
         }
+        if(suffix[0]==0) return 0;
         unordered_map<int,int>mpp;
-        int ans=INT_MAX;
+        int ans=INT_MAX;int rem=0;//prefix remainder
         for(int i=0;i<nums.size();i++){
+                rem=(rem+nums[i]%p)%p;
                 int a=(i==nums.size()-1?0:suffix[i+1]);
                 if(i!=nums.size()-1&&a==0)
                 ans=min(ans,i+1);
@@ -23,7 +19,7 @@ public:
                    int idx=mpp[b];
                    ans=min(ans,i-idx);
                 }
-                mpp[prefix[i]]=i;
+                mpp[rem]=i;// go on updating as we required smallest
         }
         return ans==INT_MAX?-1:ans;
     }
