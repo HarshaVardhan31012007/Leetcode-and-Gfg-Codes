47539a66e5bd65465fbc047ea6d1798d4407ec59
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-01 20:51:35 +0530

    Time: 5 ms (63.64%), Space: 24.1 MB (9.09%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -21,11 +21,21 @@ public:
         // return st.size();
 
 
-        int ans=1;
-        sort(s.begin(),s.end());
-        for(int i=1;i<s.length();i++){
-            if(s[i]==s[i-1]) continue;
+        // int ans=1;
+        // sort(s.begin(),s.end());
+        // for(int i=1;i<s.length();i++){
+        //     if(s[i]==s[i-1]) continue;
+        //     ans++;
+        // }
+        // return ans;
+
+
+        int freq[26]={0};
+        int ans=0;
+        for(auto &each:s){
+            if(freq[each-'a']==0)
             ans++;
+            freq[each-'a']++;
         }
         return ans;
     }
