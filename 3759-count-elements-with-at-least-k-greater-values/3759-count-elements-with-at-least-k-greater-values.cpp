370ac8c8d4c9152ca564a7d80d2ff27486908500
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-01 18:01:25 +0530

    Time: 146 ms (40%), Space: 191 MB (35%) - LeetHub

--- 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
@@ -15,15 +15,32 @@ public:
         // return ans;
 
 
-        sort(nums.begin(),nums.end());
-        int ans=0;
+        // sort(nums.begin(),nums.end());
+        // int ans=0;
+        // for(int i=0;i<nums.size();i++){
+        //     int idx=upper_bound(nums.begin()+i,nums.end(),nums[i])-nums.begin();
+        //     if(nums.size()-idx>=k){
+        //         ans+=idx-i;
+        //     }
+        //     i=idx-1;
+        // }
+        // return ans;
+
+
+
+        if(k==0) return nums.size();
+        priority_queue<int>pq;
         for(int i=0;i<nums.size();i++){
-            int idx=upper_bound(nums.begin()+i,nums.end(),nums[i])-nums.begin();
-            if(nums.size()-idx>=k){
-                ans+=idx-i;
-            }
-            i=idx-1;
+            pq.push(nums[i]);
         }
-        return ans;
+        k--;
+        while(!pq.empty()&&(k-->0)){
+            pq.pop();
+        }
+        int num=pq.top();
+        while(!pq.empty()&&num==pq.top()){
+            pq.pop();
+        }
+        return pq.size();
     }
 };
