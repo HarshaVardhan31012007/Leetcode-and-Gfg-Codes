f2b7af8ff3a9b2f7854bb102d986df81377b3b54
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-02 20:32:00 +0530

    Time: 69 ms (52.37%), Space: 195.5 MB (49.21%) - LeetHub

---
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    // const long long mod=1e9+7;
+    // long long countTrapezoids(vector<vector<long long>>& polong longs) {
+    //     unordered_map<long long,long long>mpp;
+    //     for(auto &polong long:polong longs){
+    //         mpp[polong long[1]]++;
+    //     }
+    //     long long sum=0;
+    //     for(auto it=mpp.begin();it!=mpp.end();it++){
+    //        if(it->second>=2)
+    //        sum=(sum%mod+(((it->second)*(it->second-1))/2)%mod)%mod;
+    //     }
+    //     long long ans=0;
+    //     for(auto it=mpp.begin();it!=mpp.end();it++){
+    //           long long temp=(sum-(((it->second)*(it->second-1))/2)%mod+mod)%mod;
+    //           ans=(ans+(temp*(((it->second)*(it->second-1))/2)%mod)%mod)%mod;
+    //     }
+    //     return ans/2;
+    // }
+
+
+
+    const long long mod=1e9+7;
+    int countTrapezoids(vector<vector<int>>& points) {
+        unordered_map<long long,long long>mpp;
+        for(auto &point:points){
+            mpp[point[1]]++;
+        }
+        long long sum=0;
+        int ans=0;
+        for(auto it=mpp.begin();it!=mpp.end();it++){
+           if(it->second>=2){
+               ans=(ans+(sum%mod*(((it->second)*(it->second-1))/2)%mod)%mod)%mod;
+               sum=(sum%mod+(((it->second)*(it->second-1))/2)%mod)%mod;
+           }
+        }
+        return ans;
+    }
+};
