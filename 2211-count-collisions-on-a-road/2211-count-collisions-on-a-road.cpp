0bd0e44bc425c21047336ad757b101630021fdca
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-04 21:53:10 +0530

    Time: 7 ms (82.03%), Space: 19.3 MB (64.06%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,29 +1,46 @@
 class Solution {
 public:
     int countCollisions(string directions) {
-           int forward=0;
-           bool stationary=false;
-           int ans=0;
-           for(int i=0;i<directions.length();i++){
-                 if(directions[i]=='S'){
-                    ans+=forward;
-                    forward=0;
-                    stationary=true;
-                 }
-                 if(directions[i]=='L'){
-                     if(forward>0){
-                       ans+=(forward-1)+2;
-                       forward=0;
-                       stationary=true;
-                     }
-                     else{
-                        if(stationary){
-                           ans+=1;
-                        }
-                     }
-                 }
-                 if(directions[i]=='R') forward++;
-           }
-           return ans;
+        //    int forward=0;
+        //    bool stationary=false;
+        //    int ans=0;
+        //    for(int i=0;i<directions.length();i++){
+        //          if(directions[i]=='S'){
+        //             ans+=forward;
+        //             forward=0;
+        //             stationary=true;
+        //          }
+        //          if(directions[i]=='L'){
+        //              if(forward>0){
+        //                ans+=(forward-1)+2;
+        //                forward=0;
+        //                stationary=true;
+        //              }
+        //              else{
+        //                 if(stationary){
+        //                    ans+=1;
+        //                 }
+        //              }
+        //          }
+        //          if(directions[i]=='R') forward++;
+        //    }
+        //    return ans;
+
+
+        int i=0;
+        while(i<directions.length()&&directions[i]=='L'){
+            i++;
+        }
+        int j=directions.length()-1;
+        while(j>=0&&directions[j]=='R'){
+            j--;
+        }
+        int collisions=0;
+        while(i<=j){
+            if(directions[i]!='S')
+            collisions++;
+            i++;
+        }
+        return collisions;
     }
 };
