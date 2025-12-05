301fe18f6bfa104d47c3a859b86c5c7829ea64d9
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-05 08:18:14 +0530

    Time: 0 ms (100%), Space: 23.3 MB (5.3%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -2,20 +2,37 @@ class Solution {
 public:
     typedef long long ll;
     int countPartitions(vector<int>& nums) {
+        // int n=nums.size();
+        // vector<ll>prefix(n,0);
+        // prefix[0]=nums[0];
+        // for(int i=1;i<n;i++){
+        //     prefix[i]=prefix[i-1]+nums[i];
+        // } 
+        // vector<ll>suffix(n,0);
+        // suffix[n-1]=nums[n-1];
+        // for(int i=n-2;i>=0;i--){
+        //     suffix[i]=suffix[i+1]+nums[i];
+        // }
+        // int ans=0;
+        // for(int i=0;i<n-1;i++){
+        //     ll curr=suffix[i+1]-prefix[i];
+        //     if(curr%2==0)
+        //     ans++;
+        // }
+        // return ans;
+
+
+
         int n=nums.size();
         vector<ll>prefix(n,0);
         prefix[0]=nums[0];
         for(int i=1;i<n;i++){
             prefix[i]=prefix[i-1]+nums[i];
-        } 
-        vector<ll>suffix(n,0);
-        suffix[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=suffix[i+1]+nums[i];
         }
+        ll sum=prefix[n-1];
         int ans=0;
         for(int i=0;i<n-1;i++){
-            ll curr=suffix[i+1]-prefix[i];
+            ll curr=sum-2*prefix[i];//sum-prefix[i]-prefix[i]
             if(curr%2==0)
             ans++;
         }
