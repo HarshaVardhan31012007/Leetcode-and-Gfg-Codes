169a15a850857cbc75f9c492aa8bc63882e5f85f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-05 18:14:04 +0530

    Time: 30 ms (92.13%), Space: 105.9 MB (63.61%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -2,22 +2,38 @@ class Solution {
 public:
     typedef long long ll;
     int maxFrequency(vector<int>& nums, int k) {
+        // sort(nums.begin(),nums.end());
+        // int n=nums.size();
+        // ll operations=0;
+        // ll ans=LLONG_MIN;
+        // ll j=0;ll i=0;ll sum=nums[0];
+        // while(j<nums.size()){
+        //     while(j<nums.size()&&((nums[j]*(j-i+1)-sum)<=k)){
+        //         ans=max(ans,j-i+1);
+        //         j++;
+        //         if(j<nums.size())  sum+=nums[j];
+        //     }
+        //     if(j<nums.size()){
+        //         sum-=nums[i];
+        //         i++;
+        //     }
+        // }
+        // return ans;
+
+
+
+
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        ll operations=0;
-        ll ans=LLONG_MIN;
-        ll j=0;ll i=0;ll sum=nums[0];
+        ll j=0;ll i=0;ll sum=0;
         while(j<nums.size()){
-            while(j<nums.size()&&((nums[j]*(j-i+1)-sum)<=k)){
-                ans=max(ans,j-i+1);
-                j++;
-                if(j<nums.size())  sum+=nums[j];
-            }
-            if(j<nums.size()){
-                sum-=nums[i];
-                i++;
-            }
+           sum+=nums[j];
+           if(((j-i+1)*nums[j]-sum)>k){
+               sum-=nums[i];
+               i++;
+           }
+           j++;
         }
-        return ans;
+        return nums.size()-i;
     }
 };
