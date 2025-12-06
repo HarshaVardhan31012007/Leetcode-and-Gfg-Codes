143c51870474b824870b4630bc669d78ead0bc1d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-06 17:40:01 +0530

    Time: 16 ms (60.35%), Space: 25 MB (64.53%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
-    // vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
-    //     vector<vector<string>>v;
-    //     unordered_map<string,vector<string>>mpp;
-
-    //     for(int i=0;i<strs.size();i++){
-    //         string curr=strs[i];
-    //         sort(curr.begin(),curr.end());
-    //         mpp[curr].push_back(strs[i]);
-    //     }
-
-    //     // for(auto each:mpp){
-    //     //     v.push_back(each.second);
-    //     // }
-    //     for(auto it=mpp.begin();it!=mpp.end();it++){
-    //         v.push_back(it->second);
-    //     }
-    //     return v;
-    // }
-   
-     array<int,256>hash(string k){
-        array<int,256>hash={0};
-        for(int i=0;i<k.size();i++){
-            hash[k[i]]++;
-        }
-        return hash;
-     }
-     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
-        vector<vector<string>>v;// or std::array<int,256>
-        map<array<int,256>,vector<string>>mpp;//here comparison function will be there
-        //but for unordered_map we have to need hash function 
+        vector<vector<string>>v;
+        unordered_map<string,vector<string>>mpp;
 
         for(int i=0;i<strs.size();i++){
-            mpp[hash(strs[i])].push_back(strs[i]);
-            }
+            string curr=strs[i];
+            sort(curr.begin(),curr.end());
+            mpp[curr].push_back(strs[i]);
+        }
 
-        for(auto each:mpp){
-            v.push_back(each.second);
+        // for(auto each:mpp){
+        //     v.push_back(each.second);
+        // }
+        for(auto it=mpp.begin();it!=mpp.end();it++){
+            v.push_back(it->second);
         }
         return v;
     }
+   
+    //  array<int,256>hash(string k){
+    //     array<int,256>hash={0};
+    //     for(int i=0;i<k.size();i++){
+    //         hash[k[i]]++;
+    //     }
+    //     return hash;
+    //  }
+    //  vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+    //     vector<vector<string>>v;// or std::array<int,256>
+    //     map<array<int,256>,vector<string>>mpp;//here comparison function will be there
+    //     //but for unordered_map we have to need hash function 
+
+    //     for(int i=0;i<strs.size();i++){
+    //         mpp[hash(strs[i])].push_back(strs[i]);
+    //         }
+
+    //     for(auto each:mpp){
+    //         v.push_back(each.second);
+    //     }
+    //     return v;
+    // }
 };
