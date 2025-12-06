72245e8c32411220e2df28125890902dc9de19a1
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-06 17:33:17 +0530

    Time: 15 ms (38.62%), Space: 18.1 MB (5.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>mpp;
+        // unordered_map<int,int>mpp;
+        // int s=-1;int e=-1;
+        // for(int i=0;i<nums.size();i++){
+        //     if(mpp.find(target-nums[i])!=mpp.end()){
+        //         s=mpp[target-nums[i]];
+        //         e=i;
+        //         break;
+        //     }
+        //     mpp[nums[i]]=i;
+        // }
+        // return {s,e};
+
+
+        unordered_map<int,vector<int>>mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]].push_back(i);
+        }
         int s=-1;int e=-1;
         for(int i=0;i<nums.size();i++){
-            if(mpp.find(target-nums[i])!=mpp.end()){
-                s=mpp[target-nums[i]];
-                e=i;
-                break;
+            int current=nums[i];
+            int required=target-nums[i];
+            if(mpp.count(required)==1){
+                if(current==required&&mpp[current].size()==1)
+                continue;
+                if(current==required&&mpp[current].size()>1)
+                return {mpp[current][0],mpp[current][1]};
+                else
+                return {mpp[required][0],i};
             }
-            mpp[nums[i]]=i;
         }
-        return {s,e};
+        return {-1,-1};
     }
 };
