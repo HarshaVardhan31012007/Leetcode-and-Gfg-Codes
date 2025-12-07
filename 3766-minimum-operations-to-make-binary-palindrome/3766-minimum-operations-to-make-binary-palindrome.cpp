c1e6b4ef9f36f32f0ec8c6ed9f4a1e34d2bb677e
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-07 07:35:13 +0530

    Time: 275 ms (38.46%), Space: 44.7 MB (100%) - LeetHub

---
+++ 3766-minimum-operations-to-make-binary-palindrome/3766-minimum-operations-to-make-binary-palindrome.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string to_binary(int n){
+        string temp="";
+        while(n){
+            int digit=n%2;
+            temp+=(digit==1?'1':'0');
+            n=n/2;
+        }
+        return temp;
+    }
+    bool ispalindrome(int n){
+        string temp=to_binary(n);
+        int i=0;
+        int j=temp.length()-1;
+        while(i<j){
+            if(temp[i]==temp[j]){
+                i++;
+                j--;
+            }
+            else
+            return false;
+        }
+        return true;
+    }
+    vector<int> minOperations(vector<int>& nums) {
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+           int n=nums[i];
+           int n1=nums[i];
+           while(1){
+              if(ispalindrome(n)) {ans.push_back(n-nums[i]);break;}
+              if(ispalindrome(n1)){ans.push_back(nums[i]-n1);break;}
+              n++;
+              n1--;
+           }
+        }
+        return ans;
+    }
+};
