2bd930589c8f66db3c154e15a0af5c5c207ef632
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-07 12:00:52 +0530

    Time: 1140 ms (14.07%), Space: 245.6 MB (19.46%) - LeetHub

---
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int largestPrime(int n) {
+        vector<bool>primes(n+1,1);
+        for(int i=2;i<=sqrt(n);i==2?i=i+1:i=i+2){
+            if(primes[i]==0) continue;
+            int j=i;
+            int p=i;
+            while(j*p<=n){
+                primes[j*p]=0;
+                p++;
+            }
+        }
+        long long sum=0;
+        vector<long long>v;
+        long long ans=0;
+        for(int i=2;i<=n;i==2?i=i+1:i=i+2){
+            if(primes[i]==0) continue;
+            sum+=i;
+            v.push_back(sum);
+            if(binary_search(v.begin(),v.end(),i))  ans=i;
+        }
+        return ans;
+    }
+};
