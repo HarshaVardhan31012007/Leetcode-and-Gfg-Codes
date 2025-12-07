57ff3926dc4ba09c33c4e8a88cde5cb56732e5f6
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-07 19:56:11 +0530

    Time: 3 ms (37.5%), Space: 9.6 MB (8.33%) - LeetHub

--- 3765-complete-prime-number/3765-complete-prime-number.cpp
+++ 3765-complete-prime-number/3765-complete-prime-number.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+    // bool isprime(int n){
+    //     if(n<=1) return false;
+    //     for(int i=2;i<=sqrt(n);i++){
+    //         if(n%i==0)
+    //             return false;
+    //     }
+    //     return true;
+    // }
+    // bool completePrime(int num) {
+    //     string s=to_string(num);
+    //     string temp="";
+    //     for(int i=0;i<s.length();i++){
+    //         temp+=s[i];
+    //         int n=stoi(temp);
+    //         if(!isprime(n))
+    //             return false;
+    //     }
+    //     temp="";
+    //     for(int i=s.length()-1;i>=0;i--){
+    //          temp=s[i]+temp;
+    //          int n=stoi(temp);
+    //          if(!isprime(n))
+    //             return false;
+    //     }
+    //     return true;
+    // }
+
+
+
     bool isprime(int n){
         if(n<=1) return false;
         for(int i=2;i<=sqrt(n);i++){
@@ -10,17 +39,13 @@ public:
     }
     bool completePrime(int num) {
         string s=to_string(num);
-        string temp="";
         for(int i=0;i<s.length();i++){
-            temp+=s[i];
-            int n=stoi(temp);
+            int n=stoi(s.substr(0,i+1));
             if(!isprime(n))
                 return false;
         }
-        temp="";
         for(int i=s.length()-1;i>=0;i--){
-             temp=s[i]+temp;
-             int n=stoi(temp);
+             int n=stoi(s.substr(i,s.length()-i));
              if(!isprime(n))
                 return false;
         }
