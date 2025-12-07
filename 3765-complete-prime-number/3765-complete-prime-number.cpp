87a68a85f873069bb8d9cb7b27c8c9ea77c86df8
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-07 07:55:23 +0530

    Time: 0 ms (100%), Space: 9.1 MB (8.33%) - LeetHub

---
+++ 3765-complete-prime-number/3765-complete-prime-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isprime(int n){
+        if(n<=1) return false;
+        for(int i=2;i<=sqrt(n);i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    bool completePrime(int num) {
+        string s=to_string(num);
+        string temp="";
+        for(int i=0;i<s.length();i++){
+            temp+=s[i];
+            int n=stoi(temp);
+            if(!isprime(n))
+                return false;
+        }
+        temp="";
+        for(int i=s.length()-1;i>=0;i--){
+             temp=s[i]+temp;
+             int n=stoi(temp);
+             if(!isprime(n))
+                return false;
+        }
+        return true;
+    }
+};
