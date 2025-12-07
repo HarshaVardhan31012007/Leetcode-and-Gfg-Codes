f7cd63737dc8063a4ba34a4d618cf6c0e09c74db
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-07 12:33:01 +0530

    Time: 902 ms (21.12%), Space: 245.7 MB (18.98%) - LeetHub

--- 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
@@ -1,24 +1,66 @@
 class Solution {
 public:
-    int largestPrime(int n) {
+    // int largestPrime(int n) {
+    //     vector<bool>primes(n+1,1);
+    //     for(int i=2;i<=sqrt(n);i==2?i=i+1:i=i+2){
+    //         if(primes[i]==0) continue;
+    //         // int j=i;
+    //         // int p=2;
+    //         int j=i;
+    //         int p=i;//2*3 and 3*3 to avoid repetitive things
+    //         while(j*p<=n){
+    //             primes[j*p]=0;
+    //             p++;
+    //         }
+    //     }
+    //     long long sum=0;
+    //     vector<long long>v;
+    //     long long ans=0;
+    //     for(int i=2;i<=n;i==2?i=i+1:i=i+2){
+    //         if(primes[i]==0) continue;
+    //         sum+=i;
+    //         v.push_back(sum);
+    //         if(binary_search(v.begin(),v.end(),i))  ans=i;
+    //     }
+    //     return ans;
+    // }
+
+
+
+     int largestPrime(int n) {
         vector<bool>primes(n+1,1);
         for(int i=2;i<=sqrt(n);i==2?i=i+1:i=i+2){
             if(primes[i]==0) continue;
+            // int j=i;
+            // int p=2;
             int j=i;
-            int p=2;
+            int p=i;//2*3 and 3*3 to avoid repetitive things
             while(j*p<=n){
                 primes[j*p]=0;
                 p++;
             }
         }
-        long long sum=0;
+        // long long sum=0;
+        // unordered_map<long long,bool>mpp;//TLE for n=199 as sum can be more and more large to store//more time to store//vector is better than map for accessing
+        // long long ans=0;
+        // for(int i=2;i<=n;i==2?i=i+1:i=i+2){
+        //     if(primes[i]==0) continue;
+        //     sum+=i;
+        //     mpp[sum]=1;
+        //     if(mpp.count(i))  ans=i;
+        // }
         vector<long long>v;
         long long ans=0;
-        for(int i=2;i<=n;i==2?i=i+1:i=i+2){
-            if(primes[i]==0) continue;
-            sum+=i;
-            v.push_back(sum);
-            if(binary_search(v.begin(),v.end(),i))  ans=i;
+        for(int i=2;i<=n;i++){
+            if(primes[i])
+            v.push_back(i);
+        }
+        long long sum=0;
+        for(auto &each:v){
+            sum+=each;
+            if(sum>n) break;
+            if(primes[sum])
+            ans=sum;
         }
         return ans;
     }
