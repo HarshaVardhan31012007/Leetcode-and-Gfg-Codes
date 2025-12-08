331cd957f74677e12db72652a5a77aac1e38213b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-08 10:37:46 +0530

    Time: 0 ms (100%), Space: 7.6 MB (93.14%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -12,14 +12,23 @@ public:
         // return ans;
 
 
-        int i=low;
-        int j=high;
-        int ans=0;
-        while(i<=j){
-            if(i%2==1)
-            ans++;
-            i++;
-        }
-        return ans;
+        // int i=low;
+        // int j=high;
+        // int ans=0;
+        // while(i<=j){
+        //     if(i%2==1)
+        //     ans++;
+        //     i++;
+        // }
+        // return ans;
+
+
+        bool olow=(low&1);
+        bool ohigh=(high&1);
+        if(olow&&!ohigh||!olow&&ohigh)
+        return (high-low+1)/2;
+        if(olow&&ohigh) return (high-low+1)/2+1;
+        else
+        return (high-low+1)/2;
     }
 };
