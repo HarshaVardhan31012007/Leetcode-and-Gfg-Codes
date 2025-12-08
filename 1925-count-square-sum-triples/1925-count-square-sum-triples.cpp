d8095e02ff8fd747d8ec32123eeae2e2442411e4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-08 21:45:34 +0530

    Time: 15 ms (40.04%), Space: 8 MB (12.61%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int ans=0;
+        for(double i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                double k=hypot(i,j);
+                if(floor(k)==k&&k>=1&&k<=n){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
