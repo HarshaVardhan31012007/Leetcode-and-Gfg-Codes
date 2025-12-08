e219454c10d8d8a0622738032350bb074b5c8d6d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-08 10:07:32 +0530

    Time: 121 ms (0%), Space: 228.3 MB (8.33%) - LeetHub

--- 3767-maximize-points-after-choosing-k-tasks/3767-maximize-points-after-choosing-k-tasks.cpp
+++ 3767-maximize-points-after-choosing-k-tasks/3767-maximize-points-after-choosing-k-tasks.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    long long maxPoints(vector<int>& technique1, vector<int>& technique2, int k) {
+    long long maxPoints(vector<int>& t1, vector<int>& t2, int k) {
         // unordered_map<int,bool>mpp;
         // long long ans=0;
         // int n=technique1.size();
@@ -59,23 +59,46 @@ public:
 
 
 
+        // long long ans=0;
+        // int n=technique1.size();
+        // priority_queue<int,vector<int>,greater<>>pq;
+        // int size=0;
+        // for(int i=0;i<n;i++){
+        //     int maxi=max(technique1[i],technique2[i]);
+        //     if(maxi==technique2[i]){
+        //         size++;
+        //         ans+=maxi;
+        //         pq.push(technique2[i]-technique1[i]);
+        //         if(size>n-k){
+        //             ans-=pq.top();
+        //             pq.pop();
+        //         }
+        //     }
+        //     else
+        //     ans+=maxi;
+        // }
+        // return ans;
+
+
+
+
         long long ans=0;
-        int n=technique1.size();
-        priority_queue<int,vector<int>,greater<>>pq;
-        int size=0;
+        int n=t1.size();
+        vector<pair<int,int>>diff;
         for(int i=0;i<n;i++){
-            int maxi=max(technique1[i],technique2[i]);
-            if(maxi==technique2[i]){
-                size++;
-                ans+=maxi;
-                pq.push(technique2[i]-technique1[i]);
-                if(size>n-k){
-                    ans-=pq.top();
-                    pq.pop();
-                }
+            diff.push_back({t1[i]-t2[i],i});
+        }
+        sort(diff.rbegin(),diff.rend());
+        for(int i=0;i<k;i++){
+            ans+=t1[diff[i].second];
+        }
+        for(int i=k;i<n;i++){
+            if(diff[i].first>0){
+                ans+=t1[diff[i].second];
             }
-            else
-            ans+=maxi;
+            else{
+                ans+=t2[diff[i].second];
+            }
         }
         return ans;
     }
