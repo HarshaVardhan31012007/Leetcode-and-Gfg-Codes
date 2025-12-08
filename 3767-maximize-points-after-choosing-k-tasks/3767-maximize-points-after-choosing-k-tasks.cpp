01719ff6ce889517151a34739f43e0919feace3b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-08 09:42:45 +0530

    Time: 20 ms (8.33%), Space: 210.1 MB (8.33%) - LeetHub

--- 3767-maximize-points-after-choosing-k-tasks/3767-maximize-points-after-choosing-k-tasks.cpp
+++ 3767-maximize-points-after-choosing-k-tasks/3767-maximize-points-after-choosing-k-tasks.cpp
@@ -34,32 +34,26 @@ public:
        
         long long ans=0;
         int n=technique1.size();
-        vector<bool>visited(n,0);
+        vector<int>diff;
         int size=0;
         for(int i=0;i<n;i++){
             int maxi=max(technique1[i],technique2[i]);
             if(maxi==technique2[i]){
-                visited[i]=1;
                 size++;
+                diff.push_back(technique2[i]-technique1[i]);
             }
             ans+=maxi;
         }
         if(n-size>=k) return ans;
         int count=k-(n-size);
-        vector<pair<int,int>>diff;
-        for(int i=0;i<n;i++){
-            diff.push_back({technique2[i]-technique1[i],i});
-        }
         sort(diff.begin(),diff.end());
         for(auto &each:diff){
-            if(visited[each.second]){
-                if(count>0){
-                    ans-=each.first;
-                    count--;
-                }
-                else
-                    break;
+            if(count>0){
+                ans-=each;
+                count--;
             }
+            else
+            break;
         }
         return ans;
     }
