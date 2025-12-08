323a83b5dc4f1c2e20ce376789db090c3696ef98
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-08 22:14:21 +0530

    Time: 0 ms (100%), Space: 7.8 MB (84.51%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -50,13 +50,35 @@ public:
 
 
 
+        // int ans=0;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=i+1;j<=n;j++){
+        //         int k=(int)sqrt(i*i+j*j*1.0);//k is always greater than or equal to 1
+        //         if(k<=n&&(k*k)==(i*i+j*j)){
+        //             ans+=2;//a2+b2 is same as b2+a2
+        //         }
+        //     }
+        // }
+        // return ans;
+
+
+
         int ans=0;
-        for(int i=1;i<=n;i++){
-            for(int j=i+1;j<=n;j++){
-                int k=(int)sqrt(i*i+j*j*1.0);//k is always greater than or equal to 1
-                if(k<=n&&(k*k)==(i*i+j*j)){
-                    ans+=2;//a2+b2 is same as b2+a2
+        for(int c=1;c<=n;c++){
+            int target=c*c;
+            int s=1;
+            int e=c-1;//as it is c2+k2>=c2 min k is 1
+            while(s<=e){
+                if(s*s+e*e==target){
+                   ans+=2;
+                   s++;
+                   e--;
                 }
+                else if(s*s+e*e<target){
+                    s++;
+                }
+                else
+                e--;
             }
         }
         return ans;
