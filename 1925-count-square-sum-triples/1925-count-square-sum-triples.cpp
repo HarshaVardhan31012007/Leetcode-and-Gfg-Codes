697570d5f59a179f8506d5a42d7b73c7ddce16c9
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-08 21:51:26 +0530

    Time: 3 ms (90.49%), Space: 7.9 MB (21.68%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -25,11 +25,23 @@ public:
 
 
 
+        // int ans=0;
+        // for(double i=1;i<=n;i++){
+        //     for(double j=1;j<=n;j++){
+        //         double k=sqrt(i*i+j*j*1.0);
+        //         if((int)k==k&&k>=1&&k<=n){
+        //             ans++;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+
         int ans=0;
-        for(double i=1;i<=n;i++){
-            for(double j=1;j<=n;j++){
-                double k=sqrt(i*i+j*j*1.0);
-                if((int)k==k&&k>=1&&k<=n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                int k=(int)sqrt(i*i+j*j*1.0);//k is always greater than or equal to 1
+                if(k<=n&&(k*k)==(i*i+j*j)){
                     ans++;
                 }
             }
