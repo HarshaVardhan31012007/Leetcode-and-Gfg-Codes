4676161af3f2d37710e5ba7c671b94e294333333
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-09 15:40:43 +0530

    Time: 568 ms (74.45%), Space: 389.2 MB (57.3%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    // const int mod=1e9+7;
+    // int specialTriplets(vector<int>& nums) {
+    //     long long n=nums.size();
+    //     vector<long long>prefix(n,0);
+    //     unordered_map<long long,long long>mpp;
+    //     mpp[nums[0]]=1;
+    //     for(long long i=1;i<n;i++){
+    //         auto it=mpp.find(nums[i]*2);
+    //         if(it!=mpp.end())
+    //         prefix[i]=(it->second)%mod;
+    //         mpp[nums[i]]++;
+    //     }
+    //     vector<long long>suffix(n,0);
+    //     unordered_map<long long,long long>mpp1;
+    //     mpp1[nums[n-1]]=1;
+    //     for(long long i=n-2;i>=0;i--){
+    //         auto it=mpp1.find(nums[i]*2);
+    //         if(it!=mpp1.end())
+    //         suffix[i]=(it->second)%mod;
+    //         mpp1[nums[i]]++;
+    //     }
+    //     long long ans=0;
+    //     for(long long i=0;i<n;i++){
+    //         ans=(ans+(prefix[i]%mod*suffix[i]%mod)%mod)%mod;
+    //     }
+    //     return ans%mod;
+    // }
+
+
+
+
+
     const int mod=1e9+7;
     int specialTriplets(vector<int>& nums) {
         long long n=nums.size();
@@ -14,16 +46,14 @@ public:
         }
         vector<long long>suffix(n,0);
         unordered_map<long long,long long>mpp1;
+        long long ans=0;
         mpp1[nums[n-1]]=1;
         for(long long i=n-2;i>=0;i--){
             auto it=mpp1.find(nums[i]*2);
             if(it!=mpp1.end())
             suffix[i]=(it->second)%mod;
-            mpp1[nums[i]]++;
-        }
-        long long ans=0;
-        for(long long i=0;i<n;i++){
             ans=(ans+(prefix[i]%mod*suffix[i]%mod)%mod)%mod;
+            mpp1[nums[i]]++;
         }
         return ans%mod;
     }
