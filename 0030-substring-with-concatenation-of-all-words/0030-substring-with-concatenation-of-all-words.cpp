10a0dc34d32057e801db47d09e8813a7acf2cbbe
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-09 22:19:27 +0530

    Time: 26 ms (41.57%), Space: 23.8 MB (62.1%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int>ans;
-        unordered_map<string,int>mpp;int m=words[0].length();
-        for(int i=0;i<words.size();i++) 
-        mpp[words[i]]++;
+       int n=s.length();
+       int m=words[0].length();
+       unordered_map<string,int>freq;
+       for(auto &word:words){
+          freq[word]++;
+       }
+       vector<int>ans;
         for(int i=0;i<m;i++){
-            unordered_map<string,int>visited;
-            int count=0;
-            for(int j=i;j<s.length();j=j+m){
-                string str=s.substr(j,m);
-                if(mpp.find(str)==mpp.end()){
-                    count=0;
-                    visited.clear();
-                }
-                else{
-                    count++;
-                    visited[str]++;
-                    cout<<str<<" "<<visited[str]<<" "<<count<<endl;
-                    while(visited[str]>mpp[str]){
-                        string temp=s.substr(j-(count-1)*m,m);
-                        visited[temp]--;
-                        count--;
-                    }
-                    if(count==words.size()){
-                      ans.push_back(j-(count-1)*m);
-                    }
-                }
-            } 
-        }
-        return ans;
+          unordered_map<string,int>visited;
+          int count=0;
+          for(int j=i;j<n;j=j+m){
+              string curr=s.substr(j,m);
+              if(freq.find(curr)==freq.end()){
+                visited.clear();
+                count=0;
+              }
+              else{
+                 visited[curr]++;
+                 count++;
+                 while(visited[curr]>freq[curr]){
+                    string start=s.substr(j-(count-1)*m,m);
+                    visited[start]--;
+                    count--;
+                 }
+                 if(count==words.size()){
+                    ans.push_back(j-(count-1)*m);
+                 }
+              }
+          }
+       }
+       return ans;
     }
 };
