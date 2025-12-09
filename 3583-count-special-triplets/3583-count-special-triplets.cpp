a3a95c07e4748aad82e3dae37db4cd3e88f49987
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-09 15:44:48 +0530

    Time: 546 ms (78.83%), Space: 383.4 MB (58.03%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -32,6 +32,35 @@ public:
 
 
 
+    // const int mod=1e9+7;
+    // int specialTriplets(vector<int>& nums) {
+    //     long long n=nums.size();
+    //     vector<long long>prefix(n,0);
+    //     unordered_map<long long,long long>mpp;
+    //     mpp[nums[0]]=1;
+    //     for(long long i=1;i<n;i++){
+    //         auto it=mpp.find(nums[i]*2);
+    //         if(it!=mpp.end())
+    //         prefix[i]=(it->second)%mod;
+    //         mpp[nums[i]]++;
+    //     }
+    //     vector<long long>suffix(n,0);
+    //     unordered_map<long long,long long>mpp1;
+    //     long long ans=0;
+    //     mpp1[nums[n-1]]=1;
+    //     for(long long i=n-2;i>=0;i--){
+    //         auto it=mpp1.find(nums[i]*2);
+    //         if(it!=mpp1.end())
+    //         suffix[i]=(it->second)%mod;
+    //         ans=(ans+(prefix[i]%mod*suffix[i]%mod)%mod)%mod;
+    //         mpp1[nums[i]]++;
+    //     }
+    //     return ans%mod;
+    // }
+
+
+
+
     const int mod=1e9+7;
     int specialTriplets(vector<int>& nums) {
         long long n=nums.size();
@@ -44,15 +73,15 @@ public:
             prefix[i]=(it->second)%mod;
             mpp[nums[i]]++;
         }
-        vector<long long>suffix(n,0);
         unordered_map<long long,long long>mpp1;
         long long ans=0;
         mpp1[nums[n-1]]=1;
         for(long long i=n-2;i>=0;i--){
             auto it=mpp1.find(nums[i]*2);
-            if(it!=mpp1.end())
-            suffix[i]=(it->second)%mod;
-            ans=(ans+(prefix[i]%mod*suffix[i]%mod)%mod)%mod;
+            if(it!=mpp1.end()){
+            long long suffix=(it->second)%mod;
+            ans=(ans+(prefix[i]%mod*suffix%mod)%mod)%mod;
+            }
             mpp1[nums[i]]++;
         }
         return ans%mod;
