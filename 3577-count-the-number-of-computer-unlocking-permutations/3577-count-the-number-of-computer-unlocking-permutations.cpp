633f13f8ce7d6b453892a5a39a6375572b1042fd
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-10 18:28:14 +0530

    Time: 0 ms (100%), Space: 92.8 MB (71%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -2,13 +2,21 @@ class Solution {
 public:
     const int mod=1e9+7;
     int countPermutations(vector<int>& complexity) {
+        // long long ans=1;
+        // int fcomplex=complexity[0];
+        // int mini=fcomplex;
+        // for(int i=1;i<complexity.size();i++){
+        //      if(mini>=complexity[i]) return 0;
+        //      ans=(ans*i)%mod;
+        //      mini=min(mini,complexity[i]);
+        // }
+        // return ans;
+
+
         long long ans=1;
-        int fcomplex=complexity[0];
-        int mini=fcomplex;
         for(int i=1;i<complexity.size();i++){
-             if(mini>=complexity[i]) return 0;
+             if(complexity[i]<=complexity[0]) return 0;
              ans=(ans*i)%mod;
-             mini=min(mini,complexity[i]);
         }
         return ans;
     }
