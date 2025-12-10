fcac9eb4c1c8c1852285507c2a376e4747c2a78a
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-10 22:14:14 +0530

    Time: 0 ms (100%), Space: 11.9 MB (66.76%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,147 +1,21 @@
-class TrieNode{
-        public:
-        char data;
-        unordered_map<char,TrieNode*>children;
-        bool isterminal;
-        TrieNode(char val){
-            this->data=val;
-            isterminal=false;
-        }
-    };
-    void insertHelp(TrieNode *root,string k){
-        if(k.length()==0){
-            root->isterminal=true;
-            return;
-        }
-        char ch=k[0];
-        TrieNode *child;
-        if(root->children.count(ch)==0){
-            child=new TrieNode(ch);
-            root->children[ch]=child;
-        }
-        else{
-            child=root->children[ch];
-        }
-        insertHelp(child,k.substr(1));
-    }
-    bool searchWord(TrieNode *root,string k){
-        if(k.length()==0){
-            return root->isterminal==true;
-        }
-        char ch=k[0];
-        if(root->children.count(ch)==0){
-            return false;
-        }
-        return searchWord(root->children[ch],k.substr(1));
-    }
-    bool searchPrefix(TrieNode *root,string k){
-        if(k.length()==0){
-            return true;
-        }
-        char ch=k[0];
-        if(root->children.count(ch)==0){
-            return false;
-        }
-        return searchPrefix(root->children[ch],k.substr(1));
-    }
-
 class Solution {
 public:
-    // void func(TrieNode *root,string &k){
-    //     if(root->children.size()>1||root->isterminal||root->children.empty()) return;
-    //     k=k+root->children.begin()->first;
-    //     func(root->children.begin()->second,k);
-    // }
-
-      void func(TrieNode *root,string &k){
-        if(root->children.size()>1||root->isterminal||root->children.empty()) return;
-        pair<char,TrieNode*>p;
-        for(auto each:root->children)
-        p=each;
-        k=k+p.first;
-        func(p.second,k);
-    }
-
     string longestCommonPrefix(vector<string>& strs) {
-        // string output="";int mini=INT_MAX;
-        // for(int i=0;i<strs.size();i++){
-        //     int p=strs[i].size();
-        //     mini=min(mini,p);
-        // }
-        // for(int i=0;i<mini;i++){
-        //     bool issame=true;
-        //     char ch=strs[0][i];
-        //     for(int j=1;j<strs.size();j++){
-        //         if(strs[j][i]!=ch){
-        //         issame=false;
-        //         break;
-        //         }
-        //     }
-        //     if(issame)
-        //     output+=ch;
-        //     else
-        //     break;
-        // }
-        // return output;
-
-    
-        // string output="";
-        // for(int i=0; ;i++){
-        //     bool issame=true;
-        //     if(i>=strs[0].size()) break;
-        //     char ch=strs[0][i];
-        //     for(int j=1;j<strs.size();j++){
-        //         if(i>=strs[j].size()){
-        //             issame=false;
-        //             break;
-        //         }
-        //         if(strs[j][i]!=ch){
-        //         issame=false;
-        //         break;
-        //         }
-        //     }
-        //     if(issame)
-        //     output+=ch;
-        //     else
-        //     break;
-        // }
-        // return output;
-
-
-
-        // string output="";
-        // int i=0;
-        // while(true){
-        //     char ch=0;
-        //     for(int j=0;j<strs.size();j++){
-        //         if(i>=strs[j].size()){
-        //             ch=0;
-        //             break;
-        //         }
-        //         if(ch==0){
-        //             ch=strs[j][i];
-        //         }
-        //         else if(strs[j][i]!=ch){
-        //         ch=0;
-        //         break;
-        //         }
-        //     }
-        //     if(ch==0)
-        //     break;
-        //     output+=ch;
-        //     i++;
-        // }
-        // return output;
-
-
-        TrieNode *root=new TrieNode('-');
-
-        for(int i=0;i<strs.size();i++){
-            insertHelp(root,strs[i]);
+        int i=0;
+        string ans="";
+        while(1){
+            char ch=0;
+            for(int j=0;j<strs.size();j++){
+                if(i>=strs[j].length()) return ans;
+                if(ch==0)
+                ch=strs[j][i];
+                else{
+                    if(ch!=strs[j][i]) return ans;
+                }
+            }
+            ans+=strs[0][i];
+            i++;
         }
-        
-        string output="";
-        func(root,output);
-        return output;
+        return "";
     }
 };
