4709ab33ed8598c0cdd10c150228bd337e0d5d59
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-11 12:32:32 +0530

    Time: 53 ms (15.95%), Space: 26.2 MB (25.57%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,51 +1,14 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<string,int>mpp;
         vector<string>ans;
-        unordered_map<string,int>visited;
-        for(int i=0;i<10;i++){
-            for(int j=i;j<s.length();j=j+10){
-               string temp=s.substr(j,10);
-               visited[temp]++;
-               if(visited[temp]>1){
-                 ans.push_back(temp);
-                 visited[temp]=INT_MIN;
-               }
-            }
+        for(int i=0;i<s.length();i++){
+            string curr=s.substr(i,10);
+            mpp[curr]++;
+            if(mpp[curr]==2)
+            ans.push_back(curr);
         }
         return ans;
     }
-
-
-    // vector<string> findRepeatedDnaSequences(string s) {
-    //     vector<string>ans;
-    //     unordered_map<string,int>visited;
-    //         if(s.length()<10) return ans;
-    //         for(int j=0;j<s.length()-9;j++){
-    //            string temp=s.substr(j,10);
-    //            visited[temp]++;
-    //            if(visited[temp]==2){
-    //              ans.push_back(temp);
-    //            }
-    //         // if(visited[temp]>1){
-    //         //      ans.push_back(temp);
-    //         //      visited[temp]=INT_MIN;
-    //         //    }
-    //         }
-    //     return ans;
-    // }
-
-    // vector<string> findRepeatedDnaSequences(string s) {
-    //     vector<string>ans;
-    //     unordered_map<string,int>visited;
-    //         if(s.length()<10) return ans;
-    //         for(int j=0;j<s.length()-9;j++){
-    //            string temp=s.substr(j,10);
-    //            visited[temp]++;
-    //         }
-    //         for(auto &[substr,freq]:visited){
-    //             if(freq>1)ans.push_back(substr);
-    //         }
-    //     return ans;
-    // }
 };
