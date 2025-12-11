75a43ecc8e8eed42bf1b3d7187c40247d0cbb029
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-11 12:50:22 +0530

    Time: 43 ms (36.71%), Space: 26.1 MB (57.44%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,13 +1,43 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
+        // unordered_map<string,int>mpp;
+        // vector<string>ans;
+        // for(int i=0;i<=(int)s.length()-10;i++){
+        //     string curr=s.substr(i,10);
+        //     mpp[curr]++;
+        //     if(mpp[curr]==2)
+        //     ans.push_back(curr);
+        // }
+        // return ans;
+
+
+        // unordered_map<string,int>mpp;
+        // vector<string>ans;
+        // for(int i=0;i<=(int)s.length()-10;i++){
+        //     string curr=s.substr(i,10);
+        //     mpp[curr]++;
+        //     if(mpp[curr]>1){
+        //     ans.push_back(curr);
+        //     mpp[curr]=INT_MIN;
+        //     }
+        // }
+        // return ans;
+
+
+
         unordered_map<string,int>mpp;
         vector<string>ans;
-        for(int i=0;i<=(int)s.length()-10;i++){
-            string curr=s.substr(i,10);
-            mpp[curr]++;
-            if(mpp[curr]==2)
-            ans.push_back(curr);
+        string temp=s.substr(0,10);
+        mpp[temp]++;
+        int j=10;
+        while(j<s.length()){
+            temp+=s[j];
+            temp.erase(temp.begin(),temp.begin()+1);
+            mpp[temp]++;
+            if(mpp[temp]==2)
+            ans.push_back(temp);
+            j++;
         }
         return ans;
     }
