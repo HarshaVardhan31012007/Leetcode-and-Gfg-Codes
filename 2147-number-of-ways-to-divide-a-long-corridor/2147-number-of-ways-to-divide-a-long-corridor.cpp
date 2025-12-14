bd816af2d9c3e07681a8e8e9f17f38c9c930d548
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-14 15:08:42 +0530

    Time: 2408 ms (5%), Space: 87 MB (14%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    // const int mod=1e9+7;
-    // int solve(string &corridor,int i,vector<int>&dp,vector<int>&prefix){
-    //     if(i>=corridor.length()) return 1;
-    //     if(dp[i]!=-1) return dp[i];
-    //     int idx=lower_bound(prefix.begin()+i,prefix.end(),(i==0?0:prefix[i-1])+2)-prefix.begin();
-    //     if(idx==corridor.length()) return 0;
-    //     if(idx==corridor.length()-1) return 1;
-    //     int ans=0;
-    //     for(int k=idx+1;k<corridor.length();k++){
-    //           if(corridor[k]=='S'){
-    //              ans=(ans+solve(corridor,k,dp,prefix))%mod;
-    //              break;
-    //           }
-    //           ans=(ans+solve(corridor,k+1,dp,prefix))%mod;
-    //     }
-    //     return dp[i]=ans;
-    // }
-    // int numberOfWays(string corridor) {
-    //     vector<int>prefix(corridor.length(),0);
-    //     prefix[0]=(corridor[0]=='S'?1:0);
-    //     for(int i=1;i<corridor.length();i++){
-    //         prefix[i]=prefix[i-1]+(corridor[i]=='S'?1:0);
-    //     }
-    //     vector<int>dp(corridor.length()+1,-1);
-    //     return solve(corridor,0,dp,prefix);
-    // }
+    const int mod=1e9+7;
+    int solve(string &corridor,int i,vector<int>&dp,vector<int>&prefix){
+        if(i>=corridor.length()) return 1;
+        if(dp[i]!=-1) return dp[i];
+        int idx=lower_bound(prefix.begin(),prefix.end(),(i==0?0:prefix[i-1])+2)-prefix.begin();
+        if(idx==corridor.length()) return 0;
+        if(idx==corridor.length()-1) return 1;
+        int ans=0;
+        for(int k=idx+1;k<=corridor.length();k++){
+              if(corridor[k]=='S'){
+                 ans=(ans+solve(corridor,k,dp,prefix))%mod;
+                 break;
+              }
+              ans=(ans+solve(corridor,k,dp,prefix))%mod;
+        }
+        return dp[i]=ans;
+    }
+    int numberOfWays(string corridor) {
+        vector<int>prefix(corridor.length(),0);
+        prefix[0]=(corridor[0]=='S'?1:0);
+        for(int i=1;i<corridor.length();i++){
+            prefix[i]=prefix[i-1]+(corridor[i]=='S'?1:0);
+        }
+        vector<int>dp(corridor.length()+1,-1);
+        return solve(corridor,0,dp,prefix);
+    }
 
 
 
@@ -60,26 +60,26 @@ public:
 
 
 
-    const int mod=1e9+7;
-    int numberOfWays(string corridor) {
-       long long count=0;
-       long long ans=1;
-       long long prefix=0;
-       for(auto &each:corridor){
-            if(each=='S'){
-                prefix++;
-            }
-            if(prefix==2){
-                count++;
-            }
-            if(prefix==3){
-                ans=(1LL*ans*count)%mod;
-                prefix=1;
-                count=0;
-            }
-       }
-       if(prefix!=2)
-       return 0;
-       return ans;
-    }
+    // const int mod=1e9+7;
+    // int numberOfWays(string corridor) {
+    //    long long count=0;
+    //    long long ans=1;
+    //    long long prefix=0;
+    //    for(auto &each:corridor){
+    //         if(each=='S'){
+    //             prefix++;
+    //         }
+    //         if(prefix==2){
+    //             count++;
+    //         }
+    //         if(prefix==3){
+    //             ans=(1LL*ans*count)%mod;
+    //             prefix=1;
+    //             count=0;
+    //         }
+    //    }
+    //    if(prefix!=2)
+    //    return 0;
+    //    return ans;
+    // }
 };
