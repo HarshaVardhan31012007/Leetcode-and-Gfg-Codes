92a484992725dea0e60b0a8c2989bc7f037537ff
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-15 23:42:00 +0530

    Time: 0 ms (100%), Space: 107 MB (57.99%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     long long getDescentPeriods(vector<int>& prices) {
-        int i=0;
-        int j=1;
-        long long ans=0;
-        while(j<prices.size()){
-            while(j<prices.size()&&prices[j-1]-prices[j]==1){
-                j++;
-            }
-            long long len=j-i;
-            ans+=(len*(len-1))/2;
-            i=j;
-            j++;
-        }
-        return ans+prices.size();
+        // int i=0;
+        // int j=1;
+        // long long ans=0;
+        // while(j<prices.size()){
+        //     while(j<prices.size()&&prices[j-1]-prices[j]==1){
+        //         j++;
+        //     }
+        //     long long len=j-i;
+        //     ans+=(len*(len-1))/2;
+        //     i=j;
+        //     j++;
+        // }
+        // return ans+prices.size();
 
 
 
@@ -33,5 +33,17 @@ public:
         //     j++;
         // }
         // return ans+prices.size();
+
+
+        long long count=1;
+        long long ans=1;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i-1]-prices[i]==1)
+            count++;
+            else
+            count=1;
+            ans+=count;
+        }
+        return ans;
     }
 };
