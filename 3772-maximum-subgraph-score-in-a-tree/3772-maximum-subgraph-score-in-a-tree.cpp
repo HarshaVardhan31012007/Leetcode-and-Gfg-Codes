a54cba6d85d7c560725b08dbfeba27fad1d3375c
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-16 13:13:28 +0530

    Time: 585 ms (13.21%), Space: 449.5 MB (5.05%) - LeetHub

--- 3772-maximum-subgraph-score-in-a-tree/3772-maximum-subgraph-score-in-a-tree.cpp
+++ 3772-maximum-subgraph-score-in-a-tree/3772-maximum-subgraph-score-in-a-tree.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
-    int indp[100005], outdp[100005];
-    unordered_map<int, vector<int>> tree;
-    vector<int> good;
-
-    void dfs1 (int node, int parentNode) {
-        indp[node] = (good[node] == 1 ? 1 : -1);
-        for (int childNode: tree[node]) {
-            if (childNode == parentNode) continue;
-
-            dfs1(childNode, node);
-            indp[node] += max(0, indp[childNode]);
+    void dfs(int s,int p,unordered_map<int,list<int>>&adjList,vector<int>&good,vector<int>&down){
+        down[s]=good[s];
+        for(auto &c:adjList[s]){
+            if(c==p) continue;
+            dfs(c,s,adjList,good,down);
+            down[s]+=(down[c]>0?down[c]:0);
         }
     }
-
-    void dfs2 (int node, int parentNode) {
-        if (parentNode == -1) {
-            outdp[node] = 0;
-        } else {
-            int val = outdp[parentNode] + (indp[parentNode] - max(0, indp[node]));
-            outdp[node] += max(0, val);
+    void dfs1(int s,int p,unordered_map<int,list<int>>&adjList,vector<int>&up,vector<int>&down){
+        if(p==-1){
+            up[s]=0;
         }
-
-        for (int childNode: tree[node]) {
-            if (childNode == parentNode) continue;
-            dfs2(childNode, node);
+        else{
+            int val=up[p]+down[p]-(down[s]>0?down[s]:0);
+            up[s]=max(0,val);
+        }
+        for(auto &c:adjList[s]){
+            if(c==p) continue;
+            dfs1(c,s,adjList,up,down);
         }
     }
-
-    vector<int> maxSubgraphScore(int n, vector<vector<int>>& edges, vector<int>& _good) {
-        good = _good;
-        for (vector<int> &edge: edges) {
-            int u = edge[0], v = edge[1];
-            tree[u].push_back(v);
-            tree[v].push_back(u);
+    vector<int> maxSubgraphScore(int n, vector<vector<int>>& edges, vector<int>& good) {
+        unordered_map<int,list<int>>adjList;
+        for(auto &edge:edges){
+            adjList[edge[0]].push_back(edge[1]);
+            adjList[edge[1]].push_back(edge[0]);
         }
-
-        dfs1(0, -1);
-        dfs2(0, -1);
-
-        vector<int> ans(n);
-        for (int i = 0; i < n; i++) {
-            ans[i] = indp[i] + outdp[i];
-        }
-
+        vector<int>up(n,0);
+        vector<int>down(n,0);
+        for(auto &each:good)
+        each=(each==0?-1:each);//score of node
+        dfs(0,-1,adjList,good,down);
+        dfs1(0,-1,adjList,up,down);
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        ans.push_back(up[i]+down[i]);
         return ans;
     }
 };
