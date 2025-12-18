46ade18092d60364673e1ecaecfd4548255fafd2
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-18 19:37:18 +0530

    Time: 4 ms (5.52%), Space: 13.3 MB (5.08%) - LeetHub

---
+++ 1023-camelcase-matching/1023-camelcase-matching.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    class Trie{
+        public:
+
+        char data;
+        unordered_map<char,Trie*>children;
+        bool isterminal;
+        int index;
+        
+        Trie(char val){
+            this->data=val;
+            this->isterminal=false;
+            this->index=-1;
+        } 
+
+        void insertHelp(Trie* root,string word,int i,int &idx){
+            if(i>=word.length()){
+                root->isterminal=true;
+                root->index=idx;
+                return;
+            }
+            if(root->children.find(word[i])==root->children.end())
+            root->children[word[i]]=new Trie(word[i]);
+            insertHelp(root->children[word[i]],word,i+1,idx);
+        }
+    };
+    void solve(Trie* root,string &pattern,int i,vector<bool>&ans){
+        if(i>=pattern.size()){
+           if(root->isterminal)
+           ans[root->index]=1;
+           for(int j=0;j<26;j++){
+               if(root->children.find(j+'a')!=root->children.end()){
+                   solve(root->children[j+'a'],pattern,i,ans);
+               }
+           }
+           return;
+        }
+       for(auto &each:root->children){
+          if(isupper(each.first)){
+             //should match
+             if(each.first!=pattern[i]) continue;
+             solve(each.second,pattern,i+1,ans);
+          }
+          else{
+             //match
+             if(each.first==pattern[i]){
+                solve(each.second,pattern,i+1,ans);
+             }
+             //not match
+             else{
+               solve(each.second,pattern,i,ans);
+             }
+          }
+       }
+    }
+    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+        Trie* root=new Trie('-');
+        int n=queries.size();
+        for(int i=0;i<queries.size();i++){
+            root->insertHelp(root,queries[i],0,i);
+        }  
+        vector<bool>ans(n,0);
+        solve(root,pattern,0,ans);
+        return ans;
+    } 
+};
