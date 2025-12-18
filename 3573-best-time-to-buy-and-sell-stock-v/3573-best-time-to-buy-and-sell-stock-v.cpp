f07eeede049ef7d0ee270e3b528090c52f6747f3
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-18 09:34:45 +0530

    Time: 736 ms (35.54%), Space: 345.4 MB (19.7%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
@@ -68,34 +68,54 @@ public:
 
 
 
-    long long solve(vector<int>&prices,int k,int i,int prev,vector<vector<vector<long long>>>&dp){    
-        if(k==0) return 0;
-        if(i==prices.size()-1){
-            return (prev==0?0:(prev==1?prices[i]:-prices[i]));
-        }
-        if(dp[i][k][prev]!=-1) return dp[i][k][prev];
+    // long long solve(vector<int>&prices,int k,int i,int prev,vector<vector<vector<long long>>>&dp){    
+    //     if(k==0) return 0;
+    //     if(i==prices.size()-1){
+    //         return (prev==0?0:(prev==1?prices[i]:-prices[i]));
+    //     }
+    //     if(dp[i][k][prev]!=-1) return dp[i][k][prev];
         
-        if(prev==0){
-           long long buy=-prices[i]+solve(prices,k,i+1,1,dp);
-           long long sell=prices[i]+solve(prices,k,i+1,2,dp);
-           long long skip=solve(prices,k,i+1,prev,dp);
-           return dp[i][k][prev]=max(buy,max(sell,skip));
-        }
+    //     if(prev==0){
+    //        long long buy=-prices[i]+solve(prices,k,i+1,1,dp);
+    //        long long sell=prices[i]+solve(prices,k,i+1,2,dp);
+    //        long long skip=solve(prices,k,i+1,prev,dp);
+    //        return dp[i][k][prev]=max(buy,max(sell,skip));
+    //     }
         
-        else if(prev==1){
-           long long sell=prices[i]+solve(prices,k-1,i+1,0,dp);
-           long long skip=solve(prices,k,i+1,prev,dp);
-           return dp[i][k][prev]=max(sell,skip);
-        }
+    //     else if(prev==1){
+    //        long long sell=prices[i]+solve(prices,k-1,i+1,0,dp);
+    //        long long skip=solve(prices,k,i+1,prev,dp);
+    //        return dp[i][k][prev]=max(sell,skip);
+    //     }
 
-        else { // else if(prev==2)
-           long long buy=-prices[i]+solve(prices,k-1,i+1,0,dp);
-           long long skip=solve(prices,k,i+1,prev,dp);
-           return dp[i][k][prev]=max(buy,skip);
-        }
-    }
+    //     else { // else if(prev==2)
+    //        long long buy=-prices[i]+solve(prices,k-1,i+1,0,dp);
+    //        long long skip=solve(prices,k,i+1,prev,dp);
+    //        return dp[i][k][prev]=max(buy,skip);
+    //     }
+    // }
+    // long long maximumProfit(vector<int>& prices, int k) {
+    //     vector<vector<vector<long long >>>dp(prices.size()+1,vector<vector<long long>>(k+1,vector<long long>(3,-1)));
+    //     return solve(prices,k,0,0,dp);
+    // }
+
+
+
+
+   
     long long maximumProfit(vector<int>& prices, int k) {
-        vector<vector<vector<long long >>>dp(prices.size()+1,vector<vector<long long>>(k+1,vector<long long>(3,-1)));
-        return solve(prices,k,0,0,dp);
+        vector<vector<vector<long long >>>dp(prices.size()+1,vector<vector<long long>>(k+1,vector<long long>(3,0)));
+        for(int i=1;i<=k;i++){
+                dp[prices.size()-1][i][1]=prices[prices.size()-1];
+                dp[prices.size()-1][i][2]=-prices[prices.size()-1];
+        }
+        for(int i=prices.size()-2;i>=0;i--){
+            for(int j=1;j<=k;j++){
+                dp[i][j][0]=max(dp[i+1][j][0],max(-prices[i]+dp[i+1][j][1],prices[i]+dp[i+1][j][2]));
+                dp[i][j][1]=max(prices[i]+dp[i+1][j-1][0],dp[i+1][j][1]);
+                dp[i][j][2]=max(dp[i+1][j][2],-prices[i]+dp[i+1][j-1][0]);
+            }
+        }
+        return dp[0][k][0];
     }
 };
