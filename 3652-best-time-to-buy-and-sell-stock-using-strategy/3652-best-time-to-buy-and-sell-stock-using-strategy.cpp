371625ed575c065af09705eb6778fcf7451c7da6
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-18 16:43:26 +0530

    Time: 7 ms (83.81%), Space: 302.8 MB (69.71%) - LeetHub

--- 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.cpp
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -1,27 +1,57 @@
 class Solution {
 public:
     long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
-        long long ans=0;
+    //     long long ans=0;
+    //     int n=prices.size();
+    //     for(int i=0;i<n;i++){
+    //         ans+=strategy[i]*prices[i];
+    //     }
+    //     long long curr=ans;
+    //     for(int i=0;i<k;i++){
+    //         curr-=strategy[i]*prices[i];
+    //         if(i>=k/2)
+    //         curr+=prices[i];
+    //     }
+    //     ans=max(ans,curr);
+    //     for(int i=k;i<n;i++){
+    //         //adddition
+    //         curr-=strategy[i]*prices[i];
+    //         curr+=prices[i];
+    //         //removal
+    //         curr+=strategy[i-k]*prices[i-k];
+    //         curr-=prices[i-k/2];
+    //         ans=max(ans,curr);
+    //     }
+    //     return ans;
+    // }
+
+
+
+
+        long long tprofit=0;
         int n=prices.size();
         for(int i=0;i<n;i++){
-            ans+=strategy[i]*prices[i];
+            tprofit+=strategy[i]*prices[i];
         }
-        long long curr=ans;
-        for(int i=0;i<k;i++){
-            curr-=strategy[i]*prices[i];
-            if(i>=k/2)
-            curr+=prices[i];
+        int i=0;
+        int j=0;
+        long long maxGain=0;
+        long long original=0;
+        long long window=0;
+        while(j<n){
+            original+=strategy[j]*prices[j];
+            if(j-i+1>k/2){
+                window+=prices[j];
+            }
+            if(j-i+1>k){
+                original-=strategy[i]*prices[i];
+                window-=prices[j-k/2];//prices[i+k/2]
+                i++;
+            }
+            if(j-i+1==k)
+            maxGain=max(maxGain,window-original);
+            j++;
         }
-        ans=max(ans,curr);
-        for(int i=k;i<n;i++){
-            //adddition
-            curr-=strategy[i]*prices[i];
-            curr+=prices[i];
-            //removal
-            curr+=strategy[i-k]*prices[i-k];
-            curr-=prices[i-k/2];
-            ans=max(ans,curr);
-        }
-        return ans;
+        return tprofit+maxGain;
     }
 };
