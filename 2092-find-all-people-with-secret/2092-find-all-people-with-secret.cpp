b3ca2c9c2bc8c8ba92152fceb1fa288bc66f1073
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-20 13:27:56 +0530

    Time: 196 ms (52.18%), Space: 194.9 MB (46.47%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
-    void dfs(int person,int t,vector<vector<pair<int,int>>>&adjList,vector<int>&time){
-            for(auto &adj:adjList[person]){
-               if(adj.second>=t&&time[adj.first]>adj.second){
-                  time[adj.first]=adj.second;
-                  dfs(adj.first,adj.second,adjList,time);
-               }
-            }
+    // void dfs(int person,int t,vector<vector<pair<int,int>>>&adjList,vector<int>&time){
+    //         for(auto &adj:adjList[person]){
+    //            if(adj.second>=t&&time[adj.first]>adj.second){
+    //               time[adj.first]=adj.second;
+    //               dfs(adj.first,adj.second,adjList,time);
+    //            }
+    //         }
+    // }
+
+    int find(int node,vector<int>&parent){
+        if(parent[node]==node) return node;
+        return parent[node]=find(parent[node],parent);
+    }
+    void unionHelp(int x,int y,vector<int>&parent,vector<int>&rank){
+        int u=find(x,parent);
+        int v=find(y,parent);
+        if(rank[u]<rank[v]){
+            parent[u]=v;
+            rank[v]++;
+        }
+        else{
+            parent[v]=u;
+            rank[u]++;
+        }
     }
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
 
@@ -160,32 +177,71 @@ public:
 
 
 
-            vector<vector<pair<int,int>>>adjList(n);
-            for(auto &meeting:meetings){
-                adjList[meeting[0]].push_back({meeting[1],meeting[2]});
-                adjList[meeting[1]].push_back({meeting[0],meeting[2]});
+            // vector<vector<pair<int,int>>>adjList(n);
+            // for(auto &meeting:meetings){
+            //     adjList[meeting[0]].push_back({meeting[1],meeting[2]});
+            //     adjList[meeting[1]].push_back({meeting[0],meeting[2]});
+            // }
+            // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+            // pq.push({0,0});
+            // pq.push({0,firstPerson});
+            // vector<int>ans;
+            // vector<int>time(n,INT_MAX);
+            // time[0]=0;
+            // time[firstPerson]=0;
+            // while(!pq.empty()){
+            //     auto top=pq.top();
+            //     pq.pop();
+            //     if (top.first > time[top.second]) continue;
+            //     for(auto &adj:adjList[top.second]){
+            //         if(top.first<=adj.second&&adj.second<time[adj.first]){
+            //             time[adj.first]=adj.second;
+            //             pq.push({time[adj.first],adj.first});
+            //         }
+            //     }
+            // }
+            // for(int i=0;i<n;i++){
+            //     if(time[i]!=INT_MAX)
+            //     ans.push_back(i);
+            // }
+            // return ans;
+
+
+
+            vector<int>parent(n,0);
+            vector<int>rank(n,0);
+            for(int i=0;i<n;i++){
+                parent[i]=i;
             }
-            priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
-            pq.push({0,0});
-            pq.push({0,firstPerson});
-            vector<int>ans;
-            vector<int>time(n,INT_MAX);
-            time[0]=0;
-            time[firstPerson]=0;
-            while(!pq.empty()){
-                auto top=pq.top();
-                pq.pop();
-                if (top.first > time[top.second]) continue;
-                for(auto &adj:adjList[top.second]){
-                    if(top.first<=adj.second&&adj.second<time[adj.first]){
-                        time[adj.first]=adj.second;
-                        pq.push({time[adj.first],adj.first});
+            sort(meetings.begin(),meetings.end(),[&](vector<int>&a,vector<int>&b){
+                 if(a[2]!=b[2])
+                 return a[2]<b[2];
+                 else if(a[1]!=b[1])
+                 return a[1]<b[1];
+                 else
+                 return a[0]<b[0];
+            });
+            unionHelp(0,firstPerson,parent,rank);
+            for(int i=0;i<meetings.size();){
+                int time=meetings[i][2];
+                vector<int>curr;
+                while(i<meetings.size()&&time==meetings[i][2]){
+                    unionHelp(meetings[i][0],meetings[i][1],parent,rank);
+                    curr.push_back(meetings[i][0]);
+                    curr.push_back(meetings[i][1]);
+                    i++;
+                }
+                for(auto &each:curr){
+                    if(parent[each]!=each&&find(each,parent)!=find(0,parent)){
+                        parent[each]=each;//reset logic as meetings is ended and no connection to secret knowing people //ony two cases wither all connected to 0 or all not connected to 0
                     }
                 }
             }
+            vector<int>ans;
             for(int i=0;i<n;i++){
-                if(time[i]!=INT_MAX)
-                ans.push_back(i);
+                if(find(i,parent)==find(0,parent)){
+                    ans.push_back(i);
+                }
             }
             return ans;
         }
