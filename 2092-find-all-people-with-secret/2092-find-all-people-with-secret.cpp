f5486f894aba8da79065884b0654f6f164036d3b
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-20 10:43:58 +0530

    Time: 116 ms (84.78%), Space: 216.7 MB (33.97%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -31,7 +31,40 @@ public:
 
 
 
-        vector<vector<pair<int,int>>>adjList(n);
+    //     vector<vector<pair<int,int>>>adjList(n);
+    //     for(auto &meeting:meetings){
+    //         adjList[meeting[0]].push_back({meeting[1],meeting[2]});
+    //         adjList[meeting[1]].push_back({meeting[0],meeting[2]});
+    //     }
+    //     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+    //     pq.push({0,0});
+    //     pq.push({0,firstPerson});
+    //     vector<int>ans;
+    //     vector<int>time(n,INT_MAX);
+    //     time[0]=0;
+    //     time[firstPerson]=0;
+    //     while(!pq.empty()){
+    //         auto top=pq.top();
+    //         pq.pop();
+    //         if (top.first > time[top.second]) continue;
+    //         for(auto &adj:adjList[top.second]){
+    //             if(top.first<=adj.second&&adj.second<time[adj.first]){
+    //                 time[adj.first]=adj.second;
+    //                 pq.push({time[adj.first],adj.first});
+    //             }
+    //         }
+    //     }
+    //     for(int i=0;i<n;i++){
+    //         if(time[i]!=INT_MAX)
+    //         ans.push_back(i);
+    //     }
+    //     return ans;
+    // }
+
+
+
+
+     vector<vector<pair<int,int>>>adjList(n);
         for(auto &meeting:meetings){
             adjList[meeting[0]].push_back({meeting[1],meeting[2]});
             adjList[meeting[1]].push_back({meeting[0],meeting[2]});
