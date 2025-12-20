bb88a832e7bb11f5f71702f83664c2cfd5837e33
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-20 12:00:07 +0530

    Time: 143 ms (73.37%), Space: 216.4 MB (33.97%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,7 +1,104 @@
 class Solution {
 public:
+    void dfs(int person,int t,vector<vector<pair<int,int>>>&adjList,vector<int>&time){
+            for(auto &adj:adjList[person]){
+               if(adj.second>=t&&time[adj.first]>adj.second){
+                  time[adj.first]=adj.second;
+                  dfs(adj.first,adj.second,adjList,time);
+               }
+            }
+    }
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        // unordered_map<int,vector<pair<int,int>>>adjList;
+
+        // map<int,vector<pair<int,int>>>mpp;
+        // for(auto &meeting:meetings){
+        //     mpp[meeting[2]].push_back({meeting[0],meeting[1]});
+        // }
+        // vector<bool>knowSecret(n,0);
+        // knowSecret[0]=1;
+        // knowSecret[firstPerson]=1;
+        // for(auto &each:mpp){
+        //     unordered_map<int,list<int>>adjList;
+        //     queue<int>q;
+        //     for(auto &[person1,person2]:each.second){
+        //         adjList[person1].push_back(person2);
+        //         adjList[person2].push_back(person1);
+        //     }
+        //     for(int i=0;i<n;i++){
+        //         if(knowSecret[i])
+        //         q.push(i);
+        //     }
+        //     while(!q.empty()){
+        //         auto front=q.front();
+        //         q.pop();
+        //         for(auto &adj:adjList[front]){
+        //             if(!knowSecret[adj]){
+        //                 q.push(adj);
+        //                 knowSecret[adj]=1;
+        //             }
+        //         }
+        //     }
+        // }
+        // vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     if(knowSecret[i])
+        //     ans.push_back(i);
+        // }
+        // return ans;
+
+
+
+        // vector<vector<pair<int,int>>>adjList(n);
+        // for(auto &meeting:meetings){
+        //     adjList[meeting[0]].push_back({meeting[1],meeting[2]});
+        //     adjList[meeting[1]].push_back({meeting[0],meeting[2]});
+        // }
+        // vector<int>time(n,INT_MAX);
+        // time[0]=0;
+        // time[firstPerson]=0;
+        // queue<pair<int,int>>q;
+        // q.push({0,0});
+        // q.push({firstPerson,0});
+        // while(!q.empty()){
+        //     auto [person,t]=q.front();
+        //     q.pop();
+        //     for(auto &adj:adjList[person]){
+        //        if(adj.second>=t&&time[adj.first]>adj.second){
+        //           time[adj.first]=adj.second;
+        //           q.push({adj.first,adj.second});
+        //        }
+        //     }
+        // }
+        // vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     if(time[i]!=INT_MAX)
+        //     ans.push_back(i);
+        // }
+        // return ans;
+
+
+
+        // vector<vector<pair<int,int>>>adjList(n);
+        // for(auto &meeting:meetings){
+        //     adjList[meeting[0]].push_back({meeting[1],meeting[2]});
+        //     adjList[meeting[1]].push_back({meeting[0],meeting[2]});
+        // }
+        // vector<int>time(n,INT_MAX);
+        // time[0]=0;
+        // time[firstPerson]=0;
+        // dfs(0,0,adjList,time);
+        // dfs(firstPerson,0,adjList,time);
+        // vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     if(time[i]!=INT_MAX)
+        //     ans.push_back(i);
+        // }
+        // return ans;
+
+
+
+
+         // unordered_map<int,vector<pair<int,int>>>adjList;
         // for(auto &meeting:meetings){
         //     adjList[meeting[0]].push_back({meeting[1],meeting[2]});
         //     adjList[meeting[1]].push_back({meeting[0],meeting[2]});
@@ -31,66 +128,65 @@ public:
 
 
 
-    //     vector<vector<pair<int,int>>>adjList(n);
-    //     for(auto &meeting:meetings){
-    //         adjList[meeting[0]].push_back({meeting[1],meeting[2]});
-    //         adjList[meeting[1]].push_back({meeting[0],meeting[2]});
-    //     }
-    //     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
-    //     pq.push({0,0});
-    //     pq.push({0,firstPerson});
-    //     vector<int>ans;
-    //     vector<int>time(n,INT_MAX);
-    //     time[0]=0;
-    //     time[firstPerson]=0;
-    //     while(!pq.empty()){
-    //         auto top=pq.top();
-    //         pq.pop();
-    //         if (top.first > time[top.second]) continue;
-    //         for(auto &adj:adjList[top.second]){
-    //             if(top.first<=adj.second&&adj.second<time[adj.first]){
-    //                 time[adj.first]=adj.second;
-    //                 pq.push({time[adj.first],adj.first});
-    //             }
-    //         }
-    //     }
-    //     for(int i=0;i<n;i++){
-    //         if(time[i]!=INT_MAX)
-    //         ans.push_back(i);
-    //     }
-    //     return ans;
-    // }
+        //     vector<vector<pair<int,int>>>adjList(n);
+        //     for(auto &meeting:meetings){
+        //         adjList[meeting[0]].push_back({meeting[1],meeting[2]});
+        //         adjList[meeting[1]].push_back({meeting[0],meeting[2]});
+        //     }
+        //     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        //     pq.push({0,0});
+        //     pq.push({0,firstPerson});
+        //     vector<int>ans;
+        //     vector<int>time(n,INT_MAX);
+        //     time[0]=0;
+        //     time[firstPerson]=0;
+        //     while(!pq.empty()){
+        //         auto top=pq.top();
+        //         pq.pop();
+        //         if (top.first > time[top.second]) continue;
+        //         for(auto &adj:adjList[top.second]){
+        //             if(top.first<=adj.second&&adj.second<time[adj.first]){
+        //                 time[adj.first]=adj.second;
+        //                 pq.push({time[adj.first],adj.first});
+        //             }
+        //         }
+        //     }
+        //     for(int i=0;i<n;i++){
+        //         if(time[i]!=INT_MAX)
+        //         ans.push_back(i);
+        //     }
+        //     return ans;
+        // }
 
 
 
-
-     vector<vector<pair<int,int>>>adjList(n);
-        for(auto &meeting:meetings){
-            adjList[meeting[0]].push_back({meeting[1],meeting[2]});
-            adjList[meeting[1]].push_back({meeting[0],meeting[2]});
-        }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
-        pq.push({0,0});
-        pq.push({0,firstPerson});
-        vector<int>ans;
-        vector<int>time(n,INT_MAX);
-        time[0]=0;
-        time[firstPerson]=0;
-        while(!pq.empty()){
-            auto top=pq.top();
-            pq.pop();
-            if (top.first > time[top.second]) continue;
-            for(auto &adj:adjList[top.second]){
-                if(top.first<=adj.second&&adj.second<time[adj.first]){
-                    time[adj.first]=adj.second;
-                    pq.push({time[adj.first],adj.first});
+            vector<vector<pair<int,int>>>adjList(n);
+            for(auto &meeting:meetings){
+                adjList[meeting[0]].push_back({meeting[1],meeting[2]});
+                adjList[meeting[1]].push_back({meeting[0],meeting[2]});
+            }
+            priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+            pq.push({0,0});
+            pq.push({0,firstPerson});
+            vector<int>ans;
+            vector<int>time(n,INT_MAX);
+            time[0]=0;
+            time[firstPerson]=0;
+            while(!pq.empty()){
+                auto top=pq.top();
+                pq.pop();
+                if (top.first > time[top.second]) continue;
+                for(auto &adj:adjList[top.second]){
+                    if(top.first<=adj.second&&adj.second<time[adj.first]){
+                        time[adj.first]=adj.second;
+                        pq.push({time[adj.first],adj.first});
+                    }
                 }
             }
+            for(int i=0;i<n;i++){
+                if(time[i]!=INT_MAX)
+                ans.push_back(i);
+            }
+            return ans;
         }
-        for(int i=0;i<n;i++){
-            if(time[i]!=INT_MAX)
-            ans.push_back(i);
-        }
-        return ans;
-    }
 };
