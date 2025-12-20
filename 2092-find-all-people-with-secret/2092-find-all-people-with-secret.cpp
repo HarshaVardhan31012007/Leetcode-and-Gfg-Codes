88a5404e9515a87bf070a7c73ad94d987b796ce2
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-20 13:32:33 +0530

    Time: 189 ms (54.08%), Space: 194.9 MB (46.47%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -221,10 +221,16 @@ public:
                 //  else
                 //  return a[0]<b[0];
 
+                // else if(a[1]!=b[1])
+                //  return a[1]>b[1];
+                //  else
+                //  return a[0]<b[0];
+
+
                 else if(a[1]!=b[1])
                  return a[1]>b[1];
                  else
-                 return a[0]<b[0];
+                 return a[0]>b[0];
             });
             unionHelp(0,firstPerson,parent,rank);
             for(int i=0;i<meetings.size();){
