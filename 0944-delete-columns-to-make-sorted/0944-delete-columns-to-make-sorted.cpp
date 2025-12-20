36e29c76a64f2e682dacacae89bf19b83771a884
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-20 10:14:12 +0530

    Time: 4 ms (45.13%), Space: 15.7 MB (7.21%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
+        // int len=strs[0].length();
+        // int count=0;
+        // for(int i=0;i<len;i++){
+        //     int j=1;
+        //     for(;j<strs.size();j++){
+        //          if(strs[j][i]<strs[j-1][i])
+        //          break;
+        //     }
+        //     if(j==strs.size())
+        //     count++;
+        // }
+        // return len-count;
+
+
+
         int len=strs[0].length();
         int count=0;
         for(int i=0;i<len;i++){
-            int j=1;
-            for(;j<strs.size();j++){
-                 if(strs[j][i]<strs[j-1][i])
-                 break;
+            for(int j=1;j<strs.size();j++){
+                 if(strs[j][i]<strs[j-1][i]){
+                    count++;
+                    break;
+                 }
             }
-            if(j==strs.size())
-            count++;
         }
-        return len-count;
+        return count;
     }
 };
