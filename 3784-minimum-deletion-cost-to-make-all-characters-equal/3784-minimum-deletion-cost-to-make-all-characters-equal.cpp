a76751765d8deea7a235c040af93854dd54e23f9
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-21 10:49:05 +0530

    Time: 8 ms (23.08%), Space: 115.2 MB (7.69%) - LeetHub

--- 3784-minimum-deletion-cost-to-make-all-characters-equal/3784-minimum-deletion-cost-to-make-all-characters-equal.cpp
+++ 3784-minimum-deletion-cost-to-make-all-characters-equal/3784-minimum-deletion-cost-to-make-all-characters-equal.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     long long minCost(string s, vector<int>& cost) {
-        unordered_map<char,long long>freq;
-        for(int i=0;i<s.length();i++)
-            freq[s[i]]+=1LL*cost[i];
-        long long sum=accumulate(cost.begin(),cost.end(),0LL);
-        long long mini=LLONG_MAX;
-        for(auto &each:freq){
-             mini=min(mini,sum-each.second);
+        // unordered_map<char,long long>freq;
+        // for(int i=0;i<s.length();i++)
+        //     freq[s[i]]+=1LL*cost[i];
+        // long long sum=accumulate(cost.begin(),cost.end(),0LL);
+        // long long mini=LLONG_MAX;
+        // for(auto &each:freq){
+        //      mini=min(mini,sum-each.second);
+        // }
+        // return mini;
+
+        long long sum=0;
+        long long freq[26]={0LL};
+        long long maxi=INT_MIN;
+        for(int i=0;i<s.length();i++){
+            freq[s[i]-'a']+=cost[i];
+            sum+=cost[i];
+            maxi=max(maxi,freq[s[i]-'a']);
         }
-        return mini;
+        return sum-maxi;
     }
 };
