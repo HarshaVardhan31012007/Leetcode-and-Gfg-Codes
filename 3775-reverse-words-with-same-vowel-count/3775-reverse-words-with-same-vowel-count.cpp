2a7f987f194c4305e990f5c612c38148ba4c21fe
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-21 18:28:34 +0530

    Time: 54 ms (63.89%), Space: 53.4 MB (52.59%) - LeetHub

--- 3775-reverse-words-with-same-vowel-count/3775-reverse-words-with-same-vowel-count.cpp
+++ 3775-reverse-words-with-same-vowel-count/3775-reverse-words-with-same-vowel-count.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        int i=0;
+    // string reverseWords(string s) {
+    //     int i=0;
+    //     int count=0;
+    //     while(i<s.length()&&s[i]!=' '){
+    //         if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
+    //             count++;
+    //         i++;
+    //     }
+    //     for(int j=i;j<s.length();j++){
+    //         int c=0;
+    //         while(j<s.length()&&s[j]!=' '){
+    //              if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u')
+    //                 c++;
+    //              j++;
+    //         }
+    //         if(c==count){
+    //             reverse(s.begin()+i,s.begin()+j);
+    //         }
+    //         i=j+1;
+    //     }
+    //     return s;
+    // }
+
+
+    int find(string &s){
         int count=0;
-        while(i<s.length()&&s[i]!=' '){
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
-                count++;
-            i++;
+        for(auto &each:s){
+            each=tolower(each);
+            if(each=='a'||each=='e'||each=='i'||each=='o'||each=='u')
+            count++;
         }
-        for(int j=i;j<s.length();j++){
-            int c=0;
-            while(j<s.length()&&s[j]!=' '){
-                 if(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u')
-                    c++;
-                 j++;
+        return count;
+    }
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word,result;
+        ss>>word;
+        int count=find(word);
+        result+=word;
+        while(ss>>word){
+            if(find(word)==count){
+                reverse(word.begin(),word.end());
             }
-            if(c==count){
-                reverse(s.begin()+i,s.begin()+j);
-            }
-            i=j+1;
+             result+=" "+word;
         }
-        return s;
+        return result;
     }
 };
