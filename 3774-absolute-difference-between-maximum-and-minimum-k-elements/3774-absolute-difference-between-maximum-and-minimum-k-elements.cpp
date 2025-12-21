3632b93f61a79f3cfc973446cddb26bda1ab2253
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-21 18:13:58 +0530

    Time: 0 ms (100%), Space: 32 MB (98.67%) - LeetHub

---
+++ 3774-absolute-difference-between-maximum-and-minimum-k-elements/3774-absolute-difference-between-maximum-and-minimum-k-elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int absDifference(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        // int one=0;
+        // for(int i=0;i<k;i++){
+        //     one+=nums[i];
+        // }
+        // int j=(int)nums.size()-1;int two=0;
+        // while(k--){
+        //     two+=nums[j];
+        //     j--;
+        // }
+        // return two-one;
+        int one=0;int two=0;
+        for(int i=0,j=nums.size()-1;i<k;i++,j--){
+           one+=nums[i];
+           two+=nums[j];
+        }
+        return two-one;
+    }
+};
