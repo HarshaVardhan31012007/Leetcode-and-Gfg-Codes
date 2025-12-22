f9e6370022d8a7fc200709329cd2be70ed2901de
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-22 23:34:03 +0530

    Time: 0 ms (100%), Space: 10.8 MB (21.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int i,vector<int>&dp){
-        if(i>=nums.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int inc=nums[i]+solve(nums,i+2,dp);
-        int exc=solve(nums,i+1,dp);
-        return dp[i]=max(inc,exc);
-    }
+    // int solve(vector<int>&nums,int i,vector<int>&dp){
+    //     if(i>=nums.size()) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int inc=nums[i]+solve(nums,i+2,dp);
+    //     int exc=solve(nums,i+1,dp);
+    //     return dp[i]=max(inc,exc);
+    // }
+    // int rob(vector<int>& nums) {
+    //     vector<int>dp(nums.size()+1,-1);
+    //     return solve(nums,0,dp);
+    // }
+
+
+    
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size()+1,-1);
-        return solve(nums,0,dp);
+        vector<int>dp(nums.size()+2,0);
+        for(int i=nums.size()-1;i>=0;i--){
+            int inc=nums[i]+dp[i+2];
+            int exc=dp[i+1];
+            dp[i]=max(inc,exc);
+        }
+        return dp[0];
     }
 };
