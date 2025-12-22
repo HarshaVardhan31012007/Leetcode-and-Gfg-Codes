9a17c44ce3d5400d8b8ae9051140b1e0497b6bce
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-22 21:02:30 +0530

    Time: 4 ms (57.44%), Space: 13.4 MB (61.54%) - LeetHub

--- 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.cpp
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.cpp
@@ -26,7 +26,31 @@ public:
 
 
 
-    bool check(vector<string>& strs,int c,int p,int m){
+    // bool check(vector<string>& strs,int c,int p,int m){
+    //     for(int i=0;i<m;i++){
+    //         if(strs[i][p]>strs[i][c])
+    //         return false;
+    //     }
+    //     return true;
+    // }
+    // int minDeletionSize(vector<string>& strs) {
+    //     int m=strs.size();
+    //     int n=strs[0].size();
+    //     vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=i-1;j>=-1;j--){//j is prev
+    //             int inc=0;
+    //             if(j==-1||check(strs,i,j,m)){
+    //                 inc=1+dp[i+1][i+1];//as prev is starts from -1 so shifted it
+    //             } 
+    //             int exc=dp[i+1][j+1];//as prev is starts from -1 so shifted it
+    //             dp[i][j+1]=max(inc,exc);//as prev is starts from -1 so shifted it
+    //         }
+    //     }
+    //     return n-dp[0][0];
+    // }
+
+     bool check(vector<string>& strs,int c,int p,int m){
         for(int i=0;i<m;i++){
             if(strs[i][p]>strs[i][c])
             return false;
@@ -36,17 +60,17 @@ public:
     int minDeletionSize(vector<string>& strs) {
         int m=strs.size();
         int n=strs[0].size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>dp(n+1,0);
         for(int i=n-1;i>=0;i--){
-            for(int j=i-1;j>=-1;j--){//j is prev
+            for(int j=-1;j<=i-1;j++){//j is prev
                 int inc=0;
                 if(j==-1||check(strs,i,j,m)){
-                    inc=1+dp[i+1][i+1];//as prev is starts from -1 so shifted it
+                    inc=1+dp[i+1];//as prev is starts from -1 so shifted it
                 } 
-                int exc=dp[i+1][j+1];//as prev is starts from -1 so shifted it
-                dp[i][j+1]=max(inc,exc);//as prev is starts from -1 so shifted it
+                int exc=dp[j+1];//as prev is starts from -1 so shifted it
+                dp[j+1]=max(inc,exc);//as prev is starts from -1 so shifted it
             }
         }
-        return n-dp[0][0];
+        return n-dp[0];
     }
 };
