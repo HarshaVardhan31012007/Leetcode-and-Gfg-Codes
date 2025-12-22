d8824107c2e22b1e960c07c01a0e98d7a80d71f8
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-23 00:13:14 +0530

    Time: 0 ms (100%), Space: 14 MB (94.7%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,16 +1,57 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int ans=1;
-        vector<int>dp(nums.size(),1);
+        // int ans=1;
+        // vector<int>dp(nums.size(),1);
+        // for(int i=0;i<nums.size();i++){
+        //    for(int j=0;j<i;j++){
+        //         if(nums[j]<nums[i]){
+        //             dp[i]=max(dp[i],dp[j]+1);
+        //         }
+        //    }
+        //    ans=max(ans,dp[i]);
+        // }
+        // return ans;
+
+        // int ans=1;
+        // int ansIndex=0;
+        // vector<int>dp(nums.size(),1);
+        // vector<int>parent(nums.size(),-1);
+        // for(int i=0;i<nums.size();i++){
+        //    for(int j=0;j<i;j++){
+        //         if(nums[j]<nums[i]&&dp[i]<dp[j]+1){
+        //             dp[i]=dp[j]+1;
+        //             parent[i]=j;
+        //         }
+        //    }
+        //    if(dp[i]>ans){
+        //      ans=dp[i];
+        //      ansIndex=i;
+        //    }
+        // }
+        // vector<int>prin;
+        // while(parent[ansIndex]!=-1){
+        //    prin.push_back(nums[ansIndex]);
+        //    ansIndex=parent[ansIndex];
+        // }
+        // prin.push_back(nums[ansIndex]);
+        // reverse(prin.begin(),prin.end());
+        // for(auto &each:prin)
+        // cout<<each<<" ";
+        // cout<<endl;
+        // return ans;
+
+
+        vector<int>ans;
         for(int i=0;i<nums.size();i++){
-           for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    dp[i]=max(dp[i],dp[j]+1);
-                }
-           }
-           ans=max(ans,dp[i]);
+            if(ans.empty()||ans.back()<nums[i])
+            ans.push_back(nums[i]);
+            else{
+                auto it=lower_bound(ans.begin(),ans.end(),nums[i]);//it is lower bound not upperbound dry run this 4 10 2 3 8 9
+                if(it!=ans.end())
+                ans[it-ans.begin()]=nums[i];
+            }
         }
-        return ans;
+        return ans.size();
     }
 };
