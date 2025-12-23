67cabeb60cfd903d6270f09c075f5ae9bfd96db0
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-23 18:52:10 +0530

    Time: 173 ms (29.73%), Space: 196.2 MB (18.15%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -78,6 +78,47 @@
 //     }
 
 
+// class Solution {
+// public:
+//     int find(vector<vector<int>>&events,int e){
+//         int ans=events.size();
+//         int i=0;
+//         int j=events.size()-1;
+//         while(i<=j){
+//             int mid=i+(j-i)/2;
+//             if(events[mid][0]>e){
+//                 ans=mid;
+//                 j=mid-1;
+//             }
+//             else{
+//                 i=mid+1;
+//             }
+//         }
+//         return ans;
+//     }
+//     int maxTwoEvents(vector<vector<int>>& events) {
+//         sort(events.begin(),events.end());//default sort by start
+//         vector<int>suffix(events.size(),0);
+//         suffix[events.size()-1]=events[events.size()-1][2];
+//         for(int i=events.size()-2;i>=0;i--){
+//            suffix[i]=max(suffix[i+1],events[i][2]);
+//         }
+//         int res=0;
+//         for(int i=0;i<events.size();i++){
+//            int idx=find(events,events[i][1]);
+//            if(idx!=events.size()){
+//               res=max(res,events[i][2]+suffix[idx]);
+//            }
+//            else
+//            res=max(res,events[i][2]);
+//         } 
+//         return res;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int find(vector<vector<int>>&events,int e){
@@ -96,24 +137,19 @@ public:
         }
         return ans;
     }
-    int maxTwoEvents(vector<vector<int>>& events) {
-        sort(events.begin(),events.end(),[](vector<int>&a,vector<int>&b){
-               return a[0]<b[0];
-        });
-        vector<int>suffix(events.size(),0);
-        suffix[events.size()-1]=events[events.size()-1][2];
-        for(int i=events.size()-2;i>=0;i--){
-           suffix[i]=max(suffix[i+1],events[i][2]);
+    int solve(vector<vector<int>>&events,int i,int count,vector<vector<int>>&dp){
+        if(count==2||i>=events.size()){
+            return 0;
         }
-        int res=0;
-        for(int i=0;i<events.size();i++){
-           int idx=find(events,events[i][1]);
-           if(idx!=events.size()){
-              res=max(res,events[i][2]+suffix[idx]);
-           }
-           else
-           res=max(res,events[i][2]);
-        } 
-        return res;
+        if(dp[i][count]!=-1) return dp[i][count];
+        int idx=find(events,events[i][1]);
+        int take=events[i][2]+solve(events,idx,count+1,dp);
+        int not_take=solve(events,i+1,count,dp);
+        return dp[i][count]=max(take,not_take);
+    }
+    int maxTwoEvents(vector<vector<int>>& events) {
+        sort(events.begin(),events.end());
+        vector<vector<int>>dp(events.size()+1,vector<int>(3,-1));
+        return solve(events,0,0,dp);
     }
 };
