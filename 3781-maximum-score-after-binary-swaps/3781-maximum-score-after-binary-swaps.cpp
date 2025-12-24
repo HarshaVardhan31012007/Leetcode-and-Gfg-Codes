270bd4b29fc49c7211f3b7e1930de7f1a82b22dc
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-24 23:43:29 +0530

    Time: 414 ms (5.03%), Space: 186.1 MB (10.29%) - LeetHub

--- 3781-maximum-score-after-binary-swaps/3781-maximum-score-after-binary-swaps.cpp
+++ 3781-maximum-score-after-binary-swaps/3781-maximum-score-after-binary-swaps.cpp
@@ -1,14 +1,65 @@
 class Solution {
 public:
-    long long maximumScore(vector<int>& nums, string s) {
-        priority_queue<int>pq;
-        long long ans=0;
+    // long long maximumScore(vector<int>& nums, string s) {
+    //     priority_queue<int>pq;
+    //     long long ans=0;
+    //     for(int j=0;j<nums.size();j++){
+    //         pq.push(nums[j]);
+    //         if(s[j]=='1'){
+    //             ans+=pq.top();
+    //             pq.pop();
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
+
+    // long long maximumScore(vector<int>& nums, string s) {
+    //     priority_queue<int>pq;
+    //     long long ans=0;
+    //     for(int j=0;j<nums.size();j++){
+    //         pq.push(nums[j]);
+    //         if(s[j]=='1'){
+    //             ans+=pq.top();
+    //             pq.pop();
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
+
+
+     long long maximumScore(vector<int>& nums, string s) {
+        // priority_queue<int>pq;
+        // long long ans=0;
+        // for(int j=0;j<nums.size();j++){
+        //     pq.push(nums[j]);
+        //     if(s[j]=='1'){
+        //         ans+=pq.top();
+        //         pq.pop();
+        //     }
+        // }
+        // return ans;
+
+
+        set<int>st;
+        vector<pair<int,int>>v;
         for(int j=0;j<nums.size();j++){
-            pq.push(nums[j]);
             if(s[j]=='1'){
-                ans+=pq.top();
-                pq.pop();
+                st.insert(j);
             }
+            v.push_back({nums[j],j});
+        }
+        sort(v.rbegin(),v.rend());
+        long long ans=0;
+        for(int i=0;i<v.size();i++){
+            if(st.empty()) break;
+            auto it=st.lower_bound(v[i].second);
+            if(it==st.end()) continue;
+            ans+=v[i].first;
+            st.erase(it);
         }
         return ans;
     }
