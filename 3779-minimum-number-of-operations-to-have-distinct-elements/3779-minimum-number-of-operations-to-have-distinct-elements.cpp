97c194fae4a5b9add40a63a86176f2ad65e77892
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-24 23:04:13 +0530

    Time: 47 ms (90.68%), Space: 186.4 MB (69.74%) - LeetHub

--- 3779-minimum-number-of-operations-to-have-distinct-elements/3779-minimum-number-of-operations-to-have-distinct-elements.cpp
+++ 3779-minimum-number-of-operations-to-have-distinct-elements/3779-minimum-number-of-operations-to-have-distinct-elements.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        unordered_map<int,pair<int,int>>mpp;
-        int idx=-1;
-        for(int i=0;i<nums.size();i++){
-            if(!mpp.count(nums[i])){
-                mpp[nums[i]]={-1,i};
-            }
-            else{
-                mpp[nums[i]]={mpp[nums[i]].second,i};
-                idx=max(idx,mpp[nums[i]].first);
-            }
+        // unordered_map<int,pair<int,int>>mpp;
+        // int idx=-1;
+        // for(int i=0;i<nums.size();i++){
+        //     if(!mpp.count(nums[i])){
+        //         mpp[nums[i]]={-1,i};
+        //     }
+        //     else{
+        //         mpp[nums[i]]={mpp[nums[i]].second,i};
+        //         idx=max(idx,mpp[nums[i]].first);
+        //     }
+        // }
+        // return ceil((idx+1)/3.0);
+
+
+        unordered_map<int,int>mpp;
+        int ans=0;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(mpp.count(nums[i])) break;
+            mpp[nums[i]]++;
+            ans++;
         }
-        return ceil((idx+1)/3.0);
+        return ceil((nums.size()-ans)/3.0);
     }
 };
