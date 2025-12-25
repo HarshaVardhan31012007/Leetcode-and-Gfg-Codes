a65a98d7ea6a18ff61bab08eb7678866ad003594
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-25 23:01:32 +0530

    Time: 57 ms (22.74%), Space: 81.8 MB (35.56%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -27,13 +27,41 @@ public:
     //     return solve(envelopes,n);
     // }
     
-    static bool comp(vector<int>&a,vector<int>&b){
-        if(a[0]==b[0])
-        return a[1]>b[1];
-        else
-        return a[0]<b[0];
-    }
-    int solve(vector<vector<int>>&envelopes,int n){
+    // static bool comp(vector<int>&a,vector<int>&b){
+    //     if(a[0]==b[0])
+    //     return a[1]>b[1];
+    //     else
+    //     return a[0]<b[0];
+    // }
+    // int solve(vector<vector<int>>&envelopes,int n){
+    //     vector<int>ans;
+    //     ans.push_back(envelopes[0][1]);
+    //     for(int i=1;i<envelopes.size();i++){
+    //         if(envelopes[i][1]>ans.back()){
+    //             ans.push_back(envelopes[i][1]);
+    //         }
+    //         else{
+    //             int index=lower_bound(ans.begin(),ans.end(),envelopes[i][1])-ans.begin();
+    //             ans[index]=envelopes[i][1];
+    //         }
+    //     }
+    //     return ans.size();
+    // }
+    // int maxEnvelopes(vector<vector<int>>&envelopes){
+    //     int n=envelopes.size();
+    //     sort(envelopes.begin(),envelopes.end(),comp);
+    //     return solve(envelopes,n);
+    // }
+
+   
+
+   
+    int maxEnvelopes(vector<vector<int>>&envelopes){
+        int n=envelopes.size();
+        sort(envelopes.begin(),envelopes.end(),[&](vector<int>&a,vector<int>&b){
+            if(a[0]==b[0]) return a[1]>b[1];
+            return a[0]<b[0];
+        });
         vector<int>ans;
         ans.push_back(envelopes[0][1]);
         for(int i=1;i<envelopes.size();i++){
@@ -41,15 +69,10 @@ public:
                 ans.push_back(envelopes[i][1]);
             }
             else{
-                int index=lower_bound(ans.begin(),ans.end(),envelopes[i][1])-ans.begin();
-                ans[index]=envelopes[i][1];
+                auto idx=lower_bound(ans.begin(),ans.end(),envelopes[i][1])-ans.begin();
+                ans[idx]=envelopes[i][1];
             }
         }
         return ans.size();
-    }
-    int maxEnvelopes(vector<vector<int>>&envelopes){
-        int n=envelopes.size();
-        sort(envelopes.begin(),envelopes.end(),comp);
-        return solve(envelopes,n);
     }
 };
