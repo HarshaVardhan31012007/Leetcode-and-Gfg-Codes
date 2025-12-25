0dfe0cfd91a852931a9d4e6c658738ff458c4145
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-25 18:12:54 +0530

    Time: 11 ms (78.38%), Space: 9.3 MB (89.99%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -23,35 +23,40 @@ public:
 
 
 
-    const int mod=1e9+7;
-    int solve(int n,int k,int target,vector<vector<int>>&dp){
-       if(target<0) return 0;
-       if(n==0){
-          if(target==0)
-          return 1;
-          else
-          return 0;
-       }
-       if(dp[n][target]!=-1) return dp[n][target];
-       int ans=0;
-       for(int i=1;i<=k;i++){
-            ans=(ans+solve(n-1,k,target-i,dp))%mod;
-       } 
-       return dp[n][target]=ans;
-    }
-    int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>dp(n+1,vector<int>(target+1,0));
-        dp[0][0]=1;
+    
+    // int numRollsToTarget(int n, int k, int target) {
+    //     vector<vector<int>>dp(n+1,vector<int>(target+1,0));
+    //     dp[0][0]=1;
+    //     for(int i=1;i<=n;i++){
+    //         for(int j=target;j>=0;j--){
+    //             int ans=0;
+    //             for(int p=1;p<=k;p++){
+    //                     if(p<=j)
+    //                     ans=(ans+dp[i-1][j-p])%mod;
+    //             } 
+    //             dp[i][j]=ans;
+    //         }
+    //     }
+    //     return dp[n][target];
+    // }
+
+
+
+     const int mod=1e9+7;
+     int numRollsToTarget(int n, int k, int target) {
+        vector<int>curr(target+1,0);
+        curr[0]=1;
         for(int i=1;i<=n;i++){
+            if(i>1) curr[0]=0;
             for(int j=target;j>=0;j--){
                 int ans=0;
                 for(int p=1;p<=k;p++){
                         if(p<=j)
-                        ans=(ans+dp[i-1][j-p])%mod;
+                        ans=(ans+curr[j-p])%mod;
                 } 
-                dp[i][j]=ans;
+                curr[j]=ans;
             }
         }
-        return dp[n][target];
+        return curr[target];
     }
 };
