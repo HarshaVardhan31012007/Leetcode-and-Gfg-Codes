8f0e38b0d0f6b41f2887a492e54ea17c77147afd
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-25 17:48:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Painting the Fence/painting-the-fence.cpp
+++ Difficulty: Medium/Painting the Fence/painting-the-fence.cpp
@@ -1,45 +1,13 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-#include <math.h>
-#include <stdio.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int solve(int n,int k,vector<int>&dp){
-        if(n==1)
-        return k;
-        if(n==2)
-        return k*k;
-        if(dp[n-1]!=-1)
-        return dp[n-1];
-        return dp[n-1]=(k-1)*(solve(n-1,k,dp)+solve(n-2,k,dp));
+        if(n==1) return k;
+        if(n==2) return k+k*(k-1);
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=(solve(n-1,k,dp)+solve(n-2,k,dp))*(k-1);
     }
     int countWays(int n, int k) {
-        vector<int>dp(n,-1);
+        vector<int>dp(n+1,-1);
         return solve(n,k,dp);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, k;
-        cin >> n >> k;
-        Solution ob;
-        cout << ob.countWays(n, k) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
