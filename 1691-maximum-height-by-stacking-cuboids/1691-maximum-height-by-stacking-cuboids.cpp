310b2f913c5d6c86383dbf5f5d1dbf2f1bc7921c
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-25 22:28:42 +0530

    Time: 6 ms (81.75%), Space: 14.9 MB (52.12%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
-    bool isSafe(vector<vector<int>>&cuboids,int c,int p){
+    // bool isSafe(vector<vector<int>>&cuboids,int c,int p){
+    //     return cuboids[c][0]>=cuboids[p][0]&&cuboids[c][1]>=cuboids[p][1]&&cuboids[c][2]>=cuboids[p][2];
+    // }
+    // int solve(vector<vector<int>>&cuboids,int i,int prev,vector<vector<int>>&dp){
+    //     if(i>=cuboids.size()) return 0;
+    //     if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+    //     int inc=0;
+    //     if(prev==-1||isSafe(cuboids,i,prev)){
+    //         inc=cuboids[i][2]+solve(cuboids,i+1,i,dp);
+    //     }
+    //     int exc=solve(cuboids,i+1,prev,dp);
+    //     return dp[i][prev+1]=max(inc,exc);
+    // }
+    // int maxHeight(vector<vector<int>>& cuboids) {
+    //     for(auto &each:cuboids)
+    //     sort(each.begin(),each.end());
+    //     sort(cuboids.begin(),cuboids.end());
+    //     vector<vector<int>>dp(cuboids.size()+1,vector<int>(cuboids.size()+1,-1));
+    //     return solve(cuboids,0,-1,dp);
+    // }
+
+
+
+     bool isSafe(vector<vector<int>>&cuboids,int c,int p){
         return cuboids[c][0]>=cuboids[p][0]&&cuboids[c][1]>=cuboids[p][1]&&cuboids[c][2]>=cuboids[p][2];
-    }
-    int solve(vector<vector<int>>&cuboids,int i,int prev,vector<vector<int>>&dp){
-        if(i>=cuboids.size()) return 0;
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        int inc=0;
-        if(prev==-1||isSafe(cuboids,i,prev)){
-            inc=cuboids[i][2]+solve(cuboids,i+1,i,dp);
-        }
-        int exc=solve(cuboids,i+1,prev,dp);
-        return dp[i][prev+1]=max(inc,exc);
     }
     int maxHeight(vector<vector<int>>& cuboids) {
         for(auto &each:cuboids)
         sort(each.begin(),each.end());
         sort(cuboids.begin(),cuboids.end());
-        vector<vector<int>>dp(cuboids.size()+1,vector<int>(cuboids.size()+1,-1));
-        return solve(cuboids,0,-1,dp);
+        vector<vector<int>>dp(cuboids.size()+1,vector<int>(cuboids.size()+1,0));
+        for(int i=cuboids.size()-1;i>=0;i--){
+            for(int prev=i-1;prev>=-1;prev--){
+                 int inc=0;
+                if(prev==-1||isSafe(cuboids,i,prev)){
+                    inc=cuboids[i][2]+dp[i+1][i+1];
+                }
+                int exc=dp[i+1][prev+1];
+                dp[i][prev+1]=max(inc,exc);
+            }
+        }
+        return dp[0][0];
     }
 };
