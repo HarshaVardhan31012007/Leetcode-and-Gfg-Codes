e6c3cfd4c268713463660c45ee625639e637f23f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-25 18:35:14 +0530

    Time: 171 ms (22.28%), Space: 111.3 MB (14.71%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,123 +1,19 @@
 class Solution {
 public:
-    // bool divide(vector<int>&nums,int k,int target){
-
-    //     //base case
-    //     if(k==nums.size()||target<0)
-    //     return 0;
-    //     if(target==0)
-    //     return 1;
-    //     int num=nums[k];
-    //     bool includeAns=divide(nums,k+1,target-num);
-    //     //exclude
-    //     bool excludeAns=divide(nums,k+1,target);
-    //     return includeAns||excludeAns;
-    // }
-    // bool canPartition(vector<int>& nums) {
-    //     // int total=0;
-    //     // for(int i=0;i<nums.size();i++){
-    //     //   total+=nums[i];
-    //     // }
-    //     int total=accumulate(nums.begin(),nums.end(),0);
-    //     if(total%2!=0)
-    //     return 0;
-    //     int target=total/2;
-    //     int i=0;
-    //     bool ans=divide(nums,i,target);
-    //     return ans;
-
-    // }
-
-
-
-
-    // bool divide(vector<int>&nums,int k,int target,vector<vector<int>>&dp){
-    //     // if(k==nums.size()||target<0)
-    //     // return 0;
-    //      if(k==nums.size())
-    //     return 0;
-    //     if(target==0)
-    //     return 1;
-    //     if(dp[target][k]!=-1)
-    //     return dp[target][k];
-    //     int num=nums[k];
-    //     bool includeAns=0;
-    //     if(num<=target)
-    //     includeAns=divide(nums,k+1,target-num,dp);
-    //     bool excludeAns=divide(nums,k+1,target,dp);
-    //     return dp[target][k]=includeAns||excludeAns;
-    // }
-    // bool canPartition(vector<int>& nums) {
-    //     int total=accumulate(nums.begin(),nums.end(),0);
-    //     if(total%2!=0)
-    //     return 0;
-    //     int target=total/2;
-    //     int i=0;
-    //     int n=nums.size();
-    //     vector<vector<int>>dp(target+1,vector<int>(n+1,-1));
-    //     bool ans=divide(nums,i,target,dp);
-    //     return ans;
-    // }
-
-
-
-    //  bool divide(vector<int>&nums,int target){
-    //    int n=nums.size();
-    //    vector<vector<int>>dp(target+1,vector<int>(n+1,0));
-    //    for(int j=0;j<=n;j++)
-    //    dp[0][j]=1;
-    //    for(int i=1;i<=target;i++){
-    //     for(int j=n-1;j>=0;j--){
-    //         int l=0;
-    //         if(nums[j]<=i)
-    //         l=dp[i-nums[j]][j+1];
-    //         int r=dp[i][j+1];
-    //         dp[i][j]=l||r;
-    //     }
-    //    }
-    //    return dp[target][0];
-    // }
-
-    // bool divide(vector<int>&nums,int target){
-    //    int n=nums.size();
-    //    vector<int>curr(target+1,-1);
-    //    vector<int>next(target+1,0);
-    //    next[0]=curr[0]=1;
-    //     for(int j=n-1;j>=0;j--){
-    //         for(int i=1;i<=target;i++){
-    //         int l=0;
-    //         if(nums[j]<=i)
-    //         l=next[i-nums[j]];
-    //         int r=next[i];
-    //         curr[i]=l||r;
-    //     }
-    //     next=curr;
-    //    }
-    //    return next[target];
-    // }
-
-
-     bool divide(vector<int>&nums,int target){
-       int n=nums.size();
-       vector<int>curr(target+1,0);
-       curr[0]=1;
-        for(int j=n-1;j>=0;j--){
-            for(int i=target;i>=1;i--){
-            int l=0;
-            if(nums[j]<=i)
-            l=curr[i-nums[j]];
-            int r=curr[i];
-            curr[i]=l||r;
-            }
-       }
-       return curr[target];
+    bool solve(vector<int>&nums,int i,int target,vector<vector<int>>&dp){
+        if(i>=nums.size()) return target==0;
+        if(target==0) return 1;
+        if(dp[i][target]!=-1) return dp[i][target];
+        bool inc=false;
+        if(nums[i]<=target) 
+        inc=solve(nums,i+1,target-nums[i],dp);
+        int exc=solve(nums,i+1,target,dp);
+        return dp[i][target]=inc||exc;
     }
     bool canPartition(vector<int>& nums) {
-        int total=accumulate(nums.begin(),nums.end(),0);
-        if(total%2!=0)
-        return 0;
-        int target=total/2;
-        bool ans=divide(nums,target);
-        return ans;
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        if((sum&1)) return false;
+        vector<vector<int>>dp(nums.size()+1,vector<int>(sum/2+1,-1));
+        return solve(nums,0,sum/2,dp);
     }
 };
