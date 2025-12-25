ecbe94d08523f29a9b8e51a240448d4df21d6c1e
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-25 16:43:12 +0530

    Time: 220 ms (12.77%), Space: 110 MB (29.08%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        long long sum=0;
-        priority_queue<int,vector<int>,greater<>>pq;
-        for(int i=0;i<happiness.size();i++){
-            if(i<k){
-               pq.push(happiness[i]);
-               sum+=happiness[i];
-            }
-            else{
-                if(happiness[i]>pq.top()){
-                    sum-=pq.top();
-                    pq.pop();
-                    pq.push(happiness[i]);
-                    sum+=happiness[i];
-                }
-            }
+        // long long sum=0;
+        // priority_queue<int,vector<int>,greater<>>pq;
+        // for(int i=0;i<happiness.size();i++){
+        //     if(i<k){
+        //        pq.push(happiness[i]);
+        //        sum+=happiness[i];
+        //     }
+        //     else{
+        //         if(happiness[i]>pq.top()){
+        //             sum-=pq.top();
+        //             pq.pop();
+        //             pq.push(happiness[i]);
+        //             sum+=happiness[i];
+        //         }
+        //     }
+        // }
+        // int turn=k-1;
+        // while(!pq.empty()){
+        //     sum-=min(turn,pq.top());
+        //     pq.pop();
+        //     turn--;
+        // }
+        // return sum;
+
+
+
+        sort(happiness.rbegin(),happiness.rend());
+        long long ans=0;
+        for(int i=0;i<k;i++){
+          ans+=happiness[i]-min(happiness[i],i);
         }
-        int turn=k-1;
-        while(!pq.empty()){
-            sum-=min(turn,pq.top());
-            pq.pop();
-            turn--;
-        }
-        return sum;
+        return ans;
     }
 };
