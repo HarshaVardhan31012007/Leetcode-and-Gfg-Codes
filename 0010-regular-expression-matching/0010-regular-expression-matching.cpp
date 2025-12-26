e54dc6110defb73caee3c3f5e528c7181c0cd8f1
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-26 16:35:36 +0530

    Time: 0 ms (100%), Space: 9.5 MB (23.95%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -31,24 +31,48 @@ public:
 
 
 
-    bool solve(string &a,string &b,int i,int j,vector<vector<int>>&dp){
-        if(i>=a.length()&&j>=b.length()) return true;
-        if(j==b.length()) return false;
-        if(dp[i][j]!=-1) return dp[i][j];
-        bool ans=false;
-        bool curr=i<a.length()&&(b[j]=='.'||a[i]==b[j]);
-        if(j+1<b.length()&&b[j+1]=='*'){
-           if(curr)
-           ans=solve(a,b,i+1,j,dp);
-           ans=ans||solve(a,b,i,j+2,dp);
-        }
-        else if(curr){
-            ans=solve(a,b,i+1,j+1,dp);
-        }
-        return dp[i][j]=ans;
-    }
+    // bool solve(string &a,string &b,int i,int j,vector<vector<int>>&dp){
+    //     if(i>=a.length()&&j>=b.length()) return true;
+    //     if(j==b.length()) return false;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     bool ans=false;
+    //     bool curr=i<a.length()&&(b[j]=='.'||a[i]==b[j]);
+    //     if(j+1<b.length()&&b[j+1]=='*'){
+    //        if(curr)
+    //        ans=solve(a,b,i+1,j,dp);
+    //        ans=ans||solve(a,b,i,j+2,dp);
+    //     }
+    //     else if(curr){
+    //         ans=solve(a,b,i+1,j+1,dp);
+    //     }
+    //     return dp[i][j]=ans;
+    // }
+    // bool isMatch(string s, string p) {
+    //     vector<vector<int>>dp(s.length()+1,vector<int>(p.length()+1,-1));
+    //     return solve(s,p,0,0,dp);
+    // }
+
+
+
+
     bool isMatch(string s, string p) {
-        vector<vector<int>>dp(s.length()+1,vector<int>(p.length()+1,-1));
-        return solve(s,p,0,0,dp);
+        vector<vector<int>>dp(s.length()+1,vector<int>(p.length()+1,0));
+        dp[s.length()][p.length()]=1;
+        for(int i=s.length();i>=0;i--){
+            for(int j=p.length()-1;j>=0;j--){
+                    bool ans=false;
+                    bool curr=i<s.length()&&(p[j]=='.'||s[i]==p[j]);
+                    if(j+1<p.length()&&p[j+1]=='*'){
+                    if(curr)
+                    ans=dp[i+1][j];
+                    ans=ans||dp[i][j+2];
+                    }
+                    else if(curr){
+                        ans=dp[i+1][j+1];
+                    }
+                    dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
     }
 };
