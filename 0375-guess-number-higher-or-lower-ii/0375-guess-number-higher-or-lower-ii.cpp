dcb59f4b5543e8454596bb8e99e1151988c4fdda
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-26 12:33:34 +0530

    Time: 83 ms (33.18%), Space: 11 MB (79.95%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,56 +1,16 @@
 class Solution {
 public:
-    // int solve(int s,int e,vector<vector<int>>&dp){
-    //  if(s>=e)
-    //  return 0;
-    //  if(dp[s][e]!=-1)
-    //  return dp[s][e];
-    //  int ans=INT_MAX;
-    //  for(int i=s;i<=e;i++){
-    //    ans=min(ans,i+max(solve(s,i-1,dp),solve(i+1,e,dp)));
-    //  }
-    //  return dp[s][e]=ans;
-    // }
-    // int getMoneyAmount(int n) {
-    //     vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-    //     return solve(1,n,dp);
-    // }
-
-
-
-    // int solve(int n){
-    //  vector<vector<int>>dp(n+2,vector<int>(n+1,0));
-    //  for(int s=n;s>=1;s--){
-    //     for(int e=1;e<=n;e++){   
-    //         if(s>=e)
-    //         continue;
-    //         else{
-    //  int ans=INT_MAX;
-    //  for(int i=s;i<=e;i++){
-    //    ans=min(ans,i+max(dp[s][i-1],dp[i+1][e]));
-    //  }
-    //  dp[s][e]=ans;
-    //         }
-    //     }
-    //  }
-    //  return dp[1][n];
-    // }
-
-
-    int solve(int n){
-     vector<vector<int>>dp(n+2,vector<int>(n+1,0));
-     for(int s=n;s>=1;s--){
-        for(int e=s+1;e<=n;e++){  //s<e means e starts from s+1 
-     int ans=INT_MAX;
-     for(int i=s;i<=e;i++){
-       ans=min(ans,i+max(dp[s][i-1],dp[i+1][e]));
-     }
-     dp[s][e]=ans;
+    int solve(int s,int e,vector<vector<int>>&dp){
+        if(s>=e) return 0;
+        if(dp[s][e]!=-1) return dp[s][e];
+        int ans=INT_MAX; 
+        for(int i=s;i<=e;i++){
+             ans=min(ans,i+max(solve(s,i-1,dp),solve(i+1,e,dp)));
         }
-     }
-     return dp[1][n];
+        return dp[s][e]=ans;
     }
     int getMoneyAmount(int n) {
-        return solve(n);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(1,n,dp);
     }
 };
