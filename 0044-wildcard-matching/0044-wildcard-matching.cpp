03d2466bca75c1382ea0374531daf8ae8a0e04c4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-26 15:28:49 +0530

    Time: 66 ms (25.21%), Space: 15.9 MB (51.32%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,27 +1,53 @@
 class Solution {
 public:
-    bool solve(string &a,string &b,int i,int j,vector<vector<int>>&dp){
-        if(i>=a.length()&&j>=b.length()) return true;
-        if(i>=a.length()){
-            for(int k=j;k<b.length();k++){
-                if(b[k]!='*')
-                return false;
-            }
-            return true;
-        }
-        if(j>=b.length()) return false;
-        if(dp[i][j]!=-1) return dp[i][j];
-        bool ans=false;
-        if(b[j]=='?'||a[i]==b[j]){
-            ans=solve(a,b,i+1,j+1,dp);
-        }
-        else if(b[j]=='*'){
-            ans=solve(a,b,i+1,j,dp)||solve(a,b,i,j+1,dp);
-        }
-        return dp[i][j]=ans;
-    }
+    // bool solve(string &a,string &b,int i,int j,vector<vector<int>>&dp){
+    //     if(i>=a.length()&&j>=b.length()) return true;
+    //     if(i>=a.length()){
+    //         for(int k=j;k<b.length();k++){
+    //             if(b[k]!='*')
+    //             return false;
+    //         }
+    //         return true;
+    //     }
+    //     if(j>=b.length()) return false;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     bool ans=false;
+    //     if(b[j]=='?'||a[i]==b[j]){
+    //         ans=solve(a,b,i+1,j+1,dp);
+    //     }
+    //     else if(b[j]=='*'){
+    //         ans=solve(a,b,i+1,j,dp)||solve(a,b,i,j+1,dp);
+    //     }
+    //     return dp[i][j]=ans;
+    // }
+    // bool isMatch(string s, string p) {
+    //     vector<vector<int>>dp(s.length()+1,vector<int>(p.length()+1,-1));
+    //     return solve(s,p,0,0,dp);
+    // }
+
+
+
+
+    
     bool isMatch(string s, string p) {
-        vector<vector<int>>dp(s.length()+1,vector<int>(p.length()+1,-1));
-        return solve(s,p,0,0,dp);
+        vector<vector<bool>>dp(s.length()+1,vector<bool>(p.length()+1,0));
+        dp[s.length()][p.length()]=1;
+        for(int j=p.length()-1;j>=0;j--){
+            if(p[j]!='*') break;
+            dp[s.length()][j]=1;
+        }
+        for(int i=s.length()-1;i>=0;i--){
+            for(int j=p.length()-1;j>=0;j--){
+                    bool ans=false;
+                    if(p[j]=='?'||s[i]==p[j]){
+                        ans=dp[i+1][j+1];
+                    }
+                    else if(p[j]=='*'){
+                        ans=dp[i+1][j]||dp[i][j+1];
+                    }
+                    dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
     }
 };
