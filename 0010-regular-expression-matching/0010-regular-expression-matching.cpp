41a106bd348ac6a3e664ba8121a239e9421bdbea
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-26 16:17:22 +0530

    Time: 0 ms (100%), Space: 9.7 MB (17.62%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,101 +1,31 @@
 class Solution {
 public:
-    // bool solve(string &s,string &p,int i,int j){
-    //   if(i==s.length()&&j==p.length()) return 1;
-    //   if(j==p.length()) return 0;
-    //   bool curr=i<s.length()&&(p[j]=='.'||s[i]==p[j]);
-    //   if(j+1<p.length()&&p[j+1]=='*'){
-    //     int emptycase=solve(s,p,i,j+2);
-    //     int precase=curr&&solve(s,p,i+1,j);
-    //     return emptycase||precase;
-    //   }
-    //   else if(curr)
-    //   return solve(s,p,i+1,j+1);
-    //   else
-    //   return false;
-    // }
-    // bool isMatch(string s, string p) {
-    //     return solve(s,p,0,0);
-    // }
-
-
-    //  bool solve(string &s,string &p,int i,int j,vector<vector<int>>&dp){
-    //   if(i==s.length()&&j==p.length()) return 1;
-    //   if(j==p.length()) return 0;
-    //   if(dp[i][j]!=-1)
-    //   return dp[i][j];
-    //   bool curr=i<s.length()&&(p[j]=='.'||s[i]==p[j]);
-    //   if(j+1<p.length()&&p[j+1]=='*'){
-    //     int emptycase=solve(s,p,i,j+2,dp);
-    //     int precase=curr&&solve(s,p,i+1,j,dp);
-    //     return dp[i][j]=emptycase||precase;
-    //   }
-    //   else if(curr)
-    //   return dp[i][j]=solve(s,p,i+1,j+1,dp);
-    //   else
-    //   return dp[i][j]=false;
-    // }
-    // bool isMatch(string s, string p) {
-    //     int m=s.size();
-    //     int n=p.size();
-    //     vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-    //     return solve(s,p,0,0,dp);
-    // }
-
-
-    // bool solve(string &s,string &p,int m,int n){
-    // vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-    // dp[m][n]=1;
-    // for(int i=m;i>=0;i--){
-    //     for(int j=n-1;j>=0;j--){
-    //   bool curr=i<s.length()&&(p[j]=='.'||s[i]==p[j]);
-    //   if(j+1<p.length()&&p[j+1]=='*'){
-    //     int emptycase=dp[i][j+2];
-    //     int precase=curr&&dp[i+1][j];
-    //     dp[i][j]=emptycase||precase;
-    //   }
-    //   else if(curr)
-    //   dp[i][j]=dp[i+1][j+1];
-    //   else
-    //   dp[i][j]=false;
-    //     }
-    // }
-    // return dp[0][0];
-    // }
-    // bool isMatch(string s, string p) {
-    //     int m=s.size();
-    //     int n=p.size();
-    //     return solve(s,p,m,n);
-    // }
-
-
-
-      bool solve(string &s,string &p,int m,int n){
-    vector<int>curr(n+1,0);
-    vector<int>next(n+1,0);
-    curr[n]=1;
-    for(int i=m;i>=0;i--){
-        for(int j=n-1;j>=0;j--){
-      bool cur=i<s.length()&&(p[j]=='.'||s[i]==p[j]);
-      if(j+1<p.length()&&p[j+1]=='*'){
-        int emptycase=curr[j+2];
-        int precase=cur&&next[j];
-        curr[j]=emptycase||precase;
-      }
-      else if(cur)
-      curr[j]=next[j+1];
-      else
-      curr[j]=false;
+     bool solve(string &a,string &b,int i,int j,vector<vector<int>>&dp){
+        if(i>=a.length()&&j>=b.length()) return true;
+        if(i>=a.length()){
+            for(int k=j;k<b.length();k++){
+                if(b[k+1]!='*')
+                return false;
+                else
+                k++;
+            }
+            return true;
         }
-        next=curr;
-        //for rem i<m and j==n
-        curr[n]=0;
-    }
-    return curr[0];
+        if(j>=b.length()) return false;
+        if(dp[i][j]!=-1) return dp[i][j];
+        bool ans=false;
+        if(j+1<b.length()&&b[j+1]=='*'){
+            if(b[j]=='.'||a[i]==b[j])
+            ans=solve(a,b,i+1,j,dp);
+            ans=ans||solve(a,b,i,j+2,dp);
+        }
+        else if(b[j]=='.'||a[i]==b[j]){
+            ans=solve(a,b,i+1,j+1,dp);
+        }
+        return dp[i][j]=ans;
     }
     bool isMatch(string s, string p) {
-        int m=s.size();
-        int n=p.size();
-        return solve(s,p,m,n);
+        vector<vector<int>>dp(s.length()+1,vector<int>(p.length()+1,-1));
+        return solve(s,p,0,0,dp);
     }
 };
