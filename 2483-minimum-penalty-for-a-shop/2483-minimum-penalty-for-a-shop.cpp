25a5caadf07f29a1f7d076661534b7c04186a233
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-26 10:22:55 +0530

    Time: 10 ms (42.62%), Space: 17.5 MB (56.07%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int n=customers.size();
+        vector<int>p(n,0);
+        p[0]=(customers[0]=='Y'?0:1);
+        for(int i=1;i<n;i++){
+           p[i]=(p[i-1]+(customers[i]=='Y'?0:1));
+        }
+        int ans=INT_MAX;
+        int mini=INT_MAX;
+        int tclose=n-p[n-1];
+        for(int i=0;i<=n;i++){
+           int cclose=tclose-(i>0?(i-p[i-1]):0);
+           int bopen=(i>0?p[i-1]:0);
+           int curr=bopen+cclose;
+           if(curr<mini){
+              mini=curr;
+              ans=i;
+           }
+        }
+        return ans;
+    }
+};
