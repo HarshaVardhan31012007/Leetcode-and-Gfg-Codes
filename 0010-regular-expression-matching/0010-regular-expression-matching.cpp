1ef68ab3a2444c5f97bc0d1df70937a5ff56e617
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-26 16:32:37 +0530

    Time: 3 ms (49.66%), Space: 9.5 MB (20.86%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,25 +1,48 @@
 class Solution {
 public:
-     bool solve(string &a,string &b,int i,int j,vector<vector<int>>&dp){
+    //  bool solve(string &a,string &b,int i,int j,vector<vector<int>>&dp){
+    //     if(i>=a.length()&&j>=b.length()) return true;
+    //     if(i>=a.length()){
+    //         for(int k=j;k<b.length();k++){
+    //             if(b[k+1]!='*')
+    //             return false;
+    //             else
+    //             k++;
+    //         }
+    //         return true;
+    //     }
+    //     if(j>=b.length()) return false;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     bool ans=false;
+    //     if(j+1<b.length()&&b[j+1]=='*'){
+    //         if(b[j]=='.'||a[i]==b[j])
+    //         ans=solve(a,b,i+1,j,dp);
+    //         ans=ans||solve(a,b,i,j+2,dp);
+    //     }
+    //     else if(b[j]=='.'||a[i]==b[j]){
+    //         ans=solve(a,b,i+1,j+1,dp);
+    //     }
+    //     return dp[i][j]=ans;
+    // }
+    // bool isMatch(string s, string p) {
+    //     vector<vector<int>>dp(s.length()+1,vector<int>(p.length()+1,-1));
+    //     return solve(s,p,0,0,dp);
+    // }
+
+
+
+    bool solve(string &a,string &b,int i,int j,vector<vector<int>>&dp){
         if(i>=a.length()&&j>=b.length()) return true;
-        if(i>=a.length()){
-            for(int k=j;k<b.length();k++){
-                if(b[k+1]!='*')
-                return false;
-                else
-                k++;
-            }
-            return true;
-        }
-        if(j>=b.length()) return false;
+        if(j==b.length()) return false;
         if(dp[i][j]!=-1) return dp[i][j];
         bool ans=false;
+        bool curr=i<a.length()&&(b[j]=='.'||a[i]==b[j]);
         if(j+1<b.length()&&b[j+1]=='*'){
-            if(b[j]=='.'||a[i]==b[j])
-            ans=solve(a,b,i+1,j,dp);
-            ans=ans||solve(a,b,i,j+2,dp);
+           if(curr)
+           ans=solve(a,b,i+1,j,dp);
+           ans=ans||solve(a,b,i,j+2,dp);
         }
-        else if(b[j]=='.'||a[i]==b[j]){
+        else if(curr){
             ans=solve(a,b,i+1,j+1,dp);
         }
         return dp[i][j]=ans;
