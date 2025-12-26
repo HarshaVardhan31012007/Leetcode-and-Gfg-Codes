4d85438dd1c3a92e01fab43d10dc494da164e27f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-26 12:39:03 +0530

    Time: 32 ms (83.94%), Space: 11 MB (79.95%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
-    int solve(int s,int e,vector<vector<int>>&dp){
+    // int solve(int s,int e,vector<vector<int>>&dp){
+    //     if(s>=e) return 0;
+    //     if(dp[s][e]!=-1) return dp[s][e];
+    //     int ans=INT_MAX; 
+    //     for(int i=s;i<=e;i++){
+    //          ans=min(ans,i+max(solve(s,i-1,dp),solve(i+1,e,dp)));
+    //     }
+    //     return dp[s][e]=ans;
+    // }
+    // int getMoneyAmount(int n) {
+    //     vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+    //     return solve(1,n,dp);
+    // }
+
+
+
+     int solve(int s,int e,vector<vector<int>>&dp){
         if(s>=e) return 0;
         if(dp[s][e]!=-1) return dp[s][e];
         int ans=INT_MAX; 
@@ -10,7 +26,16 @@ public:
         return dp[s][e]=ans;
     }
     int getMoneyAmount(int n) {
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(1,n,dp);
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        for(int s=n;s>=1;s--){
+            for(int e=s+1;e<=n;e++){
+                    int ans=INT_MAX; 
+                    for(int i=s;i<=e;i++){
+                        ans=min(ans,i+max(dp[s][i-1],dp[i+1][e]));
+                    }
+                    dp[s][e]=ans;
+            }
+        }
+        return dp[1][n];
     }
 };
