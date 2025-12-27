9b52d28efcba53914b78229f58c33afb7d89a271
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-27 12:31:17 +0530

    Time: 78 ms (47.42%), Space: 98.1 MB (70.22%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        vector<long long>ans(n,0);
+        sort(meetings.begin(),meetings.end());
+        vector<long long>end(n,0);
+        for(int i=0;i<meetings.size();i++){
+            long long int s=meetings[i][0];long long int e=meetings[i][1];
+            bool flag=1;long long int mini=LLONG_MAX;long long int idx=-1;
+            for(int j=0;j<n;j++){
+                if(end[j]<=s){
+                    end[j]=e;
+                    ans[j]++;
+                    flag=0;
+                    break;
+                }
+                if(end[j]<mini){
+                    mini=end[j];
+                    idx=j;
+                }
+            }
+            if(flag){
+                ans[idx]++;
+                end[idx]=end[idx]+1LL*(e-s);
+            }
+        }
+        int num=-1;long long int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(ans[i]>maxi){
+                maxi=ans[i];
+                num=i;
+            }
+        }
+        return num;
+    }
+};
