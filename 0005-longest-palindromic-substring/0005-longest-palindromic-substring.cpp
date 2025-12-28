20879683897f9b36c7479c613165f331be2b6d24
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 16:07:33 +0530

    Time: 375 ms (13.36%), Space: 296.8 MB (5.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
-    string check(string &s,int i,int j){
-        while(i>=0&&j<s.length()){
-            if(s[i]==s[j]){
-                i--;j++;
-            }
-            else
-            break;
-        }
-        return s.substr(i+1,j-i-1);
+    // string check(string &s,int i,int j){
+    //     while(i>=0&&j<s.length()){
+    //         if(s[i]==s[j]){
+    //             i--;j++;
+    //         }
+    //         else
+    //         break;
+    //     }
+    //     return s.substr(i+1,j-i-1);
+    // }
+    // string longestPalindrome(string s) {
+    //     string ans="";
+    //     for(int i=0;i<s.length();i++){
+    //         string temp=check(s,i,i);
+    //         if(temp.length()>ans.length()) ans=temp;
+    //         temp=check(s,i,i+1);
+    //         if(temp.length()>ans.length()) ans=temp;
+    //     }
+    //     return ans;
+    // }
+
+
+    
+    bool checkpalindrome(string &s,int i,int j,vector<vector<int>>&dp){
+       if(i>=j) return true; 
+       if(s[i]==s[j])
+       return dp[i][j]=checkpalindrome(s,i+1,j-1,dp);
+       else{
+          return false;
+       }
     }
     string longestPalindrome(string s) {
+        vector<vector<int>>dp(s.length()+1,vector<int>(s.length()+1,-1));
         string ans="";
         for(int i=0;i<s.length();i++){
-            string temp=check(s,i,i);
-            if(temp.length()>ans.length()) ans=temp;
-            temp=check(s,i,i+1);
-            if(temp.length()>ans.length()) ans=temp;
+           for(int j=i;j<s.length();j++){
+               if(j-i+1>ans.size()&&checkpalindrome(s,i,j,dp)){
+                   ans=s.substr(i,j-i+1);
+               }
+           }
         }
         return ans;
     }
