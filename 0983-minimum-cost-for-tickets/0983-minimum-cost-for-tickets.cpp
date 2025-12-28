cde304a7f79bb066daaed9765a7e5d6f157c6db0
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 15:13:14 +0530

    Time: 2 ms (31.24%), Space: 13.4 MB (47.8%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-    int solve(int i,vector<int>&days,vector<int>&costs,vector<int>&dp){
-          if(i>=days.size()) return 0;
-          if(dp[i]!=-1) return dp[i];
-          int one=costs[0]+solve(i+1,days,costs,dp);
-          int j=i;
-          while(j<days.size()&&days[j]<days[i]+7)
-          j++;
-          int seven=costs[1]+solve(j,days,costs,dp);
-          j=i;
-          while(j<days.size()&&days[j]<days[i]+30)
-          j++;
-          int thirty=costs[2]+solve(j,days,costs,dp);
-          return dp[i]=min(one,min(seven,thirty));
-    }
+    // int solve(int i,vector<int>&days,vector<int>&costs,vector<int>&dp){
+    //       if(i>=days.size()) return 0;
+    //       if(dp[i]!=-1) return dp[i];
+    //       int one=costs[0]+solve(i+1,days,costs,dp);
+    //       int j=i;
+    //       while(j<days.size()&&days[j]<days[i]+7)
+    //       j++;
+    //       int seven=costs[1]+solve(j,days,costs,dp);
+    //       j=i;
+    //       while(j<days.size()&&days[j]<days[i]+30)
+    //       j++;
+    //       int thirty=costs[2]+solve(j,days,costs,dp);
+    //       return dp[i]=min(one,min(seven,thirty));
+    // }
+    // int mincostTickets(vector<int>& days, vector<int>& costs) {
+    //     vector<int>dp(days.size()+1,-1);
+    //     return solve(0,days,costs,dp);
+    // }
+
+
+
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int>dp(days.size()+1,-1);
-        return solve(0,days,costs,dp);
+        vector<int>dp(days.size()+1,0);
+        for(int i=days.size()-1;i>=0;i--){
+            int one=costs[0]+dp[i+1];
+            int j=i;
+            while(j<days.size()&&days[j]<days[i]+7)
+            j++;
+            int seven=costs[1]+dp[j];
+            j=i;
+            while(j<days.size()&&days[j]<days[i]+30)
+            j++;
+            int thirty=costs[2]+dp[j];
+            dp[i]=min(one,min(seven,thirty));
+        }
+        return dp[0];
     }
 };
