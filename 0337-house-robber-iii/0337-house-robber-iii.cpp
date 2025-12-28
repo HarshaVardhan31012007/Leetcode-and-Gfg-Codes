d6c3d794d929b557783e20ad68179d66db2ad3e5
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 22:37:54 +0530

    Time: 5 ms (39.99%), Space: 25.4 MB (37.27%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -11,17 +11,34 @@
  */
 class Solution {
 public:
-    pair<int,int>solve(TreeNode* root){
-        if(!root) return {0,0};
-        if(!root->left&&!root->right) return {root->val,0};
-        auto l=solve(root->left);
-        auto r=solve(root->right);
-        int inc=root->val+l.second+r.second;
-        int exc=max(l.first,l.second)+max(r.first,r.second);
-        return {inc,exc};
-    }
+    // pair<int,int>solve(TreeNode* root){
+    //     if(!root) return {0,0};
+    //     if(!root->left&&!root->right) return {root->val,0};
+    //     auto l=solve(root->left);
+    //     auto r=solve(root->right);
+    //     int inc=root->val+l.second+r.second;
+    //     int exc=max(l.first,l.second)+max(r.first,r.second);
+    //     return {inc,exc};
+    // }
+    // int rob(TreeNode* root) {
+    //    auto ans=solve(root);
+    //    return max(ans.first,ans.second);
+    // }
+
+
+    map<TreeNode*,int>mpp;
     int rob(TreeNode* root) {
-       auto ans=solve(root);
-       return max(ans.first,ans.second);
+        if(!root) return 0;
+        if(!root->left&!root->right) return root->val;
+        if(mpp.count(root)) return mpp[root];
+        int inc=root->val;
+        if(root->left){
+            inc+=rob(root->left->left)+rob(root->left->right);
+        }
+        if(root->right){
+            inc+=rob(root->right->left)+rob(root->right->right);
+        }
+        int exc=rob(root->left)+rob(root->right);
+        return mpp[root]=max(inc,exc);
     }
 };
