d697529aa8162fd57404a311aefb06f53f042867
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 14:54:29 +0530

    Time: 124 ms (19.13%), Space: 14.1 MB (20.99%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=1;i<=sqrt(n);i++){
             if(i*i<=n){
                 int recans=solve(n-i*i,dp);
-                if(recans!=INT_MAX)
+                // if(recans!=INT_MAX)
                 ans=min(ans,1+recans);
             }
         }
