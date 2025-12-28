e15259733f8f5a8740c6f265a7de240626d7bdb4
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 20:07:33 +0530

    Time: 0 ms (100%), Space: 8 MB (42.35%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,40 +1,16 @@
 class Solution {
 public:
-    int solve(int s,int e,vector<vector<int>>&dp){
-        if(s>=e) return 1;
-        if(dp[s][e]!=-1) return dp[s][e];
-        int count=0;
-        for(int i=s;i<=e;i++){
-          int l=solve(s,i-1,dp);
-          int r=solve(i+1,e,dp);
-          count+=l*r;
+    int solve(int n,vector<int>&dp) {
+        if(n==0||n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=solve(i,dp)*solve(n-i-1,dp);
         }
-        return dp[s][e]=count;
+        return dp[n]=ans;
     }
-    int numTrees(int n) {
-        //method 1
-        // if(n==0||n==1)
-        // return  1;
-        // int ans=0;
-        // for(int i=0;i<n;i++){
-        //      ans+=numTrees(i)*numTrees(n-i-1);
-        // }
-        // return ans;
-
-        //Method 2 //dp
-
-        // vector<int>result(n+1,0);
-        // result[0]=result[1]=1;
-        // for(int i=2;i<=n;i++){
-        //     for(int j=0;j<i;j++){
-        //         result[i]+=result[j]*result[i-j-1];
-        //     }
-        // }
-        // return result[n];
-
-
-        //method 3
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(1,n,dp);
+    int numTrees(int n){
+         vector<int>dp(n+1,-1);
+         return solve(n,dp);
     }
 };
