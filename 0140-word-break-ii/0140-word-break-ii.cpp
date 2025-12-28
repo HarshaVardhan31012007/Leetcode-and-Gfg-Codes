c7ed5f3d6f2556e48bb49ba74255f9e3d6f33b6d
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 17:43:21 +0530

    Time: 0 ms (100%), Space: 11 MB (14.77%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,56 +1,57 @@
 class Solution {
 public:
-    class Trie{
-         public:
-         char data;
-         Trie* children[26];
-         bool isterminal;
-         Trie(char data){
-            this->data=data;
-            for(int i=0;i<26;i++)
-            this->children[i]=NULL;
-            this->isterminal=false;
-         }
-         bool find(Trie* &root,string &s,int i){
-            if(i>=s.length()) return root->isterminal;
-            if(root->children[s[i]-'a']!=NULL)
-            return find(root->children[s[i]-'a'],s,i+1);
-            else
-            return false;
-         }
-      };
-      vector<string>solve(string &s,int i,vector<vector<string>>&dp,Trie* &root){
-        if(i>=s.length()) return {""};
-        if(!dp[i].empty()) return dp[i];
+//    bool check(string &temp,vector<string>& wordDict){
+//         for(auto &i:wordDict){
+//            if(i==temp)
+//            return true;
+//         }
+//         return false;
+//     }
+//     void solve(int i,string &s,vector<string>& wordDict,vector<string>&ans,string t){
+//         if(i==s.size()) {
+//             t.pop_back();
+//             ans.push_back(t);
+//         }
+//         string temp="";
+//         for(int j=i;j<s.length();j++){
+//             temp=temp+s[j];
+//             if(check(temp,wordDict)){
+//              solve(j+1,s,wordDict,ans,t+temp+" ");
+//             }
+//         }
+//     }
+//     vector<string> wordBreak(string s, vector<string>& wordDict) {
+//         vector<string>ans;
+//         string t="";
+//         solve(0,s,wordDict,ans,t);
+//         return ans;
+//     }
+
+
+
+    unordered_map<string,bool>mpp;
+    unordered_map<int,vector<string>>dp;
+    vector<string>solve(int i,string &s){
+        if(i==s.size()) return {""};
+        if(dp.find(i)!=dp.end()) return dp[i];
         string temp="";
         vector<string>ans;
         for(int j=i;j<s.length();j++){
-            temp+=s[j];
-            if(root->find(root,temp,0)){
-               vector<string>temp1=solve(s,j+1,dp,root);
-               for(auto &each:temp1){\
-                  if(!each.empty())
-                  ans.push_back(temp+" "+each);
-                  else
-                  ans.push_back(temp);
-               }
+            temp=temp+s[j];
+            if(mpp[temp]==1){
+             auto right=solve(j+1,s);
+             for(auto &k:right){
+               string end="";
+               if(k.size()>0) end=" "+k;
+               ans.push_back(temp+end);
+             }
             }
         }
         return dp[i]=ans;
     }
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        Trie* root=new Trie('-');
-        for(auto &each:wordDict){
-            Trie* curr=root;
-            for(int i=0;i<each.length();i++){
-               if(curr->children[each[i]-'a']==NULL)
-               curr->children[each[i]-'a']=new Trie(each[i]);
-               curr=curr->children[each[i]-'a'];
-            }
-            curr->isterminal=true;
-        }
-        vector<vector<string>>dp(s.length()+1);
-        vector<string>ans=solve(s,0,dp,root);
-        return ans;
+        for(auto &i:wordDict)
+        mpp[i]=1;
+        return solve(0,s);
     }
 };
