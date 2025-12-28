eb147ef13e365e29e36435fdd67c8fa30690b1ec
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 17:01:25 +0530

    Time: 2 ms (72.08%), Space: 12 MB (64.42%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
-    bool solve(string &s,int i,vector<int>&dp,unordered_set<string>&st){
+    // bool solve(string &s,int i,vector<int>&dp,unordered_set<string>&st){
+    //     if(i>=s.length()) return true;
+    //     if(dp[i]!=-1) return dp[i];
+    //     string temp="";bool ans=false;
+    //     for(int j=i;j<s.length();j++){
+    //         temp+=s[j];
+    //         if(st.find(temp)!=st.end()){
+    //            ans=ans||solve(s,j+1,dp,st);
+    //         }
+    //     }
+    //     return dp[i]=ans;
+    // }
+    // bool wordBreak(string s, vector<string>& wordDict) {
+    //     unordered_set<string>st;
+    //     for(auto &each:wordDict)
+    //     st.insert(each);
+    //     vector<int>dp(s.length()+1,-1);
+    //     return solve(s,0,dp,st);
+    // }
+
+
+    
+    
+     bool solve(string &s,int i,vector<int>&dp,unordered_map<string,int>&mpp){
         if(i>=s.length()) return true;
         if(dp[i]!=-1) return dp[i];
         string temp="";bool ans=false;
         for(int j=i;j<s.length();j++){
             temp+=s[j];
-            if(st.find(temp)!=st.end()){
-               ans=ans||solve(s,j+1,dp,st);
+            if(mpp.find(temp)!=mpp.end()){
+               ans=ans||solve(s,j+1,dp,mpp);
             }
         }
         return dp[i]=ans;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string>st;
+        unordered_map<string,int>mpp;
         for(auto &each:wordDict)
-        st.insert(each);
+        mpp[each]=1;
         vector<int>dp(s.length()+1,-1);
-        return solve(s,0,dp,st);
+        return solve(s,0,dp,mpp);
     }
 };
