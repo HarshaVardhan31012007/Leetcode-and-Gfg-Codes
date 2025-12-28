3f4009d1332431a5d1018cf673281182af3848cd
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 16:57:02 +0530

    Time: 3 ms (69.63%), Space: 11.9 MB (67.2%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,56 +1,22 @@
 class Solution {
 public:
-    // bool check(string &temp,vector<string>& wordDict){
-    //     for(auto &i:wordDict){
-    //        if(i==temp)
-    //        return true;
-    //     }
-    //     return false;
-    // }
-    // bool solve(int i,string &s,vector<string>& wordDict,vector<int>&dp){
-    //     if(i==s.size()) return 1;
-    //     if(dp[i]!=-1)
-    //     return dp[i];
-    //     string temp="";
-    //     bool flag=false;
-    //     for(int j=i;j<s.length();j++){
-    //         temp=temp+s[j];
-    //         if(check(temp,wordDict)){
-    //             flag=flag||solve(j+1,s,wordDict,dp);
-    //         }
-    //     }
-    //     return dp[i]=flag;
-    // }
-    // bool wordBreak(string s, vector<string>& wordDict) {
-    //     vector<int>dp(s.length()+1,-1);
-    //     return solve(0,s,wordDict,dp);
-    // }
-
-
-     bool check(string &temp,vector<string>& wordDict){
-        for(auto &i:wordDict){
-           if(i==temp)
-           return true;
-        }
-        return false;
-    }
-    bool solve(string &s,vector<string>& wordDict){
-        vector<int>dp(s.length()+1,0);
-        dp[s.length()]=1;
-        for(int i=s.length()-1;i>=0;i--){
-        string temp="";
-        bool flag=false;
+    bool solve(string &s,int i,vector<int>&dp,unordered_set<string>&st){
+        if(i>=s.length()) return true;
+        if(dp[i]!=-1) return dp[i];
+        string temp="";bool ans=false;
         for(int j=i;j<s.length();j++){
-            temp=temp+s[j];
-            if(check(temp,wordDict)){
-                flag=flag||dp[j+1];
+            temp+=s[j];
+            if(st.find(temp)!=st.end()){
+               ans=ans||solve(s,j+1,dp,st);
             }
         }
-        dp[i]=flag;
-        }
-        return dp[0];
+        return dp[i]=ans;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        return solve(s,wordDict);
+        unordered_set<string>st;
+        for(auto &each:wordDict)
+        st.insert(each);
+        vector<int>dp(s.length()+1,-1);
+        return solve(s,0,dp,st);
     }
 };
