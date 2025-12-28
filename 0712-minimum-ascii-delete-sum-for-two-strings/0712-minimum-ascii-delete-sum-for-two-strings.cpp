8114c4b7c22a6f8acf09ff1559de8be32cc5f6ce
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 15:46:50 +0530

    Time: 13 ms (81.56%), Space: 9.6 MB (96.35%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -44,24 +44,50 @@ public:
 
 
 
+    // int minimumDeleteSum(string s1, string s2) {
+    //     vector<vector<int>>dp(s1.length()+1,vector<int>(s2.length()+1,INT_MAX));
+    //     dp[s1.length()][s2.length()]=0;
+    //     for(int i=s1.length()-1;i>=0;i--){
+    //        dp[i][s2.length()]=dp[i+1][s2.length()]+s1[i];
+    //     }
+    //     for(int i=s2.length()-1;i>=0;i--){
+    //        dp[s1.length()][i]=dp[s1.length()][i+1]+s2[i];
+    //     }
+    //     for(int i=s1.length()-1;i>=0;i--){
+    //         for(int j=s2.length()-1;j>=0;j--){
+    //             if(s1[i]==s2[j]){
+    //                dp[i][j]=dp[i+1][j+1];
+    //                continue;
+    //             }
+    //             dp[i][j]=min(s1[i]+dp[i+1][j],s2[j]+dp[i][j+1]);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+
+
+    
     int minimumDeleteSum(string s1, string s2) {
-        vector<vector<int>>dp(s1.length()+1,vector<int>(s2.length()+1,INT_MAX));
-        dp[s1.length()][s2.length()]=0;
-        for(int i=s1.length()-1;i>=0;i--){
-           dp[i][s2.length()]=dp[i+1][s2.length()]+s1[i];
-        }
+        vector<int>next(s2.length()+1,INT_MAX);
+        vector<int>curr(s2.length()+1,INT_MAX);
+        next[s2.length()]=0;
         for(int i=s2.length()-1;i>=0;i--){
-           dp[s1.length()][i]=dp[s1.length()][i+1]+s2[i];
+           next[i]=next[i+1]+s2[i];
         }
         for(int i=s1.length()-1;i>=0;i--){
-            for(int j=s2.length()-1;j>=0;j--){
+            for(int j=s2.length();j>=0;j--){
+                if(j==s2.length()){
+                    curr[j]=next[j]+s1[i];
+                    continue;
+                }
                 if(s1[i]==s2[j]){
-                   dp[i][j]=dp[i+1][j+1];
+                   curr[j]=next[j+1];
                    continue;
                 }
-                dp[i][j]=min(s1[i]+dp[i+1][j],s2[j]+dp[i][j+1]);
+                curr[j]=min(s1[i]+next[j],s2[j]+curr[j+1]);
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
