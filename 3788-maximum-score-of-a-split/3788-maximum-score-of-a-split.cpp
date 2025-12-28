8634fbad991f78bbbd249b4a66b6561534c1a59f
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 10:53:10 +0530

    Time: 0 ms (100%), Space: 138.7 MB (100%) - LeetHub

--- 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.cpp
+++ 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     long long maximumScore(vector<int>& nums) {
-        vector<long long>suffix(nums.size(),0);
-        suffix[nums.size()-1]=nums[nums.size()-1];
-        for(long long i=nums.size()-2;i>=0;i--){
-            suffix[i]=min(suffix[i+1],1LL*nums[i]);
+        // vector<long long>suffix(nums.size(),0);
+        // suffix[nums.size()-1]=nums[nums.size()-1];
+        // for(long long i=nums.size()-2;i>=0;i--){
+        //     suffix[i]=min(suffix[i+1],1LL*nums[i]);
+        // }
+        // long long p=0;
+        // long long ans=LLONG_MIN;
+        // for(int i=0;i<(int)nums.size()-1;i++){
+        //     p+=nums[i];
+        //     ans=max(ans,p-suffix[i+1]);
+        // }
+        // return ans;
+
+
+
+        
+        long long n=nums.size();
+        long long ans=LLONG_MAX;
+        long long mini=nums[n-1];
+        long long s=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            ans=min(ans,s+mini);
+            s+=nums[i];
+            mini=min(mini,1LL*nums[i]);
         }
-        long long p=0;
-        long long ans=LLONG_MIN;
-        for(int i=0;i<(int)nums.size()-1;i++){
-            p+=nums[i];
-            ans=max(ans,p-suffix[i+1]);
-        }
-        return ans;
+        return s-ans;
     }
 };
