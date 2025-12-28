6be8ea724f83f0bd49f9a9ffecc501b794704444
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 17:08:17 +0530

    Time: 1 ms (74.16%), Space: 14.4 MB (51.75%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -23,23 +23,71 @@ public:
 
     
     
-     bool solve(string &s,int i,vector<int>&dp,unordered_map<string,int>&mpp){
+    //  bool solve(string &s,int i,vector<int>&dp,unordered_map<string,int>&mpp){
+    //     if(i>=s.length()) return true;
+    //     if(dp[i]!=-1) return dp[i];
+    //     string temp="";bool ans=false;
+    //     for(int j=i;j<s.length();j++){
+    //         temp+=s[j];
+    //         if(mpp.find(temp)!=mpp.end()){
+    //            ans=ans||solve(s,j+1,dp,mpp);
+    //         }
+    //     }
+    //     return dp[i]=ans;
+    // }
+    // bool wordBreak(string s, vector<string>& wordDict) {
+    //     unordered_map<string,int>mpp;
+    //     for(auto &each:wordDict)
+    //     mpp[each]=1;
+    //     vector<int>dp(s.length()+1,-1);
+    //     return solve(s,0,dp,mpp);
+    // }
+
+
+
+      class Trie{
+         public:
+         char data;
+         Trie* children[26];
+         bool isterminal;
+         Trie(char data){
+            this->data=data;
+            for(int i=0;i<26;i++)
+            this->children[i]=NULL;
+            this->isterminal=false;
+         }
+         bool find(Trie* &root,string &s,int i){
+            if(i>=s.length()) return root->isterminal;
+            if(root->children[s[i]-'a']!=NULL)
+            return find(root->children[s[i]-'a'],s,i+1);
+            else
+            return false;
+         }
+      };
+      bool solve(string &s,int i,vector<int>&dp,Trie* &root){
         if(i>=s.length()) return true;
         if(dp[i]!=-1) return dp[i];
         string temp="";bool ans=false;
         for(int j=i;j<s.length();j++){
             temp+=s[j];
-            if(mpp.find(temp)!=mpp.end()){
-               ans=ans||solve(s,j+1,dp,mpp);
+            if(root->find(root,temp,0)){
+               ans=ans||solve(s,j+1,dp,root);
             }
         }
         return dp[i]=ans;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_map<string,int>mpp;
-        for(auto &each:wordDict)
-        mpp[each]=1;
+        Trie* root=new Trie('-');
+        for(auto &each:wordDict){
+            Trie* curr=root;
+            for(int i=0;i<each.length();i++){
+               if(curr->children[each[i]-'a']==NULL)
+               curr->children[each[i]-'a']=new Trie(each[i]);
+               curr=curr->children[each[i]-'a'];
+            }
+            curr->isterminal=true;
+        }
         vector<int>dp(s.length()+1,-1);
-        return solve(s,0,dp,mpp);
+        return solve(s,0,dp,root);
     }
 };
