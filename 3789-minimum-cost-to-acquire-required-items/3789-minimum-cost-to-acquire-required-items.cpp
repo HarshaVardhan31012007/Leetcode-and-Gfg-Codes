4bc9339d6af3f8f7d337bc2f3558831e495f4dbc
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 11:30:05 +0530

    Time: 0 ms (100%), Space: 9.3 MB (28.57%) - LeetHub

--- 3789-minimum-cost-to-acquire-required-items/3789-minimum-cost-to-acquire-required-items.cpp
+++ 3789-minimum-cost-to-acquire-required-items/3789-minimum-cost-to-acquire-required-items.cpp
@@ -1,30 +1,53 @@
 class Solution {
 public:
     long long minimumCost(int cost1, int cost2, int costBoth, int need1, int need2) {
-       long long both=0;
+    //    long long both=0;
+    //     long long cost=0;
+    //     if(need1>need2){
+    //         long long diff=need1-need2;
+    //         if(cost1<costBoth){
+    //              cost+=cost1*diff*1LL;
+    //         }
+    //         else{
+    //              cost+=costBoth*diff*1LL;
+    //         }
+    //         need1-=diff;
+    //     }
+    //     else if(need2>need1){
+    //         long long diff=need2-need1;
+    //         if(cost2<costBoth){
+    //             cost+=cost2*diff*1LL;
+    //         }
+    //         else{
+    //             cost+=costBoth*diff*1LL;
+    //         }
+    //         need2-=diff;
+    //     }
+    //     long long curr=min(cost1+cost2,costBoth);
+    //     cost+=curr*need1*1LL;
+    //     return cost;
+
+
         long long cost=0;
-        if(need1>need2){
-            long long diff=need1-need2;
-            if(cost1<costBoth){
-                 cost+=cost1*diff*1LL;
+        if(costBoth<cost1+cost2){
+            cost+=1LL*min(need1,need2)*costBoth;
+            if(need1>need2){
+               if(cost1<costBoth)
+                  cost+=1LL*(need1-need2)*cost1;
+                else
+                  cost+=1LL*(need1-need2)*costBoth;
             }
             else{
-                 cost+=costBoth*diff*1LL;
+               if(cost2<costBoth)
+                  cost+=1LL*(need2-need1)*cost2;
+               else
+                  cost+=1LL*(need2-need1)*costBoth;
             }
-            need1-=diff;
         }
-        else if(need2>need1){
-            long long diff=need2-need1;
-            if(cost2<costBoth){
-                cost+=cost2*diff*1LL;
-            }
-            else{
-                cost+=costBoth*diff*1LL;
-            }
-            need2-=diff;
+        else{
+            cost+=1LL*cost1*need1;
+            cost+=1LL*cost2*need2;
         }
-        long long curr=min(cost1+cost2,costBoth);
-        cost+=curr*need1*1LL;
         return cost;
     }
 };
