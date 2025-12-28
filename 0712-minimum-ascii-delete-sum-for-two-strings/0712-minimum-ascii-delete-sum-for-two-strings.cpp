bdc371feb8b89efc74489261151256eae9b79809
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-28 15:42:07 +0530

    Time: 11 ms (87.48%), Space: 18.3 MB (23.96%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -20,36 +20,46 @@ public:
 
 
 
-     int solve(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
-        if(i>=s1.length()&&j>=s2.length()) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i<s1.length()&&j<s2.length()&&s1[i]==s2[j]) 
-        return dp[i][j]=solve(s1,s2,i+1,j+1,dp);
-        int l=INT_MAX;
-        if(i<s1.length())
-            l=s1[i]+solve(s1,s2,i+1,j,dp);
-        int r=INT_MAX;
-        if(j<s2.length())
-           r=s2[j]+solve(s1,s2,i,j+1,dp);
-        return dp[i][j]=min(l,r);
-    }
+    // int minimumDeleteSum(string s1, string s2) {
+    //     vector<vector<int>>dp(s1.length()+1,vector<int>(s2.length()+1,INT_MAX));
+    //     dp[s1.length()][s2.length()]=0;
+    //     for(int i=s1.length();i>=0;i--){
+    //         for(int j=s2.length();j>=0;j--){
+    //             if(i==s1.length()&&j==s2.length()) continue;
+    //             if(i<s1.length()&&j<s2.length()&&s1[i]==s2[j]){
+    //                dp[i][j]=dp[i+1][j+1];
+    //                continue;
+    //             }
+    //             int l=INT_MAX;
+    //             if(i<s1.length())
+    //                 l=s1[i]+dp[i+1][j];
+    //             int r=INT_MAX;
+    //             if(j<s2.length())
+    //             r=s2[j]+dp[i][j+1];
+    //             dp[i][j]=min(l,r);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+
+
+
     int minimumDeleteSum(string s1, string s2) {
         vector<vector<int>>dp(s1.length()+1,vector<int>(s2.length()+1,INT_MAX));
         dp[s1.length()][s2.length()]=0;
-        for(int i=s1.length();i>=0;i--){
-            for(int j=s2.length();j>=0;j--){
-                if(i==s1.length()&&j==s2.length()) continue;
-                if(i<s1.length()&&j<s2.length()&&s1[i]==s2[j]){
+        for(int i=s1.length()-1;i>=0;i--){
+           dp[i][s2.length()]=dp[i+1][s2.length()]+s1[i];
+        }
+        for(int i=s2.length()-1;i>=0;i--){
+           dp[s1.length()][i]=dp[s1.length()][i+1]+s2[i];
+        }
+        for(int i=s1.length()-1;i>=0;i--){
+            for(int j=s2.length()-1;j>=0;j--){
+                if(s1[i]==s2[j]){
                    dp[i][j]=dp[i+1][j+1];
                    continue;
                 }
-                int l=INT_MAX;
-                if(i<s1.length())
-                    l=s1[i]+dp[i+1][j];
-                int r=INT_MAX;
-                if(j<s2.length())
-                r=s2[j]+dp[i][j+1];
-                dp[i][j]=min(l,r);
+                dp[i][j]=min(s1[i]+dp[i+1][j],s2[j]+dp[i][j+1]);
             }
         }
         return dp[0][0];
