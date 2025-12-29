7f690daa7861d1cf7f18625e1ea1a648e4ee3e34
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-30 00:25:27 +0530

    Time: 317 ms (7.24%), Space: 227 MB (5.35%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,5 +1,32 @@
 class Solution {
 public:
+    // int solve(vector<int>&stoneValue,int i,int flag,vector<vector<int>>&dp){
+    //     if(i>=stoneValue.size()) return 0;
+    //     if(dp[i][flag]!=-1) return dp[i][flag];
+    //     int sum=0;
+    //     int ans=(flag?INT_MAX:INT_MIN);
+    //     for(int j=1;j<=min(3,(int)stoneValue.size()-i);j++){
+    //        sum+=stoneValue[i+j-1];
+    //        if(!flag)
+    //        ans=max(ans,sum+solve(stoneValue,i+j,!flag,dp));
+    //        else
+    //        ans=min(ans,solve(stoneValue,i+j,!flag,dp));
+    //     }
+    //     return dp[i][flag]=ans;
+    // }
+    // string stoneGameIII(vector<int>& stoneValue) {
+    //     int n=stoneValue.size();
+    //     vector<vector<int>>dp(n+1,vector<int>(2,-1));
+    //     int alice=solve(stoneValue,0,0,dp);
+    //     int t=accumulate(stoneValue.begin(),stoneValue.end(),0);
+    //     int bob=t-alice;
+    //     if(alice>bob) return "Alice";
+    //     else if(bob>alice) return "Bob";
+    //     return "Tie";
+    // }
+
+
+
     int solve(vector<int>&stoneValue,int i,int flag,vector<vector<int>>&dp){
         if(i>=stoneValue.size()) return 0;
         if(dp[i][flag]!=-1) return dp[i][flag];
@@ -10,18 +37,16 @@ public:
            if(!flag)
            ans=max(ans,sum+solve(stoneValue,i+j,!flag,dp));
            else
-           ans=min(ans,solve(stoneValue,i+j,!flag,dp));
+           ans=min(ans,-sum+solve(stoneValue,i+j,!flag,dp));
         }
         return dp[i][flag]=ans;
     }
     string stoneGameIII(vector<int>& stoneValue) {
         int n=stoneValue.size();
         vector<vector<int>>dp(n+1,vector<int>(2,-1));
-        int alice=solve(stoneValue,0,0,dp);
-        int t=accumulate(stoneValue.begin(),stoneValue.end(),0);
-        int bob=t-alice;
-        if(alice>bob) return "Alice";
-        else if(bob>alice) return "Bob";
+        int ans=solve(stoneValue,0,0,dp);
+        if(ans>0) return "Alice";
+        else if(ans<0) return "Bob";
         return "Tie";
     }
 };
