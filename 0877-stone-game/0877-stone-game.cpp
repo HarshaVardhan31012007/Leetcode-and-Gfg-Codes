21ea75aa9859c7e85b602161b20fadebce97ad10
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-29 23:29:12 +0530

    Time: 316 ms (5.01%), Space: 119.9 MB (5.01%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-        return 1;
+    // bool stoneGame(vector<int>& piles) {
+    //     return 1;
+    // 
+    //}
+
+    // int solve(vector<int>&piles,int i,int j,vector<vector<int>>&dp){
+    //     if(i>j) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if((j-i+1)%2==0){
+    //         return dp[i][j]=max(piles[i]+solve(piles,i+1,j,dp),piles[j]+solve(piles,i,j-1,dp));
+    //     }
+    //     else{
+    //         return dp[i][j]=min(-piles[i]+solve(piles,i+1,j,dp),-piles[j]+solve(piles,i,j-1,dp));
+    //     }
+    // }
+    // bool stoneGame(vector<int>& piles) {
+    //     int n=piles.size();
+    //     vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+    //     int ans=solve(piles,0,n-1,dp);
+    //     return ans>0;
+    // }
+
+
+
+    int solve(vector<int>&piles,int i,int j,int flag,vector<vector<vector<int>>>&dp){
+      if(i>j) return 0;
+      if(dp[i][j][flag]!=-1) return dp[i][j][flag];
+      if(!flag)
+      return dp[i][j][flag]=max(piles[i]+solve(piles,i+1,j,!flag,dp),piles[j]+solve(piles,i,j-1,!flag,dp));
+      else
+      return dp[i][j][flag]=min(solve(piles,i+1,j,!flag,dp),solve(piles,i,j-1,!flag,dp));
     }
+    bool stoneGame(vector<int>& piles) {
+        int n=piles.size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(2,-1)));
+        auto ans=solve(piles,0,n-1,0,dp);
+        auto t=accumulate(piles.begin(),piles.end(),0LL);
+        return ans>t-ans;
+    }
+    
 };
