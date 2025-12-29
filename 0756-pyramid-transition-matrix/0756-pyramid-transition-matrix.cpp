7d34706e1f45faeada6dc7aa80b47cadaf06b833
HARSHA VARDHAN <hv1900019@gmail.com>
2025-12-29 18:45:09 +0530

    Time: 383 ms (40%), Space: 15.4 MB (59.46%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
@@ -1,13 +1,40 @@
 class Solution {
 public:
-    map<tuple<string,string,int>,bool>mpp1;
+    // map<tuple<string,string,int>,bool>mpp1;
+    // bool isPyramid(string &bottom,string &temp,unordered_map<string,vector<char>>&mpp,int i){
+    //     if(bottom.length()==1) return true;
+    //     if(i==bottom.length()-1){
+    //         string t="";
+    //         return isPyramid(temp,t,mpp,0);
+    //     }
+    //     if(mpp1.count({bottom,temp,i})) return mpp1[{bottom,temp,i}];
+    //     string curr=bottom.substr(i,2);
+    //     if(!mpp.count(curr)) return false;
+    //     bool ans=false;
+    //     for(auto &each:mpp[curr]){
+    //         temp.push_back(each);
+    //         ans=ans||isPyramid(bottom,temp,mpp,i+1);
+    //         temp.pop_back();
+    //     }
+    //     return mpp1[{bottom,temp,i}]=ans;
+    // }
+    // bool pyramidTransition(string bottom, vector<string>& allowed) {
+    //     unordered_map<string,vector<char>>mpp;
+    //     for(auto &each:allowed){
+    //         mpp[each.substr(0,2)].push_back(each[2]);
+    //     }
+    //     string temp="";
+    //     return isPyramid(bottom,temp,mpp,0);
+    // }
+
+    map<string,bool>mpp1;
     bool isPyramid(string &bottom,string &temp,unordered_map<string,vector<char>>&mpp,int i){
         if(bottom.length()==1) return true;
         if(i==bottom.length()-1){
             string t="";
-            return isPyramid(temp,t,mpp,0);
+            if(mpp1.count(temp)) return mpp1[temp];
+            return mpp1[temp]=isPyramid(temp,t,mpp,0);
         }
-        if(mpp1.count({bottom,temp,i})) return mpp1[{bottom,temp,i}];
         string curr=bottom.substr(i,2);
         if(!mpp.count(curr)) return false;
         bool ans=false;
@@ -16,7 +43,7 @@ public:
             ans=ans||isPyramid(bottom,temp,mpp,i+1);
             temp.pop_back();
         }
-        return mpp1[{bottom,temp,i}]=ans;
+        return ans;
     }
     bool pyramidTransition(string bottom, vector<string>& allowed) {
         unordered_map<string,vector<char>>mpp;
