d4185d8e8ef84de95138ab541266e2a99657d294
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-01 19:43:41 +0530

    Time: 198 ms (41.16%), Space: 13.7 MB (79.22%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,60 +1,19 @@
 class Solution {
 public:
-    // map<vector<int>,int>dp;
-    // int solve(vector<int>&arr){
-    //     if(arr.size()==0) return 0;
-    //     if(dp.find(arr)!=dp.end()) return dp[arr];
-    //     int ans=INT_MIN;
-    //     int n=arr.size();
-    //     for(int i=0;i<n;i++){
-    //         int t=arr[i];
-    //         int prev=i-1>=0?arr[i-1]:1;
-    //         int curr=i+1<n?arr[i+1]:1;
-    //         arr.erase(arr.begin()+i);
-    //         ans=max(ans,prev*t*curr+solve(arr));
-    //         arr.insert(arr.begin()+i,t);
-    //     }
-    //     return dp[arr]=ans;
-    // }
-    // int maxCoins(vector<int>& nums) {
-    //    return solve(nums); 
-    // }
-
-    // int solve(vector<int>&nums,int s,int e,vector<vector<int>>&dp){
-    //     if(s>e) return 0;
-    //     if(dp[s][e]!=-1) return dp[s][e];
-    //     int ans=INT_MIN;
-    //     for(int i=s;i<=e;i++){
-    //         ans=max(ans,nums[s-1]*nums[i]*nums[e+1]+solve(nums,s,i-1,dp)+solve(nums,i+1,e,dp));
-    //     }
-    //     return dp[s][e]=ans;
-    // }
-    // int maxCoins(vector<int>& nums) {
-    //    nums.insert(nums.begin(),1);
-    //    nums.insert(nums.end(),1);
-    //    int n=nums.size();
-    //    vector<vector<int>>dp(n,vector<int>(n,-1));
-    //    return solve(nums,1,n-2,dp); 
-    // }
-
-
-     int solve(vector<int>&nums){
-       nums.insert(nums.begin(),1);
-       nums.insert(nums.end(),1);
-       int n=nums.size();
-       vector<vector<int>>dp(n,vector<int>(n,0));
-       for(int s=n-2;s>=1;s--){
-        for(int e=s;e<=n-2;e++){
+    int solve(vector<int>&nums,int i,int j,vector<vector<int>>&dp){
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
         int ans=INT_MIN;
-        for(int i=s;i<=e;i++){
-            ans=max(ans,nums[s-1]*nums[i]*nums[e+1]+dp[s][i-1]+dp[i+1][e]);
+        for(int k=i;k<=j;k++){
+            ans=max(ans,nums[i-1]*nums[k]*nums[j+1]+solve(nums,i,k-1,dp)+solve(nums,k+1,j,dp));
         }
-        dp[s][e]=ans;
-        }
-       }
-       return dp[1][n-2];
+        return dp[i][j]=ans;
     }
     int maxCoins(vector<int>& nums) {
-       return solve(nums); 
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(nums,1,n-2,dp);
     }
 };
