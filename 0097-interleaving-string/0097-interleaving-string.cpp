1819dd06b40eeac47164061611d24bcd61f408fe
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-03 00:22:42 +0530

    Time: 5 ms (42.36%), Space: 9.9 MB (30.59%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
-    bool solve(string &s1,string &s2,string &s3,int i,int j,int k,vector<vector<vector<int>>>&dp){
-        if(k>=s3.size()&&j>=s2.length()&&i>=s1.length()) return true;
-        if(dp[i][j][k]!=-1) return dp[i][j][k];
+    // bool solve(string &s1,string &s2,string &s3,int i,int j,int k,vector<vector<vector<int>>>&dp){
+    //     if(k>=s3.size()&&j>=s2.length()&&i>=s1.length()) return true;
+    //     if(dp[i][j][k]!=-1) return dp[i][j][k];
+    //     bool ans=false;
+    //     if(i<s1.length()&&k<s3.length()&&s1[i]==s3[k]){
+    //         ans=ans||solve(s1,s2,s3,i+1,j,k+1,dp);
+    //     }
+    //     if(j<s2.length()&&k<s3.length()&&s2[j]==s3[k]){
+    //         ans=ans||solve(s1,s2,s3,i,j+1,k+1,dp);
+    //     }
+    //     return dp[i][j][k]=ans;
+    // }
+    // bool isInterleave(string s1, string s2, string s3) {
+    //     vector<vector<vector<int>>>dp(s1.length()+1,vector<vector<int>>(s2.length()+1,vector<int>(s3.length()+1,-1)));
+    //     return solve(s1,s2,s3,0,0,0,dp);
+    // }
+
+
+
+     bool solve(string &s1,string &s2,string &s3,int i,int j,vector<vector<int>>&dp){
+        if(i+j>=s3.length()&&j>=s2.length()&&i>=s1.length()) return true;
+        if(dp[i][j]!=-1) return dp[i][j];
         bool ans=false;
-        if(i<s1.length()&&k<s3.length()&&s1[i]==s3[k]){
-            ans=ans||solve(s1,s2,s3,i+1,j,k+1,dp);
+        if(i<s1.length()&&i+j<s3.length()&&s1[i]==s3[i+j]){
+            ans=ans||solve(s1,s2,s3,i+1,j,dp);
         }
-        if(j<s2.length()&&k<s3.length()&&s2[j]==s3[k]){
-            ans=ans||solve(s1,s2,s3,i,j+1,k+1,dp);
+        if(j<s2.length()&&i+j<s3.length()&&s2[j]==s3[i+j]){
+            ans=ans||solve(s1,s2,s3,i,j+1,dp);
         }
-        return dp[i][j][k]=ans;
+        return dp[i][j]=ans;
     }
     bool isInterleave(string s1, string s2, string s3) {
-        vector<vector<vector<int>>>dp(s1.length()+1,vector<vector<int>>(s2.length()+1,vector<int>(s3.length()+1,-1)));
-        return solve(s1,s2,s3,0,0,0,dp);
+        vector<vector<int>>dp(s1.length()+1,vector<int>(s2.length()+1,-1));
+        return solve(s1,s2,s3,0,0,dp);
     }
 };
