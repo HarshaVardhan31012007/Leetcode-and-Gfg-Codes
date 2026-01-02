1fc3ed21abd2b04f331b93c6154e5e7edf29a5e3
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-03 00:25:50 +0530

    Time: 0 ms (100%), Space: 9.6 MB (43.35%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -19,8 +19,27 @@ public:
 
 
 
+    //  bool solve(string &s1,string &s2,string &s3,int i,int j,vector<vector<int>>&dp){
+    //     if(i+j>=s3.length()&&j>=s2.length()&&i>=s1.length()) return true;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     bool ans=false;
+    //     if(i<s1.length()&&i+j<s3.length()&&s1[i]==s3[i+j]){
+    //         ans=ans||solve(s1,s2,s3,i+1,j,dp);
+    //     }
+    //     if(j<s2.length()&&i+j<s3.length()&&s2[j]==s3[i+j]){
+    //         ans=ans||solve(s1,s2,s3,i,j+1,dp);
+    //     }
+    //     return dp[i][j]=ans;
+    // }
+    // bool isInterleave(string s1, string s2, string s3) {
+    //     vector<vector<int>>dp(s1.length()+1,vector<int>(s2.length()+1,-1));
+    //     return solve(s1,s2,s3,0,0,dp);
+    // }
+    
+
+
      bool solve(string &s1,string &s2,string &s3,int i,int j,vector<vector<int>>&dp){
-        if(i+j>=s3.length()&&j>=s2.length()&&i>=s1.length()) return true;
+        if(j>=s2.length()&&i>=s1.length()) return true;
         if(dp[i][j]!=-1) return dp[i][j];
         bool ans=false;
         if(i<s1.length()&&i+j<s3.length()&&s1[i]==s3[i+j]){
@@ -32,7 +51,26 @@ public:
         return dp[i][j]=ans;
     }
     bool isInterleave(string s1, string s2, string s3) {
+        if(s3.length()!=s1.length()+s2.length()) return false;
         vector<vector<int>>dp(s1.length()+1,vector<int>(s2.length()+1,-1));
         return solve(s1,s2,s3,0,0,dp);
     }
+
+
+
+
+    //  bool solve(string &s1,string &s2,string &s3,int i,int j,vector<vector<int>>&dp){
+    //     if(i+j>=s3.length()&&j>=s2.length()&&i>=s1.length()) return true;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+        
+    // }
+    // bool isInterleave(string s1, string s2, string s3) {
+    //     vector<vector<int>>dp(s1.length()+1,vector<int>(s2.length()+1,-1));
+    //     dp[0][0]=s3
+    //     for(int i=s1.length()-1;i>=0;i--){
+    //         for(int j=s2.length()-1;j>=0;j--){
+
+    //         }
+    //     }
+    // }
 };
