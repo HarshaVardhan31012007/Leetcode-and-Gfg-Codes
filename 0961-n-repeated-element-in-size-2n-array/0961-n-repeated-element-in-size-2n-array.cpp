094eaec05d0a4cab1c871e1f823e32912b477a4a
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-02 10:35:11 +0530

    Time: 0 ms (100%), Space: 28.4 MB (69.74%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(i>0&&nums[i]==nums[i-1])
+        // sort(nums.begin(),nums.end());
+        // for(int i=1;i<nums.size();i++){
+        //     if(i>0&&nums[i]==nums[i-1])
+        //     return nums[i];
+        // }
+        // return -1;
+
+
+
+        unordered_set<int>st;
+        for(int i=0;i<nums.size();i++){
+            if(st.count(nums[i]))
             return nums[i];
+            else
+            st.insert(nums[i]);
         }
         return -1;
     }
