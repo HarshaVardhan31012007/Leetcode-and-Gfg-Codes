ba336ee7870f128c89b3ac7fcdcb6f98942ca000
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-04 14:23:32 +0530

    Time: 419 ms (22.22%), Space: 318 MB (11.11%) - LeetHub

--- 3795-minimum-subarray-length-with-distinct-sum-at-least-k/3795-minimum-subarray-length-with-distinct-sum-at-least-k.cpp
+++ 3795-minimum-subarray-length-with-distinct-sum-at-least-k/3795-minimum-subarray-length-with-distinct-sum-at-least-k.cpp
@@ -1,3 +1,34 @@
+// class Solution {
+// public:
+//     int minLength(vector<int>& nums, int k) {
+//         int i=0;int j=0;
+//         int ans=INT_MAX;
+//         unordered_map<int,int>mpp;
+//         long long int sum=0;
+//         while(j<nums.size()){
+//             if(!mpp.count(nums[j])){
+//                 sum+=nums[j];
+//                 mpp[nums[j]]=j;
+//             }
+//             while(i<=j&&sum>=k){
+//                 ans=min(ans,j-i+1);
+//                 int idx=mpp[nums[i]];
+//                 if(idx==i){
+//                     sum-=nums[i];
+//                     mpp.erase(nums[i]);
+//                 }
+//                 i++;
+//             }
+//             mpp[nums[j]]=j;
+//             j++;
+//         }
+//         return ans==INT_MAX?-1:ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int minLength(vector<int>& nums, int k) {
@@ -8,18 +39,21 @@ public:
         while(j<nums.size()){
             if(!mpp.count(nums[j])){
                 sum+=nums[j];
-                mpp[nums[j]]=j;
+                mpp[nums[j]]++;
             }
+            else
+            mpp[nums[j]]++;
             while(i<=j&&sum>=k){
                 ans=min(ans,j-i+1);
-                int idx=mpp[nums[i]];
-                if(idx==i){
+                int c=mpp[nums[i]];
+                if(c==1){
                     sum-=nums[i];
                     mpp.erase(nums[i]);
                 }
+                else
+                mpp[nums[i]]--;
                 i++;
             }
-            mpp[nums[j]]=j;
             j++;
         }
         return ans==INT_MAX?-1:ans;
