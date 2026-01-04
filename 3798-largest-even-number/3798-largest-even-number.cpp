0c0163668085e6dfac60d5079784f1ce58b6fd81
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-04 17:59:45 +0530

    Time: 0 ms (100%), Space: 8.6 MB (73.69%) - LeetHub

--- 3798-largest-even-number/3798-largest-even-number.cpp
+++ 3798-largest-even-number/3798-largest-even-number.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
+    // string largestEven(string s) {
+    //     int i=s.length()-1;
+    //     while(i>=0){
+    //         if(s[i]=='2')
+    //             break;
+    //         i--;
+    //     }
+    //     return s.substr(0,i+1);
+    // }
+
+
     string largestEven(string s) {
         int i=s.length()-1;
         while(i>=0){
             if(s[i]=='2')
                 break;
+            s.pop_back();
             i--;
         }
-        return s.substr(0,i+1);
+        return s;
     }
 };
