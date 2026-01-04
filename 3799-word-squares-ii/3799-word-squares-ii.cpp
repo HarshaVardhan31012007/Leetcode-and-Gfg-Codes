53b6ff9a2f89c02af7db6fb106d656a64d3279d8
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-04 18:16:39 +0530

    Time: 187 ms (27.22%), Space: 59.5 MB (67.62%) - LeetHub

---
+++ 3799-word-squares-ii/3799-word-squares-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<string>> wordSquares(vector<string>& words) {
+        vector<vector<string>>ans;
+        int n=words.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j==i) continue;
+                for(int k=0;k<n;k++){
+                    if(k==j||k==i) continue;
+                    for(int p=0;p<n;p++){
+                        if(p==k||p==j||p==i) continue;
+                        string top=words[i];
+                        string left=words[j];
+                        string right=words[k];
+                        string bottom=words[p];
+                        if(top[0]==left[0]&&top[3]==right[0]&&bottom[0]==left[3]&&bottom[3]==right[3])
+                            ans.push_back({top,left,right,bottom});
+                    }
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
