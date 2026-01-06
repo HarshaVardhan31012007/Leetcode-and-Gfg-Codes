2449d65cc40bb8449453fca85972bd767b33fe93
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-06 18:56:04 +0530

    Time: 8 ms (24.97%), Space: 108.3 MB (89.84%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,28 +11,49 @@
  */
 class Solution {
 public:
+    // int maxLevelSum(TreeNode* root) {
+    //     queue<TreeNode*>q;
+    //     int sum=INT_MIN;int level=1;int ans=0;
+    //     if(root)
+    //     q.push(root);
+    //     while(!q.empty()){
+    //         int size=q.size();int csum=0;
+    //         for(int i=0;i<size;i++){
+    //             auto front=q.front();
+    //             q.pop();
+    //             csum+=front->val;
+    //             if(front->left)
+    //             q.push(front->left);
+    //             if(front->right)
+    //             q.push(front->right);
+    //         }
+    //         if(csum>sum){
+    //             sum=csum;
+    //             ans=level;
+    //         }
+    //         level++;
+    //     }
+    //     return ans;
+    // }
+
+
+
+    void solve(TreeNode* root,int level,map<int,int>&mpp){
+        if(!root) return;
+        mpp[level]+=root->val;
+        solve(root->left,level+1,mpp);
+        solve(root->right,level+1,mpp);
+    }
     int maxLevelSum(TreeNode* root) {
-        queue<TreeNode*>q;
-        int sum=INT_MIN;int level=1;int ans=0;
-        if(root)
-        q.push(root);
-        while(!q.empty()){
-            int size=q.size();int csum=0;
-            for(int i=0;i<size;i++){
-                auto front=q.front();
-                q.pop();
-                csum+=front->val;
-                if(front->left)
-                q.push(front->left);
-                if(front->right)
-                q.push(front->right);
-            }
-            if(csum>sum){
-                sum=csum;
-                ans=level;
-            }
-            level++;
+      map<int,int>mpp;
+      solve(root,0,mpp);
+      int ans=0;int maxi=INT_MIN;
+      for(auto &each:mpp){
+        if(each.second>maxi){
+            maxi=each.second;
+            ans=each.first+1;
         }
-        return ans;
+      }
+      return ans;
     }
 };
