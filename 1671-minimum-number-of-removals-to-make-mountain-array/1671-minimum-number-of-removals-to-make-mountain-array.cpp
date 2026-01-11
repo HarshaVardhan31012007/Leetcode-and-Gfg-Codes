06b1aa097822f1703aeac48966a7c1adcbeda755
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-11 22:51:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int>& nums) {
+         int n=nums.size();
+         vector<int>dp1(n,1);vector<int>dp2(n,1);
+         for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]&&dp1[i]<dp1[j]+1){
+                    dp1[i]=dp1[j]+1;
+                }
+            }
+         }
+         for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]>nums[j]&&dp2[i]<dp2[j]+1){
+                    dp2[i]=dp2[j]+1;
+                }
+            }
+         }
+         int maxi=INT_MIN;
+         for(int i=0;i<n;i++){
+            maxi=max(maxi,dp1[i]+dp2[i]-1);
+         }
+         return n-maxi;
+    }
+};
