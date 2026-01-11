c9c1deb5a4f5351ef89da65cca7bdf6c75f68056
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-11 12:22:46 +0530

    Time: 0 ms (100%), Space: 9.8 MB (68.84%) - LeetHub

---
+++ 3803-count-residue-prefixes/3803-count-residue-prefixes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // int residuePrefixes(string s) {
+    //     int c=0;int ans=0;
+    //     unordered_map<int,int>mpp;
+    //     for(int i=0;i<s.length();i++){
+    //         if(!mpp.count(s[i]))
+    //             c++;
+    //         if(c==((i+1)%3))
+    //             ans++; 
+    //         mpp[s[i]]=1;
+    //     }
+    //     return ans;
+    // }
+
+
+    int residuePrefixes(string s) {
+        int c=0;int ans=0;
+        int freq[26]={0};
+        for(int i=0;i<s.length();i++){
+            if(freq[s[i]-'a']==0)
+                c++;
+            if(c==((i+1)%3))
+                ans++; 
+            freq[s[i]-'a']++;
+        }
+        return ans;
+    }
+};
