bec1863916aea136b39c607332ff1b9b93e5f176
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-11 12:23:31 +0530

    Time: 0 ms (100%), Space: 9.7 MB (84.42%) - LeetHub

--- 3803-count-residue-prefixes/3803-count-residue-prefixes.cpp
+++ 3803-count-residue-prefixes/3803-count-residue-prefixes.cpp
@@ -14,15 +14,29 @@ public:
     // }
 
 
+    // int residuePrefixes(string s) {
+    //     int c=0;int ans=0;
+    //     int freq[26]={0};
+    //     for(int i=0;i<s.length();i++){
+    //         if(freq[s[i]-'a']==0)
+    //             c++;
+    //         if(c==((i+1)%3))
+    //             ans++; 
+    //         freq[s[i]-'a']++;
+    //     }
+    //     return ans;
+    // }
+
+
     int residuePrefixes(string s) {
         int c=0;int ans=0;
-        int freq[26]={0};
+        bool freq[26]={0};
         for(int i=0;i<s.length();i++){
             if(freq[s[i]-'a']==0)
                 c++;
             if(c==((i+1)%3))
                 ans++; 
-            freq[s[i]-'a']++;
+            freq[s[i]-'a']=1;
         }
         return ans;
     }
