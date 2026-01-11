3bdba107db704ce80bc3c8f1e4fcb020a0c0c098
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-11 16:08:58 +0530

    Time: 20 ms (90.41%), Space: 107.3 MB (94.79%) - LeetHub

--- 3805-count-caesar-cipher-pairs/3805-count-caesar-cipher-pairs.cpp
+++ 3805-count-caesar-cipher-pairs/3805-count-caesar-cipher-pairs.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     long long countPairs(vector<string>& words) {
+        // int n=words.size();
+        // int m=words[0].length();
+        // unordered_map<string,int>mpp;
+        // long long ans=0;
+        // for(int i=0;i<n;i++){
+        //     for(int k=0;k<26;k++){
+        //         string s="";
+        //         for(int j=0;j<m;j++){
+        //             char ch=((words[i][j]-'a'+k)%26)+'a';
+        //             s+=ch;
+        //         }
+        //         if(mpp.count(s))
+        //             ans+=mpp[s];
+        //     }
+        //     mpp[words[i]]++;
+        // }
+        // return ans;
+
+
+
         int n=words.size();
         int m=words[0].length();
         unordered_map<string,int>mpp;
-        long long ans=0;
         for(int i=0;i<n;i++){
-            for(int k=0;k<26;k++){
-                string s="";
-                for(int j=0;j<m;j++){
-                    char ch=((words[i][j]-'a'+k)%26)+'a';
-                    s+=ch;
-                }
-                if(mpp.count(s))
-                    ans+=mpp[s];
+            int rot=words[i][0]-'a';
+            for(auto &each:words[i]){
+                each='a'+((each-'a')-rot+26)%26;
             }
             mpp[words[i]]++;
+        }
+        long long ans=0;
+        for(auto &each:mpp){
+            ans+=(1LL*(each.second)*(each.second-1))/2;
         }
         return ans;
     }
