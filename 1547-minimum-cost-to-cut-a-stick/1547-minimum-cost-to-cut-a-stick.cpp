06f51fe347290d79f80d6f8bd9be9d11b390a274
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-12 12:59:01 +0530

    Time: 42 ms (12.55%), Space: 14.2 MB (10.22%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    //  int solve(vector<int>&cuts,int s,int e,vector<vector<int>>&dp){
+    //     if(dp[s][e]!=-1)
+    //     return dp[s][e];
+    //     int ans=INT_MAX;
+    //     for(auto &cut:cuts){
+    //       if(cut>s&&cut<e)
+    //       ans=min(ans,e-s+solve(cuts,s,cut,dp)+solve(cuts,cut,e,dp));
+    //     }
+    //     return dp[s][e]=(ans==INT_MAX)?0:ans;
+    // }
+    // int minCost(int n, vector<int>& cuts) {
+    //     vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+    //     return solve(cuts,0,n,dp);
+    // }
+
+
+
     int solve(vector<int>&cuts,int i,int j,vector<vector<int>>&dp){
         if(j-i+1<=2) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
