5c9304323f73c53d86cb3e3993537a42433a9c18
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-12 13:38:31 +0530

    Time: 0 ms (100%), Space: 17.2 MB (39.19%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    int solve(vector<int>&prices,int i,int flag,vector<vector<int>>&dp){
-        if(i>=prices.size()) return 0;
-        if(dp[i][flag]!=-1) return dp[i][flag];
-        int ans=0;
-        //buy
-        if(flag==0)
-        ans=max(ans,-prices[i]+solve(prices,i+1,1,dp));
-        //sell
-        if(flag==1)
-        ans=max(ans,prices[i]+solve(prices,i+1,0,dp));
-        //skip
-        ans=max(ans,solve(prices,i+1,flag,dp));
-        return dp[i][flag]=ans;
-    }
+    // int solve(vector<int>&prices,int i,int flag,vector<vector<int>>&dp){
+    //     if(i>=prices.size()) return 0;
+    //     if(dp[i][flag]!=-1) return dp[i][flag];
+    //     int ans=0;
+    //     //buy
+    //     if(flag==0)
+    //     ans=max(ans,-prices[i]+solve(prices,i+1,1,dp));
+    //     //sell
+    //     if(flag==1)
+    //     ans=max(ans,prices[i]+solve(prices,i+1,0,dp));
+    //     //skip
+    //     ans=max(ans,solve(prices,i+1,flag,dp));
+    //     return dp[i][flag]=ans;
+    // }
+    // int maxProfit(vector<int>& prices) {
+    //     vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
+    //     return solve(prices,0,0,dp);
+    // }
+
+
+
+   
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
-        return solve(prices,0,0,dp);
+        int ans=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]>prices[i-1])
+            ans+=prices[i]-prices[i-1];
+        }
+        return ans;
     }
 };
