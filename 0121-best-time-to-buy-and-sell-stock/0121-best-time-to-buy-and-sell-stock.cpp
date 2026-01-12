a68b2e63a499f5366dc1218e98aafcf8d3c65bf1
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-12 13:07:11 +0530

    Time: 3 ms (29.05%), Space: 97.5 MB (7.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,53 +1,13 @@
 class Solution {
 public:
-    void solve(vector<int>&prices,int i,int &bestbuy,int &maxprofit){
-        if(i==prices.size()) return;
-        if(prices[i]<bestbuy) bestbuy=prices[i];
-        maxprofit=max(maxprofit,prices[i]-bestbuy);
-        solve(prices,i+1,bestbuy,maxprofit);
+    int solve(vector<int>&prices,int i,int &mini){
+        if(i>=prices.size()) return 0;
+        int curr=prices[i]-mini;
+        mini=min(mini,prices[i]);
+        return max(curr,solve(prices,i+1,mini));
     }
     int maxProfit(vector<int>& prices) {
-        //method 1
-        // int n=prices.size();
-        // int maxi=0;//as min profit is zero in case of no transactions
-        // for(int i=0;i<n;i++){
-        //     int buyc=prices[i];
-        //     for(int j=i+1;j<n;j++){
-        //       int sellc=prices[j];
-        //       maxi=max(maxi,sellc-buyc);
-        //     }
-        // }
-        // return maxi;
-       
-        //method 2
-        // int n=prices.size();
-        // int maxi=INT_MIN;//if INT-MIN then if condition me equality hona chahihe//if 0 is there then equality no need
-        // int bestbuy=prices[0];
-        // for(int i=0;i<n;i++){
-        //     if(prices[i]>=bestbuy){
-        //         maxi=max(maxi,prices[i]-bestbuy);
-        //     }
-        //     bestbuy=min(bestbuy,prices[i]);
-        // }
-        // return maxi;
-
-        
-        //  int n=prices.size();
-        // int maxi=0;//if INT-MIN then if condition me equality hona chahihe//if 0 is there then equality no need
-        // int bestbuy=prices[0];
-        // for(int i=1;i<n;i++){
-        //     if(prices[i]>=bestbuy)//if you dont write also it works
-        //     maxi=max(maxi,prices[i]-bestbuy);
-        //     bestbuy=min(bestbuy,prices[i]);
-        // }
-        // return maxi;
-
-
-
-        //method 3
-        int bestbuy=INT_MAX;
-        int maxprofit=INT_MIN;
-        solve(prices,0,bestbuy,maxprofit);
-        return maxprofit;
+        int mini=INT_MAX;
+        return max(solve(prices,0,mini),0);
     }
 };
