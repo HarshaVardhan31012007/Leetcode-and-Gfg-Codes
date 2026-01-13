648220437ab0d096e8ee8b41993c9c9b6ba0022c
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-13 23:25:14 +0530

    Time: 78 ms (85.84%), Space: 198.7 MB (52.02%) - LeetHub

--- 3453-separate-squares-i/3453-separate-squares-i.cpp
+++ 3453-separate-squares-i/3453-separate-squares-i.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
-    double find(vector<vector<int>>& squares, double mid) {
-        double up = 0, down = 0;
-        for (auto &each : squares) {
-            double y = each[1];
-            double l = each[2];
-
-            if (y >= mid) {
-                up += l * l;
-            } else if (mid >= y + l) {
-                down += l * l;
-            } else {
-                up += l * (y + l - mid);
-                down += l * (mid - y);
+    double find(vector<vector<int>>&squares,double mid){
+        double up=0;
+        double down=0;
+        for(auto &each:squares){
+            double l=each[2];
+            if(each[1]>=mid){
+                up+=l*l;
+            }
+            else if(mid>=each[1]+l){
+                down+=l*l;
+            }
+            else{
+                up+=l*(each[1]+l-mid);
+                down+=l*(mid-each[1]);
             }
         }
-        return up - down;
+        return up-down;
     }
-
     double separateSquares(vector<vector<int>>& squares) {
-        double s = 1e9, e = 0;
-
-        for (auto &each : squares) {
-            s = min(s, (double)each[1]);
-            e = max(e, (double)(each[1] + each[2]));
+        double s=(int)1e9;
+        double e=0;
+        for(auto &each:squares){
+            s=min(s,(double)each[1]);
+            e=max(e,(double)(each[1]+each[2]));
         }
-
-        for (int i = 0; i < 80; i++) {  // fixed iterations = stable precision
-            double mid = (s + e) / 2.0;
-            double diff = find(squares, mid);
-
-            if (diff > 0)
-                s = mid;
-            else
-                e = mid;
+        double ans;
+        while(e-s>=0.00001){
+            double mid=(s+e)/2.0;
+            double diff=find(squares,mid);
+            if(diff==0){
+               ans=mid;
+               e=mid;
+            }
+            else if(diff<0){
+                e=mid-0.000001;
+            }
+            else{
+               s=mid+0.000001;
+            }
         }
-
-        return (s + e) / 2.0;
+        return (s+e)/2.0;
     }
 };
