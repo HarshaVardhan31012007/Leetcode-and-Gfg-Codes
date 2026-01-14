022770d674828179050e87cd0c7133dfa40b81de
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-14 10:50:12 +0530

    Time: 107 ms (56.1%), Space: 198.8 MB (52.33%) - LeetHub

--- 3453-separate-squares-i/3453-separate-squares-i.cpp
+++ 3453-separate-squares-i/3453-separate-squares-i.cpp
@@ -44,6 +44,52 @@
 
 
 
+// class Solution {
+// public:
+//     double find(vector<vector<int>>&squares,double mid){
+//         double up=0;
+//         double down=0;
+//         for(auto &each:squares){
+//             double l=each[2];
+//             if(each[1]>=mid){
+//                 up+=l*l;
+//             }
+//             else if(mid>=each[1]+l){
+//                 down+=l*l;
+//             }
+//             else{
+//                 up+=l*(each[1]+l-mid);
+//                 down+=l*(mid-each[1]);
+//             }
+//         }
+//         return up-down;
+//     }
+//     double separateSquares(vector<vector<int>>& squares) {
+//         double s=(int)1e9;
+//         double e=0;
+//         for(auto &each:squares){
+//             s=min(s,(double)each[1]);
+//             e=max(e,(double)(each[1]+each[2]));
+//         }
+//         double ans;
+//         while(e-s>0.000001){
+//             double mid=(s+e)/2.0;
+//             double diff=find(squares,mid);
+//             if(diff<=0){
+//                 ans=mid;
+//                 e=mid;
+//             }
+//             else{
+//                s=mid;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     double find(vector<vector<int>>&squares,double mid){
@@ -76,13 +122,12 @@ public:
             double mid=(s+e)/2.0;
             double diff=find(squares,mid);
             if(diff<=0){
-                ans=mid;
                 e=mid;
             }
             else{
                s=mid;
             }
         }
-        return ans;
+        return e;//s to e all can be ans
     }
 };
