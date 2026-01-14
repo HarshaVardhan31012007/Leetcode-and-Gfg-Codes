0d98c83546d72749e72e753ecd1583b847363ff9
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-14 22:33:23 +0530

    Time: 4 ms (13.24%), Space: 13.1 MB (78.58%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -21,16 +21,34 @@ public:
 
 
 
+    // int mincostTickets(vector<int>& days, vector<int>& costs) {
+    //     vector<int>dp(days.size()+1,0);
+    //     for(int i=days.size()-1;i>=0;i--){
+    //         int one=costs[0]+dp[i+1];
+    //         int j=lower_bound(days.begin()+i+1,days.end(),days[i]+7)-days.begin();
+    //         int seven=costs[1]+dp[j];
+    //         j=lower_bound(days.begin()+i+1,days.end(),days[i]+30)-days.begin();
+    //         int thirty=costs[2]+dp[j];
+    //         dp[i]=min(one,min(seven,thirty));
+    //     }
+    //     return dp[0];
+    // }
+
+
+
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int>dp(days.size()+1,0);
-        for(int i=days.size()-1;i>=0;i--){
-            int one=costs[0]+dp[i+1];
-            int j=lower_bound(days.begin()+i+1,days.end(),days[i]+7)-days.begin();
-            int seven=costs[1]+dp[j];
-            j=lower_bound(days.begin()+i+1,days.end(),days[i]+30)-days.begin();
-            int thirty=costs[2]+dp[j];
-            dp[i]=min(one,min(seven,thirty));
+        queue<pair<int,int>>week;
+        queue<pair<int,int>>month;
+        int ans=0;
+        for(auto &day:days){
+            while(!week.empty()&&day>=week.front().first+7)
+            week.pop();
+            while(!month.empty()&&day>=month.front().first+30)
+            month.pop();
+            week.push(make_pair(day,ans+costs[1]));
+            month.push(make_pair(day,ans+costs[2]));
+            ans=min(ans+costs[0],min(week.front().second,month.front().second));
         }
-        return dp[0];
+        return ans;
     }
 };
