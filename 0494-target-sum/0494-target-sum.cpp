5e9851ee5247c551fb1f4861c8b0a3592a9b9a1f
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-14 18:26:15 +0530

    Time: 27 ms (42.59%), Space: 61.2 MB (8.33%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -42,31 +42,31 @@ public:
     // }
 
 
-      int solve(vector<int>&nums,int target){
-        int t=accumulate(nums.begin(),nums.end(),0);
-        vector<int>curr(2*t+1,0);
-        vector<int>next(2*t+1,0);
-        for(int sum=-t;sum<=t;sum++){
-            if(sum==target)
-            next[sum+t]=1;
-        }
-        for(int i=nums.size()-1;i>=0;i--){
-            for(int sum=-t;sum<=t;sum++){
-        int a=0;
-        if(sum-nums[i]>=-t)
-        a=next[sum-nums[i]+t];
-        int b=0;
-        if(sum+nums[i]<=t)
-        b=next[sum+nums[i]+t];
-        curr[sum+t]=a+b;
-            }
-            next=curr;
-        }
-        return curr[0+t];
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums,target);
-    }
+    //   int solve(vector<int>&nums,int target){
+    //     int t=accumulate(nums.begin(),nums.end(),0);
+    //     vector<int>curr(2*t+1,0);
+    //     vector<int>next(2*t+1,0);
+    //     for(int sum=-t;sum<=t;sum++){
+    //         if(sum==target)
+    //         next[sum+t]=1;
+    //     }
+    //     for(int i=nums.size()-1;i>=0;i--){
+    //         for(int sum=-t;sum<=t;sum++){
+    //     int a=0;
+    //     if(sum-nums[i]>=-t)
+    //     a=next[sum-nums[i]+t];
+    //     int b=0;
+    //     if(sum+nums[i]<=t)
+    //     b=next[sum+nums[i]+t];
+    //     curr[sum+t]=a+b;
+    //         }
+    //         next=curr;
+    //     }
+    //     return curr[0+t];
+    // }
+    // int findTargetSumWays(vector<int>& nums, int target) {
+    //     return solve(nums,target);
+    // }
 
     //  int solve(vector<int>&nums,int target,int i,map<pair<int,int>,int>&dp){
     //     if(i==nums.size()){
@@ -101,4 +101,24 @@ public:
     // int findTargetSumWays(vector<int>& nums, int target) {
     //     return solve(nums,target);
     // }
+
+
+
+
+     int solve(vector<int>&nums,int target,int i,int t,vector<vector<int>>&dp){
+        if(i==nums.size()){
+            if(target==0)
+            return 1;
+            return 0;
+        }
+        if(dp[i][target+2000]!=-1) return dp[i][target+2000];
+        int a=solve(nums,target-nums[i],i+1,t,dp);
+        int b=solve(nums,target+nums[i],i+1,t,dp);
+        return dp[i][target+2000]=a+b;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int t=accumulate(nums.begin(),nums.end(),0);
+        vector<vector<int>>dp(nums.size()+1,vector<int>(4001,-1));
+        return solve(nums,target,0,t,dp);
+    }
 };
