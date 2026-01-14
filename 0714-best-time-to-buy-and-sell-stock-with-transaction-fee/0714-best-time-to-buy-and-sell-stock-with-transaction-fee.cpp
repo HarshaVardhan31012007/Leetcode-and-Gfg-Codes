a8e9b37cbb1bb2ea355d74a29d0843093a6dba40
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-14 17:59:45 +0530

    Time: 109 ms (29.83%), Space: 100.5 MB (11.34%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-    //   int solve(vector<int>&prices,int i,int flag,int fee,vector<vector<int>>&dp){
-    //     if(i==prices.size()) return 0;
-    //     if(dp[i][flag]!=-1) return dp[i][flag];
-    //     int profit=0;
-    //     if(flag)
-    //      profit=max(profit,-prices[i]+solve(prices,i+1,0,fee,dp));
-    //      else
-    //     profit=max(profit,prices[i]-fee+solve(prices,i+1,1,fee,dp));
-    //     profit=max(profit,solve(prices,i+1,flag,fee,dp));
-    //     return dp[i][flag]=profit;
-    //  }
-    //  int maxProfit(vector<int>& prices,int fee) {
-    //     vector<vector<int>>dp(prices.size()+1,vector<int>(2,-1));
-    //     return solve(prices,0,1,fee,dp);
-    //  }
-
-     int solve(vector<int>&prices,int fee){
-         vector<int>curr(2,0);
-         vector<int>next(2,0);
-         for(int i=prices.size()-1;i>=0;i--){
-            for(int flag=0;flag<=1;flag++){
-                int profit=0;
-                if(flag)
-                profit=max(profit,-prices[i]+next[0]);
-                else
-                profit=max(profit,prices[i]-fee+next[1]);
-                profit=max(profit,next[flag]);
-                curr[flag]=profit;
-            }
-            next=curr;
-         }
-         return curr[1];
-     }
-     int maxProfit(vector<int>& prices,int fee) {
-        return solve(prices,fee);
-     }
+    int solve(vector<int>&prices,int i,int flag,int fee,vector<vector<int>>&dp){
+        if(i>=prices.size()) return 0;
+        if(dp[i][flag]!=-1) return dp[i][flag];
+        int ans=0;
+        //buy
+        if(flag==0)
+        ans=max(ans,-prices[i]+solve(prices,i+1,1,fee,dp));
+        //sell
+        if(flag==1)
+        ans=max(ans,prices[i]-fee+solve(prices,i+1,0,fee,dp));
+        //skip
+        ans=max(ans,solve(prices,i+1,flag,fee,dp));
+        return dp[i][flag]=ans;
+    }
+    int maxProfit(vector<int>& prices,int fee) {
+        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
+        return solve(prices,0,0,fee,dp);
+    }
 };
