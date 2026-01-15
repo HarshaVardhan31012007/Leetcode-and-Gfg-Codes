7f100e24330ec89e9ff16b937f7706f79964c4e1
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-15 11:16:06 +0530

    Time: 71 ms (0.87%), Space: 33.3 MB (7.83%) - LeetHub

---
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        unordered_set<int>st1;unordered_set<int>st2;
+        for(auto &each:hBars){
+           st1.insert(each);
+        }
+        for(auto &each:vBars){
+           st2.insert(each);
+        }
+        int ans=0;
+        for(int i=0;i<hBars.size();i++){
+            for(int j=0;j<vBars.size();j++){
+                int x=hBars[i];
+                int y=vBars[j];
+                int up=x;
+                while(up>1){
+                    if(st1.count(up)){
+                        up=up-1;
+                    }
+                    else
+                    break;
+                }
+                int down=x;
+                while(down<n+2){
+                    if(st1.count(down)){
+                        down=down+1;
+                    }
+                    else
+                    break;
+                }
+                int vl=down-up;
+                int left=y;
+                while(left>1){
+                    if(st2.count(left))
+                    left=left-1;
+                    else
+                    break;
+                }
+                int right=y;
+                while(right<m+2){
+                    if(st2.count(right))
+                    right=right+1;
+                    else
+                    break;
+                }
+                int hl=right-left;
+                int l=min(hl,vl);
+                ans=max(ans,l*l);
+            }
+        }
+        return ans;
+    }
+};
