93e7bd59d0a9e38ed98650ed9f0edb2ea8506539
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-15 16:06:55 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&dp){
-        if(n==0) return 1;
-        if(n<0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
-    }
+    int dp[46]={0};
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        if(n==1||n==0) return 1;
+        if(dp[n]!=0) return dp[n];
+        return dp[n]=climbStairs(n-1)+climbStairs(n-2);
     }
 };
