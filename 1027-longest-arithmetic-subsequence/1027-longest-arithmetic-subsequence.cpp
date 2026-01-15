cfa47a830690140ca56e3a8b86f99815a5416fc8
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-15 17:26:20 +0530

    Time: 1140 ms (48.68%), Space: 248.2 MB (21.35%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -5,7 +5,7 @@ public:
         int ans=0;
         for(int i=0;i<nums.size();i++){
             for(int j=0;j<i;j++){
-                int diff=nums[j]-nums[i];
+                int diff=nums[i]-nums[j];
                 dp[i][diff]=((dp[j].count(diff))?dp[j][diff]+1:2);
                 ans=max(ans,dp[i][diff]);
             }
