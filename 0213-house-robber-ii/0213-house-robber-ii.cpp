48ecd8fa7e4b4836bd1a97f8a93706719871e99d
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-16 10:33:08 +0530

    Time: 0 ms (100%), Space: 10.9 MB (37.55%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
-        int findmax(vector<int>&nums,int s,int e,vector<vector<int>>&dp){
-
-       //base case
-       if(s>e){
-         return 0;
-       }
-       if(dp[s][e]!=-1)
-       return dp[s][e];
-       //include
-       int includekaAns=nums[s]+findmax(nums,s+2,e,dp);
-       //exclude
-       int excludekaAns=0+findmax(nums,s+1,e,dp);
-       int finalAns=max(includekaAns,excludekaAns);
-       return dp[s][e]=finalAns;
-
+    int solve(vector<int>&nums,int i,int end,vector<int>&dp){
+        if(i>end) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int inc=nums[i]+solve(nums,i+2,end,dp);
+        int exc=solve(nums,i+1,end,dp);
+        return dp[i]=max(inc,exc);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n==1)
-        return nums[0];
-        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-        // as n-1,0 are adjacent we cannot rob both 
-        //one case we caanot n-1
-        //other case is we cananot rob 0
-        // int ans1=findmax(nums,0,n-2);
-        // int ans2=findmax(nums,1,n-1);
-      
-        // cases for in which we have to rob
-        int ans1=nums[0]+findmax(nums,2,n-2,dp);
-        int ans2=findmax(nums,1,n-1,dp);
-        return max(ans1,ans2);
+        vector<int>dp1(n+1,-1);
+        vector<int>dp2(n+1,-1);
+        return max(nums[0]+solve(nums,2,n-2,dp1),solve(nums,1,n-1,dp2));
     }
-    
 };
