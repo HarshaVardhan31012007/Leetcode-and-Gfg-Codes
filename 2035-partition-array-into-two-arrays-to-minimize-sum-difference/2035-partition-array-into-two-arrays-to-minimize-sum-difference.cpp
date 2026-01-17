28c5f9177979b8dc94b75667d6e14abcc6730268
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-17 22:44:30 +0530

    Time: 486 ms (21.81%), Space: 73.5 MB (34.86%) - LeetHub

---
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -0,0 +1,128 @@
+class Solution {
+public:
+    // int solve(vector<int>&nums,int i,int sum,int n,int total,vector<vector<unordered_map<int,int>>>&dp){
+    //     if(n==0){
+    //        int other=total-sum;
+    //        return abs(sum-other);
+    //     }
+    //     if(i>=nums.size()){
+    //         return 1e8;
+    //     }
+    //     if(dp[i][n].count(sum)) return dp[i][n][sum];
+    //     int inc=solve(nums,i+1,sum+nums[i],n-1,total,dp);
+    //     int exc=solve(nums,i+1,sum,n,total,dp);
+    //     return dp[i][n][sum]=min(inc,exc);
+    // }
+    // int minimumDifference(vector<int>& nums) {
+    //     int t=accumulate(nums.begin(),nums.end(),0);
+    //     vector<vector<unordered_map<int,int>>>dp(nums.size()+1,vector<unordered_map<int,int>>(nums.size()/2+1));
+    //     return solve(nums,0,0,nums.size()/2,t,dp);
+    // }
+
+//  int minimumDifference(vector<int>& arr) {
+//         int t=accumulate(arr.begin(),arr.end(),0);
+//     int n=arr.size();
+//         int mid=n/2;
+//         vector<int>left(arr.begin(),arr.begin()+mid);
+//         vector<int>right(arr.begin()+mid,arr.end());
+//         int m=left.size();n=right.size();
+//         vector<int>leftS,rightS;
+//         for(int mask=0;mask<(1<<m);mask++){
+//             int temp=mask;
+//             int i=0;int sum=0;
+//             while(temp){
+//                 if(temp&1){
+//                     sum+=left[i];
+//                 }
+//                 temp=temp>>1;i++;
+//             }
+//             leftS.push_back(sum);
+//         }
+//         for(int mask=0;mask<(1<<n);mask++){
+//             int temp=mask;
+//             int i=0;int sum=0;
+//             while(temp){
+//                 if(temp&1){
+//                     sum+=right[i];
+//                 }
+//                 temp=temp>>1;i++;
+//             }
+//             rightS.push_back(sum);
+//         }
+//         sort(rightS.begin(),rightS.end());
+//         int ans=0;
+//         for(auto &each:leftS){
+//             int need=k-each;
+//             int count=upper_bound(rightS.begin(),rightS.end(),need)-lower_bound(rightS.begin(),rightS.end(),need);
+//             ans+=count;
+//         }
+//         return ans;
+//}
+
+
+
+
+    int minimumDifference(vector<int>& nums) {
+        int n = nums.size();
+        int half = n / 2;
+
+        vector<int> A(nums.begin(), nums.begin() + half);
+        vector<int> B(nums.begin() + half, nums.end());
+
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        vector<vector<int>> left(half + 1), right(half + 1);
+
+        // Generate subset sums for A
+        for (int mask = 0; mask < (1 << half); mask++) {
+            int cnt = 0, s = 0;
+            for (int i = 0; i < half; i++) {
+                if (mask & (1 << i)) {
+                    cnt++;
+                    s += A[i];
+                }
+            }
+            left[cnt].push_back(s);
+        }
+
+        // Generate subset sums for B
+        int n2 = n - half;
+        for (int mask = 0; mask < (1 << n2); mask++) {
+            int cnt = 0, s = 0;
+            for (int i = 0; i < n2; i++) {
+                if (mask & (1 << i)) {
+                    cnt++;
+                    s += B[i];
+                }
+            }
+            right[cnt].push_back(s);
+        }
+
+        // Sort right subsets
+        for (int i = 0; i <= half; i++) {
+            sort(right[i].begin(), right[i].end());
+        }
+
+        int ans = INT_MAX;
+
+        // Combine
+        for (int k = 0; k <= half; k++) {
+            for (int s1 : left[k]) {
+                int need = half - k;
+                auto &vec = right[need];
+                int target = sum / 2 - s1;
+
+                auto it = lower_bound(vec.begin(), vec.end(), target);
+
+                if (it != vec.end()) {
+                    ans = min(ans, abs(sum - 2 * (s1 + *it)));
+                }
+                if (it != vec.begin()) {
+                    --it;
+                    ans = min(ans, abs(sum - 2 * (s1 + *it)));
+                }
+            }
+        }
+        return ans;
+    }
+};
