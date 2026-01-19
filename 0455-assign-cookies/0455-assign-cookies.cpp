9cb9c3ed7e084bff596593f60f7d4d1caa682c5b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-19 23:37:07 +0530

    Time: 8 ms (61.08%), Space: 44.8 MB (76.31%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
+    // int findContentChildren(vector<int>& g, vector<int>& s) {
+    //     multiset<int>st(s.begin(),s.end());
+    //     int ans=0;
+    //     for(auto &each:g){
+    //         auto it=st.lower_bound(each);
+    //         if(it!=st.end()){
+    //            ans++;
+    //            st.erase(it);
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        multiset<int>st(s.begin(),s.end());
-        // unordered_map<int,int>freq;
-        // for(auto &each:s)
-        // freq[each]++;
-        int ans=0;
-        for(auto &each:g){
-            auto it=st.lower_bound(each);
-            if(it!=st.end()){
+        sort(s.begin(),s.end());
+        sort(g.begin(),g.end());
+        int i=0,j=0;int ans=0;
+        while(j<s.size()&&i<g.size()){
+            if(g[i]<=s[j]){
                ans++;
-               st.erase(it);
+               i++;
             }
+            j++;
         }
         return ans;
     }
