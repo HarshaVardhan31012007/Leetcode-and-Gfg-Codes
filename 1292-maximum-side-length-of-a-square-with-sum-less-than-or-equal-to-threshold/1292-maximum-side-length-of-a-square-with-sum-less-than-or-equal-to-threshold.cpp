e6080dc394f344d043df9fa766baa59d9d819229
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-19 21:06:01 +0530

    Time: 178 ms (11.37%), Space: 32.8 MB (60.91%) - LeetHub

---
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    // int maxSideLength(vector<vector<int>>& mat, int threshold) {
+    //       int m=mat.size();
+    //       int n=mat[0].size();
+    //       vector<vector<int>>prefixr(m,vector<int>(n,0));
+    //       for(int i=0;i<m;i++){
+    //         int sum=0;
+    //         for(int j=0;j<n;j++){
+    //             sum+=mat[i][j];
+    //             prefixr[i][j]=sum;
+    //         }
+    //       }
+    //       int ans=0;
+    //       for(int i=0;i<m;i++){
+    //         for(int j=0;j<n;j++){
+    //             for(int k=min(m-i,n-j);k>=1;k--){
+    //                 int sum=0;
+    //                 for(int r=0;r<k;r++){
+    //                   sum+=prefixr[i+r][j+k-1]-(j>0?prefixr[i+r][j-1]:0);
+    //                 }
+    //                 if(sum<=threshold){
+    //                     ans=max(ans,k);
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //       }
+    //       return ans;
+    // }
+
+
+     int maxSideLength(vector<vector<int>>& mat, int threshold) {
+          int m=mat.size();
+          int n=mat[0].size();
+          vector<vector<int>>prefixr(m,vector<int>(n,0));
+          for(int i=0;i<m;i++){
+            int sum=0;
+            for(int j=0;j<n;j++){
+                sum+=mat[i][j];
+                prefixr[i][j]=sum;
+            }
+          }
+          int ans=0;
+          for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                    int s=1;int e=min(m-i,n-j);
+                    while(s<=e){
+                        int mid=s+(e-s)/2;
+                        int sum=0;
+                        for(int r=0;r<mid;r++){
+                        sum+=prefixr[i+r][j+mid-1]-(j>0?prefixr[i+r][j-1]:0);
+                        }
+                        if(sum<=threshold){
+                            ans=max(ans,mid);
+                            s=mid+1;
+                        }
+                        else{
+                            e=mid-1;
+                        }
+                    }
+            }
+          }
+          return ans;
+    }
+};
