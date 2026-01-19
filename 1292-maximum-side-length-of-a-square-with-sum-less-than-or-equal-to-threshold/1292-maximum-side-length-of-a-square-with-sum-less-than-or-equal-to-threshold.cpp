e81769b07d442ae6daa08af24c79aa46599a436c
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-19 21:35:33 +0530

    Time: 25 ms (31.36%), Space: 32.9 MB (36.82%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -66,6 +66,37 @@ public:
 
 
 
+    //  int maxSideLength(vector<vector<int>>& mat, int threshold) {
+    //       int m=mat.size();
+    //       int n=mat[0].size();
+    //       vector<vector<int>>prefixr(m,vector<int>(n,0));
+    //       for(int i=0;i<m;i++){
+    //         for(int j=0;j<n;j++){
+    //             prefixr[i][j]=mat[i][j]+(i>0?prefixr[i-1][j]:0)+(j>0?prefixr[i][j-1]:0)-(i>0&&j>0?prefixr[i-1][j-1]:0);
+    //         }
+    //       }
+    //       int ans=0;
+    //       for(int i=0;i<m;i++){
+    //         for(int j=0;j<n;j++){
+    //                 int s=1;int e=min(m-i,n-j);
+    //                 while(s<=e){
+    //                     int mid=s+(e-s)/2;
+    //                     int sum=prefixr[i+mid-1][j+mid-1]-(j>0?prefixr[i+mid-1][j-1]:0)-(i>0?prefixr[i-1][j+mid-1]:0)+(i>0&&j>0?prefixr[i-1][j-1]:0);
+    //                     if(sum<=threshold){
+    //                         ans=max(ans,mid);
+    //                         s=mid+1;
+    //                     }
+    //                     else{
+    //                         e=mid-1;
+    //                     }
+    //                 }
+    //         }
+    //       }
+    //       return ans;
+    // }
+
+
+
      int maxSideLength(vector<vector<int>>& mat, int threshold) {
           int m=mat.size();
           int n=mat[0].size();
@@ -75,23 +106,28 @@ public:
                 prefixr[i][j]=mat[i][j]+(i>0?prefixr[i-1][j]:0)+(j>0?prefixr[i][j-1]:0)-(i>0&&j>0?prefixr[i-1][j-1]:0);
             }
           }
-          int ans=0;
-          for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                    int s=1;int e=min(m-i,n-j);
-                    while(s<=e){
-                        int mid=s+(e-s)/2;
+            int ans=0;
+            int s=1;int e=min(m,n);
+            while(s<=e){
+                int mid=s+(e-s)/2;
+                bool flag=false;
+                for(int i=0;i+mid<=m;i++){
+                    for(int j=0;j+mid<=n;j++){
                         int sum=prefixr[i+mid-1][j+mid-1]-(j>0?prefixr[i+mid-1][j-1]:0)-(i>0?prefixr[i-1][j+mid-1]:0)+(i>0&&j>0?prefixr[i-1][j-1]:0);
                         if(sum<=threshold){
                             ans=max(ans,mid);
-                            s=mid+1;
-                        }
-                        else{
-                            e=mid-1;
+                            flag=true;
+                            break;
                         }
                     }
-            }
-          }
+                }
+                if(flag){
+                    ans=max(ans,mid);
+                    s=mid+1;
+                }
+                else
+                e=mid-1;
+            }        
           return ans;
     }
 };
