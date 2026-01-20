4d748845c69dda9755db1d7018c675bc3711ed30
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-20 17:50:40 +0530

    Time: 3 ms (83.5%), Space: 274.9 MB (77.36%) - LeetHub

--- 3809-best-reachable-tower/3809-best-reachable-tower.cpp
+++ 3809-best-reachable-tower/3809-best-reachable-tower.cpp
@@ -1,15 +1,44 @@
 class Solution {
 public:
-    vector<int> bestTower(vector<vector<int>>& towers, vector<int>& center, int radius) {
-         sort(towers.begin(),towers.end());
+    // vector<int> bestTower(vector<vector<int>>& towers, vector<int>& center, int radius) {
+    //      sort(towers.begin(),towers.end());
+    //      int ans=INT_MIN;
+    //      int ansx=-1;int ansy=-1;
+    //      for(int i=0;i<towers.size();i++){
+    //         int x=towers[i][0];int y=towers[i][1];
+    //         int dist=abs(x-center[0])+abs(y-center[1]);
+    //         if(dist<=radius&&towers[i][2]>ans){
+    //             ans=towers[i][2];
+    //             ansx=x;ansy=y;
+    //         }
+    //      }
+    //      return {ansx,ansy};
+    // }
+
+
+
+     vector<int> bestTower(vector<vector<int>>& towers, vector<int>& center, int radius) {
          int ans=INT_MIN;
          int ansx=-1;int ansy=-1;
          for(int i=0;i<towers.size();i++){
             int x=towers[i][0];int y=towers[i][1];
             int dist=abs(x-center[0])+abs(y-center[1]);
-            if(dist<=radius&&towers[i][2]>ans){
-                ans=towers[i][2];
-                ansx=x;ansy=y;
+            if(dist<=radius){
+                if(towers[i][2]>ans){
+                    ans=towers[i][2];
+                    ansx=x;ansy=y;
+                }
+                else if(towers[i][2]==ans){
+                    if(x<ansx){
+                        ansx=x;
+                        ansy=y;
+                    }
+                    else if(ansx==x){
+                        if(y<ansy){
+                            ansy=y;
+                        }
+                    }
+                }
             }
          }
          return {ansx,ansy};
