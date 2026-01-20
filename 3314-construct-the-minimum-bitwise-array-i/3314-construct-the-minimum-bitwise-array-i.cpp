c7703030b1cfe2a862c9ebfe98d050cf9680b468
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-20 13:38:32 +0530

    Time: 3 ms (37.87%), Space: 25 MB (92.31%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -5,7 +5,7 @@ public:
         vector<int>ans(n,-1);
         for(int i=0;i<n;i++){
             int mask=nums[i];
-            for(int sub=mask;sub>=1;sub=(sub-1)&mask){
+            for(int sub=mask;sub>=1;sub--){
                 if((sub|sub+1)==mask){
                     ans[i]=sub;
                 }
@@ -13,4 +13,18 @@ public:
         }
         return ans;
     }
+
+    // vector<int> minBitwiseArray(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int>ans(n,-1);
+    //     for(int i=0;i<n;i++){
+    //         int mask=nums[i];
+    //         for(int sub=mask;sub>=1;sub=(sub-1)&mask){
+    //             if((sub|sub+1)==mask){
+    //                 ans[i]=sub;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
