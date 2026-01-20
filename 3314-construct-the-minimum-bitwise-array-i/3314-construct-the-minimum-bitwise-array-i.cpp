d554a5863d2fe8e68809be0782c1668b149cb94d
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-20 13:43:13 +0530

    Time: 0 ms (100%), Space: 25.1 MB (78.7%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    vector<int> minBitwiseArray(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>ans(n,-1);
-        for(int i=0;i<n;i++){
-            int mask=nums[i];
-            for(int sub=mask;sub>=1;sub--){
-                if((sub|sub+1)==mask){
-                    ans[i]=sub;
-                }
-            }
-        }
-        return ans;
-    }
+    // vector<int> minBitwiseArray(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int>ans(n,-1);
+    //     for(int i=0;i<n;i++){
+    //         int mask=nums[i];
+    //         for(int sub=mask;sub>=1;sub--){
+    //             if((sub|sub+1)==mask){
+    //                 ans[i]=sub;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
 
     // vector<int> minBitwiseArray(vector<int>& nums) {
     //     int n=nums.size();
@@ -27,4 +27,24 @@ public:
     //     }
     //     return ans;
     // }
+
+
+
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(n,-1);
+        for(int i=0;i<n;i++){
+            int mask=nums[i];
+            if(!(mask&1)) continue;
+            int zero=-1;
+            while(mask&1){
+               zero=zero<<1;
+               mask=mask>>1;
+            }
+            zero=zero>>1;
+            int val=(nums[i]&zero);
+            ans[i]=val-1;
+        }
+        return ans;
+    }
 };
