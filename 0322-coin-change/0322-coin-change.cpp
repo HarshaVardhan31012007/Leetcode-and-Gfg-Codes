3c476e92f2755673abe6fb1c07aea07c1cfdf3bf
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-21 20:42:58 +0530

    Time: 56 ms (16.83%), Space: 53.9 MB (8.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -58,22 +58,43 @@ public:
     // }
 
 
-    int solve(vector<int>&coins,int amount,vector<int>&dp){
+    // int solve(vector<int>&coins,int amount,vector<int>&dp){
+    //     if(amount==0) return 0;
+    //     if(dp[amount]!=-1) return dp[amount];
+    //     int mini=INT_MAX;
+    //     for(auto &each:coins){
+    //         if(each<=amount){
+    //             int recans=solve(coins,amount-each,dp);
+    //             if(recans!=INT_MAX)
+    //             mini=min(mini,1+recans);
+    //         }
+    //     }
+    //     return dp[amount]=mini;
+    // }
+    // int coinChange(vector<int>& coins, int amount) {
+    //     vector<int>dp(amount+1,-1);
+    //     int mini=solve(coins,amount,dp);
+    //     return (mini==INT_MAX)?-1:mini;
+    // }
+
+
+
+    int solve(vector<int>&coins,int i,int amount,vector<vector<int>>&dp){
         if(amount==0) return 0;
-        if(dp[amount]!=-1) return dp[amount];
-        int mini=INT_MAX;
-        for(auto &each:coins){
-            if(each<=amount){
-                int recans=solve(coins,amount-each,dp);
-                if(recans!=INT_MAX)
-                mini=min(mini,1+recans);
-            }
+        if(i>=coins.size()) return INT_MAX;
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int inc=INT_MAX;
+        if(amount>=coins[i]){
+            int reccall=solve(coins,i,amount-coins[i],dp);
+            if(reccall!=INT_MAX)
+            inc=1+reccall;
         }
-        return dp[amount]=mini;
+        int exc=solve(coins,i+1,amount,dp);
+        return dp[i][amount]=min(inc,exc);
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        int mini=solve(coins,amount,dp);
+        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
+        int mini=solve(coins,0,amount,dp);
         return (mini==INT_MAX)?-1:mini;
     }
 };
