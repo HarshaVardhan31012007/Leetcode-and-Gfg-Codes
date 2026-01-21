e34f1a5ec843b7f0813134e28647a49f8234af3c
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-21 19:01:07 +0530

    Time: 0 ms (100%), Space: 28.4 MB (31.07%) - LeetHub

--- 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
+++ 3315-construct-the-minimum-bitwise-array-ii/3315-construct-the-minimum-bitwise-array-ii.cpp
@@ -30,20 +30,42 @@ public:
 
 
 
-    vector<int> minBitwiseArray(vector<int>& nums) {
+    // vector<int> minBitwiseArray(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int>ans(n,-1);
+    //     for(int i=0;i<n;i++){
+    //         int mask=nums[i];
+    //         if(!(mask&1)) continue;
+    //         int zero=-1;
+    //         while(mask&1){
+    //            zero=zero<<1;
+    //            mask=mask>>1;
+    //         }
+    //         zero=zero>>1;
+    //         int val=(nums[i]&zero);
+    //         ans[i]=val-1;
+    //     }
+    //     return ans;
+    // }
+
+
+     vector<int> minBitwiseArray(vector<int>& nums) {
         int n=nums.size();
-        vector<int>ans(n,-1);
+        vector<int>ans;
         for(int i=0;i<n;i++){
             int mask=nums[i];
-            if(!(mask&1)) continue;
-            int zero=-1;
+            if(!(mask&1)) {
+               ans.push_back(-1);
+               continue;
+            }
+            int mask1=1;
             while(mask&1){
-               zero=zero<<1;
+               mask1=mask1<<1;
                mask=mask>>1;
             }
-            zero=zero>>1;
-            int val=(nums[i]&zero);
-            ans[i]=val-1;
+            mask1=mask1>>1;
+            int val=(nums[i]&(~(mask1)));
+            ans.push_back(val);
         }
         return ans;
     }
