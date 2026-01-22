8e93caf351d3d325179013a3c988ac1b97422c67
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-22 09:31:07 +0530

    Time: 3 ms (40.33%), Space: 26.4 MB (95.94%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
     int minimumPairRemoval(vector<int>& nums) {
-        int ans=0;
+        // int ans=0;
+        // while(1){
+        //     int idx=-1;int sum=1e5;bool flag=1;
+        //     for(int i=0;i<(int)nums.size()-1;i++){
+        //         if(nums[i]+nums[i+1]<sum){
+        //             sum=nums[i]+nums[i+1];
+        //             idx=i;
+        //         }
+        //         if(nums[i+1]<nums[i])
+        //         flag=0;
+        //     }
+        //     if(flag)
+        //     return ans;
+        //     ans++;
+        //     vector<int>temp;
+        //     for(int i=0;i<nums.size();i++){
+        //         if(idx!=i)
+        //         temp.push_back(nums[i]);
+        //         else
+        //         temp.push_back(nums[i]+nums[++i]);
+        //     }
+        //     nums=temp;
+        // }
+        // return ans;
+
+
+
+         int ans=0;
         while(1){
             int idx=-1;int sum=1e5;bool flag=1;
             for(int i=0;i<(int)nums.size()-1;i++){
@@ -15,14 +42,8 @@ public:
             if(flag)
             return ans;
             ans++;
-            vector<int>temp;
-            for(int i=0;i<nums.size();i++){
-                if(idx!=i)
-                temp.push_back(nums[i]);
-                else
-                temp.push_back(nums[i]+nums[++i]);
-            }
-            nums=temp;
+            nums[idx+1]+=nums[idx];
+            nums.erase(nums.begin()+idx,nums.begin()+idx+1);
         }
         return ans;
     }
