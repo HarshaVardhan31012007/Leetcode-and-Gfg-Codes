50ac640b75860a7e164557f9c4f181250f1e6b8b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-22 16:19:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -1,28 +1,42 @@
 class Solution {
   public:
     int longestCommonSubstr(string& s1, string& s2) {
-        int n=s1.size();
-        int m=s2.size();
+        // int n=s1.size();
+        // int m=s2.size();
+        // int ans=0;
+        // for(int diff=-(n-1);diff<m;diff++){
+        //     int i,j;
+        //     if(diff>=0){
+        //         i=0;j=i+diff;
+        //     }
+        //     else{
+        //         j=0;i=j+abs(diff);
+        //     }
+        //     int curr=0;
+        //     for(;i<n&&j<m;i++,j++){
+        //         if(s1[i]==s2[j]){
+        //             curr++;
+        //         }
+        //         else{
+        //             ans=max(ans,curr);
+        //         }
+        //     }
+        //     ans=max(ans,curr);
+        // }
+        // return ans;
+        
+        
+        int m=s1.size();
+        int n=s2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
         int ans=0;
-        for(int diff=-(n-1);diff<m;diff++){
-            int i,j;
-            if(diff>=0){
-                i=0;j=i+diff;
-            }
-            else{
-                j=0;i=j+abs(diff);
-            }
-            int curr=0;
-            for(;i<n&&j<m;i++,j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(s1[i]==s2[j]){
-                    curr++;
+                    dp[i][j]=1+(i>0&&j>0?dp[i-1][j-1]:0);
                 }
-                else{
-                    ans=max(ans,curr);
-                    curr=0;
-                }
+                ans=max(ans,dp[i][j]);
             }
-            ans=max(ans,curr);
         }
         return ans;
     }
