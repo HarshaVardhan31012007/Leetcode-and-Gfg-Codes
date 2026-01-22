304efb6ff1ea54f18f7e3f06e0bbae96c3489ea6
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-22 09:26:57 +0530

    Time: 11 ms (11.71%), Space: 29.5 MB (14.96%) - LeetHub

---
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumPairRemoval(vector<int>& nums) {
+        int ans=0;
+        while(1){
+            int idx=-1;int sum=1e5;bool flag=1;
+            for(int i=0;i<(int)nums.size()-1;i++){
+                if(nums[i]+nums[i+1]<sum){
+                    sum=nums[i]+nums[i+1];
+                    idx=i;
+                }
+                if(nums[i+1]<nums[i])
+                flag=0;
+            }
+            if(flag)
+            return ans;
+            ans++;
+            vector<int>temp;
+            for(int i=0;i<nums.size();i++){
+                if(idx!=i)
+                temp.push_back(nums[i]);
+                else
+                temp.push_back(nums[i]+nums[++i]);
+            }
+            nums=temp;
+        }
+        return ans;
+    }
+};
