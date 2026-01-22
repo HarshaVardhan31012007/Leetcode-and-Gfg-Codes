f3177bd5d490f49eddecf3854edfc8c3563dcddf
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-22 09:31:52 +0530

    Time: 0 ms (100%), Space: 26.4 MB (81.3%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -28,6 +28,27 @@ public:
 
 
 
+        //  int ans=0;
+        // while(1){
+        //     int idx=-1;int sum=1e5;bool flag=1;
+        //     for(int i=0;i<(int)nums.size()-1;i++){
+        //         if(nums[i]+nums[i+1]<sum){
+        //             sum=nums[i]+nums[i+1];
+        //             idx=i;
+        //         }
+        //         if(nums[i+1]<nums[i])
+        //         flag=0;
+        //     }
+        //     if(flag)
+        //     return ans;
+        //     ans++;
+        //     nums[idx+1]+=nums[idx];
+        //     nums.erase(nums.begin()+idx,nums.begin()+idx+1);
+        // }
+        // return ans;
+
+
+
          int ans=0;
         while(1){
             int idx=-1;int sum=1e5;bool flag=1;
