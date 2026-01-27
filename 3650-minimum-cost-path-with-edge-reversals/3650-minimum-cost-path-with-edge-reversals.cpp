0112e278b673bf0da5accae4fd34093f9194c6fa
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-28 00:01:59 +0530

    Time: 447 ms (22.59%), Space: 377.3 MB (10.57%) - LeetHub

---
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+        unordered_map<int,list<pair<int,int>>>out;
+        unordered_map<int,list<pair<int,int>>>in;
+        for(auto &each:edges){
+            out[each[0]].push_back({each[1],each[2]});
+            in[each[1]].push_back({each[0],each[2]});
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        vector<int>cost(n,INT_MAX);
+        cost[0]=0;
+        pq.push({0,0});
+        while(!pq.empty()){
+            auto top=pq.top();
+            pq.pop();
+            if(top.second==(n-1)) return top.first;
+            for(auto &each:out[top.second]){
+                if(1LL*cost[top.second]+1LL*each.second<1LL*cost[each.first]){
+                    cost[each.first]=cost[top.second]+each.second;
+                    pq.push({cost[each.first],each.first});
+                }
+            }
+            for(auto &each:in[top.second]){
+                if(1LL*cost[top.second]+2LL*each.second<1LL*cost[each.first]){
+                    cost[each.first]=cost[top.second]+2*each.second;
+                    pq.push({cost[each.first],each.first});
+                }
+            }
+        }
+        return -1;
+    }
+};
