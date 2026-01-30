16a11caae8d3f109a0b6fc20285c98182a0581a8
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-30 16:27:41 +0530

    Time: 301 ms (28.71%), Space: 296.5 MB (12.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+   // int lengthOfLIS(vector<int>& nums) {
         // int ans=1;
         // vector<int>dp(nums.size(),1);
         // for(int i=0;i<nums.size();i++){
@@ -41,17 +41,76 @@ public:
         // cout<<endl;
         // return ans;
 
+        // vector<int>ans;
+        // for(int i=0;i<nums.size();i++){
+        //     if(ans.empty()||ans.back()<nums[i])
+        //     ans.push_back(nums[i]);
+        //     else{
+        //         auto it=lower_bound(ans.begin(),ans.end(),nums[i]);//it is lower bound not upperbound dry run this 4 10 4 3 8 9
+        //         if(it!=ans.end())
+        //         ans[it-ans.begin()]=nums[i];
+        //     }
+        // }
+        // return ans.size();
+   // }
 
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            if(ans.empty()||ans.back()<nums[i])
-            ans.push_back(nums[i]);
-            else{
-                auto it=lower_bound(ans.begin(),ans.end(),nums[i]);//it is lower bound not upperbound dry run this 4 10 4 3 8 9
-                if(it!=ans.end())
-                ans[it-ans.begin()]=nums[i];
-            }
+
+      //int findSubsequences(int n,vector<int>&nums){
+    //      vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+    //      for(int j=0;j<n+1;j++){
+    //         dp[n][j]=0;
+    //      }
+    //     for(int curr=n-1;curr>=0;curr--){
+    //       for(int prev=curr-1;prev>=-1;prev--){
+    //         int l=0;
+    //         if(prev==-1||nums[prev]<nums[curr])
+    //         l=1+dp[curr+1][curr+1];
+    //         int r=dp[curr+1][prev+1];
+    //         dp[curr][prev+1]=max(l,r);
+    //       }
+    //     }
+    //     return dp[0][0];
+    // }
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int n=nums.size();
+    //     return findSubsequences(n,nums);
+    // }
+
+
+
+    int findSubsequences(int n,vector<int>&nums){
+         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+         for(int j=0;j<n+1;j++){
+            dp[n][j]=0;
+         }
+        for(int curr=n-1;curr>=0;curr--){
+          for(int prev=curr-1;prev>=-1;prev--){
+            int l=0;
+            if(prev==-1||nums[prev]<nums[curr])
+            l=1+dp[curr+1][curr+1];
+            int r=dp[curr+1][prev+1];
+            dp[curr][prev+1]=max(l,r);
+          }
         }
-        return ans.size();
+        int i=0;
+        int prev=-1;
+        while(i<n){
+            int maxi=INT_MIN;int temp=prev;
+            if(dp[i+1][prev+1]>maxi){
+                maxi=dp[i+1][prev+1];
+            }
+            if((prev==-1||nums[prev]<nums[i])&&1+dp[i+1][i+1]>maxi){
+                maxi=dp[i+1][i+1];
+                prev=i;
+            }
+            if(temp!=prev)
+            cout<<nums[prev]<<endl;
+            i++;
+        }
+        return dp[0][0];
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        return findSubsequences(n,nums);
     }
 };
