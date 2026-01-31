7bae27491333967a754883b294d952c8da2e1d0b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-01-31 18:21:39 +0530

    Time: 0 ms (100%), Space: 19.8 MB (24.67%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        sort(letters.begin(),letters.end());
-        auto it=upper_bound(letters.begin(),letters.end(),target);
-        if(it==letters.end()) return letters[0];
-        return *it;
+        // sort(letters.begin(),letters.end());
+        // auto it=upper_bound(letters.begin(),letters.end(),target);
+        // if(it==letters.end()) return letters[0];
+        // return *it;
+
+
+        int temp=123;
+        for(int i=0;i<letters.size();i++){
+            if(letters[i]>target&&letters[i]<temp){
+                 temp=letters[i];
+            }
+        }
+        return temp==123?letters[0]:temp;
     }
 };
