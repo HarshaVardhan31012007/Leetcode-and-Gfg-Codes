5323a2ac18427f66f3bd8844e6d45485361d4b6e
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-06 22:27:57 +0530

    Time: 67 ms (6.1%), Space: 105.1 MB (52.91%) - LeetHub

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        //its wrong //see greedy wrong//[8,99,65,16,39]
+        // sort(nums.begin(),nums.end());
+        // int n=nums.size();
+        // int i=0;
+        // int j=n-1;
+        // while(j-i+1>2){
+        //     if(nums[i]*k>=nums[j]){
+        //         return n-(j-i+1);
+        //     }
+        //     int diff=(nums[i+1]*k-nums[j]);
+        //     if(diff<(nums[i]*k-nums[j-1])){
+        //         j--;
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+        // if((j-i+1)==2){
+        //     if(nums[i]*k>=nums[j]){
+        //         return n-(j-i+1);
+        //     }
+        // }
+        // return n-1;
+
+
+
+
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int ans=n-1;
+        for(int i=0;i<n;i++){
+            auto it=upper_bound(nums.begin()+i+1,nums.end(),1LL*nums[i]*k);
+            if(it!=nums.begin()){
+                it--;
+                int size=(it-(nums.begin()+i))+1;
+                ans=min(ans,n-size);
+            }
+        }
+        return ans;
+    }
+};
