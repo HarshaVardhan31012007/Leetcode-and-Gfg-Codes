d3abbf256db97bbadfadfb9943036f5106da537a
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-08 00:30:01 +0530

    Time: 52 ms (48.21%), Space: 182.3 MB (10.73%) - LeetHub

--- 3829-design-ride-sharing-system/3829-design-ride-sharing-system.cpp
+++ 3829-design-ride-sharing-system/3829-design-ride-sharing-system.cpp
@@ -1,41 +1,88 @@
+// class RideSharingSystem {
+// public:
+//     set<pair<int,int>>rider;
+//     set<pair<int,int>>driver;
+//     unordered_map<int,int>mpp;
+//     int time;
+//     RideSharingSystem() {
+//         time=0;
+//     }
+    
+//     void addRider(int riderId) {
+//         rider.insert({time,riderId});
+//         mpp[riderId]=time;
+//         time++;
+//     }
+    
+//     void addDriver(int driverId) {
+//         driver.insert({time,driverId});
+//         time++;
+//     }
+    
+//     vector<int> matchDriverWithRider() {
+//         if(!driver.empty()&&!rider.empty()){
+//             int d=(driver.begin()->second);
+//             int r=(rider.begin()->second);
+//             mpp.erase(rider.begin()->second);
+//             driver.erase(driver.begin());
+//             rider.erase(rider.begin());
+//             return {d,r};
+//         }
+//         return {-1,-1};
+//     }
+    
+//     void cancelRider(int riderId) {
+//         if(mpp.count(riderId)){
+//             rider.erase({mpp[riderId],riderId});
+//             mpp.erase(riderId);
+//         }
+//     }
+// };
+
+
+
+
 class RideSharingSystem {
 public:
-    set<pair<int,int>>rider;
-    set<pair<int,int>>driver;
-    unordered_map<int,int>mpp;
-    int time;
+    queue<int>rider;
+    queue<int>driver;
     RideSharingSystem() {
-        time=0;
+        
     }
     
     void addRider(int riderId) {
-        rider.insert({time,riderId});
-        mpp[riderId]=time;
-        time++;
+      rider.push(riderId);
     }
     
     void addDriver(int driverId) {
-        driver.insert({time,driverId});
-        time++;
+       driver.push(driverId);
     }
     
     vector<int> matchDriverWithRider() {
-        if(!driver.empty()&&!rider.empty()){
-            int d=(driver.begin()->second);
-            int r=(rider.begin()->second);
-            mpp.erase(rider.begin()->second);
-            driver.erase(driver.begin());
-            rider.erase(rider.begin());
-            return {d,r};
-        }
-        return {-1,-1};
+       if(!rider.empty()&&!driver.empty()){
+          int a=rider.front();
+          int b=driver.front();
+          rider.pop();
+          driver.pop();
+          return {b,a};
+       }
+       return {-1,-1};
     }
     
     void cancelRider(int riderId) {
-        if(mpp.count(riderId)){
-            rider.erase({mpp[riderId],riderId});
-            mpp.erase(riderId);
-        }
+       queue<int>q2;
+       while(!rider.empty()){
+          if(rider.front()==riderId)
+          rider.pop();
+          else{
+            q2.push(rider.front());
+            rider.pop();
+          }
+       }
+       while(!q2.empty()){
+        rider.push(q2.front());
+        q2.pop();
+       }
     }
 };
 
