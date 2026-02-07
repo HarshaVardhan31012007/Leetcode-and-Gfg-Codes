cd9fdd840c5e532b4e17009ac7332539b080d4f8
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-08 00:25:16 +0530

    Time: 174 ms (5.18%), Space: 151.1 MB (23.04%) - LeetHub

---
+++ 3829-design-ride-sharing-system/3829-design-ride-sharing-system.cpp
@@ -0,0 +1,50 @@
+class RideSharingSystem {
+public:
+    set<pair<int,int>>rider;
+    set<pair<int,int>>driver;
+    unordered_map<int,int>mpp;
+    int time;
+    RideSharingSystem() {
+        time=0;
+    }
+    
+    void addRider(int riderId) {
+        rider.insert({time,riderId});
+        mpp[riderId]=time;
+        time++;
+    }
+    
+    void addDriver(int driverId) {
+        driver.insert({time,driverId});
+        time++;
+    }
+    
+    vector<int> matchDriverWithRider() {
+        if(!driver.empty()&&!rider.empty()){
+            int d=(driver.begin()->second);
+            int r=(rider.begin()->second);
+            mpp.erase(rider.begin()->second);
+            driver.erase(driver.begin());
+            rider.erase(rider.begin());
+            return {d,r};
+        }
+        return {-1,-1};
+    }
+    
+    void cancelRider(int riderId) {
+        cout<<mpp[riderId]<<endl;
+        if(mpp.count(riderId)){
+            rider.erase({mpp[riderId],riderId});
+            mpp.erase(riderId);
+        }
+    }
+};
+
+/**
+ * Your RideSharingSystem object will be instantiated and called as such:
+ * RideSharingSystem* obj = new RideSharingSystem();
+ * obj->addRider(riderId);
+ * obj->addDriver(driverId);
+ * vector<int> param_3 = obj->matchDriverWithRider();
+ * obj->cancelRider(riderId);
+ */
