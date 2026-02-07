c5a56cc60c44446d455f75b38e70b4b9ebe39503
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-07 23:30:56 +0530

    Time: 18 ms (75.74%), Space: 40.5 MB (29.59%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
+        // int n=s.length();
+        // vector<int>b_count(n+1,0);
+        // for(int i=n-1;i>=0;i--){
+        //     b_count[i]=b_count[i+1]+(s[i]=='b'?1:0);
+        // }
+        // int a_count=0;
+        // int seq=INT_MIN;
+        // for(int i=0;i<n;i++){
+        //     a_count+=(s[i]=='a'?1:0);
+        //     seq=max(seq,a_count+b_count[i]);
+        // }
+        // return n-seq;
+
+
+
         int n=s.length();
-        vector<int>b_count(n+1,0);
+        vector<int>a_count(n+1,0);
         for(int i=n-1;i>=0;i--){
-            b_count[i]=b_count[i+1]+(s[i]=='b'?1:0);
+            a_count[i]=a_count[i+1]+(s[i]=='a'?1:0);
         }
-        int a_count=0;
-        int seq=INT_MIN;
+        int b_count=0;
+        int del=INT_MAX;
         for(int i=0;i<n;i++){
-            a_count+=(s[i]=='a'?1:0);
-            seq=max(seq,a_count+b_count[i]);
+            b_count+=(s[i]=='b'?1:0);
+            del=min(del,a_count[i]+b_count-1);
         }
-        return n-seq;
+        return del;
     }
 };
