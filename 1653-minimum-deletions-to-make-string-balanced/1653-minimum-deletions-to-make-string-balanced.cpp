349662540ca71d62c62f796ae70a9a5359cdf162
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-07 22:35:15 +0530

    Time: 43 ms (27.96%), Space: 40.5 MB (27.66%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n=s.length();
+        vector<int>b_count(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            b_count[i]=b_count[i+1]+(s[i]=='b'?1:0);
+        }
+        int a_count=0;
+        int seq=INT_MIN;
+        for(int i=0;i<n;i++){
+            a_count+=(s[i]=='a'?1:0);
+            seq=max(seq,a_count+b_count[i]);
+        }
+        return n-seq;
+    }
+};
