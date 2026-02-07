427b1e925570d8ac3f1f00635530ce5226ad549e
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-07 12:51:51 +0530

    Time: 466 ms (22.06%), Space: 184.1 MB (6.45%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -55,6 +55,94 @@ public:
 
 
 
+    //  long long minimumCost(vector<int>& nums, int k, int dist) {
+    //     int i=1;int j=i+dist;int n=nums.size();
+    //     long long int sum=0;
+    //     long long int ans=LLONG_MAX;
+    //     set<pair<long long int,int>>st;
+    //     set<pair<long long int,int>>st1;
+    //     for(int p=i;p<=j;p++){
+    //         if(st.size()<k-1){
+    //             st.insert({nums[p],p});
+    //             sum+=nums[p];
+    //         }
+    //         else{
+    //             auto it=st.end();
+    //             it--;
+    //             if(it->first<=nums[p]){
+    //                 st1.insert({nums[p],p});
+    //             }
+    //             else{
+    //                 st.insert({nums[p],p});
+    //                 sum+=nums[p];
+    //                 it=st.end();
+    //                 it--;
+    //                 sum-=it->first;
+    //                 st1.insert({it->first,it->second});
+    //                 st.erase(it);
+    //             }
+    //         }
+    //     }
+    //     ans=min(ans,nums[0]+sum);
+    //     cout<<sum<<endl;
+    //     if(st.count({nums[i],i})){
+    //         st.erase({nums[i],i});
+    //         sum-=nums[i];
+    //         if(!st1.empty()){
+    //             auto it=st1.begin();
+    //             st.insert({it->first,it->second});
+    //             sum+=it->first;
+    //             st1.erase(it);
+    //         }
+    //     }
+    //     else{
+    //         st1.erase({nums[i],i});
+    //     }
+    //     i++;j++;
+    //     while(j<n){
+    //         if(st.size()<k-1){
+    //            st.insert({nums[j],j});
+    //            sum+=nums[j];
+    //         }
+    //         else{
+    //             auto it=st.end();
+    //             it--;
+    //             if(it->first<=nums[j]){
+    //                 st1.insert({nums[j],j});
+    //             }
+    //             else{
+    //                 st.insert({nums[j],j});
+    //                 sum+=nums[j];
+    //                 it=st.end();
+    //                 it--;
+    //                 sum-=it->first;
+    //                 st1.insert({it->first,it->second});
+    //                 st.erase(it);
+    //             }
+    //         }
+    //         ans=min(ans,1LL*nums[0]+sum);
+    //         if(st.count({nums[i],i})){
+    //             st.erase({nums[i],i});
+    //             sum-=nums[i];
+    //             if(!st1.empty()){
+    //                 auto it=st1.begin();
+    //                 st.insert({it->first,it->second});
+    //                 sum+=it->first;
+    //                 st1.erase(it);
+    //             }
+    //         }
+    //         else{
+    //             st1.erase({nums[i],i});
+    //         }
+    //         i++;
+    //         j++;
+    //     }
+    //     return ans;
+    // }
+
+
+
+
      long long minimumCost(vector<int>& nums, int k, int dist) {
         int i=1;int j=i+dist;int n=nums.size();
         long long int sum=0;
@@ -62,29 +150,17 @@ public:
         set<pair<long long int,int>>st;
         set<pair<long long int,int>>st1;
         for(int p=i;p<=j;p++){
-            if(st.size()<k-1){
-                st.insert({nums[p],p});
-                sum+=nums[p];
-            }
-            else{
+            st.insert({nums[p],p});
+            sum+=nums[p];
+            if(st.size()>k-1){
                 auto it=st.end();
                 it--;
-                if(it->first<=nums[p]){
-                    st1.insert({nums[p],p});
-                }
-                else{
-                    st.insert({nums[p],p});
-                    sum+=nums[p];
-                    it=st.end();
-                    it--;
-                    sum-=it->first;
-                    st1.insert({it->first,it->second});
-                    st.erase(it);
-                }
+                sum-=it->first;
+                st1.insert({it->first,it->second});
+                st.erase(it);
             }
         }
         ans=min(ans,nums[0]+sum);
-        cout<<sum<<endl;
         if(st.count({nums[i],i})){
             st.erase({nums[i],i});
             sum-=nums[i];
@@ -100,25 +176,14 @@ public:
         }
         i++;j++;
         while(j<n){
-            if(st.size()<k-1){
-               st.insert({nums[j],j});
-               sum+=nums[j];
-            }
-            else{
+            st.insert({nums[j],j});
+            sum+=nums[j];
+            if(st.size()>k-1){
                 auto it=st.end();
                 it--;
-                if(it->first<=nums[j]){
-                    st1.insert({nums[j],j});
-                }
-                else{
-                    st.insert({nums[j],j});
-                    sum+=nums[j];
-                    it=st.end();
-                    it--;
-                    sum-=it->first;
-                    st1.insert({it->first,it->second});
-                    st.erase(it);
-                }
+                sum-=it->first;
+                st1.insert({it->first,it->second});
+                st.erase(it);
             }
             ans=min(ans,1LL*nums[0]+sum);
             if(st.count({nums[i],i})){
