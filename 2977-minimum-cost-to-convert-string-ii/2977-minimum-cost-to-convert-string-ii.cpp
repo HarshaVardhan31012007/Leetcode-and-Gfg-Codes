01bb1a4d7b89b6ab492aa8c7dcc1d1f9ac61c530
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-08 12:39:45 +0530

    Time: 397 ms (60.33%), Space: 146.1 MB (55.94%) - LeetHub

--- 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.cpp
+++ 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.cpp
@@ -1,88 +1,172 @@
-struct Trie {
-    Trie* child[26];
-    int id;
-
-    Trie() {
-        for (int i = 0; i < 26; ++i) {
-            child[i] = nullptr;
-        }
-        id = -1;
-    }
-};
-
-int add(Trie* node, const string& word, int& index) {
-    for (char ch : word) {
-        int i = ch - 'a';
-        if (!node->child[i]) {
-            node->child[i] = new Trie();
-        }
-        node = node->child[i];
-    }
-    if (node->id == -1) {
-        node->id = ++index;
-    }
-    return node->id;
-}
-
-void update(long long& x, long long y) {
-    if (x == -1 || y < x) {
-        x = y;
-    }
-}
-
 class Solution {
 public:
-    long long minimumCost(string source, string target,
-                          vector<string>& original, vector<string>& changed,
-                          vector<int>& cost) {
-        int n = source.size();
-        int m = original.size();
-        Trie* root = new Trie();
+    // long long minimumCost(string source, string target,
+    //                       vector<string>& original,
+    //                       vector<string>& changed,
+    //                       vector<int>& cost) {
 
-        int p = -1;
-        vector<vector<int>> G(m * 2, vector<int>(m * 2, inf));
-        for (int i = 0; i < m * 2; ++i) {
-            G[i][i] = 0;
-        }
-        for (int i = 0; i < m; ++i) {
-            int x = add(root, original[i], p);
-            int y = add(root, changed[i], p);
-            G[x][y] = min(G[x][y], cost[i]);
-        }
+    //     unordered_map<string, unordered_map<string, int>> mpp;
+    //     unordered_set<string> st;
 
-        for (int k = 0; k <= p; ++k) {
-            for (int i = 0; i <= p; ++i) {
-                for (int j = 0; j <= p; ++j) {
-                    G[i][j] = min(G[i][j], G[i][k] + G[k][j]);
-                }
-            }
-        }
+    //     for (int i = 0; i < original.size(); i++) {
+    //         if (mpp.count(original[i]) &&
+    //             mpp[original[i]].count(changed[i])) {
 
-        vector<long long> f(n, -1);
-        for (int j = 0; j < n; ++j) {
-            if (j > 0 && f[j - 1] == -1) {
-                continue;
-            }
-            long long base = (j == 0 ? 0 : f[j - 1]);
-            if (source[j] == target[j]) {
-                update(f[j], base);
-            }
-            Trie* u = root;
-            Trie* v = root;
-            for (int i = j; i < n; ++i) {
-                u = u->child[source[i] - 'a'];
-                v = v->child[target[i] - 'a'];
-                if (!u || !v) {
-                    break;
-                }
-                if (u->id != -1 && v->id != -1 && G[u->id][v->id] != inf) {
-                    update(f[i], base + G[u->id][v->id]);
-                }
-            }
-        }
-        return f[n - 1];
+    //             mpp[original[i]][changed[i]] =
+    //                 min(mpp[original[i]][changed[i]], cost[i]);
+    //         } else {
+    //             mpp[original[i]][changed[i]] = cost[i];
+    //         }
+
+    //         mpp[original[i]][original[i]] = 0;
+    //         mpp[changed[i]][changed[i]] = 0;
+
+    //         st.insert(original[i]);
+    //         st.insert(changed[i]);
+    //     }
+
+    //     for (auto it = st.begin(); it != st.end(); it++) {
+    //         for (auto it1 = st.begin(); it1 != st.end(); it1++) {
+    //             for (auto it2 = st.begin(); it2 != st.end(); it2++) {
+
+    //                 if (mpp.count(*it1) &&
+    //                     mpp.count(*it) &&
+    //                     mpp[*it1].count(*it) &&
+    //                     mpp[*it].count(*it2)) {
+
+    //                     if (mpp[*it1].count(*it2)) {
+    //                         mpp[*it1][*it2] = min(
+    //                             mpp[*it1][*it2],
+    //                             mpp[*it1][*it] + mpp[*it][*it2]
+    //                         );
+    //                     } else {
+    //                         mpp[*it1][*it2] =
+    //                             mpp[*it1][*it] + mpp[*it][*it2];
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     int n = source.length();
+    //     vector<int> dp(n + 1, INT_MAX);
+
+    //     for (int i = 0; i < n; i++) {
+    //         int j = i;
+    //         string prev1 = "";
+    //         string prev2 = "";
+
+    //         while (j >= 0) {
+    //             string s = source[j] + prev1;
+    //             string t = target[j] + prev2;
+
+    //             if ((j == 0 || dp[j - 1] != INT_MAX) &&
+    //                 mpp.count(s) &&
+    //                 mpp[s].count(t)) {
+
+    //                 if (j > 0) {
+    //                     dp[i] = min(dp[i],
+    //                                 mpp[s][t] + dp[j - 1]);
+    //                 } else {
+    //                     dp[i] = mpp[s][t];
+    //                 }
+    //             }
+
+    //             if (s == t) {
+    //                 if (j > 0) {
+    //                     dp[i] = min(dp[i], dp[j - 1]);
+    //                 } else {
+    //                     dp[i] = 0;
+    //                 }
+    //             }
+
+    //             j--;
+
+    //             prev1 = s;
+    //             prev2 = t;
+    //         }
+    //     }
+
+    //     return dp[n - 1] == INT_MAX ? -1 : dp[n - 1];
+    // }
+
+
+
+   long long minimumCost(string source, string target,
+                      vector<string>& original,
+                      vector<string>& changed,
+                      vector<int>& cost) {
+
+    const long long INF = 1e18;
+
+    // ---- map strings to ids ----
+    unordered_map<string,int> id;
+    int idx = 0;
+    for(int i=0;i<original.size();i++){
+        if(!id.count(original[i])) id[original[i]] = idx++;
+        if(!id.count(changed[i]))  id[changed[i]]  = idx++;
     }
 
-private:
-    static constexpr int inf = INT_MAX / 2;
+    int m = idx;
+
+    // ---- distance matrix ----
+    vector<vector<long long>> dist(m, vector<long long>(m, INF));
+    for(int i=0;i<m;i++) dist[i][i]=0;
+
+    for(int i=0;i<original.size();i++){
+        int u = id[original[i]];
+        int v = id[changed[i]];
+        dist[u][v] = min(dist[u][v], (long long)cost[i]);
+    }
+
+    // ---- Floyd Warshall ----
+    for(int k=0;k<m;k++)
+        for(int i=0;i<m;i++)
+            for(int j=0;j<m;j++)
+                if(dist[i][k]<INF && dist[k][j]<INF)
+                    dist[i][j] = min(dist[i][j],
+                                     dist[i][k]+dist[k][j]);
+
+    // ---- collect rule lengths ----
+    unordered_set<int> lens;
+    for(auto &s: original) lens.insert(s.size());
+
+    int n = source.size();
+    vector<long long> dp(n+1, INF);
+    dp[0] = 0;
+
+    // ---- prefix DP ----
+    for(int i=1;i<=n;i++){
+
+        // case: keep single char unchanged
+        if(source[i-1]==target[i-1] && dp[i-1]<INF)
+            dp[i] = dp[i-1];
+
+        // try rule lengths
+        for(int len: lens){
+            if(len > i) continue;
+
+            int j = i-len;
+            if(dp[j]==INF) continue;
+
+            string s1 = source.substr(j,len);
+            string t1 = target.substr(j,len);
+
+            if(s1 == t1){
+                dp[i] = min(dp[i], dp[j]);
+                continue;
+            }
+
+            if(id.count(s1) && id.count(t1)){
+                long long c = dist[id[s1]][id[t1]];
+                if(c < INF)
+                    dp[i] = min(dp[i], dp[j] + c);
+            }
+        }
+    }
+
+    return dp[n]==INF ? -1 : dp[n];
+}
+
+
 };
