91caf03ff73f073d82e08c1ce15064edab74e376
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-12 18:44:58 +0530

    Time: 43 ms (96.75%), Space: 11.4 MB (94.41%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     int longestBalanced(string s) {
+        // int n=s.length();
+        // int ans=0;
+        // for(int i=0;i<s.length();i++){
+        //     int freq[26]={0};
+        //     for(int j=i;j<s.length();j++){
+        //         freq[s[j]-'a']++;
+        //         int mini=INT_MAX;int maxi=INT_MIN;
+        //         for(int k=0;k<26;k++){
+        //             if(freq[k]==0) continue;
+        //             maxi=max(maxi,freq[k]);
+        //             mini=min(mini,freq[k]);
+        //         }
+        //         if(mini==maxi){
+        //                 ans=max(ans,j-i+1);
+        //         }
+        //     }
+        // }
+        // return ans;
+
+
+
         int n=s.length();
         int ans=0;
         for(int i=0;i<s.length();i++){
-            int freq[26]={0};
+            int freq[26]={0};int dist=0;int maxi=INT_MIN;
             for(int j=i;j<s.length();j++){
+                if(freq[s[j]-'a']==0) dist++;
                 freq[s[j]-'a']++;
-                int mini=INT_MAX;int maxi=INT_MIN;
-                for(int k=0;k<26;k++){
-                    if(freq[k]==0) continue;
-                    maxi=max(maxi,freq[k]);
-                    mini=min(mini,freq[k]);
-                }
-                if(mini==maxi){
-                        ans=max(ans,j-i+1);
-                }
+                maxi=max(maxi,freq[s[j]-'a']);
+                int len=j-i+1;
+                if(len==maxi*dist)
+                ans=max(ans,j-i+1);
             }
         }
         return ans;
