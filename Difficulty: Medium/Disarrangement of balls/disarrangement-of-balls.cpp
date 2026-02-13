6f41ced291d384228aef110d878fabdc0a870997
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-14 02:04:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Disarrangement of balls/disarrangement-of-balls.cpp
+++ Difficulty: Medium/Disarrangement of balls/disarrangement-of-balls.cpp
@@ -2,14 +2,11 @@
 
 class Solution {
   public:
-    int solve(int n,vector<int>&dp){
-        if(n==1||n==2)
-        return n-1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=(n-1)*(solve(n-1,dp)+solve(n-2,dp));
-    }
+    int arr[13]={0};
     int countDer(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        if(n==1) return 0;
+        if(n==2) return 1;
+        if(arr[n]!=0) return arr[n];
+        return arr[n]=(n-1)*(countDer(n-1)+countDer(n-2));
     }
 };
