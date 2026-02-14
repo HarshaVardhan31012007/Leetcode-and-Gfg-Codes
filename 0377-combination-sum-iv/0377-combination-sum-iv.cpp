17f0b2a479f61e8bf97eb9e1c269a6d62f044f45
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-14 13:05:27 +0530

    Time: 3 ms (38.52%), Space: 9 MB (98.05%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,34 +1,39 @@
+// class Solution {
+// public:
+//     int solve(vector<int>&nums,int i,int target,vector<vector<int>>&dp){
+//         if(target==0) return 1;
+//         if(i>=nums.size()) return 0;
+//         if(dp[i][target]!=-1) return dp[i][target];
+//         int inc=0;
+//         if(target>=nums[i]){
+//            inc=solve(nums,i,target-nums[i],dp);
+//         }
+//         int exc=solve(nums,i+1,target,dp);
+//         return dp[i][target]=inc+exc;
+//     }
+//     int combinationSum4(vector<int>& nums, int target) {
+//         vector<vector<int>>dp(nums.size()+1,vector<int>(target+1,-1));
+//         return solve(nums,0,target,dp);
+//     }
+// };
+
+
+
 class Solution {
 public:
-    // int solve(vector<int>&nums,int target,vector<int>&dp){
-    //     if(target==0) return 1;
-    //     if(target<0) return 0;
-    //     if(dp[target]!=-1) return dp[target];
-    //     int ans=0;
-    //     for(auto &num:nums){
-    //       ans=ans+solve(nums,target-num,dp);
-    //     }
-    //     return dp[target]=ans;
-    // }
-    // int combinationSum4(vector<int>& nums, int target) {
-    //     vector<int>dp(target+1,-1);
-    //     return solve(nums,target,dp);
-    // }
-
-    int solve(vector<int>&nums,int t,vector<int>&dp){
-        for(int target=1;target<=t;target++){
-         long ans=0;
-        for(auto &num:nums){
-          if(num<=target)
-          ans=ans+dp[target-num];
+    int solve(vector<int>&nums,int target,vector<int>&dp){
+        if(target==0) return 1;
+        if(dp[target]!=-1) return dp[target];
+        int ans=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]<=target){
+                ans+=solve(nums,target-nums[j],dp);
+            }
         }
-        dp[target]=ans;
-        }
-        return dp[t];
+        return dp[target]=ans;
     }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int>dp(target+1,0);
-        dp[0]=1;
+        vector<int>dp(target+1,-1);
         return solve(nums,target,dp);
     }
 };
