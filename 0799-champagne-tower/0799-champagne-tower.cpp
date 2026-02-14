f9a5437bec5aeaa4c92c8d645117065e2cdbe0c8
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-14 11:29:08 +0530

    Time: 6 ms (50.36%), Space: 9.4 MB (79.66%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        // long long n=(query_row*(query_row+1))/2;
+        // if((poured-n)<=0) return 0.0;
+        // long long n1=query_row+1;
+        int row=query_row;
+        vector<double>v(1);
+        v[0]=poured;
+        double ans=v[0];
+        for(int i=1;i<=row;i++){
+            v.push_back(0.0);
+            for(int j=i;j>=0;j--){
+                v[j]=(j!=0&&v[j-1]>=1?((v[j-1]-1)/2.0):0)+(j!=i&&v[j]>=1?((v[j]-1)/2.0):0);
+                if(j==query_glass&&i==row){
+                    ans=v[j];
+                    break;
+                }
+            }
+        }
+        return (ans>=1)?1.0:ans;
+    }
+};
