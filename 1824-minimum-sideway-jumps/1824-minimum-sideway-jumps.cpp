7a7743bfe95715dda2468c266c63e66a38596762
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-14 12:35:06 +0530

    Time: 416 ms (15.86%), Space: 387.1 MB (11.57%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,3 +1,31 @@
+// class Solution {
+// public:
+//     int solve(vector<int>&obstacles,int i,int lane,vector<vector<int>>&dp){
+//         if(i==obstacles.size()-1) return 0;
+//         if(dp[i][lane]!=-1) return dp[i][lane];
+//         int ans=1e9;
+//         if(obstacles[i+1]!=lane)
+//         ans=min(ans,solve(obstacles,i+1,lane,dp));
+//         if(lane!=1&&obstacles[i]!=1&&obstacles[i+1]!=1){
+//             ans=min(ans,1+solve(obstacles,i+1,1,dp));
+//         }
+//         if(lane!=2&&obstacles[i]!=2&&obstacles[i+1]!=2){
+//             ans=min(ans,1+solve(obstacles,i+1,2,dp));
+//         }
+//         if(lane!=3&&obstacles[i]!=3&&obstacles[i+1]!=3){
+//             ans=min(ans,1+solve(obstacles,i+1,3,dp));
+//         }
+//         return dp[i][lane]=ans;
+//     }
+//     int minSideJumps(vector<int>& obstacles) {
+//         vector<vector<int>>dp(obstacles.size()+1,vector<int>(4,-1));
+//         return solve(obstacles,0,2,dp);
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int solve(vector<int>&obstacles,int i,int lane,vector<vector<int>>&dp){
@@ -6,14 +34,16 @@ public:
         int ans=1e9;
         if(obstacles[i+1]!=lane)
         ans=min(ans,solve(obstacles,i+1,lane,dp));
-        if(lane!=1&&obstacles[i]!=1&&obstacles[i+1]!=1){
-            ans=min(ans,1+solve(obstacles,i+1,1,dp));
-        }
-        if(lane!=2&&obstacles[i]!=2&&obstacles[i+1]!=2){
-            ans=min(ans,1+solve(obstacles,i+1,2,dp));
-        }
-        if(lane!=3&&obstacles[i]!=3&&obstacles[i+1]!=3){
-            ans=min(ans,1+solve(obstacles,i+1,3,dp));
+        else{
+            if(lane!=1&&obstacles[i]!=1){
+                ans=min(ans,1+solve(obstacles,i+1,1,dp));
+            }
+            if(lane!=2&&obstacles[i]!=2){
+                ans=min(ans,1+solve(obstacles,i+1,2,dp));
+            }
+            if(lane!=3&&obstacles[i]!=3){
+                ans=min(ans,1+solve(obstacles,i+1,3,dp));
+            }
         }
         return dp[i][lane]=ans;
     }
