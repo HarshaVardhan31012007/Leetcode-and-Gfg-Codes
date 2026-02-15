9d0a7c6a744d435254f5eb985e7a45292285cd2f
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-15 12:19:40 +0530

    Time: 23 ms (87.5%), Space: 10.5 MB (65.11%) - LeetHub

--- 3844-longest-almost-palindromic-substring/3844-longest-almost-palindromic-substring.cpp
+++ 3844-longest-almost-palindromic-substring/3844-longest-almost-palindromic-substring.cpp
@@ -62,68 +62,102 @@ public:
 
 
     
-    int almostPalindromic(string s) {
-       int n=s.length();
-       static int dp[2501][2501][3];
-       for(int i=0;i<=n;i++){
-           for(int flag=0;flag<=2;flag++){
-               if(flag==0)
-                dp[i][i][0]=1;
-                else
-                dp[i][i][flag]=0;
-           }
-       }
-       for(int i=0;i<=n;i++){
-           for(int j=i-1;j>=0;j--){
-            dp[i][j][0]=0;
-           }
-       }
-       for(int i=n-1;i>=0;i--){
-           for(int j=i+1;j<n;j++){
-               for(int flag=0;flag<=2;flag++){
-                               if(flag==2){
-                        int curr=INT_MIN;
-                        int ans=INT_MIN;
-                        if(s[i]==s[j]){
-                            int t=2+dp[i+1][j-1][1];
-                            if(t>=0&&t>curr){
-                                ans=(j-i+1);curr=t;
-                            }
-                        }
-                            int t=dp[i][j][1];
-                            if(t>=0&&t>curr){
-                                ans=(j-i+1);curr=t;
-                            }
-                        int t1=dp[i][j-1][2];
-                        if(t1>ans){
-                            ans=t1;
-                        }
-                        int t2=dp[i+1][j][2];
-                        if(t2>ans){
-                            ans=t2;
-                        }
-                        dp[i][j][flag]=ans;
-                    }
-                    else if(flag==0){
-                        if(s[i]==s[j])
-                            dp[i][j][flag]=2+dp[i+1][j-1][0];
-                        else
-                             dp[i][j][flag]=INT_MIN;
-                    }
-                    else{
-                        int curr=INT_MIN;
-                        if(s[i]==s[j]){
-                            curr=max(curr,2+dp[i+1][j-1][1]);
-                        }
-                        if(flag){
-                             curr=max(curr,dp[i+1][j][0]);
-                             curr=max(curr,dp[i][j-1][0]);
-                        }
-                        dp[i][j][flag]=curr;
-                    }
-               }
-           }
-       }
-        return dp[0][n-1][2];
+    // int almostPalindromic(string s) {
+    //    int n=s.length();
+    //    static int dp[2501][2501][3];
+    //    for(int i=0;i<=n;i++){
+    //        for(int flag=0;flag<=2;flag++){
+    //            if(flag==0)
+    //             dp[i][i][0]=1;
+    //             else
+    //             dp[i][i][flag]=0;
+    //        }
+    //    }
+    //    for(int i=0;i<=n;i++){
+    //        for(int j=i-1;j>=0;j--){
+    //         dp[i][j][0]=0;
+    //        }
+    //    }
+    //    for(int i=n-1;i>=0;i--){
+    //        for(int j=i+1;j<n;j++){
+    //            for(int flag=0;flag<=2;flag++){
+    //                            if(flag==2){
+    //                     int curr=INT_MIN;
+    //                     int ans=INT_MIN;
+    //                     if(s[i]==s[j]){
+    //                         int t=2+dp[i+1][j-1][1];
+    //                         if(t>=0&&t>curr){
+    //                             ans=(j-i+1);curr=t;
+    //                         }
+    //                     }
+    //                         int t=dp[i][j][1];
+    //                         if(t>=0&&t>curr){
+    //                             ans=(j-i+1);curr=t;
+    //                         }
+    //                     int t1=dp[i][j-1][2];
+    //                     if(t1>ans){
+    //                         ans=t1;
+    //                     }
+    //                     int t2=dp[i+1][j][2];
+    //                     if(t2>ans){
+    //                         ans=t2;
+    //                     }
+    //                     dp[i][j][flag]=ans;
+    //                 }
+    //                 else if(flag==0){
+    //                     if(s[i]==s[j])
+    //                         dp[i][j][flag]=2+dp[i+1][j-1][0];
+    //                     else
+    //                          dp[i][j][flag]=INT_MIN;
+    //                 }
+    //                 else{
+    //                     int curr=INT_MIN;
+    //                     if(s[i]==s[j]){
+    //                         curr=max(curr,2+dp[i+1][j-1][1]);
+    //                     }
+    //                     if(flag){
+    //                          curr=max(curr,dp[i+1][j][0]);
+    //                          curr=max(curr,dp[i][j-1][0]);
+    //                     }
+    //                     dp[i][j][flag]=curr;
+    //                 }
+    //            }
+    //        }
+    //    }
+    //     return dp[0][n-1][2];
+    // }
+
+
+
+    void check(string &str,int i,int j,int &ans){
+        while(i>=0&&j<str.length()&&str[i]==str[j]){
+            i--;j++;
+        }
+        ans=max(ans,j-i-1);
+    }
+    int almostPalindromic(string str) {
+        int n=str.length();
+        int ans=2;
+        for(int i=0;i<n;i++){
+            int s=i;int e=i;
+            while(s>=0&&e<n&&str[s]==str[e]){
+                s--;e++;
+            }
+            ans=max(ans,e-s-1);
+            if(s>=0)
+            check(str,s-1,e,ans);
+            if(e<n)
+            check(str,s,e+1,ans);
+            s=i;e=i+1;
+            while(s>=0&&e<n&&str[s]==str[e]){
+                s--;e++;
+            }
+            ans=max(ans,e-s-1);
+            if(s>=0)
+            check(str,s-1,e,ans);
+            if(e<n)
+            check(str,s,e+1,ans);
+        }
+        return ans;
     }
 };
