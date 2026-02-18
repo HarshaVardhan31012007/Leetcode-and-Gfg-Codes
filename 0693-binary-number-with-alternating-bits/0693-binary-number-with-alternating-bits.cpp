6d10878bef507b8d86f154dde3b3cb4bca15d8f8
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-18 13:57:41 +0530

    Time: 0 ms (100%), Space: 8 MB (5.5%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
+    // bool hasAlternatingBits(int n) {
+    //     bool check=true;
+    //     int prev=-1;
+    //     while(n){
+    //         int bit=n%2;
+    //         if(prev!=-1&&bit!=1-prev){
+    //            check=false;
+    //            break;
+    //         }
+    //         prev=bit;
+    //         n=n/2;
+    //     }
+    //     return check;
+    // }
+
+
     bool hasAlternatingBits(int n) {
-        bool check=true;
-        int prev=-1;
-        while(n){
-            int bit=n%2;
-            if(prev!=-1&&bit!=1-prev){
-               check=false;
-               break;
-            }
-            prev=bit;
-            n=n/2;
+        if(n%2==1){
+            double k=log2(n*3LL+1)/2.0;
+            if(k==(int)k) return true;
         }
-        return check;
+        else{
+           double k=log2((n*3LL+2)/2.0)/2.0;
+           if(k==(int)k) return true;
+        }
+        return false;
     }
 };
