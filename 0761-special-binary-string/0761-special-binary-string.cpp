43f8ce0aeed454b09856212df6c7b4579ba45112
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-21 23:24:56 +0530

    Time: 0 ms (100%), Space: 10.6 MB (7.45%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.cpp
+++ 0761-special-binary-string/0761-special-binary-string.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-    string makeLargestSpecial(string s) {
-        if (s == "") return s;
-        vector<string> ans;
-        int cnt = 0;
-        for (int i = 0, j = 0; i < s.size(); ++i) {
-            cnt += s[i] == '1' ? 1 : -1;
-            if (cnt == 0) {
-                ans.push_back("1" + makeLargestSpecial(s.substr(j + 1, i - j - 1)) + "0");
-                j = i + 1;
+    string makeLargestSpecialString(string s){
+        int n=s.length();
+        int count=0;int start=0;
+        vector<string>childs;
+        for(int i=0;i<n;i++){
+            if(s[i]=='1'){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count==0){
+                string subproblem=s.substr(start+1,i-start-1);
+                string chunk='1'+makeLargestSpecialString(subproblem)+'0';
+                childs.push_back(chunk);
+                start=i+1;
             }
         }
-        sort(ans.begin(), ans.end(), greater<string>{});
-        return accumulate(ans.begin(), ans.end(), ""s);
+        sort(childs.rbegin(),childs.rend());
+        string ans="";
+        for(auto &each:childs){
+            ans+=each;
+        }
+        return ans;
+    }
+    string makeLargestSpecial(string s) {
+        return makeLargestSpecialString(s);
     }
 };
