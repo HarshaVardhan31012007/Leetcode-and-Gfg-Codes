34f0dc56fdb261c4db0f8253c887d86846e230e4
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-22 17:36:34 +0530

    Time: 26 ms (63.46%), Space: 16.7 MB (59.32%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid,int i,int j,int m,int n){
-        grid[i][j]='0';
-        vector<pair<int,int>>dir={{0,1},{0,-1},{1,0},{-1,0}};
-        for(auto &each:dir){
-            int newX=i+each.first;
-            int newY=j+each.second;
-            if(newX>=0 && newX<m &&newY>=0 && newY<n && grid[newX][newY]=='1'){
-                dfs(grid,newX,newY,m,n);
-            }
+    void expandLand(int i,int j,vector<vector<char>>&grid,vector<vector<bool>>&visited){
+        visited[i][j]=1;
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
+        for(int k=0;k<4;k++){
+            int newr=i+dx[k];
+            int newc=j+dy[k];
+            if(newc<0||newr<0||newr>=grid.size()||newc>=grid[0].size()) continue;
+            if(!visited[newr][newc]&&grid[newr][newc]=='1')
+            expandLand(newr,newc,grid,visited);
         }
     }
     int numIslands(vector<vector<char>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        int islands=0;
+        vector<vector<bool>>visited(m,vector<bool>(n,0));
+        int no_of_islands=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'){
-                  dfs(grid,i,j,m,n);
-                  islands++;
+                if(!visited[i][j]&&grid[i][j]=='1'){
+                   expandLand(i,j,grid,visited);
+                   no_of_islands++;
                 }
             }
         }
-        return islands;
+        return no_of_islands;
     }
 };
