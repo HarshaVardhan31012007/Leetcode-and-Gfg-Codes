8786e3bcd0ea6f82767246bfa3f6f32be00f2985
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-22 10:56:30 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.75%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -18,20 +18,35 @@ public:
     // }
 
 
-    int binaryGap(int n) {
-          int num=n;
-          int prev=-1;
-          int ans=0;int c=0;
-          while(num>0){
-            int bit=(num&1);
-            if(bit==1){
-                if(prev!=-1)
-                   ans=max(ans,c-prev);
-                prev=c;
+    // int binaryGap(int n) {
+    //       int num=n;
+    //       int prev=-1;
+    //       int ans=0;int c=0;
+    //       while(num>0){
+    //         int bit=(num&1);
+    //         if(bit==1){
+    //             if(prev!=-1)
+    //                ans=max(ans,c-prev);
+    //             prev=c;
+    //         }
+    //         c++;
+    //         num=num>>1;
+    //       }
+    //       return ans;
+    // }
+
+
+
+     int binaryGap(int n) {
+         int ans=0;
+         int prev=-1;
+         for(int i=0;i<32;i++){
+            if(((n>>i)&1)==1){
+            if(prev!=-1)
+               ans=max(ans,i-prev);
+               prev=i;
             }
-            c++;
-            num=num>>1;
-          }
-          return ans;
+         }
+         return ans;
     }
 };
