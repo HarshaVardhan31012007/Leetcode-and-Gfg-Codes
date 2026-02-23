364a890abff3c25c3c3efbbe5662fb5c2844d274
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-24 01:01:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -1,71 +1,45 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 class Solution {
   public:
-    void dfs(int s,unordered_map<int,list<pair<int,int>>>&adjList,unordered_map<int,bool>&visited,vector<int>&distance){
-        visited[s]=1;
-        for(auto &nbr:adjList[s]){
-            if(!visited[nbr.first]&&distance[s]+nbr.second<distance[nbr.first]){
-                distance[nbr.first]=distance[s]+nbr.second;
-                dfs(nbr.first,adjList,visited,distance);
-            }
+    void topoSort(int src,vector<int>&visited,unordered_map<int,list<pair<int,int>>>&adjList,stack<int>&st){
+        visited[src]=1;
+        for(auto &adj:adjList[src]){
+            int v=adj.first;
+            if(!visited[v])
+            topoSort(v,visited,adjList,st);
         }
-        visited[s]=0;
+        st.push(src);
     }
     vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
-        unordered_map<int,bool>visited;
         unordered_map<int,list<pair<int,int>>>adjList;
-        for(int i=0;i<E;i++){
-            adjList[edges[i][0]].push_back({edges[i][1],edges[i][2]});
+        for(auto &each:edges){
+            int u=each[0];
+            int v=each[1];
+            int w=each[2];
+            adjList[u].push_back({v,w});
         }
         vector<int>distance(V,INT_MAX);
         distance[0]=0;
-        dfs(0,adjList,visited,distance);
-        for(auto &each:distance){
-            if(each==INT_MAX)
-            each=-1;
+        stack<int>st;
+        vector<int>visited(V,0);
+        for(int i=0;i<V;i++){
+           if(!visited[i])
+           topoSort(i,visited,adjList,st);
         }
+        while(!st.empty()){
+            auto top=st.top();
+            st.pop();
+            for(auto &adj:adjList[top]){
+                int v=adj.first;int w=adj.second;
+                if(distance[top]!=INT_MAX)
+                distance[v]=min(distance[v],w+distance[top]);
+            }
+        }
+        for(auto &each:distance)
+        each=(each==INT_MAX?-1:each);
         return distance;
     }
 };
 
 
 
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> edges;
-        for (int i = 0; i < m; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 3; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            edges.push_back(temp);
-        }
-        Solution obj;
-        vector<int> res = obj.shortestPath(n, m, edges);
-        for (auto x : res) {
-            cout << x << " ";
-        }
-        cout << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-}
-
-// } Driver Code Ends
