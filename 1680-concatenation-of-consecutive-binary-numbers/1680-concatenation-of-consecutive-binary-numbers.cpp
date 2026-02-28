941525c2e2320b86a5ca4c164ad268ce56daafb3
HARSHA VARDHAN <hv1900019@gmail.com>
2026-02-28 11:19:27 +0530

    Time: 52 ms (42.28%), Space: 8.1 MB (41.61%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
+    // const int mod=1e9+7;
+    // int concatenatedBinary(int n) {
+    //     long long int ans=0;
+    //     int c=1;
+    //     for(int num=n;num>=1;num--){
+    //         int x=num;
+    //         while(x){
+    //             int digit=(x&1);
+    //             if(digit){
+    //                 ans=(ans+(1LL*c*digit)%mod)%mod;
+    //             }
+    //             else{
+    //                ans=(ans+(1LL*c*digit)%mod)%mod;
+    //             }
+    //             x=x>>1;
+    //             c=(2LL*c)%mod;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
     const int mod=1e9+7;
     int concatenatedBinary(int n) {
         long long int ans=0;
-        int c=1;
-        for(int num=n;num>=1;num--){
-            int x=num;
-            while(x){
-                int digit=(x&1);
-                if(digit){
-                    ans=(ans+(1LL*c*digit)%mod)%mod;
-                }
-                else{
-                   ans=(ans+(1LL*c*digit)%mod)%mod;
-                }
-                x=x>>1;
-                c=(2LL*c)%mod;
-            }
+        for(int num=1;num<=n;num++){
+            int digits=(log2(num)+1);
+            ans=((ans<<digits)%mod+num%mod)%mod;
         }
         return ans;
     }
