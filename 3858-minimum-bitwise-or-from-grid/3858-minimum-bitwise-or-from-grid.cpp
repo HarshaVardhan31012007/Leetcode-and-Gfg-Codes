450f476d2614950ed507816a4fdf0783969b25cf
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-01 12:46:10 +0530

    Time: 11 ms (56.3%), Space: 125.9 MB (75.62%) - LeetHub

--- 3858-minimum-bitwise-or-from-grid/3858-minimum-bitwise-or-from-grid.cpp
+++ 3858-minimum-bitwise-or-from-grid/3858-minimum-bitwise-or-from-grid.cpp
@@ -1,24 +1,57 @@
 class Solution {
 public:
-    unordered_map<int,unordered_set<int>>dp;
-    void solve(int i,vector<vector<int>>& grid,int m,int n,int val,int &mini){
-        if(val>=mini) return;
-        if(i>=m){
-            mini=min(mini,val);
-            return;
-        }
-        if(dp.count(i)){
-            if(dp[i].count(val)) return;
-        }
-        for(int j=0;j<n;j++){
-            solve(i+1,grid,m,n,val|grid[i][j],mini);
-        }
-        dp[i].insert(val);
-    }
+    // unordered_map<int,unordered_set<int>>dp;
+    // void solve(int i,vector<vector<int>>& grid,int m,int n,int val,int &mini){
+    //     if(val>=mini) return;
+    //     if(i>=m){
+    //         mini=min(mini,val);
+    //         return;
+    //     }
+    //     if(dp.count(i)){
+    //         if(dp[i].count(val)) return;
+    //     }
+    //     for(int j=0;j<n;j++){
+    //         solve(i+1,grid,m,n,val|grid[i][j],mini);
+    //     }
+    //     dp[i].insert(val);
+    // }
+    // int minimumOR(vector<vector<int>>& grid) {
+    //     int m=grid.size();int n=grid[0].size();
+    //     int mini=INT_MAX;
+    //     solve(0,grid,m,n,0,mini);
+    //     return mini;
+    // }
+
+
+
+    
     int minimumOR(vector<vector<int>>& grid) {
         int m=grid.size();int n=grid[0].size();
-        int mini=INT_MAX;
-        solve(0,grid,m,n,0,mini);
-        return mini;
+        int ans=0;
+        int forbidden=0;
+        for(int k=31;k>=0;k--){
+            bool possible=true;
+            int tempforbidden=forbidden|(1<<k);
+            for(int i=0;i<m;i++){
+                bool found=false;
+                for(int j=0;j<n;j++){
+                     if((grid[i][j]&tempforbidden)==0){
+                        found=true;
+                        break;
+                     }
+                }
+                if(!found){
+                    possible=false;
+                    break;
+                }
+            }
+            if(!possible){
+               ans=(ans|(1<<k));
+            }
+            else{
+                forbidden=tempforbidden;
+            }
+        }
+        return ans;
     }
 };
