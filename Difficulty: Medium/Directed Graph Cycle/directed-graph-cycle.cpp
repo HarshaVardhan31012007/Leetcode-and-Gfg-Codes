110ad63eada196a59d924315e77fb6f2cacd1d7a
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-04 01:29:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,38 +1,29 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-     bool detectCycle(vector<vector<int>>& edges,unordered_map<int,bool>&path,unordered_map<int,bool>&visited,int s){
-        visited[s]=true;
-        path[s]=true;
+     bool detectCycle(vector<vector<int>>& edges,vector<int>&visited,int s){
+        visited[s]=2;
         for(auto &c:edges){
             int k;
             if(s==c[0]) k=c[1];
             else continue;
             if(!visited[k]){
-                int ans=detectCycle(edges,path,visited,k);
+                int ans=detectCycle(edges,visited,k);
                 if(ans==true)
                 return true;
             }
-            else if(visited[k]==true && path[k]==true){
+            else if(visited[k]==2){
                 return true;
             }
         }
-        path[s]=false;
+        visited[s]=1;
         return false;
     }
     bool isCyclic(int V, vector<vector<int>> &edges) {
         bool ans=false;
-        unordered_map<int,bool>visited;
-        unordered_map<int,bool>path;
+        vector<int>visited(V,0);
         for(int i=0;i<V;i++){
             if(!visited[i]){
-                ans=detectCycle(edges,path,visited,i);
+                ans=detectCycle(edges,visited,i);
                 if(ans==true)
                 return true;
             }
@@ -40,32 +31,3 @@ class Solution {
         return false;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    cin.ignore();
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-        cin.ignore();
-        vector<vector<int>> edges;
-        for (int i = 1; i <= E; i++) {
-            int u, v;
-            cin >> u >> v;
-            edges.push_back({u, v});
-        }
-
-        Solution obj;
-        bool ans = obj.isCyclic(V, edges);
-        if (ans)
-            cout << "true\n";
-        else
-            cout << "false\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
