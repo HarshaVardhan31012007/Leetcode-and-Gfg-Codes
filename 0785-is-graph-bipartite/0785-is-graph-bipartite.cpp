bcb86b7996b89018ba3df7d2e876f8ac1897c5c2
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-03 23:50:57 +0530

    Time: 0 ms (100%), Space: 17.4 MB (76.62%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,48 +1,16 @@
 class Solution {
 public:
-    // bool dfs(int src,int mark,vector<vector<int>>&graph,vector<int>&visited){
-    //     visited[src]=mark;
-    //     bool ans=true;
-    //     for(auto &each:graph[src]){
-    //         if(visited[each]==-1)
-    //         ans=ans&&dfs(each,1-mark,graph,visited);
-    //         else{
-    //             if(visited[each]!=1-mark) return false;
-    //         }
-    //     }
-    //     return ans;
-    // }
-    // bool isBipartite(vector<vector<int>>& graph) {
-    //     int n=graph.size();
-    //     int ans=true;
-    //     vector<int>visited(n,-1);
-    //     for(int i=0;i<n;i++){
-    //         if(visited[i]==-1){
-    //            ans=ans&&dfs(i,0,graph,visited);
-    //         }
-    //     }
-    //     return ans;
-    // }
-
-
-    bool bfs(int src,vector<vector<int>>& graph,vector<int>&visited,int color){
-        queue<int>q;
-        q.push(src);
+    bool dfs(int src,int color,vector<vector<int>>&graph,vector<int>&visited){
         visited[src]=color;
-        while(!q.empty()){
-            auto front=q.front();
-            q.pop();
-            for(auto &each:graph[front]){
-                if(visited[each]==-1){
-                    visited[each]=!visited[front];
-                    q.push(each);
-                }
-                else{
-                    if(visited[each]==visited[front]) return false;
-                }
+        bool ans=true;
+        for(auto &each:graph[src]){
+            if(visited[each]==-1)
+            ans=ans&&dfs(each,!color,graph,visited);
+            else{
+                if(visited[each]==color) return false;
             }
         }
-        return true;
+        return ans;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
@@ -50,9 +18,41 @@ public:
         vector<int>visited(n,-1);
         for(int i=0;i<n;i++){
             if(visited[i]==-1){
-               ans=ans&&bfs(i,graph,visited,0);
+               ans=ans&&dfs(i,0,graph,visited);
             }
         }
         return ans;
     }
+
+
+    // bool bfs(int src,vector<vector<int>>& graph,vector<int>&visited,int color){
+    //     queue<int>q;
+    //     q.push(src);
+    //     visited[src]=color;
+    //     while(!q.empty()){
+    //         auto front=q.front();
+    //         q.pop();
+    //         for(auto &each:graph[front]){
+    //             if(visited[each]==-1){
+    //                 visited[each]=!visited[front];
+    //                 q.push(each);
+    //             }
+    //             else{
+    //                 if(visited[each]==visited[front]) return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+    // bool isBipartite(vector<vector<int>>& graph) {
+    //     int n=graph.size();
+    //     int ans=true;
+    //     vector<int>visited(n,-1);
+    //     for(int i=0;i<n;i++){
+    //         if(visited[i]==-1){
+    //            ans=ans&&bfs(i,graph,visited,0);
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
