7db850b3b171ab4c58a66e26fbce12b207af3f07
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-03 22:27:38 +0530

    Time: 3 ms (15.24%), Space: 17.7 MB (15.36%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,16 +1,48 @@
 class Solution {
 public:
-    bool dfs(int src,int mark,vector<vector<int>>&graph,vector<int>&visited){
+    // bool dfs(int src,int mark,vector<vector<int>>&graph,vector<int>&visited){
+    //     visited[src]=mark;
+    //     bool ans=true;
+    //     for(auto &each:graph[src]){
+    //         if(visited[each]==-1)
+    //         ans=ans&&dfs(each,1-mark,graph,visited);
+    //         else{
+    //             if(visited[each]!=1-mark) return false;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    // bool isBipartite(vector<vector<int>>& graph) {
+    //     int n=graph.size();
+    //     int ans=true;
+    //     vector<int>visited(n,-1);
+    //     for(int i=0;i<n;i++){
+    //         if(visited[i]==-1){
+    //            ans=ans&&dfs(i,0,graph,visited);
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
+    bool bfs(int src,vector<vector<int>>& graph,vector<int>&visited,int mark){
+        queue<int>q;
+        q.push(src);
         visited[src]=mark;
-        bool ans=true;
-        for(auto &each:graph[src]){
-            if(visited[each]==-1)
-            ans=ans&&dfs(each,1-mark,graph,visited);
-            else{
-                if(visited[each]!=1-mark) return false;
+        while(!q.empty()){
+            auto front=q.front();
+            q.pop();
+            for(auto &each:graph[front]){
+                if(visited[each]==-1){
+                    visited[each]=1-visited[front];
+                    q.push(each);
+                }
+                else{
+                    if(visited[each]!=1-visited[front]) return false;
+                }
             }
         }
-        return ans;
+        return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
@@ -18,7 +50,7 @@ public:
         vector<int>visited(n,-1);
         for(int i=0;i<n;i++){
             if(visited[i]==-1){
-               ans=ans&&dfs(i,0,graph,visited);
+               ans=ans&&bfs(i,graph,visited,0);
             }
         }
         return ans;
