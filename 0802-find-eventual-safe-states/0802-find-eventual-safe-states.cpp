55df38d74eadd01db3fb109d45847542748cf170
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-04 23:50:36 +0530

    Time: 52 ms (37.21%), Space: 66.3 MB (39.28%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,30 +1,63 @@
 class Solution {
 public:
-    bool dfs(int src,vector<vector<int>>&graph,vector<int>&visited){
-        visited[src]=2;
-        for(auto &adj:graph[src]){
-            if(!visited[adj]){
-                bool ans=dfs(adj,graph,visited);
-                if(ans) return true;
-            }
-            else if(visited[adj]==2) return true;
-        }
-        visited[src]=1;
-        return false;
-    }
+    // bool dfs(int src,vector<vector<int>>&graph,vector<int>&visited){
+    //     visited[src]=2;
+    //     for(auto &adj:graph[src]){
+    //         if(!visited[adj]){
+    //             bool ans=dfs(adj,graph,visited);
+    //             if(ans) return true;
+    //         }
+    //         else if(visited[adj]==2) return true;
+    //     }
+    //     visited[src]=1;
+    //     return false;
+    // }
+    // vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    //     int no_of_nodes=graph.size();
+    //     vector<int>visited(no_of_nodes,0);
+    //     for(int i=0;i<no_of_nodes;i++){
+    //         if(!visited[i]){
+    //             dfs(i,graph,visited);
+    //         }
+    //     }
+    //     vector<int>safeStates;
+    //     for(int i=0;i<no_of_nodes;i++){
+    //         if(visited[i]==1)
+    //         safeStates.push_back(i);
+    //     }
+    //     return safeStates;
+    // }
+
+
+
+   
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int no_of_nodes=graph.size();
-        vector<int>visited(no_of_nodes,0);
+        vector<vector<int>>revgraph(no_of_nodes);
+        vector<int>indegree(no_of_nodes,0);
         for(int i=0;i<no_of_nodes;i++){
-            if(!visited[i]){
-                dfs(i,graph,visited);
+            for(auto &each:graph[i]){
+                revgraph[each].push_back(i);
+                indegree[i]++;
             }
         }
-        vector<int>safeStates;
+        queue<int>q;
         for(int i=0;i<no_of_nodes;i++){
-            if(visited[i]==1)
-            safeStates.push_back(i);
+            if(indegree[i]==0)
+            q.push(i);
         }
+        vector<int>safeStates;
+        while(!q.empty()){
+            auto front=q.front();
+            q.pop();
+            safeStates.push_back(front);
+            for(auto &adj:revgraph[front]){
+                indegree[adj]--;
+                if(indegree[adj]==0)
+                q.push(adj);
+            }
+        }
+        sort(safeStates.begin(),safeStates.end());
         return safeStates;
     }
 };
