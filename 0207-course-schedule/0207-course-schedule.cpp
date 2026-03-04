6234e2a0767bb98bc5055a3e5ab6bbc8a8434497
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-04 20:58:29 +0530

    Time: 1 ms (84.12%), Space: 19.1 MB (82.14%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,77 +1,29 @@
 class Solution {
 public:
-   //Valid topological sort means no cycle hence we should check cycle present or not
-
-   //By BFS
-
-    void topoSort(vector<vector<int>>& prerequisites,vector<int>&ans,int n){
-        vector<int>indegree(n);
-        queue<int>q;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>dependents(numCourses,0);
+        vector<vector<int>>adjCourses(numCourses);
         for(auto &each:prerequisites){
-            indegree[each[0]]++; 
+            int u=each[0];int v=each[1];
+            adjCourses[u].push_back(v);
+            dependents[v]++;
         }
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0)
-            q.push(i);
+        queue<int>q;
+        for(int course=0;course<numCourses;course++){
+            if(dependents[course]==0)
+            q.push(course);
         }
+        int completed=0;
         while(!q.empty()){
-            auto front=q.front();
-            ans.push_back(front);
+            auto current=q.front();
             q.pop();
-            for(auto &each:prerequisites){
-                int k;
-                if(each[1]==front) k=each[0];
-                else continue;
-                indegree[k]--;
-                if(indegree[k]==0)
-                q.push(k);
+            completed++;
+            for(auto &adj:adjCourses[current]){
+                 dependents[adj]--;
+                 if(dependents[adj]==0)
+                 q.push(adj);
             }
         }
+        return completed==numCourses;
     }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>ans;
-        int n=numCourses;
-        topoSort(prerequisites,ans,n);
-        if(ans.size()==n)
-        return true;
-        return false;
-    }
-
-
-//by DFS
-
-
-//    bool checkCycle(vector<vector<int>>& prerequisites,unordered_map<int,bool>&visited,unordered_map<int,bool>&path,int s){
-//     visited[s]=1;path[s]=1;
-//     for(auto &each:prerequisites){
-//         int k;
-//         if(each[1]==s) k=each[0];
-//         else continue;
-//         if(!visited[k]){
-//             bool ans=checkCycle(prerequisites,visited,path,k);
-//             if(ans==true)
-//             return true;
-//         }
-//         else if(visited[k]==true&&path[k]==true){
-//             return true;
-//         }
-//     }
-//     path[s]=false;
-//     return false;
-//    }
-//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-//         unordered_map<int,bool>visited;
-//         unordered_map<int,bool>path;
-//         int n=numCourses;
-//         bool ans=0;
-//         for(int i=0;i<n;i++){
-//             if(!visited[i]){
-//                 ans=checkCycle(prerequisites,visited,path,i);
-//                 if(ans==true){
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
 };
