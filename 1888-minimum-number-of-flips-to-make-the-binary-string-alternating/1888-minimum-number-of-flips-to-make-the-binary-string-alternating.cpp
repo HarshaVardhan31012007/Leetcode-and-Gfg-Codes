5657d7bca4c061fee299f6f3f96e3542fe98ea1d
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-07 14:44:26 +0530

    Time: 15 ms (67.6%), Space: 17.2 MB (61.6%) - LeetHub

---
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minFlips(string s) {
+        int n=s.length();
+        s=s+s;//imp to get all states after applying type1
+        //in s // all are susbtrings that can be get by sliding window
+        int ans=0;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(i&1){
+                if(s[i]=='0') cnt++;
+            }
+            else{
+                if(s[i]=='1') cnt++;
+            }
+        }
+        ans=min(cnt,n-cnt);
+        int i=0;
+        for(int j=n;j<2*n;j++){
+            //removal
+            if(i&1){
+                if(s[i]=='0') cnt--;
+            }
+            else{
+                if(s[i]=='1') cnt--;
+            }
+            i++;
+            //addition
+            if(j&1){
+                if(s[j]=='0') cnt++;
+            }
+            else{
+                if(s[j]=='1') cnt++;
+            }
+            //compute ans
+            ans=min(ans,min(cnt,n-cnt));
+        }
+        return ans;
+    }
+};
