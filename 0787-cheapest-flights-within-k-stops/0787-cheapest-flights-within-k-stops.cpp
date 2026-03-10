b90402f871602763b57d4e674befcd349900c018
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-10 13:38:34 +0530

    Time: 4 ms (47.7%), Space: 18 MB (57.67%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+    // int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    //     unordered_map<int,list<pair<int,int>>>adjList;
+    //     for(auto &each:flights){
+    //          int u=each[0];
+    //          int v=each[1];
+    //          int w=each[2];
+    //          adjList[u].push_back({v,w});
+    //     }
+    //     vector<vector<int>>price(n,vector<int>(k+2,INT_MAX));
+    //     price[src][k+1]=0;
+    //     priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<>>pq;
+    //     pq.push({0,{src,k+1}});
+    //     while(!pq.empty()){
+    //         auto top=pq.top();
+    //         int cprice=top.first;
+    //         int city=top.second.first;
+    //         int stops=top.second.second;
+    //         pq.pop();
+    //         if(city==dst) return cprice;
+    //         for(auto &adj:adjList[city]){
+    //             int v=adj.first;int w=adj.second;
+    //             if(cprice+w<price[v][stops]&&stops>0){
+    //                 price[v][stops]=cprice+w;
+    //                 pq.push({price[v][stops],{v,stops-1}});
+    //             }
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+
+
+
+
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         unordered_map<int,list<pair<int,int>>>adjList;
         for(auto &each:flights){
@@ -8,25 +42,24 @@ public:
              int w=each[2];
              adjList[u].push_back({v,w});
         }
-        vector<vector<int>>price(n,vector<int>(k+2,INT_MAX));
-        price[src][k+1]=0;
+        vector<int>price(n,INT_MAX);
+        price[src]=0;
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<>>pq;
-        pq.push({0,{src,k+1}});
+        pq.push({0,{src,0}});
         while(!pq.empty()){
             auto top=pq.top();
-            int cprice=top.first;
+            int cprice=top.second.second;
             int city=top.second.first;
-            int stops=top.second.second;
+            int stops=top.first;
             pq.pop();
-            if(city==dst) return cprice;
             for(auto &adj:adjList[city]){
                 int v=adj.first;int w=adj.second;
-                if(cprice+w<price[v][stops]&&stops>0){
-                    price[v][stops]=cprice+w;
-                    pq.push({price[v][stops],{v,stops-1}});
+                if(cprice+w<price[v]&&stops<=k){
+                    price[v]=cprice+w;
+                    pq.push({stops+1,{v,price[v]}});
                 }
             }
         }
-        return -1;
+        return price[dst]==INT_MAX?-1:price[dst];
     }
 };
