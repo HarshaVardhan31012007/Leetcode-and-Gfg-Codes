1ebe905b313c99be4f6ef0785af655f0d1c56974
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-11 21:34:27 +0530

    Time: 0 ms (100%), Space: 7.8 MB (55.01%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
-        for(int b=2;b<=n-2;b++){
-            string s="";
-            int num=n;
-            while(num!=0){
-                s+=to_string(num%b);
-                num=num/2;
-            }
-            int i=0;int j=s.length()-1;
-            while(i<j){
-                if(s[i]==s[j]){
-                    i++;j--;
-                }
-                else{
-                    return false;
-                }
-            }
-        }
+        // for(int b=2;b<=n-2;b++){
+        //     string s="";
+        //     int num=n;
+        //     while(num!=0){
+        //         s+=to_string(num%b);
+        //         num=num/2;
+        //     }
+        //     int i=0;int j=s.length()-1;
+        //     while(i<j){
+        //         if(s[i]==s[j]){
+        //             i++;j--;
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+        if(n>=4) return false;
         return true;
     }
 };
