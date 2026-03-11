66eb42d13c49c3a136f5936ec115fd79707e0e0d
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-11 18:27:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.cpp
+++ Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.cpp
@@ -1,60 +1,26 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 
 class Solution {
   public:
     int minimumMultiplications(vector<int>& arr, int start, int end) {
-        queue<int>q;
-        const int mod=100000;
-        vector<int>ans(100000,-1);
-        q.push(start);
-        ans[start]=0;
-        while(!q.empty()){
-            auto front=q.front();
-            q.pop();
-            if(front==end) return ans[end];
-            for(int i=0;i<arr.size();i++){
-                int n=(front*arr[i]) % mod;
-                if(ans[n]==-1){
-                q.push(n);
-                ans[n]=ans[front]+1;
+        vector<int>nSteps(100000,INT_MAX);
+        nSteps[start]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        pq.push({0,start});
+        while(!pq.empty()){
+            pair<int,int>curr=pq.top();
+            int currSteps=curr.first;
+            int currNumber=curr.second;
+            pq.pop();
+            if(currNumber==end) return currSteps;
+            for(auto &num:arr){
+                int newStart=((currNumber*num)%100000);
+                if(currSteps+1<nSteps[newStart]){
+                    nSteps[newStart]=currSteps+1;
+                    pq.push({nSteps[newStart],newStart});
                 }
             }
         }
         return -1;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<int> arr(n);
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
-        int start, end;
-        cin >> start >> end;
-        Solution obj;
-        cout << obj.minimumMultiplications(arr, start, end) << endl;
-    
-cout << "~" << "\n";
-}
-}
-
-// } Driver Code Ends
