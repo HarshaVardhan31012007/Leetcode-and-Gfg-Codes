003dc3b65f62a7be18a333f707bae6f118dd8d85
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-13 13:11:55 +0530

    Time: 1 ms (96.77%), Space: 29 MB (65.81%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -43,12 +43,44 @@ public:
 
 
 
-    bool possible(int mountainHeight,vector<int>&workerTimes,long long mid){
+    // bool possible(int mountainHeight,vector<int>&workerTimes,long long mid){
+    //     long long noOfWorkers=workerTimes.size();
+    //     long long contributedHeight=0;
+    //     for(int i=0;i<noOfWorkers;i++){
+    //         int currWorkerTime=workerTimes[i];
+    //         long long ans=(-1LL+sqrt(1LL+((8LL*mid)/currWorkerTime)))/2LL;
+    //         if(ans<0) ans=0;
+    //         contributedHeight+=ans;
+    //     }
+    //     return contributedHeight>=mountainHeight;
+    // }
+    // long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+    //     long long noOfWorkers=workerTimes.size();
+    //     long long s=1;
+    //     long long maxWorkerTime=*max_element(workerTimes.begin(),workerTimes.end());
+    //     long long e=(1LL*(maxWorkerTime)*mountainHeight*(mountainHeight+1))/2;
+    //     long long ans=-1;
+    //     while(s<=e){
+    //         long long mid=s+(e-s)/2;
+    //         if(possible(mountainHeight,workerTimes,mid)){
+    //             ans=mid;
+    //             e=mid-1;
+    //         }
+    //         else{
+    //             s=mid+1;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
+
+     bool possible(int mountainHeight,vector<int>&workerTimes,long long mid){
         long long noOfWorkers=workerTimes.size();
         long long contributedHeight=0;
         for(int i=0;i<noOfWorkers;i++){
             int currWorkerTime=workerTimes[i];
-            long long ans=(-1LL+sqrt(1LL+((8LL*mid)/currWorkerTime)))/2LL;
+            long long ans=(-0.5+sqrt(0.25+(2.0*mid)/currWorkerTime));
             if(ans<0) ans=0;
             contributedHeight+=ans;
         }
