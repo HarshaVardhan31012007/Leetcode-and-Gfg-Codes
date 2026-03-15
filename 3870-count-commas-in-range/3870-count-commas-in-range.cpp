3838498884b596ea2388d4e7d19237f5f6229af6
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-15 23:38:00 +0530

    Time: 20 ms (12.62%), Space: 8.5 MB (86.82%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int find(int n){
+        int c=0;
+        while(n>0){
+            c++;
+            n=n/10;
+        }
+        return c;
+    }
+    int countCommas(int n) {
+        int sum=0;
+        for(int i=1000;i<=n;i++){
+            int digits=find(i);
+            sum+=digits/4;
+        }
+        return sum;
+    }
+};
