94fce1ee053726c2bd11d417fcaf18dfd438caf4
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-16 00:19:06 +0530

    Time: 0 ms (100%), Space: 8.9 MB (85.51%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    long long find(long long n){
-        long long c=0;
-        while(n>0){
-            c++;
-            n=n/10;
-        }
-        return c;
-    }
+    // long long find(long long n){
+    //     long long c=0;
+    //     while(n>0){
+    //         c++;
+    //         n=n/10;
+    //     }
+    //     return c;
+    // }
+    // long long countCommas(long long n) {
+    //     vector<long long>digits={0,0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5};
+    //     long long ndigits=find(n);
+    //     long long sum=0;long long c=1000;
+    //     for(int i=4;i<ndigits;i++){
+    //         sum+=9*c*digits[i];
+    //         c=c*10;
+    //     }
+    //     if(ndigits>=4)
+    //     sum+=(n-c+1)*digits[ndigits];
+    //     return sum;
+    // }
+
+
+
     long long countCommas(long long n) {
-        vector<long long>digits={0,0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5};
-        long long ndigits=find(n);
-        long long sum=0;long long c=1000;
-        for(int i=4;i<ndigits;i++){
-            sum+=9*c*digits[i];
-            c=c*10;
-        }
-        if(ndigits>=4)
-        sum+=(n-c+1)*digits[ndigits];
-        return sum;
+       if(n<1000LL) return 0;
+       else if(n<1000000LL) return n-1000LL+1;
+       else if(n<1000000000LL) return (n-1000000LL+1)*2+(999999LL-1000LL+1);
+       else if(n<1000000000000LL)  return (n-1000000000LL+1)*3+(999999999LL-1000000LL+1)*2+(999999LL-1000LL+1);
+       else if(n<1000000000000000LL) return (n-1000000000000LL+1)*4+(999999999999LL-1000000000LL+1)*3+(999999999LL-1000000LL+1)*2+(999999LL-1000LL+1);
+       return 5+(999999999999999LL-1000000000000LL+1)*4+(999999999999LL-1000000000LL+1)*3+(999999999LL-1000000LL+1)*2+(999999LL-1000LL+1);
     }
 };
