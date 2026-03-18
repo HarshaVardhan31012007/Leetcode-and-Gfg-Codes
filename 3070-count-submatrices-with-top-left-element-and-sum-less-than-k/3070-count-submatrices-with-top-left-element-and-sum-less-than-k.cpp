83b76a30862cd7c515537106a16fd9aa8909eee4
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-18 17:28:33 +0530

    Time: 20 ms (33.15%), Space: 101.1 MB (34.83%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -5,11 +5,16 @@ public:
         vector<vector<int>>prefix(m,vector<int>(n,0));
         int ans=0;
         for(int i=0;i<m;i++){
+            bool flag=false;
             for(int j=0;j<n;j++){
                  prefix[i][j]=grid[i][j]+((i>0?prefix[i-1][j]:0)+(j>0?prefix[i][j-1]:0)-(i>0&&j>0?prefix[i-1][j-1]:0));
-                 if(prefix[i][j]<=k) ans++;
+                 if(prefix[i][j]<=k){
+                    flag=true;
+                    ans++;
+                 }
                  else break;
             }
+            if(!flag) break;
         }
         return ans;
     }
