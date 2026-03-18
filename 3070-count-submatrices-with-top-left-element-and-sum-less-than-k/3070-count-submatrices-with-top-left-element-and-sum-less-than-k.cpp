ca6383f467d8ef4c12665c4349a5b2a2b33f1cfd
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-18 17:43:16 +0530

    Time: 3 ms (99.44%), Space: 90.4 MB (82.02%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int countSubmatrices(vector<vector<int>>& grid, int k) {
-        int m=grid.size();int n=grid[0].size();
-        vector<vector<int>>prefix(m,vector<int>(n,0));
+    int countSubmatrices(vector<vector<int>>& prefix, int k) {
+        int m=prefix.size();int n=prefix[0].size();
         int ans=0;
         for(int i=0;i<m;i++){
             bool flag=false;
             for(int j=0;j<n;j++){
-                 prefix[i][j]=grid[i][j]+((i>0?prefix[i-1][j]:0)+(j>0?prefix[i][j-1]:0)-(i>0&&j>0?prefix[i-1][j-1]:0));
+                 prefix[i][j]=prefix[i][j]+((i>0?prefix[i-1][j]:0)+(j>0?prefix[i][j-1]:0)-(i>0&&j>0?prefix[i-1][j-1]:0));
                  if(prefix[i][j]<=k){
                     flag=true;
                     ans++;
