16e5e5e344fbe84c1ac67603ce2225178fa3be22
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-19 16:24:44 +0530

    Time: 43 ms (80.49%), Space: 129.7 MB (62.6%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -3,16 +3,15 @@ public:
     int numberOfSubmatrices(vector<vector<char>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>>v(m+1,vector<int>(n+1,0));
-        vector<vector<int>>v1(m+1,vector<int>(n+1,0));
+        vector<vector<pair<int,int>>>v(m+1,vector<pair<int,int>>(n+1,{0,0}));
         int ans=0;
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 int curr=((grid[i-1][j-1]=='.')?0:(grid[i-1][j-1]=='X'?1:-1));
-                v[i][j]=curr+v[i-1][j]+v[i][j-1]-v[i-1][j-1];
+                v[i][j].first=curr+v[i-1][j].first+v[i][j-1].first-v[i-1][j-1].first;
                 int curr1=((grid[i-1][j-1]=='.')?1:0);
-                v1[i][j]=curr1+v1[i-1][j]+v1[i][j-1]-v1[i-1][j-1];
-                if(v1[i][j]!=(i*j)&&v[i][j]==0){
+                v[i][j].second=curr1+v[i-1][j].second+v[i][j-1].second-v[i-1][j-1].second;
+                if(v[i][j].second!=(i*j)&&v[i][j].first==0){
                    ans++;
                 }
             }
