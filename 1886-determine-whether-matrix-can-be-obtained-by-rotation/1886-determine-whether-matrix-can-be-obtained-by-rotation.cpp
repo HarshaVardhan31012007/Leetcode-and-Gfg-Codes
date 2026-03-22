d17b785a3ebcb87e3f15c20e641332e061e93a9e
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-22 13:27:48 +0530

    Time: 0 ms (100%), Space: 14.5 MB (70.77%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -4,32 +4,47 @@ public:
         int m=target.size();
         int n=target[0].size();
         if(m!=n) return false;
-        bool flag=true;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                 if(mat[i][j]!=target[i][j]){
-                    flag=false;
-                    break;
-                 }
-            }
-        }
-        if(flag) return true;
-        int count=3;
-        while(count--){
-            vector<vector<int>>temp(m,vector<int>(n));
-            bool flag=true;
+        // bool flag=true;
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //          if(mat[i][j]!=target[i][j]){
+        //             flag=false;
+        //             break;
+        //          }
+        //     }
+        // }
+        // if(flag) return true;
+        // int count=3;
+        // while(count--){
+        //     vector<vector<int>>temp(m,vector<int>(n));
+        //     bool flag=true;
+        //     for(int i=0;i<m;i++){
+        //         for(int j=0;j<n;j++){
+        //             int curr=mat[i][j];
+        //             int rotated=target[j][n-i-1];
+        //             if(curr!=rotated){
+        //                 flag=false;
+        //             }
+        //             temp[j][n-i-1]=mat[i][j];
+        //         }
+        //     } 
+        //     if(flag) return true;
+        //     mat=temp;
+        // }
+        // return false;
+
+
+
+        
+        for(int i=0;i<4;i++){
+            if(mat==target) return true;
             for(int i=0;i<m;i++){
-                for(int j=0;j<n;j++){
-                    int curr=mat[i][j];
-                    int rotated=target[j][n-i-1];
-                    if(curr!=rotated){
-                        flag=false;
-                    }
-                    temp[j][n-i-1]=mat[i][j];
-                }
-            } 
-            if(flag) return true;
-            mat=temp;
+               for(int j=i+1;j<n;j++){
+                 swap(mat[i][j],mat[j][i]);
+               }
+            }
+            for(int i=0;i<m;i++)
+            reverse(mat[i].begin(),mat[i].end());
         }
         return false;
     }
