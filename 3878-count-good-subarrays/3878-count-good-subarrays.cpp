0fe5c9ce81bd7ba9c602ab0d906e4894794f0db4
HARSHA VARDHAN <hv1900019@gmail.com>
2026-03-22 22:26:59 +0530

    Time: 52 ms (97.52%), Space: 168 MB (92.16%) - LeetHub

--- 3878-count-good-subarrays/3878-count-good-subarrays.cpp
+++ 3878-count-good-subarrays/3878-count-good-subarrays.cpp
@@ -84,23 +84,51 @@ public:
 
 
 
+        // long long n=nums.size();
+        // vector<int>prefix(n,-1);
+        // stack<int>st;
+        // for(int i=n-1;i>=0;i--){
+        //    while(!st.empty()&&((nums[st.top()]|nums[i])>nums[st.top()]||nums[i]==nums[st.top()])){
+        //       prefix[st.top()]=i;
+        //       st.pop();
+        //    }
+        //    st.push(i);
+        // }
+        // vector<int>suffix(n,n);
+        // stack<int>st1;
+        // for(int i=0;i<n;i++){
+        //    while(!st1.empty()&&(nums[st1.top()]|nums[i])>nums[st1.top()]){
+        //       suffix[st1.top()]=i;
+        //       st1.pop();
+        //    }
+        //    st1.push(i);
+        // }
+        // long long int ans=0;
+        // for(int i=0;i<n;i++)
+        // ans+=(i-prefix[i])*(suffix[i]-i);
+        // return ans;
+
+
+
         long long n=nums.size();
-        vector<int>prefix(n,-1);
+        vector<int>suffix(n,n);
         stack<int>st;
         for(int i=n-1;i>=0;i--){
-           while(!st.empty()&&((nums[st.top()]|nums[i])>nums[st.top()]||nums[i]==nums[st.top()])){
-              prefix[st.top()]=i;
+           while(!st.empty()&&(nums[st.top()]|nums[i])==nums[i]){
               st.pop();
            }
+           if(!st.empty())
+           suffix[i]=st.top();
            st.push(i);
         }
-        vector<int>suffix(n,n);
+        vector<int>prefix(n,-1);
         stack<int>st1;
         for(int i=0;i<n;i++){
-           while(!st1.empty()&&(nums[st1.top()]|nums[i])>nums[st1.top()]){
-              suffix[st1.top()]=i;
-              st1.pop();
+           while(!st1.empty()&&((nums[st1.top()]|nums[i])==nums[i])&&nums[st1.top()]!=nums[i]){
+             st1.pop();
            }
+           if(!st1.empty())
+           prefix[i]=st1.top();
            st1.push(i);
         }
         long long int ans=0;
