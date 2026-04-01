6b0122cbe88156da554f9493505a2bcd16c65b65
HARSHA VARDHAN <hv1900019@gmail.com>
2026-04-01 21:57:55 +0530

    Time: 2406 ms (5.01%), Space: 55 MB (7.33%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
-// bool solve(vector<int>&nums,int i,vector<int>&dp){
-//   if(i==nums.size()-1) return true;
-//   if(i>=nums.size()) return false;
-//   if(nums[i]==0) return false;//no need
-//   if(dp[i]!=-1) return dp[i];//noneed of nums[i]==0 return false//loop takes 
-//   //care of it
-//   bool ans=false;
-//   for(int jump=1;jump<=nums[i];jump++){
-//    ans=ans||solve(nums,i+jump,dp);
-//   }
-//   return dp[i]=ans;
-// }
-// bool canJump(vector<int>& nums) {
-//     vector<int>dp(nums.size()+1,-1);
-//     return solve(nums,0,dp);
-// }
-
-
-
-bool solve(vector<int>&nums){
-  vector<int>dp(nums.size()+1,0);
-  dp[nums.size()-1]=1;
-  for(int i=nums.size()-2;i>=0;i--){
-  bool ans=false;
-  for(int jump=1;jump<=nums[i];jump++){
-    if(i+jump<nums.size())
-   ans=ans||dp[i+jump];
-  }
-  dp[i]=ans;
-  }
-  return dp[0];
-}
-bool canJump(vector<int>& nums) {
-    return solve(nums);
-}
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>arr(n,INT_MAX);
+        arr[0]=0;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(j+nums[j]>=i&&arr[j]!=INT_MAX)
+                arr[i]=min(arr[i],1+arr[j]);
+            }
+        }
+        return arr[n-1]!=INT_MAX;
+    }
 };
