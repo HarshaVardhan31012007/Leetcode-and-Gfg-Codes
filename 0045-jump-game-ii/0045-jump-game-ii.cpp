bbd1d2ae1869b1cdb167214b0f2de6c7f935dd0e
HARSHA VARDHAN <hv1900019@gmail.com>
2026-04-03 01:01:53 +0530

    Time: 0 ms (100%), Space: 20.4 MB (83.46%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int i,vector<int>&dp){
-    if(i==nums.size()-1) return 0;
-    if(i>=nums.size()) return INT_MAX;
-    if(dp[i]!=-1) return dp[i];
-    int ans=INT_MAX;
-    for(int jump=1;jump<=nums[i];jump++){
-    int recAns=solve(nums,i+jump,dp);
-    if(recAns!=INT_MAX)
-    ans=min(ans,1+recAns);
-    }
-    return dp[i]=ans;
-    }
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size()+1,-1);
-        return solve(nums,0,dp);
+        int n=nums.size();
+        int l=0,r=0;
+        int ans=0;
+        while(r<n-1){
+            int farthest=INT_MIN;
+            for(int k=l;k<=r;k++){
+                farthest=max(farthest,k+nums[k]);
+            }
+            l=r+1;
+            r=farthest;
+            ans++;
+        }
+        return ans;
     }
 };
