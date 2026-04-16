752da1681bf0a7b7e8b4681096e5b95741448584
HARSHA VARDHAN <hv1900019@gmail.com>
2026-04-16 23:24:01 +0530

    Time: 354 ms (15.92%), Space: 240.6 MB (7.46%) - LeetHub

---
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        unordered_map<int,int>mpp;
+        unordered_map<int,int>ans;
+        int n=nums.size();
+        for(int i=0;i<2*n;i++){
+            if(i>=n&&mpp.count(nums[i%n])&&(i%n)!=((mpp[nums[i%n]])%n)){
+                if(!ans.count(i%n))
+                ans[i%n]=i-mpp[nums[i%n]];
+                else
+                ans[i%n]=min(ans[i%n],i-mpp[nums[i%n]]);
+            }
+            mpp[nums[i%n]]=i;
+        }
+        mpp.clear();
+        for(int i=2*n-1;i>=0;i--){
+            if(i<n&&mpp.count(nums[i%n])&&(i%n)!=(mpp[nums[i%n]]%n)){
+                if(!ans.count(i%n))
+                ans[i%n]=mpp[i%n]-i;
+                else
+                ans[i%n]=min(ans[i%n],mpp[nums[i%n]]-i);
+            }
+            mpp[nums[i%n]]=i;
+        }
+
+        vector<int>ans1;
+        for(auto &each:queries){
+            int currans=(ans[each]==0?-1:ans[each]);
+            ans1.push_back(currans);
+        }
+        return ans1;
+    }
+};
