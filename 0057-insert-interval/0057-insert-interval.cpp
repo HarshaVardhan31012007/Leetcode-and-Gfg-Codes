fdcb6839e33a6254a0924142e7292851111ff2e7
HARSHA VARDHAN <hv1900019@gmail.com>
2026-04-20 17:17:37 +0530

    Time: 0 ms (100%), Space: 21.8 MB (20.36%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,31 +2,52 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
          int n=intervals.size();
-         int i=0;
-         int newS=newInterval[0];
-         int newE=newInterval[1];
-         vector<vector<int>>ans;
-         while(i<n){
-            if(intervals[i][0]>=newS){
-                break;
-            }
+        //  int i=0;
+        //  int newS=newInterval[0];
+        //  int newE=newInterval[1];
+        //  vector<vector<int>>ans;
+        //  while(i<n){
+        //     if(intervals[i][0]>=newS){
+        //         break;
+        //     }
+        //     ans.push_back(intervals[i]);
+        //     i++;
+        //  }
+        // if(!ans.empty()){
+        //     if(ans.back()[1]<newS)
+        //     ans.push_back({newS,newE});
+        //     else
+        //     ans.back()[1]=max(ans.back()[1],newE);
+        // }
+        // else{
+        //     ans.push_back(newInterval);
+        // }
+        // for(int j=i;j<n;j++){
+        //     if(ans.back()[1]<intervals[j][0])
+        //     ans.push_back(intervals[j]);
+        //     else
+        //     ans.back()[1]=max(ans.back()[1],intervals[j][1]);
+        // }
+        // return ans;
+
+
+
+
+        int i=0;
+        vector<vector<int>>ans;
+        while(i<n&&intervals[i][1]<newInterval[0]){
+             ans.push_back(intervals[i]);
+             i++;
+        }
+        while(i<n&&intervals[i][0]<=newInterval[1]){
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+            newInterval[1]=max(newInterval[1],intervals[i][1]);
+            i++;  
+        }
+        ans.push_back(newInterval);
+        while(i<n){
             ans.push_back(intervals[i]);
             i++;
-         }
-        if(!ans.empty()){
-            if(ans.back()[1]<newS)
-            ans.push_back({newS,newE});
-            else
-            ans.back()[1]=max(ans.back()[1],newE);
-        }
-        else{
-            ans.push_back(newInterval);
-        }
-        for(int j=i;j<n;j++){
-            if(ans.back()[1]<intervals[j][0])
-            ans.push_back(intervals[j]);
-            else
-            ans.back()[1]=max(ans.back()[1],intervals[j][1]);
         }
         return ans;
     }
