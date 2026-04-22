0bf659b703a1829015a321cbd26529b91e9f8c91
HARSHA VARDHAN <hv1900019@gmail.com>
2026-04-22 23:48:40 +0530

    Time: 6 ms (5.19%), Space: 8.1 MB (49.41%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int mini=0;
+        int maxi=0;
+        for(int i=0;i<s.length();i++){
+            cout<<s[i]<<endl;
+            if(s[i]=='('){
+                mini++;
+                maxi++;
+            }
+            else if(s[i]==')'){
+                mini--;
+                maxi--;
+            }
+            else{
+                mini=max(0,mini-1);
+                maxi=maxi+1;
+            }
+            mini=max(0,mini);
+            if(maxi<0) return false;
+        }
+        if(0>=mini){
+            return true;
+        }
+        return false;
+    }
+};
