2c834d751b23fa74bc64266f1c9c976bd08c5465
HARSHA VARDHAN <hv1900019@gmail.com>
2026-04-24 13:35:12 +0530

    Time: 8 ms (6.16%), Space: 25.8 MB (5.15%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+       vector<pair<int,int>>v;
+       for(int i=0;i<ratings.size();i++){
+          v.push_back({ratings[i],i});
+       }
+       sort(v.begin(),v.end());
+       vector<int>candies(ratings.size(),1);
+       int candie=ratings.size();
+       for(int i=0;i<v.size();i++){
+          if(v[i].second>0&&ratings[v[i].second]>ratings[v[i].second-1]){
+            int before=candies[v[i].second];
+            candies[v[i].second]=max(candies[v[i].second],candies[v[i].second-1]+1);
+            candie+=candies[v[i].second]-before;
+          }
+          if(v[i].second+1<ratings.size()&&ratings[v[i].second]>ratings[v[i].second+1]){
+            int before=candies[v[i].second];
+            candies[v[i].second]=max(candies[v[i].second],candies[v[i].second+1]+1);
+            candie+=candies[v[i].second]-before;
+          }
+       }
+       return candie;
+    }
+};
