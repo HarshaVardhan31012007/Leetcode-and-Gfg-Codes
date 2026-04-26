f4f2d4905e704c63f76f9f424ad6133a5c57d205
HARSHA VARDHAN <hv1900019@gmail.com>
2026-04-26 14:12:26 +0530

    Time: 0 ms (100%), Space: 23.1 MB (29.19%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
+    // int candy(vector<int>& ratings) {
+    //    vector<pair<int,int>>v;
+    //    for(int i=0;i<ratings.size();i++){
+    //       v.push_back({ratings[i],i});
+    //    }
+    //    sort(v.begin(),v.end());
+    //    vector<int>candies(ratings.size(),1);
+    //    int candie=ratings.size();
+    //    for(int i=0;i<v.size();i++){
+    //       if(v[i].second>0&&ratings[v[i].second]>ratings[v[i].second-1]){
+    //         int before=candies[v[i].second];
+    //         candies[v[i].second]=max(candies[v[i].second],candies[v[i].second-1]+1);
+    //         candie+=candies[v[i].second]-before;
+    //       }
+    //       if(v[i].second+1<ratings.size()&&ratings[v[i].second]>ratings[v[i].second+1]){
+    //         int before=candies[v[i].second];
+    //         candies[v[i].second]=max(candies[v[i].second],candies[v[i].second+1]+1);
+    //         candie+=candies[v[i].second]-before;
+    //       }
+    //    }
+    //    return candie;
+    // }
+
+
+
     int candy(vector<int>& ratings) {
-       vector<pair<int,int>>v;
-       for(int i=0;i<ratings.size();i++){
-          v.push_back({ratings[i],i});
-       }
-       sort(v.begin(),v.end());
        vector<int>candies(ratings.size(),1);
-       int candie=ratings.size();
-       for(int i=0;i<v.size();i++){
-          if(v[i].second>0&&ratings[v[i].second]>ratings[v[i].second-1]){
-            int before=candies[v[i].second];
-            candies[v[i].second]=max(candies[v[i].second],candies[v[i].second-1]+1);
-            candie+=candies[v[i].second]-before;
-          }
-          if(v[i].second+1<ratings.size()&&ratings[v[i].second]>ratings[v[i].second+1]){
-            int before=candies[v[i].second];
-            candies[v[i].second]=max(candies[v[i].second],candies[v[i].second+1]+1);
-            candie+=candies[v[i].second]-before;
-          }
+       for(int i=1;i<ratings.size();i++){
+          if(ratings[i]>ratings[i-1])
+          candies[i]=max(candies[i],candies[i-1]+1);
        }
-       return candie;
-    }
+       for(int i=ratings.size()-2;i>=0;i--){
+          if(ratings[i]>ratings[i+1])
+          candies[i]=max(candies[i],candies[i+1]+1);
+       }
+       return accumulate(candies.begin(),candies.end(),0);
+    }       
+    
 };
