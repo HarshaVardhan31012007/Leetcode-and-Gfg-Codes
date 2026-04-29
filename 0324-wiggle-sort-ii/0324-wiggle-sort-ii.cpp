2dc1b5972a87e1bb1c70626001514efa4f863b07
HARSHA VARDHAN <hv1900019@gmail.com>
2026-04-29 17:50:52 +0530

    Time: 0 ms (100%), Space: 21.3 MB (98.45%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -1,25 +1,58 @@
 class Solution {
 public:
+    //void wiggleSort(vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        // int n=nums.size();
+        // int j=n/2;
+        // if(n&1) j++;
+        // int init=j;
+        // int i=init-1;
+        // j=n-1;
+        // vector<int>arr;
+        // while(i>=0&&j>=init){
+        //     arr.push_back(nums[i]);
+        //     arr.push_back(nums[j]);
+        //     i--;
+        //     j--;
+        // }
+        // if(i>=0){
+        //     arr.push_back(nums[i]);
+        //     i--;
+        // }
+        // for(int i=0;i<n;i++)
+        // nums[i]=arr[i];
+    //}
+
+
+
+    int A(int i, int n) {
+        return (2 * i + 1) % (n | 1);
+    }
+
     void wiggleSort(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int j=n/2;
-        if(n&1) j++;
-        int init=j;
-        int i=init-1;
-        j=n-1;
-        vector<int>arr;
-        while(i>=0&&j>=init){
-            arr.push_back(nums[i]);
-            arr.push_back(nums[j]);
-            i--;
-            j--;
+        int n = nums.size();
+
+        // Step 1: Find median
+        auto midPtr = nums.begin() + n / 2;
+        nth_element(nums.begin(), midPtr, nums.end());
+        int median = *midPtr;
+
+        // Step 2: 3-way partition (Dutch National Flag)
+        int i = 0, j = 0, k = n - 1;
+
+        while (j <= k) {
+            if (nums[A(j, n)] > median) {
+                swap(nums[A(i, n)], nums[A(j, n)]);
+                i++;
+                j++;
+            } 
+            else if (nums[A(j, n)] < median) {
+                swap(nums[A(j, n)], nums[A(k, n)]);
+                k--;
+            } 
+            else {
+                j++;
+            }
         }
-        if(i>=0){
-            arr.push_back(nums[i]);
-            i--;
-        }
-        for(int i=0;i<n;i++)
-        nums[i]=arr[i];
     }
 };
