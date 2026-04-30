8cacf363c6ba9f8a848e17e145578a88d2e57a97
HARSHA VARDHAN <hv1900019@gmail.com>
2026-04-30 12:46:58 +0530

    Time: 0 ms (100%), Space: 11.4 MB (88.51%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -23,21 +23,41 @@ public:
         // return ans;
 
 
+        // int i=0;
+        // int j=0;
+        // int arr[256]={0};
+        // int n=s.length();
+        // int ans=0;
+        // while(j<n){
+        //     if(arr[s[j]]==0){
+        //         arr[s[j]]=1;
+        //     }
+        //     else{
+        //         while(s[i]!=s[j]){
+        //             arr[s[i]]=0;
+        //             i++;
+        //         }
+        //         i++;
+        //     }
+        //     ans=max(ans,j-i+1);
+        //     j++;
+        // }
+        // return ans;
+
+     
         int i=0;
         int j=0;
-        int arr[256]={0};
+        int arr[256];
+        for(int i=0;i<256;i++) arr[i]=-1;
         int n=s.length();
         int ans=0;
         while(j<n){
-            if(arr[s[j]]==0){
-                arr[s[j]]=1;
+            if(arr[s[j]]<i||arr[s[j]]==-1){
+                arr[s[j]]=j;
             }
             else{
-                while(s[i]!=s[j]){
-                    arr[s[i]]=0;
-                    i++;
-                }
-                i++;
+                i=arr[s[j]]+1;
+                arr[s[j]]=j;
             }
             ans=max(ans,j-i+1);
             j++;
