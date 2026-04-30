1e7e6609feff6f9b02c12a0552cdcddf51d5bd07
HARSHA VARDHAN <hv1900019@gmail.com>
2026-04-30 12:14:48 +0530

    Time: 3 ms (15.9%), Space: 46.2 MB (62.48%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
+        // int n=cardPoints.size();
+        // int len=n-k;
+        // int total=0;
+        // int minSum=INT_MAX;
+        // int sum=0;
+        // for(int i=0;i<len;i++){
+        //     sum+=cardPoints[i];
+        //     total+=cardPoints[i];
+        // }
+        // minSum=min(minSum,sum);
+        // for(int k=len;k<n;k++){
+        //     sum-=cardPoints[k-len];
+        //     sum+=cardPoints[k];
+        //     minSum=min(minSum,sum);
+        //     total+=cardPoints[k];
+        // }
+        // return total-minSum;
+
+
+
         int n=cardPoints.size();
-        int len=n-k;
-        int total=0;
-        int minSum=INT_MAX;
         int sum=0;
-        for(int i=0;i<len;i++){
-            sum+=cardPoints[i];
-            total+=cardPoints[i];
+        int i=0;
+        for(;i<k;i++)
+        sum+=cardPoints[i];
+        int maxSum=sum;
+        i--;
+        int j=n-1;
+        while(j>=n-k){
+            sum-=cardPoints[i--];
+            sum+=cardPoints[j--];
+            maxSum=max(maxSum,sum);
         }
-        minSum=min(minSum,sum);
-        for(int k=len;k<n;k++){
-            sum-=cardPoints[k-len];
-            sum+=cardPoints[k];
-            minSum=min(minSum,sum);
-            total+=cardPoints[k];
-        }
-        return total-minSum;
+        return maxSum;
     }
 };
