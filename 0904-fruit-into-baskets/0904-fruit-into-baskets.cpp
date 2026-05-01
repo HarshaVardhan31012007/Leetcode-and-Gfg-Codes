df3890000a271559a9a8d4375abd5f54debe324b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-01 17:05:40 +0530

    Time: 2 ms (97.4%), Space: 79.7 MB (97.07%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -112,24 +112,79 @@ public:
 
 
 
-    unordered_map<int,int>freq;
-       int i=0;
-       int j=0;
-       int n=fruits.size();int c=0;
-       int ans=0;
-       while(j<n){
-          freq[fruits[j]]++;
-          c++;
-          if(freq.size()>2){
-             freq[fruits[i]]--;
-             c--;
-             if(freq[fruits[i]]==0) freq.erase(fruits[i]);
-             i++;
-          } 
-          if(freq.size()<=2)
-          ans=max(ans,c);
-          j++;
-       }
-       return ans;
+    // unordered_map<int,int>freq;
+    //    int i=0;
+    //    int j=0;
+    //    int n=fruits.size();int c=0;
+    //    int ans=0;
+    //    while(j<n){
+    //       freq[fruits[j]]++;
+    //       c++;
+    //       if(freq.size()>2){
+    //          freq[fruits[i]]--;
+    //          c--;
+    //          if(freq[fruits[i]]==0) freq.erase(fruits[i]);
+    //          i++;
+    //       } 
+    //       if(freq.size()<=2)
+    //       ans=max(ans,c);
+    //       j++;
+    //    }
+    //    return ans;
+
+
+
+    // unordered_map<int,int>freq;
+    //    int i=0;
+    //    int j=0;
+    //    int n=fruits.size();
+    //    int ans=0;
+    //    while(j<n){
+    //       freq[fruits[j]]++;
+    //       if(freq.size()>2){
+    //          freq[fruits[i]]--;
+    //          if(freq[fruits[i]]==0) freq.erase(fruits[i]);
+    //          i++;
+    //       } 
+    //       if(freq.size()<=2)
+    //       ans=max(ans,j-i+1);
+    //       j++;
+    //    }
+    //    return ans;
+
+
+
+        int basket1=-1;int basket2=-1;
+        int c1=0;int c2=0;
+        int n=fruits.size();
+        int i=0;
+        int j=0;
+        int ans=0;
+        while(j<n){
+            if(fruits[j]==basket1){
+                c1++;
+            }
+            else if(fruits[j]==basket2){
+                c2++;
+            }
+            else{
+                while(c1!=0&&c2!=0){
+                    if(fruits[i]==basket1) c1--;
+                    else if(fruits[i]==basket2) c2--;
+                    i++;
+                }
+                if(c1==0){
+                    basket1=fruits[j];
+                    c1=1;
+                }
+                else if(c2==0){
+                    basket2=fruits[j];
+                    c2=1;
+                }
+            }
+            ans=max(ans,c1+c2);
+            j++;
+        }
+        return ans;
     }
 };
