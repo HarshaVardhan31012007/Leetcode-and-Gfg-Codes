d51f131ef7b5292ce7cb6bd6060945ec3047fefe
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-01 18:54:01 +0530

    Time: 6 ms (69.08%), Space: 11 MB (80.58%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,17 +1,44 @@
 class Solution {
 public:
+    // int solve(string &s,int k){
+    //     int i=0;
+    //     int j=0;
+    //     int n=s.length();
+    //     unordered_map<int,int>mpp;
+    //     int count=0;
+    //     while(j<n){
+    //        mpp[s[j]]++;
+    //        while(mpp.size()>k){
+    //          mpp[s[i]]--;
+    //          if(mpp[s[i]]==0) mpp.erase(s[i]);
+    //          i++;
+    //        }
+    //        count+=(j-i+1);
+    //        j++;
+    //     }
+    //     return count;
+    // }
+    // int numberOfSubstrings(string s) {
+    //     return solve(s,3)-solve(s,2);
+    // }
+
+
+
     int solve(string &s,int k){
         int i=0;
         int j=0;
         int n=s.length();
-        unordered_map<int,int>mpp;
+        int a=0;int b=0;int c=0;
         int count=0;
         while(j<n){
-           mpp[s[j]]++;
-           while(mpp.size()>k){
-             mpp[s[i]]--;
-             if(mpp[s[i]]==0) mpp.erase(s[i]);
-             i++;
+           if(s[j]=='a') a++;
+           else if(s[j]=='b') b++;
+           else if(s[j]=='c') c++;
+           while(a!=0&&b!=0&&c!=0){
+              if(s[i]=='a') a--;
+              else if(s[i]=='b') b--;
+              else if(s[i]=='c') c--;
+              i++;
            }
            count+=(j-i+1);
            j++;
@@ -19,6 +46,7 @@ public:
         return count;
     }
     int numberOfSubstrings(string s) {
-        return solve(s,3)-solve(s,2);
+        int n=s.length();
+        return (1LL*n*(n+1))/2-solve(s,2);
     }
 };
