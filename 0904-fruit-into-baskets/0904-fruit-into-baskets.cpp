35f938c18dda451a58ff62980e22f5a995edf3ed
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-01 13:02:47 +0530

    Time: 39 ms (73.1%), Space: 84.9 MB (34.44%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -36,54 +36,78 @@ public:
 
 
 
-        int basket1=-1;int basket2=-1;
-        int c1=0;int c2=0;
-        int lastpos1=-1;int lastpos2=-1;
-        int n=fruits.size();
-        int i=0;
-        int j=0;
-        int ans=0;
-        unordered_map<int,unordered_map<int,int>>freq;
-        while(j<n){
-            freq[j][basket1]=c1;
-            freq[j][basket2]=c2;
-            if(fruits[j]==basket1){
-                c1++;
-                lastpos1=j;
-            }
-            else if(fruits[j]==basket2){
-                c2++;
-                lastpos2=j;
-            }
-            else{
-                if(lastpos1==-1){
-                    basket1=fruits[j];
-                    c1=1;
-                    lastpos1=j;
-                }
-                else if(lastpos2==-1){
-                    basket2=fruits[j];
-                    c2=1;
-                    lastpos2=j;
-                }
-                else if(lastpos1<lastpos2){
-                    i=lastpos1+1;
-                    basket1=fruits[j];
-                    c1=1;
-                    c2-=freq[i][basket2];
-                    lastpos1=j;
-                }
-                else{
-                    i=lastpos2+1;
-                    basket2=fruits[j];
-                    c2=1;
-                    c1-=freq[i][basket1];
-                    lastpos2=j;
-                }
-            }
-            ans=max(ans,c1+c2);
-            j++;
-        }
-        return ans;
+        // int basket1=-1;int basket2=-1;
+        // int c1=0;int c2=0;
+        // int lastpos1=-1;int lastpos2=-1;
+        // int n=fruits.size();
+        // int i=0;
+        // int j=0;
+        // int ans=0;
+        // unordered_map<int,unordered_map<int,int>>freq;
+        // while(j<n){
+        //     freq[j][basket1]=c1;
+        //     freq[j][basket2]=c2;
+        //     if(fruits[j]==basket1){
+        //         c1++;
+        //         lastpos1=j;
+        //     }
+        //     else if(fruits[j]==basket2){
+        //         c2++;
+        //         lastpos2=j;
+        //     }
+        //     else{
+        //         if(lastpos1==-1){
+        //             basket1=fruits[j];
+        //             c1=1;
+        //             lastpos1=j;
+        //         }
+        //         else if(lastpos2==-1){
+        //             basket2=fruits[j];
+        //             c2=1;
+        //             lastpos2=j;
+        //         }
+        //         else if(lastpos1<lastpos2){
+        //             i=lastpos1+1;
+        //             basket1=fruits[j];
+        //             c1=1;
+        //             c2-=freq[i][basket2];
+        //             lastpos1=j;
+        //         }
+        //         else{
+        //             i=lastpos2+1;
+        //             basket2=fruits[j];
+        //             c2=1;
+        //             c1-=freq[i][basket1];
+        //             lastpos2=j;
+        //         }
+        //     }
+        //     ans=max(ans,c1+c2);
+        //     j++;
+        // }
+        // return ans;
+
+
+
+
+
+       unordered_map<int,int>freq;
+       int i=0;
+       int j=0;
+       int n=fruits.size();
+       int ans=0;
+       while(j<n){
+          freq[fruits[j]]++;
+          while(freq.size()>2){
+             freq[fruits[i]]--;
+             if(freq[fruits[i]]==0) freq.erase(fruits[i]);
+             i++;
+          } 
+          int c=0;
+          for(auto &each:freq)
+          c+=each.second;
+          ans=max(ans,c);
+          j++;
+       }
+       return ans;
     }
 };
