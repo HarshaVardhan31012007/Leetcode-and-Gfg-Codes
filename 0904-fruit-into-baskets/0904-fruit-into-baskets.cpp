e46e599d2961c403dc08eedc73cc05d21585d668
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-01 12:47:55 +0530

    Time: 675 ms (5.07%), Space: 404.4 MB (8.32%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,32 +1,84 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        // int basket1=-1;int basket2=-1;
+        // int c1=0;int c2=0;
+        // int n=fruits.size();
+        // int i=0;
+        // int j=0;
+        // int ans=0;
+        // while(j<n){
+        //     if(fruits[j]==basket1){
+        //         c1++;
+        //     }
+        //     else if(fruits[j]==basket2){
+        //         c2++;
+        //     }
+        //     else{
+        //         while(c1!=0&&c2!=0){
+        //             if(fruits[i]==basket1) c1--;
+        //             else if(fruits[i]==basket2) c2--;
+        //             i++;
+        //         }
+        //         if(c1==0){
+        //             basket1=fruits[j];
+        //             c1=1;
+        //         }
+        //         else if(c2==0){
+        //             basket2=fruits[j];
+        //             c2=1;
+        //         }
+        //     }
+        //     ans=max(ans,c1+c2);
+        //     j++;
+        // }
+        // return ans;
+
+
+
         int basket1=-1;int basket2=-1;
         int c1=0;int c2=0;
+        int lastpos1=-1;int lastpos2=-1;
         int n=fruits.size();
         int i=0;
         int j=0;
         int ans=0;
+        unordered_map<int,unordered_map<int,int>>freq;
         while(j<n){
+            freq[j][basket1]=c1;
+            freq[j][basket2]=c2;
             if(fruits[j]==basket1){
                 c1++;
+                lastpos1=j;
             }
             else if(fruits[j]==basket2){
                 c2++;
+                lastpos2=j;
             }
             else{
-                while(c1!=0&&c2!=0){
-                    if(fruits[i]==basket1) c1--;
-                    else if(fruits[i]==basket2) c2--;
-                    i++;
-                }
-                if(c1==0){
+                if(lastpos1==-1){
                     basket1=fruits[j];
                     c1=1;
+                    lastpos1=j;
                 }
-                else if(c2==0){
+                else if(lastpos2==-1){
                     basket2=fruits[j];
                     c2=1;
+                    lastpos2=j;
+                }
+                else if(lastpos1<lastpos2){
+                    i=lastpos1+1;
+                    basket1=fruits[j];
+                    c1=1;
+                    c2-=freq[i][basket2];
+                    lastpos1=j;
+                }
+                else{
+                    i=lastpos2+1;
+                    basket2=fruits[j];
+                    c2=1;
+                    c1-=freq[i][basket1];
+                    lastpos2=j;
                 }
             }
             ans=max(ans,c1+c2);
