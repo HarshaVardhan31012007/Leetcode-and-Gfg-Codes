e47c7eefcd83a68115ae9d69a47985826eb971df
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-01 19:09:42 +0530

    Time: 2 ms (90.81%), Space: 10.9 MB (94.85%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -24,29 +24,47 @@ public:
 
 
 
-    int solve(string &s,int k){
-        int i=0;
-        int j=0;
-        int n=s.length();
-        int a=0;int b=0;int c=0;
-        int count=0;
-        while(j<n){
-           if(s[j]=='a') a++;
-           else if(s[j]=='b') b++;
-           else if(s[j]=='c') c++;
-           while(a!=0&&b!=0&&c!=0){
-              if(s[i]=='a') a--;
-              else if(s[i]=='b') b--;
-              else if(s[i]=='c') c--;
-              i++;
-           }
-           count+=(j-i+1);
-           j++;
-        }
-        return count;
-    }
+    // int solve(string &s,int k){
+    //     int i=0;
+    //     int j=0;
+    //     int n=s.length();
+    //     int a=0;int b=0;int c=0;
+    //     int count=0;
+    //     while(j<n){
+    //        if(s[j]=='a') a++;
+    //        else if(s[j]=='b') b++;
+    //        else if(s[j]=='c') c++;
+    //        while(a!=0&&b!=0&&c!=0){
+    //           if(s[i]=='a') a--;
+    //           else if(s[i]=='b') b--;
+    //           else if(s[i]=='c') c--;
+    //           i++;
+    //        }
+    //        count+=(j-i+1);
+    //        j++;
+    //     }
+    //     return count;
+    // }
+    // int numberOfSubstrings(string s) {
+    //     int n=s.length();
+    //     return (1LL*n*(n+1))/2-solve(s,2);
+    // }
+
+
+
+     
     int numberOfSubstrings(string s) {
         int n=s.length();
-        return (1LL*n*(n+1))/2-solve(s,2);
+        int a=-1;int b=-1;int c=-1;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='a') a=i;
+            if(s[i]=='b') b=i;
+            if(s[i]=='c') c=i;
+            if(a!=-1&&b!=-1&&c!=-1){
+                ans+=min({a,b,c})+1;
+            }
+        }
+        return ans;
     }
 };
