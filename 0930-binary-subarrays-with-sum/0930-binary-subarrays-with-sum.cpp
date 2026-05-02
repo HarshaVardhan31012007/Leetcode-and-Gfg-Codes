0e0065a58b8240e55cee49a1838da35d1742b51f
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-02 22:30:44 +0530

    Time: 4 ms (46.59%), Space: 32.7 MB (40.65%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -25,8 +25,32 @@ public:
 
 
 
-    int solve(vector<int>&nums,int t){
-        if(t<0) return 0;
+    // int solve(vector<int>&nums,int t){
+    //     if(t<0) return 0;
+    //      int sum=0;
+    //      int i=0;
+    //      int j=0;
+    //      int n=nums.size();
+    //      int ans=0;
+    //      while(j<n){
+    //         sum+=nums[j];
+    //         while(sum>t){
+    //            sum-=nums[i];
+    //            i++;
+    //         }
+    //         ans+=(j-i+1);
+    //         j++;
+    //      }
+    //      return ans;
+    // }
+    // int numSubarraysWithSum(vector<int>& nums, int goal) {
+    //     return solve(nums,goal)-solve(nums,goal-1);
+    // }
+
+
+
+    
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
          int sum=0;
          int i=0;
          int j=0;
@@ -34,16 +58,31 @@ public:
          int ans=0;
          while(j<n){
             sum+=nums[j];
-            while(sum>t){
+            while(sum>goal){
                sum-=nums[i];
                i++;
             }
-            ans+=(j-i+1);
+            if(sum==goal){
+                int c=1;int start=i;
+                while(i<n&&nums[i]==0){
+                    c++;i++;
+                }
+                int c1=1;
+                j++;
+                while(j<n&&nums[j]==0){
+                    j++;
+                    c1++;
+                }
+                if(i<j)
+                ans+=c*c1;
+                else{
+                    int len=j-start;
+                    ans+=(len*(len+1))/2;
+                }
+            }
+            else
             j++;
          }
          return ans;
-    }
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return solve(nums,goal)-solve(nums,goal-1);
     }
 };
