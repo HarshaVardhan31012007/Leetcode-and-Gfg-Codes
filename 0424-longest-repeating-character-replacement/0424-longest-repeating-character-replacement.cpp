e1d7e80acb74603ff37c983c994c95cdf04e2e2b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-02 16:00:22 +0530

    Time: 3 ms (66.42%), Space: 10.8 MB (92.47%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,28 +1,53 @@
 class Solution {
 public:
-    int solve(string &s,int k,char ch){
+    // int solve(string &s,int k,char ch){
+    //     int n=s.length();
+    //     int i=0;
+    //     int j=0;
+    //     int changes=0;
+    //     int ans=0;
+    //     while(j<n){
+    //         if(s[j]!=ch) changes++;
+    //         if(changes>k){
+    //             if(s[i]!=ch) changes--;
+    //             i++;
+    //         }
+    //         if(changes<=k)
+    //         ans=max(ans,j-i+1);
+    //         j++;
+    //     }
+    //     return ans;
+    // }
+    // int characterReplacement(string s, int k) {
+    //     int ans=0;
+    //     for(int i=0;i<26;i++){
+    //         char ch=(i+'A');
+    //         ans=max(ans,solve(s,k,ch));
+    //     }
+    //     return ans;
+    // }
+
+
+    int characterReplacement(string s, int k) {
         int n=s.length();
+        int freq[26]={0};
         int i=0;
         int j=0;
-        int changes=0;
+        int maxfreq=0;
         int ans=0;
         while(j<n){
-            if(s[j]!=ch) changes++;
-            if(changes>k){
-                if(s[i]!=ch) changes--;
+            freq[s[j]-'A']++;
+            maxfreq=max(maxfreq,freq[s[j]-'A']);
+            while(((j-i+1)-maxfreq)>k){
+                freq[s[i]-'A']--;
+                maxfreq=0;
+                for(int k=0;k<26;k++)
+                maxfreq=max(maxfreq,freq[k]);
                 i++;
             }
-            if(changes<=k)
+            if(((j-i+1)-maxfreq)<=k)
             ans=max(ans,j-i+1);
             j++;
-        }
-        return ans;
-    }
-    int characterReplacement(string s, int k) {
-        int ans=0;
-        for(int i=0;i<26;i++){
-            char ch=(i+'A');
-            ans=max(ans,solve(s,k,ch));
         }
         return ans;
     }
