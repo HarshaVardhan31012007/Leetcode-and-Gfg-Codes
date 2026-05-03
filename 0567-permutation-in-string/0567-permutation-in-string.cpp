a87d5eb130ab0f06ab43ddc63c4e74cab32428e8
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-03 18:18:38 +0530

    Time: 0 ms (100%), Space: 9.8 MB (88.34%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool check(int arr[]){
-        for(int i=0;i<26;i++){
-            if(arr[i]!=0)
-            return false;
-        }
-        return true;
-    }
     bool checkInclusion(string s1, string s2) {
         if(s1.length()>s2.length()) return false;
-       int arr[26]={0};
-       for(int i=0;i<s1.length();i++)
-       arr[s1[i]-'a']++;
-       int i=0;int j=s1.length()-1;
-       for(int p=i;p<=j;p++)
-       arr[s2[p]-'a']--;
-       if(check(arr)) return true;
-       j++;
-       while(j<s2.length()){
-         arr[s2[i++]-'a']++;
-         arr[s2[j++]-'a']--;
-         if(check(arr)) return true;
-       }
-       return false;
+        int m=s1.length();
+        int n=s2.length();
+        int freq[26]={0};
+        for(auto &each:s1)
+        freq[each-'a']++;
+        int count=m;
+        for(int j=0;j<m;j++){
+            if(freq[s2[j]-'a']>0) count--;
+            freq[s2[j]-'a']--;
+        }
+        if(count==0)
+        return true;
+        for(int k=m;k<n;k++){
+            freq[s2[k-m]-'a']++;
+            if(freq[s2[k-m]-'a']>0) count++;
+            if(freq[s2[k]-'a']>0) count--;
+            freq[s2[k]-'a']--;
+            if(count==0)
+            return true;
+        }
+        return false;
     }
 };
