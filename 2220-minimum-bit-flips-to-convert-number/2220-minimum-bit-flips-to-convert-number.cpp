d696489bd0047bf18794c7b463e8f5ac37f23b1f
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-05 17:16:47 +0530

    Time: 0 ms (100%), Space: 8.1 MB (26.3%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int ans=0;
-        while(start>0||goal>0){
-            int a=(start>0?start%2:0);
-            int b=(goal>0?goal%2:0);
-            if(a!=b) ans++;
-            if(start>0)
-            start=start/2;
-            if(goal>0)
-            goal=goal/2;
-        }
-        return ans;
+        // int ans=0;
+        // while(start>0||goal>0){
+        //     int a=(start>0?start%2:0);
+        //     int b=(goal>0?goal%2:0);
+        //     if(a!=b) ans++;
+        //     if(start>0)
+        //     start=start/2;
+        //     if(goal>0)
+        //     goal=goal/2;
+        // }
+        // return ans;
+
+
+
+        int x=start^goal;
+        int count=__builtin_popcount(x);
+        return count;
     }
 };
