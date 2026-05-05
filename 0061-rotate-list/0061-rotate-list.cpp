6177fd475b7eda7380e8b9303e365bb7b6d2a3e9
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-05 21:26:59 +0530

    Time: 0 ms (100%), Space: 16.5 MB (31.2%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,106 +10,31 @@
  */
 class Solution {
 public:
-    // ListNode* reverseLL(ListNode* prev,ListNode* curr,int &len){
-    //      while(curr!=NULL){
-    //         len++;
-    //         ListNode* forward=curr->next;
-    //         curr->next=prev;
-    //         prev=curr;
-    //         curr=forward;
-    //     }
-    //     return prev;
-    // }
-    // ListNode* rotateRight(ListNode* head, int k) {
-    //     if(!head) return head;
-    //     ListNode* prev=NULL;
-    //     ListNode* curr=head;
-    //     int len=0;
-    //     curr=reverseLL(prev,curr,len);
-    //     prev=NULL;
-    //     k=k%len;
-    //     if(k==0){
-    //        return reverseLL(prev,curr,len); 
-    //     }
-    //     head=curr;
-    //     for(int i=0;i<k;i++){
-    //         ListNode* forward=curr->next;
-    //         curr->next=prev;
-    //         prev=curr;
-    //         curr=forward;
-    //     }
-    //     ListNode* a=NULL;
-    //     ListNode* b=curr;
-    //     len=0;
-    //     head->next=reverseLL(a,b,len);
-    //     return prev;
-    // }
-
-
-
-    //  ListNode* reverseLL(ListNode* prev,ListNode* curr,int &len){
-    //      while(curr!=NULL){
-    //         len++;
-    //         ListNode* forward=curr->next;
-    //         curr->next=prev;
-    //         prev=curr;
-    //         curr=forward;
-    //     }
-    //     return prev;
-    // }
-    // ListNode* rotateRight(ListNode* head, int k) {
-    //     if(!head) return head;
-    //     ListNode* prev=NULL;
-    //     ListNode* curr=head;
-    //     int len=0;
-    //     curr=reverseLL(prev,curr,len);
-    //     prev=NULL;
-    //     k=k%len;
-    //     head=curr;
-    //     for(int i=0;i<k;i++){
-    //         ListNode* forward=curr->next;
-    //         curr->next=prev;
-    //         prev=curr;
-    //         curr=forward;
-    //     }
-    //     ListNode* a=NULL;
-    //     ListNode* b=curr;
-    //     len=0;
-    //     if(k!=0)
-    //     head->next=reverseLL(a,b,len);
-    //     else
-    //     return reverseLL(a,b,len);
-    //     return prev;
-    // }
-
-
-    int getLength(ListNode* head){
-        int len=0;
+    int numberOfNodes(ListNode* head){
+        int c=0;
         while(head!=NULL){
-            len++;
-            head=head->next;
+           c++;
+           head=head->next;
         }
-        return len;
+        return c;
     }
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head) return head;// len=0 then k%len is not valid //here rotate same we get null
-        int len=getLength(head);
+        if(!head||!head->next) return head;
+        int len=numberOfNodes(head);
         k=k%len;
-        if(k==0)
+        if(k==0) return head;
+        ListNode* temp=head;
+        int skip=len-k;
+        ListNode* prev=NULL;
+        while(skip--){
+           prev=head;
+           head=head->next;
+        }
+        ListNode* temp1=head;
+        prev->next=NULL;
+        while(temp1->next)
+        temp1=temp1->next;
+        temp1->next=temp;
         return head;
-        int moves=len-k-1;
-        ListNode* newHead=NULL;
-        ListNode* it=head;
-        for(int i=0;i<moves;i++){
-           it=it->next;
-        }
-        newHead=it->next;
-        it->next=NULL;
-        it=newHead;
-        while(it->next){
-            it=it->next;
-        }
-        it->next=head;
-        return newHead;
     }
 };
