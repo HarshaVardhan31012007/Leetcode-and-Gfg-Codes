5573ce427e0b3ba2eaadc106bc2037a73e3b86c2
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-05 21:48:26 +0530

    Time: 0 ms (100%), Space: 16.2 MB (99.91%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,31 +10,60 @@
  */
 class Solution {
 public:
-    int numberOfNodes(ListNode* head){
-        int c=0;
-        while(head!=NULL){
-           c++;
-           head=head->next;
+    // int numberOfNodes(ListNode* head){
+    //     int c=0;
+    //     while(head!=NULL){
+    //        c++;
+    //        head=head->next;
+    //     }
+    //     return c;
+    // }
+    // ListNode* rotateRight(ListNode* head, int k) {
+    //     if(!head||!head->next) return head;
+    //     int len=numberOfNodes(head);
+    //     k=k%len;
+    //     if(k==0) return head;
+    //     ListNode* temp=head;
+    //     int skip=len-k;
+    //     ListNode* prev=NULL;
+    //     while(skip--){
+    //        prev=head;
+    //        head=head->next;
+    //     }
+    //     ListNode* temp1=head;
+    //     prev->next=NULL;
+    //     while(temp1->next)
+    //     temp1=temp1->next;
+    //     temp1->next=temp;
+    //     return head;
+    // }
+
+
+
+    void solve(ListNode* head,ListNode* &node,ListNode* &lastNode,int &k,int &len){
+        if(!head) return;
+        len++;
+        if(head&&!head->next){
+            lastNode=head;
+            k=k%len;
         }
-        return c;
+        solve(head->next,node,lastNode,k,len);
+        if(k==0&&head->next){
+            node=head;
+        }
+        k--;
     }
     ListNode* rotateRight(ListNode* head, int k) {
         if(!head||!head->next) return head;
-        int len=numberOfNodes(head);
-        k=k%len;
-        if(k==0) return head;
-        ListNode* temp=head;
-        int skip=len-k;
-        ListNode* prev=NULL;
-        while(skip--){
-           prev=head;
-           head=head->next;
-        }
-        ListNode* temp1=head;
-        prev->next=NULL;
-        while(temp1->next)
-        temp1=temp1->next;
-        temp1->next=temp;
+        ListNode* node=NULL;
+        ListNode* lastNode=NULL;
+        int len=0;
+        solve(head,node,lastNode,k,len);
+        if(!node) return head;
+        ListNode* temp=node->next;
+        node->next=NULL;
+        lastNode->next=head;
+        head=temp;
         return head;
     }
 };
