8e916d89c76226de3620e1e92e276abc5c3b9666
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-05 19:02:01 +0530

    Time: 6 ms (89.75%), Space: 22.7 MB (98.39%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.cpp
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-        int n=nums.size();
+        // int n=nums.size();
+        // int ans=0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         int count=__builtin_popcount(nums[i]^nums[j]);
+        //         ans+=count;
+        //     }
+        // }
+        // return ans;
+
+
         int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int count=__builtin_popcount(nums[i]^nums[j]);
-                ans+=count;
+        for(int i=0;i<32;i++){
+            int perBit=0;
+            for(auto x:nums){
+                perBit+=((x>>i)&1);
             }
+            ans+=perBit*(nums.size()-perBit);
         }
         return ans;
     }
