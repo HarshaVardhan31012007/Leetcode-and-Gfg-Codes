6b862789c289b2e3afadaa81a26838637f73c5f4
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-05 11:16:42 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.2%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
-    int divide(long long int dividend,long long int divisor) {
-        long long int a=dividend;
-        long long int b=divisor;
-        divisor=abs(divisor);
-        dividend=abs(dividend);
-        long long int count=0;
-        while(dividend>=divisor){
-            long long int temp=1;
-            while(temp*divisor<=dividend){
-                temp=(temp<<1);
+    // int divide(long long int dividend,long long int divisor) {
+    //     long long int a=dividend;
+    //     long long int b=divisor;
+    //     divisor=abs(divisor);
+    //     dividend=abs(dividend);
+    //     long long int count=0;
+    //     while(dividend>=divisor){
+    //         long long int temp=1;
+    //         while(temp*divisor<=dividend){
+    //             temp=(temp<<1);
+    //         }
+    //         temp=temp>>1;
+    //         count+=temp;
+    //         dividend-=temp*divisor;
+    //     }
+    //     if((a^b)<0) count=-count;
+    //     if(count>INT_MAX) return INT_MAX;
+    //     if(count<INT_MIN) return INT_MIN;
+    //     return count;
+    // }
+
+
+    int divide(int dividend,int divisor) {
+        if(dividend==divisor) return 1;
+        if(divisor==1) return dividend;
+        if(dividend==INT_MIN&&divisor==-1) return INT_MAX;
+        long a=dividend;
+        long b=divisor;
+        bool isPositive=((a^b)>=0);
+        a=abs(a);
+        b=abs(b);
+        long ans=0;
+        while(a>=b){
+            long count=0;
+            while(a>=(b<<(count+1))){
+                count++;
             }
-            temp=temp>>1;
-            count+=temp;
-            dividend-=temp*divisor;
+            ans+=(1<<count);
+            a-=(b<<(count));
         }
-        if((a^b)<0) count=-count;
-        if(count>INT_MAX) return INT_MAX;
-        if(count<INT_MIN) return INT_MIN;
-        return count;
+        return isPositive?ans:-ans;
     }
 };
