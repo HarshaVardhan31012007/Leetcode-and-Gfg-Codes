760858490fb1770f3e599d5cf513148cea825a22
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-06 18:17:30 +0530

    Time: 0 ms (100%), Space: 8.2 MB (6.2%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        // if(n==0) return 1;
-        // long long pos=log2(n);
-        // pos++;
-        // long long temp=(1<<pos);
-        // temp--;
-        // return (~n)&temp;
-
         if(n==0) return 1;
-        int mask=0;
-        int m=n;
-        while(m!=0){
-            mask=(mask<<1)|1;
-            m=m>>1;
-        }
-        return ~n&mask;
+        return ~n&(~(-1<<((int)(log2(n)))));
     }
 };
