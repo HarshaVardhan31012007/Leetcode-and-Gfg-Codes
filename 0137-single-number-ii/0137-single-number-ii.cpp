d6325f687675171fe24bf918b9af1fdb16dca6c9
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-06 17:29:45 +0530

    Time: 3 ms (31.41%), Space: 13.4 MB (64.06%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -8,12 +8,33 @@ public:
         // }
 
 
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(i>0&&nums[i]==nums[i-1]) continue;
-            if(i+1<nums.size()&&nums[i]==nums[i+1]) continue;
-            return nums[i];
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size();i++){
+        //     if(i>0&&nums[i]==nums[i-1]) continue;
+        //     if(i+1<nums.size()&&nums[i]==nums[i+1]) continue;
+        //     return nums[i];
+        // }
+        // return -1;
+
+
+        int ans=0;
+        for(int i=0;i<32;i++){
+            int c=0;
+            int c1=0;
+            for(auto &each:nums){
+                if((each>>i)&1){
+                    c++;
+                }
+                else{
+                    c1++;
+                }
+            }
+            c=c%3;
+            c1=c1%3;
+            if(c!=0){
+               ans=(ans|(1<<i));
+            }
         }
-        return -1;
+        return ans;
     }
 };
