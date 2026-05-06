dac21ae4120a341230929aaf7f0163337e141e41
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-06 12:50:55 +0530

    Time: 0 ms (100%), Space: 14.1 MB (30.16%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int val=0;
+        // int val=0;
+        // for(auto &each:nums){
+        //     val=(val^each);
+        // }
+        // int bit=-1;
+        // for(int i=0;i<32;i++){
+        //     if((val>>i)&1){
+        //        bit=i;
+        //        break;
+        //     }
+        // }
+        // int a=0;
+        // int b=0;
+        // for(auto &each:nums){
+        //     if((each>>bit)&1){
+        //         a=(a^each);
+        //     }
+        //     else{
+        //         b=(b^each);
+        //     }
+        // }
+        // return {a,b};
+
+
+
+        long long int val=0;
         for(auto &each:nums){
             val=(val^each);
         }
-        int bit=-1;
-        for(int i=0;i<32;i++){
-            if((val>>i)&1){
-               bit=i;
-               break;
-            }
-        }
+       // val=(val&(-val));
+        val=((val&(val-1))^val);
         int a=0;
         int b=0;
         for(auto &each:nums){
-            if((each>>bit)&1){
+            if(val&each){
                 a=(a^each);
             }
             else{
