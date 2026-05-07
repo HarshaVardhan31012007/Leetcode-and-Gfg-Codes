8737c699008b0a4a46d6cefdd54076281a2264cd
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-07 19:58:28 +0530

    Time: 0 ms (100%), Space: 8.6 MB (42.89%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,97 +1,33 @@
 class Solution {
 public:
-    double solve(double x,int n){
-        if(n==0) return 1;
-        if(n&1)
-        return x*solve(x*x,n/2);
-        else
-        return solve(x*x,n/2);
-    }
     double myPow(double x, int n) {
-        // double product=1;
-        // //cases for decreasing time
         // if(n==0) return 1.0;
-        // if(x==0) return 0.0;
+        // if(x==0.0) return 0.0;
         // if(x==1.0) return 1.0;
-        // if(x==-1.0&&n%2==0) return 1.0;
-        // if(x==-1.0&&n%2!=0) return -1.0;
-        // //edge case// store n in long variable to avoid this edge case
-        //  if(n==INT_MIN){
-        //   if(x==1.0||x==-1.0)
-        //   return 1.0;
-        //   else
-        //   return 0.0;
-        //  }
-        //  //this has to be included to dec time if above cases are not included
-        // //  if(n==INT_MAX&&(x==1.0||x==-1.0)){
-        // //     return x;
-        // //  }
-        // int N=abs(n);
-        // if(x==1.0)
-        // return 1.0;
-        // for(int i=0;i<N;i++){
-        // product=product*x;
-        // }
-        // return (n>0)?product:1/product;
-
-        //method 2
-        //cases for decreasing time
-        // if(n==0) return 1.0;
         // if(n==1) return x;
-        // if(x==0) return 0.0;
-        // if(x==1.0) return 1.0;
-        // if(x==-1.0&&n%2==0) return 1.0;
-        // if(x==-1.0&&n%2!=0) return -1.0;
-        
-        // long newN=n;
-        // if(newN<0){
-        //     x=1/x;
-        //     newN=-newN;
+        // double ans=x;
+        // bool isneg=(n<0);
+        // long n1=abs((long long)n);
+        // for(int i=1;i<n1;i++){
+        //     ans=(ans*x);
         // }
-        // double ans=1;
-        // while(newN>0){
-        //     if(newN%2==1){
-        //         ans=ans*x;
-        //     }
-        //     x*=x;
-        //     newN=newN/2;
-        // }
-        // return ans;
+        // return isneg?1/(double)ans:ans;
 
 
-        // if(n==0) return 1.0;
-        // if(n==1) return x;
-        // if(x==0) return 0.0;
-        // if(x==1.0) return 1.0;
-        // if(x==-1.0&&(n&1)) return -1.0;
-        // if(x==-1.0&&!(n&1)) return 1.0;
-        // double ans=1;
-        // long newN=n;
-        // if(newN<0){
-        //     x=1/x;
-        //     newN=-newN;
-        // }
-        // while(newN>0){
-        //     if(newN&1){
-        //         ans=ans*x;
-        //     }
-        //     x=x*x;
-        //     newN=newN>>1;
-        // }
-        // return ans;
-
-
-
-        //recursion
-        long int newn=n;
         if(n==0) return 1.0;
-        if(n==1) return x;
         if(x==0.0) return 0.0;
         if(x==1.0) return 1.0;
-        if(x==-1.0&&n%2==0) return 1.0;
-        if(x==-1.0&&n%2!=0) return -1.0;
-         if(newn<0)
-         x=1/x;
-         return solve(x,newn);
+        if(n==1) return x;
+        double ans=1.0;
+        bool isneg=(n<0);
+        long n1=abs((long long)n);
+        while(n1!=0){
+            if(n1&1){
+               ans=(ans*x);
+            }
+            x=(x*x);
+            n1=n1/2;
+        }
+        return isneg?1/(double)ans:ans;
     }
 };
