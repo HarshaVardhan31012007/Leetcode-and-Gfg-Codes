09c521396de737fef07935e20098d9c3302f11fe
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-07 21:25:46 +0530

    Time: 0 ms (100%), Space: 17.8 MB (22.09%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -6,15 +6,12 @@ public:
        int maxi1=INT_MIN;
        for(int i=0;i<nums.size();i++){
           p=p*nums[i];
-          if(p>=0)
           ans=max(ans,p);
-          else{
-            ans=max(ans,p);
+          if(p<0){
             if(maxi1!=INT_MIN)
             ans=max(ans,p/maxi1);
+            maxi1=max(maxi1,p);
           }
-          if(p<0)
-          maxi1=max(maxi1,p);
           if(p==0){
             p=1;
             maxi1=INT_MIN;
