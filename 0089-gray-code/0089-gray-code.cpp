59d71fce069d55eabe74b801932096aadbd0a286
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-07 09:41:08 +0530

    Time: 7 ms (14.24%), Space: 16.5 MB (12.86%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -21,17 +21,59 @@ public:
     // }
 
 
-    vector<string>helper(int n){
+    // vector<string>helper(int n){
+    //     if(n==1){
+    //         return {"0","1"};
+    //     }
+    //     vector<string>temp=helper(n-1);
+    //     vector<string>ans;
+    //     for(int i=0;i<temp.size();i++){
+    //         ans.push_back(temp[i]+"0");
+    //     }
+    //     for(int i=temp.size()-1;i>=0;i--){
+    //         ans.push_back(temp[i]+"1");
+    //     }
+    //     return ans;
+    // }
+    // vector<int> grayCode(int n) {
+    //     // vector<int>ans;
+    //     // vector<int>temp;
+    //     // temp.push_back(0);
+    //     // unordered_set<int>st;
+    //     // n=((1<<n)-1);
+    //     // solve(1,n,st,ans,temp);
+    //     // return temp;
+
+
+    //     // vector<int>ans;
+    //     // for(int i=0;i<(1<<n);i++){
+    //     //     ans.push_back(i^(i>>1));
+    //     // }
+    //     // return ans;
+
+
+    //     vector<string>result=helper(n);
+    //     vector<int>ans;
+    //     for(auto &each:result){
+    //         bitset<32>bt(each);
+    //         ans.push_back(bt.to_ulong());
+    //     }
+    //     return ans;
+    // }
+
+
+
+    vector<int>helper(int n){
         if(n==1){
-            return {"0","1"};
+            return {0,1};
         }
-        vector<string>temp=helper(n-1);
-        vector<string>ans;
+        vector<int>temp=helper(n-1);
+        vector<int>ans;
         for(int i=0;i<temp.size();i++){
-            ans.push_back(temp[i]+"0");
+            ans.push_back(temp[i]*2);
         }
         for(int i=temp.size()-1;i>=0;i--){
-            ans.push_back(temp[i]+"1");
+            ans.push_back(temp[i]*2+1);
         }
         return ans;
     }
@@ -52,12 +94,8 @@ public:
         // return ans;
 
 
-        vector<string>result=helper(n);
-        vector<int>ans;
-        for(auto &each:result){
-            bitset<32>bt(each);
-            ans.push_back(bt.to_ulong());
-        }
+    
+        vector<int>ans=helper(n);
         return ans;
     }
 };
