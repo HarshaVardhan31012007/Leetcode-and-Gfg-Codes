035bc19acbb6fdce69ebc0d6e74acd4c9ba8c8b1
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-07 15:29:51 +0530

    Time: 6 ms (36.93%), Space: 9 MB (38.09%) - LeetHub

--- 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.cpp
+++ 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.cpp
@@ -26,6 +26,41 @@
 
 
 
+// class Solution {
+// public:
+//     vector<bool>isprime;
+//     int findrev(int n){
+//         int k=0;
+//         while(n){
+//             k=k*10+(n%10);
+//             n=n/10;
+//         } 
+//         return k;
+//     }
+//     int sumOfPrimesInRange(int n) {
+//         isprime.assign(1001,1);
+//         isprime[0]=0;isprime[1]=0;
+//         for(int i=2;i*i<=1000;i++){
+//             for(int j=i*i;j<=1000;j+=i){
+//                 isprime[j]=0;
+//             }
+//         }
+//         int m=findrev(n);
+//         int l=min(m,n);
+//         int r=max(m,n);
+//         int sum=0;
+//         for(int i=l;i<=r;i++){
+//             if(isprime[i]){
+//                 sum+=i;
+//             }
+//         }
+//         return sum;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     vector<bool>isprime;
@@ -38,19 +73,34 @@ public:
         return k;
     }
     int sumOfPrimesInRange(int n) {
-        isprime.assign(1001,1);
-        isprime[0]=0;isprime[1]=0;
-        for(int i=2;i*i<=1000;i++){
-            for(int j=i*i;j<=1000;j+=i){
-                isprime[j]=0;
-            }
-        }
         int m=findrev(n);
         int l=min(m,n);
         int r=max(m,n);
+        int k=sqrt(r);
+        vector<bool>isprime(k+1,1);
+        isprime[0]=isprime[1]=0;
+        for(int i=2;i*i<=k;i++){
+            for(int j=i*i;j<=k;j+=i){
+               isprime[j]=0;
+            }
+        }
+        vector<bool>isprime1(r-l+1,1);
+        for(int i=2;i*i<=r;i++){
+            if(isprime[i]){
+                int one=((l/i)*i);
+                if(one<l) one+=i;
+                int first=max(i*i,one);
+                for(int j=first;j<=r;j+=i){
+                   isprime1[j-l]=0;
+                }
+            }
+        }
+        if(l==1){
+            isprime1[0]=0;
+        }
         int sum=0;
         for(int i=l;i<=r;i++){
-            if(isprime[i]){
+            if(isprime1[i-l]){
                 sum+=i;
             }
         }
