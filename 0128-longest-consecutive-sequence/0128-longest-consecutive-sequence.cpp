77d04f3f5b2c685d69691f7dc3b85543232ec8a7
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-07 22:42:19 +0530

    Time: 130 ms (12.23%), Space: 89 MB (49.39%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    class DSU{
-       public:
-       vector<int>parent;
-       vector<int>rank;
-       DSU(int n){
-         rank.resize(n,0);
-         parent.resize(n);
-         for(int i=0;i<n;i++){
-            parent[i]=i;
-         }
-       }  
-       int find(int node){
-         if(parent[node]==node) return node;
-         return parent[node]=find(parent[node]);
-       }
-       void Union(int a,int b){
-         a=find(a);
-         b=find(b);
-         if(a==b) return;
-         if(rank[a]<rank[b]){
-            parent[a]=b;
-         }
-         else if(rank[a]>rank[b]){
-            parent[b]=a;
-         }
-         else{
-            parent[b]=a;
-            rank[a]++;
-         }
-       }
-    };
+    // class DSU{
+    //    public:
+    //    vector<int>parent;
+    //    vector<int>rank;
+    //    DSU(int n){
+    //      rank.resize(n,0);
+    //      parent.resize(n);
+    //      for(int i=0;i<n;i++){
+    //         parent[i]=i;
+    //      }
+    //    }  
+    //    int find(int node){
+    //      if(parent[node]==node) return node;
+    //      return parent[node]=find(parent[node]);
+    //    }
+    //    void Union(int a,int b){
+    //      a=find(a);
+    //      b=find(b);
+    //      if(a==b) return;
+    //      if(rank[a]<rank[b]){
+    //         parent[a]=b;
+    //      }
+    //      else if(rank[a]>rank[b]){
+    //         parent[b]=a;
+    //      }
+    //      else{
+    //         parent[b]=a;
+    //         rank[a]++;
+    //      }
+    //    }
+    // };
     int longestConsecutive(vector<int>& nums) {
         // unordered_map<int,int>mpp;
         // int ans=INT_MIN;
@@ -68,25 +68,44 @@ public:
         // return ans;
 
 
-        unordered_map<int,int>mpp;
-        int n=nums.size();
-        DSU dsu(n);
-        for(int i=0;i<n;i++){
-            if(!mpp.count(nums[i]))
-            mpp[nums[i]]=i;
-            if(mpp.count(nums[i]-1)){
-                dsu.Union(mpp[nums[i]-1],mpp[nums[i]]);
-            }
-            if(mpp.count(nums[i]+1)){
-                dsu.Union(mpp[nums[i]+1],mpp[nums[i]]);
-            }
-        }
-        unordered_map<int,int>mpp1;
+        // unordered_map<int,int>mpp;
+        // int n=nums.size();
+        // DSU dsu(n);
+        // for(int i=0;i<n;i++){
+        //     if(!mpp.count(nums[i]))
+        //     mpp[nums[i]]=i;
+        //     if(mpp.count(nums[i]-1)){
+        //         dsu.Union(mpp[nums[i]-1],mpp[nums[i]]);
+        //     }
+        //     if(mpp.count(nums[i]+1)){
+        //         dsu.Union(mpp[nums[i]+1],mpp[nums[i]]);
+        //     }
+        // }
+        // unordered_map<int,int>mpp1;
+        // int ans=0;
+        // for(int i=0;i<n;i++){
+        //     int p=dsu.find(i);
+        //     mpp1[p]++;
+        //     ans=max(ans,mpp1[p]);
+        // }
+        // return ans;
+
+
+
+        unordered_set<int>st;
+        for(auto &each:nums)
+        st.insert(each);
         int ans=0;
-        for(int i=0;i<n;i++){
-            int p=dsu.find(i);
-            mpp1[p]++;
-            ans=max(ans,mpp1[p]);
+        for(auto &each:st){
+            if(st.find(each-1)==st.end()){
+                int temp=0;
+                int num=each;
+                while(st.count(num)){
+                    temp++;
+                    num++;
+                }
+                ans=max(ans,temp);
+            }
         }
         return ans;
     }
