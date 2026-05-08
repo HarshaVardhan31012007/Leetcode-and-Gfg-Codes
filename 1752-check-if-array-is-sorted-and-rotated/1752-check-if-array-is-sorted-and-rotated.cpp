6d9d1f26a979f7a3a50e71d39a12d59786a24b2a
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-08 20:52:08 +0530

    Time: 0 ms (100%), Space: 11.4 MB (6.78%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
+        // int n=nums.size();
+        // int i=1;
+        // while(i<n){
+        //     if(nums[i]>=nums[i-1]){
+        //         i++;
+        //     }
+        //     else
+        //     break;
+        // }
+        // if(i==n) return true;
+        // i++;
+        // while(i<n){
+        //     if(nums[i]>=nums[i-1]){
+        //         i++;
+        //     }
+        //     else
+        //     break;
+        // }
+        // if(i==n){
+        //     return nums[0]>=nums[n-1];
+        // }
+        // return false;
+
+
+
         int n=nums.size();
-        int i=1;
-        while(i<n){
+        int c=0;
+        for(int i=1;i<n;i++){
             if(nums[i]>=nums[i-1]){
-                i++;
+                c++;
             }
-            else
-            break;
         }
-        if(i==n) return true;
-        i++;
-        while(i<n){
-            if(nums[i]>=nums[i-1]){
-                i++;
-            }
-            else
-            break;
-        }
-        if(i==n){
-            return nums[0]>=nums[n-1];
-        }
+        if(c==(n-1)) return true;
+        if(c==(n-2)) return nums[0]>=nums[n-1];
         return false;
     }
 };
