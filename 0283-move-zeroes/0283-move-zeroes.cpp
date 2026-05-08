7bee5833c4718fc481d521208cb7ca2480ee7b5b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-08 19:11:34 +0530

    Time: 0 ms (100%), Space: 23.9 MB (18.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& s) {
-        int n=s.size();
+    void moveZeroes(vector<int>& nums) {
+        // int n=nums.numsize();
+        // int i=0;
+        // int j=0;
+        // while(j<n){
+        //     while(j<n&&nums[j]==0) j++;
+        //     while(i<n&&nums[i]!=0) i++;
+        //     if(i<n&&j<n&&i<j){
+        //       nums[i]=nums[j];
+        //       nums[j]=0;
+        //       i++;j++;
+        //     }
+        //     elnumse if(i<n&&j<n&&i>=j){
+        //        j=i+1;
+        //     }
+        //     elnumse
+        //     break;
+        // }
+
+
+        int n=nums.size();
         int i=0;
         int j=0;
         while(j<n){
-            while(j<n&&s[j]==0) j++;
-            while(i<n&&s[i]!=0) i++;
-            if(i<n&&j<n&&i<j){
-              s[i]=s[j];
-              s[j]=0;
-              i++;j++;
-            }
-            else if(i<n&&j<n&&i>=j){
-               j=i+1;
-            }
-            else
-            break;
+           if(nums[i]==0&&nums[j]!=0){
+              swap(nums[i],nums[j]);
+           }
+           if(nums[i]!=0) i++;
+           j++;
         }
     }
 };
