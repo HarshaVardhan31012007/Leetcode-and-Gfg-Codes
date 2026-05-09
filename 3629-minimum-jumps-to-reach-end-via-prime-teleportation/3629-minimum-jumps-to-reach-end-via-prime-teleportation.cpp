269d508cbcab0dcf6a1f39f1cf8798da2882b116
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-09 16:45:10 +0530

    Time: 2475 ms (5.16%), Space: 532.1 MB (5.15%) - LeetHub

--- 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
+++ 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -1,57 +1,52 @@
-const int MX = 1000001;
-vector<int> factors[MX];
-bool initialized = []() {
-    for (int i = 2; i < MX; ++i) {
-        if (factors[i].empty()) {
-            for (int j = i; j < MX; j += i) {
-                factors[j].push_back(i);
-            }
-        }
-    }
-    return true;
-}();
-
 class Solution {
 public:
     int minJumps(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, vector<int>> edges;
-        for (int i = 0; i < n; ++i) {
-            for (int p : factors[nums[i]]) {
-                edges[p].push_back(i);
+        int maxi=INT_MIN;
+        unordered_map<int,vector<int>>mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]].push_back(i);
+            maxi=max(maxi,nums[i]);
+        }
+        vector<int>isprime(maxi+1,1);
+        isprime[0]=isprime[1]=0;
+        for(int i=2;i*i<=maxi;i++){
+            for(int j=i*i;j<=maxi;j+=i){
+                isprime[j]=0;
             }
         }
-        int res = 0;
-        vector<bool> seen(n, false);
-        seen[0] = true;
-        vector<int> q = {0};
-        while (true) {
-            vector<int> q2;
-            for (int i : q) {
-                if (i == n - 1) return res;
-                if (i > 0 && !seen[i - 1]) {
-                    seen[i - 1] = true;
-                    q2.push_back(i - 1);
-                }
-                if (i < n - 1 && !seen[i + 1]) {
-                    seen[i + 1] = true;
-                    q2.push_back(i + 1);
-                }
-                if (factors[nums[i]].size() == 1) {
-                    int p = nums[i];
-                    if (edges.count(p)) {
-                        for (int j : edges[p]) {
-                            if (!seen[j]) {
-                                seen[j] = true;
-                                q2.push_back(j);
-                            }
-                        }
-                        edges[p].clear();
-                    }
-                }
-            }
-            q = move(q2);
-            res++;
+        queue<int>q;
+        q.push(0);
+        vector<int>jumps(nums.size(),-1);
+        jumps[0]=0;
+        unordered_set<int>visited;
+        while(!q.empty()){
+           int i=q.front();
+           q.pop();
+           if(i==(nums.size()-1)) return jumps[i];
+           if(i>0){
+             if(jumps[i-1]==-1){
+                q.push(i-1);
+                jumps[i-1]=jumps[i]+1;
+             }
+           }
+           if(i+1<nums.size()){
+             if(jumps[i+1]==-1){
+                q.push(i+1);
+                jumps[i+1]=jumps[i]+1;
+             }
+           }
+           if(!isprime[nums[i]]||visited.count(nums[i])) continue;
+           for(int k=nums[i];k<=maxi;k+=nums[i]){
+               if(visited.count(k)) continue;
+               for(auto &idx:mpp[k]){
+                   if(jumps[idx]==-1){
+                     q.push(idx);
+                     jumps[idx]=jumps[i]+1;
+                   }
+               }
+               visited.insert(k);
+           }
         }
+        return -1;
     }
 };
