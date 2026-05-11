2e681ccc3b02621e59af91e1f2a16be28dfeb9df
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-11 21:46:59 +0530

    Time: 39 ms (87.74%), Space: 64.7 MB (98.38%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,13 +1,38 @@
+// class Solution {
+// public:
+//     int solve(int i,int p,int n,vector<int> adjList [],vector<bool>&hasApple){
+//         int ans=0;
+//         for(auto &adj:adjList[i]){
+//             if(adj!=p){
+//                 ans+=solve(adj,i,n,adjList,hasApple);
+//             }
+//         }
+//         if(p!=-1&&(hasApple[i]||ans>0)) ans+=2;
+//         return ans;
+//     }
+//     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+//         vector<int>adjList[n];
+//         for(auto &each:edges){
+//             adjList[each[0]].push_back(each[1]);
+//             adjList[each[1]].push_back(each[0]);
+//         }
+//         return solve(0,-1,n,adjList,hasApple);
+//     }
+// };
+
+
 class Solution {
 public:
     int solve(int i,int p,int n,vector<int> adjList [],vector<bool>&hasApple){
         int ans=0;
         for(auto &adj:adjList[i]){
             if(adj!=p){
-                ans+=solve(adj,i,n,adjList,hasApple);
+                int child=solve(adj,i,n,adjList,hasApple);
+                if(hasApple[adj]||child>0)
+                ans+=2;
+                ans+=child;
             }
         }
-        if(p!=-1&&(hasApple[i]||ans>0)) ans+=2;
         return ans;
     }
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
