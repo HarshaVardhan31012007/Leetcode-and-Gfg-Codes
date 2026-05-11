7b991029f6af7514a85c0c8d4331fc6434c6b753
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-11 21:37:46 +0530

    Time: 87 ms (12.7%), Space: 77.7 MB (8.57%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(int i,int p,int n,unordered_map<int,list<int>>&adjList,vector<bool>&hasApple){
+        int ans=0;
+        for(auto &adj:adjList[i]){
+            if(adj!=p){
+                ans+=solve(adj,i,n,adjList,hasApple);
+            }
+        }
+        if(hasApple[i]&&p!=-1)
+        ans+=2;
+        else if(ans>0&&p!=-1)
+        ans+=2;
+        return ans;
+    }
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        unordered_map<int,list<int>>adjList;
+        for(auto &each:edges){
+            adjList[each[0]].push_back(each[1]);
+            adjList[each[1]].push_back(each[0]);
+        }
+        return solve(0,-1,n,adjList,hasApple);
+    }
+};
