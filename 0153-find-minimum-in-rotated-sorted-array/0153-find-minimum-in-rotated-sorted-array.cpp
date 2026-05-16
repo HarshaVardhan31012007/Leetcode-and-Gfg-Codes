625892fb9f35c308c536c4ba8cc707d62d14d4e1
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-16 17:51:13 +0530

    Time: 0 ms (100%), Space: 14.3 MB (10.83%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -17,18 +17,37 @@ public:
         // return nums[0];
 
 
+        // int n=nums.size();
+        // int s=0;
+        // int e=n-1;
+        // while(s<e){
+        //     int mid=s+(e-s)/2;
+        //     if(nums[mid]>nums[e]){
+        //         s=mid+1;
+        //     }
+        //     else{
+        //         e=mid;
+        //     }
+        // }
+        // return nums[e];
+
+
+
         int n=nums.size();
         int s=0;
         int e=n-1;
-        while(s<e){
+        int ans=INT_MAX;
+        while(s<=e){
             int mid=s+(e-s)/2;
-            if(nums[mid]>nums[e]){
-                s=mid+1;
+            if(nums[s]<=nums[mid]){
+               ans=min(ans,nums[s]);
+               s=mid+1;
             }
             else{
-                e=mid;
+               ans=min(ans,nums[mid]);
+               e=mid-1;
             }
         }
-        return nums[e];
+        return ans;
     }
 };
