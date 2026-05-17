673467cc0701a0c0bb4b0fccd261f21b1a4f3703
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-17 23:25:33 +0530

    Time: 0 ms (100%), Space: 17.9 MB (94.2%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
+        // int n=nums.size();
+        // int s=0;
+        // int e=n-1;
+        // int ans=-1;
+        // while(s<=e){
+        //     while(s<e&&nums[e]==nums[e-1]) e--;
+        //     int mid=s+(e-s)/2;
+        //     if(ans==-1||nums[mid]<=nums[ans])
+        //     ans=mid;
+        //     if(nums[mid]>=nums[e]){
+        //        s=mid+1;
+        //     }
+        //     else{
+        //        e=mid-1;
+        //     }
+        // }
+        // if(ans>0){
+        //     if(binary_search(nums.begin(),nums.begin()+ans,target))
+        //     return true;
+        // }
+        // if(binary_search(nums.begin()+ans,nums.end(),target))
+        // return true;
+        // return false;
+
+
+
         int n=nums.size();
         int s=0;
         int e=n-1;
         int ans=-1;
-        while(s<=e){
+        while(s<e){
             while(s<e&&nums[e]==nums[e-1]) e--;
             int mid=s+(e-s)/2;
-            if(ans==-1||nums[mid]<=nums[ans])
-            ans=mid;
             if(nums[mid]>=nums[e]){
                s=mid+1;
             }
             else{
-               e=mid-1;
+               e=mid;
             }
         }
-        if(ans>0){
-            if(binary_search(nums.begin(),nums.begin()+ans,target))
-            return true;
-        }
-        if(binary_search(nums.begin()+ans,nums.end(),target))
+        if(binary_search(nums.begin(),nums.begin()+e,target))
+        return true;
+        if(binary_search(nums.begin()+e,nums.end(),target))
         return true;
         return false;
     }
