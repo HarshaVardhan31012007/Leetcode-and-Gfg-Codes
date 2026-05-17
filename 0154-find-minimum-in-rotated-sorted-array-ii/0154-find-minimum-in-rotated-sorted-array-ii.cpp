74d149d8eee257dfd36f986e7da7400b3d9a2183
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-17 23:01:01 +0530

    Time: 0 ms (100%), Space: 16.2 MB (27.29%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -20,19 +20,40 @@ public:
 
 
 
+        // int n = nums.size();
+        // int s=0;
+        // int e=n-1;
+        // int ans=INT_MAX;
+        // while(s<=e){
+        //     int mid=s+(e-s)/2;
+        //     if(s==e) ans=min(ans,nums[s]);
+        //     if(nums[mid]>nums[e]){
+        //         ans=min(ans,nums[s]);
+        //         s=mid+1;
+        //     }
+        //     else if(nums[mid]<nums[e]){
+        //         ans=min(ans,nums[mid]);
+        //         e=mid-1;
+        //     }
+        //     else{
+        //         e--;
+        //     }
+        // }
+        // return ans;
+
+
         int n = nums.size();
         int s=0;
         int e=n-1;
         int ans=INT_MAX;
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(s==e) ans=min(ans,nums[s]);
+            if(nums[mid]<ans)
+            ans=nums[mid];
             if(nums[mid]>nums[e]){
-                ans=min(ans,nums[s]);
                 s=mid+1;
             }
             else if(nums[mid]<nums[e]){
-                ans=min(ans,nums[mid]);
                 e=mid-1;
             }
             else{
