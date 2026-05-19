6d6219498199888f7f0f1b344f82decc080f1ffc
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-19 10:17:39 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        // next_permutation(nums.begin(),nums.end());
+
+        int n=nums.size();
+        if(n==1) return;
+        int i=n-2;
+        while(i>=0){
+            if(nums[i]<nums[i+1]){
+                break;
+            }
+            i--;
+        }
+        if(i<0){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        int j=i+1;
+        while(j<n){
+            if(nums[j]>nums[i]){
+                j++;
+            }
+            else
+            break;
+        }
+        j--;
+        swap(nums[i],nums[j]);
+        reverse(nums.begin()+i+1,nums.end());
     }
 };
