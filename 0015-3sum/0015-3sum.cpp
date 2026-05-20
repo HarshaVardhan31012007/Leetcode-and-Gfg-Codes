2f8579701c5ddf61cd635ca9c7566158d2ba5a83
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-20 23:58:28 +0530

    Time: 36 ms (97.31%), Space: 29.2 MB (44.38%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,73 +1,66 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& arr) {
-        // vector<vector<int>>ans;
-        // int n=arr.size();
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // int n=nums.size();
         // set<vector<int>>st;
         // for(int i=0;i<n;i++){
         //     for(int j=i+1;j<n;j++){
         //         for(int k=j+1;k<n;k++){
-        //             if(arr[i]+arr[j]+arr[k]==0){
-        //                vector<int>temp={arr[i],arr[j],arr[k]};
-        //                sort(temp.begin(),temp.end());
-        //                if(st.find(temp)==st.end()){
-        //                 ans.push_back(temp);
+        //             if(nums[i]+nums[j]+nums[k]==0){
+        //                 vector<int>temp={nums[i],nums[j],nums[k]};
+        //                 sort(temp.begin(),temp.end());
         //                 st.insert(temp);
-        //                }
-        //                //or in loop
-        //                //sort temp
-        //                //st.insert(temp);
-        //                //after loop
-        //                //vector<int>ans(st.begin(),st.end());
+        //             }
+        //         }
+        //     }
+        // }
+        // vector<vector<int>>ans;
+        // for(auto &each:st)
+        // ans.push_back(each);
+        // return ans;
+
+
+        // int n=nums.size();
+        // vector<vector<int>>ans;
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<n;i++){
+        //     if(i>0&&nums[i]==nums[i-1]) continue;
+        //     for(int j=i+1;j<n;j++){
+        //         if(j>i+1&&nums[j]==nums[j-1]) continue;
+        //         for(int k=j+1;k<n;k++){
+        //             if(k>j+1&&nums[k]==nums[k-1]) continue;
+        //             if(nums[i]+nums[j]+nums[k]==0){
+        //                 ans.push_back({nums[i],nums[j],nums[k]});
         //             }
         //         }
         //     }
         // }
         // return ans;
 
-        //method 2
-       
-        // int n=arr.size();
-        // set<vector<int>>st;
-        // for(int i=0;i<n;i++){
-        //     set<int>s;
-        //     for(int j=i+1;j<n;j++){
-        //         int target=-(arr[i]+arr[j]);
-        //         if(s.find(target)!=s.end()){
-        //          vector<int>temp={arr[i],arr[j],target};
-        //          sort(temp.begin(),temp.end());
-        //         st.insert(temp);
-        //         }
-        //         s.insert(arr[j]);
-        //     }
-        // }
-        // vector<vector<int>>ans(st.begin(),st.end());
-        // return ans;
 
-        //method 3
-         int n=arr.size();
+
+        int n=nums.size();
         vector<vector<int>>ans;
-        sort(arr.begin(),arr.end());
+        sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
-            if(i>0&&arr[i]==arr[i-1]) continue;
-           int j=i+1;
-           int k=n-1;
-           while(j<k){
-            int sum=arr[i]+arr[j]+arr[k];
-            if(sum<0)
-            j++;
-            else if(sum>0)
-            k--;
-            else{
-                vector<int>temp={arr[i],arr[j],arr[k]};
-                ans.push_back(temp);
-                j++;
-                k--;
-               // while(j<k&&arr[j]==arr[j-1])j++;//or 
-               while(j<k&&arr[k]==arr[k+1])k--;
-               //this any one ot these two should be there
+            if(i>0&&nums[i]==nums[i-1]) continue;
+            int s=i+1;
+            int e=n-1;
+            int t=-nums[i];
+            while(s<e){
+                if(nums[s]+nums[e]<t){
+                    s++;
+                }
+                else if(nums[s]+nums[e]>t){
+                    e--;
+                }
+                else{
+                    ans.push_back({nums[i],nums[s],nums[e]});
+                    s++;
+                    e--;
+                    while(s<e&&nums[s]==nums[s-1]) s++;
+                }
             }
-           }
         }
         return ans;
     }
