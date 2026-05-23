99947e68f635d2fe654a9ce12b2d4ce82a6f3557
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-23 18:30:10 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.93%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,33 @@
 class Solution {
 public:
-void binary_firstoccurence(vector<int>arr,int n,int target,int &ansIndex){
-    int s=0;
-    int e=n-1;
-    int mid=s+(e-s)/2;
-    while(s<=e){
-        if(arr[mid]==target){
-            ansIndex=mid;
-            e=mid-1;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        int s=0;
+        int e=n-1;
+        int ans1=-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]>=target){
+                if(nums[mid]==target)
+                ans1=mid;
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
         }
-        if(arr[mid]<target)
-        s=mid+1;
-        if(arr[mid]>target)
-        e=mid-1;
-
-        mid=s+(e-s)/2;
-
-    }
-}
-void binary_lastoccurence(vector<int>arr,int n,int target,int &ansIndex){
-    int s=0;
-    int e=n-1;
-    int mid=s+(e-s)/2;
-    while(s<=e){
-        if(arr[mid]==target){
-            ansIndex=mid;
-            s=mid+1;
+        int ans2=-1;s=0;e=n-1;
+         while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]<=target){
+                if(nums[mid]==target)
+                ans2=mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
         }
-        if(arr[mid]<target)
-        s=mid+1;
-        if(arr[mid]>target)
-        e=mid-1;
-
-        mid=s+(e-s)/2;
-
-    }
-}
-    vector<int> searchRange(vector<int>& arr, int target) {
-        int n=arr.size();
-        int ansIndex1=-1;
-        int ansIndex2=-1;
-        binary_firstoccurence(arr,n,target,ansIndex1);
-        binary_lastoccurence(arr,n,target,ansIndex2);
-        vector<int>temp(2);
-        temp[0]=ansIndex1;
-        temp[1]=ansIndex2;
-        return temp;
+        return {ans1,ans2};
     }
 };
