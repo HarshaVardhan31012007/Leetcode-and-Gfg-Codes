d10ca4542d93df1de676c1afdb63528438d0cdb3
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-26 10:31:18 +0530

    Time: 0 ms (100%), Space: 8.9 MB (78.89%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     int numberOfSpecialChars(string word) {
         int arr[26]={0};
-        int brr[26]={0};
         int c=0;
         for(auto &each:word){
             int idx;
             if(isupper(each)){
                idx=each-'A';
-               if(arr[idx]==0)
+               if(arr[idx]==-1||arr[idx]==0)
                arr[idx]++;
+               if(arr[idx]==0){
+                  c++;
+                  arr[idx]=50;
+               }
             }
             else{
                idx=each-'a';
-               if(brr[idx]==0)
-               brr[idx]++;
-            }
-            if(arr[idx]+brr[idx]==2){
-               c++;
-               arr[idx]=brr[idx]=-1;
+               if(arr[idx]==1||arr[idx]==0)
+               arr[idx]--;
+               if(arr[idx]==0){
+                  c++;
+                  arr[idx]=50;
+               }
             }
         }
         return c;
