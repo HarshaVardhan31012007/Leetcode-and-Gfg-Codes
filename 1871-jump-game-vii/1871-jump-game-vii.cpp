ca4681de74c558cfa014e7f14677a510a68692e1
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-26 13:05:37 +0530

    Time: 4 ms (91.61%), Space: 44.2 MB (5.62%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    bool dfs(int i,int mini,int maxi,string &s,int &n,int &minJump,int &maxJump,vector<int>&visited){
+    bool dfs(int i,int maxi,string &s,int &n,int &minJump,int &maxJump,vector<int>&visited){
         visited[i]=1;
         if(i==(n-1)) return true;
-        bool ans=false;
-        int maxi1=maxi+1;
-        for(int j=mini;j<=min(maxi,n-1);j++){
-           if(visited[j]==0&&s[j]=='0'){
-             ans=ans||dfs(j,max(maxi1,j+minJump),j+maxJump,s,n,minJump,maxJump,visited);
-             maxi1=max(maxi1,j+maxJump+1);
-           }
+        int end=i+maxJump;
+        int start=(i+minJump);
+        if(start<=maxi) start=maxi+1;
+        int maxi1=min(end,n-1);
+        for(int j=start;j<=min(end,n-1);j++){
+            if(visited[j]==0&&s[j]=='0'){
+                bool ans=dfs(j,maxi1,s,n,minJump,maxJump,visited);
+                if(ans) return true;
+                maxi1=max(maxi1,min(j+maxJump,n-1));
+            }
         }
-        return ans;
+        return false;
     }
     bool canReach(string s, int minJump, int maxJump) {
         int n=s.length();
         if(s[n-1]=='1') return false;
+        cout<<n<<endl;
         vector<int>visited(n,0);
-        visited[0]=1;
-        return dfs(0,minJump,maxJump,s,n,minJump,maxJump,visited);
+        return dfs(0,0,s,n,minJump,maxJump,visited);
     }
 };
