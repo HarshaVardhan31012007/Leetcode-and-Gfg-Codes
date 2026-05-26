da42625abd5c915d9ae712fc82c31ddf0ccf2e71
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-26 18:16:05 +0530

    Time: 11 ms (54.19%), Space: 31.1 MB (18.79%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool dfs(int i,int maxi,string &s,int &n,int &minJump,int &maxJump,vector<int>&visited){
-        visited[i]=1;
-        if(i==(n-1)) return true;
-        int end=i+maxJump;
-        int start=(i+minJump);
-        if(start<=maxi) start=maxi+1;
-        int maxi1=min(end,n-1);
-        for(int j=start;j<=min(end,n-1);j++){
-            if(visited[j]==0&&s[j]=='0'){
-                bool ans=dfs(j,maxi1,s,n,minJump,maxJump,visited);
-                if(ans) return true;
-                maxi1=max(maxi1,min(j+maxJump,n-1));
-            }
-        }
-        return false;
-    }
     bool canReach(string s, int minJump, int maxJump) {
         int n=s.length();
         if(s[n-1]=='1') return false;
         vector<int>visited(n,0);
-        return dfs(0,0,s,n,minJump,maxJump,visited);
+        queue<int>q;
+        q.push(0);
+        visited[0]=1;
+        int maxi=0;
+        while(!q.empty()){
+            int idx=q.front();
+            q.pop();
+            if(idx==(n-1)) return true;
+            for(int j=max(maxi+1,idx+minJump);j<=min(idx+maxJump,n-1);j++){
+                maxi=max(maxi,j);
+                if(visited[j]==0&&s[j]=='0'){
+                    q.push(j);
+                    visited[j]=1;
+                }
+            }
+        }
+        return false;
     }
 };
