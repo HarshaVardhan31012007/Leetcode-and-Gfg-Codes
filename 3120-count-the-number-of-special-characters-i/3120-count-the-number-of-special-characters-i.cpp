3a9908ddeaed96afb7a430ff6ecc6ec42d79e1c6
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-26 11:03:18 +0530

    Time: 1 ms (39.91%), Space: 9.4 MB (40.14%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
+        // unordered_map<char,int>mpp;
+        // int count=0;
+        // for(auto &each:word)
+        // mpp[each]++;
+        // for(auto &each:mpp){
+        //     if(islower(each.first)){
+        //         if(mpp[each.first]>=1&&mpp[(char)((each.first-'a')+'A')]>=1){
+        //            count++;
+        //            mpp[each.first]=0;
+        //            mpp[(each.first-'a')+'A']=0;
+        //         }
+        //     }
+        // }
+        // return count;
+
+
+
+
         unordered_map<char,int>mpp;
         int count=0;
         for(auto &each:word)
         mpp[each]++;
-        for(auto &each:mpp){
-            if(islower(each.first)){
-                if(mpp[each.first]>=1&&mpp[(char)((each.first-'a')+'A')]>=1){
-                   count++;
-                   mpp[each.first]=0;
-                   mpp[(each.first-'a')+'A']=0;
+        for(int i=0;i<26;i++){
+            if(mpp.count(i+'a')){
+                if(mpp.count(i+'A')){
+                    count++;
+                    mpp.erase(i+'a');
+                    mpp.erase(i+'A');
                 }
             }
         }
