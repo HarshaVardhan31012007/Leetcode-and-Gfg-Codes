6039fb545203995a0d709888f0c05a6d58c4538b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-05-27 14:26:34 +0530

    Time: 9 ms (76.76%), Space: 20.9 MB (9.18%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,48 +1,94 @@
 class Solution {
 public:
-    int n;
 
-    int dfs(int i,vector<int>& arr,int d,vector<int>& dp) {
+    //wrong approach
 
-        if(dp[i] != -1) {
-            return dp[i];
-        }
+    // void fillprevG(vector<int>&arr,int n,vector<int>&prevG){
+    //     stack<int>st;
+    //     for(int i=0;i<n;i++){
+    //         while(!st.empty()&&arr[st.top()]<arr[i]){
+    //             st.pop();
+    //         }
+    //         if(!st.empty())
+    //         prevG[i]=st.top();
+    //         st.push(i);
+    //     }
+    // }
+    // void fillnextG(vector<int>&arr,int n,vector<int>&nextG){
+    //     stack<int>st;
+    //     for(int i=n-1;i>=0;i--){
+    //         while(!st.empty()&&arr[st.top()]<arr[i]){
+    //             st.pop();
+    //         }
+    //         if(!st.empty())
+    //         nextG[i]=st.top();
+    //         st.push(i);
+    //     }
+    // }
+    // int maxJumps(vector<int>& arr, int d) {
+    //     int n=arr.size();
+    //     vector<int>prevG(n,-1);
+    //     vector<int>nextG(n,n);
+    //     fillprevG(arr,n,prevG);
+    //     fillnextG(arr,n,nextG);
+    //     vector<int>dp1(n,1);
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i-1;j>=max((i-d),prevG[i]+1);j--){
+    //              dp1[i]=max(dp1[i],dp1[j]+1);
+    //         }
+    //     }
+    //     vector<int>dp2(n,1);
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=i+1;j<=min((i+d),nextG[i]-1);j++){
+    //              dp2[i]=max(dp2[i],dp2[j]+1);
+    //         }
+    //     }
+    //     vector<int>dp(n,0);
+    //     for(int i=0;i<n;i++){
+    //        dp[i]=max(dp1[i],dp2[i]);
+    //     }
+    //     int ans=1;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=max((i-d),prevG[i]+1);j<=min((i+d),nextG[i]-1);j++){
+    //             dp[i]=max(dp[i],dp[j]+1);
+    //         }
+    //         ans=max(ans,dp[i]);
+    //     }
+    //     for(int i=n-1;i>=0;i--){
+    //         for(int j=max((i-d),prevG[i]+1);j<=min((i+d),nextG[i]-1);j++){
+    //             dp[i]=max(dp[i],dp[j]+1);
+    //         }
+    //         ans=max(ans,dp[i]);
+    //     }
+    //     return ans;
+    // }
 
-        int ans = 1;
 
-        // move right
-        for(int j = i + 1;j <= min(n - 1, i + d);j++) {
-            // blocked
-            if(arr[j] >= arr[i]) {
-                break;
-            }
-
-            ans = max(ans,1 + dfs(j, arr, d, dp));
-        }
-
-        // move left
-        for(int j = i - 1;j >= max(0, i - d);j--) {
-            // blocked
-            if(arr[j] >= arr[i]) {
-                break;
-            }
-
-            ans = max(ans,1 + dfs(j, arr, d, dp));
-        }
-
-        return dp[i] = ans;
-    }
 
     int maxJumps(vector<int>& arr, int d) {
-        n = arr.size();
-
-        vector<int> dp(n, -1);
-        int ans = 1;
-
-        for(int i = 0; i < n; i++) {
-            ans = max(ans,dfs(i, arr, d, dp));
+        vector<pair<int,int>>v;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            v.push_back({arr[i],i});
         }
-
+        sort(v.begin(),v.end());
+        int ans=1;
+        vector<int>dp(n,1);
+        for(int i=0;i<n;i++){
+            int val=v[i].first;
+            int idx=v[i].second;
+            int maxi=1;
+            for(int j=idx-1;j>=max((idx-d),0);j--){
+                if(arr[j]>=arr[idx]) break;
+                maxi=max(maxi,dp[j]+1);
+            }
+            for(int j=idx+1;j<=min((idx+d),n-1);j++){
+                if(arr[j]>=arr[idx]) break;
+                maxi=max(maxi,dp[j]+1);
+            }
+            dp[idx]=maxi;
+            ans=max(ans,dp[idx]);
+        }
         return ans;
     }
 };
