b774885b2b84df027242610c149371d4a8a1afa0
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-01 13:53:37 +0530

    Time: 0 ms (100%), Space: 22.3 MB (17.96%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,23 +1,53 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        int n=asteroids.size();
-        vector<int>ans;
-        for(int i=0;i<n;i++){
+        // int n=asteroids.size();
+        // vector<int>ans;
+        // for(int i=0;i<n;i++){
+        //     bool destroy=false;
+        //     while(!ans.empty()&&ans.back()>0&&asteroids[i]<0){
+        //         if(ans.back()<abs(asteroids[i])){
+        //             ans.pop_back();
+        //         }
+        //         else if(ans.back()>=abs(asteroids[i])){
+        //             destroy=true;
+        //             if(ans.back()==abs(asteroids[i]))
+        //             ans.pop_back();
+        //             break;
+        //         }
+        //     }
+        //     if(!destroy)
+        //     ans.push_back(asteroids[i]);
+        // }
+        // return ans;
+
+
+
+
+        stack<int>st;
+        for(int i=asteroids.size()-1;i>=0;i--){
             bool destroy=false;
-            while(!ans.empty()&&ans.back()>0&&asteroids[i]<0){
-                if(ans.back()<abs(asteroids[i])){
-                    ans.pop_back();
-                }
-                else if(ans.back()>=abs(asteroids[i])){
+            while(!st.empty()&&st.top()<0&&asteroids[i]>0){
+                if(abs(st.top())==asteroids[i]){
+                    st.pop();
                     destroy=true;
-                    if(ans.back()==abs(asteroids[i]))
-                    ans.pop_back();
                     break;
+                }
+                else if(abs(st.top())>asteroids[i]){
+                    destroy=true;
+                    break;
+                }
+                else{
+                    st.pop();
                 }
             }
             if(!destroy)
-            ans.push_back(asteroids[i]);
+            st.push(asteroids[i]);
+        }
+        vector<int>ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
         }
         return ans;
     }
