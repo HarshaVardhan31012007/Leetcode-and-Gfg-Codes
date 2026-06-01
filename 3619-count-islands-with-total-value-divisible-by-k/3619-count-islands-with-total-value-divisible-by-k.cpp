d721e24b48d7197c142ef3c1f3e6d681a5a01063
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-01 17:25:22 +0530

    Time: 147 ms (19.65%), Space: 207.4 MB (15.55%) - LeetHub

---
+++ 3619-count-islands-with-total-value-divisible-by-k/3619-count-islands-with-total-value-divisible-by-k.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int bfs(int &row,int &col,vector<vector<int>>&grid,int &k,vector<vector<int>>&visited){
+        queue<pair<int,int>>q;
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
+        int m=grid.size();
+        int n=grid[0].size();
+        q.push({row,col});
+        visited[row][col]=1;
+        int total=0;
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            total=(total+grid[r][c])%k;
+            for(int i=0;i<4;i++){
+                int newr=r+dx[i];
+                int newc=c+dy[i];
+                if(newr<0||newc<0||newr>=m||newc>=n||visited[newr][newc]||grid[newr][newc]==0) continue;
+                visited[newr][newc]=1;
+                q.push({newr,newc});
+            }
+        }
+        return total;
+    }
+    int countIslands(vector<vector<int>>& grid, int k) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>visited(m,vector<int>(n,0));
+        int nislands=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!visited[i][j]&&grid[i][j]>0){
+                    if(bfs(i,j,grid,k,visited)==0){
+                        nislands++;
+                    }
+                }
+            }
+        }
+        return nislands;
+    }
+};
