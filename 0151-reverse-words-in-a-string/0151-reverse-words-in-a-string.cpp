347f5541ca3c6f5c4bc80678c953ef7938365dc5
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-02 20:10:14 +0530

    Time: 0 ms (100%), Space: 10.3 MB (62.05%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,22 +1,69 @@
 class Solution {
+    //public:
+    // string reverseWords(string s) {
+    //     int n=s.length();
+    //     string ans="";
+    //     string temp="";
+    //     for(int j=n-1;j>=0;j--){
+    //        if(s[j]!=' '){
+    //           temp+=s[j];
+    //        }
+    //        if(j==0||(s[j]==' ')){
+    //           if(!temp.empty()){
+    //             reverse(temp.begin(),temp.end());
+    //             ans+=temp+" ";
+    //             temp="";
+    //           }
+    //        }
+    //     }
+    //     if(!ans.empty()&&ans.back()==' ') ans.pop_back();
+    //     return ans;
+    // }
+
+
+
+    private:
+    void reverseString(string &s, int start, int end) {
+        while (start < end) {
+            swap(s[start++], s[end--]);
+        }
+    }
+
 public:
     string reverseWords(string s) {
-        int n=s.length();
-        string ans="";
-        string temp="";
-        for(int j=n-1;j>=0;j--){
-           if(s[j]!=' '){
-              temp+=s[j];
-           }
-           if(j==0||(s[j]==' ')){
-              if(!temp.empty()){
-                reverse(temp.begin(),temp.end());
-                ans+=temp+" ";
-                temp="";
-              }
-           }
+        int n = s.length();
+
+        // Reverse the entire string
+        reverseString(s, 0, n - 1);
+
+        int i = 0, j = 0, start = 0, end = 0;
+
+        while (j < n) {
+            // Skip spaces
+            while (j < n && s[j] == ' ') j++;
+            if (j == n) break;
+
+            start = i;
+
+            // Copy the word characters forward
+            while (j < n && s[j] != ' ') {
+                s[i++] = s[j++];
+            }
+
+            end = i - 1;
+
+            // Reverse the current word using start and end
+            reverseString(s, start, end);
+
+            // Add a space after the word if it's not the last word
+            if (j < n) {
+                s[i++] = ' ';
+            }
         }
-        if(!ans.empty()&&ans.back()==' ') ans.pop_back();
-        return ans;
+
+        // Remove trailing space if present
+        if (i > 0 && s[i - 1] == ' ') i--;
+
+        return s.substr(0, i);
     }
 };
