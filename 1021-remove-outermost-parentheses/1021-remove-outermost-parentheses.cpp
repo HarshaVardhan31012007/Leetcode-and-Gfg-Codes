ac0ffae9c1147d0d4a77aa5a9341718be5f0fb8d
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-02 10:55:57 +0530

    Time: 0 ms (100%), Space: 9 MB (53.77%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -3,13 +3,12 @@ public:
     string removeOuterParentheses(string s) {
         int c=0;
         string ans="";
-        int start=0;
         for(int i=0;i<s.length();i++){
-            if(c==0) start=i;
+            if(c!=0) ans+=s[i];
             if(s[i]=='(') c++;
             else c--;
             if(c==0){
-               ans+=s.substr(start+1,i-start-1);
+               ans.pop_back();
             }
         }
         return ans;
