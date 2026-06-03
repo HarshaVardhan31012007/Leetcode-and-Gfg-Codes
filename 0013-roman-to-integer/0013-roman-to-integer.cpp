8311a20837facf5cb2e85a40e9bd40fb0542b2a1
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-03 20:00:07 +0530

    Time: 5 ms (60.15%), Space: 12.2 MB (71.25%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,19 +2,36 @@ class Solution {
 public:
     int romanToInt(string s) {
         int n=s.length();
-        unordered_map<string,int>mpp;
-        mpp["I"]=1;mpp["V"]=5;mpp["X"]=10;mpp["L"]=50;mpp["C"]=100;mpp["D"]=500;
-        mpp["M"]=1000;mpp["IV"]=4;mpp["IX"]=9;mpp["XL"]=40;mpp["XC"]=90;
-        mpp["CD"]=400;mpp["CM"]=900;
-        int ans=0;string ch="";
+        // unordered_map<string,int>mpp;
+        // mpp["I"]=1;mpp["V"]=5;mpp["X"]=10;mpp["L"]=50;mpp["C"]=100;mpp["D"]=500;
+        // mpp["M"]=1000;mpp["IV"]=4;mpp["IX"]=9;mpp["XL"]=40;mpp["XC"]=90;
+        // mpp["CD"]=400;mpp["CM"]=900;
+        // int ans=0;string ch="";
+        // for(int i=0;i<n;i++){
+        //     string temp=(string(1,s[i])+(i==n-1?ch:string(1,s[i+1])));
+        //     if(i+1<n&&mpp.count(temp)){
+        //         ans+=mpp[temp];
+        //         i++;
+        //     }
+        //     else{
+        //         ans+=mpp[string(1,s[i])];
+        //     }
+        // }
+        // return ans;
+
+
+
+
+        unordered_map<char,int>mpp;
+        mpp['I']=1;mpp['V']=5;mpp['X']=10;mpp['L']=50;mpp['C']=100;mpp['D']=500;
+        mpp['M']=1000;
+        int ans=0;
         for(int i=0;i<n;i++){
-            string temp=(string(1,s[i])+(i==n-1?ch:string(1,s[i+1])));
-            if(i+1<n&&mpp.count(temp)){
-                ans+=mpp[temp];
-                i++;
+            if(i+1<n&&mpp[s[i]]<mpp[s[i+1]]){
+                ans-=mpp[s[i]];
             }
             else{
-                ans+=mpp[string(1,s[i])];
+                ans+=mpp[s[i]];
             }
         }
         return ans;
