a79d344944d79cee2b694044089d75dce5a6fc6d
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-03 12:07:35 +0530

    Time: 0 ms (100%), Space: 36.5 MB (43.84%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,76 +1,21 @@
 class Solution {
 public:
-    int bs(vector<int>&arr,int s,int e,int t){
-        int ans=e+1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(arr[mid]<t)
-            s=mid+1;
-            if(arr[mid]>=t){
-                ans=mid;
-                e=mid-1;
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n=arr.size();
+        int i=0;
+        int j=n-1;
+        while((j-i+1)>k){
+            if(abs(arr[i]-x)<=abs(arr[j]-x)){
+                j--;
+            }
+            else if(abs(arr[i]-x)>abs(arr[j]-x)){
+                i++;
             }
         }
-        return ans;
-    }
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        // priority_queue<pair<int,int>>pq;
-        // for(int i=0;i<arr.size();i++){
-        //     pq.push({abs(arr[i]-x),arr[i]});
-        //     if(pq.size()>k)
-        //     pq.pop();
-        // }
-        // vector<int>ans;
-        // while(!pq.empty()){
-        //     ans.push_back(pq.top().second);
-        //     pq.pop();
-        // }
-        // sort(ans.begin(),ans.end());
-        // return ans;
-
-        
-
-        // int s=0;
-        // int e=arr.size()-1;
-        // vector<int>ans;
-        // while(e-s>=k){
-        //     if(x-arr[s]>arr[e]-x)
-        //     s++;
-        //     else
-        //     e--;
-        // }
-        // for(int i=s;i<=e;i++){
-        //    ans.push_back(arr[i]);
-        // }
-        // return ans;
-
-
-
-        //M1
-        // int idx=bs(arr,0,arr.size()-1,x);
-        int h=arr.size();
-        auto it=lower_bound(arr.begin(),arr.end(),x);
-        if(it!=arr.end())
-        h=it-arr.begin();
-       // int l=idx-1;int h=idx;
-       int l=h-1;
         vector<int>ans;
-        // while(h-l-1<k){
-           // while(h-l<=k){
-            while(k--){
-           if(l<0)
-           h++;
-           else if(h==arr.size())
-           l--;
-           else if(arr[h]-x < x-arr[l])
-           h++;
-           else
-           l--;
+        for(int k=i;k<=j;k++){
+            ans.push_back(arr[k]);
         }
-        // for(int i=l+1;i<=h-1;i++){
-        //     ans.push_back(arr[i]);
-        // }
-        // return ans;
-        return vector<int>(arr.begin()+l+1,arr.begin()+h);
+        return ans;
     }
 };
