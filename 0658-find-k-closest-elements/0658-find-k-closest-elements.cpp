d66e1d8a6f661cfd13f5336bdd26c4f8a3756499
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-03 12:16:12 +0530

    Time: 0 ms (100%), Space: 36.4 MB (60.22%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,19 +1,50 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        // int n=arr.size();
+        // int i=0;
+        // int j=n-1;
+        // while((j-i+1)>k){
+        //     if(abs(arr[i]-x)<=abs(arr[j]-x)){
+        //         j--;
+        //     }
+        //     else if(abs(arr[i]-x)>abs(arr[j]-x)){
+        //         i++;
+        //     }
+        // }
+        // vector<int>ans;
+        // for(int k=i;k<=j;k++){
+        //     ans.push_back(arr[k]);
+        // }
+        // return ans;
+
+
+
         int n=arr.size();
-        int i=0;
-        int j=n-1;
-        while((j-i+1)>k){
-            if(abs(arr[i]-x)<=abs(arr[j]-x)){
-                j--;
-            }
-            else if(abs(arr[i]-x)>abs(arr[j]-x)){
-                i++;
-            }
+        int idx=lower_bound(arr.begin(),arr.end(),x)-arr.begin();
+        int i=idx-1;int j=idx;
+        if(idx==n){
+           i--;j--;
+        }
+        if(idx==0){
+            i++;j++;
         }
         vector<int>ans;
-        for(int k=i;k<=j;k++){
+        while((j-i-1)<k){
+            if(i==-1){
+                j++;
+            }
+            else if(j==n){
+                i--;
+            }
+            else if(abs(arr[i]-x)<=abs(arr[j]-x)){
+                i--;
+            }
+            else{
+                j++;
+            }
+        }
+        for(int k=i+1;k<j;k++){
             ans.push_back(arr[k]);
         }
         return ans;
