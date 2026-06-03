c29423d2df6a159551d91439ced2ca4275ff8c7c
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-03 19:00:08 +0530

    Time: 0 ms (100%), Space: 8.8 MB (8.25%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        int c=0;
+        // int c=0;
+        // int maxi=0;
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i]=='(') c++;
+        //     else if(s[i]==')') c--;
+        //     maxi=max(maxi,c);
+        // }
+        // return maxi;
+
+
+
+        stack<int>st;
         int maxi=0;
         for(int i=0;i<s.length();i++){
-            if(s[i]=='(') c++;
-            else if(s[i]==')') c--;
-            maxi=max(maxi,c);
+            if(s[i]=='(') st.push('(');
+            else if(s[i]==')'){
+                st.pop();
+            }
+            maxi=max(maxi,(int)st.size());
         }
         return maxi;
     }
