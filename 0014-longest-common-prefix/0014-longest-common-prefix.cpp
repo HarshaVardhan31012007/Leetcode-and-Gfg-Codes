f7712bb52caf9c504dc8e7770e87c3848b6dbeff
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-03 15:43:04 +0530

    Time: 0 ms (100%), Space: 12.3 MB (10.06%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -2,53 +2,41 @@ class Solution {
 public:
     class Trie{
         public:
-        char data;
         Trie* children[26];
         bool isterminal;
-        Trie(char val){
-            this->data=val;
+        int size=0;
+        int firstNodeidx;
+        Trie(){
             for(int i=0;i<26;i++)
             this->children[i]=NULL;
             this->isterminal=false;
+            this->firstNodeidx=-1;
+            this->size=0;
         }
     };
     void insert(Trie* root,string &each){
         for(int i=0;i<each.length();i++){
             if(root->children[each[i]-'a']==NULL){
-                root->children[each[i]-'a']=new Trie(each[i]);
+                root->children[each[i]-'a']=new Trie();
+                root->size++;
+                if(root->firstNodeidx==-1){
+                    root->firstNodeidx=(each[i]-'a');
+                }
             }
             root=root->children[each[i]-'a'];
         }
         root->isterminal=true;
     }
     string longestCommonPrefix(vector<string>& strs) {
-        Trie* root=new Trie('-');
+        Trie* root=new Trie();
         for(auto &each:strs){
            insert(root,each);
         }
-        bool flag=true;
-        Trie* root1=root;
         string ans="";
-        if(root->isterminal) return ans;
-        while(1){
-            Trie* temp=NULL;
-            for(int i=0;i<26;i++){
-                 if(temp==NULL&&root1->children[i])
-                 temp=root1->children[i];
-                 else if(temp&&root1->children[i]){
-                    flag=false;
-                    break;
-                 }
-            }
-            if(temp==NULL) break;
-            if(flag){
-                ans+=temp->data;
-                root1=temp;
-                if(temp->isterminal) return ans;
-            }
-            else{
-                break;
-            }
+        while(root->size==1){
+            if(root->isterminal) return ans;
+            ans+=(root->firstNodeidx+'a');
+            root=root->children[root->firstNodeidx];
         }
         return ans;
     }
