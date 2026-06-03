cefc866b530044dfdb59530e2d4c097bcb973de1
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-03 10:13:09 +0530

    Time: 2 ms (58.45%), Space: 16.9 MB (94.87%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,13 +1,45 @@
 class Solution {
 public:
+    int find(vector<vector<int>>& matrix, int mid){
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int count=0;
+        for(int i=0;i<m;i++){
+            auto it=upper_bound(matrix[i].begin(),matrix[i].end(),mid);
+            count+=it-matrix[i].begin();
+        }
+        return count;
+    }
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int>pq;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                pq.push(matrix[i][j]);
-                if(pq.size()>k) pq.pop();
+        // priority_queue<int>pq;
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j<matrix[0].size();j++){
+        //         pq.push(matrix[i][j]);
+        //         if(pq.size()>k) pq.pop();
+        //     }
+        // }
+        // return pq.top();
+
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int s=INT_MAX;
+        int e=INT_MIN;
+        for(int i=0;i<m;i++){
+            s=min(s,matrix[i][0]);
+            e=max(e,matrix[i][n-1]);
+        }
+        int ans;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            int cnt=find(matrix,mid);
+            if(cnt>=k){
+                ans=mid;
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
             }
         }
-        return pq.top();
+        return ans;
     }
 };
