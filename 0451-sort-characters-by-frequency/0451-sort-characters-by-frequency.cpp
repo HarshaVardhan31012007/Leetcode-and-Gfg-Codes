1cf1078c844da6a8bcd65cf45e19a8673d742a5b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-03 17:00:14 +0530

    Time: 2 ms (62.98%), Space: 10.3 MB (97.42%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        int freq[256]={0};
+        // int freq[256]={0};
+        // for(auto &each:s)
+        // freq[each]++;
+        // sort(s.begin(),s.end(),[&](char a,char b){
+        //     if(freq[a]!=freq[b])
+        //     return freq[a]>freq[b];
+        //     else
+        //     return a>b;
+        // });
+        // return s;
+
+        pair<char,int>freq[256];
+        for(int i=0;i<256;i++){
+            freq[i]={i,0};
+        }
         for(auto &each:s)
-        freq[each]++;
-        sort(s.begin(),s.end(),[&](char a,char b){
-            if(freq[a]!=freq[b])
-            return freq[a]>freq[b];
-            else
-            return a>b;
+        freq[each].second++;
+        sort(freq,freq+256,[&](pair<char,int>&p1,pair<char,int>&p2){
+            if(p1.second!=p2.second) return p1.second>p2.second;
+            else return p1.first>p2.first;
         });
+        int i=0;
+        for(int j=0;j<256;j++){
+            if(freq[j].second!=0){
+                char ch=freq[j].first;
+                while(freq[j].second--){
+                    s[i++]=ch;
+                }
+            }
+        }
         return s;
     }
 };
