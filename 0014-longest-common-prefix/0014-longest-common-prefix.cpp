85b31266988ae478716413469c8be31e85d93ea2
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-03 15:53:38 +0530

    Time: 0 ms (100%), Space: 11.8 MB (59.77%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,42 +1,59 @@
+// class Solution {
+// public:
+//     class Trie{
+//         public:
+//         Trie* children[26];
+//         bool isterminal;
+//         int size=0;
+//         int firstNodeidx;
+//         Trie(){
+//             for(int i=0;i<26;i++)
+//             this->children[i]=NULL;
+//             this->isterminal=false;
+//             this->firstNodeidx=-1;
+//             this->size=0;
+//         }
+//     };
+//     void insert(Trie* root,string &each){
+//         for(int i=0;i<each.length();i++){
+//             if(root->children[each[i]-'a']==NULL){
+//                 root->children[each[i]-'a']=new Trie();
+//                 root->size++;
+//                 if(root->firstNodeidx==-1){
+//                     root->firstNodeidx=(each[i]-'a');
+//                 }
+//             }
+//             root=root->children[each[i]-'a'];
+//         }
+//         root->isterminal=true;
+//     }
+//     string longestCommonPrefix(vector<string>& strs) {
+//         Trie* root=new Trie();
+//         for(auto &each:strs){
+//            insert(root,each);
+//         }
+//         string ans="";
+//         while(root->size==1){
+//             if(root->isterminal) return ans;
+//             ans+=(root->firstNodeidx+'a');
+//             root=root->children[root->firstNodeidx];
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-    class Trie{
-        public:
-        Trie* children[26];
-        bool isterminal;
-        int size=0;
-        int firstNodeidx;
-        Trie(){
-            for(int i=0;i<26;i++)
-            this->children[i]=NULL;
-            this->isterminal=false;
-            this->firstNodeidx=-1;
-            this->size=0;
-        }
-    };
-    void insert(Trie* root,string &each){
-        for(int i=0;i<each.length();i++){
-            if(root->children[each[i]-'a']==NULL){
-                root->children[each[i]-'a']=new Trie();
-                root->size++;
-                if(root->firstNodeidx==-1){
-                    root->firstNodeidx=(each[i]-'a');
-                }
-            }
-            root=root->children[each[i]-'a'];
-        }
-        root->isterminal=true;
-    }
     string longestCommonPrefix(vector<string>& strs) {
-        Trie* root=new Trie();
-        for(auto &each:strs){
-           insert(root,each);
-        }
+        sort(strs.begin(),strs.end());
+        int minlength=min(strs[0].length(),strs[strs.size()-1].length());
         string ans="";
-        while(root->size==1){
-            if(root->isterminal) return ans;
-            ans+=(root->firstNodeidx+'a');
-            root=root->children[root->firstNodeidx];
+        for(int i=0;i<minlength;i++){
+            if(strs[0][i]!=strs[strs.size()-1][i]) break;
+            ans+=strs[0][i];
         }
         return ans;
     }
