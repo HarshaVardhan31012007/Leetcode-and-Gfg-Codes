e74c4f581ac447f9a322daadb5e26d4f0c1f75cc
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-04 22:00:46 +0530

    Time: 5 ms (70.92%), Space: 14.2 MB (39.7%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -22,21 +22,51 @@ public:
 
 
 
+        // int n=s.length();
+        // int i=0;int j=0;
+        // vector<int>dp(n,1);
+        // while(j<n){
+        //     s[i++]=s[j];
+        //     int idx=i-1;
+        //     if(idx>0&&s[idx]==s[idx-1]){
+        //         dp[idx]=dp[idx-1]+1;
+        //     }
+        //     else
+        //     dp[idx]=1;
+        //     if(dp[idx]==k)
+        //     i-=k;
+        //     j++;
+        // }
+        // return s.substr(0,i);
+
+
+
         int n=s.length();
-        int i=0;int j=0;
-        vector<int>dp(n,1);
-        while(j<n){
-            s[i++]=s[j];
-            int idx=i-1;
-            if(idx>0&&s[idx]==s[idx-1]){
-                dp[idx]=dp[idx-1]+1;
+        int i=n-1;int j=n-1;
+        stack<pair<char,int>>st;
+        for(int i=n-1;i>=0;i--){
+            if(st.empty()){
+                st.push({s[i],1});
             }
-            else
-            dp[idx]=1;
-            if(dp[idx]==k)
-            i-=k;
-            j++;
+            else{
+                if(st.top().first==s[i]){
+                    st.top().second++;
+                }
+                else{
+                    st.push({s[i],1});
+                }
+            }
+            if(st.top().second==k) st.pop();
         }
-        return s.substr(0,i);
+        string ans="";
+        while(!st.empty()){
+            char ch=st.top().first;
+            int c=st.top().second;
+            st.pop();
+            while(c--){
+                ans+=ch;
+            }
+        }
+        return ans;
     }
 };
