329aa3633510130e9c68d822b92f287a7307d30f
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-04 22:44:27 +0530

    Time: 8 ms (61.2%), Space: 14 MB (62.54%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -22,51 +22,51 @@ public:
 
 
 
-        // int n=s.length();
-        // int i=0;int j=0;
-        // vector<int>dp(n,1);
-        // while(j<n){
-        //     s[i++]=s[j];
-        //     int idx=i-1;
-        //     if(idx>0&&s[idx]==s[idx-1]){
-        //         dp[idx]=dp[idx-1]+1;
-        //     }
-        //     else
-        //     dp[idx]=1;
-        //     if(dp[idx]==k)
-        //     i-=k;
-        //     j++;
-        // }
-        // return s.substr(0,i);
-
-
-
         int n=s.length();
-        int i=n-1;int j=n-1;
-        stack<pair<char,int>>st;
-        for(int i=n-1;i>=0;i--){
-            if(st.empty()){
-                st.push({s[i],1});
+        int i=0;int j=0;
+        vector<int>dp(n,1);
+        while(j<n){
+            s[i++]=s[j];
+            int idx=i-1;
+            if(idx>0&&s[idx]==s[idx-1]){
+                dp[idx]=dp[idx-1]+1;
             }
-            else{
-                if(st.top().first==s[i]){
-                    st.top().second++;
-                }
-                else{
-                    st.push({s[i],1});
-                }
-            }
-            if(st.top().second==k) st.pop();
+            else
+            dp[idx]=1;
+            if(dp[idx]==k)
+            i-=k;
+            j++;
         }
-        string ans="";
-        while(!st.empty()){
-            char ch=st.top().first;
-            int c=st.top().second;
-            st.pop();
-            while(c--){
-                ans+=ch;
-            }
-        }
-        return ans;
+        return s.substr(0,i);
+
+
+
+        // int n=s.length();
+        // int i=n-1;int j=n-1;
+        // stack<pair<char,int>>st;
+        // for(int i=n-1;i>=0;i--){
+        //     if(st.empty()){
+        //         st.push({s[i],1});
+        //     }
+        //     else{
+        //         if(st.top().first==s[i]){
+        //             st.top().second++;
+        //         }
+        //         else{
+        //             st.push({s[i],1});
+        //         }
+        //     }
+        //     if(st.top().second==k) st.pop();
+        // }
+        // string ans="";
+        // while(!st.empty()){
+        //     char ch=st.top().first;
+        //     int c=st.top().second;
+        //     st.pop();
+        //     while(c--){
+        //         ans+=ch;
+        //     }
+        // }
+        // return ans;
     }
 };
