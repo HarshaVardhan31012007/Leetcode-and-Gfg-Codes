7a6e7196b2baa07e9a65783969945fdf546b3102
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-04 23:07:26 +0530

    Time: 4 ms (18.22%), Space: 9.3 MB (72.03%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // void find(string &s,string &part){
+    //     int idx=s.find(part);
+    //     if(idx==s.length()) return;
+    //     s=s.substr(0,idx)+s.substr(idx+part.length());
+    //     find(s,part);
+    // }
+    // string removeOccurrences(string s, string part) {
+    //     find(s,part);
+    //     cout<<s<<endl;
+    //     return s;
+    // }
+
+
+   
+    string removeOccurrences(string s, string part) {
+        int idx=s.find(part);
+        while(idx!=string::npos){
+            s=s.erase(idx,part.length());
+            idx=s.find(part);
+        }
+        return s;
+    }
+};
