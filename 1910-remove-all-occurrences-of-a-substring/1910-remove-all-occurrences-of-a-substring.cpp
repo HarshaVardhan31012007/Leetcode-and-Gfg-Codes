7710bef671ab47af211ca81af6dc7c7aa0ca391b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-04 23:08:00 +0530

    Time: 3 ms (35.36%), Space: 11.8 MB (16.62%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    // void find(string &s,string &part){
-    //     int idx=s.find(part);
-    //     if(idx==s.length()) return;
-    //     s=s.substr(0,idx)+s.substr(idx+part.length());
-    //     find(s,part);
-    // }
-    // string removeOccurrences(string s, string part) {
-    //     find(s,part);
-    //     cout<<s<<endl;
-    //     return s;
-    // }
+    void find(string &s,string &part){
+        int idx=s.find(part);
+        if(idx==string::npos) return;
+        s=s.substr(0,idx)+s.substr(idx+part.length());
+        find(s,part);
+    }
+    string removeOccurrences(string s, string part) {
+        find(s,part);
+        return s;
+    }
 
 
    
-    string removeOccurrences(string s, string part) {
-        int idx=s.find(part);
-        while(idx!=string::npos){
-            s=s.erase(idx,part.length());
-            idx=s.find(part);
-        }
-        return s;
-    }
+    // string removeOccurrences(string s, string part) {
+    //     int idx=s.find(part);
+    //     while(idx!=string::npos){
+    //         s=s.erase(idx,part.length());
+    //         idx=s.find(part);
+    //     }
+    //     return s;
+    // }
 };
