7bb8f8eb9f689fce83cf64f172de1a6af1a762df
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-05 13:12:34 +0530

    Time: 2 ms (23.55%), Space: 9.3 MB (72.65%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,38 +1,73 @@
 class Solution {
 public:
+    // string decodeString(string s) {
+    //     int n=s.length();
+    //     stack<string>st;
+    //     for(int i=0;i<n;i++){
+    //        if(s[i]==']'){
+    //           string temp="";
+    //           while(!st.empty()&&st.top()!="["){
+    //                temp+=st.top();
+    //                st.pop();
+    //           }
+    //           st.pop();
+    //           int num=0;int c=1;
+    //           while(!st.empty()&&isdigit(st.top()[0])){
+    //              num+=c*(st.top()[0]-'0');
+    //              st.pop();
+    //              c=c*10;
+    //           }
+    //           string temp1="";
+    //           while(num>0){
+    //             temp1+=temp;
+    //             num--;
+    //           }
+    //           st.push(temp1);
+    //        }
+    //        else
+    //        st.push(string(1,s[i]));
+    //     }
+    //     string ans="";
+    //     while(!st.empty()){
+    //         ans+=st.top();
+    //         st.pop();
+    //     }
+    //     reverse(ans.begin(),ans.end());
+    //     return ans;
+    // }
+
+
+    string helper(string &s,int &i,int n){
+        string temp="";
+        while(i<n){
+            int num=0;
+            while(i<s.length()&&isdigit(s[i])){
+                num=num*10+(s[i]-'0');
+                i++;
+            }
+            while(i<s.length()&&islower(s[i])){
+                temp+=s[i];
+                i++;
+            }
+            if(i==n) return temp;
+            if(s[i]=='['){
+                i++;
+                string rec=helper(s,i,n);
+                while(num>0){
+                    temp+=rec;
+                    num--;
+                }
+            }
+            else if(s[i]==']'){
+                i++;
+                return temp;
+            }
+        }
+        return temp;
+    }
     string decodeString(string s) {
         int n=s.length();
-        stack<string>st;
-        for(int i=0;i<n;i++){
-           if(s[i]==']'){
-              string temp="";
-              while(!st.empty()&&st.top()!="["){
-                   temp+=st.top();
-                   st.pop();
-              }
-              st.pop();
-              int num=0;int c=1;
-              while(!st.empty()&&isdigit(st.top()[0])){
-                 num+=c*(st.top()[0]-'0');
-                 st.pop();
-                 c=c*10;
-              }
-              string temp1="";
-              while(num>0){
-                temp1+=temp;
-                num--;
-              }
-              st.push(temp1);
-           }
-           else
-           st.push(string(1,s[i]));
-        }
-        string ans="";
-        while(!st.empty()){
-            ans+=st.top();
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        int i=0;
+        return helper(s,i,n);
     }
 };
