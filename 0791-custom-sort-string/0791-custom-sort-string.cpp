9b9fa12b1efbd4f094c1760ef47e24b9298a47f1
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-05 20:34:35 +0530

    Time: 0 ms (100%), Space: 8.2 MB (69.23%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,11 +1,31 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        sort(s.begin(),s.end(),[&](char &a,char &b){
-            int idx1=order.find(a);
-            int idx2=order.find(b);
-            return idx1<idx2;
-        });
-        return s;
+        // sort(s.begin(),s.end(),[&](char &a,char &b){
+        //     int idx1=order.find(a);
+        //     int idx2=order.find(b);
+        //     return idx1<idx2;
+        // });
+        // return s;
+
+
+        int arr[26]={0};
+        for(int i=0;i<s.length();i++){
+            arr[s[i]-'a']++;
+        }
+        string ans="";
+        for(int i=0;i<order.length();i++){
+            while(arr[order[i]-'a']>0){
+                ans+=order[i];
+                arr[order[i]-'a']--;
+            }
+        }
+        for(int i=0;i<26;i++){
+            while(arr[i]>0){
+                ans+=(i+'a');
+                arr[i]--;
+            }
+        }
+        return ans;
     }
 };
