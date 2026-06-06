a171a1200f683a7f70a5bbed80607c9605d7f95b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-06 14:03:02 +0530

    Time: 0 ms (100%), Space: 10.6 MB (66.85%) - LeetHub

--- 0068-text-justification/0068-text-justification.cpp
+++ 0068-text-justification/0068-text-justification.cpp
@@ -1,118 +1,56 @@
 class Solution {
 public:
-    // vector<string> fullJustify(vector<string>& words, int maxWidth) {
-    //     vector<string>temp;
-    //     vector<string>ans;
-    //     string temp1="";
-    //     int size=0;
-    //     for(int i=0;i<words.size();i++){
-    //          if(size+words[i].length()<maxWidth){
-    //             size+=words[i].length()+1;
-    //             temp.push_back(words[i]);
-    //          }
-    //          else if(size+words[i].length()==maxWidth){
-    //              for(auto &each:temp)
-    //              temp1=temp1+each+" ";
-    //              temp1+=words[i];
-    //              ans.push_back(temp1);
-    //              size=0;
-    //              temp1="";
-    //              temp.clear();
-    //          }
-    //          else{
-    //             size--;
-    //             int extraspaces=maxWidth-size;
-    //             int extra=0;
-    //             if(temp.size()>1)
-    //             extra=extraspaces/(temp.size()-1);
-    //             else
-    //             extra=extraspaces;
-    //             int extraOne=0;
-    //             if(temp.size()>1)
-    //             extraOne=(extraspaces%(temp.size()-1));
-    //             if(temp.size()!=1)
-    //             temp1+=temp[0]+" ";
-    //             else
-    //             temp1+=temp[0];
-    //             for(int i=0;i<extra;i++)
-    //             temp1+=" ";
-    //             if(extraOne>0){
-    //             temp1+=" ";
-    //             extraOne--;
-    //             }
-    //             for(int i=1;i<temp.size()-1;i++){
-    //                 temp1+=temp[i]+" ";
-    //                 for(int j=0;j<extra;j++)
-    //                 temp1+=" ";
-    //                 if(extraOne>0){
-    //                 temp1+=" ";
-    //                 extraOne--;
-    //                 }
-    //             }
-    //             if(temp.size()>1)
-    //             temp1+=temp[temp.size()-1];
-    //             ans.push_back(temp1);
-    //             size=words[i].length()+1;
-    //             temp.clear();
-    //             temp1="";
-    //             temp.push_back(words[i]); 
-    //          }
-    //     }
-    //     if(temp.size()==1)
-    //     temp1+=temp[0];
-    //     else if(!temp.empty()){
-    //          for(int i=0;i<temp.size()-1;i++)
-    //          temp1+=temp[i]+" ";
-    //          if(temp.size()>1)
-    //          temp1+=temp[temp.size()-1];
-    //     }
-    //     if(temp1.length()>0){
-    //     for(int i=temp1.length();i<maxWidth;i++)
-    //     temp1+=" ";
-    //     ans.push_back(temp1);
-    //     }
-    //     return ans;
-    // }
-
-
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        int n=words.size();
         vector<string>ans;
-        vector<string>currLine;
-        int charCount=0;
-        for(int i=0;i<words.size();i++){
-            if(charCount+currLine.size()+words[i].size()>maxWidth){
-                   int extraspaces=maxWidth-charCount;
-                   int spaceBetween=extraspaces/max(1,(int)(currLine.size()-1));
-                   int remainder=extraspaces%max(1,(int)(currLine.size()-1));
-                   for(int j=0;j<currLine.size()-1;j++){
-                      for(int k=0;k<spaceBetween;k++)
-                      currLine[j]+=" ";
-                      if(remainder>0){
-                        currLine[j]+=" ";
-                        remainder--;
-                      }
-                   }
-                   if(currLine.size()==1){
-                    while(extraspaces--)
-                    currLine[0]+=" ";
-                   }
-                   string temp="";
-                   for(auto &each:currLine)
-                   temp+=each;
-                   ans.push_back(temp);
-                   currLine.clear();
-                   charCount=0;
+        int start=-1;
+        int len=0;
+        for(int i=0;i<n;i++){
+            int c=(start==-1?0:1);
+            if(len+c+words[i].length()<=maxWidth){
+                if(start==-1) start=i;
+                len+=c;
+                len+=words[i].length();
             }
-            currLine.push_back(words[i]);
-            charCount+=words[i].length();
+            else{
+                string temp="";
+                string empty="";
+                int nwords=(i-start);
+                if(nwords==1){
+                    string temp=words[start];
+                    while(temp.length()<maxWidth) temp+=" ";
+                    ans.push_back(temp);
+                    len=words[i].length();
+                    start=i;
+                    continue;
+                }
+                int length=(len-(nwords-1));
+                int every=(maxWidth-length)/(nwords-1);
+                int rem=(maxWidth-length)%(nwords-1);
+                for(int i=0;i<every;i++)
+                empty+=" ";
+                for(int j=start;j<i;j++){
+                    temp+=words[j];
+                    if(j<i-1){
+                        temp+=empty;
+                        if(rem>0){
+                            temp+=" ";
+                            rem--;
+                        }
+                    }
+                }
+                ans.push_back(temp);
+                len=words[i].length();
+                start=i;
+            }
         }
         string temp="";
-        for(auto &each:currLine)
-        temp+=each+" ";
-        temp.pop_back();
-        int rightspaces=maxWidth-temp.length();
-        while(rightspaces--)
-        temp+=" ";
+        for(int j=start;j<n;j++){
+            temp+=words[j];
+            if(j<n-1)
+            temp+=" ";
+        }
+        while(temp.length()<maxWidth) temp+=" ";
         ans.push_back(temp);
         return ans;
     }
