d6ff6246975b9a018ad21e53c8eac00bf346cf1e
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-08 10:13:15 +0530

    Time: 10 ms (46.42%), Space: 13.2 MB (12.99%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,41 +8,14 @@
  */
 class Solution {
 public:
-    ListNode *detect(ListNode* &head,ListNode* &prev){
-        ListNode* s=head;
-        ListNode* f=head;
-        while(f!=NULL){
-            f=f->next;
-            if(f!=NULL){
-                prev=s;
-                f=f->next;
-                s=s->next;
-                if(s==f)
-                return s;
-            }
-        }
-        return nullptr;
-    }
     ListNode *detectCycle(ListNode *head) {
-        ListNode *prev=NULL;
-        ListNode *hasCycle=detect(head,prev);
-        if(!hasCycle)
-        return nullptr;
-        ListNode* slow=hasCycle;
-        ListNode* fast=head;
-        while(slow!=fast){
-             prev=slow;
-             slow=slow->next;
-             fast=fast->next;
-         }
-         prev->next=NULL;
-         //or
-        //  ListNode* temp=hasCycle;// it will not work if slow==fast eqaul point and 
-        // loop starting point is same // or complete loop(no node is outside loop)
-        //  while(temp->next!=slow){
-        //     temp=temp->next;
-        //  }
-        //  temp->next=NULL
-         return slow;
+        unordered_set<ListNode*>st;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            if(st.find(temp)!=st.end()) return  temp;
+            st.insert(temp);
+            temp=temp->next;
+        }
+        return NULL;
     }
 };
