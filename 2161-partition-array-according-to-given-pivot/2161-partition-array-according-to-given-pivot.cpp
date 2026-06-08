ac78b702c1939af29c09e6fe50920dbe2ac44730
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-08 21:31:09 +0530

    Time: 7 ms (62.22%), Space: 133.3 MB (68.13%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,50 +1,50 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        // int n=nums.size();
-        // vector<int>ans(n);
-        // int j=0;
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]<pivot){
-        //         ans[j++]=nums[i];
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]==pivot){
-        //         ans[j++]=nums[i];
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]>pivot){
-        //         ans[j++]=nums[i];
-        //     }
-        // }
-        // return ans;
-
-
-
         int n=nums.size();
-        vector<int>ans(n);
-        int countless=0;
-        int countequal=0;
-        for(auto &each:nums){
-            if(each<pivot) countless++;
-            else if(each==pivot) countequal++;
+        vector<int>ans;
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot){
+                ans.push_back(nums[i]);
+            }
         }
-        int i=0;
-        int j=countless;
-        int k=countless+countequal;
-        for(auto &each:nums){
-            if(each<pivot){
-                ans[i++]=each;
+        for(int i=0;i<n;i++){
+            if(nums[i]==pivot){
+                ans.push_back(nums[i]);
             }
-            else if(each==pivot){
-                ans[j++]=each;
-            }
-            else{
-                ans[k++]=each;
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>pivot){
+                ans.push_back(nums[i]);
             }
         }
         return ans;
+
+
+
+        // int n=nums.size();
+        // vector<int>ans(n);
+        // int countless=0;
+        // int countequal=0;
+        // for(auto &each:nums){
+        //     if(each<pivot) countless++;
+        //     else if(each==pivot) countequal++;
+        // }
+        // int i=0;
+        // int j=countless;
+        // int k=countless+countequal;
+        // for(auto &each:nums){
+        //     if(each<pivot){
+        //         ans[i++]=each;
+        //     }
+        //     else if(each==pivot){
+        //         ans[j++]=each;
+        //     }
+        //     else{
+        //         ans[k++]=each;
+        //     }
+        // }
+        // return ans;
     }
 };
