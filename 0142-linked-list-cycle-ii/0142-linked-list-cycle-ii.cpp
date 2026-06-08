e4ee4d8386c8e8ab8492925dd3c58ac42bc8a6ee
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-08 11:03:39 +0530

    Time: 5 ms (83.45%), Space: 11.3 MB (53.32%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,13 +9,29 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*>st;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            if(st.find(temp)!=st.end()) return  temp;
-            st.insert(temp);
-            temp=temp->next;
+        // unordered_set<ListNode*>st;
+        // ListNode* temp=head;
+        // while(temp!=NULL){
+        //     if(st.find(temp)!=st.end()) return  temp;
+        //     st.insert(temp);
+        //     temp=temp->next;
+        // }
+        // return NULL;
+
+
+        if(!head||!head->next) return NULL;
+        ListNode* s=head;ListNode* f=head;
+        while(f&&f->next){
+            s=s->next;
+            f=f->next->next;
+            if(s==f) break;
         }
-        return NULL;
+        if(s!=f) return NULL;
+        s=head;
+        while(s!=f){
+            s=s->next;
+            f=f->next;
+        }
+        return s;
     }
 };
