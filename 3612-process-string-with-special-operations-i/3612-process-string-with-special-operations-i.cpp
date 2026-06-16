39507cd204650ec1e80f6c584edf0aa51cc0de49
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-16 15:43:35 +0530

    Time: 2 ms (79.92%), Space: 39.9 MB (42.05%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string processStr(string s) {
+        int n=s.length();
+        string ans="";
+        for(int i=0;i<n;i++){
+            if(isalpha(s[i])){
+                ans+=s[i];
+            }
+            else if(s[i]=='#'){
+                ans+=ans;
+            }
+            else if(s[i]=='*'){
+                if(!ans.empty()) ans.pop_back();
+            }
+            else
+            reverse(ans.begin(),ans.end());
+        }
+        return ans;
+    }
+};
