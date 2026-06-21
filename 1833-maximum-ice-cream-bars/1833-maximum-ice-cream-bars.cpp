67a0d07dada24a8fd742e153a0977098b0e3d8f6
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-21 09:01:27 +0530

    Time: 41 ms (22.11%), Space: 80.3 MB (61.09%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+        int ans=0;
+        for(auto &each:costs){
+            coins-=each;
+            if(coins<0) break;
+            ans++;
+        }
+        return ans;
+    }
+};
