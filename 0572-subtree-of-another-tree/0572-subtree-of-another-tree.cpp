ac871f955a01b643508bd050e7f04e02bf73e785
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-26 10:42:42 +0530

    Time: 17 ms (5.38%), Space: 42.3 MB (5.24%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,18 +11,36 @@
  */
 class Solution {
 public:
-    bool isSame(TreeNode* root,TreeNode* root1){
-        if(!root&&!root1) return true;
-        if(!root||!root1) return false;
-        if(root->val!=root1->val) return false;
-        return isSame(root->left,root1->left)&&isSame(root->right,root1->right);
+    // bool isSame(TreeNode* root,TreeNode* root1){
+    //     if(!root&&!root1) return true;
+    //     if(!root||!root1) return false;
+    //     if(root->val!=root1->val) return false;
+    //     return isSame(root->left,root1->left)&&isSame(root->right,root1->right);
+    // }
+    // bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+    //     if(!root) return false;
+    //     if(root->val==subRoot->val){
+    //         if(isSame(root,subRoot))
+    //         return true;
+    //     }
+    //     return isSubtree(root->left,subRoot)||isSubtree(root->right,subRoot);
+    // }
+
+
+    string solve(TreeNode* subRoot){
+        if(!subRoot) return "NULL";
+        return to_string(subRoot->val)+" "+solve(subRoot->left)+" "+solve(subRoot->right);
+    }
+    string check(TreeNode* root,string &subtree,bool &ans){
+        if(!root) return "NULL";
+        string temp=(to_string(root->val)+" "+check(root->left,subtree,ans)+" "+check(root->right,subtree,ans));
+        if(!ans&&temp==subtree) ans=true;
+        return temp;
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(!root) return false;
-        if(root->val==subRoot->val){
-            if(isSame(root,subRoot))
-            return true;
-        }
-        return isSubtree(root->left,subRoot)||isSubtree(root->right,subRoot);
+        string subtree=solve(subRoot);
+        bool ans=false;
+        check(root,subtree,ans);
+        return ans;
     }
 };
