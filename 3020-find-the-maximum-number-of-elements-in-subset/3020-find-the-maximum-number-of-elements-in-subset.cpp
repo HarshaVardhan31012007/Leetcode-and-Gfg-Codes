984b693a1eb8f9da29a43c20b67180a8c7ecf4c9
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-27 11:05:15 +0530

    Time: 118 ms (41.71%), Space: 129.1 MB (15.43%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -1,31 +1,64 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
+        // long long n=nums.size();
+        // unordered_map<long long,long long>mpp;
+        // for(auto &each:nums){
+        //    mpp[each]++;
+        // }
+        // long long ans=0;
+        // for(long long i=0;i<n;i++){
+        //     long long x=nums[i];
+        //     if(x==1){
+        //         continue;
+        //     }
+        //     long long p=0;
+        //     while(1){
+        //         if(!mpp.count(x)){
+        //             p--;
+        //             break;
+        //         }
+        //         if(mpp.count(x)&&mpp[x]==1) break;
+        //         x=x*x;
+        //         p++;
+        //     }
+        //     ans=max(ans,2*p+1);
+        // }
+        // if(mpp[1]&1) ans=max(ans,mpp[1]);
+        // else ans=max(ans,mpp[1]-1);
+        // return ans;
+
+
         long long n=nums.size();
         unordered_map<long long,long long>mpp;
+        long long ans=0;
         for(auto &each:nums){
            mpp[each]++;
         }
-        long long ans=0;
+        if(mpp.count(1)){
+           if(mpp[1]&1)
+           ans=max(ans,mpp[1]);
+           else
+           ans=max(ans,mpp[1]-1);
+        }
         for(long long i=0;i<n;i++){
             long long x=nums[i];
             if(x==1){
                 continue;
             }
-            long long p=0;
-            while(1){
-                if(!mpp.count(x)){
-                    p--;
-                    break;
-                }
-                if(mpp.count(x)&&mpp[x]==1) break;
+            long long int p=0;
+            while(mpp[x]>=2){
+                p+=2;
                 x=x*x;
-                p++;
             }
-            ans=max(ans,2*p+1);
+            if(mpp[x]==1){
+                p+=1;
+            }
+            else{
+                p-=1;
+            }
+            ans=max(ans,p);
         }
-        if(mpp[1]&1) ans=max(ans,mpp[1]);
-        else ans=max(ans,mpp[1]-1);
         return ans;
     }
 };
