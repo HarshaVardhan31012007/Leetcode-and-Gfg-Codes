5924abbb47f6ada5a4e425294b8640feacf98dee
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-28 18:55:37 +0530

    Time: 79 ms (96.31%), Space: 192.5 MB (49.59%) - LeetHub

--- 3975-filter-occupied-intervals/3975-filter-occupied-intervals.cpp
+++ 3975-filter-occupied-intervals/3975-filter-occupied-intervals.cpp
@@ -14,28 +14,42 @@ public:
         }
         int size=ans.size();
         vector<vector<int>>temp;
-        int i=0;
-        while(i<size&&ans[i][1]<freeStart){
-            temp.push_back(ans[i]);
-            i++;
-        }
-        if(i==size) return temp;
-        int currS=ans[i][0];
-        int currE=(freeStart-1);
-        if(currS<=currE){
-            temp.push_back({currS,currE});
-        }
-        while(i<size&&ans[i][0]<=freeEnd){
-            currE=ans[i][1];
-            i++;
-        }
-        currS=freeEnd+1;
-        if(currS<=currE){
-            temp.push_back({currS,currE});
-        }
-        while(i<size){
-            temp.push_back(ans[i]);
-            i++;
+        // int i=0;
+        // while(i<size&&ans[i][1]<freeStart){
+        //     temp.push_back(ans[i]);
+        //     i++;
+        // }
+        // if(i==size) return temp;
+        // int currS=ans[i][0];
+        // int currE=(freeStart-1);
+        // if(currS<=currE){
+        //     temp.push_back({currS,currE});
+        // }
+        // while(i<size&&ans[i][0]<=freeEnd){
+        //     currE=ans[i][1];
+        //     i++;
+        // }
+        // currS=freeEnd+1;
+        // if(currS<=currE){
+        //     temp.push_back({currS,currE});
+        // }
+        // while(i<size){
+        //     temp.push_back(ans[i]);
+        //     i++;
+        // }
+
+
+        for(int i=0;i<size;i++){
+            if(ans[i][1]<freeStart||ans[i][0]>freeEnd){
+                temp.push_back(ans[i]);
+                continue;
+            }
+            if(ans[i][0]<freeStart){
+                temp.push_back({ans[i][0],freeStart-1});
+            }
+            if(ans[i][1]>freeEnd){
+                temp.push_back({freeEnd+1,ans[i][1]});
+            }
         }
         return temp;
     }
