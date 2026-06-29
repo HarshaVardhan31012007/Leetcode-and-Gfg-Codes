e8ae288bb889d385dc01b17fd2b227302fa2feb7
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-29 22:17:33 +0530

    Time: 33 ms (33.66%), Space: 65.9 MB (40.51%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.cpp
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -47,34 +47,83 @@
 
 
 
+// class BrowserHistory {
+// public:
+//     vector<string>browserPages;
+//     int current;
+//     BrowserHistory(string homepage) {
+//         browserPages.push_back(homepage);
+//         current=0;
+//     }
+    
+//     void visit(string url) {
+//         int size=browserPages.size();
+//         int noOfDeletes=(size-(current+1));
+//         while(noOfDeletes--){
+//             browserPages.pop_back();
+//         }
+//         browserPages.push_back(url);
+//         current=current+1;
+//     }
+    
+//     string back(int steps) {
+//         current=max(0,current-steps);
+//         return browserPages[current];
+//     }
+    
+//     string forward(int steps) {
+//         int size=browserPages.size();
+//         current=min(size-1,current+steps);
+//         return browserPages[current];
+//     }
+// };
+
+// /**
+//  * Your BrowserHistory object will be instantiated and called as such:
+//  * BrowserHistory* obj = new BrowserHistory(homepage);
+//  * obj->visit(url);
+//  * string param_2 = obj->back(steps);
+//  * string param_3 = obj->forward(steps);
+//  */
+
+
+class Node{
+    public:
+    string data;
+    Node* next;
+    Node* prev;
+    Node(string temp){
+        this->data=temp;
+        this->next=this->prev=NULL;
+    }
+};
 class BrowserHistory {
 public:
-    vector<string>browserPages;
-    int current;
+    Node* head;
+    Node* current;
     BrowserHistory(string homepage) {
-        browserPages.push_back(homepage);
-        current=0;
+        head=new Node(homepage);
+        current=head;
     }
     
     void visit(string url) {
-        int size=browserPages.size();
-        int noOfDeletes=(size-(current+1));
-        while(noOfDeletes--){
-            browserPages.pop_back();
-        }
-        browserPages.push_back(url);
-        current=current+1;
+        current->next=new Node(url);
+        current->next->prev=current;
+        current=current->next;
     }
     
     string back(int steps) {
-        current=max(0,current-steps);
-        return browserPages[current];
+        while(steps--&&current->prev){
+            current=current->prev;
+        }
+        return current->data;
     }
     
     string forward(int steps) {
-        int size=browserPages.size();
-        current=min(size-1,current+steps);
-        return browserPages[current];
+        while(steps--&&current->next){
+            current=current->next;
+        }
+        return current->data;
     }
 };
 
