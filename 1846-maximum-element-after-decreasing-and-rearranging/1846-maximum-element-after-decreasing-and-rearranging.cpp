e404c36c833278ae3f81fc64001fc0eed927d8a0
HARSHA VARDHAN <hv1900019@gmail.com>
2026-06-29 09:29:34 +0530

    Time: 4 ms (88.52%), Space: 57.3 MB (4.48%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
+        // sort(arr.begin(),arr.end());
+        // int n=arr.size();
+        // int ans=0;
+        // if(arr[0]>1){
+        //     arr[0]=1;
+        //     ans++;
+        // }
+        // for(int i=1;i<n;i++){
+        //     if((arr[i]-arr[i-1])<=1){
+        //         continue;
+        //     }
+        //     arr[i]=arr[i-1]+1;
+        //     ans++;
+        // }
+        // return arr[n-1];
+
+
+        
         int n=arr.size();
+        vector<int>freq(n+1,0);
+        for(int i=0;i<n;i++){
+            freq[min(arr[i],n)]++;
+        }
         int ans=0;
-        if(arr[0]>1){
-            arr[0]=1;
-            ans++;
+        for(int i=1;i<=n;i++){
+            if(freq[i]==0) continue;
+             ans=min(ans+freq[i],i);
         }
-        for(int i=1;i<n;i++){
-            if((arr[i]-arr[i-1])<=1){
-                continue;
-            }
-            arr[i]=arr[i-1]+1;
-            ans++;
-        }
-        return arr[n-1];
+        return ans;
     }
 };
