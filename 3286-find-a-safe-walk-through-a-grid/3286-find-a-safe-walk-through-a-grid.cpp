fc6834fada9b40918b404f1763558c921b0e75b1
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-02 10:28:54 +0530

    Time: 24 ms (48.07%), Space: 39.3 MB (23.11%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    // bool solve(vector<vector<int>>&grid,int i,int j,int m,int n,int health){
+    //     if(i<0||j<0||i>=m||j>=n||grid[i][j]==-1||health<=0) return false;
+    //     if(i==m-1&&j==n-1) return (health-grid[i][j])>0;
+    //     int val=grid[i][j];
+    //     grid[i][j]=-1;
+    //     bool ans=solve(grid,i,j-1,m,n,health-val)||solve(grid,i,j+1,m,n,health-val)||solve(grid,i-1,j,m,n,health-val)||solve(grid,i+1,j,m,n,health-val);
+    //     grid[i][j]=val;
+    //     return ans;
+    // }
+    // bool findSafeWalk(vector<vector<int>>& grid, int health) {
+    //     int m=grid.size();
+    //     int n=grid[0].size();
+    //     return solve(grid,0,0,m,n,health);
+    // }
+
+
+
+    
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        // int m=grid.size();
+        // int n=grid[0].size();
+        // priority_queue<vector<int>,vector<vector<int>>,greater<>>pq;
+        // pq.push({grid[0][0],0,0});
+        // vector<vector<int>>minHealth(m,vector<int>(n,INT_MAX));
+        // minHealth[0][0]=grid[0][0];
+        // int dx[]={-1,1,0,0};
+        // int dy[]={0,0,-1,1};
+        // int minH=INT_MAX;
+        // while(!pq.empty()){
+        //     int r=pq.top()[1];
+        //     int c=pq.top()[2];
+        //     int h=pq.top()[0];
+        //     pq.pop();
+        //     if(h!=minHealth[r][c]) continue;
+        //     if(r==(m-1)&&c==(n-1)){
+        //         minH=h;
+        //         break;
+        //     }
+        //     for(int k=0;k<4;k++){
+        //         int newr=r+dx[k];
+        //         int newc=c+dy[k];
+        //         if(newr<0||newc<0||newr>=m||newc>=n) continue;
+        //         if(h+grid[newr][newc]<minHealth[newr][newc]){
+        //             minHealth[newr][newc]=h+grid[newr][newc];
+        //             pq.push({minHealth[newr][newc],newr,newc});
+        //         }
+        //     }
+        // }
+        // return health>minH;
+
+
+
+        int m=grid.size();
+        int n=grid[0].size();
+        deque<vector<int>>q;
+        q.push_back({grid[0][0],0,0});
+        vector<vector<int>>minHealth(m,vector<int>(n,INT_MAX));
+        minHealth[0][0]=grid[0][0];
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
+        int minH=INT_MAX;
+        while(!q.empty()){
+            int r=q.front()[1];
+            int c=q.front()[2];
+            int h=q.front()[0];
+            q.pop_front();
+            if(h!=minHealth[r][c]) continue;
+            if(r==(m-1)&&c==(n-1)){
+                minH=h;
+                break;
+            }
+            for(int k=0;k<4;k++){
+                int newr=r+dx[k];
+                int newc=c+dy[k];
+                if(newr<0||newc<0||newr>=m||newc>=n) continue;
+                if(h+grid[newr][newc]<minHealth[newr][newc]){
+                    minHealth[newr][newc]=h+grid[newr][newc];
+                    if(grid[newr][newc]==0)
+                    q.push_front({minHealth[newr][newc],newr,newc});
+                    else
+                    q.push_back({minHealth[newr][newc],newr,newc});
+                }
+            }
+        }
+        return health>minH;
+    }
+};
