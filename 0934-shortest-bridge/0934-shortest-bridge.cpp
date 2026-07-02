d6b160068156203b9c7877419031823cc14d1335
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-02 12:43:28 +0530

    Time: 12 ms (40.27%), Space: 26.2 MB (29.38%) - LeetHub

---
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int shortestBridge(vector<vector<int>>& grid) {
+        int r,c;
+        int m=grid.size();
+        int n=grid[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    r=i;
+                    c=j;
+                    break;
+                }
+            }
+        }
+        queue<pair<int,int>>q1;
+        queue<vector<int>>q2;
+        q1.push({r,c});
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
+        vector<vector<int>>visited(m,vector<int>(n,0));
+        visited[r][c]=1;
+        while(!q1.empty()){
+            int i=q1.front().first;
+            int j=q1.front().second;
+            q1.pop();
+            bool flag=false;
+            for(int k=0;k<4;k++){
+                int newi=i+dx[k];
+                int newj=j+dy[k];
+                if(newi<0||newj<0||newi>=m||newj>=n||visited[newi][newj]){
+                    continue;
+                }
+                if(grid[newi][newj]==0){
+                    flag=true;
+                    continue;
+                }
+                visited[newi][newj]=1;
+                q1.push({newi,newj});
+            }
+            if(flag){
+                q2.push({0,i,j});
+            }
+        }
+        while(!q2.empty()){
+            int d=q2.front()[0];
+            int i=q2.front()[1];
+            int j=q2.front()[2];
+            q2.pop();
+            for(int k=0;k<4;k++){
+                int newi=i+dx[k];
+                int newj=j+dy[k];
+                if(newi<0||newj<0||newi>=m||newj>=n||visited[newi][newj]==1) continue;
+                visited[newi][newj]=1;
+                if(grid[newi][newj]==1) return d;
+                q2.push({d+1,newi,newj});
+            }
+        }
+        return -1;
+    }
+};
