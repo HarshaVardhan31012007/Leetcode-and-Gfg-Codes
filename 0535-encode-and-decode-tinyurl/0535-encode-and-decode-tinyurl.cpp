d2c409a47a5d68efdff775089d358db5c51e928f
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-03 23:11:05 +0530

    Time: 3 ms (67.07%), Space: 10 MB (62.82%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -2,13 +2,21 @@ class Solution {
 public:
 
     // Encodes a URL to a shortened URL.
+    unordered_map<string,string>mpp;
+    char i=0;
     string encode(string longUrl) {
-        return longUrl;
+        string temp="";
+        temp+=i;i++;
+        mpp[temp]=longUrl;
+        return "http://tinyurl.com/"+temp;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        int index=(int)shortUrl.size()-1;
+        while(shortUrl[index]!='/') index--;
+        string s=shortUrl.substr(index+1);
+        return mpp[s];
     }
 };
 
