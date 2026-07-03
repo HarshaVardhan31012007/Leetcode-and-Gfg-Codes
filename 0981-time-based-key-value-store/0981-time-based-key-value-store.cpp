a53e49e4249e12100b985e459cec7a8d0ccbf500
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-03 20:04:07 +0530

    Time: 67 ms (24.32%), Space: 136.9 MB (66.03%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -11,10 +11,25 @@ public:
     
     string get(string key, int timestamp) {
         if(!mpp.count(key)) return "";
-        auto it=upper_bound(mpp[key].begin(),mpp[key].end(),make_pair(timestamp,"{"));
-        if(it==mpp[key].begin()) return "";
-        it--;
-        return it->second;
+        // auto it=upper_bound(mpp[key].begin(),mpp[key].end(),make_pair(timestamp,"{"));
+        // if(it==mpp[key].begin()) return "";
+        // it--;
+        // return it->second;
+        vector<pair<int,string>>&arr=mpp[key];
+        int s=0;
+        int e=(int)arr.size()-1;
+        string ans="";
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid].first<=timestamp){
+                ans=arr[mid].second;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return ans;
     }
 };
 
