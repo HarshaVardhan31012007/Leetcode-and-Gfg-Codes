c7239ec3d6d46b532ddabf4a70c8fdc5ebca207a
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-04 17:29:43 +0530

    Time: 23 ms (84.74%), Space: 68.1 MB (65.08%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.cpp
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -1,8 +1,45 @@
+// using ll=long long;
+// class UndergroundSystem {
+// public:
+//     unordered_map<ll,pair<ll,string>>checkInStation;
+//     unordered_map<string,unordered_map<string,pair<double,ll>>>avgTimeHelper;
+//     UndergroundSystem() {
+        
+//     }
+    
+//     void checkIn(int id, string stationName, int t) {
+//         checkInStation[id]={t,stationName};
+//     }
+    
+//     void checkOut(int id, string stationName, int t) {
+//         string startStation=checkInStation[id].second;
+//         int startTime=checkInStation[id].first;
+//         checkInStation.erase(id);
+//         avgTimeHelper[startStation][stationName].first+=(t-startTime);
+//         avgTimeHelper[startStation][stationName].second++;
+//     }
+    
+//     double getAverageTime(string startStation, string endStation) {
+//         double t=avgTimeHelper[startStation][endStation].first;
+//         ll c=avgTimeHelper[startStation][endStation].second;
+//         return t/c;
+//     }
+// };
+
+// /**
+//  * Your UndergroundSystem object will be instantiated and called as such:
+//  * UndergroundSystem* obj = new UndergroundSystem();
+//  * obj->checkIn(id,stationName,t);
+//  * obj->checkOut(id,stationName,t);
+//  * double param_3 = obj->getAverageTime(startStation,endStation);
+//  */
+
+
 using ll=long long;
 class UndergroundSystem {
 public:
     unordered_map<ll,pair<ll,string>>checkInStation;
-    unordered_map<string,unordered_map<string,pair<ll,ll>>>avgTimeHelper;
+    unordered_map<string,pair<double,ll>>avgTimeHelper;
     UndergroundSystem() {
         
     }
@@ -14,14 +51,14 @@ public:
     void checkOut(int id, string stationName, int t) {
         string startStation=checkInStation[id].second;
         int startTime=checkInStation[id].first;
-        avgTimeHelper[startStation][stationName].first+=(t-startTime);
-        avgTimeHelper[startStation][stationName].second++;
+        auto &temp=avgTimeHelper[startStation+" "+stationName];
+        temp.first+=(t-startTime);
+        temp.second++;
     }
     
     double getAverageTime(string startStation, string endStation) {
-        ll t=avgTimeHelper[startStation][endStation].first;
-        ll c=avgTimeHelper[startStation][endStation].second;
-        return (double)t/c;
+        auto &temp=avgTimeHelper[startStation+" "+endStation];
+        return temp.first/temp.second;
     }
 };
 
