79b12ca4cbf0d0ec7346b7ae2c7d0080147fe9e8
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-07 19:07:44 +0530

    Time: 11 ms (38.57%), Space: 19 MB (64.88%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -11,47 +11,50 @@
  */
 class Solution {
 public:
-    // int ans=0;
-    // void check(TreeNode *root,int &ans,long long int k){
-    //    if(root==NULL){
-    //     return;
-    //    }
+    int ans=0;
+    void check(TreeNode *root,int &ans,long long int k){
+       if(root==NULL){
+        return;
+       }
     //    if(k==root->val){
     //     ans=ans+1;
     //    }
-    //    k=k-root->val;
-    //    check(root->left,ans,k);
-    //    check(root->right,ans,k);
-    // }
-    // int pathSum(TreeNode* root, long long int targetSum) {
-    //     if(root){
-    //     check(root,ans,targetSum);
-    //     pathSum(root->left,targetSum);
-    //     pathSum(root->right,targetSum);
-    //     }
-    //     return ans;
-    // }
-
-
-    int solve(TreeNode* root,int t,vector<int>&v){
-        if(!root) return 0;
-        v.push_back(root->val);
-        int l=solve(root->left,t,v);
-        int r=solve(root->right,t,v);
-        int curr=0;
-        long long int sum=0;
-        for(int i=v.size()-1;i>=0;i--){
-             sum+=v[i];
-             if(sum==t)
-             curr++;
-        }
-        v.pop_back();
-        return curr+l+r;
+       k=k-root->val;
+       if(k==0){
+          ans++;
+       }
+       check(root->left,ans,k);
+       check(root->right,ans,k);
     }
     int pathSum(TreeNode* root, long long int targetSum) {
-       vector<int>v;
-       return solve(root,targetSum,v);
+        if(root){
+        check(root,ans,targetSum);
+        pathSum(root->left,targetSum);
+        pathSum(root->right,targetSum);
+        }
+        return ans;
     }
+
+
+    // int solve(TreeNode* root,int t,vector<int>&v){
+    //     if(!root) return 0;
+    //     v.push_back(root->val);
+    //     int l=solve(root->left,t,v);
+    //     int r=solve(root->right,t,v);
+    //     int curr=0;
+    //     long long int sum=0;
+    //     for(int i=v.size()-1;i>=0;i--){
+    //          sum+=v[i];
+    //          if(sum==t)
+    //          curr++;
+    //     }
+    //     v.pop_back();
+    //     return curr+l+r;
+    // }
+    // int pathSum(TreeNode* root, long long int targetSum) {
+    //    vector<int>v;
+    //    return solve(root,targetSum,v);
+    // }
 
 
     // int solve(TreeNode* root,int t,unordered_map<long long,int>&visited,long long int &sum){
