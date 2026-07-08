5455311ac6cbe9138efe05abb45cb1e7aadcfb7b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-08 18:36:38 +0530

    Time: 3 ms (16.39%), Space: 8.6 MB (80.31%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -21,25 +21,48 @@ public:
     // }
 
 
-    int solve(string &s,int i,int n,vector<int>&dp){
-        if(i>=n) return 1;
-        if(dp[i]!=-1) return dp[i];
-        int ans=0;
-        int num1=(s[i]-'0');
-        if(num1>=1&&num1<=9){
-            ans+=solve(s,i+1,n,dp);
-        }
+    // int solve(string &s,int i,int n,vector<int>&dp){
+    //     if(i>=n) return 1;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int ans=0;
+    //     int num1=(s[i]-'0');
+    //     if(num1>=1&&num1<=9){
+    //         ans+=solve(s,i+1,n,dp);
+    //     }
 
-        if(i+1<n){
-            int num2=10*(s[i]-'0')+(s[i+1]-'0');
-            if(num2>=10&&num2<=26)
-            ans+=solve(s,i+2,n,dp);
-        }
-        return dp[i]=ans;
-    }
+    //     if(i+1<n){
+    //         int num2=10*(s[i]-'0')+(s[i+1]-'0');
+    //         if(num2>=10&&num2<=26)
+    //         ans+=solve(s,i+2,n,dp);
+    //     }
+    //     return dp[i]=ans;
+    // }
+    // int numDecodings(string s) {
+    //     int n=s.length();
+    //     vector<int>dp(n,-1);
+    //     return solve(s,0,n,dp);
+    // }
+
+
+    
     int numDecodings(string s) {
         int n=s.length();
-        vector<int>dp(n,-1);
-        return solve(s,0,n,dp);
+        vector<int>dp(n+1,0);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            int ans=0;
+            int num1=(s[i]-'0');
+            if(num1>=1&&num1<=9){
+                ans+=dp[i+1];
+            }
+
+            if(i+1<n){
+                int num2=10*(s[i]-'0')+(s[i+1]-'0');
+                if(num2>=10&&num2<=26)
+                ans+=dp[i+2];
+            }
+            dp[i]=ans;
+        }
+        return dp[0];
     }
 };
