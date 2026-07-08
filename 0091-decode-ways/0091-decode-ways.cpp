36bf8119a6c2588c85ff46fc1feffc916067ace2
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-08 18:29:52 +0530

    Time: 0 ms (100%), Space: 8.5 MB (80.31%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
+    // int solve(string &s,int i,int n,vector<int>&dp){
+    //     if(i>=n) return 1;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int num1=stoi(s.substr(i,1));
+    //     int num2=stoi(s.substr(i,2));
+    //     int ans=0;
+    //     if(num1>=1&&num1<=9){
+    //         ans+=solve(s,i+1,n,dp);
+    //     }
+    //     if(num2<=26&&num2>=10){
+    //         ans+=solve(s,i+2,n,dp);
+    //     }
+    //     return dp[i]=ans;
+    // }
+    // int numDecodings(string s) {
+    //     int n=s.length();
+    //     vector<int>dp(n,-1);
+    //     return solve(s,0,n,dp);
+    // }
+
+
     int solve(string &s,int i,int n,vector<int>&dp){
         if(i>=n) return 1;
         if(dp[i]!=-1) return dp[i];
-        int num1=stoi(s.substr(i,1));
-        int num2=stoi(s.substr(i,2));
         int ans=0;
+        int num1=(s[i]-'0');
         if(num1>=1&&num1<=9){
             ans+=solve(s,i+1,n,dp);
         }
-        if(num2<=26&&num2>=10){
+
+        if(i+1<n){
+            int num2=10*(s[i]-'0')+(s[i+1]-'0');
+            if(num2>=10&&num2<=26)
             ans+=solve(s,i+2,n,dp);
         }
         return dp[i]=ans;
