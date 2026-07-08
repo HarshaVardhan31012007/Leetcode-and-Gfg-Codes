2a518e22df091093bf233dca5beff44774ed1877
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-08 06:57:54 +0530

    Time: 0 ms (100%), Space: 16.5 MB (46.54%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // TreeNode* build(vector<int>&preorder,int s,int e){
+    //     if(s>e) return NULL;
+    //     TreeNode* root=new TreeNode(preorder[s]);
+    //     int j=s+1;
+    //     while(j<=e){
+    //         if(preorder[j]>root->val){
+    //             break;
+    //         }
+    //         j++;
+    //     }
+    //     root->left=build(preorder,s+1,j-1);
+    //     root->right=build(preorder,j,e);
+    //     return root;
+    // }
+    // TreeNode* bstFromPreorder(vector<int>& preorder) {
+    //     return build(preorder,0,preorder.size()-1);
+    // }
+
+
+
+    // TreeNode* build(vector<int>&preorder,int &i,int mini,int maxi){
+    //     if(i==preorder.size()||mini>=preorder[i]||preorder[i]>=maxi) return NULL;
+    //     TreeNode* root=new TreeNode(preorder[i++]);
+    //     root->left=build(preorder,i,mini,root->val);
+    //     root->right=build(preorder,i,root->val,maxi);
+    //     return root;
+    // }
+    // TreeNode* bstFromPreorder(vector<int>& preorder) {
+    //     int i=0;
+    //     return build(preorder,i,0,1001);
+    // }
+
+
+
+    TreeNode* build(vector<int>&preorder,int &i,int maxi){
+        if(i==preorder.size()||preorder[i]>=maxi) return NULL;
+        TreeNode* root=new TreeNode(preorder[i++]);
+        root->left=build(preorder,i,root->val);
+        root->right=build(preorder,i,maxi);
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i=0;
+        return build(preorder,i,1001);
+    }
+};
