84e99ddc68ee9527a8bb5cebec261f9653291348
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-20 09:36:24 +0530

    Time: 2 ms (49.52%), Space: 19.3 MB (14.15%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    // vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+    //     int m=grid.size();
+    //     int n=grid[0].size();
+    //     vector<vector<int>>ans(m,vector<int>(n,0));
+    //     for(int i=0;i<m;i++){
+    //         for(int j=0;j<n;j++){
+    //             int newindex=(i*n+j+k)%(m*n);
+    //             int r=newindex/n;
+    //             int c=newindex%n;
+    //             ans[r][c]=grid[i][j];
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<int>temp;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                temp.push_back(grid[i][j]);
+            }
+        }
+        k=k%(m*n);
+        reverse(temp.begin(),temp.end());
+        reverse(temp.begin(),temp.begin()+k);
+        reverse(temp.begin()+k,temp.end());
+        int idx=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                grid[i][j]=temp[idx++];
+            }
+        }
+        return grid;
+    }
+};
