dd114e6c5b3e55c63aa183da853e1cec8070c13e
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-21 14:36:06 +0530

    Time: 47 ms (82.43%), Space: 81 MB (34.23%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -33,34 +33,63 @@
 
 
 
+// class Solution {
+// public:
+//     int maxActiveSectionsAfterTrade(string s) {
+//         int n=s.length();
+//         int pzero=-1;
+//         int pone=-1;
+//         int maxi=0;
+//         int active=0;
+//         for(int i=0;i<n;){
+//             if(s[i]=='0'){
+//                 if(pzero!=-1&&pzero<i-1){
+//                     int c=pzero-pone;
+//                     pone=i-1;
+//                     while(i<n&&s[i]=='0'){
+//                         c++;i++;
+//                     }
+//                     pzero=i-1;
+//                     maxi=max(maxi,c);
+//                 }
+//                 else
+//                 pzero=i++;
+//             }
+//             else{
+//                 active++;
+//                 if(pzero==-1)
+//                 pone=i;
+//                 i++;
+//             }
+//         }
+//         return maxi+active;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
         int n=s.length();
-        int pzero=-1;
-        int pone=-1;
-        int maxi=0;
+        vector<int>inactiveBlocks;
         int active=0;
         for(int i=0;i<n;){
             if(s[i]=='0'){
-                if(pzero!=-1&&pzero<i-1){
-                    int c=pzero-pone;
-                    pone=i-1;
-                    while(i<n&&s[i]=='0'){
-                        c++;i++;
-                    }
-                    pzero=i-1;
-                    maxi=max(maxi,c);
+                int start=i;
+                while(i<n&&s[i]=='0'){
+                    i++;
                 }
-                else
-                pzero=i++;
+                inactiveBlocks.push_back(i-start);
             }
             else{
-                active++;
-                if(pzero==-1)
-                pone=i;
-                i++;
+               i++;
+               active++;
             }
+        }
+        int maxi=0;
+        for(int i=1;i<inactiveBlocks.size();i++){
+            maxi=max(maxi,inactiveBlocks[i]+inactiveBlocks[i-1]);
         }
         return maxi+active;
     }
