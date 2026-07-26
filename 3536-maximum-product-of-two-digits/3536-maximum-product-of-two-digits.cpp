85b3e422391f6a6262fd6c600db2d1c606f3f669
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-26 16:45:26 +0530

    Time: 0 ms (100%), Space: 8.7 MB (59.04%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -3,22 +3,18 @@ public:
     int maxProduct(int n) {
         int maxi=INT_MIN;
         int secondmaxi=INT_MIN;
-        int c=0;
         int x=n;
         while(x){
             int digit=x%10;
-            if(maxi<digit){
+            if(maxi<=digit){
                 secondmaxi=maxi;
                 maxi=digit;
-                c=0;
             }
-            if(maxi==digit) c++;
-            if(digit!=maxi&&secondmaxi<digit){
+            else if(digit>secondmaxi){
                 secondmaxi=digit;
             }
             x/=10;
         }
-        if(c>1) return maxi*maxi;
         return maxi*secondmaxi;
     }
 };
