001fbe87de1aaf8d32e220fbe3d0b641f910dc06
HARSHA VARDHAN <hv1900019@gmail.com>
2026-07-29 00:40:14 +0530

    Time: 347 ms (34.6%), Space: 146.1 MB (39.52%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,3 +1,40 @@
+// class Solution {
+// public:
+//     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+//         vector<set<int>>ans(n);
+//         vector<int>inorder(n,0);
+//         vector<vector<int>>adj(n);
+//         for(auto &each:edges){
+//             adj[each[0]].push_back(each[1]);
+//             inorder[each[1]]++;
+//         }
+//         queue<int>q;
+//         for(int i=0;i<n;i++){
+//             if(inorder[i]==0){
+//                 q.push(i);
+//             }
+//         }
+//         while(!q.empty()){
+//             int node=q.front();
+//             q.pop();
+//             for(auto &adjNode:adj[node]){
+//                 ans[adjNode].insert(ans[node].begin(),ans[node].end());
+//                 ans[adjNode].insert(node);
+//                 inorder[adjNode]--;
+//                 if(inorder[adjNode]==0)
+//                 q.push(adjNode);
+//             }
+//         }
+//         vector<vector<int>>temp;
+//         for(auto &each:ans){
+//             temp.push_back(vector<int>(each.begin(),each.end()));
+//         }
+//         return temp;
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
@@ -14,15 +51,21 @@ public:
                 q.push(i);
             }
         }
+        vector<int>topo;
         while(!q.empty()){
             int node=q.front();
             q.pop();
+            topo.push_back(node);
             for(auto &adjNode:adj[node]){
-                ans[adjNode].insert(ans[node].begin(),ans[node].end());
-                ans[adjNode].insert(node);
                 inorder[adjNode]--;
                 if(inorder[adjNode]==0)
                 q.push(adjNode);
+            }
+        }
+        for(auto &node:topo){
+            for(auto &each:adj[node]){
+                ans[each].insert(ans[node].begin(),ans[node].end());
+                ans[each].insert(node);
             }
         }
         vector<vector<int>>temp;
