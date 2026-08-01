0bb50a3481d9d20a29846ab874849a42f4e7eaeb
HARSHA VARDHAN <hv1900019@gmail.com>
2026-08-01 19:27:21 +0530

    Time: 3 ms (39.21%), Space: 11.3 MB (6.22%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int i,int j,vector<vector<int>>&dp){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=max(nums[i]-solve(nums,i+1,j,dp),nums[j]-solve(nums,i,j-1,dp)); 
+    int solve(vector<int>&nums,int s,int e,int turn,vector<vector<vector<int>>>&dp){
+        if(s==e) return nums[s];
+        if(dp[s][e][turn]!=-1) return dp[s][e][turn];
+        int ans=(turn?INT_MAX:INT_MIN);
+        if(turn){
+            ans=min(ans,solve(nums,s+1,e,!turn,dp));
+            ans=min(ans,solve(nums,s,e-1,!turn,dp));
+        }   
+        else{
+            ans=max(ans,nums[s]+solve(nums,s+1,e,!turn,dp));
+            ans=max(ans,nums[e]+solve(nums,s,e-1,!turn,dp));
+        }
+        return dp[s][e][turn]=ans;
     }
     bool predictTheWinner(vector<int>& nums) {
-       vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-       int ans=solve(nums,0,nums.size()-1,dp);
-       return ans>=0;
+        int n=nums.size();
+        int total=accumulate(nums.begin(),nums.end(),0);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(2,-1)));
+        int player1=solve(nums,0,n-1,0,dp);
+        int player2=total-player1;
+        return player1>=player2;
     }
 };
