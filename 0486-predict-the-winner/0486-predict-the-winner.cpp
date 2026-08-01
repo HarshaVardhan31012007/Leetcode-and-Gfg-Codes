cd8856c9d9b3704ab92563ef1c4fa01c36b72c2f
HARSHA VARDHAN <hv1900019@gmail.com>
2026-08-01 19:29:51 +0530

    Time: 0 ms (100%), Space: 10.3 MB (16.44%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,25 +1,41 @@
+// class Solution {
+// public:
+//     int solve(vector<int>&nums,int s,int e,int turn,vector<vector<vector<int>>>&dp){
+//         if(s==e) return nums[s];
+//         if(dp[s][e][turn]!=-1) return dp[s][e][turn];
+//         int ans=(turn?INT_MAX:INT_MIN);
+//         if(turn){
+//             ans=min(ans,solve(nums,s+1,e,!turn,dp));
+//             ans=min(ans,solve(nums,s,e-1,!turn,dp));
+//         }   
+//         else{
+//             ans=max(ans,nums[s]+solve(nums,s+1,e,!turn,dp));
+//             ans=max(ans,nums[e]+solve(nums,s,e-1,!turn,dp));
+//         }
+//         return dp[s][e][turn]=ans;
+//     }
+//     bool predictTheWinner(vector<int>& nums) {
+//         int n=nums.size();
+//         int total=accumulate(nums.begin(),nums.end(),0);
+//         vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(2,-1)));
+//         int player1=solve(nums,0,n-1,0,dp);
+//         int player2=total-player1;
+//         return player1>=player2;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    int solve(vector<int>&nums,int s,int e,int turn,vector<vector<vector<int>>>&dp){
+    int solve(vector<int>&nums,int s,int e,vector<vector<int>>&dp){
         if(s==e) return nums[s];
-        if(dp[s][e][turn]!=-1) return dp[s][e][turn];
-        int ans=(turn?INT_MAX:INT_MIN);
-        if(turn){
-            ans=min(ans,solve(nums,s+1,e,!turn,dp));
-            ans=min(ans,solve(nums,s,e-1,!turn,dp));
-        }   
-        else{
-            ans=max(ans,nums[s]+solve(nums,s+1,e,!turn,dp));
-            ans=max(ans,nums[e]+solve(nums,s,e-1,!turn,dp));
-        }
-        return dp[s][e][turn]=ans;
+        if(dp[s][e]!=-1) return dp[s][e];
+        return dp[s][e]=max(nums[s]-solve(nums,s+1,e,dp),nums[e]-solve(nums,s,e-1,dp));
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        int total=accumulate(nums.begin(),nums.end(),0);
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(2,-1)));
-        int player1=solve(nums,0,n-1,0,dp);
-        int player2=total-player1;
-        return player1>=player2;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(nums,0,n-1,dp)>=0;
     }
 };
