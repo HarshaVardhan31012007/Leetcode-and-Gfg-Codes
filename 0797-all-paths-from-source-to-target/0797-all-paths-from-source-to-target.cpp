8a185f9183c53c9a7cb3d1f0da8d1f95fb88a11b
HARSHA VARDHAN <hv1900019@gmail.com>
2026-08-02 21:33:26 +0530

    Time: 0 ms (100%), Space: 13.7 MB (78.08%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
+    // void dfs(int s,int e,vector<vector<int>>&graph,vector<int>&temp,vector<vector<int>>&ans){
+    //     temp.push_back(s);
+    //     if(s==e){
+    //         ans.push_back(temp);
+    //         temp.pop_back();
+    //         return;
+    //     }
+    //     for(auto &adjNode:graph[s]){
+    //         dfs(adjNode,e,graph,temp,ans);
+    //     }
+    //     temp.pop_back();
+    // }
+    // vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+    //     vector<vector<int>>ans;
+    //     vector<int>temp;
+    //     int n=graph.size();
+    //     dfs(0,n-1,graph,temp,ans);
+    //     return ans;
+    // }
+
+
+
     void dfs(int s,int e,vector<vector<int>>&graph,vector<int>&temp,vector<vector<int>>&ans){
-        temp.push_back(s);
         if(s==e){
             ans.push_back(temp);
-            temp.pop_back();
             return;
         }
         for(auto &adjNode:graph[s]){
+            temp.push_back(adjNode);
             dfs(adjNode,e,graph,temp,ans);
+            temp.pop_back();
         }
-        temp.pop_back();
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>>ans;
         vector<int>temp;
         int n=graph.size();
+        temp.push_back(0);
         dfs(0,n-1,graph,temp,ans);
         return ans;
     }
