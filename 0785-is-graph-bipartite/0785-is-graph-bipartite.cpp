0284dd583efebc7e25ef6354f28df439fec81e70
HARSHA VARDHAN <hv1900019@gmail.com>
2026-08-02 21:29:30 +0530

    Time: 0 ms (100%), Space: 17.5 MB (58.75%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -24,31 +24,60 @@
 
 
 
+// class Solution {
+// public:
+//     bool possible(int node,int currColor,vector<vector<int>>&graph,vector<int>&color){
+//         queue<int>q;
+//         q.push(node);
+//         color[node]=currColor;
+//         while(!q.empty()){
+//             int fNode=q.front();
+//             q.pop();
+//             for(auto &adjNode:graph[fNode]){
+//                 if(color[adjNode]==color[fNode]) return false;
+//                 if(color[adjNode]==-1){
+//                     color[adjNode]=1-color[fNode];
+//                     q.push(adjNode);
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+//     bool isBipartite(vector<vector<int>>& graph) {
+//         int n=graph.size();
+//         vector<int>color(n,-1);
+//         for(int i=0;i<n;i++){
+//             if(color[i]==-1){
+//                 if(!possible(i,0,graph,color)) return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    bool possible(int node,int currColor,vector<vector<int>>&graph,vector<int>&color){
-        queue<int>q;
-        q.push(node);
-        color[node]=currColor;
-        while(!q.empty()){
-            int fNode=q.front();
-            q.pop();
-            for(auto &adjNode:graph[fNode]){
-                if(color[adjNode]==color[fNode]) return false;
-                if(color[adjNode]==-1){
-                    color[adjNode]=1-color[fNode];
-                    q.push(adjNode);
-                }
-            }
-        }
-        return true;
+    int find(int node,vector<int>&parent){
+        if(node==parent[node]) return node;
+        return parent[node]=find(parent[node],parent);
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int>color(n,-1);
+        vector<int>parent(n,0);
+        for(int i=0;i<n;i++)
+        parent[i]=i;
         for(int i=0;i<n;i++){
-            if(color[i]==-1){
-                if(!possible(i,0,graph,color)) return false;
+            if(graph[i].empty()) continue;
+            int x=find(graph[i][0],parent);
+            for(auto &adjNode:graph[i]){
+               int u=i;
+               int v=adjNode;
+               u=find(u,parent);
+               v=find(v,parent);
+               if(u==v) return false;
+               parent[v]=x;
             }
         }
         return true;
