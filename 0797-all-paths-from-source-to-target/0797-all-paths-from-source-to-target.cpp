5971417b484fe650f6f4129c45e627d1d85d1dd4
HARSHA VARDHAN <hv1900019@gmail.com>
2026-08-02 22:25:56 +0530

    Time: 21 ms (8.22%), Space: 22.9 MB (5.19%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -22,23 +22,47 @@ public:
 
 
 
-    void dfs(int s,int e,vector<vector<int>>&graph,vector<int>&temp,vector<vector<int>>&ans){
-        if(s==e){
-            ans.push_back(temp);
-            return;
-        }
-        for(auto &adjNode:graph[s]){
-            temp.push_back(adjNode);
-            dfs(adjNode,e,graph,temp,ans);
-            temp.pop_back();
-        }
-    }
+    // void dfs(int s,int e,vector<vector<int>>&graph,vector<int>&temp,vector<vector<int>>&ans){
+    //     if(s==e){
+    //         ans.push_back(temp);
+    //         return;
+    //     }
+    //     for(auto &adjNode:graph[s]){
+    //         temp.push_back(adjNode);
+    //         dfs(adjNode,e,graph,temp,ans);
+    //         temp.pop_back();
+    //     }
+    // }
+    // vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+    //     vector<vector<int>>ans;
+    //     vector<int>temp;
+    //     int n=graph.size();
+    //     temp.push_back(0);
+    //     dfs(0,n-1,graph,temp,ans);
+    //     return ans;
+    // }
+
+
+
+    
+    
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>>ans;
-        vector<int>temp;
         int n=graph.size();
-        temp.push_back(0);
-        dfs(0,n-1,graph,temp,ans);
+        queue<vector<int>>q;
+        q.push({0});
+        while(!q.empty()){
+            vector<int>curr=q.front();
+            q.pop();
+            if(curr.back()==(n-1)){
+                ans.push_back(curr);
+            }
+            for(auto &adjNode:graph[curr.back()]){
+                vector<int>path=curr;
+                path.push_back(adjNode);
+                q.push(path);
+            }
+        }
         return ans;
     }
 };
