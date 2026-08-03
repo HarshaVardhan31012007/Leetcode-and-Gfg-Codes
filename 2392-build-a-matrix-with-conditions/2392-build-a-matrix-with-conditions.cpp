24c89ad6d6205b964e822fd5b09bb2f8afdda191
HARSHA VARDHAN <hv1900019@gmail.com>
2026-08-04 01:29:12 +0530

    Time: 9 ms (77.15%), Space: 63.8 MB (43.55%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    bool findtopo(vector<vector<int>>&v,int k,vector<int>&temp){
-        vector<int>inorder(k,-1);
+    bool findtopo(vector<vector<int>>&v,int k,vector<int>&temp,int &idx){
+        vector<int>inorder(k);
         vector<vector<int>>adj(k);
-        int nodes=0;
         for(auto &each:v){
-            if(inorder[each[0]-1]==-1){
-                inorder[each[0]-1]=0;
-                nodes++;
-            }
-            if(inorder[each[1]-1]==-1){
-                inorder[each[1]-1]=0;
-                nodes++;
-            }
             inorder[each[1]-1]++;
             adj[each[0]-1].push_back(each[1]-1);
         }
@@ -21,12 +12,11 @@ public:
             if(inorder[i]==0)
             q.push(i);
         }
-        int idx=0;
         while(!q.empty()){
             int node=q.front();
             q.pop();
             temp[node]=idx++;
-            nodes--;
+            k--;
             for(auto &adjNode:adj[node]){
                 inorder[adjNode]--;
                 if(inorder[adjNode]==0){
@@ -34,20 +24,17 @@ public:
                 }
             }
         }
-        for(auto &each:temp){
-            if(each==-1)
-            each=idx++;
-        }
-        return nodes==0;
+        return k==0;
     }
     vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
         vector<int>row(k,-1),col(k,-1);
         vector<vector<int>>ans(k,vector<int>(k,0));
-        if(!findtopo(rowConditions,k,row)) return {};
-        if(!findtopo(colConditions,k,col)) return {};
+        int idx1=0,idx2=0;
+        if(!findtopo(rowConditions,k,row,idx1)) return {};
+        if(!findtopo(colConditions,k,col,idx2)) return {};
         for(int i=0;i<k;i++){
-            int r=row[i];
-            int c=col[i];
+            int r=(row[i]==-1?idx1:row[i]);
+            int c=(col[i]==-1?idx2:col[i]);
             ans[r][c]=(i+1);
         }
         return ans;
