1cf1e361f72609084af176e7e8c07000e89fdf44
HARSHA VARDHAN <hv1900019@gmail.com>
2026-08-05 01:28:58 +0530

    Time: 299 ms (29.78%), Space: 59.8 MB (76.31%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.cpp
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -33,32 +33,66 @@ public:
 
 
 
+        // int n=routes.size();
+        // unordered_map<int,vector<int>>mpp;
+        // for(int i=0;i<n;i++){
+        //     for(auto &each:routes[i]){
+        //         mpp[each].push_back(i);
+        //     }
+        // }
+        // queue<int>q;
+        // q.push(source);
+        // unordered_set<int>visitedStops;
+        // vector<int>visitedBuses(n,0);
+        // visitedStops.insert(source);
+        // int buses=0;
+        // while(!q.empty()){
+        //     int size=q.size();
+        //     while(size--){
+        //         int stop=q.front();
+        //         q.pop();
+        //         if(stop==target) return buses;
+        //         for(auto &bus:mpp[stop]){
+        //             if(visitedBuses[bus]) continue;
+        //             visitedBuses[bus]=1;
+        //             for(auto &stops:routes[bus]){
+        //                 if(visitedStops.count(stops)) continue;
+        //                 q.push(stops);
+        //                 visitedStops.insert(stops);
+        //             }
+        //         }
+        //     }
+        //     buses++;
+        // }
+        // return -1;
+
+
+
         int n=routes.size();
         unordered_map<int,vector<int>>mpp;
+        queue<int>q;
+        unordered_set<int>visitedStops;
+        vector<int>visitedBuses(n,0);
+        if(source==target) return 0;
         for(int i=0;i<n;i++){
             for(auto &each:routes[i]){
                 mpp[each].push_back(i);
+                if(each==source)
+                q.push(i);
             }
         }
-        queue<int>q;
-        q.push(source);
-        unordered_set<int>visitedStops;
-        vector<int>visitedBuses(n,0);
-        visitedStops.insert(source);
-        int buses=0;
+        int buses=1;
         while(!q.empty()){
             int size=q.size();
             while(size--){
-                int stop=q.front();
+                int bus=q.front();
                 q.pop();
-                if(stop==target) return buses;
-                for(auto &bus:mpp[stop]){
-                    if(visitedBuses[bus]) continue;
-                    visitedBuses[bus]=1;
-                    for(auto &stops:routes[bus]){
-                        if(visitedStops.count(stops)) continue;
-                        q.push(stops);
-                        visitedStops.insert(stops);
+                for(auto &stop:routes[bus]){
+                    if(stop==target) return buses;
+                    for(auto &b:mpp[stop]){
+                        if(visitedBuses[b]) continue;
+                        visitedBuses[b]=1;
+                        q.push(b);
                     }
                 }
             }
