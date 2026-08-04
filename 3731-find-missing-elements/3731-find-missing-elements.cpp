b26d200380ef7549719747fb50e07c3e7aad6bbc
HARSHA VARDHAN <hv1900019@gmail.com>
2026-08-04 13:41:20 +0530

    Time: 0 ms (100%), Space: 32.8 MB (41.55%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> findMissingElements(vector<int>& nums) {
         int maxi=INT_MIN;
         int mini=INT_MAX;
-        unordered_set<int>st;
+        int arr[100]={0};
         for(auto &each:nums){
             maxi=max(maxi,each);
             mini=min(mini,each);
-            st.insert(each);
+            arr[each-1]=1;
         }
         vector<int>ans;
         for(int i=mini+1;i<maxi;i++){
-            if(!st.count(i))
+            if(arr[i-1]==0)
             ans.push_back(i);
         }
         return ans;
