c36765da3d2d85ca7144f8fb81e6bfe4c8c74e90
HARSHA VARDHAN <hv1900019@gmail.com>
2026-08-06 00:40:01 +0530

    Time: 170 ms (74.14%), Space: 304.1 MB (74.14%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -1,33 +1,76 @@
+// class Solution {
+// public:
+//     void dfs(int node,vector<vector<int>>&adj,vector<int>&suspicious,bool &flag,int &color){
+//         suspicious[node]=color;
+//         for(auto &adjNode:adj[node]){
+//             if(!suspicious[adjNode])
+//             dfs(adjNode,adj,suspicious,flag,color);
+//             else if(flag&&suspicious[adjNode]==1)
+//             flag=false;
+//         }
+//     }
+//     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+//         vector<vector<int>>adj(n);
+//         for(auto &each:invocations){
+//             adj[each[0]].push_back(each[1]);
+//         }
+//         vector<int>suspicious(n,0);
+//         bool flag=false;
+//         int color=1;
+//         dfs(k,adj,suspicious,flag,color);
+//         flag=true;
+//         color=2;
+//         for(int i=0;i<n;i++){
+//             if(!suspicious[i])
+//             dfs(i,adj,suspicious,flag,color);
+//         }
+//         vector<int>ans;
+//         for(int i=0;i<n;i++){
+//             if(suspicious[i]==1&&flag)
+//             continue;
+//             ans.push_back(i);
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-    void dfs(int node,vector<vector<int>>&adj,vector<int>&suspicious,bool &flag,int &color){
-        suspicious[node]=color;
-        for(auto &adjNode:adj[node]){
-            if(!suspicious[adjNode])
-            dfs(adjNode,adj,suspicious,flag,color);
-            else if(flag&&suspicious[adjNode]==1)
-            flag=false;
-        }
-    }
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
         vector<vector<int>>adj(n);
+        vector<int>indegree(n,0);
         for(auto &each:invocations){
             adj[each[0]].push_back(each[1]);
+            indegree[each[1]]++;
         }
-        vector<int>suspicious(n,0);
-        bool flag=false;
-        int color=1;
-        dfs(k,adj,suspicious,flag,color);
-        flag=true;
-        color=2;
+        queue<int>q;
+        q.push(k);
+        vector<int>visited(n,0);
+        visited[k]=1;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto &adjNode:adj[node]){
+                indegree[adjNode]--;
+                if(!visited[adjNode]){
+                    visited[adjNode]=1;
+                    q.push(adjNode);
+                }
+            }
+        }
+        bool flag=true;
         for(int i=0;i<n;i++){
-            if(!suspicious[i])
-            dfs(i,adj,suspicious,flag,color);
+            if(visited[i]&&indegree[i]>0){
+                flag=false;
+                break;
+            }
         }
         vector<int>ans;
         for(int i=0;i<n;i++){
-            if(suspicious[i]==1&&flag)
-            continue;
+            if(visited[i]&&flag) continue;
             ans.push_back(i);
         }
         return ans;
